77268ecf6ee96fa5969b60fceccc71c765028958
Yevhen Shyshkin <yshyshkin@magecore.com>
2015-01-13 13:06:36 +0200

    BB-254: Create demo data migration  - small fixed in CSV data  - added demo fixture

---
+++ src/OroB2B/Bundle/ProductBundle/Migrations/Data/Demo/ORM/LoadProductDemoData.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace OroB2B\Bundle\ProductBundle\Migrations\Data\Demo\ORM;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManager;
+
+use Oro\Bundle\UserBundle\Entity\User;
+use OroB2B\Bundle\ProductBundle\Entity\Product;
+
+class LoadProductDemoData extends AbstractFixture implements ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        /** @var EntityManager $manager */
+        $user = $this->getUser($manager);
+        $businessUnit = $user->getOwner();
+        $organization = $user->getOrganization();
+
+        $locator = $this->container->get('file_locator');
+        $filePath = $locator->locate('@OroB2BProductBundle/Migrations/Data/Demo/ORM/data/products.csv');
+        if (is_array($filePath)) {
+            $filePath = current($filePath);
+        }
+
+        $handler = fopen($filePath, "r");
+        $headers = fgetcsv($handler, 1000, ",");
+
+        while (($data = fgetcsv($handler, 1000, ",")) !== false) {
+            $row = array_combine($headers, array_values($data));
+
+            $product = new Product();
+            $product->setOwner($businessUnit)
+                ->setOrganization($organization)
+                ->setSku($row['productCode']);
+
+            $manager->persist($product);
+        }
+
+        fclose($handler);
+
+        $manager->flush();
+    }
+
+    /**
+     * @param EntityManager $manager
+     * @return User
+     * @throws \LogicException
+     */
+    protected function getUser(EntityManager $manager)
+    {
+        $user = $manager->getRepository('OroUserBundle:User')
+            ->createQueryBuilder('user')
+            ->orderBy('user.id', 'ASC')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getSingleResult();
+
+        if (!$user) {
+            throw new \LogicException('There are no users in system');
+        }
+
+        return $user;
+    }
+}
