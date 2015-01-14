b13faefc061e0819dfee3d756c20da3decd4b65d
Vitaliy Berdylo <vberdylo@magecore.com>
2015-01-14 13:00:54 +0200

    BB-255: Create functional tests. - CR fixes

--- src/OroB2B/Bundle/ProductBundle/Tests/Functional/Controller/ProductControllerTest.php
+++ src/OroB2B/Bundle/ProductBundle/Tests/Functional/Controller/ProductControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace OroB2B\Bundle\ProductBundle\Tests\Functional;
+namespace OroB2B\Bundle\ProductBundle\Tests\Functional\Controller;
 
 use Symfony\Component\DomCrawler\Form;
 
@@ -10,8 +10,11 @@ use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
  * @outputBuffering enabled
  * @dbIsolation
  */
-class ControllersTest extends WebTestCase
+class ProductControllerTest extends WebTestCase
 {
+    const TEST_SKU = 'SKU-001';
+    const UPDATED_SKU = 'SKU-001-updated';
+
     protected function setUp()
     {
         $this->initClient([], $this->generateBasicAuthHeader());
@@ -19,19 +22,21 @@ class ControllersTest extends WebTestCase
 
     public function testIndex()
     {
-        $this->client->request('GET', $this->getUrl('orob2b_product_index'));
+        $crawler = $this->client->request('GET', $this->getUrl('orob2b_product_index'));
         $result = $this->client->getResponse();
         $this->assertHtmlResponseStatusCodeEquals($result, 200);
+        $this->assertContains("products-grid", $crawler->html());
     }
 
     public function testCreate()
     {
         $crawler = $this->client->request('GET', $this->getUrl('orob2b_product_create'));
+        $organization = $this->getContainer()->get('security.context')->getToken()->getOrganizationContext()->getId();
 
         /** @var Form $form */
         $form = $crawler->selectButton('Save and Close')->form();
-        $form['orob2b_product_form[sku]'] = 'SKU-001';
-        $form['orob2b_product_form[owner]'] = '1';
+        $form['orob2b_product_form[sku]'] = self::TEST_SKU;
+        $form['orob2b_product_form[owner]'] = $organization;
 
         $this->client->followRedirects(true);
         $crawler = $this->client->submit($form);
@@ -47,17 +52,21 @@ class ControllersTest extends WebTestCase
      */
     public function testUpdate()
     {
-        $response = $this->client->requestGrid('products-grid', ['products-grid[_filter][sku][value]' => 'SKU-001']);
+        $response = $this->client->requestGrid(
+            'products-grid',
+            ['products-grid[_filter][sku][value]' => self::TEST_SKU]
+        );
 
         $result = $this->getJsonResponseContent($response, 200);
         $result = reset($result['data']);
+        $this->assertEquals(self::TEST_SKU, $result['sku']);
 
         $id = $result['id'];
         $crawler = $this->client->request('GET', $this->getUrl('orob2b_product_update', ['id' => $id]));
 
         /** @var Form $form */
         $form = $crawler->selectButton('Save and Close')->form();
-        $form['orob2b_product_form[sku]'] = 'SKU-001-updated';
+        $form['orob2b_product_form[sku]'] = self::UPDATED_SKU;
 
         $this->client->followRedirects(true);
         $crawler = $this->client->submit($form);
@@ -72,18 +81,21 @@ class ControllersTest extends WebTestCase
     /**
      * @depends testUpdate
      * @param int $id
+     * @return int
      */
     public function testView($id)
     {
-        $crawler = $this->client->request('GET', $this->getUrl('orob2b_product_view', array('id' => $id)));
+        $crawler = $this->client->request('GET', $this->getUrl('orob2b_product_view', ['id' => $id]));
 
         $result = $this->client->getResponse();
         $this->assertHtmlResponseStatusCodeEquals($result, 200);
-        $this->assertRegExp("/SKU-001-updated - Products - Product management/", $crawler->html());
+        $this->assertContains('SKU-001-updated - Products - Product management', $crawler->html());
+
+        return $id;
     }
 
     /**
-     * @depends testUpdate
+     * @depends testView
      * @param int $id
      */
     public function testDelete($id)
