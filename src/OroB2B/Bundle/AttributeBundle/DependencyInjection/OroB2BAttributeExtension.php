a118dc70bc66c285a081cd535acb1dc3e1689bdb
Yevhen Shyshkin <yshyshkin@magecore.com>
2015-01-26 20:03:33 +0200

    BB-266: Create formType for fallback selector(system, parent-locale, custom)  - added form_type.yml  - added form type for attribute property fallback

--- src/OroB2B/Bundle/AttributeBundle/DependencyInjection/OroB2BAttributeExtension.php
+++ src/OroB2B/Bundle/AttributeBundle/DependencyInjection/OroB2BAttributeExtension.php
@@ -16,5 +16,6 @@ class OroB2BAttributeExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('form_types.yml');
     }
 }
---
+++ src/OroB2B/Bundle/AttributeBundle/Form/Type/AttributePropertyFallbackType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace OroB2B\Bundle\AttributeBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\Options;
+
+use OroB2B\Bundle\AttributeBundle\Model\FallbackType;
+
+class AttributePropertyFallbackType extends AbstractType
+{
+    const NAME = 'orob2b_attribute_property_fallback';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return self::NAME;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getParent()
+    {
+        return 'choice';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'required'           => false,
+                'empty_value'        => 'orob2b.attribute.form.fallback.none',
+                'enabled_fallbacks'  => [],
+                'existing_fallbacks' => [
+                    FallbackType::PARENT_LOCALE => 'orob2b.attribute.form.fallback.parent_locale',
+                    FallbackType::SYSTEM        => 'orob2b.attribute.form.fallback.system_attribute',
+                ],
+            ]
+        );
+
+        $resolver->setNormalizers(
+            array(
+                'choices' => function (Options $options, $value) {
+                    if (!empty($value)) {
+                        return $value;
+                    }
+
+                    // system fallback is always enabled
+                    $enabledFallbacks = array_merge([FallbackType::SYSTEM], $options['enabled_fallbacks']);
+
+                    $choices = $options['existing_fallbacks'];
+                    foreach (array_keys($choices) as $fallback) {
+                        if (!in_array($fallback, $enabledFallbacks)) {
+                            unset($choices[$fallback]);
+                        }
+                    }
+
+                    return $choices;
+                }
+            )
+        );
+    }
+}
