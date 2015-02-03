3912194908f5ac2bde888564b13e3b8266f9e317
Vitaliy Berdylo <vberdylo@magecore.com>
2015-02-03 12:36:22 +0200

    BB-277: Create validation constraints. - Add unit tests

---
+++ src/OroB2B/Bundle/AttributeBundle/Tests/Unit/Validator/Constraints/GreaterThanZeroTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace OroB2B\Bundle\AttributeBundle\Tests\Unit\Validator\Constraints;
+
+use OroB2B\Bundle\AttributeBundle\Validator\Constraints\GreaterThanZero;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\GreaterThanValidator;
+
+class GreaterThanZeroTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var GreaterThanZero */
+    protected $constraint;
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject|\Symfony\Component\Validator\ExecutionContextInterface */
+    protected $context;
+
+    /** @var \Symfony\Component\Validator\Constraints\GreaterThanValidator */
+    protected $validator;
+
+    protected function setUp()
+    {
+        $this->constraint = new GreaterThanZero();
+        $this->context = $this->getMock('Symfony\Component\Validator\ExecutionContextInterface');
+        $this->validator = new GreaterThanValidator();
+        $this->validator->initialize($this->context);
+    }
+
+    public function testConfiguration()
+    {
+        $this->assertEquals(
+            'Symfony\Component\Validator\Constraints\GreaterThanValidator',
+            $this->constraint->validatedBy()
+        );
+        $this->assertEquals(Constraint::PROPERTY_CONSTRAINT, $this->constraint->getTargets());
+    }
+
+    public function testGetAlias()
+    {
+        $this->assertEquals('greater_than_zero', $this->constraint->getAlias());
+    }
+
+    public function testGetDefaultOption()
+    {
+        $this->assertEquals(null, $this->constraint->getDefaultOption());
+    }
+
+    /**
+     * @dataProvider validateDataProvider
+     * @param mixed $data
+     * @param boolean $correct
+     */
+    public function testValidate($data, $correct)
+    {
+        if (!$correct) {
+            $this->context->expects($this->once())
+                ->method('addViolation')
+                ->with($this->constraint->message);
+        } else {
+            $this->context->expects($this->never())
+                ->method('addViolation');
+        }
+
+        $this->validator->validate($data, $this->constraint);
+    }
+
+    public function validateDataProvider()
+    {
+        return [
+            'correct' => [
+                'data' => 20,
+                'correct' => true
+            ],
+            'not correct' => [
+                'data' => -20,
+                'correct' => false
+            ]
+        ];
+    }
+}
