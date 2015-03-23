ec3c9177cb7d6c1b83b9b7b7a541e096b75eef11
Yevhen Shyshkin <yshyshkin@magecore.com>
2015-03-23 12:41:21 +0200

    BB-349: Set route parameters to Slug entity  - refactored event listener  - added functional test

--- src/OroB2B/Bundle/CMSBundle/EventListener/PageSlugListener.php
+++ src/OroB2B/Bundle/CMSBundle/EventListener/PageSlugListener.php
@@ -2,7 +2,6 @@
 
 namespace OroB2B\Bundle\CMSBundle\EventListener;
 
-use Doctrine\DBAL\Types\Type;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 
 use OroB2B\Bundle\CMSBundle\Entity\Page;
@@ -36,28 +35,19 @@ class PageSlugListener
             return;
         }
 
-        $unitOfWork = $event->getEntityManager()->getUnitOfWork();
-        $connection = $event->getEntityManager()->getConnection();
-
         $expectedRoute = 'orob2b_cms_page_view';
         $expectedParameters = ['id' => $page->getId()];
 
         foreach ($page->getSlugs() as $slug) {
             $actualRoute = $slug->getRouteName();
             $actualParameters = $slug->getRouteParameters();
+
             if ($actualRoute !== $expectedRoute) {
-                $unitOfWork->scheduleExtraUpdate($slug, ['routeName' => [$actualRoute, $expectedRoute]]);
+                $slug->setRouteName($expectedRoute);
             }
+
             if ($actualParameters !== $expectedParameters) {
-                $unitOfWork->scheduleExtraUpdate(
-                    $slug,
-                    [
-                        'routeParameters' => [
-                            $connection->convertToDatabaseValue($actualParameters, Type::TARRAY),
-                            $connection->convertToDatabaseValue($expectedParameters, Type::TARRAY),
-                        ]
-                    ]
-                );
+                $slug->setRouteParameters($expectedParameters);
             }
         }
     }
