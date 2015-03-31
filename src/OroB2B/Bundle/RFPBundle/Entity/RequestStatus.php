1e319a653d05c80bac16d560cdcdaa560a12dea7
Denys Temchenko <denis.temchenko@gmail.com>
2015-03-31 15:49:49 +0000

    BB-377 RFP Status entity translation fix

--- src/OroB2B/Bundle/RFPBundle/Entity/RequestStatus.php
+++ src/OroB2B/Bundle/RFPBundle/Entity/RequestStatus.php
@@ -42,7 +42,15 @@ class RequestStatus implements Translatable
     /**
      * @var string
      *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
      * @ORM\Column(name="label", type="string", length=255)
+     * @Gedmo\Translatable
      */
     private $label;
 
@@ -58,7 +66,7 @@ class RequestStatus implements Translatable
      *
      * @ORM\Column(name="deleted", type="boolean")
      */
-    private $deleted;
+    private $deleted = false;
 
     /**
      * @var string
@@ -66,7 +74,6 @@ class RequestStatus implements Translatable
      * @Gedmo\Locale()
      */
     protected $locale;
-
 
     /**
      * Get id
@@ -76,6 +83,29 @@ class RequestStatus implements Translatable
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return RequestStatus
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
     }
 
     /**
