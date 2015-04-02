1c498094233cbca679440df78626a7f2703222a3
Yevhen Shyshkin <yshyshkin@magecore.com>
2015-04-02 16:00:36 +0300

    BB-361: Refactor directory structure of /app/ directory for admin and frontend  - added functional test for cache clear command

---
+++ src/Oro/Bundle/ApplicationBundle/Tests/Functional/Command/CacheClearCommandTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Oro\Bundle\ApplicationBundle\Tests\Functional\Command;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\NullOutput;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+
+class CacheClearCommandTest extends WebTestCase
+{
+    public function testExecute()
+    {
+        $this->initClient();
+
+        /** @var KernelInterface $kernel */
+        $kernel = $this->getContainer()->get('kernel');
+        $application = new Application($kernel);
+
+        /** @var Bundle $bundle */
+        foreach ($kernel->getBundles() as $bundle) {
+            $bundle->registerCommands($application);
+        }
+
+        $command = $application->find('cache:clear');
+
+        // make sure that command is from current bundle and it executes without errors
+        $this->assertInstanceOf('Oro\Bundle\ApplicationBundle\Command\CacheClearCommand', $command);
+        $this->assertEquals(0, $command->run(new ArgvInput(['application', 'cache:clear']), new NullOutput()));
+    }
+}
