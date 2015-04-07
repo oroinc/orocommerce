706e207c94989e4da61ecb7aa242a2183f93179c
Vitaliy Berdylo <vberdylo@magecore.com>
2015-04-07 16:14:28 +0300

    BB-362: Refactor directory structure of /app/ directory for installer - CR fixes

--- src/Oro/Bundle/ApplicationBundle/Command/CacheClearCommand.php
+++ src/Oro/Bundle/ApplicationBundle/Command/CacheClearCommand.php
@@ -3,10 +3,45 @@
 namespace Oro\Bundle\ApplicationBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\CacheClearCommand as SymfonyCacheClearCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
+
+use Oro\Bundle\InstallerBundle\CommandExecutor;
 
 class CacheClearCommand extends SymfonyCacheClearCommand
 {
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->getApplication()->getDefinition()->getOption('app')->setDefault(null);
+        if (!$input->getOption('app')) {
+            $hosts = array_keys($this->getContainer()->getParameter('application_hosts'));
+            $commandExecutor = new CommandExecutor(
+                $input->hasOption('env') ? $input->getOption('env') : null,
+                $output,
+                $this->getApplication(),
+                $this->getContainer()->get('oro_cache.oro_data_cache_manager')
+            );
+            foreach ($hosts as $host) {
+                $output->write($host.': ');
+                $commandExecutor->runCommand(
+                    'cache:clear',
+                    array(
+                        '--app'                 => $host,
+                        '--no-optional-warmers' => $input->getOption('no-optional-warmers'),
+                        '--no-warmup' => $input->getOption('no-warmup'),
+                        '--process-isolation'   => true
+                    )
+                );
+            }
+        } else {
+            parent::execute($input, $output);
+        }
+    }
+
     /**
      * @inheritdoc
      */
