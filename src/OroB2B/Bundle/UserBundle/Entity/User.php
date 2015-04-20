5df314540a7cc0135095fd9c04d2a75e67e227f4
Vitaliy Berdylo <vberdylo@magecore.com>
2015-04-20 16:49:30 +0300

    BB-414: Create OroB2BUserBundle

--- src/OroB2B/Bundle/UserBundle/Entity/User.php
+++ src/OroB2B/Bundle/UserBundle/Entity/User.php
@@ -21,7 +21,7 @@ class User extends BaseUser
 
     /**
      * @ORM\ManyToMany(targetEntity="OroB2B\Bundle\UserBundle\Entity\Group")
-     * @ORM\JoinTable(name="orob2b_user_user_group",
+     * @ORM\JoinTable(name="orob2b_user_group",
      *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
      *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
      * )
