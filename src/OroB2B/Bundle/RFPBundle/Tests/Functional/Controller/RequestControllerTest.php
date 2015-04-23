e1b4eee1a212411729d6c9d42bcc88b3f0e685c1
Pavel Romanenko <me@pavlunya.com>
2015-04-23 18:42:53 +0300

    BB-407: Create functional tests for request submission - created WebTestCase - modified RequestControllerTest

--- src/OroB2B/Bundle/RFPBundle/Tests/Functional/Controller/RequestControllerTest.php
+++ src/OroB2B/Bundle/RFPBundle/Tests/Functional/Controller/RequestControllerTest.php
@@ -2,78 +2,22 @@
 
 namespace OroB2B\Bundle\RFPBundle\Tests\Functional\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Bundle\FrameworkBundle\Client;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpKernel\KernelInterface;
+use OroB2B\Bundle\FrontendBundle\Test\WebTestCase;
 
 class RequstControllerTest extends WebTestCase
 {
-    /**
-     * @var Client
-     */
-    protected $client;
-
-    /**
-     * @var ContainerInterface
-     */
-    protected static $container;
-
-    /**
-     * @return ContainerInterface
-     */
-    public function getContainer()
-    {
-        return static::$container;
-    }
-
-    /**
-     * @return Client
-     */
-    public function getClient()
-    {
-        return $this->client;
-    }
-
-    /**
-     * @return KernelInterface
-     */
-    public function getKernel()
-    {
-        return static::$kernel;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected static function createClient(array $options = array(), array $server = array())
-    {
-        if (null !== static::$kernel) {
-            static::$kernel->shutdown();
-        }
-
-        static::$kernel = static::createKernel($options);
-        static::$kernel->setApplication('frontend');
-        static::$kernel->boot();
-
-        static::$container = static::$kernel->getContainer();
-
-        $client = static::$kernel->getContainer()->get('test.client');
-        $client->setServerParameters($server);
-
-        return $client;
-    }
-
     /**
      * {@inheritdoc}
      */
     protected function setUp()
     {
-        $this->client = $this->createClient();
+        $this->initClient();
     }
 
     public function testLol()
     {
-        var_dump($this->getContainer()->getParameter('orob2b_rfp.form.type.request.class'));
+        $crawler = $this->client->request('GET', $this->getUrl('orob2b_rfp_request_create'));
+
+        echo $crawler->html();
     }
 }
