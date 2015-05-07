41a8501f1308e1df3f920b4b20a0cc763e5465a3
Yevhen Shyshkin <yshyshkin@magecore.com>
2015-05-07 19:54:57 +0300

    BB-449: Create CustomerBundle and CustomerAdminBundle  - fixed ORM annotations

--- src/OroB2B/Bundle/CustomerBundle/Entity/CustomerGroup.php
+++ src/OroB2B/Bundle/CustomerBundle/Entity/CustomerGroup.php
@@ -5,6 +5,15 @@ namespace OroB2B\Bundle\CustomerBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\Collection;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(
+ *      name="orob2b_customer_group",
+ *      indexes={
+ *          @ORM\Index(name="orob2b_customer_group_name_idx", columns={"name"})
+ *      }
+ * )
+ */
 class CustomerGroup extends AbstractCustomerGroup
 {
     /**
