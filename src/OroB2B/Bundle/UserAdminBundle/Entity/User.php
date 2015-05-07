01db290b44960ed01449f8df433f02ae43b88f41
Vitaliy Berdylo <vberdylo@magecore.com>
2015-05-07 12:14:24 +0300

    BB-418 Update CRUD Users - Add welcome email - Add password generator

--- src/OroB2B/Bundle/UserAdminBundle/Entity/User.php
+++ src/OroB2B/Bundle/UserAdminBundle/Entity/User.php
@@ -4,11 +4,26 @@ namespace OroB2B\Bundle\UserAdminBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
+
 use OroB2B\Bundle\UserBundle\Entity\AbstractUser as BaseUser;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="orob2b_user")
+ * @Config(
+ *      routeName="orob2b_user_admin_user_index",
+ *      routeView="orob2b_user_admin_user_view",
+ *      defaultValues={
+ *          "entity"={
+ *              "icon"="icon-briefcase"
+ *          },
+ *          "security"={
+ *              "type"="ACL",
+ *              "group_name"=""
+ *          }
+ *      }
+ * )
  * @SuppressWarnings(PHPMD.TooManyFields)
  */
 class User extends BaseUser
