3a63067805a527e7d1934f9f32675e0f5f69fad2
Mykhailo Sulyma <msulima@magecore.com>
2015-05-18 18:50:09 +0300

    BB-496: Implement rounding  - added system configuration for product unit rounding type  - added rounding service  - added tests

---
+++ src/OroB2B/Bundle/ProductBundle/Tests/Unit/Rounding/RoundingServiceTest.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace OroB2B\Bundle\ProductBundle\Tests\Unit\Rounding;
+
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
+
+use OroB2B\Bundle\ProductBundle\Rounding\RoundingService;
+
+class RoundingServiceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var RoundingService
+     */
+    protected $service;
+
+    /**
+     * @var ConfigManager|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $configManager;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->configManager = $this->getMockBuilder('Oro\Bundle\ConfigBundle\Config\ConfigManager', ['get'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->service = new RoundingService($this->configManager);
+    }
+
+    /**
+     * @dataProvider roundProvider
+     *
+     * @param string $roundingType
+     * @param float|integer $value
+     * @param integer $precision
+     * @param float|integer $expectedValue
+     */
+    public function testRound($roundingType, $value, $precision, $expectedValue)
+    {
+        $this->configManager->expects($this->once())
+            ->method('get')
+            ->with('orob2b_product.unit_rounding_type')
+            ->willReturn($roundingType);
+
+        $this->assertEquals($expectedValue, $this->service->round($value, $precision));
+    }
+
+    /**
+     * @return array
+     */
+    public function roundProvider()
+    {
+        return [
+            'half_up more then half fraction' => [
+                'roundingType' => RoundingService::HALF_UP,
+                'value' => 5.5555,
+                'precision' => 3,
+                'expectedValue' => 5.556,
+            ],
+            'half_up less then half fraction' => [
+                'roundingType' => RoundingService::HALF_UP,
+                'value' => 5.5554,
+                'precision' => 3,
+                'expectedValue' => 5.555,
+            ],
+            'half_down more then half fraction' => [
+                'roundingType' => RoundingService::HALF_DOWN,
+                'value' => 5.5555,
+                'precision' => 3,
+                'expectedValue' => 5.555,
+            ],
+            'half_down less then half fraction' => [
+                'roundingType' => RoundingService::HALF_DOWN,
+                'value' => 5.5554,
+                'precision' => 3,
+                'expectedValue' => 5.555,
+            ],
+            'ceil' => [
+                'roundingType' => RoundingService::CEIL,
+                'value' => 5.5555,
+                'precision' => 3,
+                'expectedValue' => 6,
+            ],
+            'floor' => [
+                'roundingType' => RoundingService::FLOOR,
+                'value' => 5.5555,
+                'precision' => 3,
+                'expectedValue' => 5,
+            ]
+        ];
+    }
+
+    /**
+     * @expectedException \OroB2B\Bundle\ProductBundle\Exception\InvalidRoundingTypeException
+     * @expectedExceptionMessage The type of the rounding is not valid.
+     *                           Allowed the following types: half_up, half_down, ceil, floor.
+     */
+    public function testInvalidRoundingTypeException()
+    {
+        $this->configManager->expects($this->once())
+            ->method('get')
+            ->with('orob2b_product.unit_rounding_type')
+            ->willReturn('test');
+
+        $this->service->round(1.15, 1);
+    }
+}
