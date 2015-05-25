a81d70ab34167e3279f69c9fbca43b6b7e8f28aa
Yauheni Kasenka <beta.screech@gmail.com>
2015-05-25 11:39:30 +0300

    BB-501:  Quotes 1 - Updated SaleExtensionTest, added missing services

--- src/OroB2B/Bundle/SaleBundle/Tests/Unit/DependencyInjection/OroB2BSaleExtensionTest.php
+++ src/OroB2B/Bundle/SaleBundle/Tests/Unit/DependencyInjection/OroB2BSaleExtensionTest.php
@@ -13,16 +13,19 @@ class OroB2BSaleExtensionTest extends ExtensionTestCase
 
         $expectedParameters = [
             'orob2b_sale.quote.class',
+            'orob2b_sale.form.type.quote.class',
             'orob2b_sale.form.handler.quote.class',
             'orob2b_sale.quote.manager.api.class',
-
+            'orob2b_sale.doctrine.subscriber.entity.class',
         ];
         $this->assertParametersLoaded($expectedParameters);
 
         $expectedDefinitions = [
             'orob2b_sale.form.quote',
+            'orob2b_sale.form.type.quote',
             'orob2b_sale.form.handler.quote',
             'orob2b_sale.quote.manager.api',
+            'orob2b_sale.doctrine.subscriber.entity',
         ];
         $this->assertDefinitionsLoaded($expectedDefinitions);
     }
