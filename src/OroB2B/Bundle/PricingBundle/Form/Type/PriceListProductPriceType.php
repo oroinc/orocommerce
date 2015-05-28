5b63a73a25943565474c7f1949008e6911e01962
Dmitry Khrysev <x86demon@gmail.com>
2015-05-28 20:00:49 +0300

    BB-533: Implement ProductPrice form type for price list product prices management

---
+++ src/OroB2B/Bundle/PricingBundle/Form/Type/PriceListProductPriceType.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace OroB2B\Bundle\PricingBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Oro\Bundle\CurrencyBundle\Form\Type\PriceType;
+
+use OroB2B\Bundle\PricingBundle\Entity\ProductPrice;
+use OroB2B\Bundle\ProductBundle\Form\Type\ProductSelectType;
+use OroB2B\Bundle\ProductBundle\Form\Type\ProductUnitSelectionType;
+
+class PriceListProductPriceType extends AbstractType
+{
+    const NAME = 'orob2b_pricing_price_list_product_price';
+
+    /**
+     * @var string
+     */
+    protected $dataClass;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        /** @var ProductPrice $data */
+        $data = $builder->getData();
+        $isNew = $data && $data->getId();
+        $hasProduct = $data && $data->getProduct();
+
+        $builder
+            ->add(
+                'product',
+                ProductSelectType::NAME,
+                [
+                    'required' => true,
+                    'label' => 'orob2b.pricing.productprice.product.label',
+                    'disabled' => !$isNew,
+                    'create_enabled' => false
+                ]
+            )
+            ->add(
+                'quantity',
+                'text',
+                [
+                    'required' => true,
+                    'label' => 'orob2b.pricing.productprice.quantity.label',
+                    'disabled' => !$hasProduct
+                ]
+            )
+            ->add(
+                'unit',
+                ProductUnitSelectionType::NAME,
+                [
+                    'required' => true,
+                    'label' => 'orob2b.pricing.productprice.unit.label',
+                    'disabled' => !$hasProduct
+                ]
+            )
+            ->add(
+                'price',
+                PriceType::NAME,
+                [
+                    'required' => true,
+                    'label' => 'orob2b.pricing.productprice.unit.label'
+                ]
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'OroB2B\Bundle\PricingBundle\Entity\ProductPrice', //$this->dataClass
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return self::NAME;
+    }
+
+    /**
+     * @param string $productClass
+     * @return PriceListProductPriceType
+     */
+    public function setDataClass($productClass)
+    {
+        $this->dataClass = $productClass;
+
+        return $this;
+    }
+}
