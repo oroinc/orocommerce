efdd31494020221c1672bd3c197692e1b6e4ea50
Yauheni Kasenka <beta.screech@gmail.com>
2015-05-29 16:10:44 +0300

    BB-561: Create View page - created orob2b_format_sale_quote_product_item twig filter - updated view page - moved service configs to service/ folder

---
+++ src/OroB2B/Bundle/SaleBundle/Twig/QuoteExtension.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace OroB2B\Bundle\SaleBundle\Twig;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Oro\Bundle\UIBundle\Twig;
+use Oro\Bundle\TranslationBundle\Translation\Translator;
+
+use OroB2B\Bundle\SaleBundle\Entity\QuoteProductItem;
+
+class QuoteExtension extends \Twig_Extension
+{
+    const NAME = 'orob2b_sale_quote';
+
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter(
+                'orob2b_format_sale_quote_product_item',
+                [$this, 'formatProductItem'],
+                ['is_safe' => ['html']]
+            ),
+        ];
+    }
+
+    /**
+     * @param QuoteProductItem $item
+     * @return string
+     */
+    public function formatProductItem(QuoteProductItem $item)
+    {
+        /* @var $translator Translator */
+        $translator = $this->container->get('translator');
+
+        /* @var $twig Twig\Environment */
+        $twig = $this->container->get('twig');
+
+        $unitFormatter  = $twig->getFilter('orob2b_format_product_unit_value')->getCallable();
+        $priceFormatter = $twig->getFilter('oro_format_price')->getCallable();
+
+        $units  = $unitFormatter($item->getQuantity(), $item->getProductUnit());
+        $price  = $priceFormatter($item->getPrice());
+        $unit   = $translator->trans(sprintf('orob2b.product_unit.%s.label.full', $item->getProductUnit()->getCode()));
+
+        $str = $translator->trans(
+            'orob2b.sale.quote.quoteproduct.quoteproductitem.product_unit.item',
+            [
+                '{units}'   => $units,
+                '{price}'   => $price,
+                '{unit}'    => $unit,
+            ]
+        );
+
+        return $str;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return static::NAME;
+    }
+}
