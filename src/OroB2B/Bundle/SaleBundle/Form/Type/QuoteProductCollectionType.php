d85d905576a43f6fa32751090ba46ce3ad2e7757
Ivan Shirko <ivan.shirko@effective-soft.com>
2015-06-01 17:06:55 +0300

    BB-560: Form Validation - Add JS-validation for ProductUnit

---
+++ src/OroB2B/Bundle/SaleBundle/Form/Type/QuoteProductCollectionType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace OroB2B\Bundle\SaleBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class QuoteProductCollectionType extends AbstractType
+{
+    const NAME = 'orob2b_sale_quote_product_collection';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'oro_collection';
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults([
+            'type' => QuoteProductType::NAME,
+            'show_form_when_empty' => false
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return self::NAME;
+    }
+}
--- src/OroB2B/Bundle/SaleBundle/Resources/public/js/app/components/quote-product-item-unit-selection-limitations-component.js
+++ src/OroB2B/Bundle/SaleBundle/Resources/public/js/app/components/quote-product-item-unit-selection-limitations-component.js
@@ -6,6 +6,7 @@ define(function (require) {
     var QuoteProductItemUnitSelectionLimitationsComponent,
         $ = require('jquery'),
         _ = require('underscore'),
+        routing = require('routing'),
         BaseComponent = require('oroui/js/app/components/base/component');
 
     QuoteProductItemUnitSelectionLimitationsComponent = BaseComponent.extend({
@@ -17,8 +18,51 @@ define(function (require) {
         /**
          * @property {Object}
          */
-        $addButton : null
+        $productSelect : null,
 
+        /**
+         * @inheritDoc
+         */
+        initialize: function (options) {
+            var containerId = options['containerId'];
+            if (!containerId) {
+                return;
+            }
+
+            this.$container = $(containerId);
+
+            this.$productSelect = this.$container.closest('.quoteproduct-form').find('select.quoteproduct-product-select');
+            this.$productSelect.on('change', _.bind(this.onChange, this));
+        },
+
+        /**
+         * Handle change select
+         */
+        onChange: function () {
+            var self = this;
+            var productId = self.$productSelect.val();
+            $.get(routing.generate('orob2b_api_get_product_available_units', {'id': productId}))
+                .done(function(data) {
+                    if (!data.successful) {
+                        return;
+                    }
+                    var newOptions = data.data;
+                    var selects = self.$container.find('.quoteproduct-productunit-select');
+
+                    $.each(selects, function(index, select) {
+                        var currentValue = $(select).val();
+                        $(select).empty();
+                        $.each(newOptions, function(key, value) {
+                            $(select)
+                                .append($('<option></option>')
+                                .attr('value', key).text(value))
+                            ;
+                        });
+                        $(select).val(currentValue);
+                        $(select).uniform('update');
+                    });
+                });
+        }
     });
 
     return QuoteProductItemUnitSelectionLimitationsComponent;
