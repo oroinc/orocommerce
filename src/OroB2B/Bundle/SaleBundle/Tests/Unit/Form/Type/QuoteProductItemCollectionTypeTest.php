b40b13f8211e44df243991cde7a94dc4f77f6106
Yauheni Kasenka <beta.screech@gmail.com>
2015-06-02 20:31:49 +0300

    BB-508:  Unit and Functional tests - updated service name for QuoteProductItem validator - added missing parameters and services to SaleExtension test - added tests for quote collections types

---
+++ src/OroB2B/Bundle/SaleBundle/Tests/Unit/Form/Type/QuoteProductItemCollectionTypeTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace OroB2B\Bundle\SaleBundle\Tests\Unit\Form\Type;
+
+use OroB2B\Bundle\SaleBundle\Form\Type\QuoteProductItemType;
+use OroB2B\Bundle\SaleBundle\Form\Type\QuoteProductItemCollectionType;
+use OroB2B\Bundle\SaleBundle\Validator\Constraints;
+
+class QuoteProductItemCollectionTypeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var QuoteProductItemCollectionType
+     */
+    protected $type;
+
+    protected function setUp()
+    {
+        $this->type = new QuoteProductItemCollectionType();
+    }
+
+    public function testSetDefaultOptions()
+    {
+        $resolver = $this->getMock('Symfony\Component\OptionsResolver\OptionsResolverInterface');
+        $resolver->expects($this->once())
+            ->method('setDefaults')
+            ->with([
+                'type'  => QuoteProductItemType::NAME,
+                'show_form_when_empty' => false,
+                'constraints' => [
+                    new Constraints\QuoteProductItems(),
+                ],
+            ])
+        ;
+
+        $this->type->setDefaultOptions($resolver);
+    }
+
+    public function testGetParent()
+    {
+        $this->assertEquals('oro_collection', $this->type->getParent());
+    }
+
+    public function testGetName()
+    {
+        $this->assertEquals(QuoteProductItemCollectionType::NAME, $this->type->getName());
+    }
+}
