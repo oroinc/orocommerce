33c7a199f664e473f98b6fde9a1db46cf3765243
Dmitry Khrysev <x86demon@gmail.com>
2015-06-02 11:41:19 +0300

    BB-436: Product Prices - fixed unit tests

--- src/OroB2B/Bundle/PricingBundle/Tests/Unit/Form/Type/Stub/PriceListSelectTypeStub.php
+++ src/OroB2B/Bundle/PricingBundle/Tests/Unit/Form/Type/Stub/PriceListSelectTypeStub.php
@@ -24,7 +24,8 @@ class PriceListSelectTypeStub extends AbstractType
     {
         $resolver->setDefaults([
             'class' => 'OroB2B\Bundle\PricingBundle\Entity\PriceList',
-            'property' => 'name'
+            'property' => 'name',
+            'create_enabled' => false
         ]);
     }
 
