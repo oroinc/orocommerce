f4e6ebc411f652fd59ef90c2427e91f32bfef91f
Dmitry Khrysev <x86demon@gmail.com>
2015-06-05 13:18:02 +0300

    BB-517: Add data converter for product price

--- src/OroB2B/Bundle/PricingBundle/DependencyInjection/OroB2BPricingExtension.php
+++ src/OroB2B/Bundle/PricingBundle/DependencyInjection/OroB2BPricingExtension.php
@@ -17,6 +17,7 @@ class OroB2BPricingExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
         $loader->load('form_types.yml');
+        $loader->load('importexport.yml');
     }
 
     /**
