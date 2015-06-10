bee508e951a7cd294704309435bac99b4bb07e7a
Denis Shimanovich <nsmuffin@gmail.com>
2015-06-10 14:57:47 +0300

    BB-620: Order activities     - Enabled notes for Order     - Enabled attachments for Order     - Enabled event activity for Order

---
+++ src/OroB2B/Bundle/OrderBundle/Migrations/Schema/v1_2/OroB2BOrderBundle.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace OroB2B\Bundle\OrderBundle\Migrations\Schema\v1_2;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\AttachmentBundle\Migration\Extension\AttachmentExtensionAwareInterface;
+use Oro\Bundle\AttachmentBundle\Migration\Extension\AttachmentExtension;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class OroB2BOrderBundle implements Migration, AttachmentExtensionAwareInterface
+{
+    const ORDER_TABLE_NAME = 'orob2b_order';
+
+    /** @var  AttachmentExtension */
+    protected $attachmentExtension;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setAttachmentExtension(AttachmentExtension $attachmentExtension)
+    {
+        $this->attachmentExtension = $attachmentExtension;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        self::addAttachmentAssociations($schema, $this->attachmentExtension);
+    }
+
+    /**
+     * Enable attachments for Order entity
+     *
+     * @param Schema $schema
+     */
+    public static function addAttachmentAssociations(Schema $schema, AttachmentExtension $attachmentExtension)
+    {
+        $attachmentExtension->addAttachmentAssociation(
+            $schema,
+            self::ORDER_TABLE_NAME
+        );
+    }
+}
