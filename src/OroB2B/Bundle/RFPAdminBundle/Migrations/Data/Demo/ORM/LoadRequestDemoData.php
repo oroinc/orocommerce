d99fb8ade267d3d945263fd67694bce2ac446bdd
Ivan Shirko <ivan.shirko@effective-soft.com>
2015-06-12 17:21:20 +0300

    BB-565: RFP Line Items - add demo data

--- src/OroB2B/Bundle/RFPAdminBundle/Migrations/Data/Demo/ORM/LoadRequestDemoData.php
+++ src/OroB2B/Bundle/RFPAdminBundle/Migrations/Data/Demo/ORM/LoadRequestDemoData.php
@@ -2,14 +2,19 @@
 
 namespace OroB2B\Bundle\RFPAdminBundle\Migrations\Data\Demo\ORM;
 
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
+use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Oro\Bundle\CurrencyBundle\Model\Price;
+use OroB2B\Bundle\ProductBundle\Entity\Product;
 use OroB2B\Bundle\RFPAdminBundle\Entity\Request;
+use OroB2B\Bundle\RFPAdminBundle\Entity\RequestProduct;
+use OroB2B\Bundle\RFPAdminBundle\Entity\RequestProductItem;
 
 class LoadRequestDemoData extends AbstractFixture implements ContainerAwareInterface, DependentFixtureInterface
 {
@@ -38,6 +43,8 @@ class LoadRequestDemoData extends AbstractFixture implements ContainerAwareInter
     {
         return [
             'OroB2B\Bundle\RFPAdminBundle\Migrations\Data\Demo\ORM\LoadRequestStatusDemoData',
+            'OroB2B\Bundle\ProductBundle\Migrations\Data\Demo\ORM\LoadProductDemoData',
+            'OroB2B\Bundle\ProductBundle\Migrations\Data\Demo\ORM\LoadProductUnitPrecisionDemoData',
         ];
     }
 
@@ -71,6 +78,7 @@ class LoadRequestDemoData extends AbstractFixture implements ContainerAwareInter
 
             $status = $statuses[rand(0, count($statuses) - 1)];
             $request->setStatus($status);
+            $this->processRequestProducts($request, $manager);
 
             $manager->persist($request);
         }
@@ -78,5 +86,78 @@ class LoadRequestDemoData extends AbstractFixture implements ContainerAwareInter
         fclose($handler);
 
         $manager->flush();
+    }
+
+    /**
+     * @param ObjectManager $manager
+     * @return Collection|Product[]
+     * @throws \LogicException
+     */
+    protected function getProducts(ObjectManager $manager)
+    {
+        $products = $manager->getRepository('OroB2BProductBundle:Product')
+            ->createQueryBuilder('p')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+
+        if (empty($products)) {
+            throw new \LogicException('There are no products in system');
+        }
+
+        return $products;
+    }
+
+    /**
+     * @return array
+     * @throws \LogicException
+     */
+    protected function getCurrencies()
+    {
+        $currencies = $this->container->get('oro_config.manager')->get('oro_currency.allowed_currencies');
+        if (empty($currencies)) {
+            $currency = $this->container->get('oro_locale.settings')->getCurrency();
+            $currencies = $currency ? [$currency] : [];
+        }
+
+        if (!$currencies) {
+            throw new \LogicException('There are no currencies in system');
+        }
+
+        return $currencies;
+    }
+
+    /**
+     * @param Request $request
+     * @param ObjectManager $manager
+     */
+    protected function processRequestProducts(Request $request, ObjectManager $manager)
+    {
+        $products = $this->getProducts($manager);
+        $currencies = $this->getCurrencies();
+        for ($i = 0; $i < rand(1, 3); $i++) {
+            $product = $products[rand(0, count($products) - 1)];
+            $unitPrecisions = $product->getUnitPrecisions();
+            $requestProduct = new RequestProduct();
+            $requestProduct->setProduct($product);
+            $requestProduct->setComment(sprintf('Notes %s', $i));
+            for ($j = 0; $j < rand(0, 3); $j++) {
+                if (!count($unitPrecisions)) {
+                    continue;
+                }
+                $productUnit = $unitPrecisions[rand(0, count($unitPrecisions) - 1)]->getUnit();
+
+                $currency = $currencies[rand(0, count($currencies) - 1)];
+                $requestProductItem = new RequestProductItem();
+                $requestProductItem
+                    ->setPrice(Price::create(rand(1, 100), $currency))
+                    ->setQuantity(rand(1, 100))
+                    ->setProductUnit($productUnit)
+                ;
+                $requestProduct->addRequestProductItem($requestProductItem);
+            }
+            $request->addRequestProduct($requestProduct);
+        }
     }
 }
