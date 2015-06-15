2702f801f712a62251cc8bb5c0a0696280d5ba4e
Ivan Shirko <ivan.shirko@effective-soft.com>
2015-06-15 19:44:16 +0300

    BB-567: Quote Line Items - Add new fields to QuoteProductItem

--- src/OroB2B/Bundle/SaleBundle/Entity/QuoteProductItem.php
+++ src/OroB2B/Bundle/SaleBundle/Entity/QuoteProductItem.php
@@ -9,6 +9,7 @@ use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
 use Oro\Bundle\CurrencyBundle\Model\Price;
 
 use OroB2B\Bundle\ProductBundle\Entity\ProductUnit;
+use OroB2B\Bundle\RFPAdminBundle\Entity\RequestProductItem;
 
 /**
  * @ORM\Table(name="orob2b_sale_quote_product_item")
@@ -28,6 +29,11 @@ use OroB2B\Bundle\ProductBundle\Entity\ProductUnit;
  */
 class QuoteProductItem
 {
+    const STATUS_REQUESTED = 10;
+    const STATUS_SUGGESTED_REPLACEMENT = 20;
+    const STATUS_ADDITIONAL = 30;
+    const STATUS_NOT_AVAILABLE = 40;
+
     /**
      * @var int
      *
@@ -87,11 +93,61 @@ class QuoteProductItem
     protected $price;
 
     /**
+     * @var float
+     *
+     * @ORM\Column(name="requested_quantity", type="float")
+     */
+    protected $requestedQuantity;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="requested_value", type="money")
+     */
+    protected $requestedValue;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="requested_currency", type="string")
+     */
+    protected $requestedCurrency;
+
+    /**
+     * @var Price
+     */
+    protected $requestedPrice;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="status", type="smallint", nullable=true)
+     */
+    protected $status;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="comment", type="text", nullable=true)
+     */
+    protected $comment;
+
+
+    /**
+     * @var RequestProductItem
+     *
+     * @ORM\ManyToOne(targetEntity="OroB2B\Bundle\RFPAdminBundle\Entity\RequestProductItem")
+     * @ORM\JoinColumn(name="request_product_item_id", referencedColumnName="id", onDelete="SET NULL")
+     */
+    protected $requestProductItem;
+
+    /**
      * @ORM\PostLoad
      */
     public function postLoad()
     {
         $this->price = Price::create($this->value, $this->currency);
+        $this->requestedPrice = Price::create($this->requestedValue, $this->requestedCurrency);
     }
 
     /**
@@ -100,8 +156,43 @@ class QuoteProductItem
      */
     public function preSave()
     {
-        $this->value    = $this->price->getValue();
+        $this->value = $this->price->getValue();
         $this->currency = $this->price->getCurrency();
+        $this->requestedValue = $this->requestedPrice->getValue();
+        $this->requestedCurrency = $this->requestedPrice->getCurrency();
+    }
+
+    /**
+     * Get Statuses Titles array
+     *
+     * @return array
+     */
+    public static function getStatusesTitles()
+    {
+        static $statusTitles = null;
+        if (null === $statusTitles) {
+            $statusTitles = [
+                static::STATUS_REQUESTED => 'orob2b.sale.quoteproductitem.status.requested',
+                static::STATUS_SUGGESTED_REPLACEMENT => 'orob2b.sale.quoteproductitem.status.suggested_replacement',
+                static::STATUS_ADDITIONAL => 'orob2b.sale.quoteproductitem.status.additional',
+                static::STATUS_NOT_AVAILABLE => 'orob2b.sale.quoteproductitem.status.not_available'
+            ];
+        }
+
+        return $statusTitles;
+    }
+
+    /**
+     * Get Status Title
+     *
+     * @return string
+     */
+    public function getStatusTitle()
+    {
+        $status = $this->getStatus();
+        $titles = static::getStatusesTitles();
+
+        return isset($titles[$status]) ? $titles[$status] : '';
     }
 
     /**
@@ -228,5 +319,106 @@ class QuoteProductItem
     public function getPrice()
     {
         return $this->price;
+    }
+
+    /**
+     * Set requestedQuantity
+     *
+     * @param float $requestedQuantity
+     * @return QuoteProductItem
+     */
+    public function setRequestedQuantity($requestedQuantity)
+    {
+        $this->requestedQuantity = $requestedQuantity;
+
+        return $this;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return float
+     */
+    public function getRequestedQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @param Price $requestedPrice
+     * @return QuoteProductItem
+     */
+    public function setRequestedPrice(Price $requestedPrice)
+    {
+        $this->requestedPrice = $requestedPrice;
+
+        $this->preSave();
+
+        return $this;
+    }
+
+    /**
+     * @return Price|null
+     */
+    public function getRequestedPrice()
+    {
+        return $this->requestedPrice;
+    }
+
+    /**
+     * @param string $comment
+     * @return QuoteProductItem
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * @param RequestProductItem $requestProductItem
+     * @return QuoteProductItem
+     */
+    public function setRequestProductItem(RequestProductItem $requestProductItem)
+    {
+        $this->requestProductItem = $requestProductItem;
+
+        return $this;
+    }
+
+    /**
+     * @return RequestProductItem
+     */
+    public function getRequestProductItem()
+    {
+        return $this->requestProductItem;
+    }
+
+    /**
+     * @param int $status
+     * @return QuoteProductItem
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
     }
 }
