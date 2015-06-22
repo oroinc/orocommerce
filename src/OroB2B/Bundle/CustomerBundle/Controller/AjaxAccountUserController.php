6b978a6eef8693604aac0dc441bea54f1bcdf473
Mykhailo Sulyma <msulima@magecore.com>
2015-06-22 13:21:47 +0300

    BB-656: Add button for manual account user confirmation on view page  - improved confirmAction

--- src/OroB2B/Bundle/CustomerBundle/Controller/AjaxAccountUserController.php
+++ src/OroB2B/Bundle/CustomerBundle/Controller/AjaxAccountUserController.php
@@ -2,13 +2,13 @@
 
 namespace OroB2B\Bundle\CustomerBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+
+use OroB2B\Bundle\CustomerBundle\Entity\AccountUser;
 
 class AjaxAccountUserController extends Controller
 {
@@ -16,25 +16,20 @@ class AjaxAccountUserController extends Controller
      * @Route("/confirm/{id}", name="orob2b_customer_account_user_confirm", requirements={"id"="\d+"})
      * @AclAncestor("orob2b_customer_account_user_update")
      *
-     * @param int $id
+     * @param AccountUser $accountUser
      * @return JsonResponse
      */
-    public function confirmAction($id)
+    public function confirmAction(AccountUser $accountUser)
     {
+        $userManager = $this->get('orob2b_account_user.manager');
+
         try {
-            $userManager = $this->get('orob2b_account_user.manager');
-            $user = $userManager->findUserBy(['id' => $id]);
-
-            if (null === $user) {
-                throw new \InvalidArgumentException(sprintf('Account user with id %d not found.', $id));
-            }
-
-            $userManager->confirmRegistration($user);
-            $userManager->updateUser($user);
+            $userManager->confirmRegistration($accountUser);
+            $userManager->updateUser($accountUser);
 
             $response = [
                 'successful' => true,
-                'message' => $this->getTranslator()->trans('orob2b.customer.controller.accountuser.confirmed.message')
+                'message' => $this->get('translator')->trans('orob2b.customer.controller.accountuser.confirmed.message')
             ];
         } catch (\Exception $e) {
             $this->get('logger')->error(
@@ -48,13 +43,5 @@ class AjaxAccountUserController extends Controller
         }
 
         return new JsonResponse($response);
-    }
-
-    /**
-     * @return TranslatorInterface
-     */
-    protected function getTranslator()
-    {
-        return $this->container->get('translator');
     }
 }
