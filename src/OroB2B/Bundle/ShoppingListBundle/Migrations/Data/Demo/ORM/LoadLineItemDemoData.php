8aee29437b3bce9de110fefb6fc844c4e3030e34
Denis Shimanovich <nsmuffin@gmail.com>
2015-06-24 15:18:16 +0300

    [WIP] BB-643: Shopping List Line Items  - New entity  - Data grid

--- src/OroB2B/Bundle/ShoppingListBundle/Migrations/Data/Demo/ORM/LoadLineItemDemoData.php
+++ src/OroB2B/Bundle/ShoppingListBundle/Migrations/Data/Demo/ORM/LoadLineItemDemoData.php
@@ -7,16 +7,37 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
 use Oro\Bundle\UserBundle\Entity\User;
+use OroB2B\Bundle\ProductBundle\Entity\Product;
+use OroB2B\Bundle\ShoppingListBundle\Entity\LineItem;
 use OroB2B\Bundle\ShoppingListBundle\Entity\ShoppingList;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class LoadLineItemDemoData extends AbstractFixture implements DependentFixtureInterface
+class LoadLineItemDemoData extends AbstractFixture implements DependentFixtureInterface, ContainerAwareInterface
 {
+    /** @var  ContainerInterface */
+    protected $container;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function getDependencies()
     {
-        return ['Oro\Bundle\UserBundle\Migrations\Data\ORM\LoadAdminUserData'];
+        return [
+            'Oro\Bundle\UserBundle\Migrations\Data\ORM\LoadAdminUserData',
+            'OroB2B\Bundle\PricingBundle\Migrations\Data\Demo\ORM\LoadPriceListDemoData',
+            'OroB2B\Bundle\ProductBundle\Migrations\Data\Demo\ORM\LoadProductDemoData',
+            'OroB2B\Bundle\ProductBundle\Migrations\Data\Demo\ORM\LoadProductUnitPrecisionDemoData',
+            'OroB2B\Bundle\ShoppingListBundle\Migrations\Data\Demo\ORM\LoadShoppingListDemoData'
+        ];
     }
 
     /**
@@ -24,6 +45,24 @@ class LoadLineItemDemoData extends AbstractFixture implements DependentFixtureIn
      */
     public function load(ObjectManager $manager)
     {
-        return;
+
+        $shoppingLists = $manager->getRepository('OroB2BShoppingListBundle:ShoppingList')->findBy([], null, 5);
+        $products = $manager->getRepository('OroB2BProductBundle:Product')->findBy([], null, 50);
+        $chunkedProducts = array_chunk($products, ceil(count($products) / count($shoppingLists)));
+
+        foreach ($shoppingLists as $index => $shoppingList) {
+            /** @var Product $product */
+            foreach ($chunkedProducts[$index] as $id => $product) {
+                $lineItem = (new LineItem())
+                    ->setShoppingList($shoppingList)
+                    ->setNotes(sprintf('Line item %d notes', $id + 1))
+                    ->setProduct($product)
+                    ->setQuantity(mt_rand(1, 25))
+                    ->setUnit($product->getUnitPrecisions()->current()->getUnit());
+                $manager->persist($lineItem);
+            }
+        }
+
+        $manager->flush();
     }
 }
