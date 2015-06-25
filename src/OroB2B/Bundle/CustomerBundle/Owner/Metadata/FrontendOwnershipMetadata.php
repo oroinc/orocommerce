0c713d7db06b990b674aac3340a743a7afb6321a
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2015-06-25 12:23:13 +0300

    BB-684: Create FrontendOwnershipMetadata - reuse ownerType field - dont support organization

--- src/OroB2B/Bundle/CustomerBundle/Owner/Metadata/FrontendOwnershipMetadata.php
+++ src/OroB2B/Bundle/CustomerBundle/Owner/Metadata/FrontendOwnershipMetadata.php
@@ -3,37 +3,21 @@
 namespace OroB2B\Bundle\CustomerBundle\Owner\Metadata;
 
 use Oro\Bundle\SecurityBundle\Owner\Metadata\OwnershipMetadata;
-use Oro\Bundle\SecurityBundle\Owner\Metadata\OwnershipMetadataInterface;
 
 /**
  * This class represents the entity ownership metadata for AccountUser
  */
-class FrontendOwnershipMetadata extends OwnershipMetadata implements OwnershipMetadataInterface
+class FrontendOwnershipMetadata extends OwnershipMetadata
 {
     const OWNER_TYPE_FRONTEND_USER = 4;
     const OWNER_TYPE_FRONTEND_CUSTOMER = 5;
 
     /**
-     * @var integer
-     */
-    protected $frontendOwnerType;
-
-    /**
-     * @var string
-     */
-    protected $frontendOwnerFieldName;
-
-    /**
-     * @var string
-     */
-    protected $frontendOwnerColumnName;
-
-    /**
      * {@inheritdoc}
      */
-    public function isBasicLevelOwned()
+    protected function getConstantName($ownerType)
     {
-        return $this->frontendOwnerType === self::OWNER_TYPE_FRONTEND_USER;
+        return sprintf('static::OWNER_TYPE_FRONTEND_%s', strtoupper($ownerType));
     }
 
     /**
@@ -41,95 +25,24 @@ class FrontendOwnershipMetadata extends OwnershipMetadata implements OwnershipMe
      */
     public function isLocalLevelOwned($deep = false)
     {
-        return $this->frontendOwnerType === self::OWNER_TYPE_FRONTEND_CUSTOMER;
+        return $this->ownerType === self::OWNER_TYPE_FRONTEND_CUSTOMER;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getOwnerType()
+    public function isBasicLevelOwned()
     {
-        return $this->frontendOwnerType;
+        return $this->ownerType === self::OWNER_TYPE_FRONTEND_USER;
     }
 
     /**
+     * Frontend entities are not owned by organization
+     *
      * {@inheritdoc}
      */
-    public function getOwnerFieldName()
+    public function isGlobalLevelOwned()
     {
-        return $this->frontendOwnerFieldName;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getOwnerColumnName()
-    {
-        return $this->frontendOwnerColumnName;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function serialize()
-    {
-        return serialize(
-            [
-                $this->ownerType,
-                $this->ownerFieldName,
-                $this->ownerColumnName,
-                $this->organizationFieldName,
-                $this->organizationColumnName,
-                $this->frontendOwnerType,
-                $this->frontendOwnerFieldName,
-                $this->frontendOwnerColumnName,
-            ]
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function unserialize($serialized)
-    {
-        list(
-            $this->ownerType,
-            $this->ownerFieldName,
-            $this->ownerColumnName,
-            $this->organizationFieldName,
-            $this->organizationColumnName,
-            $this->frontendOwnerType,
-            $this->frontendOwnerFieldName,
-            $this->frontendOwnerColumnName,
-            ) = unserialize($serialized);
-    }
-
-    /**
-     * @param string $frontendOwnerType
-     * @param string $frontendOwnerFieldName
-     * @param string $frontendOwnerColumnName
-     */
-    public function setFrontendOwner($frontendOwnerType, $frontendOwnerFieldName, $frontendOwnerColumnName)
-    {
-        $const = sprintf('static::OWNER_TYPE_FRONTEND_%s', strtoupper($frontendOwnerType));
-
-        if (defined($const)) {
-            $this->frontendOwnerType = constant($const);
-        } else {
-            if (!empty($frontendOwnerType)) {
-                throw new \InvalidArgumentException(sprintf('Unknown frontend owner type: %s.', $frontendOwnerType));
-            }
-            $this->frontendOwnerType = self::OWNER_TYPE_NONE;
-        }
-
-        $this->frontendOwnerFieldName = $frontendOwnerFieldName;
-        if ($this->frontendOwnerType !== self::OWNER_TYPE_NONE && empty($this->frontendOwnerFieldName)) {
-            throw new \InvalidArgumentException('The frontend owner field name must not be empty.');
-        }
-
-        $this->frontendOwnerColumnName = $frontendOwnerColumnName;
-        if ($this->frontendOwnerType !== self::OWNER_TYPE_NONE && empty($this->frontendOwnerColumnName)) {
-            throw new \InvalidArgumentException('The frontend owner column name must not be empty.');
-        }
+        return false;
     }
 }
