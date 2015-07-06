fe894e6c7f36a072ba4fdebd726867aabdb5b64f
Alexandr Torchenko <torchenko@gmail.com>
2015-07-06 15:02:11 +0300

    BB-648 Duplicate products:   - add product status   - base duplicator structure

---
+++ src/OroB2B/Bundle/ProductBundle/Duplicator/SkuIncrementor.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace OroB2B\Bundle\ProductBundle\Duplicator;
+
+
+class SkuIncrementor
+{
+
+}
---
+++ src/OroB2B/Bundle/ProductBundle/Entity/Repository/ProductRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace OroB2B\Bundle\ProductBundle\Entity\Repository;
+
+
+class ProductRepository
+{
+
+}
