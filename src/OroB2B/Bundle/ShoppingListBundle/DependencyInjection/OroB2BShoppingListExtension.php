59f9d76f8852f4d9cfeb2922d3531d829d1ef046
Mykhailo Popok <mipo@ciklum.com>
2015-07-08 14:19:42 +0300

    BB-664: Adding Products to Shopping Lists by Customers - added action button to frontend products grid

--- src/OroB2B/Bundle/ShoppingListBundle/DependencyInjection/OroB2BShoppingListExtension.php
+++ src/OroB2B/Bundle/ShoppingListBundle/DependencyInjection/OroB2BShoppingListExtension.php
@@ -17,5 +17,6 @@ class OroB2BShoppingListExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
         $loader->load('form_types.yml');
+        $loader->load('listeners.yml');
     }
 }
