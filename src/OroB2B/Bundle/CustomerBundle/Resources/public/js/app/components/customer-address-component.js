cf81bcb4a37c01dd9108a2d6cba4d79a6f1a004d
Ilya Antipenko <iantipenko@grossum.com>
2015-07-09 14:36:51 +0300

    BB-637: Account Address Book  - Add dispose for customer-address-component

--- src/OroB2B/Bundle/CustomerBundle/Resources/public/js/app/components/customer-address-component.js
+++ src/OroB2B/Bundle/CustomerBundle/Resources/public/js/app/components/customer-address-component.js
@@ -9,31 +9,43 @@ define(function (require) {
 
     CustomerAddressComponent = BaseComponent.extend({
         initialize: function (options) {
-            var targetElement = $(options._sourceElement);
+            this.targetElement = $(options._sourceElement);
             if (options.disableDefaultWithoutType) {
-                this.disableDefaultWithoutType(targetElement);
+                this.disableDefaultWithoutType();
             }
 
             if (options.disableRepeatedTypes) {
-                this.disableRepeatedTypes(targetElement);
+                this.disableRepeatedTypes();
             }
         },
 
-        disableDefaultWithoutType: function (targetElement) {
+        dispose: function() {
+            if (this.disposed || !this.targetElement) {
+                return;
+            }
+
+            this.targetElement.off('[name$="[defaults][default][]"]');
+            this.targetElement.off('[name$="[types][]"]');
+
+            CustomerAddressComponent.__super__.dispose.call(this);
+        },
+
+        disableDefaultWithoutType: function () {
             /**
              * Switch off default checkbox when type unselected
              */
-            targetElement.on('click', '[name$="[defaults][default][]"]', function() {
+            var self = this;
+            this.targetElement.on('click', '[name$="[defaults][default][]"]', function() {
                 if (this.checked) {
-                    targetElement.find('[name$="[types][]"][value="' + this.value + '"]').each(function (idx, el) {
+                    self.targetElement.find('[name$="[types][]"][value="' + this.value + '"]').each(function (idx, el) {
                         el.checked = true;
                     });
                 }
             });
 
-            targetElement.on('click', '[name$="[types][]"]', function() {
+            this.targetElement.on('click', '[name$="[types][]"]', function() {
                 var defaultTypeName = this.name.replace('[types][]', '[defaults][default][]');
-                var defaultCheckboxes = targetElement.find('[name$="' + defaultTypeName + '"][value="' + this.value + '"]');
+                var defaultCheckboxes = self.targetElement.find('[name$="' + defaultTypeName + '"][value="' + this.value + '"]');
                 if (!this.checked) {
                     defaultCheckboxes.each(function (idx, el) {
                         el.checked = false;
@@ -42,13 +54,14 @@ define(function (require) {
             });
         },
 
-        disableRepeatedTypes: function(targetElement) {
+        disableRepeatedTypes: function() {
             /**
              * Allow only 1 item with selected type
              */
-            targetElement.on('click', '[name$="[defaults][default][]"]', function() {
+            var self = this;
+            this.targetElement.on('click', '[name$="[defaults][default][]"]', function() {
                 if (this.checked) {
-                    targetElement.find('[name$="[defaults][default][]"][value="' + this.value + '"]').each(function (idx, el) {
+                    self.find('[name$="[defaults][default][]"][value="' + this.value + '"]').each(function (idx, el) {
                         el.checked = false;
                     });
                     this.checked = true;
