0c719b040b50d32f9f6dabbc6e4f67d115ee47a5
Mykhailo Popok <mipo@ciklum.com>
2015-07-16 12:53:45 +0300

    BB-664: Adding Products to Shopping Lists by Customers - removed unused repo method

--- src/OroB2B/Bundle/ShoppingListBundle/Entity/Repository/ShoppingListRepository.php
+++ src/OroB2B/Bundle/ShoppingListBundle/Entity/Repository/ShoppingListRepository.php
@@ -2,9 +2,7 @@
 namespace OroB2B\Bundle\ShoppingListBundle\Entity\Repository;
 
 use Doctrine\ORM\EntityRepository;
-use Doctrine\Common\Collections\ArrayCollection;
 
-use OroB2B\Bundle\ShoppingListBundle\Entity\ShoppingList;
 use OroB2B\Bundle\CustomerBundle\Entity\AccountUser;
 
 class ShoppingListRepository extends EntityRepository
@@ -22,22 +20,5 @@ class ShoppingListRepository extends EntityRepository
             ->andWhere('list.isCurrent = 1')
             ->setParameter('accountUser', $accountUser)
             ->getQuery()->getOneOrNullResult();
-    }
-
-    /**
-     * @param string[] $labels
-     *
-     * @return ShoppingList[]|ArrayCollection
-     */
-    public function findInLabels(array $labels)
-    {
-        $qb = $this->createQueryBuilder('shopping_list');
-
-        $qb
-            ->select('shopping_list')
-            ->where($qb->expr()->in('shopping_list.label', ':labels'))
-            ->setParameter('labels', $labels)
-            ->getQuery()
-            ->execute();
     }
 }
