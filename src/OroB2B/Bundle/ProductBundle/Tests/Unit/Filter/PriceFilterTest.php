de4f5186ca85904e5c43f53f89c88f5c86128e8a
Oleksandr Nechyporenko <onechyporenko@magecore.com>
2015-07-16 19:12:35 +0300

    BB-781: Implement custom filter type for product price  - add PriceFilter unit test

---
+++ src/OroB2B/Bundle/ProductBundle/Tests/Unit/Filter/PriceFilterTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace OroB2B\Bundle\ProductBundle\Tests\Unit\Filter;
+
+use OroB2B\Bundle\ProductBundle\Filter\PriceFilter;
+
+class PriceFilterTest extends \PHPUnit_Framework_TestCase
+{
+    protected $priceFilter;
+
+    public function setUp()
+    {
+        $form = $this->getMock('Symfony\Component\Form\Test\FormInterface');
+
+        $formFactory = $this->getMock('Symfony\Component\Form\FormFactoryInterface');
+        $formFactory->expects($this->any())
+            ->method('create')
+            ->will($this->returnValue($form));
+
+        $filterUtility = $this->getMockBuilder('Oro\Bundle\FilterBundle\Filter\FilterUtility')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $filterUtility->expects($this->any())
+            ->method('getExcludeParams')
+            ->will($this->returnValue([]));
+
+        $this->priceFilter = new PriceFilter($formFactory, $filterUtility);
+    }
+
+    public function testApply()
+    {
+        $ds = $this->getMockBuilder('Oro\Bundle\FilterBundle\Datasource\Orm\OrmFilterDatasourceAdapter')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->assertEquals(true, $this->priceFilter->apply($ds, []));
+    }
+
+    public function testGetMetadata()
+    {
+        $metadata = $this->priceFilter->getMetadata();
+        $this->assertEquals(true, array_key_exists('unitChoices', $metadata));
+    }
+}
