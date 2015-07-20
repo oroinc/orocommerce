2ad1f36df6670a3c7b38bbedb844a6196ce72693
Ivan Shirko <ivan.shirko@effective-soft.com>
2015-07-20 17:34:06 +0300

    BB-795: Create a page with the list of all Quotes BB-794: Create a view Page for a Quote - add view page - add list page - fix frontend_ownership - set security

---
+++ src/OroB2B/Bundle/SaleBundle/Controller/Frontend/QuoteController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace OroB2B\Bundle\SaleBundle\Controller\Frontend;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Oro\Bundle\FormBundle\Model\UpdateHandler;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+use Oro\Bundle\SecurityBundle\Annotation\Acl;
+
+use OroB2B\Bundle\SaleBundle\Entity\Quote;
+use OroB2B\Bundle\SaleBundle\Form\Type\QuoteType;
+
+class QuoteController extends Controller
+{
+    /**
+     * @Route("/view/{id}", name="orob2b_sale_frontend_quote_view", requirements={"id"="\d+"})
+     * @Template("OroB2BSaleBundle:Quote/Frontend:view.html.twig")
+     * @param Quote $quote
+     * @return array
+     */
+    public function viewAction(Quote $quote)
+    {
+        return [
+            'entity' => $quote
+        ];
+    }
+
+    /**
+     * @Route("/", name="orob2b_sale_frontend_quote_index")
+     * @Template("OroB2BSaleBundle:Quote/Frontend:index.html.twig")
+     * @return array
+     */
+    public function indexAction()
+    {
+        return [
+            'entity_class' => $this->container->getParameter('orob2b_sale.entity.quote.class')
+        ];
+    }
+
+    /**
+     * @Route("/info/{id}", name="orob2b_sale_frontend_quote_info", requirements={"id"="\d+"})
+     * @Template("OroB2BSaleBundle:Quote/Frontend/widget:info.html.twig")
+     *
+     * @param Quote $quote
+     * @return array
+     */
+    public function infoAction(Quote $quote)
+    {
+        return [
+            'entity' => $quote
+        ];
+    }
+}
