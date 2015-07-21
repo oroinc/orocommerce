efd07a039a2a9dbf64e6574b4d9be9b4e66c7594
Oleksandr Zahorulia <zaol@ciklum.com>
2015-07-21 17:53:45 +0300

    BB-725: Shopping List Form Type
    - Fixed import bug after merge

--- src/OroB2B/Bundle/ShoppingListBundle/Controller/Frontend/ShoppingListController.php
+++ src/OroB2B/Bundle/ShoppingListBundle/Controller/Frontend/ShoppingListController.php
@@ -9,6 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+use Oro\Bundle\SecurityBundle\Annotation\Acl;
 
 use OroB2B\Bundle\CustomerBundle\Entity\AccountUser;
 use OroB2B\Bundle\ShoppingListBundle\Entity\ShoppingList;
