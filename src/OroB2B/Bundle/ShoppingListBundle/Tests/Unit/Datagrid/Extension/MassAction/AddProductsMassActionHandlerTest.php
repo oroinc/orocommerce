3ab55a8012325752978d9a78d8c430f4d3b43162
Denis Shimanovich <nsmuffin@gmail.com>
2015-07-22 17:05:56 +0300

    [WIP] BB-726:  Create add to shopping list mass action             - Tests

---
+++ src/OroB2B/Bundle/ShoppingListBundle/Tests/Unit/Datagrid/Extension/MassAction/AddProductsMassActionHandlerTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace OroB2B\Bundle\ShoppingListBundle\Tests\Unit\Datagrid\Extension\MassAction;
+
+class AddProductsMassActionHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+
+    }
+
+    public function testGetProductsQueryBuilder()
+    {
+
+    }
+
+    public function testIsAllSelected()
+    {
+
+    }
+
+    public function testGetShoppingList()
+    {
+
+    }
+
+    public function testGetResponse()
+    {
+        
+    }
+}
