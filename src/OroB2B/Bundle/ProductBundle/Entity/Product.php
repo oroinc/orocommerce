0e61da6dfc6cb779b3c39862f2c6da621cda0141
Denis Shimanovich <nsmuffin@gmail.com>
2015-07-27 15:22:10 +0300

    BB-726:  Create add to shopping list mass action                 - Fix

--- src/OroB2B/Bundle/ProductBundle/Entity/Product.php
+++ src/OroB2B/Bundle/ProductBundle/Entity/Product.php
@@ -17,7 +17,7 @@ use OroB2B\Bundle\ProductBundle\Model\ExtendProduct;
 
 /**
  * @ORM\Table(name="orob2b_product")
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="OroB2B\Bundle\ProductBundle\Entity\Repository\ProductRepository")
  * @Config(
  *      routeName="orob2b_product_index",
  *      routeView="orob2b_product_view",
