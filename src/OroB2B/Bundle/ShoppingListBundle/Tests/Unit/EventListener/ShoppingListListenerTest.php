6a02824773b4bfb59aaaafe038bd5b9b06250636
Denis Shimanovich <nsmuffin@gmail.com>
2015-08-03 18:05:16 +0300

    BB-728: Adding Products to Shopping Lists by Customers              - ShoppingListListener test

---
+++ src/OroB2B/Bundle/ShoppingListBundle/Tests/Unit/EventListener/ShoppingListListenerTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace OroB2B\Bundle\ShoppingListBundle\Tests\Unit\EventListener;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+
+use OroB2B\Bundle\CustomerBundle\Entity\AccountUser;
+use OroB2B\Bundle\ShoppingListBundle\Entity\ShoppingList;
+use OroB2B\Bundle\ShoppingListBundle\EventListener\ShoppingListListener;
+
+class ShoppingListListenerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ShoppingList
+     */
+    protected $shoppingListOne;
+    /**
+     * @var ShoppingList
+     */
+    protected $shoppingListTwo;
+
+    public function setUp()
+    {
+        $this->shoppingListOne = (new ShoppingList())
+            ->setCurrent(true)
+            ->setOwner(new AccountUser());
+        $this->shoppingListTwo = new ShoppingList();
+    }
+
+    public function testPostRemove()
+    {
+        $listener = new ShoppingListListener();
+        $lifecycleEventArgs = $this->getEventArgs();
+        $listener->postRemove($lifecycleEventArgs);
+        $this->assertTrue($this->shoppingListTwo->isCurrent());
+    }
+
+    /**
+     * @return LifecycleEventArgs|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getEventArgs()
+    {
+        $lifecycleEventArgs = $this->getMockBuilder('Doctrine\ORM\Event\LifecycleEventArgs')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $lifecycleEventArgs->expects($this->once())
+            ->method('getEntity')
+            ->willReturn($this->shoppingListOne);
+        $lifecycleEventArgs->expects($this->once())
+            ->method('getEntityManager')
+            ->willReturn($this->getEntityManager());
+
+        return $lifecycleEventArgs;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|EntityManager
+     */
+    protected function getEntityManager()
+    {
+        $uow = $this->getMockBuilder('Doctrine\ORM\UnitOfWork')->disableOriginalConstructor()->getMock();
+
+        $repository = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+            ->disableOriginalConstructor()
+            ->setMethods(['findLatestForAccountUserExceptCurrent'])
+            ->getMock();
+        $repository->expects($this->once())
+            ->method('findLatestForAccountUserExceptCurrent')
+            ->willReturn($this->shoppingListTwo);
+
+        $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')->disableOriginalConstructor()->getMock();
+        $em->expects($this->once())
+            ->method('getUnitOfWork')
+            ->willReturn($uow);
+        $em->expects($this->once())
+            ->method('getRepository')
+            ->with('OroB2B\Bundle\ShoppingListBundle\Entity\ShoppingList')
+            ->willReturn($repository);
+
+        return $em;
+    }
+}
