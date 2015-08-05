73772c4551a710c888471baab64a4108073b0c71
Mykhailo Sulyma <msulima@magecore.com>
2015-08-05 17:50:58 +0300

    BB-885: Update backend Order index and view pages  - improve rendering addresses  - add fixtures for functional tests

--- src/OroB2B/Bundle/OrderBundle/Tests/Functional/DataFixtures/LoadOrders.php
+++ src/OroB2B/Bundle/OrderBundle/Tests/Functional/DataFixtures/LoadOrders.php
@@ -6,10 +6,32 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
+use Oro\Bundle\UserBundle\Entity\User;
+
 use OroB2B\Bundle\OrderBundle\Entity\Order;
+use OroB2B\Bundle\OrderBundle\Entity\OrderAddress;
+use OroB2B\Bundle\PaymentBundle\Entity\PaymentTerm;
 
 class LoadOrders extends AbstractFixture implements DependentFixtureInterface
 {
+    const ORDER_1 = 'simple_order';
+
+    /**
+     * @var array
+     */
+    protected $orders = [
+        self::ORDER_1 => [
+            'user' => LoadOrderUsers::ORDER_USER_1,
+            'billingAddress' => LoadOrderAddressData::ORDER_ADDRESS_1,
+            'shippingAddress' => LoadOrderAddressData::ORDER_ADDRESS_2,
+            'poNumber' => '1234567890',
+            'customerNotes' => 'Test account user notes',
+            'currency' => 'USD',
+            'subtotal' => '15000',
+            'paymentTerm' => LoadPaymentTermData::PAYMENT_TERM_NET_10
+        ],
+    ];
+
     /**
      * {@inheritdoc}
      */
@@ -17,6 +39,8 @@ class LoadOrders extends AbstractFixture implements DependentFixtureInterface
     {
         return [
             'OroB2B\Bundle\OrderBundle\Tests\Functional\DataFixtures\LoadOrderUsers',
+            'OroB2B\Bundle\OrderBundle\Tests\Functional\DataFixtures\LoadOrderAddressData',
+            'OroB2B\Bundle\OrderBundle\Tests\Functional\DataFixtures\LoadPaymentTermData',
         ];
     }
 
@@ -25,24 +49,45 @@ class LoadOrders extends AbstractFixture implements DependentFixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        $this->createOrder($manager, 'simple_order');
+        foreach ($this->orders as $name => $order) {
+            $this->createOrder($manager, $name, $order);
+        }
 
         $manager->flush();
     }
 
     /**
      * @param ObjectManager $manager
-     * @param string        $name
-     *
+     * @param string $name
+     * @param array $orderData
      * @return Order
      */
-    protected function createOrder(ObjectManager $manager, $name)
+    protected function createOrder(ObjectManager $manager, $name, array $orderData)
     {
-        $user = $this->getReference('order.simple_user');
+        /** @var User $user */
+        $user = $this->getReference($orderData['user']);
+
+        /** @var OrderAddress $billingAddress */
+        $billingAddress = $this->getReference($orderData['billingAddress']);
+
+        /** @var OrderAddress $shippingAddress */
+        $shippingAddress = $this->getReference($orderData['shippingAddress']);
+
+        /** @var PaymentTerm $paymentTerm */
+        $paymentTerm = $this->getReference($orderData['paymentTerm']);
 
         $order = new Order();
-        $order->setOwner($user);
-        $order->setOrganization($user->getOrganization());
+        $order
+            ->setIdentifier($name)
+            ->setOwner($user)
+            ->setOrganization($user->getOrganization())
+            ->setBillingAddress($billingAddress)
+            ->setShippingAddress($shippingAddress)
+            ->setPaymentTerm($paymentTerm)
+            ->setShipUntil(new \DateTime())
+            ->setCurrency($orderData['currency'])
+            ->setPoNumber($orderData['poNumber'])
+            ->setSubtotal($orderData['subtotal']);
 
         $manager->persist($order);
         $this->addReference($name, $order);
