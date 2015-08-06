5be8a156e721b6aabbb626ef6d08f80efe84ad9b
Mykhailo Sulyma <msulima@magecore.com>
2015-08-06 13:25:16 +0300

    BB-890: Create payment terms provider  - payment term provider  - test

---
+++ src/OroB2B/Bundle/PaymentBundle/Provider/PaymentTermProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace OroB2B\Bundle\PaymentBundle\Provider;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+use OroB2B\Bundle\AccountBundle\Entity\Account;
+use OroB2B\Bundle\PaymentBundle\Entity\PaymentTerm;
+use OroB2B\Bundle\PaymentBundle\Entity\Repository\PaymentTermRepository;
+
+class PaymentTermProvider
+{
+    /**
+     * @var ManagerRegistry
+     */
+    protected $registry;
+
+    /**
+     * @var string
+     */
+    protected $paymentTermClass;
+
+    /**
+     * @param ManagerRegistry $registry
+     * @param string $paymentTermClass
+     */
+    public function __construct(ManagerRegistry $registry, $paymentTermClass)
+    {
+        $this->registry = $registry;
+        $this->paymentTermClass = $paymentTermClass;
+    }
+
+    /**
+     * @param Account $account
+     * @return PaymentTerm|null
+     */
+    public function getPaymentTerm(Account $account)
+    {
+        $paymentTerm = $this->getPaymentTermRepository()->getOnePaymentTermByAccount($account);
+
+        if (!$paymentTerm && $account->getGroup()) {
+            $paymentTerm = $this->getPaymentTermRepository()->getOnePaymentTermByAccountGroup($account->getGroup());
+        }
+
+        return $paymentTerm;
+    }
+
+    /**
+     * @return PaymentTermRepository
+     */
+    protected function getPaymentTermRepository()
+    {
+        return $this->registry->getManagerForClass($this->paymentTermClass)->getRepository($this->paymentTermClass);
+    }
+}
