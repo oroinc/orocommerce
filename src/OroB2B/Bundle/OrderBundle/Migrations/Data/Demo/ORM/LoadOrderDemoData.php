d82d0de3add21c419b489da73d1ef6a89e060cb1
Dmitry Khrysev <x86demon@gmail.com>
2015-08-06 17:50:22 +0300

    BB-885: Update backend Order index and view pages

--- src/OroB2B/Bundle/OrderBundle/Migrations/Data/Demo/ORM/LoadOrderDemoData.php
+++ src/OroB2B/Bundle/OrderBundle/Migrations/Data/Demo/ORM/LoadOrderDemoData.php
@@ -6,6 +6,8 @@ use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
 use Oro\Bundle\AddressBundle\Entity\Country;
 use Oro\Bundle\AddressBundle\Entity\Region;
 use Oro\Bundle\UserBundle\Entity\User;
@@ -36,23 +38,25 @@ class LoadOrderDemoData extends AbstractFixture implements DependentFixtureInter
     /**
      * @var array
      */
-    protected $addresses = [
-        [
-            'label' => 'Office Address',
-            'country' => 'US',
-            'city' => 'Rochester',
-            'region' => 'US-NY',
-            'street' => '1215 Caldwell Road',
-            'postalCode' => '14608',
-        ],
-        [
-            'label' => 'Warehouse Address',
-            'country' => 'US',
-            'city' => 'Romney',
-            'region' => 'US-IN',
-            'street' => '2413 Capitol Avenue',
-            'postalCode' => '47981',
-        ]
+    protected $billingAddressesData = [
+        'label' => 'Office Address',
+        'country' => 'US',
+        'city' => 'Rochester',
+        'region' => 'US-NY',
+        'street' => '1215 Caldwell Road',
+        'postalCode' => '14608'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $shippingAddressData = [
+        'label' => 'Warehouse Address',
+        'country' => 'US',
+        'city' => 'Romney',
+        'region' => 'US-IN',
+        'street' => '2413 Capitol Avenue',
+        'postalCode' => '47981'
     ];
 
     /**
@@ -67,41 +71,40 @@ class LoadOrderDemoData extends AbstractFixture implements DependentFixtureInter
     }
 
     /**
+     * @param EntityManager $manager
      * {@inheritdoc}
      */
     public function load(ObjectManager $manager)
     {
-        /** @var User[] $users */
-        $users = $manager->getRepository('OroUserBundle:User')->findAll();
+        /** @var EntityRepository $userRepository */
+        $userRepository = $manager->getRepository('OroUserBundle:User');
+        /** @var User $user */
+        $user = $userRepository->findOneBy([]);
 
-        // create orders
-        foreach ($users as $user) {
-            $order = new Order();
-            $order
-                ->setOwner($user)
-                ->setAccountUser($this->getAccountUser($manager))
-                ->setOrganization($user->getOrganization())
-                ->setIdentifier($user->getId())
-                ->setBillingAddress($this->createOrderAddress($manager, $this->addresses[0]))
-                ->setShippingAddress($this->createOrderAddress($manager, $this->addresses[1]))
-                ->setPaymentTerm($this->getPaymentTermByLabel($manager, LoadPaymentTermDemoData::PAYMENT_TERM_NET_10))
-                ->setShipUntil(new \DateTime())
-                ->setCurrency('USD')
-                ->setPoNumber('CV032342USDD')
-                ->setSubtotal(15535.88);
+        $order = new Order();
+        $order
+            ->setOwner($user)
+            ->setAccountUser($this->getAccountUser($manager))
+            ->setOrganization($user->getOrganization())
+            ->setIdentifier($user->getId())
+            ->setBillingAddress($this->createOrderAddress($manager, $this->billingAddressesData))
+            ->setShippingAddress($this->createOrderAddress($manager, $this->shippingAddressData))
+            ->setPaymentTerm($this->getPaymentTermByLabel($manager, LoadPaymentTermDemoData::PAYMENT_TERM_NET_10))
+            ->setShipUntil(new \DateTime())
+            ->setCurrency('USD')
+            ->setPoNumber('CV032342USDD')
+            ->setSubtotal(15535.88);
 
-            $manager->persist($order);
-        }
-
+        $manager->persist($order);
         $manager->flush();
     }
 
     /**
-     * @param ObjectManager $manager
+     * @param EntityManager $manager
      * @param array $address
      * @return OrderAddress
      */
-    protected function createOrderAddress(ObjectManager $manager, array $address)
+    protected function createOrderAddress(EntityManager $manager, array $address)
     {
         $orderAddress = new OrderAddress();
         $orderAddress
@@ -133,11 +136,11 @@ class LoadOrderDemoData extends AbstractFixture implements DependentFixtureInter
     }
 
     /**
-     * @param ObjectManager $manager
+     * @param EntityManager $manager
      * @param string $iso2Code
      * @return Country|null
      */
-    protected function getCountryByIso2Code(ObjectManager $manager, $iso2Code)
+    protected function getCountryByIso2Code(EntityManager $manager, $iso2Code)
     {
         if (!array_key_exists($iso2Code, $this->countries)) {
             $this->countries[$iso2Code] = $manager->getReference('OroAddressBundle:Country', $iso2Code);
@@ -147,11 +150,11 @@ class LoadOrderDemoData extends AbstractFixture implements DependentFixtureInter
     }
 
     /**
-     * @param ObjectManager $manager
+     * @param EntityManager $manager
      * @param string $code
      * @return Region|null
      */
-    protected function getRegionByIso2Code(ObjectManager $manager, $code)
+    protected function getRegionByIso2Code(EntityManager $manager, $code)
     {
         if (!array_key_exists($code, $this->regions)) {
             $this->regions[$code] = $manager->getReference('OroAddressBundle:Region', $code);
