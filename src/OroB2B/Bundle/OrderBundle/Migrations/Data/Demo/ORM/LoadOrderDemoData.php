cfc648da9f623520c710b95b7ee7c9341ecca5b1
Vitaliy Berdylo <vberdylo@magecore.com>
2015-08-10 16:09:02 +0300

    BB-894: Create demo data

--- src/OroB2B/Bundle/OrderBundle/Migrations/Data/Demo/ORM/LoadOrderDemoData.php
+++ src/OroB2B/Bundle/OrderBundle/Migrations/Data/Demo/ORM/LoadOrderDemoData.php
@@ -8,15 +8,12 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
-use Oro\Bundle\AddressBundle\Entity\Country;
-use Oro\Bundle\AddressBundle\Entity\Region;
 use Oro\Bundle\UserBundle\Entity\User;
 
 use OroB2B\Bundle\AccountBundle\Entity\AccountUser;
 use OroB2B\Bundle\OrderBundle\Entity\Order;
 use OroB2B\Bundle\OrderBundle\Entity\OrderAddress;
 use OroB2B\Bundle\PaymentBundle\Entity\PaymentTerm;
-use OroB2B\Bundle\PaymentBundle\Migrations\Data\Demo\ORM\LoadPaymentTermDemoData;
 
 class LoadOrderDemoData extends AbstractFixture implements DependentFixtureInterface
 {
@@ -28,35 +25,49 @@ class LoadOrderDemoData extends AbstractFixture implements DependentFixtureInter
     /**
      * @var array
      */
-    protected $countries = [];
-
-    /**
-     * @var array
-     */
-    protected $regions = [];
-
-    /**
-     * @var array
-     */
-    protected $billingAddressesData = [
-        'label' => 'Office Address',
-        'country' => 'US',
-        'city' => 'Rochester',
-        'region' => 'US-NY',
-        'street' => '1215 Caldwell Road',
-        'postalCode' => '14608'
-    ];
-
-    /**
-     * @var array
-     */
-    protected $shippingAddressData = [
-        'label' => 'Warehouse Address',
-        'country' => 'US',
-        'city' => 'Romney',
-        'region' => 'US-IN',
-        'street' => '2413 Capitol Avenue',
-        'postalCode' => '47981'
+    protected $orders = [
+        [
+            'billingAddress' => 'Billing Address 01',
+            'shippingAddress' => 'Shipping Address 01',
+            'subtotal' => 15535.88,
+            'currency' => 'USD',
+            'poNumber' => 'CV032342USDD',
+            'paymentTerm' => 'net 10',
+            'customerNotes' => 'Please, call before delivery'
+        ],
+        [
+            'billingAddress' => 'Billing Address 02',
+            'shippingAddress' => 'Shipping Address 02',
+            'subtotal' => 20100.00,
+            'currency' => 'USD',
+            'poNumber' => 'AB10100USD',
+            'paymentTerm' => 'net 15',
+            'customerNotes' => 'Please, contact sales'
+        ],
+        [
+            'billingAddress' => 'Billing Address 03',
+            'shippingAddress' => 'Shipping Address 03',
+            'subtotal' => 99.99,
+            'currency' => 'EUR',
+            'poNumber' => 'FA1000EUR',
+            'paymentTerm' => 'net 10'
+        ],
+        [
+            'billingAddress' => 'Billing Address 04',
+            'shippingAddress' => 'Shipping Address 04',
+            'subtotal' => 5600.50,
+            'currency' => 'EUR',
+            'poNumber' => 'RT104568EUR',
+            'paymentTerm' => 'net 15'
+        ],
+        [
+            'billingAddress' => 'Billing Address 05',
+            'shippingAddress' => 'Shipping Address 05',
+            'subtotal' => 7800.99,
+            'currency' => 'USD',
+            'poNumber' => 'RT104568000',
+            'paymentTerm' => 'net 15'
+        ]
     ];
 
     /**
@@ -66,7 +77,8 @@ class LoadOrderDemoData extends AbstractFixture implements DependentFixtureInter
     {
         return [
             'OroB2B\Bundle\PaymentBundle\Migrations\Data\Demo\ORM\LoadPaymentTermDemoData',
-            'OroB2B\Bundle\AccountBundle\Migrations\Data\Demo\ORM\LoadAccountDemoData'
+            'OroB2B\Bundle\AccountBundle\Migrations\Data\Demo\ORM\LoadAccountDemoData',
+            'OroB2B\Bundle\OrderBundle\Migrations\Data\Demo\ORM\LoadOrderAddressDemoData'
         ];
     }
 
@@ -78,91 +90,45 @@ class LoadOrderDemoData extends AbstractFixture implements DependentFixtureInter
     {
         /** @var EntityRepository $userRepository */
         $userRepository = $manager->getRepository('OroUserBundle:User');
+
         /** @var User $user */
         $user = $userRepository->findOneBy([]);
 
         $accountUser = $this->getAccountUser($manager);
-        $order = new Order();
-        $order
-            ->setOwner($user)
-            ->setAccount($accountUser->getAccount())
-            ->setAccountUser($accountUser)
-            ->setOrganization($user->getOrganization())
-            ->setIdentifier($user->getId())
-            ->setBillingAddress($this->createOrderAddress($manager, $this->billingAddressesData))
-            ->setShippingAddress($this->createOrderAddress($manager, $this->shippingAddressData))
-            ->setPaymentTerm($this->getPaymentTermByLabel($manager, LoadPaymentTermDemoData::PAYMENT_TERM_NET_10))
-            ->setShipUntil(new \DateTime())
-            ->setCurrency('USD')
-            ->setPoNumber('CV032342USDD')
-            ->setSubtotal(15535.88);
 
-        $manager->persist($order);
+        foreach ($this->orders as $orderData) {
+            $order = new Order();
+
+            /** @var OrderAddress $billingAddress */
+            $billingAddress = $this->getReference($orderData['billingAddress']);
+
+            /** @var OrderAddress $shippingAddress */
+            $shippingAddress = $this->getReference($orderData['shippingAddress']);
+
+            /** @var PaymentTerm $paymentTerm */
+            $paymentTerm = $this->getReference($orderData['paymentTerm']);
+
+            $order
+                ->setOwner($user)
+                ->setAccount($accountUser->getAccount())
+                ->setAccountUser($accountUser)
+                ->setOrganization($user->getOrganization())
+                ->setBillingAddress($billingAddress)
+                ->setShippingAddress($shippingAddress)
+                ->setPaymentTerm($paymentTerm)
+                ->setShipUntil(new \DateTime())
+                ->setCurrency($orderData['currency'])
+                ->setPoNumber($orderData['poNumber'])
+                ->setSubtotal($orderData['subtotal']);
+
+            if (isset($orderData['customerNotes'])) {
+                $order->setCustomerNotes($orderData['customerNotes']);
+            }
+
+            $manager->persist($order);
+        }
+
         $manager->flush();
-    }
-
-    /**
-     * @param EntityManager $manager
-     * @param array $address
-     * @return OrderAddress
-     */
-    protected function createOrderAddress(EntityManager $manager, array $address)
-    {
-        $orderAddress = new OrderAddress();
-        $orderAddress
-            ->setLabel($address['label'])
-            ->setCountry($this->getCountryByIso2Code($manager, $address['country']))
-            ->setCity($address['city'])
-            ->setRegion($this->getRegionByIso2Code($manager, $address['region']))
-            ->setStreet($address['street'])
-            ->setPostalCode($address['postalCode']);
-
-        $manager->persist($orderAddress);
-
-        return $orderAddress;
-    }
-
-    /**
-     * @param ObjectManager $manager
-     * @param string $label
-     * @return PaymentTerm|null
-     */
-    protected function getPaymentTermByLabel(ObjectManager $manager, $label)
-    {
-        if (!array_key_exists($label, $this->paymentTerms)) {
-            $this->paymentTerms[$label] = $manager->getRepository('OroB2BPaymentBundle:PaymentTerm')
-                ->findOneBy(['label' => $label]);
-        }
-
-        return $this->paymentTerms[$label];
-    }
-
-    /**
-     * @param EntityManager $manager
-     * @param string $iso2Code
-     * @return Country|null
-     */
-    protected function getCountryByIso2Code(EntityManager $manager, $iso2Code)
-    {
-        if (!array_key_exists($iso2Code, $this->countries)) {
-            $this->countries[$iso2Code] = $manager->getReference('OroAddressBundle:Country', $iso2Code);
-        }
-
-        return $this->countries[$iso2Code];
-    }
-
-    /**
-     * @param EntityManager $manager
-     * @param string $code
-     * @return Region|null
-     */
-    protected function getRegionByIso2Code(EntityManager $manager, $code)
-    {
-        if (!array_key_exists($code, $this->regions)) {
-            $this->regions[$code] = $manager->getReference('OroAddressBundle:Region', $code);
-        }
-
-        return $this->regions[$code];
     }
 
     /**
