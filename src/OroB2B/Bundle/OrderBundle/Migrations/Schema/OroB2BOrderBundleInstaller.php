6d1ebb4fe6f8641f1be559db0714b6f5496c37a5
Dmitry Khrysev <x86demon@gmail.com>
2015-08-19 15:37:50 +0300

    BB-938: Order Line Items

--- src/OroB2B/Bundle/OrderBundle/Migrations/Schema/OroB2BOrderBundleInstaller.php
+++ src/OroB2B/Bundle/OrderBundle/Migrations/Schema/OroB2BOrderBundleInstaller.php
@@ -3,15 +3,14 @@
 namespace OroB2B\Bundle\OrderBundle\Migrations\Schema;
 
 use Doctrine\DBAL\Schema\Schema;
-
+use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtension;
+use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtensionAwareInterface;
+use Oro\Bundle\AttachmentBundle\Migration\Extension\AttachmentExtension;
+use Oro\Bundle\AttachmentBundle\Migration\Extension\AttachmentExtensionAwareInterface;
 use Oro\Bundle\MigrationBundle\Migration\Installation;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 use Oro\Bundle\NoteBundle\Migration\Extension\NoteExtension;
 use Oro\Bundle\NoteBundle\Migration\Extension\NoteExtensionAwareInterface;
-use Oro\Bundle\AttachmentBundle\Migration\Extension\AttachmentExtension;
-use Oro\Bundle\AttachmentBundle\Migration\Extension\AttachmentExtensionAwareInterface;
-use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtension;
-use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtensionAwareInterface;
 
 class OroB2BOrderBundleInstaller implements
     Installation,
@@ -57,7 +56,7 @@ class OroB2BOrderBundleInstaller implements
      */
     public function getMigrationVersion()
     {
-        return 'v1_1';
+        return 'v1_2';
     }
 
     /**
@@ -68,10 +67,12 @@ class OroB2BOrderBundleInstaller implements
         /** Tables generation **/
         $this->createOroB2BOrderTable($schema);
         $this->createOroB2BOrderAddressTable($schema);
+        $this->createOrob2BOrderLineItemTable($schema);
 
         /** Foreign keys generation **/
         $this->addOroB2BOrderForeignKeys($schema);
         $this->addOroB2BOrderAddressForeignKeys($schema);
+        $this->addOrob2BOrderLineItemForeignKeys($schema);
     }
 
     /**
@@ -87,6 +88,7 @@ class OroB2BOrderBundleInstaller implements
         $table->addColumn('user_owner_id', 'integer', ['notnull' => false]);
         $table->addColumn('shipping_address_id', 'integer', ['notnull' => false]);
         $table->addColumn('billing_address_id', 'integer', ['notnull' => false]);
+        $table->addColumn('price_list_id', 'integer', ['notnull' => false]);
         $table->addColumn('identifier', 'string', ['notnull' => false, 'length' => 255]);
         $table->addColumn('created_at', 'datetime', ['comment' => '(DC2Type:datetime)']);
         $table->addColumn('updated_at', 'datetime', ['comment' => '(DC2Type:datetime)']);
@@ -144,6 +146,38 @@ class OroB2BOrderBundleInstaller implements
     }
 
     /**
+     * Create orob2b_order_line_item table
+     *
+     * @param Schema $schema
+     */
+    protected function createOrob2BOrderLineItemTable(Schema $schema)
+    {
+        $table = $schema->createTable('orob2b_order_line_item');
+        $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('product_unit_id', 'string', ['notnull' => false, 'length' => 255]);
+        $table->addColumn('product_id', 'integer', ['notnull' => false]);
+        $table->addColumn('order_id', 'integer', ['notnull' => false]);
+        $table->addColumn('product_sku', 'string', ['length' => 255]);
+        $table->addColumn('free_form_product', 'string', ['notnull' => false, 'length' => 255]);
+        $table->addColumn('quantity', 'float', ['notnull' => false]);
+        $table->addColumn('product_unit_code', 'string', ['length' => 255]);
+        $table->addColumn(
+            'value',
+            'money',
+            ['notnull' => false, 'precision' => 19, 'scale' => 4, 'comment' => '(DC2Type:money)']
+        );
+        $table->addColumn('currency', 'string', ['notnull' => false, 'length' => 255]);
+        $table->addColumn('price_type', 'integer', []);
+        $table->addColumn('ship_by', 'date', ['notnull' => false, 'comment' => '(DC2Type:date)']);
+        $table->addColumn('from_external_source', 'boolean', []);
+        $table->addColumn('comment', 'text', ['notnull' => false]);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['product_id'], 'idx_de9136094584665a', []);
+        $table->addIndex(['product_unit_id'], 'idx_de91360929646bbd', []);
+        $table->addIndex(['order_id'], 'idx_de9136098d9f6d38', []);
+    }
+
+    /**
      * Add orob2b_order foreign keys.
      *
      * @param Schema $schema
@@ -193,6 +227,12 @@ class OroB2BOrderBundleInstaller implements
             ['id'],
             ['onUpdate' => null, 'onDelete' => 'SET NULL']
         );
+        $table->addForeignKeyConstraint(
+            $schema->getTable('orob2b_price_list'),
+            ['price_list_id'],
+            ['id'],
+            ['onUpdate' => null, 'onDelete' => 'SET NULL']
+        );
     }
 
     /**
@@ -226,6 +266,34 @@ class OroB2BOrderBundleInstaller implements
             ['country_code'],
             ['iso2_code'],
             ['onUpdate' => null, 'onDelete' => null]
+        );
+    }
+
+    /**
+     * Add orob2b_order_line_item foreign keys.
+     *
+     * @param Schema $schema
+     */
+    protected function addOrob2BOrderLineItemForeignKeys(Schema $schema)
+    {
+        $table = $schema->getTable('orob2b_order_line_item');
+        $table->addForeignKeyConstraint(
+            $schema->getTable('orob2b_product_unit'),
+            ['product_unit_id'],
+            ['code'],
+            ['onUpdate' => null, 'onDelete' => 'SET NULL']
+        );
+        $table->addForeignKeyConstraint(
+            $schema->getTable('orob2b_product'),
+            ['product_id'],
+            ['id'],
+            ['onUpdate' => null, 'onDelete' => 'SET NULL']
+        );
+        $table->addForeignKeyConstraint(
+            $schema->getTable('orob2b_order'),
+            ['order_id'],
+            ['id'],
+            ['onUpdate' => null, 'onDelete' => 'CASCADE']
         );
     }
 }
---
+++ src/OroB2B/Bundle/OrderBundle/Migrations/Schema/v1_2/AddLineItems.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace OroB2B\Bundle\OrderBundle\Migrations\Schema\v1_2;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class AddLineItems implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $this->createOrob2BOrderLineItemTable($schema);
+        $this->addOrob2BOrderLineItemForeignKeys($schema);
+        $this->updateOrderTable($schema);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    protected function updateOrderTable(Schema $schema)
+    {
+        $table = $schema->getTable('orob2b_order');
+        $table->addColumn('price_list_id', 'integer', ['notnull' => false]);
+        $table->addForeignKeyConstraint(
+            $schema->getTable('orob2b_price_list'),
+            ['price_list_id'],
+            ['id'],
+            ['onUpdate' => null, 'onDelete' => 'SET NULL']
+        );
+    }
+
+    /**
+     * Create orob2b_order_line_item table
+     *
+     * @param Schema $schema
+     */
+    protected function createOrob2BOrderLineItemTable(Schema $schema)
+    {
+        $table = $schema->createTable('orob2b_order_line_item');
+        $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('product_unit_id', 'string', ['notnull' => false, 'length' => 255]);
+        $table->addColumn('product_id', 'integer', ['notnull' => false]);
+        $table->addColumn('order_id', 'integer', ['notnull' => false]);
+        $table->addColumn('product_sku', 'string', ['length' => 255]);
+        $table->addColumn('free_form_product', 'string', ['notnull' => false, 'length' => 255]);
+        $table->addColumn('quantity', 'float', ['notnull' => false]);
+        $table->addColumn('product_unit_code', 'string', ['length' => 255]);
+        $table->addColumn(
+            'value',
+            'money',
+            ['notnull' => false, 'precision' => 19, 'scale' => 4, 'comment' => '(DC2Type:money)']
+        );
+        $table->addColumn('currency', 'string', ['notnull' => false, 'length' => 255]);
+        $table->addColumn('price_type', 'integer', []);
+        $table->addColumn('ship_by', 'date', ['notnull' => false, 'comment' => '(DC2Type:date)']);
+        $table->addColumn('from_external_source', 'boolean', []);
+        $table->addColumn('comment', 'text', ['notnull' => false]);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['product_id'], 'idx_de9136094584665a', []);
+        $table->addIndex(['product_unit_id'], 'idx_de91360929646bbd', []);
+        $table->addIndex(['order_id'], 'idx_de9136098d9f6d38', []);
+    }
+
+    /**
+     * Add orob2b_order_line_item foreign keys.
+     *
+     * @param Schema $schema
+     */
+    protected function addOrob2BOrderLineItemForeignKeys(Schema $schema)
+    {
+        $table = $schema->getTable('orob2b_order_line_item');
+        $table->addForeignKeyConstraint(
+            $schema->getTable('orob2b_product_unit'),
+            ['product_unit_id'],
+            ['code'],
+            ['onUpdate' => null, 'onDelete' => 'SET NULL']
+        );
+        $table->addForeignKeyConstraint(
+            $schema->getTable('orob2b_product'),
+            ['product_id'],
+            ['id'],
+            ['onUpdate' => null, 'onDelete' => 'SET NULL']
+        );
+        $table->addForeignKeyConstraint(
+            $schema->getTable('orob2b_order'),
+            ['order_id'],
+            ['id'],
+            ['onUpdate' => null, 'onDelete' => 'CASCADE']
+        );
+    }
+}
