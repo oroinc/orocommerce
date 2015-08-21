2a34e968089e2f8b96f3b9c6742ee590d79593fa
Oleksandr Nechyporenko <onechyporenko@magecore.com>
2015-08-21 15:49:32 +0300

    BB-989: Apply subtotal recalculation on line items changes  - call subtotal update on line item change

---
+++ src/OroB2B/Bundle/OrderBundle/Resources/public/js/app/views/line-item-abstract-view.js
@@ -0,0 +1,84 @@
+define(function(require) {
+    'use strict';
+
+    var LineItemAbstractView;
+    var $ = require('jquery');
+    var _ = require('underscore');
+    var SubtotalsListener = require('orob2border/js/app/listener/subtotals-listener');
+    var BaseView = require('oroui/js/app/views/base/view');
+
+    /**
+     * @export orob2border/js/app/views/line-item-abstract-view
+     * @extends oroui.app.views.base.View
+     * @class orob2border.app.views.LineItemAbstractView
+     */
+    LineItemAbstractView = BaseView.extend({
+        /**
+         * @property {Object}
+         */
+        options: {
+            ftid: ''
+        },
+
+        /**
+         * @property {jQuery}
+         */
+        $fields: null,
+
+        /**
+         * @property {Object}
+         */
+        fieldsByName: null,
+
+        /**
+         * @inheritDoc
+         */
+        initialize: function(options) {
+            this.options = $.extend(true, {}, this.options, options || {});
+            if (!this.options.ftid) {
+                this.options.ftid = this.$el.data('content').replace(/[^a-zA-Z0-9]+/g, '_').replace(/_+$/, '');
+            }
+
+            this.initLayout().done(_.bind(this.handleLayoutInit, this));
+        },
+
+        /**
+         * Doing something after loading child components
+         */
+        handleLayoutInit: function() {
+            var self = this;
+
+            this.$fields = this.$el.find(':input[data-ftid]');
+            this.fieldsByName = {};
+            this.$fields.each(function() {
+                var $field = $(this);
+                var name = self.normalizeName($field.data('ftid').replace(self.options.ftid + '_', ''));
+                self.fieldsByName[name] = $field;
+            });
+        },
+
+        /**
+         * Convert name with "_" to name with upper case, example: some_name > someName
+         *
+         * @param {String} name
+         *
+         * @returns {String}
+         */
+        normalizeName: function(name) {
+            name = name.split('_');
+            for (var i = 1, iMax = name.length; i < iMax; i++) {
+                name[i] = name[i][0].toUpperCase() + name[i].substr(1);
+            }
+            return name.join('');
+        },
+
+        /**
+         * @param {jQuery|Array} $fields
+         */
+        subtotalFields: function($fields) {
+            SubtotalsListener.listen($fields);
+        }
+    });
+
+    return LineItemAbstractView;
+});
