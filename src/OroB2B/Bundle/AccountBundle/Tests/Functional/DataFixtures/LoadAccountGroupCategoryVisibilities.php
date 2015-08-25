3a5a2fe93de831d5cffc626840fe3157772835dc
Mykhailo Popok <mipo@ciklum.com>
2015-08-25 12:08:00 +0300

    BB-576: Category Visibility - func tests

---
+++ src/OroB2B/Bundle/AccountBundle/Tests/Functional/DataFixtures/LoadAccountGroupCategoryVisibilities.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace OroB2B\Bundle\AccountBundle\Tests\Functional\DataFixtures;
+
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use OroB2B\Bundle\AccountBundle\Entity\AccountGroup;
+use OroB2B\Bundle\AccountBundle\Entity\AccountGroupCategoryVisibility;
+use OroB2B\Bundle\CatalogBundle\Entity\Category;
+
+class LoadAccountGroupCategoryVisibilities extends AbstractFixture implements DependentFixtureInterface
+{
+    /**
+     * @var array
+     */
+    protected static $data = [
+        [
+            'name' => 'account_group_category_visibility.1',
+            'category' => 'Test First Level',
+            'group' => 'account_group.group1',
+        ],
+        [
+            'name' => 'account_group_category_visibility.2',
+            'category' => 'Test Second Level',
+            'group' => 'account_group.group1',
+        ],
+        [
+            'name' => 'account_group_category_visibility.3',
+            'category' => 'Test Second Level',
+            'group' => 'account_group.group2',
+        ],
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->createAccountGroupCategoryVisibility($manager);
+
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDependencies()
+    {
+        return [
+            'OroB2B\Bundle\CatalogBundle\Tests\Functional\DataFixtures\LoadCategoryData',
+            'OroB2B\Bundle\AccountBundle\Tests\Functional\DataFixtures\LoadGroups',
+        ];
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
+    protected function createAccountGroupCategoryVisibility(ObjectManager $manager)
+    {
+        foreach (self::$data as $visibilityData) {
+            /** @var Category $category */
+            $category = $this->getReference($visibilityData['category']);
+            /** @var AccountGroup $accountGroup */
+            $accountGroup = $this->getReference($visibilityData['group']);
+
+            $visibility = (new AccountGroupCategoryVisibility())
+                ->setCategory($category)
+                ->setAccountGroup($accountGroup);
+            $manager->persist($visibility);
+            $this->addReference($visibilityData['name'], $visibility);
+        }
+    }
+}
--- src/OroB2B/Bundle/AccountBundle/Tests/Functional/Entity/Repository/AccountGroupCategoryVisibilityRepositoryTest.php
+++ src/OroB2B/Bundle/AccountBundle/Tests/Functional/Entity/Repository/AccountGroupCategoryVisibilityRepositoryTest.php
@@ -4,16 +4,16 @@ namespace OroB2B\Bundle\AccountBundle\Tests\Functional\Entity\Repository;
 
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 
-use OroB2B\Bundle\AccountBundle\Entity\Repository\AccountCategoryVisibilityRepository;
+use OroB2B\Bundle\AccountBundle\Entity\Repository\AccountGroupCategoryVisibilityRepository;
 use OroB2B\Bundle\CatalogBundle\Entity\Category;
 
 /**
  * @dbIsolation
  */
-class AccountCategoryVisibilityRepositoryTest extends WebTestCase
+class AccountGroupCategoryVisibilityRepositoryTest extends WebTestCase
 {
     /**
-     * @var AccountCategoryVisibilityRepository
+     * @var AccountGroupCategoryVisibilityRepository
      */
     protected $repository;
 
@@ -23,11 +23,11 @@ class AccountCategoryVisibilityRepositoryTest extends WebTestCase
 
         $this->repository = $this->getContainer()
             ->get('doctrine')
-            ->getRepository('OroB2BAccountBundle:AccountCategoryVisibility');
+            ->getRepository('OroB2BAccountBundle:AccountGroupCategoryVisibility');
 
         $this->loadFixtures(
             [
-                'OroB2B\Bundle\AccountBundle\Tests\Functional\DataFixtures\LoadAccountCategoryVisibilities'
+                'OroB2B\Bundle\AccountBundle\Tests\Functional\DataFixtures\LoadAccountGroupCategoryVisibilities'
             ]
         );
     }
@@ -35,20 +35,20 @@ class AccountCategoryVisibilityRepositoryTest extends WebTestCase
     /**
      * @dataProvider visibilityDataProvider
      *
-     * @param array  $accountReferences
+     * @param array  $accountGroupReferences
      * @param string $categoryReference
      * @param array  $expected
      */
-    public function testFindForAccounts($accountReferences, $categoryReference, array $expected)
+    public function testFindForAccountGroups($accountGroupReferences, $categoryReference, array $expected)
     {
-        $accounts = [];
-        foreach ($accountReferences as $accountReference) {
-            $accounts[] = $this->getReference($accountReference);
+        $accountGroups = [];
+        foreach ($accountGroupReferences as $accountGroupReference) {
+            $accountGroups[] = $this->getReference($accountGroupReference);
         }
         /** @var Category $category */
         $category = $this->getReference($categoryReference);
 
-        $actual = $this->repository->findForAccounts($accounts, $category);
+        $actual = $this->repository->findForAccountGroups($accountGroups, $category);
         $this->assertCount(count($expected), $actual);
 
         foreach ($expected as $i => $visibilityReference) {
@@ -64,31 +64,31 @@ class AccountCategoryVisibilityRepositoryTest extends WebTestCase
         return [
             [
                 [
-                    'account.level_1',
+                    'account_group.group1',
                 ],
                 'Test First Level',
                 [
-                    'account_category_visibility.1',
+                    'account_group_category_visibility.1',
                 ]
             ],
             [
                 [
-                    'account.level_1',
+                    'account_group.group1',
                 ],
                 'Test Second Level',
                 [
-                    'account_category_visibility.2',
+                    'account_group_category_visibility.2',
                 ]
             ],
             [
                 [
-                    'account.level_1',
-                    'account.level_1.4',
+                    'account_group.group1',
+                    'account_group.group2',
                 ],
                 'Test Second Level',
                 [
-                    'account_category_visibility.2',
-                    'account_category_visibility.3',
+                    'account_group_category_visibility.2',
+                    'account_group_category_visibility.3',
                 ]
             ],
         ];
