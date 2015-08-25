83a582adbf6288287781a176a76c31bf28ba733d
Ivan Shirko <ivan.shirko@effective-soft.com>
2015-08-25 17:35:14 +0300

    BB-917: Frontend: Create Edit page for RFP - add Edit RFP page - add line items for create\edit form - add pages titles - allow create RFP without authorization

--- src/OroB2B/Bundle/RFPBundle/Controller/Frontend/RequestController.php
+++ src/OroB2B/Bundle/RFPBundle/Controller/Frontend/RequestController.php
@@ -3,18 +3,19 @@
 namespace OroB2B\Bundle\RFPBundle\Controller\Frontend;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-use Oro\Bundle\UserBundle\Entity\User;
+use Oro\Bundle\FormBundle\Model\UpdateHandler;
 
-use OroB2B\Bundle\WebsiteBundle\Manager\WebsiteManager;
-
-use OroB2B\Bundle\RFPBundle\Form\Type\FrontendRequestType;
+use OroB2B\Bundle\AccountBundle\Entity\AccountUser;
 use OroB2B\Bundle\RFPBundle\Entity\Request as RFPRequest;
+use OroB2B\Bundle\RFPBundle\Form\Type\FrontendRequestType;
+use OroB2B\Bundle\WebsiteBundle\Manager\WebsiteManager;
 
 class RequestController extends Controller
 {
@@ -58,22 +59,66 @@ class RequestController extends Controller
     }
 
     /**
-     * @Route("/create", name="orob2b_rfp_request_create")
-     * @Method("GET")
-     * @Template("OroB2BRFPBundle:Request/Frontend:create.html.twig")
+     * @Route("/create", name="orob2b_rfp_frontend_request_create")
+     * @Template("OroB2BRFPBundle:Request/Frontend:update.html.twig")
      *
      * @return array
      */
     public function createAction()
     {
         $rfpRequest = new RFPRequest();
+        $user = $this->getUser();
+        if ($user instanceof AccountUser) {
+            $rfpRequest
+                ->setAccountUser($user)
+                ->setAccount($user->getAccount())
+                ->setFirstName($user->getFirstName())
+                ->setLastName($user->getLastName())
+                ->setCompany($user->getOrganization()->getName())
+                ->setEmail($user->getEmail())
+            ;
+        }
 
-        $form = $this->createCreateForm($rfpRequest);
+        return $this->update($rfpRequest);
+    }
 
-        return [
-            'entity' => $rfpRequest,
-            'form'   => $form->createView()
-        ];
+    /**
+     * @Route("/update/{id}", name="orob2b_rfp_frontend_request_update", requirements={"id"="\d+"})
+     * @Template("OroB2BRFPBundle:Request/Frontend:update.html.twig")
+     * @param RFPRequest $rfpRequest
+     *
+     * @return array|RedirectResponse
+     */
+    public function updateAction(RFPRequest $rfpRequest)
+    {
+        return $this->update($rfpRequest);
+    }
+
+    /**
+     * @param RFPRequest $rfpRequest
+     * @return array|RedirectResponse
+     */
+    protected function update(RFPRequest $rfpRequest)
+    {
+        /* @var $handler UpdateHandler */
+        $handler = $this->get('oro_form.model.update_handler');
+        return $handler->handleUpdate(
+            $rfpRequest,
+            $this->createForm(FrontendRequestType::NAME, $rfpRequest),
+            function (RFPRequest $rfpRequest) {
+                return [
+                    'route'         => 'orob2b_rfp_request_update',
+                    'parameters'    => ['id' => $rfpRequest->getId()]
+                ];
+            },
+            function (RFPRequest $rfpRequest) {
+                return [
+                    'route'         => 'orob2b_rfp_request_view',
+                    'parameters'    => ['id' => $rfpRequest->getId()]
+                ];
+            },
+            $this->get('translator')->trans('orob2b.rfp.controller.request.saved.message')
+        );
     }
 
     /**
@@ -157,7 +202,6 @@ class RequestController extends Controller
 
         return $form;
     }
-
     /**
      * Creates HTMLPurifier
      *
