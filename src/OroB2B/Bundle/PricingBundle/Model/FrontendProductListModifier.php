b9f6d20bee0cb07b7bc1ca4e9d6a55d14407d074
Dmitry Khrysev <x86demon@gmail.com>
2015-08-25 14:31:10 +0300

    BB-981: Limit grid for product combobox

--- src/OroB2B/Bundle/PricingBundle/Model/FrontendProductListModifier.php
+++ src/OroB2B/Bundle/PricingBundle/Model/FrontendProductListModifier.php
@@ -7,7 +7,6 @@ use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 use OroB2B\Bundle\AccountBundle\Entity\AccountUser;
-use OroB2B\Bundle\PricingBundle\Provider\PriceListProvider;
 
 class FrontendProductListModifier
 {
@@ -17,14 +16,18 @@ class FrontendProductListModifier
     protected $tokenStorage;
 
     /**
-     * @var PriceListProvider
+     * @var PriceListTreeHandler
      */
-    protected $priceListProvider;
+    protected $priceListTreeHandler;
 
-    public function __construct(TokenStorageInterface $tokenStorage, PriceListProvider $priceListProvider)
+    /**
+     * @param TokenStorageInterface $tokenStorage
+     * @param PriceListTreeHandler $priceListTreeHandler
+     */
+    public function __construct(TokenStorageInterface $tokenStorage, PriceListTreeHandler $priceListTreeHandler)
     {
         $this->tokenStorage = $tokenStorage;
-        $this->priceListProvider = $priceListProvider;
+        $this->priceListTreeHandler = $priceListTreeHandler;
     }
 
     /**
@@ -35,7 +38,7 @@ class FrontendProductListModifier
         $token = $this->tokenStorage->getToken();
         /** @var AccountUser $user */
         if ($token && ($user = $token->getUser()) instanceof AccountUser) {
-            $priceList = $this->priceListProvider->getPriceListByAccount($user->getAccount());
+            $priceList = $this->priceListTreeHandler->getPriceList($user);
 
             if ($priceList) {
                 $rootAliases = $queryBuilder->getRootAliases();
