1c10088347741622dad382e783968d33df52301a
Oleksandr Nechyporenko <onechyporenko@magecore.com>
2015-09-01 13:24:09 +0300

    BB-1015: Improve UI  - fix free form unit choices

--- src/OroB2B/Bundle/OrderBundle/Form/Type/OrderLineItemType.php
+++ src/OroB2B/Bundle/OrderBundle/Form/Type/OrderLineItemType.php
@@ -2,6 +2,8 @@
 
 namespace OroB2B\Bundle\OrderBundle\Form\Type;
 
+use Doctrine\Common\Persistence\ManagerRegistry;
+
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,10 +14,44 @@ use OroB2B\Bundle\OrderBundle\Entity\OrderLineItem;
 use OroB2B\Bundle\ProductBundle\Form\Type\ProductSelectType;
 use OroB2B\Bundle\ProductBundle\Entity\Repository\ProductUnitRepository;
 use OroB2B\Bundle\ProductBundle\Form\Type\ProductUnitSelectionType;
+use OroB2B\Bundle\ProductBundle\Formatter\ProductUnitLabelFormatter;
 
 class OrderLineItemType extends AbstractOrderLineItemType
 {
     const NAME = 'orob2b_order_line_item';
+
+    /**
+     * @var ManagerRegistry
+     */
+    protected $registry;
+
+    /**
+     * @var string
+     */
+    protected $productUnitClass;
+
+    /**
+     * @var ProductUnitLabelFormatter
+     */
+    protected $productUnitFormatter;
+
+    /**
+     * @param ManagerRegistry $registry
+     * @param ProductUnitLabelFormatter $productUnitFormatter
+     */
+    public function __construct(ManagerRegistry $registry, ProductUnitLabelFormatter $productUnitFormatter)
+    {
+        $this->registry = $registry;
+        $this->productUnitFormatter = $productUnitFormatter;
+    }
+
+    /**
+     * @param string $productUnitClass
+     */
+    public function setProductUnitClass($productUnitClass)
+    {
+        $this->productUnitClass = $productUnitClass;
+    }
 
     /**
      * {@inheritdoc}
@@ -26,7 +62,10 @@ class OrderLineItemType extends AbstractOrderLineItemType
 
         $resolver->setDefault(
             'page_component_options',
-            ['view' => 'orob2border/js/app/views/line-item-view']
+            [
+                'view' => 'orob2border/js/app/views/line-item-view',
+                'freeFormUnits' => $this->getFreeFormUnits(),
+            ]
         );
     }
 
@@ -115,5 +154,12 @@ class OrderLineItemType extends AbstractOrderLineItemType
                 }
             ]
         );
+    }
+
+    protected function getFreeFormUnits()
+    {
+        $units = $this->registry->getRepository($this->productUnitClass)->findBy([], ['code' => 'ASC']);
+        $units = $this->productUnitFormatter->formatChoices($units);
+        return $units;
     }
 }
