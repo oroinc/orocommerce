b8ded6e1072ff0860d8c9e58754b0a9da01a1971
Yevhen Shyshkin <yshyshkin@magecore.com>
2015-09-08 17:40:10 +0300

    BB-1056: Add quick add controller action  - moved logic form quick add controller to form handler  - added unit tests for form handler, collection transformer and quick add form type

--- src/OroB2B/Bundle/ProductBundle/Controller/Frontend/QuickAddController.php
+++ src/OroB2B/Bundle/ProductBundle/Controller/Frontend/QuickAddController.php
@@ -5,18 +5,14 @@ namespace OroB2B\Bundle\ProductBundle\Controller\Frontend;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-use OroB2B\Bundle\ProductBundle\Form\Type\QuickAddType;
-use OroB2B\Bundle\ProductBundle\Model\ComponentProcessorInterface;
-
 class QuickAddController extends Controller
 {
     /**
-     * Process quick add
-     *
      * @Route("/", name="orob2b_product_frontend_quick_add")
      * @Template("OroB2BProductBundle:QuickAdd\Frontend:add.html.twig")
      *
@@ -25,69 +21,13 @@ class QuickAddController extends Controller
      */
     public function addAction(Request $request)
     {
-        $response = null;
-        $formOptions = [];
-        $flashBag = $this->get('session')->getFlashBag();
+        $result = $this->get('orob2b_product.form_handler.quick_add')->process($request);
 
-        $processor = $this->getProcessor($this->getComponentName($request));
-        if ($processor) {
-            $formOptions = ['validation_required' => $processor->isValidationRequired()];
-        }
-
-        $form = $this->createForm(QuickAddType::NAME, null, $formOptions);
-        if ($request->isMethod(Request::METHOD_POST)) {
-            if ($processor) {
-                $form->submit($request);
-                if ($processor->isAllowed()) {
-                    if ($form->isValid()) {
-                        $products = $form->get(QuickAddType::PRODUCTS_FIELD_NAME)->getData();
-                        $response = $processor->process(is_array($products) ? $products : [], $request);
-                        if (!$response) {
-                            // reset form
-                            $form = $this->createForm(QuickAddType::NAME, null, $formOptions);
-                        }
-                    }
-                } else {
-                    $flashBag->add(
-                        'error',
-                        $this->get('translator')->trans('orob2b.product.frontend.component_not_allowed.message')
-                    );
-                }
-            } else {
-                $flashBag->add(
-                    'error',
-                    $this->get('translator')->trans('orob2b.product.frontend.component_not_found.message')
-                );
-            }
-        }
+        /** @var FormInterface $form */
+        $form = $result['form'];
+        /** @var Response|null $response */
+        $response = $result['response'];
 
         return $response ?: ['form' => $form->createView()];
-    }
-
-    /**
-     * @param Request $request
-     * @return null|string
-     */
-    protected function getComponentName(Request $request)
-    {
-        $formData = $request->get(QuickAddType::NAME);
-
-        $name = null;
-        if (isset($formData[QuickAddType::COMPONENT_FIELD_NAME])) {
-            $name = $formData[QuickAddType::COMPONENT_FIELD_NAME];
-        }
-
-        return $name;
-    }
-
-    /**
-     * @param string $name
-     * @return null|ComponentProcessorInterface
-     */
-    protected function getProcessor($name)
-    {
-        $processorsRegistry = $this->get('orob2b_product.component_processor.registry');
-
-        return $processorsRegistry->getProcessorByName($name);
     }
 }
