b72190f7850b0b2cb93efa43b2d6641178fc0672
Yevhen Shyshkin <yshyshkin@magecore.com>
2015-09-09 17:12:01 +0300

    BB-1096: Dump appropriate files depends on theme name  - added custom token parser to dump demo styles  - add twig extension

---
+++ src/OroB2B/Bundle/FrontendBundle/Twig/DemoThemeExtension.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace OroB2B\Bundle\FrontendBundle\Twig;
+
+use Symfony\Bundle\AsseticBundle\Factory\AssetFactory;
+
+use Oro\Bundle\AsseticBundle\AssetsConfiguration;
+use Oro\Bundle\ThemeBundle\Model\ThemeRegistry;
+
+class DemoThemeExtension extends \Twig_Extension
+{
+    const NAME = 'orob2b_frontend_theme';
+
+    /**
+     * @var AssetsConfiguration
+     */
+    protected $assetsConfiguration;
+
+    /**
+     * @var AssetFactory
+     */
+    protected $assetFactory;
+
+    /**
+     * @var ThemeRegistry
+     */
+    protected $themeRegistry;
+
+    /**
+     * @param AssetFactory $assetFactory
+     * @param AssetsConfiguration $assetsConfiguration
+     * @param ThemeRegistry $themeRegistry
+     */
+    public function __construct(
+        AssetFactory $assetFactory,
+        AssetsConfiguration $assetsConfiguration,
+        ThemeRegistry $themeRegistry
+    ) {
+        $this->assetFactory = $assetFactory;
+        $this->assetsConfiguration = $assetsConfiguration;
+        $this->themeRegistry = $themeRegistry;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTokenParsers()
+    {
+        return [
+            new FrontendAsseticTokenParser(
+                $this->assetsConfiguration,
+                $this->assetFactory,
+                $this->themeRegistry,
+                'orob2b_css',
+                'css/*.css'
+            )
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return self::NAME;
+    }
+}
---
+++ src/OroB2B/Bundle/FrontendBundle/Twig/FrontendAsseticTokenParser.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace OroB2B\Bundle\FrontendBundle\Twig;
+
+use Assetic\Factory\AssetFactory;
+
+use Oro\Bundle\AsseticBundle\AssetsConfiguration;
+use Oro\Bundle\AsseticBundle\Twig\AsseticTokenParser;
+use Oro\Bundle\ThemeBundle\Model\ThemeRegistry;
+
+class FrontendAsseticTokenParser extends AsseticTokenParser
+{
+    const FRONTEND_THEME = 'demo';
+
+    /**
+     * @var ThemeRegistry
+     */
+    protected $themeRegistry;
+
+    /**
+     * @param AssetsConfiguration $assetsConfiguration
+     * @param AssetFactory $assetFactory
+     * @param ThemeRegistry $themeRegistry
+     * @param string $tag
+     * @param string $output
+     */
+    public function __construct(
+        AssetsConfiguration $assetsConfiguration,
+        AssetFactory $assetFactory,
+        ThemeRegistry $themeRegistry,
+        $tag,
+        $output
+    ) {
+        parent::__construct($assetsConfiguration, $assetFactory, $tag, $output);
+
+        $this->themeRegistry = $themeRegistry;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function createAsseticNode(
+        \Twig_NodeInterface $body,
+        array $filters,
+        array $attributes,
+        $lineno
+    ) {
+        $currentTheme = $this->themeRegistry->getActiveTheme()->getName();
+        $this->themeRegistry->setActiveTheme(self::FRONTEND_THEME);
+
+        $node = parent::createAsseticNode($body, $filters, $attributes, $lineno);
+
+        $this->themeRegistry->setActiveTheme($currentTheme);
+
+        return $node;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function createDebugAsseticNode(
+        \Twig_NodeInterface $body,
+        array $filters,
+        array $attributes,
+        $lineno
+    ) {
+        $currentTheme = $this->themeRegistry->getActiveTheme()->getName();
+        $this->themeRegistry->setActiveTheme(self::FRONTEND_THEME);
+
+        $node = parent::createDebugAsseticNode($body, $filters, $attributes, $lineno);
+
+        $this->themeRegistry->setActiveTheme($currentTheme);
+
+        return $node;
+    }
+}
