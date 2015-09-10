32dfad15dd255dbed48f5328b725e48576b1608d
Andrey Yatsenco <yatsenco@gmail.com>
2015-09-10 12:43:57 +0300

    BB-1101: Create js implementation of category tree filter at product list page (backend) - fixed namespace bug

--- src/OroB2B/Bundle/CatalogBundle/Controller/ProductController.php
+++ src/OroB2B/Bundle/CatalogBundle/Controller/ProductController.php
@@ -6,9 +6,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 use OroB2B\Bundle\CatalogBundle\Entity\Category;
-use Symfony\Component\HttpFoundation\Request;
 
 class ProductController extends Controller
 {
