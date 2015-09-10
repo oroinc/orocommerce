51273d1789de0a8624661012667691b06c11c40c
Roman Yarmolenko <devxypro@gmail.com>
2015-09-10 16:01:03 +0300

    BB-1103: Create DatagridListener - add datagrid listener and request handler - add unit tests

---
+++ src/OroB2B/Bundle/CatalogBundle/Handler/RequestProductHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace OroB2B\Bundle\CatalogBundle\Handler;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class RequestProductHandler
+{
+    const CATEGORY_ID_KEY = 'category_id';
+
+    /** @var  Request|null */
+    protected $request;
+
+    public function setRequest($request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * @return bool|integer
+     */
+    public function getCategoryId()
+    {
+        if (!$this->request) {
+            return false;
+        }
+
+        $value = $this->request->get(self::CATEGORY_ID_KEY);
+
+        return $value ? (int)$value : false;
+    }
+}
