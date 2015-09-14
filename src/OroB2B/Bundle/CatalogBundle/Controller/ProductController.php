bc71550d8f38e58e44d00552451277277c3e3444
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2015-09-14 18:24:45 +0300

    BB-1107: Functional tests

--- src/OroB2B/Bundle/CatalogBundle/Controller/ProductController.php
+++ src/OroB2B/Bundle/CatalogBundle/Controller/ProductController.php
@@ -6,9 +6,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 use OroB2B\Bundle\CatalogBundle\Entity\Category;
+use OroB2B\Bundle\CatalogBundle\Handler\RequestProductHandler;
 
 class ProductController extends Controller
 {
@@ -16,14 +16,14 @@ class ProductController extends Controller
      * @Route("/sidebar", name="orob2b_catalog_category_product_sidebar")
      * @Template
      *
-     * @param Request $request
      * @return array
      */
-    public function sidebarAction(Request $request)
+    public function sidebarAction()
     {
-        if ($request->query->has('categoryId')) {
-            $defaultCategoryId = $request->query->get('categoryId');
-        } else {
+        /** @var RequestProductHandler $requestProductHandler */
+        $requestProductHandler = $this->get('orob2b_catalog.handler.request_product');
+        $defaultCategoryId = $requestProductHandler->getCategoryId();
+        if (!$defaultCategoryId) {
             /** @var Category $rootCategory */
             $rootCategory = $this->getDoctrine()->getRepository('OroB2BCatalogBundle:Category')->getMasterCatalogRoot();
             $defaultCategoryId = $rootCategory->getId();
