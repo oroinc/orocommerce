b290e2c4ba51df313f798e8eba58a727495b3047
Vitaliy Berdylo <vberdylo@magecore.com>
2015-09-14 21:09:49 +0300

    BB-776: Browse RFPs (customer) - fixes

--- src/OroB2B/Bundle/RFPBundle/Controller/Frontend/RequestController.php
+++ src/OroB2B/Bundle/RFPBundle/Controller/Frontend/RequestController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Oro\Bundle\FormBundle\Model\UpdateHandler;
