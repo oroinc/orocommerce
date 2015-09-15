7adc28176e72e370a7a4442fdc0fd58fe1fac8bf
Andrey Yatsenco <yatsenco@gmail.com>
2015-09-15 15:46:18 +0300

    BB-1105: Add option “Include Sub-Categories” to category filter at product list - combined datagrid listeners

--- src/OroB2B/Bundle/CatalogBundle/EventListener/DatagridListener.php
+++ src/OroB2B/Bundle/CatalogBundle/EventListener/DatagridListener.php
@@ -2,19 +2,56 @@
 
 namespace OroB2B\Bundle\CatalogBundle\EventListener;
 
+use Doctrine\Bundle\DoctrineBundle\Registry;
+
+use Oro\Bundle\DataGridBundle\Event\PreBuild;
+use Oro\Bundle\DataGridBundle\EventListener\DatasourceBindParametersListener;
 use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
 use Oro\Bundle\DataGridBundle\Event\BuildBefore;
+
+use OroB2B\Bundle\CatalogBundle\Entity\Category;
+use OroB2B\Bundle\CatalogBundle\Entity\Repository\CategoryRepository;
+use OroB2B\Bundle\CatalogBundle\Handler\RequestProductHandler;
 
 class DatagridListener
 {
     const CATEGORY_COLUMN = 'category_name';
+
+    /** @var Registry */
+    protected $doctrine;
+
+    /** @var RequestProductHandler */
+    protected $requestProductHandler;
+
+    /** @var string */
+    protected $dataClass;
+
+    /**
+     * @param Registry $doctrine
+     * @param RequestProductHandler $requestProductHandler
+     */
+    public function __construct(Registry $doctrine, RequestProductHandler $requestProductHandler)
+    {
+        $this->doctrine = $doctrine;
+        $this->requestProductHandler = $requestProductHandler;
+    }
 
     /**
      * @param BuildBefore $event
      */
     public function onBuildBeforeProductsSelect(BuildBefore $event)
     {
+        $this->addCategoryJoin($event->getConfig());
         $this->addCategoryRelation($event->getConfig());
+    }
+
+    /**
+     * @param PreBuild $event
+     */
+    public function onPreBuildProducts(PreBuild $event)
+    {
+        $this->addFilterByCategory($event);
+        $this->addCategoryJoin($event->getConfig());
     }
 
     /**
@@ -23,17 +60,8 @@ class DatagridListener
     protected function addCategoryRelation(DatagridConfiguration $config)
     {
         // select
-        $categoryTitleSelect = 'categoryTitle.string as ' . self::CATEGORY_COLUMN;
+        $categoryTitleSelect = 'categoryTitle.string as '.self::CATEGORY_COLUMN;
         $this->addConfigElement($config, '[source][query][select]', $categoryTitleSelect);
-
-        // joins
-        $joinCategory = [
-            'join' => 'OroB2BCatalogBundle:Category',
-            'alias' => 'productCategory',
-            'conditionType' => 'WITH',
-            'condition' => 'product MEMBER OF productCategory.products'
-        ];
-        $this->addConfigElement($config, '[source][query][join][left]', $joinCategory);
 
         $joinCategoryTitles = [
             'join' => 'productCategory.titles',
@@ -63,9 +91,58 @@ class DatagridListener
 
     /**
      * @param DatagridConfiguration $config
-     * @param string                $path
-     * @param mixed                 $element
-     * @param mixed                 $key
+     */
+    protected function addCategoryJoin(DatagridConfiguration $config)
+    {
+        // joins
+        $joinCategory = [
+            'join' => 'OroB2BCatalogBundle:Category',
+            'alias' => 'productCategory',
+            'conditionType' => 'WITH',
+            'condition' => 'product MEMBER OF productCategory.products'
+        ];
+        $this->addConfigElement($config, '[source][query][join][left]', $joinCategory);
+    }
+
+    /**
+     * @param PreBuild $event
+     */
+    protected function addFilterByCategory(PreBuild $event)
+    {
+        $categoryId = $this->requestProductHandler->getCategoryId();
+        if (!$categoryId) {
+            return;
+        }
+
+        /** @var CategoryRepository $repo */
+        $repo = $this->doctrine->getRepository($this->dataClass);
+        /** @var Category $category */
+        $category = $repo->find($categoryId);
+        if (!$category) {
+            return;
+        }
+
+        $includeSubcategoriesChoice = $this->requestProductHandler->getIncludeSubcategoriesChoice();
+        $productCategoryIds = [$categoryId];
+        if ($includeSubcategoriesChoice) {
+            $productCategoryIds = array_merge($repo->getChildrenIds($category), $productCategoryIds);
+        }
+
+        $config = $event->getConfig();
+        $config->offsetSetByPath('[source][query][where][and]', ['productCategory.id IN (:productCategoryIds)']);
+        $config->offsetSetByPath(
+            DatasourceBindParametersListener::DATASOURCE_BIND_PARAMETERS_PATH,
+            ['productCategoryIds']
+        );
+        $parameters = $event->getParameters();
+        $parameters->set('productCategoryIds', $productCategoryIds);
+    }
+
+    /**
+     * @param DatagridConfiguration $config
+     * @param string $path
+     * @param mixed $element
+     * @param mixed $key
      */
     protected function addConfigElement(DatagridConfiguration $config, $path, $element, $key = null)
     {
@@ -76,5 +153,13 @@ class DatagridListener
             $select[] = $element;
         }
         $config->offsetSetByPath($path, $select);
+    }
+
+    /**
+     * @param string $dataClass
+     */
+    public function setDataClass($dataClass)
+    {
+        $this->dataClass = $dataClass;
     }
 }
--- src/OroB2B/Bundle/CatalogBundle/Tests/Unit/EventListener/DatagridListenerTest.php
+++ src/OroB2B/Bundle/CatalogBundle/Tests/Unit/EventListener/DatagridListenerTest.php
@@ -2,14 +2,25 @@
 
 namespace OroB2B\Bundle\CatalogBundle\Tests\Unit\EventListener;
 
+use Doctrine\Bundle\DoctrineBundle\Registry;
+
 use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
 use Oro\Bundle\DataGridBundle\Datagrid\DatagridInterface;
 use Oro\Bundle\DataGridBundle\Event\BuildBefore;
+use Oro\Bundle\DataGridBundle\Datagrid\ParameterBag;
+use Oro\Bundle\DataGridBundle\EventListener\DatasourceBindParametersListener;
 
 use OroB2B\Bundle\CatalogBundle\EventListener\DatagridListener;
+use OroB2B\Bundle\CatalogBundle\Entity\Category;
+use OroB2B\Bundle\CatalogBundle\Entity\Repository\CategoryRepository;
+use OroB2B\Bundle\CatalogBundle\Handler\RequestProductHandler;
 
 class DatagridListenerTest extends \PHPUnit_Framework_TestCase
 {
+    const DATA_CLASS = 'OroB2B\Bundle\CatalogBundle\Entity\Category';
+    const QUERY_AND_PATH = '[source][query][where][and]';
+    const CATEGORY_ID_ALIAS = 'productCategoryIds';
+
     /**
      * @var array
      */
@@ -58,6 +69,25 @@ class DatagridListenerTest extends \PHPUnit_Framework_TestCase
         ]
     ];
 
+    /** @var  Registry|\PHPUnit_Framework_MockObject_MockObject */
+    protected $doctrine;
+
+    /** @var RequestProductHandler|\PHPUnit_Framework_MockObject_MockObject */
+    protected $requestProductHandler;
+
+    /** @var  DatagridListener */
+    protected $listener;
+
+    public function setUp()
+    {
+        $this->doctrine = $this->getMockBuilder('Doctrine\Bundle\DoctrineBundle\Registry')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->requestProductHandler = $this->getMock('OroB2B\Bundle\CatalogBundle\Handler\RequestProductHandler');
+        $this->listener = new DatagridListener($this->doctrine, $this->requestProductHandler);
+        $this->listener->setDataClass(self::DATA_CLASS);
+    }
+
     public function testOnBuildBeforeProductsSelect()
     {
         /** @var \PHPUnit_Framework_MockObject_MockObject|DatagridInterface $datagrid */
@@ -65,12 +95,129 @@ class DatagridListenerTest extends \PHPUnit_Framework_TestCase
         $config = DatagridConfiguration::create([]);
 
         $event = new BuildBefore($datagrid, $config);
-        $listener = new DatagridListener();
-        $listener->onBuildBeforeProductsSelect($event);
+
+        $this->listener->onBuildBeforeProductsSelect($event);
 
         $expected = self::$expectedTemplate;
-        $expected['source']['query']['select'] = ['categoryTitle.string as ' . DatagridListener::CATEGORY_COLUMN];
+        $expected['source']['query']['select'] = ['categoryTitle.string as '.DatagridListener::CATEGORY_COLUMN];
 
         $this->assertEquals($expected, $config->toArray());
+    }
+
+    /**
+     * @dataProvider childrenIdsDataProvider
+     *
+     * @param int $catId
+     * @param boolean $includeSubcategoriesChoice
+     * @param array $childrenIds
+     * @param array $expectedParameters
+     */
+    public function testOnPreBuild(
+        $catId,
+        $includeSubcategoriesChoice,
+        array $childrenIds,
+        array $expectedParameters = []
+    ) {
+        $category = new Category();
+        $this->requestProductHandler->expects($this->once())->method('getCategoryId')->willReturn($catId);
+        $this->requestProductHandler
+            ->expects($this->once())
+            ->method('getIncludeSubcategoriesChoice')
+            ->willReturn($includeSubcategoriesChoice);
+
+        /** @var CategoryRepository|\PHPUnit_Framework_MockObject_MockObject $repo */
+        $repo = $this->getMockBuilder('OroB2B\Bundle\CatalogBundle\Entity\Repository\CategoryRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $repo->expects($this->once())->method('find')->with($catId)->willReturn($category);
+        $repo->expects($includeSubcategoriesChoice ? $this->once() : $this->never())
+            ->method('getChildrenIds')->with($category)->willReturn($childrenIds);
+        $this->doctrine->expects($this->once())->method('getRepository')->with(self::DATA_CLASS)->willReturn($repo);
+
+        $event = $this->createPreBuildEvent();
+        /** @var $config DatagridConfiguration|\PHPUnit_Framework_MockObject_MockObject */
+        $config = $this->getMockBuilder('Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $event->expects($this->any())->method('getConfig')->willReturn($config);
+
+        $params = new ParameterBag();
+        $event->expects($this->any())->method('getParameters')->willReturn($params);
+
+        $config->expects($this->at(0))
+            ->method('offsetSetByPath')
+            ->with(self::QUERY_AND_PATH, [sprintf('productCategory.id IN (:%s)', self::CATEGORY_ID_ALIAS)]);
+        $config->expects($this->at(1))
+            ->method('offsetSetByPath')
+            ->with(DatasourceBindParametersListener::DATASOURCE_BIND_PARAMETERS_PATH, [self::CATEGORY_ID_ALIAS]);
+
+        $this->listener->onPreBuildProducts($event);
+        $this->assertEquals($expectedParameters, $params->all());
+    }
+
+    /**
+     * @return array
+     */
+    public function childrenIdsDataProvider()
+    {
+        return [
+            [
+                'catId' => 1,
+                'includeSubcategories' => true,
+                'childrenIds' => [2, 3],
+                'expectedParameters' => ['productCategoryIds' => [2, 3, 1]]
+            ],
+            [
+                'catId' => 1,
+                'includeSubcategories' => true,
+                'childrenIds' => [],
+                'expectedParameters' => ['productCategoryIds' => [1]]
+            ],
+            [
+                'catId' => 1,
+                'includeSubcategories' => false,
+                'childrenIds' => [],
+                'expectedParameters' => ['productCategoryIds' => [1]]
+            ],
+        ];
+    }
+
+    public function testOnPreBuildWithoutExistingCategory()
+    {
+        $catId = 1;
+        $this->requestProductHandler->expects($this->once())->method('getCategoryId')->willReturn($catId);
+        /** @var CategoryRepository|\PHPUnit_Framework_MockObject_MockObject $repo */
+        $repo = $this->getMockBuilder('OroB2B\Bundle\CatalogBundle\Entity\Repository\CategoryRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $repo->expects($this->once())->method('find')->with($catId)->willReturn(null);
+        $this->doctrine->expects($this->once())->method('getRepository')->with(self::DATA_CLASS)->willReturn($repo);
+        $repo->expects($this->never())->method('getChildrenIds');
+        $event = $this->createPreBuildEvent();
+        /** @var $config DatagridConfiguration|\PHPUnit_Framework_MockObject_MockObject */
+        $config = $this->getMockBuilder('Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $event->expects($this->any())->method('getConfig')->willReturn($config);
+
+        $this->listener->onPreBuildProducts($event);
+    }
+
+    public function testOnPreBuildWithoutCategoryId()
+    {
+        $event = $this->createPreBuildEvent();
+        $this->requestProductHandler->expects($this->once())->method('getCategoryId')->willReturn(false);
+        $this->doctrine->expects($this->never())->method('getRepository');
+        $this->listener->onPreBuildProducts($event);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Oro\Bundle\DataGridBundle\Event\PreBuild
+     */
+    protected function createPreBuildEvent()
+    {
+        return $this->getMockBuilder('Oro\Bundle\DataGridBundle\Event\PreBuild')
+            ->disableOriginalConstructor()
+            ->getMock();
     }
 }
