bdbe51a6e1a0aa6c76b5fbe996c3bd682f06ef7f
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2015-09-15 14:05:56 +0300

    BB-1105: Add option “Include Sub-Categories” to category filter at product list - cr fixes

--- src/OroB2B/Bundle/SaleBundle/Tests/Unit/DependencyInjection/OroB2BSaleExtensionTest.php
+++ src/OroB2B/Bundle/SaleBundle/Tests/Unit/DependencyInjection/OroB2BSaleExtensionTest.php
@@ -14,7 +14,6 @@ class OroB2BSaleExtensionTest extends ExtensionTestCase
         $expectedParameters = [
             'orob2b_sale.entity.quote.class',
             'orob2b_sale.quote.manager.api.class',
-            'orob2b_sale.event_listener.entity_subscriber.class',
             // validators
             'orob2b_sale.validator.quote_product_unit.class',
             // form types
@@ -29,7 +28,6 @@ class OroB2BSaleExtensionTest extends ExtensionTestCase
 
         $expectedDefinitions = [
             'orob2b_sale.quote.manager.api',
-            'orob2b_sale.event_listener.entity_subscriber',
             // validators
             'orob2b_sale.validator.quote_product_unit',
             // form types
