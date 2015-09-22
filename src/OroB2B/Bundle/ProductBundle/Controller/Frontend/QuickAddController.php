3733d178558db6858da53f9b68c0f64078725b43
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2015-09-22 12:37:30 +0300

    BB-1167: Secure product list and view - remove excess import

--- src/OroB2B/Bundle/ProductBundle/Controller/Frontend/QuickAddController.php
+++ src/OroB2B/Bundle/ProductBundle/Controller/Frontend/QuickAddController.php
@@ -10,8 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
-
 class QuickAddController extends Controller
 {
     /**
