eeb34c269f23d273702343a97bd6d5e85fd31a8e
Mykhailo Sulyma <msulima@magecore.com>
2015-09-24 19:24:36 +0300

    BB-1223: Update JS component  - remove js validation component  - update table row js component

--- src/OroB2B/Bundle/SaleBundle/Resources/public/js/app/components/quote-product-to-order-component.js
+++ src/OroB2B/Bundle/SaleBundle/Resources/public/js/app/components/quote-product-to-order-component.js
@@ -6,7 +6,9 @@ define(function(require) {
     var QuoteProductToOrderComponent;
     var BaseComponent = require('oroui/js/app/components/base/component');
     var _ = require('underscore');
+    var __ = require('orotranslation/js/translator');
     var $ = require('jquery');
+    var routing = require('routing');
 
     QuoteProductToOrderComponent = BaseComponent.extend({
         /**
@@ -15,19 +17,28 @@ define(function(require) {
         options: {
             offerSelector: '.radiobox',
             quantitySelector: '.quantity',
+            unitInputSelector: '.unitInput',
             unitSelector: '.unit',
             unitPriceSelector: '.unitPrice',
             data_attributes: {
                 unit: 'unit',
                 quantity: 'quantity',
                 price: 'price'
-            }
+            },
+            matchOfferRoute: 'orob2b_sale_quote_frontend_quote_product_match_offer',
+            quoteProductId: null,
+            offerNotFoundMessage: 'orob2b.sale.quoteproductoffer.configurable.offer.blank'
         },
 
         /**
          * @property {jQuery.Element}
          */
         $quantity: null,
+
+        /**
+         * @property {jQuery.Element}
+         */
+        $unitInput: null,
 
         /**
          * @property {jQuery.Element}
@@ -40,16 +51,23 @@ define(function(require) {
         $unitPrice: null,
 
         /**
+         * @property {Number}
+         */
+        quantityChange: null,
+
+        /**
          * @inheritDoc
          */
         initialize: function(options) {
             this.options = _.defaults(options || {}, this.options);
 
             this.$quantity = $(this.options.quantitySelector);
+            this.$unitInput = $(this.options.unitInputSelector);
             this.$unit = $(this.options.unitSelector);
             this.$unitPrice = $(this.options.unitPriceSelector);
 
             this.options._sourceElement.on('change', this.options.offerSelector, _.bind(this.onSelectorChange, this));
+            this.addQuantityEvents();
         },
 
         /**
@@ -61,6 +79,92 @@ define(function(require) {
             this.updateQuantityInputValue(Number(target.data(this.options.data_attributes.quantity)));
             this.updateUnitValue(String(target.data(this.options.data_attributes.unit)));
             this.updateUnitPriceValue(String(target.data(this.options.data_attributes.price)));
+        },
+
+        onQuantityChange: function() {
+            var self = this;
+
+            if (!this.$quantity.val()) {
+                return;
+            }
+
+            $.ajax({
+                url: routing.generate(
+                    this.options.matchOfferRoute,
+                    {
+                        id: this.options.quoteProductId,
+                        unit: this.$unitInput.val(),
+                        qty: this.$quantity.val()
+                    }
+                ),
+                type: 'GET',
+                success: function(response) {
+                    self.removeFieldErrors(self.$quantity);
+
+                    if (!_.isEmpty(response)) {
+                        self.updateUnitPriceValue(String(response.price));
+                        self.updateSelector(response.id);
+                    } else {
+                        self.addFieldErrors(self.$quantity, __(self.options.offerNotFoundMessage));
+                    }
+                }
+            });
+        },
+
+        addQuantityEvents: function() {
+            this.$quantity.change(_.bind(function() {
+                if (this.quantityChange) {
+                    clearTimeout(this.quantityChange);
+                }
+
+                this.onQuantityChange.call(this);
+            }, this));
+
+            this.$quantity.keyup(_.bind(function() {
+                if (this.quantityChange) {
+                    clearTimeout(this.quantityChange);
+                }
+
+                this.removeFieldErrors(this.$quantity);
+                this.quantityChange = setTimeout(_.bind(this.onQuantityChange, this), 1500);
+            }, this));
+        },
+
+        /**
+         * @param {jQuery.Element|string} field
+         */
+        removeFieldErrors: function(field) {
+            var $field = $(field);
+            var $container = $field.parent();
+
+            $container.removeClass('validation-error');
+            $container.find('.error').removeClass('error');
+            $container.siblings('.validation-failed').hide().text('');
+        },
+
+        /**
+         * @param {jQuery.Element|string} field
+         * @param {String[]|String} messages
+         */
+        addFieldErrors: function(field, messages) {
+            var $field = $(field);
+            var $container = $field.parent();
+            var $errorContainer = $container.siblings('.validation-failed');
+
+            if (!$errorContainer.length) {
+                $errorContainer = $('<span class="validation-failed"></span>');
+                $container.after($errorContainer);
+            }
+
+            var text = _.isArray(messages) ? messages.join('; ') : messages;
+
+            if ($errorContainer.text().length) {
+                text = $errorContainer.text() + '; ' + text;
+            }
+
+            $errorContainer.text(text).show();
+            $field.addClass('error');
+            $container.addClass('validation-error');
         },
 
         /**
@@ -76,6 +180,7 @@ define(function(require) {
          * @param {String} unit
          */
         updateUnitValue: function(unit) {
+            this.$unitInput.val(unit);
             this.$unit.text(unit);
         },
 
@@ -84,6 +189,13 @@ define(function(require) {
          */
         updateUnitPriceValue: function(price) {
             this.$unitPrice.text(price);
+        },
+
+        /**
+         * @param {Integer} id
+         */
+        updateSelector: function(id) {
+            $(this.options.offerSelector + '[value="' + id + '"]').prop('checked', 'checked');
         },
 
         dispose: function() {
