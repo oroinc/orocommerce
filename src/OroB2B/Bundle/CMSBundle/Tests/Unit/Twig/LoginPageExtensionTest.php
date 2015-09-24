06001299d012f3658d55f59d71c950980abe23ff
dmalavsky <dmalavsky@grossum.com>
2015-09-24 14:25:36 +0300

    BB-956: Apply loginPage settings for login form     - apply setting to login form

--- src/OroB2B/Bundle/CMSBundle/Tests/Unit/Twig/LoginPageExtensionTest.php
+++ src/OroB2B/Bundle/CMSBundle/Tests/Unit/Twig/LoginPageExtensionTest.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\Persistence\ObjectRepository;
 
 use Oro\Bundle\AttachmentBundle\Entity\File;
+
 use OroB2B\Bundle\CMSBundle\Entity\LoginPage;
 use OroB2B\Bundle\CMSBundle\Twig\LoginPageExtension;
 
@@ -22,15 +23,16 @@ class LoginPageExtensionTest extends \PHPUnit_Framework_TestCase
      */
     protected $loginPage;
 
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|ManagerRegistry
+     */
+    protected $managerRegistry;
+
     public function setUp()
     {
-        /** @var \PHPUnit_Framework_MockObject_MockObject|ManagerRegistry $managerRegistry */
-        $managerRegistry = $this->getMockBuilder('Doctrine\Common\Persistence\ManagerRegistry')
+        $this->managerRegistry = $this->getMockBuilder('Doctrine\Common\Persistence\ManagerRegistry')
             ->disableOriginalConstructor()
             ->getMock();
-
-        /** @var \PHPUnit_Framework_MockObject_MockObject|ObjectRepository $objectRepository */
-        $objectRepository = $this->getMock('Doctrine\Common\Persistence\ObjectRepository');
 
         $backgroundFile = new File();
         $logoFile = new File();
@@ -40,28 +42,7 @@ class LoginPageExtensionTest extends \PHPUnit_Framework_TestCase
         $this->loginPage->setBottomContent('bottom');
         $this->loginPage->setCss('css');
 
-        $this->loginPage->setBackgroundImage($backgroundFile);
-        $this->loginPage->setLogoImage($logoFile);
-
-        $objectRepository->expects($this->any())
-            ->method('findOneBy')
-            ->with([])
-            ->willReturn($this->loginPage);
-
-        /** @var \PHPUnit_Framework_MockObject_MockObject|ObjectManager $objectManager */
-        $objectManager = $this->getMock('Doctrine\Common\Persistence\ObjectManager');
-
-        $objectManager->expects($this->exactly(1))
-            ->method('getRepository')
-            ->with('OroB2BCMSBundle:LoginPage')
-            ->willReturn($objectRepository);
-
-        $managerRegistry->expects($this->exactly(1))
-            ->method('getManagerForClass')
-            ->with('OroB2BCMSBundle:LoginPage')
-            ->willReturn($objectManager);
-
-        $this->extension = new LoginPageExtension($managerRegistry);
+        $this->extension = new LoginPageExtension($this->managerRegistry);
     }
 
     public function testGetName()
@@ -69,34 +50,47 @@ class LoginPageExtensionTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(LoginPageExtension::NAME, $this->extension->getName());
     }
 
-    /**
-     * Test related method
-     */
     public function testGetFunctions()
     {
         $functionList = [
-            ['orob2b_login_css', 'getCss'],
-            ['orob2b_login_top_content', 'getTopContent'],
-            ['orob2b_login_bottom_content', 'getBottomContent'],
-            ['orob2b_login_logo_image', 'getLogoImage'],
-            ['orob2b_login_background_image', 'getBackgroundImage'],
+            ['orob2b_login_page', 'getDefaultLoginPage']
         ];
 
         $functions = $this->extension->getFunctions();
         $this->assertCount(count($functionList), $functions);
 
-
         for ($i = 0; $i < count($functionList); $i++) {
             /** @var \Twig_SimpleFunction $function */
             $function = $functions[$i];
             $this->assertInstanceOf('\Twig_SimpleFunction', $function);
-            $this->assertEquals(key($functionList[$i][0]), $function->getName());
+            $this->assertEquals($functionList[$i][0], $function->getName());
             $this->assertEquals([$this->extension, $functionList[$i][1]], $function->getCallable());
         }
     }
 
-    public function testGetCss()
+    public function testGetDefaultLoginPage()
     {
+        /** @var \PHPUnit_Framework_MockObject_MockObject|ObjectRepository $objectRepository */
+        $objectRepository = $this->getMock('Doctrine\Common\Persistence\ObjectRepository');
 
+        $objectRepository->expects($this->once())
+            ->method('findOneBy')
+            ->with([])
+            ->willReturn($this->loginPage);
+
+        /** @var \PHPUnit_Framework_MockObject_MockObject|ObjectManager $objectManager */
+        $objectManager = $this->getMock('Doctrine\Common\Persistence\ObjectManager');
+
+        $objectManager->expects($this->once())
+            ->method('getRepository')
+            ->with('OroB2BCMSBundle:LoginPage')
+            ->willReturn($objectRepository);
+
+        $this->managerRegistry->expects($this->once())
+            ->method('getManagerForClass')
+            ->with('OroB2BCMSBundle:LoginPage')
+            ->willReturn($objectManager);
+
+        $this->extension->getDefaultLoginPage();
     }
 }
--- src/OroB2B/Bundle/CMSBundle/Twig/LoginPageExtension.php
+++ src/OroB2B/Bundle/CMSBundle/Twig/LoginPageExtension.php
@@ -4,7 +4,7 @@ namespace OroB2B\Bundle\CMSBundle\Twig;
 
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\Common\Persistence\ObjectRepository;
-use Oro\Bundle\AttachmentBundle\Entity\File;
+
 use OroB2B\Bundle\CMSBundle\Entity\LoginPage;
 
 class LoginPageExtension extends \Twig_Extension
@@ -39,68 +39,14 @@ class LoginPageExtension extends \Twig_Extension
     public function getFunctions()
     {
         return [
-            new \Twig_SimpleFunction('orob2b_login_css', [$this, 'getCss']),
-            new \Twig_SimpleFunction('orob2b_login_top_content', [$this, 'getTopContent']),
-            new \Twig_SimpleFunction('orob2b_login_bottom_content', [$this, 'getBottomContent']),
-            new \Twig_SimpleFunction('orob2b_login_logo_image', [$this, 'getLogoImage']),
-            new \Twig_SimpleFunction('orob2b_login_background_image', [$this, 'getBackgroundImage']),
+            new \Twig_SimpleFunction('orob2b_login_page', [$this, 'getDefaultLoginPage'], ['is_safe' => ['html']]),
         ];
-    }
-
-    /**
-     * @return string
-     */
-    public function getCss()
-    {
-        return $this->getDefaultLoginPage()->getCss();
-    }
-
-    /**
-     * @return string
-     */
-    public function getTopContent()
-    {
-        return $this->getDefaultLoginPage()->getTopContent();
-    }
-
-    /**
-     * @return string
-     */
-    public function getBottomContent()
-    {
-        return $this->getDefaultLoginPage()->getBottomContent();
-    }
-
-    /**
-     * @return File
-     */
-    public function getLogoImage()
-    {
-        $image = $this->getDefaultLoginPage()->getLogoImage();
-        if (!$image) {
-            return;
-        }
-
-        return $image->getFilename();
-    }
-
-    /**
-     * @return File
-     */
-    public function getBackgroundImage()
-    {
-        $image = $this->getDefaultLoginPage()->getBackgroundImage();
-        if (!$image) {
-            return;
-        }
-
-        return $image->getFilename();
     }
 
     /**
      * @return LoginPage
      */
-    protected function getDefaultLoginPage()
+    public function getDefaultLoginPage()
     {
         // Get one default login page
         return $this->getLoginPageRepository()->findOneBy([]);
