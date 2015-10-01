d0ae10fc942535ae094543e973dddbcd40b66421
Mykhailo Sulyma <msulima@magecore.com>
2015-10-01 14:46:26 +0300

    BB-1279: Modify list of rendered categories in tree on product grid on frontend  - update category tree handler  - tests

--- src/OroB2B/Bundle/CatalogBundle/JsTree/CategoryTreeHandler.php
+++ src/OroB2B/Bundle/CatalogBundle/JsTree/CategoryTreeHandler.php
@@ -2,20 +2,60 @@
 
 namespace OroB2B\Bundle\CatalogBundle\JsTree;
 
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+use Oro\Bundle\SecurityBundle\SecurityFacade;
+use Oro\Bundle\UserBundle\Entity\User;
+
+use OroB2B\Bundle\AccountBundle\Entity\AccountUser;
+use OroB2B\Bundle\AccountBundle\Storage\CategoryVisibilityStorage;
 use OroB2B\Bundle\CatalogBundle\Entity\Category;
 use OroB2B\Component\Tree\Handler\AbstractTreeHandler;
 
 class CategoryTreeHandler extends AbstractTreeHandler
 {
+    /** @var SecurityFacade */
+    protected $securityFacade;
+
+    /** @var CategoryVisibilityStorage */
+    protected $categoryVisibilityStorage;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param SecurityFacade $securityFacade
+     * @param CategoryVisibilityStorage $categoryVisibilityStorage
+     */
+    public function __construct(
+        $entityClass,
+        ManagerRegistry $managerRegistry,
+        SecurityFacade $securityFacade,
+        CategoryVisibilityStorage $categoryVisibilityStorage
+    ) {
+        parent::__construct($entityClass, $managerRegistry);
+
+        $this->securityFacade = $securityFacade;
+        $this->categoryVisibilityStorage = $categoryVisibilityStorage;
+    }
+
     /**
      * @return array
      */
     public function createTree()
     {
-        $tree = $this->getEntityRepository()
-            ->getChildrenWithTitles(null, false, 'left', 'ASC');
+        $categories = $this->getEntityRepository()->getChildrenWithTitles(null, false, 'left', 'ASC');
+        $categories = $this->formatTree($categories);
 
-        return $this->formatTree($tree);
+        $user = $this->securityFacade->getLoggedUser();
+        if (!$user instanceof User) {
+            /** @var AccountUser $user */
+            $categories = $this->filterCategories(
+                $categories,
+                $user instanceof AccountUser ? $user->getAccount()->getId() : null
+            );
+        }
+
+        return $categories;
     }
 
     /**
@@ -67,5 +107,79 @@ class CategoryTreeHandler extends AbstractTreeHandler
                 'opened' => $entity->getParentCategory() === null
             ]
         ];
+    }
+
+    /**
+     * @param array|Category[] $categories
+     * @param int|null $accountId
+     * @return array
+     */
+    protected function filterCategories(array $categories, $accountId = null)
+    {
+        $visibilityData = $this->categoryVisibilityStorage->getCategoryVisibilityData($accountId);
+
+        $isVisible = $visibilityData->isVisible();
+        $ids = $visibilityData->getIds();
+
+        $idsToUnset = [];
+
+        $tree = $this->prepareTree($categories);
+        foreach ($tree as $item) {
+            if (($isVisible && !in_array($item['id'], $ids, true)) ||
+                (!$isVisible && in_array($item['id'], $ids, true))
+            ) {
+                $this->collectIdsToUnset($item, $idsToUnset);
+            }
+        }
+
+        $idsToUnset = array_unique($idsToUnset);
+
+        foreach ($tree as $key => &$item) {
+            if (in_array($item['id'], $idsToUnset, true)) {
+                unset($tree[$key]);
+            }
+
+            unset($item['children']);
+        }
+
+        return array_values($tree);
+    }
+
+    /**
+     * @param array $categories
+     * @return array
+     */
+    protected function prepareTree(array $categories)
+    {
+        $tree = [];
+
+        foreach ($categories as $category) {
+            $tree[$category['id']] = $category;
+        }
+
+        foreach ($tree as &$category) {
+            $parentId = $category['parent'];
+
+            if ($parentId && $parentId !== '#') {
+                $tree[$category['parent']]['children'][] = &$category;
+            }
+        }
+
+        return $tree;
+    }
+
+    /**
+     * @param array $category
+     * @param array $idsToUnset
+     */
+    protected function collectIdsToUnset(array $category, array &$idsToUnset)
+    {
+        $idsToUnset[] = $category['id'];
+
+        if (array_key_exists('children', $category)) {
+            foreach ($category['children'] as $child) {
+                $this->collectIdsToUnset($child, $idsToUnset);
+            }
+        }
     }
 }
