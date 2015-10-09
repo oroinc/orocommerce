9511b26be5043124b38182dd292f63bdf5d2335e
Oleksandr Nechyporenko <onechyporenko@magecore.com>
2015-10-09 11:44:12 +0300

    BB-1305: Create empty page to edit product visibility  - add ACL  - change URL

--- src/OroB2B/Bundle/AccountBundle/Controller/ProductVisibilityController.php
+++ src/OroB2B/Bundle/AccountBundle/Controller/ProductVisibilityController.php
@@ -5,6 +5,8 @@ namespace OroB2B\Bundle\AccountBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Oro\Bundle\SecurityBundle\Annotation\Acl;
+
 use OroB2B\Bundle\ProductBundle\Entity\Product;
 
 class ProductVisibilityController
@@ -12,6 +14,12 @@ class ProductVisibilityController
     /**
      * @Route("/edit/{id}", name="orob2b_account_product_visibility_edit", requirements={"id"="\d+"})
      * @Template
+     * @Acl(
+     *      id="orob2b_product_visibility_edit",
+     *      type="entity",
+     *      class="OroB2BProductBundle:Product",
+     *      permission="EDIT"
+     * )
      *
      * @param Product $product
      * @return array
