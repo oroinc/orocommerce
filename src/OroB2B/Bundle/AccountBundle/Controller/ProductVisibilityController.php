60a77190adad77a94f84006641e2de3e4fd3d93c
Oleksandr Nechyporenko <onechyporenko@magecore.com>
2015-10-12 19:09:45 +0300

    BB-1310: Create empty widget controller action  - add product argument to websiteWidgetAction

--- src/OroB2B/Bundle/AccountBundle/Controller/ProductVisibilityController.php
+++ src/OroB2B/Bundle/AccountBundle/Controller/ProductVisibilityController.php
@@ -4,6 +4,7 @@ namespace OroB2B\Bundle\AccountBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 
@@ -28,17 +29,24 @@ class ProductVisibilityController
     }
 
     /**
-     * @Route("/website/{id}", name="orob2b_account_product_visibility_website_edit", requirements={"id"="\d+"})
+     * @Route(
+     *      "/edit/{productId}/website/{id}",
+     *      name="orob2b_account_product_visibility_website",
+     *      requirements={"productId"="\d+", "id"="\d+"}
+     * )
+     * @ParamConverter("product", options={"id" = "productId"})
      * @Template("OroB2BAccountBundle:ProductVisibility/widget:website.html.twig")
      * @AclAncestor("orob2b_website_view")
      *
+     * @param Product $product
      * @param Website $website
      * @return array
      */
-    public function websiteAction(Website $website)
+    public function websiteWidgetAction(Product $product, Website $website)
     {
         return [
-            'entity' => $website,
+            'product' => $product,
+            'website' => $website,
         ];
     }
 }
