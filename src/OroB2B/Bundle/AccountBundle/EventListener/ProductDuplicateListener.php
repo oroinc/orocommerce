1a2af23feb047b9f19296bc68cead76a65e0d43d
Roman Yarmolenko <devxypro@gmail.com>
2015-10-19 17:37:01 +0300

    BB-1402: Add ability to duplicate visibility in duplicated product - update service listener

--- src/OroB2B/Bundle/AccountBundle/EventListener/ProductDuplicateListener.php
+++ src/OroB2B/Bundle/AccountBundle/EventListener/ProductDuplicateListener.php
@@ -2,12 +2,36 @@
 
 namespace OroB2B\Bundle\AccountBundle\EventListener;
 
+use Doctrine\Bundle\DoctrineBundle\Registry;
+
 use OroB2B\Bundle\ProductBundle\Event\ProductDuplicateAfterEvent;
 
 class ProductDuplicateListener
 {
+    /** @var Registry */
+    protected $doctrine;
+
+    /** @var string */
+    protected $className;
+
+    /**
+     * @param Registry $registry
+     */
+    public function __construct(Registry $registry)
+    {
+        $this->doctrine = $registry;
+    }
+
     public function onDuplicateProduct(ProductDuplicateAfterEvent $event)
     {
-        
+
+    }
+
+    /**
+     * @param string $className
+     */
+    public function setClassName($className)
+    {
+        $this->className = $className;
     }
 }
