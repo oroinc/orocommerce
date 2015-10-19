b35c26fa74fe2fea48b460866f25face045d1512
Dmitry Khrysev <x86demon@gmail.com>
2015-10-19 14:56:08 +0300

    BB-1380: Add functional tests

--- src/OroB2B/Bundle/FrontendBundle/Tests/Functional/Controller/FrontendControllerTest.php
+++ src/OroB2B/Bundle/FrontendBundle/Tests/Functional/Controller/FrontendControllerTest.php
@@ -6,8 +6,13 @@ use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 
 use Oro\Component\Testing\Fixtures\LoadAccountUserData;
 
+/**
+ * @dbIsolation
+ */
 class FrontendControllerTest extends WebTestCase
 {
+    const FRONTEND_THEME_CONFIG_KEY = 'oro_b2b_frontend.frontend_theme';
+
     public function testRedirectToLogin()
     {
         $this->initClient();
@@ -31,6 +36,32 @@ class FrontendControllerTest extends WebTestCase
         $crawler = $this->client->followRedirect();
         $this->assertNotContains($this->getBackendPrefix(), $crawler->html());
         $this->assertEquals('Products', $crawler->filter('h1.oro-subtitle')->html());
+    }
+
+    /**
+     * @depends testRedirectToLogin
+     */
+    public function testThemeSwitch()
+    {
+        $this->initClient([], [], true);
+        
+        // Switch to layout theme
+        $configManager = $this->getContainer()->get('oro_config.manager');
+        $this->assertEmpty($configManager->get(self::FRONTEND_THEME_CONFIG_KEY));
+        $configManager->set(self::FRONTEND_THEME_CONFIG_KEY, 'default');
+        $configManager->flush();
+
+        $this->client->request('GET', $this->getUrl('_frontend'));
+        $crawler = $this->client->followRedirect();
+        $this->assertEquals('Hello World!', $crawler->filter('title')->html());
+
+        // Check that after selecting of layout there is an ability to switch to oro theme
+        $configManager->set(self::FRONTEND_THEME_CONFIG_KEY, '');
+        $configManager->flush();
+
+        $this->client->request('GET', $this->getUrl('_frontend'));
+        $crawler = $this->client->followRedirect();
+        $this->assertEquals('Login', $crawler->filter('h2.title')->html());
     }
 
     /**
