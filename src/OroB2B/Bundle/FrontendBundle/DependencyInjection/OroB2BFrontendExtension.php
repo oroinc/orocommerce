cc6282d9c7399c0788996239df99959187e7e68e
Yevhen Shyshkin <yshyshkin@magecore.com>
2015-10-20 13:34:07 +0300

    BB-1371: Move security configuration to specific bundles in platform  - fixed security configuration build  - updated unit tests

--- src/OroB2B/Bundle/FrontendBundle/DependencyInjection/OroB2BFrontendExtension.php
+++ src/OroB2B/Bundle/FrontendBundle/DependencyInjection/OroB2BFrontendExtension.php
@@ -3,31 +3,18 @@
 namespace OroB2B\Bundle\FrontendBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader;
 
-use Oro\Bundle\SecurityBundle\DependencyInjection\Extension\SecurityExtensionHelper;
-use Oro\Component\DependencyInjection\ExtendedContainerBuilder;
-
-class OroB2BFrontendExtension extends Extension implements PrependExtensionInterface
+class OroB2BFrontendExtension extends Extension
 {
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function prepend(ContainerBuilder $container)
-    {
-        /** @var ExtendedContainerBuilder $container */
-        SecurityExtensionHelper::makeFirewallLatest($container, 'frontend');
     }
 }
