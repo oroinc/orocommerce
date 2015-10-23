b3289a2c7e2c25b03361152d6c4c47721bbae7d4
Andrey Yatsenco <yatsenco@gmail.com>
2015-10-23 11:52:24 +0300

    BB-1077: Update form type to handle product row collection - removed unused use statements

--- src/OroB2B/Bundle/ProductBundle/Form/Type/ProductRowCollectionType.php
+++ src/OroB2B/Bundle/ProductBundle/Form/Type/ProductRowCollectionType.php
@@ -6,8 +6,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 use Oro\Bundle\FormBundle\Form\Type\CollectionType;
