9658ec71fed2152c16f35ebea048d2265bebff54
Roman Yarmolenko <devxypro@gmail.com>
2015-10-23 12:09:38 +0300

    BB-1313: Functional tests - duplicate product visibility test

--- src/OroB2B/Bundle/AccountBundle/Tests/Functional/Controller/ProductVisibilityControllerTest.php
+++ src/OroB2B/Bundle/AccountBundle/Tests/Functional/Controller/ProductVisibilityControllerTest.php
@@ -2,22 +2,70 @@
 
 namespace OroB2B\Bundle\AccountBundle\Tests\Functional\Controller;
 
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+use Doctrine\ORM\EntityManager;
+
 use Oro\Component\Testing\WebTestCase;
 
+use OroB2B\Bundle\AccountBundle\Entity\Visibility\AccountGroupProductVisibility;
+use OroB2B\Bundle\AccountBundle\Entity\Visibility\AccountProductVisibility;
+use OroB2B\Bundle\AccountBundle\Entity\Visibility\ProductVisibility;
+use OroB2B\Bundle\AccountBundle\Entity\Account;
+use OroB2B\Bundle\AccountBundle\Entity\AccountGroup;
+use OroB2B\Bundle\AccountBundle\Tests\Functional\DataFixtures\LoadAccounts;
+use OroB2B\Bundle\AccountBundle\Tests\Functional\DataFixtures\LoadGroups;
 use OroB2B\Bundle\ProductBundle\Entity\Product;
 use OroB2B\Bundle\ProductBundle\Tests\Functional\DataFixtures\LoadProducts;
 use OroB2B\Bundle\WebsiteBundle\Migrations\Data\ORM\LoadWebsiteData;
 use OroB2B\Bundle\WebsiteBundle\Tests\Functional\DataFixtures\LoadWebsiteData as TestFixturesLoadWebsiteData;
-use Symfony\Component\DomCrawler\Crawler;
-use Symfony\Component\PropertyAccess\PropertyAccess;
 
 /**
  * @dbIsolation
  */
 class ProductVisibilityControllerTest extends WebTestCase
 {
+    const ALL_KEY = 'all';
+    const ACCOUNT_KEY = 'account';
+    const ACCOUNT_GROUP_KEY = 'accountGroup';
+    const VISIBILITY_KEY = 'visibility';
+    const FORM_TYPE_NAME = 'orob2b_account_website_scoped_data_type';
+    const VISIBLE = 'visible';
+    const HIDDEN = 'hidden';
+    const PRODUCT_VISIBILITY_CLASS = 'OroB2BAccountBundle:Visibility\ProductVisibility';
+    const ACCOUNT_PRODUCT_VISIBILITY_CLASS = 'OroB2BAccountBundle:Visibility\AccountProductVisibility';
+    const ACCOUNT_GROUP_PRODUCT_VISIBILITY_CLASS = 'OroB2BAccountBundle:Visibility\AccountGroupProductVisibility';
+
     /** @var  Product */
     protected $product;
+
+    /** @var  Account */
+    protected $account;
+
+    /** @var  AccountGroup */
+    protected $group;
+
+    /** @var  integer[] */
+    protected $websiteIds = [];
+
+    /** @var  string */
+    protected $visibilityToAllDefaultWebsite;
+
+    /** @var  string */
+    protected $visibilityToAccountDefaultWebsite;
+
+    /** @var  string */
+    protected $visibilityToAccountGroupDefaultWebsite;
+
+    /** @var  string */
+    protected $visibilityToAllNotDefaultWebsite;
+
+    /** @var  string */
+    protected $visibilityToAccountNotDefaultWebsite;
+
+    /** @var  string */
+    protected $visibilityToAccountGroupNotDefaultWebsite;
 
     protected function setUp()
     {
@@ -31,44 +79,216 @@ class ProductVisibilityControllerTest extends WebTestCase
         );
 
         $this->product = $this->getReference(LoadProducts::PRODUCT_1);
+        $this->account = $this->getReference(LoadAccounts::DEFAULT_ACCOUNT_NAME);
+        $this->group = $this->getReference(LoadGroups::GROUP1);
+        $this->visibilityToAllDefaultWebsite = self::VISIBLE;
+        $this->visibilityToAccountDefaultWebsite = json_encode(
+            [$this->account->getId() => [self::VISIBILITY_KEY => self::VISIBLE]]
+        );
+        $this->visibilityToAccountGroupDefaultWebsite = json_encode(
+            [$this->group->getId() => [self::VISIBILITY_KEY => self::VISIBLE]]
+        );
+
+        $this->visibilityToAllNotDefaultWebsite = self::HIDDEN;
+        $this->visibilityToAccountNotDefaultWebsite = json_encode(
+            [$this->account->getId() => [self::VISIBILITY_KEY => self::HIDDEN]]
+        );
+        $this->visibilityToAccountGroupNotDefaultWebsite = json_encode(
+            [$this->group->getId() => [self::VISIBILITY_KEY => self::HIDDEN]]
+        );
     }
 
-    public function testView()
+    public function testUpdate()
     {
         $this->submitForm();
+        $crawler = $this->getVisibilityPage();
+        $form = $crawler->selectButton('Save and Close')->form();
+        $parameters = $this->explodeArrayPaths($form->getValues());
+
+        //first tab checks
+        $defaultWebsiteParams = $parameters[self::FORM_TYPE_NAME][$this->websiteIds[0]];
+        $this->assertEquals(
+            $defaultWebsiteParams[self::ALL_KEY],
+            $this->visibilityToAllDefaultWebsite
+        );
+        $this->assertEquals(
+            $defaultWebsiteParams[self::ACCOUNT_KEY],
+            $this->visibilityToAccountDefaultWebsite
+        );
+        $this->assertEquals(
+            $defaultWebsiteParams[self::ACCOUNT_GROUP_KEY],
+            $this->visibilityToAccountGroupDefaultWebsite
+        );
+
+        //second tab checks
+        $notDefaultWebsiteVisibilities = $this->getSelectedVisibilitiesThroughAjax(
+            $this->product->getId(),
+            $this->websiteIds[1]
+        );
+        $this->assertEquals($notDefaultWebsiteVisibilities[self::ALL_KEY], $this->visibilityToAllNotDefaultWebsite);
+        $this->assertEquals(
+            $notDefaultWebsiteVisibilities[self::ACCOUNT_KEY],
+            $this->visibilityToAccountNotDefaultWebsite
+        );
+        $this->assertEquals(
+            $notDefaultWebsiteVisibilities[self::ACCOUNT_GROUP_KEY],
+            $this->visibilityToAccountGroupNotDefaultWebsite
+        );
     }
 
+    /**
+     * @depends testUpdate
+     */
+    public function testDuplicateProduct()
+    {
+        $this->client->followRedirects(true);
+        $this->client->request(
+            'GET',
+            $this->getUrl('orob2b_product_duplicate', ['id' => $this->product->getId()])
+        );
+
+        $result = $this->client->getResponse();
+        $this->assertHtmlResponseStatusCodeEquals($result, 200);
+        /** @var EntityManager $em */
+        $em = $this->client->getContainer()->get('doctrine')->getManager();
+        $duplicatedProduct = $em->getRepository('OroB2BProductBundle:Product')
+            ->findOneBySku(sprintf('%s-1', $this->product->getSku()));
+        $classNames = [
+            self::PRODUCT_VISIBILITY_CLASS,
+            self::ACCOUNT_PRODUCT_VISIBILITY_CLASS,
+            self::ACCOUNT_GROUP_PRODUCT_VISIBILITY_CLASS,
+        ];
+        foreach ($classNames as $className) {
+            $visibilities = $em->getRepository($className)->findBy(['product' => $duplicatedProduct]);
+            $this->assertCount(2, $visibilities);
+            foreach ($visibilities as $visibility) {
+                $em->remove($visibility);
+            }
+        }
+        $em->flush();
+    }
+
+    /**
+     * @depends testDuplicateProduct
+     */
+    public function testDeleteVisibilityOnSetDefault()
+    {
+        /** @var EntityManager $em */
+        $em = $this->client->getContainer()->get('doctrine')->getManager();
+        $productVisibilityRepo = $em->getRepository(self::PRODUCT_VISIBILITY_CLASS);
+        $accountProductVisibilityRepo = $em->getRepository(self::ACCOUNT_PRODUCT_VISIBILITY_CLASS);
+        $accountGroupProductVisibilityRepo = $em->getRepository(self::ACCOUNT_GROUP_PRODUCT_VISIBILITY_CLASS);
+
+        $this->assertCount(2, $productVisibilityRepo->findAll());
+        $this->assertCount(2, $accountProductVisibilityRepo->findAll());
+        $this->assertCount(2, $accountGroupProductVisibilityRepo->findAll());
+
+        $this->visibilityToAllDefaultWebsite = ProductVisibility::getDefault($this->product);
+        $this->visibilityToAccountDefaultWebsite = json_encode(
+            [$this->account->getId() => [self::VISIBILITY_KEY => AccountProductVisibility::getDefault($this->product)]]
+        );
+        $this->visibilityToAccountGroupDefaultWebsite = json_encode(
+            [
+                $this->group->getId() => [
+                    self::VISIBILITY_KEY => AccountGroupProductVisibility::getDefault($this->product),
+                ],
+            ]
+        );
+
+        $this->visibilityToAllNotDefaultWebsite = $this->visibilityToAllDefaultWebsite;
+        $this->visibilityToAccountDefaultWebsite = $this->visibilityToAccountNotDefaultWebsite;
+        $this->visibilityToAccountGroupDefaultWebsite = $this->visibilityToAccountGroupNotDefaultWebsite;
+
+        $this->submitForm();
+
+        $this->assertCount(0, $productVisibilityRepo->findAll());
+        $this->assertCount(0, $accountProductVisibilityRepo->findAll());
+        $this->assertCount(0, $accountGroupProductVisibilityRepo->findAll());
+    }
+
+    /**
+     * @return Crawler
+     */
     protected function submitForm()
     {
-        $crawler = $this->client->request(
-            'GET',
-            $this->getUrl('orob2b_account_product_visibility_edit', ['id' => $this->product->getId()])
-        );
-        $this->assertHtmlResponseStatusCodeEquals($this->client->getResponse(), 200);
+        $crawler = $this->getVisibilityPage();
 
         $websiteTitles = [
             LoadWebsiteData::DEFAULT_WEBSITE_NAME,
             TestFixturesLoadWebsiteData::WEBSITE1,
             TestFixturesLoadWebsiteData::WEBSITE2,
         ];
-        $websiteIds = [];
         $counter = 0;
+
         // Test exits tabs
         $crawler->filterXPath('//div[@class="oro-tabs"]//li')->each(
-            function (Crawler $node) use (&$counter, &$websiteIds, $websiteTitles) {
+            function (Crawler $node) use (&$counter, $websiteTitles) {
                 $this->assertContains($websiteTitles[$counter], $node->text());
                 $options = json_decode($node->filterXPath('//a/@data-page-component-options')->text(), true);
-                $websiteIds[] = $options['options']['alias'];
+                $this->websiteIds[] = $options['options']['alias'];
                 $counter++;
             }
         );
+        $parameters[self::FORM_TYPE_NAME] =
+            [
+                $this->websiteIds[0] => [
+                    self::ALL_KEY => $this->visibilityToAllDefaultWebsite,
+                    self::ACCOUNT_KEY => $this->visibilityToAccountDefaultWebsite,
+                    self::ACCOUNT_GROUP_KEY => $this->visibilityToAccountGroupDefaultWebsite,
+                ],
+                $this->websiteIds[1] => [
+                    self::ALL_KEY => $this->visibilityToAllNotDefaultWebsite,
+                    self::ACCOUNT_KEY => $this->visibilityToAccountNotDefaultWebsite,
+                    self::ACCOUNT_GROUP_KEY => $this->visibilityToAccountGroupNotDefaultWebsite,
+                ],
+                '_token' => $crawler->filterXPath(sprintf('//input[@name="%s[_token]"]/@value', self::FORM_TYPE_NAME))
+                    ->text(),
+            ];
+        $crawler = $this->client->request(
+            'POST',
+            $this->getUrl('orob2b_account_product_visibility_edit', ['id' => $this->product->getId()]),
+            $parameters
+        );
+        $response = $this->client->getResponse();
+        $this->assertHtmlResponseStatusCodeEquals($response, 200);
 
-        $form = $crawler->selectButton('Save and Close')->form();
-        $parameters = $this->explodeArrayPaths($form->getValues());
-        $token = $crawler->filterXPath('//input[@name="orob2b_account_website_scoped_data_type[_token]"]/@value')
-            ->text();
+        return $crawler;
+    }
 
+    /**
+     * @param $productId
+     * @param $websiteId
+     * @return array
+     */
+    protected function getSelectedVisibilitiesThroughAjax($productId, $websiteId)
+    {
+        $crawler = $this->client->request(
+            'GET',
+            $this->getUrl(
+                'orob2b_account_product_visibility_website',
+                ['productId' => $productId, 'id' => $websiteId]
+            )
+        );
 
+        return [
+            self::ALL_KEY => $crawler
+                ->filterXPath(
+                    sprintf(
+                        '//select[@name="%s[%d][all]"]/option[@selected]/@value',
+                        self::FORM_TYPE_NAME,
+                        $websiteId
+                    )
+                )
+                ->text(),
+            self::ACCOUNT_KEY => $this->getChangeSetRawData(
+                $crawler,
+                sprintf('account-product-visibility-changeset-%d', $websiteId)
+            ),
+            self::ACCOUNT_GROUP_KEY => $this->getChangeSetRawData(
+                $crawler,
+                sprintf('accountgroup-product-visibility-changeset-%d', $websiteId)
+            ),
+        ];
     }
 
     /**
@@ -83,10 +303,37 @@ class ProductVisibilityControllerTest extends WebTestCase
             if (!$pos = strpos($key, '[')) {
                 continue;
             }
-            $key = '['.substr($key, 0, $pos).']'.substr($key, $pos);
+            $key = '[' . substr($key, 0, $pos) . ']' . substr($key, $pos);
             $accessor->setValue($parameters, $key, $val);
         }
 
         return $parameters;
+    }
+
+    /**
+     * @param Crawler $crawler
+     * @param string $changeSetId
+     * @return string
+     */
+    protected function getChangeSetRawData(Crawler $crawler, $changeSetId)
+    {
+        return $crawler->filterXPath(
+            sprintf('//input[@id="%s"]/@value', $changeSetId)
+        )->text();
+    }
+
+    /**
+     * @return Crawler
+     */
+    protected function getVisibilityPage()
+    {
+        $this->client->followRedirects();
+        $crawler = $this->client->request(
+            'GET',
+            $this->getUrl('orob2b_account_product_visibility_edit', ['id' => $this->product->getId()])
+        );
+        $this->assertHtmlResponseStatusCodeEquals($this->client->getResponse(), 200);
+
+        return $crawler;
     }
 }
