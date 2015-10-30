b86bfbc893972160e23b6ab7c1099f1f69663d50
Oleg Popadko <oleg@popadko.com>
2015-10-30 12:12:15 +0200

    BB-1437: Update existing form types to set correct scope - add scope to ProductReplacment field of ProductSelectType

--- src/OroB2B/Bundle/SaleBundle/Form/Type/QuoteProductType.php
+++ src/OroB2B/Bundle/SaleBundle/Form/Type/QuoteProductType.php
@@ -167,6 +167,9 @@ class QuoteProductType extends AbstractType
                 'required' => false,
                 'label' => 'orob2b.sale.quoteproduct.product_replacement.label',
                 'create_enabled' => false,
+                'data_parameters' => [
+                    'scope' => 'quote'
+                ]
             ])
             ->add('productReplacementSku', 'text', [
                 'required' => false,
