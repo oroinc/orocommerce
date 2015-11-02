c18defa5dca6ff98af33845813795c71db7c02bf
Ilya Antipenko <iantipenko@grossum.com>
2015-11-02 12:51:31 +0200

    BB-1385: Add listener to add both backend and frontend audits to backend grid  - Replace original audit-grid and audit-history-grid with listeners

--- src/OroB2B/Bundle/AccountBundle/Audit/AuditDatagridListener.php
+++ src/OroB2B/Bundle/AccountBundle/Audit/AuditDatagridListener.php
@@ -2,17 +2,29 @@
 
 namespace OroB2B\Bundle\AccountBundle\Audit;
 
+use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
 use Oro\Bundle\DataGridBundle\Event\BuildBefore;
+use Oro\Bundle\DataGridBundle\Provider\ConfigurationProviderInterface;
 
 class AuditDatagridListener
 {
+    const CUSTOM_AUDIT_GRID = 'b2b-audit-grid';
+    const CUSTOM_HISTORY_AUDIT_GRID = 'b2b-audit-history-grid';
+
+    /**
+     * @param ConfigurationProviderInterface $configurationProvider
+     */
+    public function __construct(ConfigurationProviderInterface $configurationProvider)
+    {
+        $this->configurationProvider = $configurationProvider;
+    }
+
     /**
      * @param BuildBefore $event
      */
     public function onHistoryBuildBefore(BuildBefore $event)
     {
-        $config = $event->getConfig();
-        $config->offsetUnsetByPath('[source][query][select][3]');
+        $this->replaceConfiguration(static::CUSTOM_HISTORY_AUDIT_GRID, $event->getConfig());
     }
 
     /**
@@ -20,7 +32,21 @@ class AuditDatagridListener
      */
     public function onAuditBuildBefore(BuildBefore $event)
     {
-        $config = $event->getConfig();
-        $config->offsetUnsetByPath('[source][query][select][8]');
+        $this->replaceConfiguration(static::CUSTOM_AUDIT_GRID, $event->getConfig());
+    }
+
+    /**
+     * @param string                $gridName
+     * @param DatagridConfiguration $configuration
+     */
+    protected function replaceConfiguration($gridName, DatagridConfiguration $configuration)
+    {
+        $datagridConfiguration = $this->configurationProvider->getConfiguration($gridName);
+        foreach ($datagridConfiguration as $key => $value) {
+            // Don't replace name of original datagrid
+            if ($key !== 'name') {
+                $configuration->offsetSet($key, $value);
+            }
+        }
     }
 }
