724ae7905dbed653304e971d105f7bfdf59efb61
Oleg Popadko <oleg@popadko.com>
2015-11-02 17:07:34 +0200

    BB-1438: Product bundle uses autocomplete handler from PricingBundle - remove ProductRemovedSelectType, move his logic to ProductSelectType

--- src/OroB2B/Bundle/ProductBundle/Form/Type/ProductSelectType.php
+++ src/OroB2B/Bundle/ProductBundle/Form/Type/ProductSelectType.php
@@ -3,13 +3,37 @@
 namespace OroB2B\Bundle\ProductBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\ChoiceList\View\ChoiceView;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\TranslatorInterface;
 
+use OroB2B\Bundle\ProductBundle\Model\ProductHolderInterface;
 use Oro\Bundle\FormBundle\Form\Type\OroEntitySelectOrCreateInlineType;
 
 class ProductSelectType extends AbstractType
 {
     const NAME = 'orob2b_product_select';
+
+    /**
+     * @var TranslatorInterface
+     */
+    protected $translator;
+
+    /**
+     * @var array
+     */
+    protected $options;
+
+    /**
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
 
     /**
      * {@inheritdoc}
@@ -20,11 +44,12 @@ class ProductSelectType extends AbstractType
             [
                 'autocomplete_alias' => 'orob2b_product',
                 'create_form_route' => 'orob2b_product_create',
+                'empty_label' => 'orob2b.product.removed',
                 'configs' => [
                     'placeholder' => 'orob2b.product.form.choose',
                     'result_template_twig' => 'OroB2BProductBundle:Product:Autocomplete/result.html.twig',
                     'selection_template_twig' => 'OroB2BProductBundle:Product:Autocomplete/selection.html.twig',
-                ]
+                ],
             ]
         );
     }
@@ -43,5 +68,36 @@ class ProductSelectType extends AbstractType
     public function getParent()
     {
         return OroEntitySelectOrCreateInlineType::NAME;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        $form = $form->getParent();
+
+        /* @var $productHolder ProductHolderInterface */
+        $productHolder = $form ? $form->getData() : null;
+
+        if (!$productHolder instanceof ProductHolderInterface || !$productHolder->getEntityIdentifier()) {
+            return;
+        }
+
+        if (!$productHolder->getProduct()) {
+            $emptyValueTitle = $this->translator->trans(
+                $options['empty_label'],
+                ['{title}' => $productHolder->getProductSku()]
+            );
+            $view->vars['configs']['placeholder'] = $emptyValueTitle;
+        }
     }
 }
