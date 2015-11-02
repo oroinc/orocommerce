d4ad9ceb7a169e70c7647ed8f9c0357e42c8a1a2
Dmitry Khrysev <x86demon@gmail.com>
2015-11-02 20:19:45 +0200

    BB-1426: Test and merge to master - CR changes

--- src/OroB2B/Bundle/ProductBundle/Tests/Functional/Form/Type/AbstractProductSelectTypeTest.php
+++ src/OroB2B/Bundle/ProductBundle/Tests/Functional/Form/Type/AbstractProductSelectTypeTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace OroB2B\Bundle\ProductBundle\Tests\Functional\Autocomplete;
+namespace OroB2B\Bundle\ProductBundle\Tests\Functional\Form\Type;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -8,13 +8,15 @@ use Symfony\Component\HttpFoundation\Response;
 
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 
+use OroB2B\Bundle\CatalogBundle\Tests\Functional\DataFixtures\LoadProductData;
+use OroB2B\Bundle\ProductBundle\Form\Type\ProductSelectType;
 use OroB2B\Bundle\ProductBundle\Entity\Manager\ProductManager;
 use OroB2B\Bundle\ProductBundle\Autocomplete\ProductVisibilityLimitedSearchHandler;
 
 /**
  * @dbIsolation
  */
-abstract class AbstractProductVisibilityLimitedSearchHandlerTest extends WebTestCase
+abstract class AbstractProductSelectTypeTest extends WebTestCase
 {
     const TEST_ENTITY_CLASS = 'OroB2B\Bundle\ProductBundle\Entity\Product';
 
@@ -23,12 +25,6 @@ abstract class AbstractProductVisibilityLimitedSearchHandlerTest extends WebTest
 
     /** @var ProductManager|\PHPUnit_Framework_MockObject_MockObject $productManager */
     protected $productManager;
-
-    /** @var string */
-    protected $scope = '';
-
-    /** @var string */
-    protected $configPath = '';
 
     /** @var array */
     protected $defaultConfigValue = ['in_stock', 'out_of_stock'];
@@ -41,9 +37,11 @@ abstract class AbstractProductVisibilityLimitedSearchHandlerTest extends WebTest
             ->disableOriginalConstructor()
             ->getMock();
 
-        $this->loadFixtures([
-            'OroB2B\Bundle\CatalogBundle\Tests\Functional\DataFixtures\LoadProductData'
-        ]);
+        $this->loadFixtures(
+            [
+                'OroB2B\Bundle\CatalogBundle\Tests\Functional\DataFixtures\LoadProductData',
+            ]
+        );
 
         $this->prepareConfig($this->defaultConfigValue);
     }
@@ -67,26 +65,44 @@ abstract class AbstractProductVisibilityLimitedSearchHandlerTest extends WebTest
             $this->getUrl(
                 'oro_form_autocomplete_search',
                 [
-                    'data_parameters' => [
-                        'scope' => $this->scope
+                    ProductSelectType::DATA_PARAMETERS => [
+                        'scope' => $this->getScope(),
                     ],
                     'name' => 'orob2b_product_visibility_limited',
                     'page' => 1,
                     'per_page' => 10,
-                    'query' => 'product'
+                    'query' => 'product',
                 ]
             )
         );
 
         $result = $this->client->getResponse();
-        $this->assertJsonResponseStatusCodeEquals($result, 200);
+        $this->checkProductsFromResponse($result, 'results', $expectedProducts);
+    }
 
-        $actualProducts = $this->getProductsFromResponse($result);
-        $this->assertCount(count($expectedProducts), $actualProducts);
+    /**
+     * @dataProvider searchDataProvider
+     * @param array $availableInventoryStatuses
+     * @param array $expectedProducts
+     */
+    public function testDatagridRestricting(array $availableInventoryStatuses, array $expectedProducts)
+    {
+        $this->prepareConfig($availableInventoryStatuses);
 
-        foreach ($actualProducts as $product) {
-            $this->assertContains($product, $expectedProducts);
-        }
+        $this->client->request(
+            'GET',
+            $this->getUrl(
+                'oro_datagrid_index',
+                ['gridName' => 'products-select-grid']
+            ),
+            [
+                ProductSelectType::DATA_PARAMETERS => [
+                    'scope' => $this->getScope(),
+                ],
+            ]
+        );
+        $result = $this->client->getResponse();
+        $this->checkProductsFromResponse($result, 'data', $expectedProducts);
     }
 
     /**
@@ -96,36 +112,36 @@ abstract class AbstractProductVisibilityLimitedSearchHandlerTest extends WebTest
     {
         return [
             [
-                'availableInventoryStatuses' => ['in_stock', 'out_of_stock' ],
+                'availableInventoryStatuses' => ['in_stock', 'out_of_stock'],
                 'expectedProducts' => [
-                    'test_product_01',
-                    'test_product_03',
-                ]
+                    LoadProductData::TEST_PRODUCT_01,
+                    LoadProductData::TEST_PRODUCT_03,
+                ],
             ],
             [
                 'availableInventoryStatuses' => ['in_stock'],
                 'expectedProducts' => [
-                    'test_product_01'
-                ]
+                    LoadProductData::TEST_PRODUCT_01,
+                ],
             ],
             [
                 'availableInventoryStatuses' => ['out_of_stock'],
                 'expectedProducts' => [
-                    'test_product_03',
-                ]
+                    LoadProductData::TEST_PRODUCT_03,
+                ],
             ],
             [
                 'availableInventoryStatuses' => ['discontinued'],
                 'expectedProducts' => [
-                    'test_product_04',
-                ]
+                    LoadProductData::TEST_PRODUCT_04,
+                ],
             ],
             [
                 'availableInventoryStatuses' => ['in_stock', 'discontinued'],
                 'expectedProducts' => [
-                    'test_product_01',
-                    'test_product_04'
-                ]
+                    LoadProductData::TEST_PRODUCT_01,
+                    LoadProductData::TEST_PRODUCT_04,
+                ],
             ],
         ];
     }
@@ -166,22 +182,36 @@ abstract class AbstractProductVisibilityLimitedSearchHandlerTest extends WebTest
     protected function prepareConfig(array $availableInventoryStatuses)
     {
         $configManager = $this->getContainer()->get('oro_config.global');
-        $configManager->set($this->configPath, $availableInventoryStatuses);
+        $configManager->set($this->getConfigPath(), $availableInventoryStatuses);
 
         $configManager->flush();
     }
 
     /**
      * @param Response $result
+     * @param string $dataFieldName
+     * @param string[] $expectedProducts
      * @return array
      */
-    protected function getProductsFromResponse(Response $result)
+    protected function checkProductsFromResponse(Response $result, $dataFieldName, array $expectedProducts)
     {
-        return array_map(
+        $this->assertJsonResponseStatusCodeEquals($result, 200);
+
+        $actualProducts = array_map(
             function ($result) {
                 return $result->sku;
             },
-            json_decode($result->getContent())->results
+            json_decode($result->getContent())->$dataFieldName
         );
+
+        foreach ($actualProducts as $product) {
+            $this->assertContains($product, $expectedProducts);
+        }
     }
+
+    /** @return string */
+    abstract public function getConfigPath();
+
+    /** @return string */
+    abstract public function getScope();
 }
