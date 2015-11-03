ebf45487afae70b2def7ecf75ed5cc92ff87aa42
Dmitry Khrysev <x86demon@gmail.com>
2015-11-03 11:53:37 +0200

    BB-1426: Test and merge to master

--- src/OroB2B/Bundle/ProductBundle/Model/ProductVisibilityQueryBuilderModifier.php
+++ src/OroB2B/Bundle/ProductBundle/Model/ProductVisibilityQueryBuilderModifier.php
@@ -38,7 +38,7 @@ class ProductVisibilityQueryBuilderModifier
 
         list($rootAlias) = $queryBuilder->getRootAliases();
 
-        $parameterName = uniqid($field, true);
+        $parameterName = str_replace('.', '', uniqid($field, true));
 
         $queryBuilder->andWhere($queryBuilder->expr()->in($rootAlias . '.' . $field, ':' . $parameterName))
             ->setParameter($parameterName, $in);
