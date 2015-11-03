13c525b169bafecc8e617a43e68082d40f059e6b
Ivan Shirko <ivan.shirko@effective-soft.com>
2015-11-03 19:50:28 +0300

    BB-1482: Hide Price Columns for Units by default - hide needed columns - update unit tests

--- src/OroB2B/Bundle/PricingBundle/Tests/Unit/EventListener/ProductPriceDatagridListenerTest.php
+++ src/OroB2B/Bundle/PricingBundle/Tests/Unit/EventListener/ProductPriceDatagridListenerTest.php
@@ -196,12 +196,14 @@ class ProductPriceDatagridListenerTest extends \PHPUnit_Framework_TestCase
                             'type' => 'twig',
                             'template' => 'OroB2BPricingBundle:Datagrid:Column/productUnitPrice.html.twig',
                             'frontend_type' => 'html',
+                            'renderable' => false,
                         ],
                         'price_column_eur_unit1' => [
                             'label' => 'orob2b.pricing.productprice.price_unit1_in_EUR.trans',
                             'type' => 'twig',
                             'template' => 'OroB2BPricingBundle:Datagrid:Column/productUnitPrice.html.twig',
                             'frontend_type' => 'html',
+                            'renderable' => false,
                         ],
                     ],
                     'filters' => [
