3076fe341767525ff0cb570319ae26ed8659f3d3
Vitaliy Berdylo <vberdylo@magecore.com>
2015-11-03 20:09:26 +0200

    BB-1449: Update listener to support both backend and frontend configuration - CR fixes - fix tests

--- src/OroB2B/Bundle/SaleBundle/Tests/Functional/Form/Type/ProductSelectTypeTest.php
+++ src/OroB2B/Bundle/SaleBundle/Tests/Functional/Form/Type/ProductSelectTypeTest.php
@@ -13,7 +13,7 @@ class ProductSelectTypeTest extends AbstractProductSelectTypeTest
     protected $scope = 'quote';
 
     /** @var string */
-    protected $configPath = 'oro_b2b_sale.backend_product_visibility.value';
+    protected $configPath = 'oro_b2b_sale.backend_product_visibility';
 
     /**
      * {@inheritdoc}
