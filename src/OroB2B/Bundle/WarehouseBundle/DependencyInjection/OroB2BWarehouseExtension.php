fdbd2d451e08f95182c83c7a3ec1dc6b09a851d9
Richard Perez <richardpq@gmail.com>
2015-11-09 11:26:52 -0500

    COM-197: create WarehouseBundle - Remove load configuration file (form_types.yml), since it doesn't exist. - add services.yml

--- src/OroB2B/Bundle/WarehouseBundle/DependencyInjection/OroB2BWarehouseExtension.php
+++ src/OroB2B/Bundle/WarehouseBundle/DependencyInjection/OroB2BWarehouseExtension.php
@@ -16,6 +16,5 @@ class OroB2BWarehouseExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-        $loader->load('form_types.yml');
     }
 }
