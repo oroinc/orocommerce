f15f6e1374a81678f7a078e9198bd5c67d3abea4
Oleksandr Nechyporenko <onechyporenko@magecore.com>
2015-11-17 18:03:51 +0200

    BB-1582: Move theme info file to theme folder  - fix tests

--- src/OroB2B/Bundle/FrontendBundle/Tests/Functional/Controller/FrontendControllerTest.php
+++ src/OroB2B/Bundle/FrontendBundle/Tests/Functional/Controller/FrontendControllerTest.php
@@ -60,7 +60,7 @@ class FrontendControllerTest extends WebTestCase
 
         $this->client->request('GET', $this->getUrl('_frontend'));
         $crawler = $this->client->followRedirect();
-        $this->assertEquals('Hello World!', $crawler->filter('title')->html());
+        $this->assertEquals('OroCommerce', $crawler->filter('title')->html());
 
         // Check that backend theme was not affected
         $crawler = $this->client->request('GET', $this->getUrl('oro_user_security_login'));
