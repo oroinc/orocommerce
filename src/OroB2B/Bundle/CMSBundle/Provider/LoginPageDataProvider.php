80c2d8d441cdff71a6bd12c1f73720847bf318ef
Oleksandr Nechyporenko <onechyporenko@magecore.com>
2015-12-03 15:34:14 +0200

    BB-1535: Create login page based on new layout  - update tests

--- src/OroB2B/Bundle/CMSBundle/Provider/LoginPageDataProvider.php
+++ src/OroB2B/Bundle/CMSBundle/Provider/LoginPageDataProvider.php
@@ -73,7 +73,7 @@ class LoginPageDataProvider implements DataProviderInterface
     /**
      * @return LoginPage
      */
-    public function getDefaultLoginPage()
+    protected function getDefaultLoginPage()
     {
         $loginPage = $this->getLoginPageRepository()->findOneBy([]);
         if (!$loginPage) {
