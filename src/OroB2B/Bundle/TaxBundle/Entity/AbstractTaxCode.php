ea35873e9e14422dc4493c7ce8a1524ec159f1c7
Ilya Antipenko <iantipenko@grossum.com>
2015-12-04 18:59:33 +0200

    BB-1653: CRUD Product Tax Code/Account Tax Code  - create entities, migrations, form types, controllers  - Create CRUD for ProductTaxCode

---
+++ src/OroB2B/Bundle/TaxBundle/Entity/AbstractTaxCode.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace OroB2B\Bundle\TaxBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\ConfigField;
+
+/**
+ * @ORM\MappedSuperclass
+ * @ORM\HasLifecycleCallbacks
+ * @Config(
+ *      defaultValues={
+ *          "entity"={
+ *              "icon"="icon-list-alt"
+ *          },
+ *          "dataaudit"={
+ *              "auditable"=true
+ *          }
+ *      }
+ * )
+ */
+abstract class AbstractTaxCode
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @ConfigField(
+     *      defaultValues={
+     *          "dataaudit"={
+     *              "auditable"=true
+     *          }
+     *      }
+     * )
+     *
+     * @var string
+     */
+    protected $code;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @ConfigField(
+     *      defaultValues={
+     *          "dataaudit"={
+     *              "auditable"=true
+     *          }
+     *      }
+     * )
+     *
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @ORM\Column(name="created_at", type="datetime")
+     * @ConfigField(
+     *      defaultValues={
+     *          "entity"={
+     *              "label"="oro.ui.created_at"
+     *          }
+     *      }
+     * )
+     *
+     * @var \DateTime
+     */
+    protected $createdAt;
+
+    /**
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     * @ConfigField(
+     *      defaultValues={
+     *          "entity"={
+     *              "label"="oro.ui.updated_at"
+     *          }
+     *      }
+     * )
+     *
+     * @var \DateTime
+     */
+    protected $updatedAt;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return $this
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return $this
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Pre persist event handler
+     *
+     * @ORM\PrePersist
+     */
+    public function prePersist()
+    {
+        $this->createdAt = new \DateTime('now', new \DateTimeZone('UTC'));
+        $this->updatedAt = new \DateTime('now', new \DateTimeZone('UTC'));
+    }
+
+    /**
+     * Pre update event handler
+     *
+     * @ORM\PreUpdate
+     */
+    public function preUpdate()
+    {
+        $this->updatedAt = new \DateTime('now', new \DateTimeZone('UTC'));
+    }
+}
---
+++ src/OroB2B/Bundle/TaxBundle/Entity/AccountTaxCode.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace OroB2B\Bundle\TaxBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
+
+use OroB2B\Bundle\AccountBundle\Entity\Account;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="orob2b_tax_account_tax_code")
+ * @ORM\HasLifecycleCallbacks
+ * @Config(
+ *      routeName="orob2b_tax_account_tax_code_index",
+ *      routeView="orob2b_tax_account_tax_code_view"
+ * )
+ */
+class AccountTaxCode extends AbstractTaxCode
+{
+    /**
+     * @ORM\OneToOne(targetEntity="OroB2B\Bundle\AccountBundle\Entity\Account")
+     * @ORM\JoinColumn(name="account_id", referencedColumnName="id")
+     *
+     * @var Account
+     */
+    protected $account;
+
+    /**
+     * Set account
+     *
+     * @param Account $account
+     * @return $this
+     */
+    public function setAccount(Account $account)
+    {
+        $this->account = $account;
+
+        return $this;
+    }
+
+    /**
+     * Get account
+     *
+     * @return Account
+     */
+    public function getAccount()
+    {
+        return $this->account;
+    }
+}
