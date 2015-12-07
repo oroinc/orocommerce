7d8fc607bd7caa51fe2dcc7851aff476c22223b7
Yevhen Shyshkin <yshyshkin@magecore.com>
2015-12-07 17:32:02 +0100

    BB-1564: Add system config to set multiple price lists as default with priority - removed constraint for empty price lists

--- src/OroB2B/Bundle/PricingBundle/Validator/Constraints/NotEmptyPriceListsValidator.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace OroB2B\Bundle\PricingBundle\Validator\Constraints;
-
-use Symfony\Component\Validator\Constraints\NotBlankValidator;
-
-class NotEmptyPriceListsValidator extends NotBlankValidator
-{
-}
