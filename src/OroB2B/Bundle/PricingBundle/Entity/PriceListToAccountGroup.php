6dfab2391bc72c1b285829ff4e0d63ef9dcea068
Oleg Popadko <oleg@popadko.com>
2015-12-08 13:23:01 +0200

    BB-1559: Create relation entities - devide migration for adding priority to price list relation tables on two migrations - reorder fields in primiry keys to fix doctrine:schema:update

--- src/OroB2B/Bundle/PricingBundle/Entity/PriceListToAccountGroup.php
+++ src/OroB2B/Bundle/PricingBundle/Entity/PriceListToAccountGroup.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 use OroB2B\Bundle\AccountBundle\Entity\AccountGroup;
 
 /**
- * @ORM\Table(name="orob2b_price_list_to_acc_gr")
+ * @ORM\Table(name="orob2b_price_list_to_acc_group")
  * @ORM\Entity(repositoryClass="OroB2B\Bundle\PricingBundle\Entity\Repository\PriceListToAccountGroupRepository")
  */
 class PriceListToAccountGroup extends BasePriceListRelation
