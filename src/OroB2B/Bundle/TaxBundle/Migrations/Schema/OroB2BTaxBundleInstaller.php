7ca18db904eef9ab9e4c92cf38a5ca0505715866
dmalavsky <dmalavsky@grossum.com>
2015-12-08 17:55:16 +0200

    BB-1655: CRUD for Taxes - add Tax entity - add controller, datagrid, and tax from type - add unit tests for entity and form type

--- src/OroB2B/Bundle/TaxBundle/Migrations/Schema/OroB2BTaxBundleInstaller.php
+++ src/OroB2B/Bundle/TaxBundle/Migrations/Schema/OroB2BTaxBundleInstaller.php
@@ -25,6 +25,7 @@ class OroB2BTaxBundleInstaller implements Installation
         /** Tables generation **/
         $this->createOroB2BTaxAccountTaxCodeTable($schema);
         $this->createOroB2BTaxProductTaxCodeTable($schema);
+        $this->createOroB2BTaxTaxTable($schema);
 
         /** Foreign keys generation **/
         $this->addOroB2BTaxAccountTaxCodeForeignKeys($schema);
@@ -69,6 +70,23 @@ class OroB2BTaxBundleInstaller implements Installation
         $table->addUniqueIndex(['product_id'], 'UNIQ_5AF53A4A4584665A');
     }
 
+    /**
+     * Create orob2b_tax_tax table
+     *
+     * @param Schema $schema
+     */
+    protected function createOroB2BTaxTaxTable(Schema $schema)
+    {
+        $table = $schema->createTable('orob2b_tax_tax');
+        $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('code', 'string', ['length' => 255]);
+        $table->addColumn('description', 'text', []);
+        $table->addColumn('rate', 'float', []);
+        $table->addColumn('created_at', 'datetime', []);
+        $table->addColumn('updated_at', 'datetime', []);
+        $table->setPrimaryKey(['id']);
+        $table->addUniqueIndex(['code'], 'UNIQ_E132B24377153098');
+    }
 
     /**
      * Add orob2b_tax_account_tax_code foreign keys.
--- src/OroB2B/Bundle/TaxBundle/Migrations/Schema/v1_0/OroB2BTaxBundle.php
+++ src/OroB2B/Bundle/TaxBundle/Migrations/Schema/v1_0/OroB2BTaxBundle.php
@@ -17,6 +17,7 @@ class OroB2BTaxBundle implements Migration
         /** Tables generation **/
         $this->createOroB2BTaxAccountTaxCodeTable($schema);
         $this->createOroB2BTaxProductTaxCodeTable($schema);
+        $this->createOroB2BTaxTaxTable($schema);
 
         /** Foreign keys generation **/
         $this->addOroB2BTaxAccountTaxCodeForeignKeys($schema);
@@ -61,6 +62,23 @@ class OroB2BTaxBundle implements Migration
         $table->addUniqueIndex(['product_id'], 'UNIQ_5AF53A4A4584665A');
     }
 
+    /**
+     * Create orob2b_tax_tax table
+     *
+     * @param Schema $schema
+     */
+    protected function createOroB2BTaxTaxTable(Schema $schema)
+    {
+        $table = $schema->createTable('orob2b_tax_tax');
+        $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('code', 'string', ['length' => 255]);
+        $table->addColumn('description', 'text', []);
+        $table->addColumn('rate', 'float', []);
+        $table->addColumn('created_at', 'datetime', []);
+        $table->addColumn('updated_at', 'datetime', []);
+        $table->setPrimaryKey(['id']);
+        $table->addUniqueIndex(['code'], 'UNIQ_E132B24377153098');
+    }
 
     /**
      * Add orob2b_tax_account_tax_code foreign keys.
