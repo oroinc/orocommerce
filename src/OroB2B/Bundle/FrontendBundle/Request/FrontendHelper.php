1df7579bf83a68a5557b94f042ecf13277c32bc7
Roman Yarmolenko <devxypro@gmail.com>
2015-12-18 20:04:10 +0200

    BB-1764: FrontendHelper refactoring - refactoring

--- src/OroB2B/Bundle/FrontendBundle/Request/FrontendHelper.php
+++ src/OroB2B/Bundle/FrontendBundle/Request/FrontendHelper.php
@@ -3,6 +3,8 @@
 namespace OroB2B\Bundle\FrontendBundle\Request;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class FrontendHelper
 {
@@ -12,19 +14,31 @@ class FrontendHelper
     protected $backendPrefix;
 
     /**
-     * @param string $backendPrefix
+     * @var RequestStack
      */
-    public function __construct($backendPrefix)
+    protected $requestStack;
+
+    /**
+     * @param string $backendPrefix
+     * @param RequestStack $requestStack
+     */
+    public function __construct($backendPrefix, RequestStack $requestStack)
     {
         $this->backendPrefix = $backendPrefix;
+        $this->requestStack = $requestStack;
     }
 
     /**
-     * @param Request $request
+     * @param Request|null $request
      * @return bool
      */
-    public function isFrontendRequest(Request $request)
+    public function isFrontendRequest(Request $request = null)
     {
+        $request = $request ?: $this->requestStack->getCurrentRequest();
+        if (!$request) {
+            throw new BadRequestHttpException('Request is not define');
+        }
+
         // the least time consuming method to check whether URL is frontend
         return strpos($request->getPathInfo(), $this->backendPrefix) !== 0;
     }
