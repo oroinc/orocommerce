5f4ff412aa97655a14bc678d47852b338b5fc08a
Yevhen Shyshkin <yshyshkin@magecore.com>
2015-12-18 17:49:17 +0100

    BB-1553: Add process for product category change handling  - fixed event listener test

--- src/OroB2B/Bundle/AccountBundle/Tests/Functional/EventListener/CategoryListenerTest.php
+++ src/OroB2B/Bundle/AccountBundle/Tests/Functional/EventListener/CategoryListenerTest.php
@@ -3,9 +3,9 @@
 namespace OroB2B\Bundle\AccountBundle\Tests\Functional\EventListener;
 
 use Doctrine\ORM\EntityManager;
+
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
-use OroB2B\Bundle\AccountBundle\Entity\Repository\ProductVisibilityRepository;
-use OroB2B\Bundle\AccountBundle\Entity\Repository\ProductVisibilityResolvedRepository;
+
 use OroB2B\Bundle\AccountBundle\Entity\Visibility\ProductVisibility;
 use OroB2B\Bundle\AccountBundle\Entity\VisibilityResolved\ProductVisibilityResolved;
 use OroB2B\Bundle\CatalogBundle\Entity\Category;
@@ -13,174 +13,170 @@ use OroB2B\Bundle\CatalogBundle\Entity\Repository\CategoryRepository;
 use OroB2B\Bundle\CatalogBundle\Tests\Functional\DataFixtures\LoadCategoryData;
 use OroB2B\Bundle\ProductBundle\Entity\Product;
 use OroB2B\Bundle\ProductBundle\Tests\Functional\DataFixtures\LoadProductData;
+use OroB2B\Bundle\WebsiteBundle\Entity\Website;
+use OroB2B\Bundle\WebsiteBundle\Tests\Functional\DataFixtures\LoadWebsiteData;
 
 /**
  * @dbIsolation
  */
 class CategoryListenerTest extends WebTestCase
 {
-
     /**
-     * @var ProductVisibilityRepository
+     * @var EntityManager
      */
-    protected $repository;
+    protected $categoryManager;
 
     /**
      * @var CategoryRepository
      */
     protected $categoryRepository;
+
     /**
-     * @var ProductVisibilityResolvedRepository
+     * @var Website
      */
-    protected $visibilityResolvedRepository;
+    protected $firstWebsite;
+
+    /**
+     * @var Website
+     */
+    protected $secondWebsite;
 
     protected function setUp()
     {
         $this->initClient();
 
-        $this->repository = $this->getContainer()
-            ->get('doctrine')
-            ->getRepository('OroB2BAccountBundle:Visibility\ProductVisibility');
-
-        $this->visibilityResolvedRepository = $this->getContainer()
-            ->get('doctrine')
-            ->getRepository('OroB2BAccountBundle:VisibilityResolved\ProductVisibilityResolved');
-
-        $this->categoryRepository = $this->getContainer()
-            ->get('doctrine')
+        $this->categoryManager = $this->getContainer()->get('doctrine')
+            ->getManagerForClass('OroB2BCatalogBundle:Category');
+        $this->categoryRepository = $this->categoryManager
             ->getRepository('OroB2BCatalogBundle:Category');
 
-        $this->loadFixtures(
-            [
-                'OroB2B\Bundle\AccountBundle\Tests\Functional\DataFixtures\LoadProductVisibilityData',
-            ]
-        );
+        $this->loadFixtures(['OroB2B\Bundle\AccountBundle\Tests\Functional\DataFixtures\LoadProductVisibilityData']);
+
+        $this->firstWebsite = $this->getReference(LoadWebsiteData::WEBSITE1);
+        $this->secondWebsite = $this->getReference(LoadWebsiteData::WEBSITE2);
     }
 
-    /**
-     * @dataProvider changeProductCategoryDataProvider
-     * @param $productName
-     * @param $categoryName
-     * @param $expected
-     * @param $expectedResolved
-     */
-    public function testChangeProductCategory($productName, $categoryName, $expected, $expectedResolved)
+    public function testChangeProductCategory()
     {
-        /**
-         * @var $product Product
-         */
-        $product = $this->getReference($productName);
+        /** @var $product Product */
+        $product = $this->getReference(LoadProductData::PRODUCT_1);
+        $previousCategory = $this->categoryRepository->findOneByProduct($product);
 
-        /**
-         * @var $category Category
-         */
-        $category = $this->getReference($categoryName);
-        $lastCategory = $this->categoryRepository->findOneByProduct($product);
-        $category = $this->categoryRepository->find($category->getId());
+        // default value is categort fallback
+        $this->assertProductVisibility($this->firstWebsite, $product, null, $previousCategory);
+        $this->assertProductVisibility($this->secondWebsite, $product, null, $previousCategory);
 
-        if ($lastCategory) {
-            $lastCategory->removeProduct($product);
-        }
+        /** @var $newCategory Category */
+        $newCategory = $this->getReference(LoadCategoryData::SECOND_LEVEL1);
+        $this->categoryManager->refresh($newCategory);
+
+        $previousCategory->removeProduct($product);
+        $newCategory->addProduct($product);
+        $this->categoryManager->flush();
+
+        // category has been changed
+        $this->assertProductVisibility($this->firstWebsite, $product, null, $newCategory);
+        $this->assertProductVisibility($this->secondWebsite, $product, null, $newCategory);
+    }
+
+    public function testRemoveProductFromCategoryAndAddProductToCategory()
+    {
+        /** @var $product Product */
+        $product = $this->getReference(LoadProductData::PRODUCT_2);
+        $category = $this->categoryRepository->findOneByProduct($product);
+
+        // default value is category fallback
+        $this->assertProductVisibility($this->firstWebsite, $product, null, $category);
+        $this->assertProductVisibility($this->secondWebsite, $product, null, $category);
+
+        $category->removeProduct($product);
+        $this->categoryManager->flush();
+
+        // fallback changed to config
+        $this->assertProductVisibility($this->firstWebsite, $product, ProductVisibility::CONFIG);
+        $this->assertProductVisibility($this->secondWebsite, $product, ProductVisibility::CONFIG);
+
         $category->addProduct($product);
-        /** @var EntityManager $manager */
-        $manager = $this->getContainer()->get('doctrine')->getManager();
-        $manager->flush([$lastCategory, $category]);
-        $actual = $this->getVisibilityByProduct($product);
-        $actualResolved = $this->getVisibilityResolvedByProduct($product);
+        $this->categoryManager->flush();
 
-        $this->assertSameSize($expected, $actual);
-        foreach ($actual as $value) {
-            $this->assertContains($value, $expected);
+        // fallback didn't return back because it was changed during category removal
+        $this->assertProductVisibility($this->firstWebsite, $product, ProductVisibility::CONFIG);
+        $this->assertProductVisibility($this->secondWebsite, $product, ProductVisibility::CONFIG);
+    }
+
+    /**
+     * @param Website $website
+     * @param Product $product
+     * @param string|null $visibilityCode
+     * @param Category|null $category
+     */
+    protected function assertProductVisibility(
+        Website $website,
+        Product $product,
+        $visibilityCode = null,
+        Category $category = null
+    ) {
+        $visibility = $this->getVisibility($website, $product);
+        if ($visibilityCode) {
+            $this->assertNotNull($visibility);
+            $this->assertEquals($visibilityCode, $visibility->getVisibility());
+        } else {
+            $this->assertNull($visibility);
         }
-        $this->assertSameSize($expectedResolved, $actualResolved);
-        foreach ($actualResolved as $key => $value) {
-            $this->assertSame($value['source'], $expectedResolved[$key]['source']);
-            $this->assertSame((int)$value['category'], $category->getId());
+
+        $resolvedVisibility = $this->getResolvedVisibility($website, $product);
+        if ($category) {
+            $this->assertNotNull($resolvedVisibility);
+            $this->assertEquals(ProductVisibilityResolved::SOURCE_CATEGORY, $resolvedVisibility->getSource());
+            $this->assertEquals($category->getId(), $resolvedVisibility->getCategory()->getId());
+        } else {
+            $this->assertNull($resolvedVisibility);
         }
+
     }
 
     /**
-     * @return array
+     * @param Website $website
+     * @param Product $product
+     * @return ProductVisibility|null
      */
-    public function changeProductCategoryDataProvider()
+    protected function getVisibility(Website $website, Product $product)
     {
-        return [
-            'change_category' => [
-                'productName' => LoadProductData::PRODUCT_1,
-                'categoryName' => LoadCategoryData::SECOND_LEVEL1,
-                'expected' => [
-                    [
-                        'visibility' => ProductVisibility::CONFIG,
-                    ]
+        $entityManager = $this->getContainer()->get('doctrine')
+            ->getManagerForClass('OroB2BAccountBundle:Visibility\ProductVisibility');
 
-                ],
-                'expectedResolved' => [
-                    [
-                        'source' => ProductVisibilityResolved::SOURCE_CATEGORY,
-                    ],
-                    [
-                        'source' => ProductVisibilityResolved::SOURCE_CATEGORY,
-                    ]
-                ],
-            ],
-        ];
-    }
-
-
-    /**
-     * @return array
-     */
-    protected function getProductsByVisibilities()
-    {
-        $website = $this->getDefaultWebsite();
-        return array_map(
-            function (ProductVisibility $visibility) {
-                return [
-                    'product' => $visibility->getProduct()->getSku(),
-                    'website' => $visibility->getWebsite()->getName(),
-                    'visibility' => $visibility->getVisibility()
-                ];
-            },
-            $this->repository->findBy(['website' => $website])
-        );
-    }
-
-    /**
-     * @return \OroB2B\Bundle\WebsiteBundle\Entity\Website
-     */
-    protected function getDefaultWebsite()
-    {
-        return $this->getContainer()
-            ->get('doctrine')
-            ->getRepository('OroB2BWebsiteBundle:Website')
-            ->getDefaultWebsite();
-    }
-
-    /**
-     * @param $product
-     * @return array
-     */
-    protected function getVisibilityByProduct($product)
-    {
-        $qb = $this->repository->createQueryBuilder('v')
-            ->select('v.visibility')
-            ->where('v.product = :product')
+        $qb = $entityManager->getRepository('OroB2BAccountBundle:Visibility\ProductVisibility')
+            ->createQueryBuilder('v')
+            ->andWhere('v.website = :website')
+            ->andWhere('v.product = :product')
+            ->setParameter('website', $website)
             ->setParameter('product', $product);
 
-        return $qb->getQuery()->getArrayResult();
+        return $qb->getQuery()->getOneOrNullResult();
     }
 
     /**
-     * @param $product
-     * @return array
+     * @param Website $website
+     * @param Product $product
+     * @return ProductVisibilityResolved|null
      */
-    protected function getVisibilityResolvedByProduct($product)
+    protected function getResolvedVisibility(Website $website, Product $product)
     {
-        $qb = $this->visibilityResolvedRepository->createQueryBuilder('v')
-            ->select(['v.source', 'IDENTITY(v.category) as category'])
-            ->where('v.product = :product')
+        $entityManager = $this->getContainer()->get('doctrine')
+            ->getManagerForClass('OroB2BAccountBundle:VisibilityResolved\ProductVisibilityResolved');
+
+        $qb = $entityManager->getRepository('OroB2BAccountBundle:VisibilityResolved\ProductVisibilityResolved')
+            ->createQueryBuilder('v')
+            ->andWhere('v.website = :website')
+            ->andWhere('v.product = :product')
+            ->setParameter('website', $website)
             ->setParameter('product', $product);
 
-        return $qb->getQuery()->getArrayResult();
+        $entity = $qb->getQuery()->getOneOrNullResult();
+        if ($entity) {
+            $entityManager->refresh($entity);
+        }
+
+        return $entity;
     }
 }
