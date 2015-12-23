fde692705ff40d61e8183e9463441040b735d640
dmalavsky <dmalavsky@grossum.com>
2015-12-23 19:13:46 +0200

    BB-1809: Add listener for edit/view page AccountGroup - add form listeners - add form extension - add autocomplete field for accountGroupTax

---
+++ src/OroB2B/Bundle/TaxBundle/Form/Extension/AccountGroupTaxExtension.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace OroB2B\Bundle\TaxBundle\Form\Extension;
+
+use Symfony\Component\Form\FormBuilderInterface;
+
+use OroB2B\Bundle\TaxBundle\Entity\AbstractTaxCode;
+use OroB2B\Bundle\AccountBundle\Entity\AccountGroup;
+use OroB2B\Bundle\TaxBundle\Entity\AccountGroupTaxCode;
+use OroB2B\Bundle\TaxBundle\Entity\Repository\AccountGroupTaxCodeRepository;
+use OroB2B\Bundle\TaxBundle\Form\Type\AccountGroupTaxCodeAutocompleteType;
+use OroB2B\Bundle\AccountBundle\Form\Type\AccountGroupType;
+
+class AccountGroupTaxExtension extends AbstractTaxExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return AccountGroupType::NAME;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function addTaxCodeField(FormBuilderInterface $builder)
+    {
+        $builder
+            ->add(
+                'taxCode',
+                AccountGroupTaxCodeAutocompleteType::NAME,
+                [
+                    'required' => false,
+                    'mapped' => false,
+                    'label' => 'orob2b.tax.taxcode.form.extension.label',
+                    'create_form_route' => null,
+                ]
+            );
+    }
+
+    /**
+     * @param AccountGroup $accountGroup
+     * @param AccountGroupTaxCode|AbstractTaxCode $taxCode
+     * @param AccountGroupTaxCode|AbstractTaxCode $taxCodeNew
+     */
+    protected function handleTaxCode(
+        $accountGroup,
+        AbstractTaxCode $taxCode = null,
+        AbstractTaxCode $taxCodeNew = null
+    ) {
+        if ($taxCode) {
+            $taxCode->removeAccountGroup($accountGroup);
+        }
+
+        if ($taxCodeNew) {
+            $taxCodeNew->addAccountGroup($accountGroup);
+        }
+    }
+
+    /**
+     * @param AccountGroup $object
+     * @return AccountGroupTaxCode
+     */
+    protected function getTaxCode($object)
+    {
+        /** @var AccountGroupTaxCodeRepository $repository */
+        $repository = $this->getRepository();
+
+        return $repository->findOneByAccountGroup($object);
+    }
+}
--- src/OroB2B/Bundle/TaxBundle/Tests/Unit/Form/Type/AccountGroupTaxCodeAutocompleteTypeTest.php
+++ src/OroB2B/Bundle/TaxBundle/Tests/Unit/Form/Type/AccountGroupTaxCodeAutocompleteTypeTest.php
@@ -5,14 +5,14 @@ namespace OroB2B\Bundle\TaxBundle\Tests\Unit\Form\Type;
 use Symfony\Component\Form\Test\FormIntegrationTestCase;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-use OroB2B\Bundle\TaxBundle\Form\Type\AccountTaxCodeAutocompleteType;
+use OroB2B\Bundle\TaxBundle\Form\Type\AccountGroupTaxCodeAutocompleteType;
 
-class AccountTaxCodeAutocompleteTypeTest extends FormIntegrationTestCase
+class AccountGroupTaxCodeAutocompleteTypeTest extends FormIntegrationTestCase
 {
-    const DATA_CLASS = 'OroB2B\Bundle\TaxBundle\Entity\AccountTaxCode';
+    const DATA_CLASS = 'OroB2B\Bundle\TaxBundle\Entity\AccountGroupTaxCode';
 
     /**
-     * @var AccountTaxCodeAutocompleteType
+     * @var AccountGroupTaxCodeAutocompleteType
      */
     protected $formType;
 
@@ -24,16 +24,16 @@ class AccountTaxCodeAutocompleteTypeTest extends FormIntegrationTestCase
     }
 
     /**
-     * @return AccountTaxCodeAutocompleteType
+     * @return AccountGroupTaxCodeAutocompleteType
      */
     protected function createTaxCodeAutocompleteType()
     {
-        return new AccountTaxCodeAutocompleteType();
+        return new AccountGroupTaxCodeAutocompleteType();
     }
 
     public function testGetName()
     {
-        $this->assertEquals('orob2b_account_tax_code_autocomplete', $this->formType->getName());
+        $this->assertEquals('orob2b_account_group_tax_code_autocomplete', $this->formType->getName());
     }
 
     public function testGetParent()
@@ -48,9 +48,9 @@ class AccountTaxCodeAutocompleteTypeTest extends FormIntegrationTestCase
         $options = $resolver->resolve();
 
         $this->assertArrayHasKey('autocomplete_alias', $options);
-        $this->assertEquals('orob2b_account_tax_code', $options['autocomplete_alias']);
+        $this->assertEquals('orob2b_account_group_tax_code', $options['autocomplete_alias']);
         $this->assertArrayHasKey('grid_name', $options);
-        $this->assertEquals('accounts-tax-code-select-grid', $options['grid_name']);
+        $this->assertEquals('account-groups-tax-code-select-grid', $options['grid_name']);
     }
 
     /**
