ee08d6a6aec48fb71ce55eaab08f6f1b5f642ec4
Yevhen Petrov <ypetrov@magecore.com>
2015-12-30 17:30:51 +0200

    BB-1840: Create stub service CombinedProductPriceResolver to create combined product prices  - added empty service

---
+++ src/OroB2B/Bundle/AccountBundle/Resolver/CombinedProductPriceResolver.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace OroB2B\Bundle\AccountBundle\Resolver;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+use OroB2B\Bundle\PricingBundle\Entity\CombinedPriceList;
+use OroB2B\Bundle\ProductBundle\Entity\Product;
+
+class CombinedProductPriceResolver
+{
+    /**
+     * @var ManagerRegistry
+     */
+    protected $registry;
+
+    /**
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    /**
+     * @param CombinedPriceList $priceList
+     */
+    public function combinePrices(CombinedPriceList $priceList)
+    {
+        //TODO: BB-1842
+    }
+
+    /**
+     * @param CombinedPriceList $combinedPriceList
+     * @param Product $product
+     */
+    public function updatePricesByProduct(CombinedPriceList $combinedPriceList, Product $product)
+    {
+        //TODO: BB-1843
+    }
+}
