298d0609538e734b07935ecf9bb81b87aadb1c47
Yevhen Petrov <ypetrov@magecore.com>
2016-01-04 17:24:37 +0200

    BB-1841: Add service to get CombinedPriceList by price lists collection  - added provider of CombinedPriceLists

---
+++ src/OroB2B/Bundle/PricingBundle/Provider/CombinedPriceListProvider.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace OroB2B\Bundle\PricingBundle\Provider;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+use OroB2B\Bundle\PricingBundle\Resolver\CombinedProductPriceResolver;
+use OroB2B\Bundle\PricingBundle\Entity\BasePriceListRelation;
+use OroB2B\Bundle\PricingBundle\Entity\CombinedPriceList;
+use OroB2B\Bundle\PricingBundle\Entity\CombinedPriceListToPriceList;
+
+class CombinedPriceListProvider
+{
+    /**
+     * @var ManagerRegistry
+     */
+    protected $registry;
+
+    /**
+     * @var string
+     */
+    protected $className;
+
+    /**
+     * @var CombinedProductPriceResolver
+     */
+    protected $resolver;
+
+    /**
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    /**
+     * @param array $priceListsRelations BasePriceListRelation[]
+     * @return CombinedPriceList
+     */
+    public function getCombinedPriceList(array $priceListsRelations)
+    {
+        $normalizedCollection = $this->normalizeCollection($priceListsRelations);
+        $identifier = $this->getCombinedPriceListIdentifier($normalizedCollection);
+        $combinedPriceList = $this->registry->getManagerForClass($this->className)
+            ->getRepository($this->className)->findBy(['name' => $identifier]);
+        if (!$combinedPriceList) {
+            $combinedPriceList = $this->createCombinedPriceList($normalizedCollection, $identifier);
+            $this->resolver->combinePrices($combinedPriceList);
+        }
+
+        return $combinedPriceList;
+    }
+
+
+    /**
+     * @param array $priceListsRelations BasePriceListRelation[]
+     * @return string
+     */
+    protected function getCombinedPriceListIdentifier(array $priceListsRelations)
+    {
+        $key = '';
+        /**
+         * @var $priceListsRelations BasePriceListRelation[]
+         */
+        foreach ($priceListsRelations as $priceListRelation) {
+
+            $isMergeAllowed = 'f';
+            if ($priceListRelation->isMergeAllowed()) {
+                $isMergeAllowed = 't';
+            }
+            $key .= $priceListRelation->getPriceList()->getId() . $isMergeAllowed;
+        }
+
+        return $key;
+    }
+
+    /**
+     * @param array $priceListsRelations BasePriceListRelation[]
+     * @return array BasePriceListRelation[]
+     */
+    protected function normalizeCollection(array $priceListsRelations)
+    {
+        $normalizedCollection = [];
+        $usedPriceMap = [];
+        /**
+         * @var $priceListsRelations BasePriceListRelation[]
+         */
+        foreach ($priceListsRelations as $priceListsRelation) {
+            $isDuplicate = false;
+            $priceList = $priceListsRelation->getPriceList();
+            if ($priceListsRelation->isMergeAllowed()) {
+                if (isset($usedPriceMap[$priceList->getId()][$priceListsRelation->isMergeAllowed()])) {
+                    $isDuplicate = true;
+                }
+            } else {
+                if (isset($usedPriceMap[$priceList->getId()])) {
+                    $isDuplicate = true;
+                }
+            }
+            if ($isDuplicate) {
+                continue;
+            }
+            $normalizedCollection[] = $priceListsRelation;
+            $usedPriceMap[$priceList->getId()][$priceListsRelation->isMergeAllowed()] = true;
+        }
+
+        return $normalizedCollection;
+    }
+
+    /**
+     * @param array $priceListsRelations BasePriceListRelation[]
+     * @param string $identifier
+     * @return CombinedPriceList
+     */
+    protected function createCombinedPriceList(array $priceListsRelations, $identifier)
+    {
+        /**
+         * @var $priceListsRelations BasePriceListRelation[]
+         */
+        $combinedPriceList = new CombinedPriceList();
+        $combinedPriceList->setName($identifier);
+        $combinedPriceList->setCurrencies($this->getCombineCurrencies($priceListsRelations));
+
+        $manager = $this->registry->getManagerForClass($this->className);
+        $manager->persist($combinedPriceList);
+        $manager->flush();
+
+        $i = 0;
+        foreach ($priceListsRelations as $priceListsRelation) {
+            $priceListToCombined = new CombinedPriceListToPriceList();
+            $priceListToCombined->setMergeAllowed($priceListsRelation->isMergeAllowed());
+            $priceListToCombined->setCombinedPriceList($combinedPriceList);
+            $priceListToCombined->setPriceList($priceListsRelation->getPriceList());
+            $priceListToCombined->setSortOrder($i++);
+            $manager->persist($priceListToCombined);
+        }
+        $manager->flush();
+
+        return $combinedPriceList;
+    }
+
+    /**
+     * @param $priceListsRelations BasePriceListRelation[]
+     * @return array
+     */
+    protected function getCombineCurrencies($priceListsRelations)
+    {
+        $currencies = [];
+        /**
+         * @var $priceListsRelations BasePriceListRelation[]
+         */
+        foreach ($priceListsRelations as $priceListsRelation) {
+            $currencies = array_merge($currencies, $priceListsRelation->getPriceList()->getCurrencies());
+        }
+        $currencies = array_unique($currencies);
+        return $currencies;
+    }
+
+    /**
+     * @param string $className
+     */
+    public function setClassName($className)
+    {
+        $this->className = $className;
+    }
+
+    /**
+     * @param mixed $resolver
+     */
+    public function setResolver($resolver)
+    {
+        $this->resolver = $resolver;
+    }
+}
---
+++ src/OroB2B/Bundle/PricingBundle/Tests/Unit/Provider/CombinedPriceListProviderTest.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace OroB2B\Bundle\PricingBundle\Tests\Unit\Provider;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+use OroB2B\Bundle\PricingBundle\Entity\BasePriceListRelation;
+use OroB2B\Bundle\PricingBundle\Provider\CombinedPriceListProvider;
+use OroB2B\Bundle\PricingBundle\Resolver\CombinedProductPriceResolver;
+
+class CombinedPriceListProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var CombinedPriceListProvider
+     */
+    protected $provider;
+
+    /**
+     * @var CombinedProductPriceResolver
+     */
+    protected $resolver;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|ManagerRegistry
+     */
+    protected $registry;
+
+    protected function setUp()
+    {
+        $this->registry = $this->getRegistryMockWithRepository();
+        $this->resolver = $this->getMockBuilder('OroB2B\Bundle\PricingBundle\Resolver\CombinedProductPriceResolver')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->provider = new CombinedPriceListProvider($this->registry);
+        $this->provider->setClassName('OroB2B\Bundle\PricingBundle\Entity\CombinedPriceList');
+        $this->provider->setResolver($this->resolver);
+    }
+
+    protected function tearDown()
+    {
+        unset($this->provider, $this->registry);
+    }
+
+    /**
+     * @dataProvider getCombinedPriceListDataProvider
+     * @param $data
+     * @param $expected
+     */
+    public function testGetCombinedPriceList($data, $expected)
+    {
+        $priceListsRelations = [];
+        foreach ($data['priceListsRelations'] as $priceListData) {
+            /**
+             * @var BasePriceListRelation
+             */
+            $priceList = $this->getMock('OroB2B\Bundle\PricingBundle\Entity\PriceList');
+            $priceList->expects($this->any())
+                ->method('getId')
+                ->willReturn($priceListData['price_list_id']);
+            $priceList->expects($this->any())
+                ->method('getCurrencies')
+                ->willReturn($priceListData['currencies']);
+
+            $priceListRelation = $this->getMockBuilder('OroB2B\Bundle\PricingBundle\Entity\BasePriceListRelation')
+                ->disableOriginalConstructor()
+                ->getMock();
+            $priceListRelation->expects($this->any())
+                ->method('getPriceList')
+                ->willReturn($priceList);
+            $priceListRelation->expects($this->any())
+                ->method('isMergeAllowed')
+                ->willReturn($priceListData['mergeAllowed']);
+
+            $priceListsRelations[] = $priceListRelation;
+        }
+
+        $combinedPriceList = $this->provider->getCombinedPriceList($priceListsRelations);
+        $this->assertInstanceOf(
+            'OroB2B\Bundle\PricingBundle\Entity\CombinedPriceList',
+            $combinedPriceList
+        );
+        $this->assertEquals($expected['name'], $combinedPriceList->getName());
+
+    }
+
+    /**
+     * @return array
+     */
+    public function getCombinedPriceListDataProvider()
+    {
+        return [
+            'duplicate price lists' => [
+                'data' => [
+                    'priceListsRelations' => [
+                        [
+                            'price_list_id' => 1,
+                            'currencies' => ['USD'],
+                            'mergeAllowed' => true,
+                        ],
+                        [
+                            'price_list_id' => 1,
+                            'currencies' => ['USD'],
+                            'mergeAllowed' => false,
+                        ],
+                        [
+                            'price_list_id' => 2,
+                            'currencies' => ['USD', 'EUR'],
+                            'mergeAllowed' => false,
+                        ],
+                        [
+                            'price_list_id' => 2,
+                            'currencies' => ['USD', 'EUR'],
+                            'mergeAllowed' => true,
+                        ],
+                    ],
+                ],
+                'expected' => [
+                    'name' => '1t2f2t',
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Symfony\Bridge\Doctrine\RegistryInterface
+     */
+    protected function getRegistryMockWithRepository()
+    {
+
+        $repository = $this->getMockBuilder('\Doctrine\Common\Persistence\ObjectRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $repository->expects($this->any())
+            ->method('findBy')
+            ->willReturn(null);
+
+        $manager = $this->getMockBuilder('\Doctrine\Common\Persistence\ObjectManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $manager->expects($this->any())
+            ->method('getRepository')
+            ->willReturn($repository);
+
+        $registry = $this->getRegistryMock();
+
+        $registry->expects($this->any())
+            ->method('getManagerForClass')
+            ->willReturn($manager);
+
+        return $registry;
+    }
+
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Symfony\Bridge\Doctrine\RegistryInterface
+     */
+    protected function getRegistryMock()
+    {
+        return $this->getMock('Symfony\Bridge\Doctrine\RegistryInterface');
+    }
+}
