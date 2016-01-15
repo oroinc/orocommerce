7c26ca7542f7a49d6df87def48cac84aa7140381
Andrey Yatsenco <yatsenco@gmail.com>
2016-01-15 14:13:42 +0200

    BB-1945: Create LocaleCacheSubscriber that will rebuild cache on all locale changes

---
+++ src/OroB2B/Bundle/MenuBundle/EventListener/LocaleListener.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace OroB2B\Bundle\MenuBundle\EventListener;
+
+use Doctrine\Common\Util\ClassUtils;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use OroB2B\Bundle\MenuBundle\Menu\DatabaseMenuProvider;
+use OroB2B\Bundle\WebsiteBundle\Entity\Locale;
+
+class LocaleListener
+{
+    /**
+     * @var DatabaseMenuProvider
+     */
+    protected $menuProvider;
+
+    /**
+     * LocaleListener constructor.
+     * @param DatabaseMenuProvider $menuProvider
+     */
+    public function __construct(DatabaseMenuProvider $menuProvider)
+    {
+        $this->menuProvider = $menuProvider;
+    }
+
+    /**
+     * @param LifecycleEventArgs $args
+     */
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        if (!$this->isLocaleEntity($entity)) {
+            return;
+        }
+        $this->rebuildCache($entity);
+    }
+
+    /**
+     * @param LifecycleEventArgs $args
+     */
+    public function postUpdate(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        if (!$this->isLocaleEntity($entity)) {
+            return;
+        }
+        $uow = $args->getEntityManager()->getUnitOfWork();
+        $changes = $uow->getEntityChangeSet($entity);
+        if (array_key_exists('parentLocale', $changes) === true) {
+            $this->rebuildCache($entity);
+        }
+    }
+
+    /**
+     * @param object $entity
+     * @return bool
+     */
+    protected function isLocaleEntity($entity)
+    {
+        return ClassUtils::getClass($entity) === 'OroB2B\Bundle\WebsiteBundle\Entity\Locale';
+    }
+
+    /**
+     * @param Locale $entity
+     */
+    protected function rebuildCache($entity)
+    {
+        $this->menuProvider->rebuildCacheByLocale($entity);
+    }
+}
