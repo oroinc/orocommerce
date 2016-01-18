6ad71bfc1127da8c23f3bb27936a0d2dd25ae801
Andrey Yatsenco <yatsenco@gmail.com>
2016-01-18 16:02:45 +0200

    BB-1924: Create MenuItemCacheSubsriber that will rebuild cache for concrete menu at all menu changes - psr fixes

--- src/OroB2B/Bundle/MenuBundle/Tests/Unit/Entity/Listener/MenuItemListenerTest.php
+++ src/OroB2B/Bundle/MenuBundle/Tests/Unit/Entity/Listener/MenuItemListenerTest.php
@@ -4,7 +4,9 @@ namespace OroB2B\Bundle\MenuBundle\Tests\Unit\Entity\Listener;
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+
 use Oro\Component\Testing\Unit\EntityTrait;
+
 use OroB2B\Bundle\FallbackBundle\Entity\LocalizedFallbackValue;
 use OroB2B\Bundle\MenuBundle\Entity\Listener\MenuItemListener;
 use OroB2B\Bundle\MenuBundle\Entity\MenuItem;
