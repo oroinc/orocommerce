8c34937f7562c34de4df4c023bab026a85c9130a
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2016-01-18 20:18:00 +0200

    BB-1890: Settings Provider digital products support

---
+++ src/OroB2B/Bundle/TaxBundle/Form/Type/ProductTaxCodeMultiSelectType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace OroB2B\Bundle\TaxBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductTaxCodeMultiSelectType extends AbstractType
+{
+    const NAME = 'orob2b_product_tax_code_multiselect';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return self::NAME;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'oro_jqueryselect2_hidden';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'autocomplete_alias' => ProductTaxCodeAutocompleteType::AUTOCOMPLETE_ALIAS,
+                'configs' => ['multiple' => true],
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['attr'] = ['data-selected-data' => json_encode((array)$form->getData())];
+    }
+}
