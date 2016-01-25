41ff267f1e7050c25cac1cc213cb2c2b21ad8343
Alexander Pobereznichenko <opobereznichenko@magecore.com>
2016-01-25 12:06:04 +0200

    BB-1853: Modify frontend product view page to use merged price list    - CR issues fix

--- src/OroB2B/Bundle/PricingBundle/Tests/Unit/EventListener/FormViewListenerTest.php
+++ src/OroB2B/Bundle/PricingBundle/Tests/Unit/EventListener/FormViewListenerTest.php
@@ -17,7 +17,7 @@ use OroB2B\Bundle\PricingBundle\Entity\PriceListToAccountGroup;
 use OroB2B\Bundle\WebsiteBundle\Entity\Website;
 use OroB2B\Bundle\AccountBundle\Entity\Account;
 use OroB2B\Bundle\AccountBundle\Entity\AccountGroup;
-use OroB2B\Bundle\PricingBundle\Entity\PriceList;
+use OroB2B\Bundle\PricingBundle\Entity\CombinedPriceList;
 use OroB2B\Bundle\PricingBundle\EventListener\FormViewListener;
 use OroB2B\Bundle\PricingBundle\Model\FrontendPriceListRequestHandler;
 use OroB2B\Bundle\ProductBundle\Entity\Product;
@@ -281,7 +281,7 @@ class FormViewListenerTest extends FormViewListenerTestCase
         /** @var Product $product */
         $product = $this->getEntity('OroB2B\Bundle\ProductBundle\Entity\Product', 11);
 
-        /** @var PriceList $priceList */
+        /** @var CombinedPriceList $priceList */
         $priceList = $this->getEntity('OroB2B\Bundle\PricingBundle\Entity\CombinedPriceList', 42);
 
         $request = new Request(['id' => $product->getId()]);
