ca33519b02b7614bc04e19294d67e06be261d44a
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2016-02-12 14:16:29 +0200

    BB-1882: Product doesn't clear variantLinks when hasVariant checkbox unchecked - add phpdoc

--- src/OroB2B/Bundle/ProductBundle/EventListener/ProductHandlerListener.php
+++ src/OroB2B/Bundle/ProductBundle/EventListener/ProductHandlerListener.php
@@ -8,6 +8,9 @@ use OroB2B\Bundle\ProductBundle\Entity\Product;
 
 class ProductHandlerListener
 {
+    /**
+     * @param AfterFormProcessEvent $event
+     */
     public function onBeforeFlush(AfterFormProcessEvent $event)
     {
         $data = $event->getData();
