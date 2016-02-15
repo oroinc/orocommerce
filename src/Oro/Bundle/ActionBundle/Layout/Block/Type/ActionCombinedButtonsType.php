192a1d07a6089c394474acf49fe5254b69a8b6a7
Roman Yarmolenko <devxypro@gmail.com>
2016-02-15 13:33:20 +0200

    BB-2080: Create combined button block - fixes

--- src/Oro/Bundle/ActionBundle/Layout/Block/Type/ActionCombinedButtonsType.php
+++ src/Oro/Bundle/ActionBundle/Layout/Block/Type/ActionCombinedButtonsType.php
@@ -11,6 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 class ActionCombinedButtonsType extends AbstractButtonsType
 {
     const NAME = 'action_combined_buttons';
+    const PRIMARY_BUTTON_SUFFIX = 'button_combined_primary';
 
     /**
      * {@inheritDoc}
@@ -46,7 +47,7 @@ class ActionCombinedButtonsType extends AbstractButtonsType
         $builderId = $builder->getId();
 
         $layoutManipulator->add(
-            $primaryActionName . '_button_combined_primary',
+            $primaryActionName . '_' . self::PRIMARY_BUTTON_SUFFIX,
             $builderId,
             ActionButtonType::NAME,
             $buttonOptions
@@ -70,6 +71,7 @@ class ActionCombinedButtonsType extends AbstractButtonsType
     public function buildView(BlockView $view, BlockInterface $block, array $options)
     {
         $view->vars['attr'] = ['data-page-component-module' => 'oroaction/js/app/components/buttons-component'];
+        $view->vars['primary_button_alias'] = $options['primary_action_name'] . '_' . self::PRIMARY_BUTTON_SUFFIX;
     }
 
     /**
