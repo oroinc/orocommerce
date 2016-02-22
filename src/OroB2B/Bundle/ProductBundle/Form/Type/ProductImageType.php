c2891e8844f50315f0b635cef446434d28bea7a4
Alex Torchenko <torchenko@gmail.com>
2016-02-22 23:41:51 +0200

    BB-1950 Multiple Product Images:  - improve ProductImageType

--- src/OroB2B/Bundle/ProductBundle/Form/Type/ProductImageType.php
+++ src/OroB2B/Bundle/ProductBundle/Form/Type/ProductImageType.php
@@ -2,14 +2,46 @@
 
 namespace OroB2B\Bundle\ProductBundle\Form\Type;
 
-use OroB2B\Bundle\ProductBundle\Entity\ProductImage;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Oro\Component\Layout\Extension\Theme\Model\Theme;
+use Oro\Component\Layout\Extension\Theme\Model\ThemeManager;
+
+use OroB2B\Bundle\ProductBundle\Entity\ProductImage;
 
 class ProductImageType extends AbstractType
 {
     const NAME = 'orob2b_product_image';
+
+    const IMAGE_TYPE_MAIN = 'main';
+    const IMAGE_TYPE_ADDITIONAL = 'additional';
+    const IMAGE_TYPE_THUMBNAIL = 'thumbnail';
+
+    /**
+     * @var string[]
+     */
+    private static $imageTypes = [
+        self::IMAGE_TYPE_MAIN,
+        self::IMAGE_TYPE_ADDITIONAL,
+        self::IMAGE_TYPE_THUMBNAIL,
+    ];
+
+    /**
+     * @var Theme
+     */
+    private $theme;
+
+    /**
+     * @param ThemeManager $themeManager
+     * @param RequestStack $requestStack
+     */
+    public function __construct(ThemeManager $themeManager, RequestStack $requestStack)
+    {
+        $this->theme = $themeManager->getTheme($requestStack->getCurrentRequest()->attributes->get('_theme'));
+    }
 
     /**
      * @param FormBuilderInterface $builder
@@ -17,37 +49,20 @@ class ProductImageType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add(
-                'image',
-                'oro_image',
-                []
-            )->add(
-                'main',
+        $builder->add('image', 'oro_image');
+
+        foreach (self::$imageTypes as $imageType) {
+            $builder->add(
+                $imageType,
                 'choice',
                 [
-                    'choices' => ['main'],
-                    'multiple' => false,
+                    'choices' => [$imageType],
+                    'multiple' => $this->defineMultipleOptionForImageType($imageType),
                     'expanded' => true,
                     'label' => null,
                 ]
-            )->add(
-                'additional',
-                'choice',
-                [
-                    'choices' => ['additional'],
-                    'multiple' => true,
-                    'expanded' => true
-                ]
-            )->add(
-                'thumbnail',
-                'choice',
-                [
-                    'choices' => ['thumbnail'],
-                    'multiple' => true,
-                    'expanded' => true
-                ]
             );
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -63,5 +78,16 @@ class ProductImageType extends AbstractType
     public function getName()
     {
         return self::NAME;
+    }
+
+    /**
+     * @param string $imageType
+     * @return bool
+     */
+    private function defineMultipleOptionForImageType($imageType)
+    {
+        $imagesConfig = $this->theme->getDataByKey('images');
+
+        return $imagesConfig['types'][$imageType]['max_number'] !== 1;
     }
 }
