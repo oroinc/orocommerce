3c02eb66b04dadc09744534dcec32af90433d911
Viktor Holovko <vholovko@grossum.com>
2016-02-29 16:50:59 +0200

    BB-2202: Add currency to tax result
    - add currency field
    - add resolver
    - add unit test

--- src/OroB2B/Bundle/TaxBundle/Model/AbstractResultElement.php
+++ src/OroB2B/Bundle/TaxBundle/Model/AbstractResultElement.php
@@ -4,6 +4,8 @@ namespace OroB2B\Bundle\TaxBundle\Model;
 
 abstract class AbstractResultElement extends AbstractResult
 {
+    const CURRENCY = 'currency';
+
     /**
      * @param string $index
      * @param string $value
@@ -12,4 +14,24 @@ abstract class AbstractResultElement extends AbstractResult
     {
         parent::offsetSet((string)$index, (string)$value);
     }
+
+    /**
+     * @param string $currency
+     * @return AbstractResultElement
+     */
+    public function setCurrency($currency)
+    {
+        $this->offsetSet(self::CURRENCY, $currency);
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return $this->getOffset(self::CURRENCY);
+    }
+
 }
