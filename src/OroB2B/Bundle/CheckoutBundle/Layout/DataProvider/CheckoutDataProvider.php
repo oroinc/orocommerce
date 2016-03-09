9cb09d1f91004cdad85a1175656de13c11231e15
Vitaliy Berdylo <vberdylo@magecore.com>
2016-03-09 21:35:04 +0200

    BB-2238: Create sidebar based on workflow item

---
+++ src/OroB2B/Bundle/CheckoutBundle/Layout/DataProvider/CheckoutDataProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace OroB2B\Bundle\CheckoutBundle\Layout\DataProvider;
+
+use Oro\Component\Layout\AbstractServerRenderDataProvider;
+use Oro\Component\Layout\ContextInterface;
+use Oro\Bundle\WorkflowBundle\Model\WorkflowManager;
+
+use OroB2B\Bundle\CheckoutBundle\Entity\Checkout;
+
+class CheckoutDataProvider extends AbstractServerRenderDataProvider
+{
+    /**
+     * @var WorkflowManager
+     */
+    protected $workflowManager;
+
+    /**
+     * @param WorkflowManager $workflowManager
+     */
+    public function __construct(WorkflowManager $workflowManager)
+    {
+        $this->workflowManager = $workflowManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getData(ContextInterface $context)
+    {
+        /** @var Checkout $checkout */
+        $checkout = $context->data()->get('checkout');
+        $workflowItem = $checkout->getWorkflowItem();
+
+        $workflow = $this->workflowManager->getWorkflow($workflowItem);
+
+        if ($workflow->getDefinition()->isStepsDisplayOrdered()) {
+            $steps = $workflow->getStepManager()->getOrderedSteps();
+        } else {
+            $steps = $workflow->getPassedStepsByWorkflowItem($workflowItem);
+        }
+
+        $currentStep = $workflowItem->getCurrentStep();
+
+        return [
+            'checkout' => $checkout,
+            'steps' => $steps,
+            'currentStep' => $currentStep
+        ];
+    }
+}
