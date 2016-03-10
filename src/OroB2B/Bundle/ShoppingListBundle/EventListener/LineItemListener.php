55a0942b120788625e551330047f1b116d11a25b
Makar Sichevoy <sichevoy@gmail.com>
2016-03-10 17:33:43 +0200

    BB-2254: Add to view Back end - fix listener on mass delete line items action

--- src/OroB2B/Bundle/ShoppingListBundle/EventListener/LineItemListener.php
+++ src/OroB2B/Bundle/ShoppingListBundle/EventListener/LineItemListener.php
@@ -2,11 +2,13 @@
 
 namespace OroB2B\Bundle\ShoppingListBundle\EventListener;
 
-use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\OnFlushEventArgs;
 
+use Doctrine\ORM\Event\PostFlushEventArgs;
 use Oro\Component\DependencyInjection\ServiceLink;
 
 use OroB2B\Bundle\ShoppingListBundle\Entity\LineItem;
+use OroB2B\Bundle\ShoppingListBundle\Entity\ShoppingList;
 use OroB2B\Bundle\ShoppingListBundle\Manager\ShoppingListManager;
 
 class LineItemListener
@@ -17,23 +19,61 @@ class LineItemListener
     protected $shoppingListManagerLink;
 
     /**
+     * @var ShoppingList[]
+     */
+    protected $shoppingLists;
+
+    /**
      * @param ServiceLink $shoppingListManagerLink
      */
     public function __construct(ServiceLink $shoppingListManagerLink)
     {
         $this->shoppingListManagerLink = $shoppingListManagerLink;
+        $this->shoppingLists = [];
     }
 
     /**
-     * @param LifecycleEventArgs $args
+     * @param OnFlushEventArgs $event
      */
-    public function postRemove(LifecycleEventArgs $args)
+    public function onFlush(OnFlushEventArgs $event)
     {
-        /** @var LineItem $lineItem */
-        $lineItem = $args->getEntity();
-        if ($lineItem instanceof LineItem) {
-            $this->getShoppingListManager()->recalculateSubtotals($lineItem->getShoppingList());
+        $uow = $event->getEntityManager()->getUnitOfWork();
+        foreach ($uow->getScheduledEntityDeletions() as $entity) {
+            if ($entity instanceof LineItem && $entity->getShoppingList()) {
+                $this->addShoppingList($entity->getShoppingList());
+            }
         }
+    }
+
+    /**
+     * @param PostFlushEventArgs $event
+     */
+    public function postFlush(PostFlushEventArgs $event)
+    {
+        $shoppingLists = $this->getShoppingLists();
+        if ($shoppingLists) {
+            foreach ($shoppingLists as $shoppingList) {
+                $this->getShoppingListManager()->recalculateSubtotals($shoppingList, false);
+            }
+            $this->shoppingLists = [];
+            $event->getEntityManager()->flush();
+        }
+    }
+
+    /**
+     * @param ShoppingList $shoppingList
+     */
+    public function addShoppingList($shoppingList)
+    {
+        $this->shoppingLists[$shoppingList->getId()] = $shoppingList;
+    }
+
+    /**
+     * @return ShoppingList[]
+     */
+    public function getShoppingLists()
+    {
+        return $this->shoppingLists;
     }
 
     /**
