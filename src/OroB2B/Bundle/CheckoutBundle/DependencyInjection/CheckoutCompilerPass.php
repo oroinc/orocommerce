669a1b174688a3fc0038064f981b409f879af061
Roman Yarmolenko <devxypro@gmail.com>
2016-03-10 17:48:57 +0200

    BB-2236: Create order summary block - pr fix

--- src/OroB2B/Bundle/CheckoutBundle/DependencyInjection/CheckoutCompilerPass.php
+++ src/OroB2B/Bundle/CheckoutBundle/DependencyInjection/CheckoutCompilerPass.php
@@ -8,16 +8,19 @@ use Symfony\Component\DependencyInjection\Reference;
 
 class CheckoutCompilerPass implements CompilerPassInterface
 {
+    const CHECKOUT_DATA_PROVIDER_MANAGER = 'orob2b_checkout.data_provider.manager';
+    const CHECKOUT_DATA_PROVIDER_TAG = 'checkout.data_provider';
+
     /**
      * {@inheritDoc}
      */
     public function process(ContainerBuilder $container)
     {
-        if (!$container->has('orob2b_checkout.data_provider.manager')) {
+        if (!$container->has(self::CHECKOUT_DATA_PROVIDER_MANAGER)) {
             return;
         }
-        $definition = $container->findDefinition('orob2b_checkout.data_provider.manager');
-        $taggedServices = $container->findTaggedServiceIds('checkout.data_provider');
+        $definition = $container->findDefinition(self::CHECKOUT_DATA_PROVIDER_MANAGER);
+        $taggedServices = $container->findTaggedServiceIds(self::CHECKOUT_DATA_PROVIDER_TAG);
         foreach ($taggedServices as $id => $tags) {
             $definition->addMethodCall(
                 'addProvider',
