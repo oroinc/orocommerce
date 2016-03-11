55da1d19b1f697c98a5bddc8b9ae855368b034f3
Yevhen Shyshkin <yshyshkin@magecore.com>
2016-03-11 19:30:05 +0100

    BB-2056: Add shopping list form to grid  - fixed listeners  - fixed UI

--- src/OroB2B/Bundle/PricingBundle/EventListener/FrontendProductPriceDatagridListener.php
+++ src/OroB2B/Bundle/PricingBundle/EventListener/FrontendProductPriceDatagridListener.php
@@ -4,33 +4,58 @@ namespace OroB2B\Bundle\PricingBundle\EventListener;
 
 use Symfony\Component\Translation\TranslatorInterface;
 
+use Doctrine\ORM\Query\Expr;
+
 use Oro\Bundle\DataGridBundle\Extension\Formatter\Property\PropertyInterface;
 use Oro\Bundle\DataGridBundle\Datasource\ResultRecord;
 use Oro\Bundle\DataGridBundle\Event\BuildBefore;
 use Oro\Bundle\DataGridBundle\Event\OrmResultAfter;
 use Oro\Bundle\LocaleBundle\Formatter\NumberFormatter;
 
+use OroB2B\Bundle\PricingBundle\Provider\UserCurrencyProvider;
 use OroB2B\Bundle\PricingBundle\Model\PriceListRequestHandler;
+use OroB2B\Bundle\ProductBundle\Formatter\ProductUnitLabelFormatter;
 
 class FrontendProductPriceDatagridListener extends AbstractProductPriceDatagridListener
 {
+    const COLUMN_UNITS = 'price_units';
+    const COLUMN_MINIMUM_PRICE = 'minimum_price';
+
+    const DATA_SEPARATOR = '{sep}';
+
     /**
      * @var NumberFormatter
      */
     protected $numberFormatter;
 
     /**
+     * @var ProductUnitLabelFormatter
+     */
+    protected $productUnitLabelFormatter;
+
+    /**
+     * @var UserCurrencyProvider
+     */
+    protected $currencyProvider;
+
+    /**
      * @param TranslatorInterface $translator
      * @param PriceListRequestHandler $priceListRequestHandler
      * @param NumberFormatter $numberFormatter
+     * @param ProductUnitLabelFormatter $productUnitLabelFormatter
+     * @param UserCurrencyProvider $currencyProvider
      */
     public function __construct(
         TranslatorInterface $translator,
         PriceListRequestHandler $priceListRequestHandler,
-        NumberFormatter $numberFormatter
+        NumberFormatter $numberFormatter,
+        ProductUnitLabelFormatter $productUnitLabelFormatter,
+        UserCurrencyProvider $currencyProvider
     ) {
         parent::__construct($translator, $priceListRequestHandler);
         $this->numberFormatter = $numberFormatter;
+        $this->productUnitLabelFormatter = $productUnitLabelFormatter;
+        $this->currencyProvider = $currencyProvider;
     }
 
     /**
@@ -38,22 +63,33 @@ class FrontendProductPriceDatagridListener extends AbstractProductPriceDatagridL
      */
     public function onResultAfter(OrmResultAfter $event)
     {
-        $currencies = $this->getCurrencies();
-        if (!$currencies) {
-            return;
-        }
-
         /** @var ResultRecord[] $records */
         $records = $event->getRecords();
 
-        $currencyIsoCode = $this->getCurrency();
+        $currencyIsoCode = $this->getCurrencies()[0];
         $priceColumn = $this->buildColumnName($currencyIsoCode);
         foreach ($records as $record) {
-            $price = $record->getValue($priceColumn);
-            if ($price) {
-                $price = $this->numberFormatter->formatCurrency($record->getValue($priceColumn), $currencyIsoCode);
-                $record->addData([$priceColumn => $price]);
+            $resultPrices = [];
+            $prices = $record->getValue($priceColumn);
+            $prices = $prices ? explode(self::DATA_SEPARATOR, $prices) : [];
+            $units = $record->getValue(self::COLUMN_UNITS);
+            $units = $units ? explode(self::DATA_SEPARATOR, $units) : [];
+            if ($prices) {
+                foreach ($units as $key => $unit) {
+                    if (isset($resultPrices[$unit])) { // there might be duplicated because of multiple units
+                        continue;
+                    }
+                    $price = (double)$prices[$key]; // order of prices and units is the same
+                    $resultPrices[$unit] = [
+                        'price' => $price,
+                        'currency' => $currencyIsoCode,
+                        'formatted_price' => $this->numberFormatter->formatCurrency($price, $currencyIsoCode),
+                        'unit' => $unit,
+                        'formatted_unit' => $this->productUnitLabelFormatter->format($unit)
+                    ];
+                }
             }
+            $record->addData([$priceColumn => $resultPrices, self::COLUMN_UNITS => null]);
         }
     }
 
@@ -62,45 +98,56 @@ class FrontendProductPriceDatagridListener extends AbstractProductPriceDatagridL
      */
     public function onBuildBefore(BuildBefore $event)
     {
-        $currencies = $this->getCurrencies();
-        if (!$currencies) {
-            return;
-        }
         $config = $event->getConfig();
-        $currency = $this->getCurrency();
+        $currency = $this->getCurrencies()[0];
 
-        $columnName = $this->buildColumnName($currency);
-        $unitColumnName = $this->buildUnitColumnName();
+        $pricesColumnName = $this->buildColumnName($currency);
+        $separator = (new Expr())->literal(self::DATA_SEPARATOR);
+        $joinAlias = $this->buildJoinAlias($pricesColumnName);
 
-        $joinAlias = $this->buildJoinAlias($columnName);
+        $selectPattern = 'GROUP_CONCAT(%s.value SEPARATOR %s) as %s';
+        $select = sprintf($selectPattern, $joinAlias, $separator, $pricesColumnName);
+        $this->addConfigElement($config, '[source][query][select]', $select);
+
+        $selectPattern = 'GROUP_CONCAT(%s.unit SEPARATOR %s) as %s';
+        $select = sprintf($selectPattern, $joinAlias, $separator, self::COLUMN_UNITS);
+        $this->addConfigElement($config, '[source][query][select]', $select);
+
         $selectPattern = 'min(%s.value) as %s';
-        $select = sprintf($selectPattern, $joinAlias, $columnName);
+        $select = sprintf($selectPattern, $joinAlias, self::COLUMN_MINIMUM_PRICE);
         $this->addConfigElement($config, '[source][query][select]', $select);
-        $selectPattern = 'IDENTITY(%s.unit) as %s';
-        $select = sprintf($selectPattern, $joinAlias, $unitColumnName);
-        $this->addConfigElement($config, '[source][query][select]', $select);
+
         $this->addConfigProductPriceJoin($config, $currency);
 
-        $this->addConfigElement($config, '[columns]', $this->createPriceColumn($currency), $columnName);
-        $this->addConfigElement($config, '[columns]', [
-            'label' => $this->translator->trans('orob2b.product.productunit.entity_label')
-        ], $unitColumnName);
+        $this->addConfigElement(
+            $config,
+            '[properties]',
+            ['type' => 'field', 'frontend_type' => PropertyInterface::TYPE_ROW_ARRAY],
+            $pricesColumnName
+        );
+        $this->addConfigElement($config, '[properties]', null, self::COLUMN_UNITS);
 
-        $this->addConfigElement($config, '[sorters][columns]', $this->getSorter($columnName), $columnName);
+        $this->addConfigElement(
+            $config,
+            '[columns]',
+            [
+                'label' => $this->translator->trans('orob2b.pricing.productprice.price_in_%currency%', [
+                    '%currency%' => $currency
+                ]),
+            ],
+            self::COLUMN_MINIMUM_PRICE
+        );
+        $this->addConfigElement(
+            $config,
+            '[sorters][columns]',
+            [
+                'data_name' => self::COLUMN_MINIMUM_PRICE,
+                'type' => PropertyInterface::TYPE_CURRENCY,
+            ],
+            self::COLUMN_MINIMUM_PRICE
+        );
 
         $this->addConfigFilter($config, $currency);
-    }
-
-    /**
-     * @param $columnName
-     * @return array
-     */
-    protected function getSorter($columnName)
-    {
-        return [
-            'data_name' => $columnName,
-            'type' => PropertyInterface::TYPE_CURRENCY,
-        ];
     }
 
     /**
@@ -108,28 +155,7 @@ class FrontendProductPriceDatagridListener extends AbstractProductPriceDatagridL
      */
     protected function buildColumnName($currencyIsoCode, $unitCode = null)
     {
-        return 'price_column';
-    }
-
-    /**
-     * @return string
-     */
-    protected function buildUnitColumnName()
-    {
-        return 'price_unit_column';
-    }
-
-    /**
-     * @param string $currency
-     * @return array
-     */
-    protected function createPriceColumn($currency)
-    {
-        return [
-            'label' => $this->translator->trans('orob2b.pricing.productprice.price_in_%currency%', [
-                '%currency%' => $currency
-            ]),
-        ];
+        return 'prices';
     }
 
     /**
@@ -141,11 +167,10 @@ class FrontendProductPriceDatagridListener extends AbstractProductPriceDatagridL
     }
 
     /**
-     * @return string
+     * @return array
      */
-    protected function getCurrency()
+    protected function getCurrencies()
     {
-        $currencies = $this->getCurrencies();
-        return reset($currencies);
+        return [$this->currencyProvider->getUserCurrency()];
     }
 }
--- src/OroB2B/Bundle/ProductBundle/EventListener/ProductDatagridViewsListener.php
+++ src/OroB2B/Bundle/ProductBundle/EventListener/ProductDatagridViewsListener.php
@@ -4,8 +4,11 @@ namespace OroB2B\Bundle\ProductBundle\EventListener;
 
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
+use Doctrine\ORM\Query\Expr;
+
 use Oro\Bundle\AttachmentBundle\Manager\AttachmentManager;
 use Oro\Bundle\DataGridBundle\Datasource\ResultRecord;
+use Oro\Bundle\DataGridBundle\Extension\Formatter\Property\PropertyInterface;
 use Oro\Bundle\DataGridBundle\Event\OrmResultAfter;
 use Oro\Bundle\DataGridBundle\Event\PreBuild;
 use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
@@ -13,9 +16,14 @@ use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
 use OroB2B\Bundle\ProductBundle\DataGrid\DataGridThemeHelper;
 use OroB2B\Bundle\ProductBundle\Entity\Product;
 use OroB2B\Bundle\ProductBundle\Entity\Repository\ProductRepository;
+use OroB2B\Bundle\ProductBundle\Formatter\ProductUnitLabelFormatter;
 
 class ProductDatagridViewsListener
 {
+    const COLUMN_PRODUCT_UNITS = 'product_units';
+
+    const DATA_SEPARATOR = '{sep}';
+
     /**
      * @var DataGridThemeHelper
      */
@@ -32,18 +40,26 @@ class ProductDatagridViewsListener
     protected $attachmentManager;
 
     /**
+     * @var ProductUnitLabelFormatter
+     */
+    protected $unitFormatter;
+
+    /**
      * @param DataGridThemeHelper $themeHelper
      * @param RegistryInterface $registry
      * @param AttachmentManager $attachmentManager
+     * @param ProductUnitLabelFormatter $unitFormatter
      */
     public function __construct(
         DataGridThemeHelper $themeHelper,
         RegistryInterface $registry,
-        AttachmentManager $attachmentManager
+        AttachmentManager $attachmentManager,
+        ProductUnitLabelFormatter $unitFormatter
     ) {
         $this->themeHelper = $themeHelper;
         $this->registry = $registry;
         $this->attachmentManager = $attachmentManager;
+        $this->unitFormatter = $unitFormatter;
     }
 
     /**
@@ -54,6 +70,22 @@ class ProductDatagridViewsListener
         $config = $event->getConfig();
         $gridName = $config->getName();
         $this->updateConfigByView($config, $this->themeHelper->getTheme($gridName));
+
+        // add all product units
+        $select = sprintf(
+            'GROUP_CONCAT(unit_precisions.unit SEPARATOR %s) as %s',
+            (new Expr())->literal(self::DATA_SEPARATOR),
+            self::COLUMN_PRODUCT_UNITS
+        );
+        $config->offsetAddToArrayByPath('[source][query][select]', [$select]);
+        $config->offsetAddToArrayByPath(
+            '[source][query][join][left]',
+            [['join' => 'product.unitPrecisions', 'alias' => 'unit_precisions']]
+        );
+        $config->offsetAddToArrayByPath(
+            '[properties]',
+            [self::COLUMN_PRODUCT_UNITS => ['type' => 'field', 'frontend_type' => PropertyInterface::TYPE_ROW_ARRAY]]
+        );
     }
 
     /**
@@ -145,7 +177,14 @@ class ProductDatagridViewsListener
                     break;
                 }
             }
-            $record->addData(['image' => $imageUrl]);
+            $units = [];
+            $concatenatedUnits = $record->getValue(self::COLUMN_PRODUCT_UNITS);
+            if ($concatenatedUnits) {
+                foreach (explode(self::DATA_SEPARATOR, $concatenatedUnits) as $unitCode) {
+                    $units[$unitCode] = $this->unitFormatter->format($unitCode);
+                }
+            }
+            $record->addData(['image' => $imageUrl, self::COLUMN_PRODUCT_UNITS => $units]);
         }
     }
 }
