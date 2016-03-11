9f68ba81339f7af1de8a33c0eb83a14b3c3c6d96
Vitaliy Berdylo <vberdylo@magecore.com>
2016-03-11 11:43:31 +0200

    BB-2246: Create Place Order action for Shopping list - remove action stub - skip action Test

--- src/OroB2B/Bundle/CheckoutBundle/Controller/Frontend/CheckoutController.php
+++ src/OroB2B/Bundle/CheckoutBundle/Controller/Frontend/CheckoutController.php
@@ -5,13 +5,11 @@ namespace OroB2B\Bundle\CheckoutBundle\Controller\Frontend;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 use Oro\Bundle\LayoutBundle\Annotation\Layout;
 use Oro\Bundle\SecurityBundle\Annotation\Acl;
 
 use OroB2B\Bundle\CheckoutBundle\Entity\Checkout;
-use OroB2B\Bundle\OrderBundle\Entity\Order;
 
 class CheckoutController extends Controller
 {
@@ -48,28 +46,5 @@ class CheckoutController extends Controller
                     'workflowStep' => $currentStep
                 ]
         ];
-    }
-
-    /**
-     * @TODO remove after BB-2245
-     * @Route(
-     *     "/success/{id}",
-     *     name="orob2b_order_frontend_success",
-     *     requirements={"id"="\d+"}
-     * )
-     * @Acl(
-     *      id="orob2b_checkout_frontend_checkout",
-     *      type="entity",
-     *      class="OroB2BCheckoutBundle:Checkout",
-     *      permission="CREATE",
-     *      group_name="commerce"
-     * )
-     *
-     * @param Order $order
-     * @return Response
-     */
-    public function successAction(Order $order)
-    {
-        return new Response($order->getId());
     }
 }
