1eff2058a69df9252efbb27448698fe3d6f66e23
Roman Yarmolenko <devxypro@gmail.com>
2016-03-17 18:55:14 +0200

    BB-2363: Add start_checout workflow action - add functional test

--- src/OroB2B/Bundle/CheckoutBundle/Tests/Functional/Model/Action/StartCheckoutTest.php
+++ src/OroB2B/Bundle/CheckoutBundle/Tests/Functional/Model/Action/StartCheckoutTest.php
@@ -21,6 +21,12 @@ class StartCheckoutTest extends WebTestCase
     /** @var  AccountUser */
     protected $user;
 
+    /** @var ManagerRegistry */
+    protected $registry;
+
+    /** @var  StartCheckout */
+    protected $action;
+
     public function setUp()
     {
         $this->initClient(
@@ -33,44 +39,63 @@ class StartCheckoutTest extends WebTestCase
                 'OroB2B\Bundle\ShoppingListBundle\Tests\Functional\DataFixtures\LoadShoppingLists',
             ]
         );
-        $this->user = $this->client
-            ->getContainer()
-            ->get('doctrine')
+        $this->registry = $this->getContainer()->get('doctrine');
+        $this->user = $this->registry
             ->getRepository('OroB2BAccountBundle:AccountUser')
             ->findOneBy(['username' => LoadAccountUserData::AUTH_USER]);
         $token = new Token();
         $token->setUser($this->user);
         $this->client->getContainer()->get('security.token_storage')->setToken($token);
+        $this->action = $this->client->getContainer()->get('orob2b_checkout.model.action.start_checkout');
     }
 
-    public function testExecute()
+    public function testFirstExecute()
     {
-        /** @var ShoppingList $shoppingList */
-        $shoppingList = $this->getReference(LoadShoppingLists::SHOPPING_LIST_1);
-        $context = new ActionData(['data' => $shoppingList]);
-        $action = $this->client->getContainer()
-            ->get('orob2b_checkout.model.action.start_checkout');
-        $poNumber = 123;
-        $settings = [
-            'allow_source_remove' => true,
-            'disallow_billing_address_edit' => false,
-            'disallow_shipping_address_edit' => false,
-            'remove_source' => true
-        ];
-        $options = [
-            StartCheckout::SOURCE_FIELD_KEY => 'shoppingList',
-            StartCheckout::SOURCE_ENTITY_KEY => $shoppingList,
-            StartCheckout::CHECKOUT_DATA_KEY => [
-                'poNumber' => $poNumber
-            ],
-            StartCheckout::SETTINGS_KEY => $settings
-        ];
-        $action->initialize($options);
-        $action->execute($context);
+        $checkouts = $this->registry->getRepository('OroB2BCheckoutBundle:Checkout')->findAll();
+        $checkoutSources = $this->registry->getRepository('OroB2BCheckoutBundle:CheckoutSource')->findAll();
+        $this->assertCount(0, $checkouts);
+        $this->assertCount(0, $checkoutSources);
+        $data = $this->getData();
+        $this->action->initialize($data['options']);
+        $this->action->execute($data['context']);
+        $this->checkData($data['shoppingList'], $data['poNumber'], $data['settings'], $data['context']);
+    }
+
+    public function testSecondExecute()
+    {
         /** @var ManagerRegistry $registry */
         $registry = $this->getContainer()->get('doctrine');
         $checkouts = $registry->getRepository('OroB2BCheckoutBundle:Checkout')->findAll();
         $checkoutSources = $registry->getRepository('OroB2BCheckoutBundle:CheckoutSource')->findAll();
+        $this->assertCount(1, $checkouts);
+        $this->assertCount(1, $checkoutSources);
+        /** @var ShoppingList $shoppingList */
+        $shoppingList = $this->getReference(LoadShoppingLists::SHOPPING_LIST_1);
+        $this->action->initialize(
+            [
+                StartCheckout::SOURCE_FIELD_KEY => 'shoppingList',
+                StartCheckout::SOURCE_ENTITY_KEY => $shoppingList
+            ]
+        );
+        $this->action->execute(new ActionData(['data' => $shoppingList]));
+        $data = $this->getData();
+        $this->checkData($data['shoppingList'], $data['poNumber'], $data['settings'], $data['context']);
+    }
+
+    /**
+     * @param $shoppingList
+     * @param $poNumber
+     * @param $settings
+     * @param $context
+     */
+    protected function checkData(
+        ShoppingList $shoppingList,
+        $poNumber,
+        array $settings,
+        ActionData $context
+    ) {
+        $checkouts = $this->registry->getRepository('OroB2BCheckoutBundle:Checkout')->findAll();
+        $checkoutSources = $this->registry->getRepository('OroB2BCheckoutBundle:CheckoutSource')->findAll();
         $this->assertCount(1, $checkouts);
         $this->assertCount(1, $checkoutSources);
         $checkout = $checkouts[0];
@@ -104,6 +129,47 @@ class StartCheckoutTest extends WebTestCase
         }
         $this->assertEquals(spl_object_hash($checkout), spl_object_hash($data->get('checkout')));
         $this->assertEquals($workflowItem->getSerializedData(), json_encode($settings));
+        $this->assertEquals($workflowItem->getEntityId(), $checkout->getId());
+        $this->assertEquals(
+            $context->offsetGet('redirectUrl'),
+            $this->client->getContainer()->get('router')->generate(
+                'orob2b_checkout_frontend_checkout',
+                ['id' => $checkout->getId()]
+            )
+        );
+    }
 
+    /**
+     * @return array
+     */
+    protected function getData()
+    {
+        /** @var ShoppingList $shoppingList */
+        $shoppingList = $this->getReference(LoadShoppingLists::SHOPPING_LIST_1);
+        $context = new ActionData(['data' => $shoppingList]);
+
+        $poNumber = 123;
+        $settings = [
+            'allow_source_remove' => true,
+            'disallow_billing_address_edit' => false,
+            'disallow_shipping_address_edit' => false,
+            'remove_source' => true
+        ];
+        $options = [
+            StartCheckout::SOURCE_FIELD_KEY => 'shoppingList',
+            StartCheckout::SOURCE_ENTITY_KEY => $shoppingList,
+            StartCheckout::CHECKOUT_DATA_KEY => [
+                'poNumber' => $poNumber
+            ],
+            StartCheckout::SETTINGS_KEY => $settings
+        ];
+
+        return [
+            'shoppingList' => $shoppingList,
+            'context' => $context,
+            'poNumber' => $poNumber,
+            'settings' => $settings,
+            'options' => $options
+        ];
     }
 }
