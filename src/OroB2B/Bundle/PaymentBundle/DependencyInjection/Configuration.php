d46fef7ed4637d8133a88d114f81b9783d044b88
Viktor Holovko <vholovko@grossum.com>
2016-03-18 20:39:27 +0200

    BB-2297: Add configuration
    - add fields
    - add js-component
    - add test

--- src/OroB2B/Bundle/PaymentBundle/DependencyInjection/Configuration.php
+++ src/OroB2B/Bundle/PaymentBundle/DependencyInjection/Configuration.php
@@ -11,6 +11,7 @@ class Configuration implements ConfigurationInterface
 {
     /**
      * {@inheritDoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
     public function getConfigTreeBuilder()
     {
@@ -21,46 +22,9 @@ class Configuration implements ConfigurationInterface
         SettingsBuilder::append(
             $rootNode,
             [
-                // Payflow Gateway
-                'payflow_gateway_enabled' => [
-                    'type' => 'boolean',
-                    'value' => false
-                ],
-                'payflow_gateway_email' => [
-                    'type' => 'email',
-                    'value' => ''
-                ],
-                'payflow_gateway_partner' => [
+                'merchant_country' => [
                     'type' => 'text',
                     'value' => ''
-                ],
-                'payflow_gateway_user' => [
-                    'type' => 'text',
-                    'value' => ''
-                ],
-                'payflow_gateway_vendor' => [
-                    'type' => 'text',
-                    'value' => ''
-                ],
-                'payflow_gateway_password' => [
-                    'type' => 'text',
-                    'value' => ''
-                ],
-                'payflow_gateway_test_mode' => [
-                    'type' => 'boolean',
-                    'value' => false
-                ],
-                'payflow_gateway_use_proxy' => [
-                    'type' => 'boolean',
-                    'value' => false
-                ],
-                'payflow_gateway_proxy_host' => [
-                    'type' => 'text',
-                    'value' => ''
-                ],
-                'payflow_gateway_proxy_port' => [
-                    'type' => 'text',
-                    'value' => 8080
                 ],
 
                 // PayPal Payments Pro
@@ -68,10 +32,27 @@ class Configuration implements ConfigurationInterface
                     'type' => 'boolean',
                     'value' => false
                 ],
-                'paypal_payments_pro_email' => [
-                    'type' => 'email',
+                'paypal_payments_pro_label' => [
+                    'type' => 'text',
+                    'value' => 'Credit Card'
+                ],
+                'paypal_payments_pro_sort_order' => [
+                    'type' => 'text',
                     'value' => ''
                 ],
+                'paypal_payments_pro_allowed_countries' => [
+                    'type' => 'text',
+                    'value' => 'all'
+                ],
+                'paypal_payments_pro_selected_countries' => [
+                    'type' => 'array',
+                    'value' => []
+                ],
+                'paypal_payments_pro_allowed_cc_types' => [
+                    'type' => 'array',
+                    'value' => ['visa', 'mastercard']
+                ],
+
                 'paypal_payments_pro_partner' => [
                     'type' => 'text',
                     'value' => ''
@@ -88,10 +69,15 @@ class Configuration implements ConfigurationInterface
                     'type' => 'text',
                     'value' => ''
                 ],
+                'paypal_payments_pro_payment_action' => [
+                    'type' => 'text',
+                    'value' => 'authorization'
+                ],
                 'paypal_payments_pro_test_mode' => [
                     'type' => 'boolean',
                     'value' => false
                 ],
+
                 'paypal_payments_pro_use_proxy' => [
                     'type' => 'boolean',
                     'value' => false
@@ -101,9 +87,105 @@ class Configuration implements ConfigurationInterface
                     'value' => ''
                 ],
                 'paypal_payments_pro_proxy_port' => [
-                    'type' => 'integer',
-                    'value' => 8080
+                    'type' => 'text',
+                    'value' => ''
                 ],
+                'paypal_payments_pro_debug_mode' => [
+                    'type' => 'boolean',
+                    'value' => false
+                ],
+                'paypal_payments_pro_enable_ssl_verification' => [
+                    'type' => 'boolean',
+                    'value' => true
+                ],
+                'paypal_payments_pro_require_cvv' => [
+                    'type' => 'boolean',
+                    'value' => true
+                ],
+                'paypal_payments_pro_validate_cvv' => [
+                    'type' => 'boolean',
+                    'value' => true
+                ],
+
+                // Payflow Gateway
+                'payflow_gateway_enabled' => [
+                    'type' => 'boolean',
+                    'value' => false
+                ],
+                'payflow_gateway_label' => [
+                    'type' => 'text',
+                    'value' => 'Credit Card'
+                ],
+                'payflow_gateway_sort_order' => [
+                    'type' => 'text',
+                    'value' => ''
+                ],
+                'payflow_gateway_allowed_countries' => [
+                    'type' => 'text',
+                    'value' => 'all'
+                ],
+                'payflow_gateway_selected_countries' => [
+                    'type' => 'array',
+                    'value' => []
+                ],
+                'payflow_gateway_allowed_cc_types' => [
+                    'type' => 'array',
+                    'value' => ['visa', 'mastercard']
+                ],
+
+                'payflow_gateway_partner' => [
+                    'type' => 'text',
+                    'value' => ''
+                ],
+                'payflow_gateway_user' => [
+                    'type' => 'text',
+                    'value' => ''
+                ],
+                'payflow_gateway_vendor' => [
+                    'type' => 'text',
+                    'value' => ''
+                ],
+                'payflow_gateway_password' => [
+                    'type' => 'text',
+                    'value' => ''
+                ],
+                'payflow_gateway_payment_action' => [
+                    'type' => 'text',
+                    'value' => 'authorization'
+                ],
+                'payflow_gateway_test_mode' => [
+                    'type' => 'boolean',
+                    'value' => false
+                ],
+
+                'payflow_gateway_use_proxy' => [
+                    'type' => 'boolean',
+                    'value' => false
+                ],
+                'payflow_gateway_proxy_host' => [
+                    'type' => 'text',
+                    'value' => ''
+                ],
+                'payflow_gateway_proxy_port' => [
+                    'type' => 'text',
+                    'value' => ''
+                ],
+                'payflow_gateway_debug_mode' => [
+                    'type' => 'boolean',
+                    'value' => false
+                ],
+                'payflow_gateway_enable_ssl_verification' => [
+                    'type' => 'boolean',
+                    'value' => true
+                ],
+                'payflow_gateway_require_cvv' => [
+                    'type' => 'boolean',
+                    'value' => true
+                ],
+                'payflow_gateway_validate_cvv' => [
+                    'type' => 'boolean',
+                    'value' => true
+                ]
             ]
         );
 
