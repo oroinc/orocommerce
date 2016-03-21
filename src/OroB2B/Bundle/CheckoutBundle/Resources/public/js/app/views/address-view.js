57978af947a596a4f0b4e08735fdb9b4b8582671
Roman Yarmolenko <devxypro@gmail.com>
2016-03-21 12:56:52 +0200

    BB-2098: Checkout Process - js fix

--- src/OroB2B/Bundle/CheckoutBundle/Resources/public/js/app/views/address-view.js
+++ src/OroB2B/Bundle/CheckoutBundle/Resources/public/js/app/views/address-view.js
@@ -32,7 +32,8 @@ define(function (require) {
 
             this.addressSelector.on('change', _.bind(this.onAddressChanged, this));
             this.regionSelector.on('change', _.bind(this.onRegionListChanged, this));
-            this.addressSelector.val(this.addressSelector.data('default')).change();
+            this.addressSelector.val(this.addressSelector.data('default'));
+            this.addressSelector.trigger("chosen:updated");
         },
 
         onAddressChanged: function (e) {
