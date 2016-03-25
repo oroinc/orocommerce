67a19933867bfd0b8f6183b90112943a62774553
Sergey Zhuravel <szhuravel@magecore.com>
2016-03-25 21:53:03 +0200

    BB-2396: Review and merge - update test

--- src/OroB2B/Bundle/TaxBundle/Tests/Unit/EventListener/Order/OrderTaxesListenerTest.php
+++ src/OroB2B/Bundle/TaxBundle/Tests/Unit/EventListener/Order/OrderTaxesListenerTest.php
@@ -5,7 +5,6 @@ namespace OroB2B\Bundle\TaxBundle\Tests\Unit\EventListener\Order;
 use Oro\Component\Testing\Unit\EntityTrait;
 
 use OroB2B\Bundle\OrderBundle\Entity\Order;
-use OroB2B\Bundle\OrderBundle\Entity\OrderLineItem;
 use OroB2B\Bundle\OrderBundle\EventListener\Order\MatchingPriceEventListener;
 use OroB2B\Bundle\OrderBundle\Pricing\PriceMatcher;
 use OroB2B\Bundle\TaxBundle\Model\Result;
@@ -75,7 +74,8 @@ class OrderTaxesListenerTest extends \PHPUnit_Framework_TestCase
     public function testOnOrderEvent(Result $result, array $expectedResult)
     {
         $order = new Order();
-        $data = new \ArrayObject();
+        $prices = [MatchingPriceEventListener::MATCHED_PRICES_KEY => ['price1' => []]];
+        $data = new \ArrayObject($prices);
 
         $this->taxManager->expects($this->once())
             ->method('getTax')
@@ -94,9 +94,11 @@ class OrderTaxesListenerTest extends \PHPUnit_Framework_TestCase
             ->method('getData')
             ->willReturn($data);
 
+        $this->priceMatcher->expects($this->once())->method('fillMatchingPrices');
+
         $this->listener->onOrderEvent($this->event);
 
-        $this->assertEquals($data->getArrayCopy(), $expectedResult);
+        $this->assertEquals($data->getArrayCopy(), array_merge($expectedResult, $prices));
     }
 
     public function testOnOrderEventTaxationDisabled()
@@ -109,189 +111,6 @@ class OrderTaxesListenerTest extends \PHPUnit_Framework_TestCase
             ->willReturn(false);
 
         $this->listener->onOrderEvent($this->event);
-    }
-
-    /**
-     * @dataProvider orderEventAddMatchedPricesDataProvider
-     * @param array $orderLineItems
-     * @param array $matchedPrices
-     * @param array $expectedLineItemsPrices
-     */
-    public function testOnOrderEventAddMatchedPrices(
-        array $orderLineItems = [],
-        array $matchedPrices = [],
-        array $expectedLineItemsPrices = []
-    ) {
-        $order = new Order();
-        $data = new \ArrayObject();
-        $data->offsetSet(MatchingPriceEventListener::MATCHED_PRICES_KEY, $matchedPrices);
-
-        array_walk(
-            $orderLineItems,
-            function (OrderLineItem $orderLineItem) use ($order) {
-                $order->addLineItem($orderLineItem);
-            }
-        );
-
-        $this->event->expects($this->once())->method('getOrder')->willReturn($order);
-        $this->event->expects($this->once())->method('getData')->willReturn($data);
-
-        $this->taxationSettingsProvider->expects($this->once())->method('isEnabled')->willReturn(true);
-
-        $this->taxManager->expects($this->once())
-            ->method('getTax')
-            ->with(
-                $this->callback(
-                    function (Order $order) use ($expectedLineItemsPrices) {
-                        foreach ($order->getLineItems() as $key => $orderLineItem) {
-                            $this->assertArrayHasKey($key, $expectedLineItemsPrices);
-                            $this->assertEquals($expectedLineItemsPrices[$key], $orderLineItem->getValue());
-                        }
-
-                        return true;
-                    }
-                )
-            )
-            ->willReturn(new Result());
-
-        $this->listener->onOrderEvent($this->event);
-    }
-
-    /**
-     * @return array
-     *
-     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
-     */
-    public function orderEventAddMatchedPricesDataProvider()
-    {
-        $product = $this->getEntity('OroB2B\Bundle\ProductBundle\Entity\Product', ['id' => 1]);
-        $invalidProduct = $this->getEntity('OroB2B\Bundle\ProductBundle\Entity\Product');
-        $productUnit = $this->getEntity('OroB2B\Bundle\ProductBundle\Entity\ProductUnit', ['code' => 'set']);
-        $invalidProductUnit = $this->getEntity('OroB2B\Bundle\ProductBundle\Entity\ProductUnit');
-
-        return [
-            'empty prices' => [],
-            'no matched prices' => [
-                [
-                    (new OrderLineItem())
-                        ->setProduct($product)
-                        ->setProductUnit($productUnit)
-                        ->setCurrency('USD')
-                        ->setQuantity('3'),
-                ],
-                ['1-set-2-USD' => ['currency' => 'USD', 'value' => 100]],
-                [null],
-            ],
-            'without product' => [
-                [
-                    (new OrderLineItem())
-                        ->setProductUnit($productUnit)
-                        ->setCurrency('USD')
-                        ->setQuantity('3'),
-                ],
-                ['1-set-2-USD' => ['currency' => 'USD', 'value' => 100]],
-                [null],
-            ],
-            'invalid product' => [
-                [
-                    (new OrderLineItem())
-                        ->setProduct($invalidProduct)
-                        ->setProductUnit($productUnit)
-                        ->setCurrency('USD')
-                        ->setQuantity('3'),
-                ],
-                ['1-set-2-USD' => ['currency' => 'USD', 'value' => 100]],
-                [null],
-            ],
-            'without productUnit' => [
-                [
-                    (new OrderLineItem())
-                        ->setProduct($product)
-                        ->setCurrency('USD')
-                        ->setQuantity('3'),
-                ],
-                ['1-set-2-USD' => ['currency' => 'USD', 'value' => 100]],
-                [null],
-            ],
-            'invalid productUnit' => [
-                [
-                    (new OrderLineItem())
-                        ->setProduct($product)
-                        ->setProductUnit($invalidProductUnit)
-                        ->setCurrency('USD')
-                        ->setQuantity('3'),
-                ],
-                ['1-set-2-USD' => ['currency' => 'USD', 'value' => 100]],
-                [null],
-            ],
-            'without currency' => [
-                [
-                    (new OrderLineItem())
-                        ->setProduct($product)
-                        ->setProduct($product)
-                        ->setQuantity('3'),
-                ],
-                ['1-set-2-USD' => ['currency' => 'USD', 'value' => 100]],
-                [null],
-            ],
-            'invalid currency' => [
-                [
-                    (new OrderLineItem())
-                        ->setProduct($product)
-                        ->setProduct($product)
-                        ->setQuantity('3')
-                        ->setCurrency(''),
-                ],
-                ['1-set-2-USD' => ['currency' => 'USD', 'value' => 100]],
-                [null],
-            ],
-            'without quantity' => [
-                [
-                    (new OrderLineItem())
-                        ->setProduct($product)
-                        ->setProduct($product)
-                        ->setCurrency('USD'),
-                ],
-                ['1-set-2-USD' => ['currency' => 'USD', 'value' => 100]],
-                [null],
-            ],
-            'invalid quantity #1' => [
-                [
-                    (new OrderLineItem())
-                        ->setProduct($product)
-                        ->setProduct($product)
-                        ->setQuantity('string')
-                        ->setCurrency('USD'),
-                ],
-                ['1-set-2-USD' => ['currency' => 'USD', 'value' => 100]],
-                [null],
-            ],
-            'invalid quantity #2' => [
-                [
-                    (new OrderLineItem())
-                        ->setProduct($product)
-                        ->setProduct($product)
-                        ->setQuantity(-2)
-                        ->setCurrency('USD'),
-                ],
-                ['1-set-2-USD' => ['currency' => 'USD', 'value' => 100]],
-                [null],
-            ],
-            'one matched price' => [
-                [
-                    (new OrderLineItem())
-                        ->setProduct($product)
-                        ->setProductUnit($productUnit)
-                        ->setCurrency('USD')
-                        ->setQuantity('3'),
-                ],
-                [
-                    '1-set-2-USD' => ['currency' => 'USD', 'value' => 100],
-                    '1-set-3-USD' => ['currency' => 'USD', 'value' => 150],
-                ],
-                ['150'],
-            ],
-        ];
     }
 
     /**
@@ -344,5 +163,59 @@ class OrderTaxesListenerTest extends \PHPUnit_Framework_TestCase
                 ],
             ],
         ];
+    }
+
+    public function testOnOrderEventNoData()
+    {
+        $order = new Order();
+        $data = new \ArrayObject();
+
+        $this->taxManager->expects($this->once())
+            ->method('getTax')
+            ->with($order)
+            ->willReturn(new Result());
+
+        $this->taxationSettingsProvider->expects($this->once())
+            ->method('isEnabled')
+            ->willReturn(true);
+
+        $this->event->expects($this->once())
+            ->method('getOrder')
+            ->willReturn($order);
+
+        $this->event->expects($this->once())
+            ->method('getData')
+            ->willReturn($data);
+
+        $this->priceMatcher->expects($this->never())->method('fillMatchingPrices');
+
+        $this->listener->onOrderEvent($this->event);
+    }
+
+    public function testOnOrderEventNoPrices()
+    {
+        $order = new Order();
+        $data = new \ArrayObject([MatchingPriceEventListener::MATCHED_PRICES_KEY => []]);
+
+        $this->taxManager->expects($this->once())
+            ->method('getTax')
+            ->with($order)
+            ->willReturn(new Result());
+
+        $this->taxationSettingsProvider->expects($this->once())
+            ->method('isEnabled')
+            ->willReturn(true);
+
+        $this->event->expects($this->once())
+            ->method('getOrder')
+            ->willReturn($order);
+
+        $this->event->expects($this->once())
+            ->method('getData')
+            ->willReturn($data);
+
+        $this->priceMatcher->expects($this->never())->method('fillMatchingPrices');
+
+        $this->listener->onOrderEvent($this->event);
     }
 }
