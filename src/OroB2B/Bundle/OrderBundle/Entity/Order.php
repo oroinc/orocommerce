506245e00cdc763889b079e3a56b8181afb0fc02
Oleg Popadko <oleg@popadko.com>
2016-04-01 18:56:07 +0700

    BB-2503: Undefined prices after converting RFQ to order   - using DI for entity listener configuration

--- src/OroB2B/Bundle/OrderBundle/Entity/Order.php
+++ src/OroB2B/Bundle/OrderBundle/Entity/Order.php
@@ -69,7 +69,6 @@ use OroB2B\Bundle\WebsiteBundle\Entity\Website;
  * @SuppressWarnings(PHPMD.ExcessiveClassLength)
  * @SuppressWarnings(PHPMD.ExcessivePublicCount)
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
- * @ORM\EntityListeners({ "OroB2B\Bundle\OrderBundle\EventListener\ORM\OrderListener" })
  */
 class Order extends ExtendOrder implements
     OrganizationAwareInterface,
