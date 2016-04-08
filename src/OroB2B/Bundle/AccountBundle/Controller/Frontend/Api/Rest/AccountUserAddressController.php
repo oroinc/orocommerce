cc89068476154c2e37ad9a334e1eda8d4215dbe1
Mykhailo Sulyma <msulima@magecore.com>
2016-04-08 15:00:59 +0300

    BB-2648: Address Book on Layouts  - updated api controller

--- src/OroB2B/Bundle/AccountBundle/Controller/Frontend/Api/Rest/AccountUserAddressController.php
+++ src/OroB2B/Bundle/AccountBundle/Controller/Frontend/Api/Rest/AccountUserAddressController.php
@@ -2,54 +2,13 @@
 
 namespace OroB2B\Bundle\AccountBundle\Controller\Frontend\Api\Rest;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
-
 use FOS\RestBundle\Controller\Annotations\NamePrefix;
 
-use Nelmio\ApiDocBundle\Annotation\ApiDoc;
-
-use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
-
-use OroB2B\Bundle\AccountBundle\Controller\Api\Rest\AbstractAccountUserAddressController;
+use OroB2B\Bundle\AccountBundle\Controller\Api\Rest\AccountUserAddressController as BaseAccountUserAddressController;
 
 /**
  * @NamePrefix("orob2b_api_account_frontend_")
  */
-class AccountUserAddressController extends AbstractAccountUserAddressController
+class AccountUserAddressController extends BaseAccountUserAddressController
 {
-    /**
-     * REST GET list
-     *
-     * @ApiDoc(
-     *      description="Get all addresses items",
-     *      resource=true
-     * )
-     * @AclAncestor("orob2b_account_account_user_view")
-     * @param int $entityId
-     *
-     * @return JsonResponse
-     */
-    public function cgetAction($entityId)
-    {
-        return parent::cgetAction($entityId);
-    }
-
-    /**
-     * REST DELETE address
-     *
-     * @ApiDoc(
-     *      description="Delete address items",
-     *      resource=true
-     * )
-     * @AclAncestor("orob2b_account_account_user_delete")
-     * @param int $entityId
-     * @param int $addressId
-     *
-     * @return Response
-     */
-    public function deleteAction($entityId, $addressId)
-    {
-        return parent::deleteAction($entityId, $addressId);
-    }
 }
