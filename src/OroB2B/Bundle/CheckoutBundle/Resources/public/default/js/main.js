118155c6be7e04a832248662dc00b804317c313d
Andrey Yatsenco <yatsenco@gmail.com>
2016-04-13 17:52:29 +0300

    BB-2575: Order Review on Checkout Steps - added remembering state of Order Summary tab

--- src/OroB2B/Bundle/CheckoutBundle/Resources/public/default/js/main.js
+++ src/OroB2B/Bundle/CheckoutBundle/Resources/public/default/js/main.js
@@ -1,11 +1,25 @@
-require(['jquery', 'oroui/js/mediator'], function(
-    jQuery,
-    mediator
-) {
-    (function($) {
-        'use strict';
-        $('.checkout-order-summary__header [data-tab-trigger]').on('tab:triggered', function() {
-            mediator.trigger('scrollable-table:reload');
-        })
-    })(jQuery);
-});
+require(['jquery', 'oroui/js/mediator', 'jquery.cookie'],
+    function(jQuery, mediator, cookie) {
+        (function($) {
+            'use strict';
+            var $orderSummaryTab = $('.checkout-order-summary__tab');
+            var $orderSummaryTabTrigger = $('.checkout-order-summary__header [data-tab-trigger]');
+            var orderTabStateCookieName = 'order-tab:state';
+
+            $orderSummaryTabTrigger.on('tab:triggered', function() {
+                mediator.trigger('scrollable-table:reload');
+
+                if ($orderSummaryTab.hasClass('active')) {
+                    $.cookie(orderTabStateCookieName, true, {path: window.location.pathname});
+                } else {
+                    $.cookie(orderTabStateCookieName, null, {path: window.location.pathname});
+                }
+            });
+
+            if ($.cookie(orderTabStateCookieName)) {
+                $orderSummaryTab.addClass('active');
+            } else {
+                $orderSummaryTab.removeClass('active');
+            }
+        })(jQuery);
+    });
