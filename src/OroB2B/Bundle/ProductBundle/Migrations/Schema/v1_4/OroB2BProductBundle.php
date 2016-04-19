f1a15b1da0265679a0ea9574040c2675d966ec03
Arsen Aliev <aliev.ars@gmail.com>
2016-04-19 20:53:41 +0300

    BB-1950 Multiple Product Images  - fixed migration (migrate old image to prodoct_image by raw sql)  - added migration for removing old image relation

--- src/OroB2B/Bundle/ProductBundle/Migrations/Schema/v1_4/OroB2BProductBundle.php
+++ src/OroB2B/Bundle/ProductBundle/Migrations/Schema/v1_4/OroB2BProductBundle.php
@@ -2,8 +2,10 @@
 
 namespace OroB2B\Bundle\ProductBundle\Migrations\Schema\v1_4;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Schema\Schema;
 
+use Doctrine\DBAL\Types\Type;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -21,6 +23,7 @@ class OroB2BProductBundle implements
 {
     const PRODUCT_TABLE_NAME = 'orob2b_product';
     const PRODUCT_IMAGE_TABLE_NAME = 'orob2b_product_image';
+    const PRODUCT_IMAGE_FIELD_NAME = 'image_id';
     const MAX_PRODUCT_IMAGE_SIZE_IN_MB = 10;
 
     /**
@@ -52,15 +55,22 @@ class OroB2BProductBundle implements
     /**
      * {@inheritdoc}
      */
+    public function setConnection(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function up(Schema $schema, QueryBag $queries)
     {
         $table = $schema->createTable(self::PRODUCT_IMAGE_TABLE_NAME);
         $table->addColumn('id', 'integer', ['autoincrement' => true]);
         $table->addColumn('product_id', 'integer', ['notnull' => true]);
-        $table->addColumn('types', 'array', ['notnull' => false, 'comment' => '(DC2Type:array)']);
+        $table->addColumn('types', 'array', ['notnull' => false]);
         $table->setPrimaryKey(['id']);
 
-        //foreign key
         $table->addForeignKeyConstraint(
             $schema->getTable(self::PRODUCT_TABLE_NAME),
             ['product_id'],
@@ -68,7 +78,6 @@ class OroB2BProductBundle implements
             ['onUpdate' => null, 'onDelete' => 'CASCADE']
         );
 
-        //image relation
         $this->attachmentExtension->addImageRelation(
             $schema,
             self::PRODUCT_IMAGE_TABLE_NAME,
@@ -79,13 +88,7 @@ class OroB2BProductBundle implements
             self::MAX_PRODUCT_IMAGE_SIZE_IN_MB
         );
 
-        //migrate old image field to product image with types
-        $queries->addPostQuery(new MigrateImageToProductImageQuery(
-            $this->container->get('oro_entity.doctrine_helper'),
-            $this->container->get('oro_layout.provider.image_type'),
-            $this->container->getParameter('orob2b_product.entity.product.class'),
-            $this->container->getParameter('orob2b_product.entity.product_image.class')
-        ));
+        $this->migrateImages($queries);
     }
 
     /**
@@ -94,5 +97,38 @@ class OroB2BProductBundle implements
     public function getOrder()
     {
         return 10;
+    }
+
+    /**
+     * @param QueryBag $queries
+     */
+    protected function migrateImages(QueryBag $queries)
+    {
+        $migrateImagesSqlMask = 'INSERT INTO %1$s (product_id, %2$s, types)
+                                 SELECT id, %2$s, \'%3$s\' FROM %4$s
+                                 WHERE %2$s IS NOT NULL';
+
+        $queries->addPostQuery(
+            sprintf(
+                $migrateImagesSqlMask,
+                self::PRODUCT_IMAGE_TABLE_NAME,
+                self::PRODUCT_IMAGE_FIELD_NAME,
+                $this->getDatabaseValueOfAllImageTypes(),
+                self::PRODUCT_TABLE_NAME
+            )
+        );
+    }
+
+    /**
+     * @return string
+     */
+    protected function getDatabaseValueOfAllImageTypes()
+    {
+        $imageTypeProvider = $this->container->get('oro_layout.provider.image_type');
+        $connection = $this->container->get('database_connection');
+
+        $allImageTypes = array_keys($imageTypeProvider->getImageTypes());
+
+        return $connection->convertToDatabaseValue($allImageTypes, Type::TARRAY);
     }
 }
