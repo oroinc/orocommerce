1128e17b63e6ae0b3d0aecb314b64e4651479fdd
Nikita Makarov <mesaverde228@gmail.com>
2016-04-21 10:38:04 +0300

    BB-2856: Add Demo Data - Added migrations demo data

---
+++ src/OroB2B/Bundle/ShippingBundle/Migrations/Data/Demo/ORM/LoadShippingOriginDemoData.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace OroB2B\Bundle\ShippingBundle\Migrations\Data\Demo\ORM;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Oro\Bundle\UserBundle\DataFixtures\UserUtilityTrait;
+
+use OroB2B\Bundle\ShippingBundle\Entity\ShippingOriginWarehouse;
+
+use OroB2B\Bundle\WarehouseBundle\Entity\Warehouse;
+
+class LoadShippingOriginDemoData extends AbstractFixture implements ContainerAwareInterface, DependentFixtureInterface
+{
+    use UserUtilityTrait;
+
+    /**
+     * @var array
+     */
+    protected $demoData = [
+        [
+            'name' => 'Warehouse With ShippingOrigin 1',
+            'shipping_origin' => [
+                'region_text' => 'Alabama',
+                'postal_code' => '36849',
+                'country' => 'US',
+                'region' => 'US-AL',
+                'city' => 'Auburn',
+                'street' => 'Street',
+                'street2' => 'Street2',
+            ]
+        ],
+        [
+            'name' => 'Warehouse With ShippingOrigin 2',
+            'shipping_origin' => [
+                'region_text' => 'Illinois',
+                'postal_code' => '60502',
+                'country' => 'US',
+                'region' => 'US-IL',
+                'city' => 'Aurora',
+                'street' => 'Street',
+                'street2' => 'Street2',
+            ]
+        ],
+        ['name' => 'Warehouse No ShippingOrigin']
+    ];
+
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDependencies()
+    {
+        return [
+            'OroB2B\Bundle\WarehouseBundle\Migrations\Data\Demo\ORM\LoadWarehouseDemoData',
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function load(ObjectManager $manager)
+    {
+        $user = $this->getFirstUser($manager);
+        $businessUnit = $user->getOwner();
+        $organization = $user->getOrganization();
+        foreach ($this->demoData as $row) {
+            $warehouse = new Warehouse();
+            $warehouse
+                ->setName($row['name'])
+                ->setOwner($businessUnit)
+                ->setOrganization($organization);
+            $manager->persist($warehouse);
+            if (isset($row['shipping_origin'])) {
+                $shippingOriginWareHouse = new ShippingOriginWarehouse($row['shipping_origin']);
+                $shippingOriginWareHouse->setWarehouse($warehouse);
+                $manager->persist($shippingOriginWareHouse);
+            }
+        }
+    }
+}
