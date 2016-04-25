c06a74377acd9120d39f33f04c1f6c614f03782b
Nikita Makarov <mesaverde228@gmail.com>
2016-04-25 09:10:16 +0300

    BB-2851: Add WarehouseShippingOrigin Provider - Fixed methods docblocks

--- src/OroB2B/Bundle/ShippingBundle/Provider/ShippingOriginProvider.php
+++ src/OroB2B/Bundle/ShippingBundle/Provider/ShippingOriginProvider.php
@@ -40,8 +40,6 @@ class ShippingOriginProvider
     }
 
     /**
-     * Returns null if Warehouse uses ShippingOrigin from system configuration
-     *
      * @param Warehouse $warehouse
      *
      * @return ShippingOrigin
@@ -63,6 +61,9 @@ class ShippingOriginProvider
         return $this->getSystemShippingOrigin();
     }
 
+    /**
+     * @return ShippingOrigin
+     */
     public function getSystemShippingOrigin()
     {
         $configData = $this->configManager->get('oro_b2b_shipping.shipping_origin', true, true);
