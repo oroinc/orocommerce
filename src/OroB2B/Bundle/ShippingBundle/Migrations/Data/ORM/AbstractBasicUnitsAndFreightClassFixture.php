4133c322e610dabe22edbc693c8506020ac206c3
Nikita Makarov <mesaverde228@gmail.com>
2016-04-27 18:33:19 +0300

    BB-2909: Add fixtures for basic units and freight class - Fixed after review

--- src/OroB2B/Bundle/ShippingBundle/Migrations/Data/ORM/AbstractBasicUnitsAndFreightClassFixture.php
+++ src/OroB2B/Bundle/ShippingBundle/Migrations/Data/ORM/AbstractBasicUnitsAndFreightClassFixture.php
@@ -2,19 +2,17 @@
 
 namespace OroB2B\Bundle\ShippingBundle\Migrations\Data\ORM;
 
-use Doctrine\ORM\EntityRepository;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
-
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Doctrine\Common\Persistence\ObjectRepository;
 
 use OroB2B\Bundle\ShippingBundle\Entity\DimensionUnit;
 use OroB2B\Bundle\ShippingBundle\Entity\FreightClass;
 use OroB2B\Bundle\ShippingBundle\Entity\WeightUnit;
 
-abstract class AbstractBasicUnitsAndFreightClassFixture extends AbstractFixture implements ContainerAwareInterface
+abstract class AbstractBasicUnitsAndFreightClassFixture extends AbstractFixture
 {
+
     /** @var array */
     protected $weightUnits = [];
 
@@ -24,27 +22,31 @@ abstract class AbstractBasicUnitsAndFreightClassFixture extends AbstractFixture 
     /** @var array */
     protected $freightClasses = [];
 
-    /**
-     * @var ContainerInterface
-     */
-    protected $container;
-
-    /**
-     * {@inheritDoc}
-     */
-    public function setContainer(ContainerInterface $container = null)
-    {
-        $this->container = $container;
-    }
+    /** @var ObjectManager */
+    protected $manager;
 
     /**
      * {@inheritDoc}
      */
     public function load(ObjectManager $manager)
     {
+        $this->manager = $manager;
+
         $this->addWeightUnits($this->weightUnits);
         $this->addDimensionUnits($this->dimensionUnits);
         $this->addFreightClasses($this->freightClasses);
+
+        $this->manager->flush();
+    }
+
+    /**
+     * @param string $class
+     *
+     * @return ObjectRepository
+     */
+    protected function getRepositoryForClass($class)
+    {
+        return $this->manager->getRepository($class);
     }
 
     /**
@@ -53,21 +55,18 @@ abstract class AbstractBasicUnitsAndFreightClassFixture extends AbstractFixture 
     protected function addWeightUnits(array $weightUnits)
     {
         $class = 'OroB2BShippingBundle:WeightUnit';
-        /** @var ObjectManager $manager */
-        $manager = $this->container->get('doctrine')->getManagerForClass($class);
 
-        /** @var EntityRepository $repository */
-        $repository = $manager->getRepository($class);
+        /** @var ObjectRepository $repository */
+        $repository = $this->getRepositoryForClass($class);
         foreach ($weightUnits as $unit) {
             if (!$repository->findOneBy(['code' => $unit['code']])) {
                 $entity = new WeightUnit();
-                $entity->setCode($unit['code']);
-                $entity->setConversionRates($unit['conversion_rates']);
-                $manager->persist($entity);
+                $entity->setCode($unit['code'])
+                    ->setConversionRates($unit['conversion_rates'])
+                ;
+                $this->manager->persist($entity);
             }
         }
-
-        $manager->flush();
     }
 
     /**
@@ -77,21 +76,17 @@ abstract class AbstractBasicUnitsAndFreightClassFixture extends AbstractFixture 
     {
         $class = 'OroB2BShippingBundle:DimensionUnit';
 
-        /** @var ObjectManager $manager */
-        $manager = $this->container->get('doctrine')->getManagerForClass($class);
-
-        /** @var EntityRepository $repository */
-        $repository = $manager->getRepository($class);
+        /** @var ObjectRepository $repository */
+        $repository = $this->getRepositoryForClass($class);
         foreach ($dimensionUnits as $unit) {
             if (!$repository->findOneBy(['code' => $unit['code']])) {
                 $entity = new DimensionUnit();
-                $entity->setCode($unit['code']);
-                $entity->setConversionRates($unit['conversion_rates']);
-                $manager->persist($entity);
+                $entity->setCode($unit['code'])
+                    ->setConversionRates($unit['conversion_rates'])
+                ;
+                $this->manager->persist($entity);
             }
         }
-
-        $manager->flush();
     }
 
     /**
@@ -101,19 +96,14 @@ abstract class AbstractBasicUnitsAndFreightClassFixture extends AbstractFixture 
     {
         $class = 'OroB2BShippingBundle:FreightClass';
 
-        /** @var ObjectManager $manager */
-        $manager = $this->container->get('doctrine')->getManagerForClass($class);
-
-        /** @var EntityRepository $repository */
-        $repository = $manager->getRepository($class);
+        /** @var ObjectRepository $repository */
+        $repository = $this->getRepositoryForClass($class);
         foreach ($freightClasses as $unit) {
             if (!$repository->findOneBy(['code' => $unit['code']])) {
                 $entity = new FreightClass();
                 $entity->setCode($unit['code']);
-                $manager->persist($entity);
+                $this->manager->persist($entity);
             }
         }
-
-        $manager->flush();
     }
 }
--- src/OroB2B/Bundle/ShippingBundle/Migrations/Data/ORM/LoadBasicUnitsAndFreightClassData.php
+++ src/OroB2B/Bundle/ShippingBundle/Migrations/Data/ORM/LoadBasicUnitsAndFreightClassData.php
@@ -4,16 +4,21 @@ namespace OroB2B\Bundle\ShippingBundle\Migrations\Data\ORM;
 
 class LoadBasicUnitsAndFreightClassData extends AbstractBasicUnitsAndFreightClassFixture
 {
+    /** @var array */
     protected $weightUnits = [
         ['code' => 'lbs' , 'conversion_rates' => []],
         ['code' => 'kg' , 'conversion_rates' => []],
     ];
+
+    /** @var array */
     protected $dimensionUnits = [
         ['code' => 'inches' , 'conversion_rates' => []],
         ['code' => 'feet & inches' , 'conversion_rates' => []],
         ['code' => 'cm' , 'conversion_rates' => []],
         ['code' => 'm' , 'conversion_rates' => []],
     ];
+
+    /** @var array */
     protected $freightClasses = [
         ['code' => 'Parcel'],
     ];
