eac99d13e1317c52ddaaadb941ecf85821574c64
Mykhailo Sulyma <msulima@magecore.com>
2016-04-27 16:17:30 +0300

    BB-2855: Update functional tests  - updated functional tests  - updated block order on pages

--- src/OroB2B/Bundle/ShippingBundle/Tests/Functional/Controller/SystemConfigurationTest.php
+++ src/OroB2B/Bundle/ShippingBundle/Tests/Functional/Controller/SystemConfigurationTest.php
@@ -2,32 +2,21 @@
 
 namespace OroB2B\Bundle\ShippingBundle\Tests\Functional\Controller;
 
-use Doctrine\ORM\EntityRepository;
-
-use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
 use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 
 use Oro\Component\PhpUtils\ArrayUtil;
 
-use OroB2B\Bundle\WarehouseBundle\Entity\Warehouse;
-
 class SystemConfigurationTest extends WebTestCase
 {
-    const WAREHOUSE_CLASS = 'OroB2B\Bundle\WarehouseBundle\Entity\Warehouse';
-
     /** @var ConfigManager */
     protected $configManager;
-
-    /** @var  DoctrineHelper */
-    protected $doctrineHelper;
 
     protected function setUp()
     {
         $this->initClient([], $this->generateBasicAuthHeader());
 
         $this->configManager = $this->getContainer()->get('oro_config.global');
-        $this->doctrineHelper = $this->getContainer()->get('oro_entity.doctrine_helper');
     }
 
     protected function tearDown()
@@ -90,75 +79,6 @@ class SystemConfigurationTest extends WebTestCase
                 'street2' => 'street3',
             ],
             $this->configManager->get('orob2b_shipping.shipping_origin')
-        );
-    }
-
-    public function testWarehouse()
-    {
-        /** @var EntityRepository $repo */
-        $repo = $this->doctrineHelper
-            ->getEntityManagerForClass(static::WAREHOUSE_CLASS)
-            ->getRepository(static::WAREHOUSE_CLASS);
-
-        /** @var Warehouse $warehouse */
-        $warehouse = $repo->findOneBy([]);
-
-        $crawler = $this->client->request(
-            'GET',
-            $this->getUrl(
-                'orob2b_warehouse_update',
-                ['id' => $warehouse->getId()]
-            )
-        );
-        $this->assertHtmlResponseStatusCodeEquals($this->client->getResponse(), 200);
-
-        $token = $this->getContainer()->get('security.csrf.token_manager')
-            ->getToken('shipping_origin')
-            ->getValue();
-        $form = $crawler->selectButton('Save settings')->form();
-        $formData = ArrayUtil::arrayMergeRecursiveDistinct(
-            $form->getPhpValues(),
-            [
-                'shipping_origin_warehouse' => [
-                    'country' => 'US',
-                    'region' => 'US-NY',
-                    'postalCode' => 'code2',
-                    'city' => 'city2',
-                    'street' => 'street2',
-                    'street2' => 'street3',
-                    '_token' => $token,
-                ],
-            ]
-        );
-
-        $this->client->followRedirects(true);
-        $this->client->request($form->getMethod(), $form->getUri(), $formData);
-
-        $this->assertHtmlResponseStatusCodeEquals($this->client->getResponse(), 200);
-
-        $token = $this->getContainer()->get('security.csrf.token_manager')
-            ->getToken('shipping_origin')
-            ->getValue();
-        $form = $crawler->selectButton('Save settings')->form();
-        // Retake all data from regenerated form and merge with expected
-        $formData = ArrayUtil::arrayMergeRecursiveDistinct(
-            $form->getPhpValues(),
-            [
-                'shipping_origin_warehouse' => [
-                    'country' => 'US',
-                    'region' => 'US-NY',
-                    'postalCode' => 'code2',
-                    'city' => 'city2',
-                    'street' => 'street2',
-                    'street2' => 'street3',
-                    '_token' => $token,
-                ],
-            ]
-        );
-
-        $this->assertEquals(
-            $formData,
-            $form->getPhpValues()
         );
     }
 }
