87bd3992004c1247eb5b7770ec8c887489bfe892
Alexandr Parkhomenko <alexandr.parhomenk@gmail.com>
2016-05-04 20:17:06 +0300

    BAP-9463: Provide compatibility with PHP7 - Fix error with configure method that doesn't exist

--- src/OroB2B/Bundle/AccountBundle/Tests/Unit/Owner/FrontendOwnerTreeProviderTest.php
+++ src/OroB2B/Bundle/AccountBundle/Tests/Unit/Owner/FrontendOwnerTreeProviderTest.php
@@ -63,15 +63,7 @@ class FrontendOwnerTreeProviderTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        $this->cache = $this->getMockForAbstractClass(
-            'Doctrine\Common\Cache\CacheProvider',
-            [],
-            '',
-            true,
-            true,
-            true,
-            ['fetch', 'save']
-        );
+        $this->cache = $this->getMock('Doctrine\Common\Cache\CacheProvider');
         $this->cache->expects($this->any())
             ->method('fetch')
             ->will($this->returnValue(false));
