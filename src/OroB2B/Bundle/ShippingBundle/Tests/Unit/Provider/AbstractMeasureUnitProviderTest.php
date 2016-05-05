9bc57d9d55836d278a77e3aea02b89edcef8f833
Yauheni Kasenka <beta.screech@gmail.com>
2016-05-05 12:03:00 +0300

    BB-2919: Add Ajax controller for search actual collection of Freight Classes - updated unit tests

--- src/OroB2B/Bundle/ShippingBundle/Tests/Unit/Provider/AbstractMeasureUnitProviderTest.php
+++ src/OroB2B/Bundle/ShippingBundle/Tests/Unit/Provider/AbstractMeasureUnitProviderTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace OroB2B\Bundle\ShippingBundle\Bundle\Tests\Unit\Provider;
+namespace OroB2B\Bundle\ShippingBundle\Tests\Unit\Provider;
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
@@ -8,6 +8,8 @@ use Doctrine\ORM\EntityRepository;
 use Oro\Bundle\ApiBundle\Util\DoctrineHelper;
 use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 
+use OroB2B\Bundle\ProductBundle\Entity\MeasureUnitInterface;
+use OroB2B\Bundle\ProductBundle\Formatter\UnitLabelFormatter;
 use OroB2B\Bundle\ShippingBundle\Provider\AbstractMeasureUnitProvider;
 
 class AbstractMeasureUnitProviderTest extends \PHPUnit_Framework_TestCase
@@ -27,6 +29,9 @@ class AbstractMeasureUnitProviderTest extends \PHPUnit_Framework_TestCase
     /** @var ConfigManager|\PHPUnit_Framework_MockObject_MockObject */
     protected $configManager;
 
+    /** @var UnitLabelFormatter|\PHPUnit_Framework_MockObject_MockObject */
+    protected $labelFormatter;
+
     protected function setUp()
     {
         $this->repo = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
@@ -45,11 +50,22 @@ class AbstractMeasureUnitProviderTest extends \PHPUnit_Framework_TestCase
             ->getMockBuilder('Oro\Bundle\ConfigBundle\Config\ConfigManager')
             ->disableOriginalConstructor()
             ->getMock();
+
+        $this->labelFormatter = $this->getMockBuilder('OroB2B\Bundle\ProductBundle\Formatter\UnitLabelFormatter')
+            ->disableOriginalConstructor()
+            ->getMock();
     }
 
     public function tearDown()
     {
-        unset($this->provider, $this->configManager, $this->doctrineHelper, $this->em, $this->repo);
+        unset(
+            $this->provider,
+            $this->configManager,
+            $this->doctrineHelper,
+            $this->em,
+            $this->repo,
+            $this->labelFormatter
+        );
     }
 
     /**
@@ -59,7 +75,7 @@ class AbstractMeasureUnitProviderTest extends \PHPUnit_Framework_TestCase
      *
      * @dataProvider unitsProvider
      */
-    public function testGetOptions($entityClass, $configEntryName, $expected)
+    public function testGetUnits($entityClass, $configEntryName, $expected)
     {
         $this->repo->expects($this->atLeastOnce())
             ->method('findAll')
@@ -84,6 +100,7 @@ class AbstractMeasureUnitProviderTest extends \PHPUnit_Framework_TestCase
         $provider = new AbstractMeasureUnitProvider($this->doctrineHelper, $this->configManager);
         $provider->setEntityClass($entityClass);
         $provider->setConfigEntryName($configEntryName);
+        $provider->setLabelFormatter($this->labelFormatter);
         $this->assertEquals($configEntryName, $this->getProperty($provider, 'configEntryName'));
         $this->assertEquals($entityClass, $this->getProperty($provider, 'entityClass'));
         return $provider;
@@ -111,6 +128,99 @@ class AbstractMeasureUnitProviderTest extends \PHPUnit_Framework_TestCase
                 'expected' => []
             ],
         ];
+    }
+
+    /**
+     * @param array $inputData
+     * @param array $expectedData
+     *
+     * @dataProvider getUnitsCodesProvider
+     */
+    public function testGetUnitsCodes(array $inputData, array $expectedData)
+    {
+        $this->repo->expects($this->once())
+            ->method('findAll')
+            ->willReturn($inputData['units']);
+
+        $this->doctrineHelper->expects($this->once())
+            ->method('getEntityRepositoryForClass')
+            ->with('entityClass')
+            ->willReturn($this->repo);
+
+        $provider = $this->createProvider('entityClass', 'configKey');
+        $this->assertEquals($expectedData, $provider->getUnitsCodes(false));
+    }
+
+    /**
+     * @return array
+     */
+    public function getUnitsCodesProvider()
+    {
+        return [
+            [
+                'input' => [
+                    'units' => [
+                        $this->getMeasureUnit('kg'),
+                        $this->getMeasureUnit('mg'),
+                    ],
+                ],
+                'expected' => ['kg', 'mg'],
+            ],
+        ];
+    }
+
+    /**
+     * @param array $inputData
+     * @param array $expectedData
+     *
+     * @dataProvider formatUnitsCodesProvider
+     */
+    public function testFormatUnitsCodes(array $inputData, array $expectedData)
+    {
+        $this->labelFormatter->expects($this->any())
+            ->method('format')
+            ->willReturnCallback(function ($item, $isShort) {
+                return $item . '.formatted' . ($isShort ? '.short' : '');
+            });
+
+        $provider = $this->createProvider('entityClass', 'configKey');
+        $this->assertEquals($expectedData, $provider->formatUnitsCodes($inputData['codes'], $inputData['isShort']));
+    }
+
+    /**
+     * @return array
+     */
+    public function formatUnitsCodesProvider()
+    {
+        return [
+            'normal' => [
+                'input' => [
+                    'codes' => ['kg', 'mg'],
+                    'isShort' => false,
+                ],
+                'expected' => ['kg.formatted', 'mg.formatted'],
+            ],
+            'short' => [
+                'input' => [
+                    'codes' => ['kg', 'mg'],
+                    'isShort' => true,
+                ],
+                'expected' => ['kg.formatted.short', 'mg.formatted.short'],
+            ],
+        ];
+    }
+
+    /**
+     * @return MeasureUnitInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getMeasureUnit($code)
+    {
+        $unit = $this->getMock('OroB2B\Bundle\ProductBundle\Entity\MeasureUnitInterface');
+        $unit->expects($this->any())
+            ->method('getCode')
+            ->willReturn($code);
+
+        return $unit;
     }
 
     /**
