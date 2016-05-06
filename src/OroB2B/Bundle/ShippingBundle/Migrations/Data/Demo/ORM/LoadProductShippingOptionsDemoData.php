658b8b8c294cbc3344e8d619917f1888360cb375
Nikita Makarov <mesaverde228@gmail.com>
2016-05-06 14:28:29 +0300

    BB-2923: Add demo data - Code Style Fixes

--- src/OroB2B/Bundle/ShippingBundle/Migrations/Data/Demo/ORM/LoadProductShippingOptionsDemoData.php
+++ src/OroB2B/Bundle/ShippingBundle/Migrations/Data/Demo/ORM/LoadProductShippingOptionsDemoData.php
@@ -6,7 +6,6 @@ use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
