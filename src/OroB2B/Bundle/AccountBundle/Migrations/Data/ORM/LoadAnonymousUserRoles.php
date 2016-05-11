9169e7a8de557300d12e2aadaac9c9f594b84ca0
Vitaliy Berdylo <vberdylo@magecore.com>
2016-05-11 11:58:27 +0300

    BB-2984: Create anonymous role fixture - CR fixes

--- src/OroB2B/Bundle/AccountBundle/Migrations/Data/ORM/LoadAnonymousUserRoles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace OroB2B\Bundle\AccountBundle\Migrations\Data\ORM;
-
-class LoadAnonymousUserRoles extends LoadAccountUserRoles
-{
-    const ROLES_FILE_NAME = 'anonymous_roles.yml';
-}
