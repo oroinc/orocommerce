19226561820c32e1c4aad15d207f8ee5afd7c12f
Roman Yarmolenko <devxypro@gmail.com>
2016-05-12 17:45:08 +0300

    BB-3089: Duplicate price list schedule - add service - add test - use service method in actions

---
+++ src/OroB2B/Bundle/PricingBundle/Duplicator/ScheduleDuplicator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace OroB2B\Bundle\PricingBundle\Duplicator;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+use OroB2B\Bundle\PricingBundle\Entity\PriceList;
+
+class ScheduleDuplicator
+{
+    /**
+     * @param PriceList $sourcePriceList
+     * @param PriceList $duplicatedPriceList
+     */
+    public function duplicateSchedule(PriceList $sourcePriceList, PriceList $duplicatedPriceList)
+    {
+        $newSchedules = new ArrayCollection();
+        foreach ($sourcePriceList->getSchedules() as $schedule) {
+            if ($schedule->getDeactivateAt() > new \DateTime('now', new \DateTimeZone('UTC'))) {
+                $newSchedules->add($schedule);
+            }
+        };
+        $duplicatedPriceList->setSchedules($newSchedules);
+    }
+}
---
+++ src/OroB2B/Bundle/PricingBundle/Tests/Unit/Duplicator/ScheduleDuplicatorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace OroB2B\Bundle\PricingBundle\Tests\Unit\Duplicator;
+
+use OroB2B\Bundle\PricingBundle\Entity\PriceList;
+use OroB2B\Bundle\PricingBundle\Duplicator\ScheduleDuplicator;
+use OroB2B\Bundle\PricingBundle\Entity\PriceListSchedule;
+
+class ScheduleDuplicatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ScheduleDuplicator
+     */
+    protected $scheduleDuplicator;
+
+    protected function setUp()
+    {
+        $this->scheduleDuplicator = new ScheduleDuplicator();
+    }
+
+    public function testDuplicateSchedule()
+    {
+        $sourcePriceList = new PriceList();
+        $schedule = new PriceListSchedule();
+        $schedule->setDeactivateAt(new \DateTime('now + 1 day', new \DateTimeZone('UTC')));
+        $pastSchedule = clone  $schedule;
+        $pastSchedule->setDeactivateAt(new \DateTime('now - 1 day', new \DateTimeZone('UTC')));
+        $sourcePriceList->addSchedule($schedule);
+        $sourcePriceList->addSchedule($pastSchedule);
+
+        $duplicatedPriceList = new PriceList();
+        $this->assertCount(0, $duplicatedPriceList->getSchedules());
+        $this->scheduleDuplicator->duplicateSchedule($sourcePriceList, $duplicatedPriceList);
+        $this->assertCount(1, $duplicatedPriceList->getSchedules());
+    }
+}
