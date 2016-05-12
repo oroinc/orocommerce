20b9ef52e7d9839e7c928a54962b13f2514baa45
Yauheni Kasenka <beta.screech@gmail.com>
2016-05-12 15:46:45 +0300

    BB-2921: Add demo extension - updated tests

--- src/OroB2B/Bundle/ShippingBundle/Tests/Unit/Provider/FreightClassesProviderTest.php
+++ src/OroB2B/Bundle/ShippingBundle/Tests/Unit/Provider/FreightClassesProviderTest.php
@@ -2,27 +2,28 @@
 
 namespace OroB2B\Bundle\ShippingBundle\Tests\Unit\Provider;
 
-use OroB2B\Bundle\ShippingBundle\Provider\FreightClassesProvider;
-
 use OroB2B\Bundle\ShippingBundle\Entity\FreightClass;
 use OroB2B\Bundle\ShippingBundle\Entity\ProductShippingOptions;
-use OroB2B\Bundle\ShippingBundle\Provider\FreightClassesProviderInterface;
+use OroB2B\Bundle\ShippingBundle\Provider\FreightClassesProvider;
+use OroB2B\Bundle\ShippingBundle\Extension\FreightClassesExtensionInterface;
 
 class FreightClassesProviderTest extends MeasureUnitProviderTest
 {
-    /**
-     * @param array $inputData
-     * @param array $expectedData
-     *
-     * @dataProvider getUnitsByProductShippingOptionsProvider
-     */
-    public function testGetUnitsByProductShippingOptions(array $inputData, array $expectedData)
-    {
-        $provider = $this->createProvider($inputData['units'], $inputData['configUnits'], $inputData['providers']);
+    /** @var FreightClassesProvider */
+    protected $provider;
 
-        $this->assertEquals(
-            array_values($expectedData),
-            $provider->getUnitsByProductShippingOptions($inputData['options'])
+    /**
+     * {@ihneritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->provider = new FreightClassesProvider(
+            $this->repository,
+            $this->configManager,
+            $this->labelFormatter,
+            self::CONFIG_ENTRY_NAME
         );
     }
 
@@ -32,14 +33,20 @@ class FreightClassesProviderTest extends MeasureUnitProviderTest
      *
      * @dataProvider getUnitsByProductShippingOptionsProvider
      */
-    public function testGetUnitsCodesByProductShippingOptions(array $inputData, array $expectedData)
+    public function testGetFreightClasses(array $inputData, array $expectedData)
     {
-        $provider = $this->createProvider($inputData['units'], $inputData['configUnits'], $inputData['providers']);
+        $this->repository->expects($this->once())->method('findAll')->willReturn($inputData['units']);
 
-        $this->assertEquals(
-            array_keys($expectedData),
-            $provider->getUnitsCodesByProductShippingOptions($inputData['options'])
-        );
+        $this->configManager->expects($this->once())
+            ->method('get')
+            ->with(self::CONFIG_ENTRY_NAME, false)
+            ->willReturn($inputData['configUnits']);
+
+        foreach ($inputData['extensions'] as $name => $extension) {
+            $this->provider->addExtension($name, $extension);
+        }
+
+        $this->assertEquals($expectedData, array_values($this->provider->getFreightClasses($inputData['options'])));
     }
 
     /**
@@ -50,7 +57,7 @@ class FreightClassesProviderTest extends MeasureUnitProviderTest
         return [
             'no providers' => [
                 'input' => [
-                    'providers' => [],
+                    'extensions' => [],
                     'options' => new ProductShippingOptions(),
                     'configUnits' => ['class50', 'class55'],
                     'units' => [
@@ -59,35 +66,15 @@ class FreightClassesProviderTest extends MeasureUnitProviderTest
                     ],
                 ],
                 'expected' => [
-                    'class50' => $this->getFreightClass('class50'),
-                    'class55' => $this->getFreightClass('class55'),
+                    $this->getFreightClass('class50'),
+                    $this->getFreightClass('class55'),
                 ],
             ],
-            'existing providers' => [
+            'existing extensions' => [
                 'input' => [
-                    'providers' => [
-                        'provider1' => $this->getClassesProvider(
-                            [
-                                $this->getFreightClass('class50'),
-                                $this->getFreightClass('class55'),
-                                $this->getFreightClass('class60'),
-                            ],
-                            new ProductShippingOptions(),
-                            [
-                                $this->getFreightClass('class50'),
-                            ]
-                        ),
-                        'provider2' => $this->getClassesProvider(
-                            [
-                                $this->getFreightClass('class50'),
-                                $this->getFreightClass('class55'),
-                                $this->getFreightClass('class60'),
-                            ],
-                            new ProductShippingOptions(),
-                            [
-                                $this->getFreightClass('class60'),
-                            ]
-                        ),
+                    'extensions' => [
+                        'extension1' => $this->getClassesExtension(['class50']),
+                        'extension2' => $this->getClassesExtension(['class60']),
                     ],
                     'options' => new ProductShippingOptions(),
                     'configUnits' => ['class50', 'class55', 'class60'],
@@ -98,62 +85,27 @@ class FreightClassesProviderTest extends MeasureUnitProviderTest
                     ],
                 ],
                 'expected' => [
-                    'class50' => $this->getFreightClass('class50'),
-                    'class60' => $this->getFreightClass('class60'),
+                    $this->getFreightClass('class50'),
+                    $this->getFreightClass('class60'),
                 ],
             ],
         ];
     }
 
     /**
-     * @param array $units
-     * @param array $configUnits
-     * @param array $providers
-     * @return FreightClassesProvider
+     * @param array $classes
+     * @return FreightClassesExtensionInterface|\PHPUnit_Framework_MockObject_MockObject
      */
-    private function createProvider(array $units, array $configUnits, array $providers)
+    protected function getClassesExtension(array $classes)
     {
-        $this->repo->expects($this->once())
-            ->method('findAll')
-            ->willReturn($units);
+        $extension = $this->getMock('OroB2B\Bundle\ShippingBundle\Extension\FreightClassesExtensionInterface');
+        $extension->expects($this->any())
+            ->method('isApplicable')
+            ->willReturnCallback(function (FreightClass $class, ProductShippingOptions $options) use ($classes) {
+                return in_array($class->getCode(), $classes, true);
+            });
 
-        $this->doctrineHelper->expects($this->once())
-            ->method('getEntityRepositoryForClass')
-            ->with('entityClass')
-            ->willReturn($this->repo);
-
-        $this->configManager->expects($this->once())
-            ->method('get')
-            ->with('configKey')
-            ->willReturn($configUnits);
-
-        $provider = new FreightClassesProvider($this->doctrineHelper, $this->configManager);
-        $provider->setEntityClass('entityClass');
-        $provider->setConfigEntryName('configKey');
-        $provider->setLabelFormatter($this->labelFormatter);
-
-        foreach ($providers as $name => $subProvider) {
-            $provider->addProvider($name, $subProvider);
-        }
-
-        return $provider;
-    }
-
-    /**
-     * @param array $input
-     * @param ProductShippingOptions $options
-     * @param array $output
-     * @return FreightClassesProviderInterface|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected function getClassesProvider(array $input, ProductShippingOptions $options, array $output)
-    {
-        $provider = $this->getMock('OroB2B\Bundle\ShippingBundle\Provider\FreightClassesProviderInterface');
-        $provider->expects($this->once())
-            ->method('getFreightClasses')
-            ->with($input, $options)
-            ->willReturn($output);
-
-        return $provider;
+        return $extension;
     }
 
     /**
