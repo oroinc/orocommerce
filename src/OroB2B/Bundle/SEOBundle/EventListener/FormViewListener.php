ab35b671bad1db268855236e3ad1f838fa9f630c
Dragos Andries <dandries@pitechnologies.ro>
2016-05-12 13:50:09 +0300

    BB-3098: added seo section on product view page.

---
+++ src/OroB2B/Bundle/SEOBundle/EventListener/FormViewListener.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace OroB2B\Bundle\SEOBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Translation\TranslatorInterface;
+
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Oro\Bundle\UIBundle\Event\BeforeListRenderEvent;
+use Oro\Bundle\UIBundle\View\ScrollData;
+
+/**
+ * @SuppressWarnings(PHPMD.TooManyMethods)
+ */
+class FormViewListener
+{
+    /**
+     * @var TranslatorInterface
+     */
+    protected $translator;
+
+    /**
+     * @var DoctrineHelper
+     */
+    protected $doctrineHelper;
+
+    /**
+     * @var RequestStack
+     */
+    protected $requestStack;
+
+    /**
+     * @param RequestStack $requestStack
+     * @param TranslatorInterface $translator
+     * @param DoctrineHelper $doctrineHelper
+     */
+    public function __construct(
+        RequestStack $requestStack,
+        TranslatorInterface $translator,
+        DoctrineHelper $doctrineHelper
+    ) {
+        $this->requestStack = $requestStack;
+        $this->translator = $translator;
+        $this->doctrineHelper = $doctrineHelper;
+    }
+
+    /**
+     * @param BeforeListRenderEvent $event
+     */
+    public function onProductView(BeforeListRenderEvent $event)
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        if (!$request) {
+            return;
+        }
+
+        $productId = (int)$request->get('id');
+        /** @var Product $product */
+        $product = $this->doctrineHelper->getEntityReference('OroB2BProductBundle:Product', $productId);
+
+        $template = $event->getEnvironment()->render(
+            'OroB2BSEOBundle:Product:seo_view.html.twig',
+            ['entity' => $product]
+        );
+        $this->addProductPricesBlock($event->getScrollData(), $template);
+    }
+
+    /**
+     * @param ScrollData $scrollData
+     * @param string $html
+     */
+    protected function addProductPricesBlock(ScrollData $scrollData, $html)
+    {
+        $blockLabel = $this->translator->trans('orob2b.seo.label');
+        $blockId = $scrollData->addBlock($blockLabel, 10);
+        $subBlockId = $scrollData->addSubBlock($blockId);
+        $scrollData->addSubBlockData($blockId, $subBlockId, $html);
+    }    
+}
