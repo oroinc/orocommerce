df22df639c959ea413c8c9ce662d5df3ebe6519e
Yevhen Petrov <ypetrov@magecore.com>
2016-05-13 18:54:28 +0300

    BB-3089: Duplicate price list schedule - fix

--- src/OroB2B/Bundle/PricingBundle/Duplicator/ScheduleDuplicator.php
+++ src/OroB2B/Bundle/PricingBundle/Duplicator/ScheduleDuplicator.php
@@ -14,6 +14,7 @@ class ScheduleDuplicator
      */
     public function duplicateSchedule(PriceList $sourcePriceList, PriceList $duplicatedPriceList)
     {
+        $duplicatedPriceList->setContainSchedule(false);
         $now = new \DateTime('now', new \DateTimeZone('UTC'));
         foreach ($sourcePriceList->getSchedules() as $schedule) {
             if ($schedule->getDeactivateAt() === null ||
