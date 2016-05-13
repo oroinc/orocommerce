16ef051ad49cbc92bbc06a1fa97f71a098cfa457
Yevhen Shyshkin <yshyshkin@magecore.com>
2016-05-13 19:06:30 +0200

    BB-2926: Make OroCommerce services private by default  - applied private file loaders

--- src/OroB2B/Bundle/TaxBundle/DependencyInjection/OroB2BTaxExtension.php
+++ src/OroB2B/Bundle/TaxBundle/DependencyInjection/OroB2BTaxExtension.php
@@ -5,7 +5,8 @@ namespace OroB2B\Bundle\TaxBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+
+use OroB2B\Bundle\FrontendBundle\DependencyInjection\Loader\PrivateYamlFileLoader;
 
 class OroB2BTaxExtension extends Extension
 {
@@ -20,7 +21,7 @@ class OroB2BTaxExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
         $container->prependExtensionConfig($this->getAlias(), $config);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new PrivateYamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
         $loader->load('form_types.yml');
     }
