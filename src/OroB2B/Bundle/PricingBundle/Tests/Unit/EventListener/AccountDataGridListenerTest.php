2f692a6c81b120e7c17ca45486a8ce1fba72dd51
Yevhen Petrov <ypetrov@magecore.com>
2016-05-17 15:15:18 +0300

    BB-3130: Add column to data grid - added empty tests

---
+++ src/OroB2B/Bundle/PricingBundle/Tests/Unit/EventListener/AccountDataGridListenerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace OroB2B\Bundle\PricingBundle\Tests\Unit\EventListener;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Oro\Bundle\DataGridBundle\Datagrid\DatagridInterface;
+use Oro\Bundle\DataGridBundle\Event\OrmResultAfter;
+use OroB2B\Bundle\PricingBundle\EventListener\AccountDataGridListener;
+
+use Symfony\Component\Translation\TranslatorInterface;
+
+class AccountDataGridListenerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|ManagerRegistry
+     */
+    protected $registry;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|ObjectManager
+     */
+    protected $manager;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|AccountDataGridListener
+     */
+    protected $listener;
+
+    public function setUp()
+    {
+        $className = 'OroB2B\Bundle\PricingBundle\Entity\Repository\PriceListToAccountRepository';
+        $repository = $this->getMockBuilder($className)->disableOriginalConstructor()->getMock();
+
+        $this->manager = $this->getMock('Doctrine\Common\Persistence\ObjectManager');
+        $this->manager->method('getRepository')->willReturnMap([
+            ['OroB2BPricingBundle:PriceListToAccount', $repository]
+        ]);
+
+        $this->registry = $this->getMock('Doctrine\Common\Persistence\ManagerRegistry');
+        $this->registry->method('getManagerForClass')->willReturn($this->manager);
+        /** @var TranslatorInterface $translator */
+        $translator = $this->getMock('Symfony\Component\Translation\TranslatorInterface');
+        $this->listener = new AccountDataGridListener($this->registry, $translator);
+    }
+
+    public function testOnResultAfter()
+    {
+        /** @var DatagridInterface $dataGrid */
+        $dataGrid = $this->getMock('Oro\Bundle\DataGridBundle\Datagrid\DatagridInterface');
+        $event = new OrmResultAfter($dataGrid);
+        $this->listener->onResultAfter($event);
+    }
+}
