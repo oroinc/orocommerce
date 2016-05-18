037a1759264831fb056c1a172699d59ccaf2fa4c
Dmitry Khrysev <dkhrysev@magecore.com>
2016-05-18 19:54:33 +0300

    BB-3182: Refactor FrontendProductDatagridListener and related logic

--- src/OroB2B/Bundle/ProductBundle/Entity/Repository/ProductRepository.php
+++ src/OroB2B/Bundle/ProductBundle/Entity/Repository/ProductRepository.php
@@ -177,8 +177,8 @@ class ProductRepository extends EntityRepository
     public function getProductsWithImage(array $products)
     {
         $qb = $this->createQueryBuilder('p');
-        return $qb->select('partial p.{id, image}, image')
-            ->innerJoin('p.image', 'image', Expr\Join::WITH, $qb->expr()->isNotNull('image.id'))
+        return $qb->select('partial p.{id}, image')
+            ->leftJoin('p.image', 'image', Expr\Join::WITH, $qb->expr()->isNotNull('image.id'))
             ->where($qb->expr()->in('p', ':products'))
             ->setParameter('products', $products)
             ->getQuery()
