0aaa1a077043dc2cebde6d35e0dd28011d0c6ce9
Oleksandr Nechyporenko <onechyporenko@magecore.com>
2016-05-19 18:36:58 +0300

    BB-3136: Remove chosen and dependency  - remove chosen

--- src/OroB2B/Bundle/FrontendBundle/Resources/public/default/js/app/modules/input-widgets.js
+++ src/OroB2B/Bundle/FrontendBundle/Resources/public/default/js/app/modules/input-widgets.js
@@ -2,15 +2,10 @@ define(function(require) {
     'use strict';
 
     var InputWidgetManager = require('oroui/js/input-widget-manager');
-    var ChosenInputWidget = require('orob2bfrontend/default/js/app/views/input-widget/chosen');
     var CheckboxInputWidget = require('orob2bfrontend/default/js/app/views/input-widget/checkbox');
     var RadioInputWidget = require('orob2bfrontend/default/js/app/views/input-widget/radio');
 
     InputWidgetManager.removeWidget('uniform-select');
-    InputWidgetManager.addWidget('chosen', {
-        selector: 'select',
-        Widget: ChosenInputWidget
-    });
 
     InputWidgetManager.addWidget('checkbox', {
         selector: 'input:checkbox',
