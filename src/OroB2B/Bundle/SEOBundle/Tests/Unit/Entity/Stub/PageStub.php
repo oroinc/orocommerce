a678d199a77098ff24648d1ff14f274b43f03d41
Boldizsár Zeyk Zsombor <zboldizsar@pitechplus.com>
2016-05-20 12:58:36 +0300

    BB-3106 Tests for SEO bundle - added unit tests for form extensions

---
+++ src/OroB2B/Bundle/SEOBundle/Tests/Unit/Entity/Stub/PageStub.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace OroB2B\Bundle\SEOBundle\Tests\Unit\Entity\Stub;
+
+use OroB2B\Bundle\CatalogBundle\Entity\Category;
+
+class PageStub extends Category
+{
+    use MetaFieldSetterGetterTrait;
+}
---
+++ src/OroB2B/Bundle/SEOBundle/Tests/Unit/Entity/Stub/ProductStub.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace OroB2B\Bundle\SEOBundle\Tests\Unit\Entity\Stub;
+
+use OroB2B\Bundle\CatalogBundle\Entity\Category;
+
+class ProductStub extends Category
+{
+    use MetaFieldSetterGetterTrait;
+}
