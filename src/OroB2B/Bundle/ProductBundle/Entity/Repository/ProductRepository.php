1a1b404d3491a1598f8bb1cd940ddeaf0deda791
Andrey Yatsenco <yatsenco@gmail.com>
2016-05-26 17:39:37 +0300

    BB-1950: Multiple product images - fixed bug after bad backmerge.

--- src/OroB2B/Bundle/ProductBundle/Entity/Repository/ProductRepository.php
+++ src/OroB2B/Bundle/ProductBundle/Entity/Repository/ProductRepository.php
@@ -177,7 +177,7 @@ class ProductRepository extends EntityRepository
     public function getProductsWithImage(array $products)
     {
         $qb = $this->createQueryBuilder('p');
-        return $qb->select('partial p.{id, image}')
+        return $qb->select('p')
             ->where('p.images IS NOT EMPTY')
             ->andwhere($qb->expr()->in('p', ':products'))
             ->setParameter('products', $products)
