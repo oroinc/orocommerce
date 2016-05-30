d8c6f1e8ad84b93a877b5848fd5b435d85b3a0a2
Alexandru Puscas <apuscas@pitechnologies.ro>
2016-05-30 10:32:33 +0300

    BB-3229: Add new column to grid. - Refactored grid extension due to review. - Removed testing due to review.

--- src/OroB2B/Bundle/AccountBundle/Tests/Functional/Controller/AccountUserControllerTest.php
+++ src/OroB2B/Bundle/AccountBundle/Tests/Functional/Controller/AccountUserControllerTest.php
@@ -118,8 +118,6 @@ class AccountUserControllerTest extends AbstractUserControllerTest
      */
     public function testIndex()
     {
-        $account = $this->getAccountRepository()->findOneBy([]);
-
         $crawler = $this->client->request('GET', $this->getUrl('orob2b_account_account_user_index'));
         $result = $this->client->getResponse();
 
@@ -128,7 +126,6 @@ class AccountUserControllerTest extends AbstractUserControllerTest
         $this->assertContains(self::FIRST_NAME, $result->getContent());
         $this->assertContains(self::LAST_NAME, $result->getContent());
         $this->assertContains(self::EMAIL, $result->getContent());
-        $this->assertContains($account->getName(), $result->getContent());
     }
 
     /**
