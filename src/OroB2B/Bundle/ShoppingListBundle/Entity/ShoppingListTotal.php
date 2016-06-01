23ba6cc75405d181de3411bba97a6aa57c16974e
Roman Yarmolenko <devxypro@gmail.com>
2016-06-01 12:12:01 +0300

    BB-3357: Create ShoppingListTotals entity - create entity

---
+++ src/OroB2B/Bundle/ShoppingListBundle/Entity/ShoppingListTotal.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace OroB2B\Bundle\ShoppingListBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\ConfigField;
+
+/**
+ * @ORM\Table(name="orob2b_shopping_list_total")
+ * @ORM\Entity()
+ *      uniqueConstraints={
+ *          @ORM\UniqueConstraint(name="orob2b_shopping_list_total_unq", columns={
+ *              "shopping_list_id",
+ *              "currency"
+ *          })
+ *      }
+ **/
+class ShoppingListTotal
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var ShoppingList
+     *
+     * @ORM\ManyToOne(targetEntity="OroB2B\Bundle\ShoppingListBundle\Entity\ShoppingList")
+     * @ORM\JoinColumn(name="shopping_list_id", referencedColumnName="id", nullable=true, onDelete="CASCADE")
+     * @ConfigField(
+     *      defaultValues={
+     *          "dataaudit"={
+     *              "auditable"=true
+     *          }
+     *      }
+     * )
+     */
+    protected $shoppingList;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="currency", type="string", length=255)
+     * @ConfigField(
+     *      defaultValues={
+     *          "dataaudit"={
+     *              "auditable"=true
+     *          }
+     *      }
+     * )
+     */
+    protected $currency;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="subtotal", type="float", length=255)
+     * @ConfigField(
+     *      defaultValues={
+     *          "dataaudit"={
+     *              "auditable"=true
+     *          }
+     *      }
+     * )
+     */
+    protected $subtotal;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="is_valid", type="boolean", options={"default"=false})
+     */
+    protected $valid = false;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return ShoppingList
+     */
+    public function getShoppingList()
+    {
+        return $this->shoppingList;
+    }
+
+    /**
+     * @param ShoppingList $shoppingList
+     * @return $this
+     */
+    public function setShoppingList($shoppingList)
+    {
+        $this->shoppingList = $shoppingList;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @param string $currency
+     * @return $this
+     */
+    public function setCurrency($currency)
+    {
+        $this->currency = $currency;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubtotal()
+    {
+        return $this->subtotal;
+    }
+
+    /**
+     * @param string $subtotal
+     * @return $this
+     */
+    public function setSubtotal($subtotal)
+    {
+        $this->subtotal = $subtotal;
+
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isValid()
+    {
+        return $this->valid;
+    }
+
+    /**
+     * @param boolean $valid
+     * @return $this
+     */
+    public function setValid($valid)
+    {
+        $this->valid = $valid;
+
+        return $this;
+    }
+}
