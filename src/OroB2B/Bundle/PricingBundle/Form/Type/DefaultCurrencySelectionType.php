8ae987f94a3d1afaaab91936b74d48eb969d0ed9
Vitaliy Berdylo <vberdylo@magecore.com>
2016-06-02 13:26:10 +0300

    BB-3246: Synchronize Default Currency with Enabled Currencies - CR fixes

--- src/OroB2B/Bundle/PricingBundle/Form/Type/DefaultCurrencySelectionType.php
+++ src/OroB2B/Bundle/PricingBundle/Form/Type/DefaultCurrencySelectionType.php
@@ -2,17 +2,127 @@
 
 namespace OroB2B\Bundle\PricingBundle\Form\Type;
 
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Intl\Intl;
+use Symfony\Component\Translation\TranslatorInterface;
+
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
+use Oro\Bundle\LocaleBundle\Model\LocaleSettings;
+use Oro\Bundle\LocaleBundle\DependencyInjection\Configuration as LocaleConfiguration;
 use Oro\Bundle\CurrencyBundle\Form\Type\CurrencySelectionType;
 
 class DefaultCurrencySelectionType extends CurrencySelectionType
 {
     const NAME = 'orob2b_pricing_default_currency_selection';
 
+    const ENABLED_CURRENCIES_NAME = 'oro_b2b_pricing___enabled_currencies';
+    const DEFAULT_CURRENCY_NAME = 'oro_b2b_pricing___default_currency';
+
+    /**
+     * @var TranslatorInterface
+     */
+    protected $translator;
+
+    /**
+     * @param ConfigManager $configManager
+     * @param LocaleSettings $localeSettings
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(
+        ConfigManager $configManager,
+        LocaleSettings $localeSettings,
+        TranslatorInterface $translator
+    ) {
+        parent::__construct($configManager, $localeSettings);
+        $this->translator = $translator;
+    }
+    
     /**
      * {@inheritDoc}
      */
     public function getName()
     {
         return static::NAME;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder->addEventListener(FormEvents::POST_SUBMIT, [$this, 'postSubmit']);
+    }
+
+    /**
+     * @param FormEvent $event
+     */
+    public function postSubmit(FormEvent $event)
+    {
+        $form = $event->getForm();
+        $rootForm = $form->getRoot();
+
+        if ($this->isSyncApplicable($rootForm)) {
+            $defaultCurrency = $this->getDefaultCurrency($rootForm);
+            $enabledCurrencies = $this->getEnabledCurrencies($rootForm);
+
+            if (!in_array($defaultCurrency, $enabledCurrencies, true)) {
+                $currencyName = Intl::getCurrencyBundle()
+                    ->getCurrencyName($defaultCurrency, $this->localeSettings->getLocale());
+
+                $form->addError(new FormError(
+                    $this->translator->trans(
+                        'orob2b.pricing.validators.is_not_enabled',
+                        ['%currency%' => $currencyName],
+                        'validators'
+                    )
+                ));
+            }
+        }
+    }
+
+    /**
+     * @param FormInterface $form
+     * @return string
+     */
+    protected function getDefaultCurrency(FormInterface $form)
+    {
+        $defaultCurrencyData = $form->get(self::DEFAULT_CURRENCY_NAME)->getData();
+
+        if ($defaultCurrencyData['use_parent_scope_value']) {
+            $defaultCurrency = LocaleConfiguration::DEFAULT_CURRENCY;
+        } else {
+            $defaultCurrency = $defaultCurrencyData['value'];
+        }
+
+        return $defaultCurrency;
+    }
+
+    /**
+     * @param FormInterface $form
+     * @return array
+     */
+    protected function getEnabledCurrencies(FormInterface $form)
+    {
+        $enabledCurrenciesData = $form->get(self::ENABLED_CURRENCIES_NAME)->getData();
+
+        if ($enabledCurrenciesData['use_parent_scope_value']) {
+            $enabledCurrencies = [LocaleConfiguration::DEFAULT_CURRENCY];
+        } else {
+            $enabledCurrencies = $enabledCurrenciesData['value'];
+        }
+
+        return $enabledCurrencies;
+    }
+
+    /**
+     * @param FormInterface $rootForm
+     * @return bool
+     */
+    protected function isSyncApplicable(FormInterface $rootForm)
+    {
+        return $rootForm && $rootForm->getName() == 'pricing' && $rootForm->has(self::ENABLED_CURRENCIES_NAME);
     }
 }
--- src/OroB2B/Bundle/PricingBundle/Tests/Unit/Form/Type/DefaultCurrencySelectionTypeTest.php
+++ src/OroB2B/Bundle/PricingBundle/Tests/Unit/Form/Type/DefaultCurrencySelectionTypeTest.php
@@ -2,55 +2,194 @@
 
 namespace OroB2B\Bundle\PricingBundle\Tests\Unit\Form\Type;
 
-use Symfony\Component\Intl\Locale\Locale;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\PreloadedExtension;
 use Symfony\Component\Form\Test\FormIntegrationTestCase;
+use Symfony\Component\Translation\Translator;
 
 use OroB2B\Bundle\PricingBundle\Form\Type\DefaultCurrencySelectionType;
+use Oro\Bundle\LocaleBundle\Model\LocaleSettings;
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 
 class DefaultCurrencySelectionTypeTest extends FormIntegrationTestCase
 {
     /**
-     * @var DefaultCurrencySelectionType
-     */
-    protected $formType;
-
-    /**
-     * @var \Oro\Bundle\ConfigBundle\Config\ConfigManager|\PHPUnit_Framework_MockObject_MockObject
+     * @var ConfigManager|\PHPUnit_Framework_MockObject_MockObject
      */
     protected $configManager;
 
     /**
-     * @var \Oro\Bundle\LocaleBundle\Model\LocaleSettings|\PHPUnit_Framework_MockObject_MockObject
+     * @var LocaleSettings
      */
     protected $localeSettings;
 
     /**
-     * {@inheritDoc}
+     * @var Translator|\PHPUnit_Framework_MockObject_MockObject
      */
-    protected function setUp()
+    protected $translator;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
     {
-        parent::setUp();
+        $this->translator = $this->getMockBuilder('Oro\Bundle\TranslationBundle\Translation\Translator')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->localeSettings = $this->getMockBuilder('Oro\Bundle\LocaleBundle\Model\LocaleSettings')
+            ->disableOriginalConstructor()
+            ->getMock();
 
         $this->configManager = $this->getMockBuilder('Oro\Bundle\ConfigBundle\Config\ConfigManager')
-            ->setMethods(['get'])
             ->disableOriginalConstructor()
             ->getMock();
 
-        $this->localeSettings = $this
-            ->getMockBuilder('Oro\Bundle\LocaleBundle\Model\LocaleSettings')
-            ->setMethods(['getCurrency', 'getLocale'])
-            ->disableOriginalConstructor()
-            ->getMock();
+        parent::setUp();
+    }
 
-        $this->localeSettings->expects($this->any())
-            ->method('getLocale')
-            ->willReturn(Locale::getDefault());
+    /**
+     * @return array
+     */
+    protected function getExtensions()
+    {
+        return [
+            new PreloadedExtension(
+                [
+                    'orob2b_pricing_default_currency_selection' => new DefaultCurrencySelectionType(
+                        $this->configManager,
+                        $this->localeSettings,
+                        $this->translator
+                    ),
+                ],
+                []
+            ),
+        ];
+    }
 
-        $this->formType = new DefaultCurrencySelectionType($this->configManager, $this->localeSettings);
+    /**
+     * @dataProvider submitFormDataProvider
+     * @param array $enableCurrencies
+     * @param string $defaultCurrency
+     * @param bool $isEnableCurrenciesDefault
+     * @param bool $isDefaultCurrencyDefault
+     * @param bool $isValid
+     */
+    public function testSubmitForm(
+        array $enableCurrencies,
+        $defaultCurrency,
+        $isEnableCurrenciesDefault,
+        $isDefaultCurrencyDefault,
+        $isValid
+    ) {
+        $this->configManager->expects($this->once())
+            ->method('get')
+            ->with('oro_currency.allowed_currencies')
+            ->willReturn(['USD', 'CAD', 'EUR']);
+
+        $form = $this->factory->create('orob2b_pricing_default_currency_selection', null, ['multiple' => true]);
+
+        /** @var FormInterface|\PHPUnit_Framework_MockObject_MockObject $parentForm */
+        $rootForm = $this->getMock('Symfony\Component\Form\FormInterface');
+        $rootForm->expects($this->any())
+            ->method('getRoot')
+            ->willReturn($rootForm);
+        $rootForm->expects($this->once())
+            ->method('getName')
+            ->willReturn('pricing');
+        $rootForm->expects($this->any())
+            ->method('has')
+            ->with(DefaultCurrencySelectionType::ENABLED_CURRENCIES_NAME)
+            ->willReturn(true);
+
+        $defaultCurrenciesForm = $this->getMock('Symfony\Component\Form\FormInterface');
+        $defaultCurrenciesForm->expects($this->any())
+            ->method('getData')
+            ->willReturn([
+                'use_parent_scope_value' => $isDefaultCurrencyDefault,
+                'value' => $defaultCurrency
+            ]);
+
+        $enabledCurrenciesForm = $this->getMock('Symfony\Component\Form\FormInterface');
+        $enabledCurrenciesForm->expects($this->any())
+            ->method('getData')
+            ->willReturn([
+                'use_parent_scope_value' => $isEnableCurrenciesDefault,
+                'value' => $enableCurrencies
+            ]);
+
+        $rootForm->expects($this->any())
+            ->method('get')
+            ->willReturnMap([
+                [DefaultCurrencySelectionType::DEFAULT_CURRENCY_NAME, $defaultCurrenciesForm],
+                [DefaultCurrencySelectionType::ENABLED_CURRENCIES_NAME, $enabledCurrenciesForm]
+            ]);
+
+
+        $form->setParent($rootForm);
+
+        $form->submit(['USD']);
+        $this->assertSame($isValid, $form->isValid());
+    }
+
+    /**
+     * @return array
+     */
+    public function submitFormDataProvider()
+    {
+        return [
+            'valid without default' => [
+                'enableCurrencies' => ['USD', 'CAD', 'EUR'],
+                'defaultCurrency' => 'USD',
+                'isEnableCurrenciesDefault' => false,
+                'isDefaultCurrencyDefault' => false,
+                'isValid' => true
+            ],
+            'invalid without default' => [
+                'enableCurrencies' => ['CAD'],
+                'defaultCurrency' => 'EUR',
+                'isEnableCurrenciesDefault' => false,
+                'isDefaultCurrencyDefault' => false,
+                'isValid' => false
+            ],
+            'valid with defaultCurrency default' => [
+                'enableCurrencies' => ['CAD', 'USD'],
+                'defaultCurrency' => 'USD',
+                'isEnableCurrenciesDefault' => false,
+                'isDefaultCurrencyDefault' => true,
+                'isValid' => true
+            ],
+            'invalid with defaultCurrency default' => [
+                'enableCurrencies' => ['CAD', 'EUR'],
+                'defaultCurrency' => '',
+                'isEnableCurrenciesDefault' => false,
+                'isDefaultCurrencyDefault' => true,
+                'isValid' => false
+            ],
+            'valid with enableCurrencies default' => [
+                'enableCurrencies' => [],
+                'defaultCurrency' => 'USD',
+                'isEnableCurrenciesDefault' => true,
+                'isDefaultCurrencyDefault' => false,
+                'isValid' => true
+            ],
+            'valid with default' => [
+                'enableCurrencies' => [],
+                'defaultCurrency' => 'USD',
+                'isEnableCurrenciesDefault' => true,
+                'isDefaultCurrencyDefault' => true,
+                'isValid' => true
+            ],
+        ];
     }
 
     public function testGetName()
     {
-        $this->assertEquals(DefaultCurrencySelectionType::NAME, $this->formType->getName());
+        $formType = new DefaultCurrencySelectionType(
+            $this->configManager,
+            $this->localeSettings,
+            $this->translator
+        );
+        $this->assertEquals(DefaultCurrencySelectionType::NAME, $formType->getName());
     }
 }
