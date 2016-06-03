ca948c041bb5a546bcceae22acdc76df2cc9f300
Yevhen Petrov <ypetrov@magecore.com>
2016-06-03 14:17:04 +0300

    BB-3365: Invalidate subtotals on ShoppingList update - invalidate subtotals by accounts

--- src/OroB2B/Bundle/ShoppingListBundle/Entity/Repository/ShoppingListTotalRepository.php
+++ src/OroB2B/Bundle/ShoppingListBundle/Entity/Repository/ShoppingListTotalRepository.php
@@ -2,13 +2,13 @@
 
 namespace OroB2B\Bundle\ShoppingListBundle\Entity\Repository;
 
+use Doctrine\DBAL\Types\Type;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Query;
 use Doctrine\ORM\Query\Expr\Join;
 
 use Oro\Bundle\BatchBundle\ORM\Query\BufferedQueryResultIterator;
-
-use OroB2B\Bundle\ShoppingListBundle\Entity\ShoppingList;
+use OroB2B\Bundle\WebsiteBundle\Entity\Website;
 
 class ShoppingListTotalRepository extends EntityRepository
 {
@@ -34,31 +34,63 @@ class ShoppingListTotalRepository extends EntityRepository
                 Join::WITH,
                 $qb->expr()->eq('lineItem.product', 'productPrice.product')
             )
-            ->where($qb->expr()->in('productPrice.priceList', $cplIds))
+            ->where($qb->expr()->in('productPrice.priceList', ':priceLists'))
             ->andWhere('total.valid = :isValid')
-            ->setParameter(':isValid', true);
+            ->setParameter(':isValid', true)
+            ->setParameter('priceLists', $cplIds, Type::SIMPLE_ARRAY);
 
-        $iterator = new BufferedQueryResultIterator($qb->getQuery());
+        $iterator = new BufferedQueryResultIterator($qb);
         $iterator->setHydrationMode(Query::HYDRATE_SCALAR);
+
+        $this->invalidateTotals($iterator);
+    }
+
+    /**
+     * @param array $accountIds
+     * @param Website $website
+     */
+    public function invalidateByAccounts(array $accountIds, Website $website)
+    {
+        if (empty($accountIds)) {
+            return;
+        }
+        $qb = $this->createQueryBuilder('total');
+        $qb->select('DISTINCT total.id')
+            ->join('total.shoppingList', 'shoppingList')
+            ->where($qb->expr()->in('shoppingList.account', ':accounts'))
+            ->andWhere($qb->expr()->eq('shoppingList.website', ':website'))
+            ->andWhere($qb->expr()->eq('total.valid', ':isValid'))
+            ->setParameter('accounts', $accountIds, Type::SIMPLE_ARRAY)
+            ->setParameter('website', $website)
+            ->setParameter('isValid', true);
+        $iterator = new BufferedQueryResultIterator($qb);
+        $iterator->setHydrationMode(Query::HYDRATE_SCALAR);
+        $this->invalidateTotals($iterator);
+    }
+
+    /**
+     * @param BufferedQueryResultIterator $iterator
+     */
+    protected function invalidateTotals(BufferedQueryResultIterator $iterator)
+    {
         $ids = [];
         $qbUpdate = $this->_em->createQueryBuilder()
             ->update($this->_entityName, 'total')
             ->set('total.valid', ':valid')
             ->setParameter('valid', false);
-
         $i = 0;
         foreach ($iterator as $total) {
             $ids[] = $total['id'];
             $i++;
             if ($i % 500 === 0) {
-                $qbUpdate->where($qb->expr()->in('total.id', $ids))
+                $qbUpdate->where($qbUpdate->expr()->in('total.id', $ids))
                     ->getQuery()
                     ->execute();
                 $ids = [];
             }
         }
         if (!empty($ids)) {
-            $qbUpdate->where($qb->expr()->in('total.id', $ids))
+            $qbUpdate->where($qbUpdate->expr()->in('total.id', $ids))
                 ->getQuery()
                 ->execute();
         }
--- src/OroB2B/Bundle/ShoppingListBundle/Tests/Functional/Entity/Repository/ShoppingListTotalRepositoryTest.php
+++ src/OroB2B/Bundle/ShoppingListBundle/Tests/Functional/Entity/Repository/ShoppingListTotalRepositoryTest.php
@@ -2,9 +2,11 @@
 
 namespace OroB2B\Bundle\ShoppingListBundle\Tests\Functional\Entity\Repository;
 
+use Oro\Bundle\EntityBundle\ORM\Registry;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 
 use OroB2B\Bundle\PricingBundle\Tests\Functional\DataFixtures\LoadCombinedProductPrices;
+use OroB2B\Bundle\ShoppingListBundle\Entity\Repository\ShoppingListTotalRepository;
 use OroB2B\Bundle\ShoppingListBundle\Entity\ShoppingList;
 use OroB2B\Bundle\ShoppingListBundle\Entity\ShoppingListTotal;
 use OroB2B\Bundle\ShoppingListBundle\Tests\Functional\DataFixtures\LoadShoppingListLineItems;
@@ -15,10 +17,16 @@ use OroB2B\Bundle\ShoppingListBundle\Tests\Functional\DataFixtures\LoadShoppingL
  */
 class ShoppingListTotalRepositoryTest extends WebTestCase
 {
+    /**
+     * @var Registry
+     */
+    protected $registry;
+
     protected function setUp()
     {
         $this->initClient([], $this->generateBasicAuthHeader());
 
+        $this->registry = $this->getContainer()->get('doctrine');
         $this->loadFixtures(
             [
                 LoadShoppingListLineItems::class,
@@ -27,10 +35,8 @@ class ShoppingListTotalRepositoryTest extends WebTestCase
         );
     }
     
-    public function testInvalidByCpl()
+    public function testInvalidateByCpl()
     {
-        $registry = $this->getContainer()->get('doctrine');
-        
         $invalidTotal = new ShoppingListTotal();
         /** @var ShoppingList $shoppingList */
         $shoppingList = $this->getReference(LoadShoppingLists::SHOPPING_LIST_1);
@@ -39,15 +45,35 @@ class ShoppingListTotalRepositoryTest extends WebTestCase
         $invalidTotal->setCurrency('USD');
         $invalidTotal->setSubtotalValue(1);
         
-        $manager = $registry->getManagerForClass('OroB2BShoppingListBundle:ShoppingListTotal');
+        $manager = $this->registry->getManagerForClass('OroB2BShoppingListBundle:ShoppingListTotal');
         $manager->persist($invalidTotal);
         $manager->flush();
 
         $cpl = $this->getReference('1f');
-        $registry->getRepository('OroB2BShoppingListBundle:ShoppingListTotal')
+        $manager->getRepository('OroB2BShoppingListBundle:ShoppingListTotal')
             ->invalidateByCpl([$cpl->getId()]);
 
         $manager->refresh($invalidTotal);
         $this->assertFalse($invalidTotal->isValid());
+    }
+
+    public function testInvalidateByAccountUsers()
+    {
+        $manager = $this->registry->getManagerForClass('OroB2BShoppingListBundle:ShoppingListTotal');
+        /** @var ShoppingListTotalRepository $repository */
+        $repository = $manager->getRepository('OroB2BShoppingListBundle:ShoppingListTotal');
+
+        $shoppingList = $this->getReference(LoadShoppingLists::SHOPPING_LIST_1);
+        $shoppingListTotal = $repository->findOneBy(['shoppingList' => $shoppingList->getId()]);
+        $shoppingListTotal->setValid(true);
+        $manager->flush();
+
+        $repository->invalidateByAccounts(
+            [$shoppingListTotal->getShoppingList()->getAccount()->getId()],
+            $shoppingListTotal->getShoppingList()->getWebsite()
+        );
+
+        $manager->refresh($shoppingListTotal);
+        $this->assertFalse($shoppingListTotal->isValid());
     }
 }
