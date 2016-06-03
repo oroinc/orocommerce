1403ca8ca0cb8d7ad551018b84facc87670b6755
Yauheni Kasenka <beta.screech@gmail.com>
2016-06-03 19:50:14 +0300

    BAP-10654: Prepare existing code for new Localization Entity - updates after merge with `feature/BAP-10607_manage_locales`

--- src/OroB2B/Bundle/WebsiteBundle/Migrations/Schema/v1_3/OroB2BWebsiteBundle.php
+++ src/OroB2B/Bundle/WebsiteBundle/Migrations/Schema/v1_3/OroB2BWebsiteBundle.php
@@ -44,16 +44,40 @@ class OroB2BWebsiteBundle implements Migration, RenameExtensionAwareInterface, N
      */
     public function up(Schema $schema, QueryBag $queries)
     {
+        $indexName = $this->nameGenerator->generateIndexName('orob2b_locale', ['parent_id'], false, true);
+
         $table = $schema->getTable('orob2b_locale');
         $table->addColumn('formatting_code', 'string', ['length' => 64]);
         $table->dropIndex('uniq_orob2b_locale_code');
+        $table->dropIndex('uniq_orob2b_locale_title');
+        $table->dropIndex($indexName);
+        $table->removeForeignKey('fk_orob2b_locale_parent_id');
 
         $this->renameExtension->renameColumn($schema, $queries, $table, 'title', 'name');
         $this->renameExtension->renameColumn($schema, $queries, $table, 'code', 'language_code');
 
         $schema->dropTable('oro_localization');
         $this->renameExtension->renameTable($schema, $queries, 'orob2b_locale', 'oro_localization');
-        $this->renameExtension->renameTable($schema, $queries, 'orob2b_websites_locales', 'orob2b_websites_localizations');
+        $this->renameExtension->renameTable(
+            $schema,
+            $queries,
+            'orob2b_websites_locales',
+            'orob2b_websites_localizations'
+        );
+
+        $indexName = $this->nameGenerator->generateIndexName('oro_localization', ['parent_id'], false, true);
+
+        $this->renameExtension->addIndex($schema, $queries, 'oro_localization', ['parent_id'], $indexName);
+        $this->renameExtension->addUniqueIndex($schema, $queries, 'oro_localization', ['name']);
+        $this->renameExtension->addForeignKeyConstraint(
+            $schema,
+            $queries,
+            'oro_localization',
+            'oro_localization',
+            ['parent_id'],
+            ['id'],
+            ['onDelete' => 'SET NULL', 'onUpdate' => null]
+        );
 
         $queries->addQuery('UPDATE `oro_localization` SET `formatting_code` = `language_code`');
     }
