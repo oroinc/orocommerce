7314bfa667b1faa4fa11ef31bcee5566583d418c
Valerii Yustyniuk <valeriy.yustunyk@gmail.com>
2016-06-06 20:00:32 +0300

    BB-3139: Fix call directly .select2 in platform and commerce package - fix call select2 in commerce package

--- src/OroB2B/Bundle/FrontendBundle/Resources/public/default/js/app/modules/input-widgets.js
+++ src/OroB2B/Bundle/FrontendBundle/Resources/public/default/js/app/modules/input-widgets.js
@@ -4,8 +4,10 @@ define(function(require) {
     var InputWidgetManager = require('oroui/js/input-widget-manager');
     var CheckboxInputWidget = require('orob2bfrontend/default/js/app/views/input-widget/checkbox');
     var RadioInputWidget = require('orob2bfrontend/default/js/app/views/input-widget/radio');
+    var Select2InputWidget = require('oroui/js/app/views/input-widget/select2');
 
     InputWidgetManager.removeWidget('uniform-select');
+    InputWidgetManager.removeWidget('select2');
 
     InputWidgetManager.addWidget('checkbox', {
         selector: 'input:checkbox',
@@ -15,5 +17,10 @@ define(function(require) {
     InputWidgetManager.addWidget('radio', {
         selector: 'input:radio',
         Widget: RadioInputWidget
+    });
+
+    InputWidgetManager.addWidget('select2', {
+        selector: 'select',
+        Widget: Select2InputWidget
     });
 });
