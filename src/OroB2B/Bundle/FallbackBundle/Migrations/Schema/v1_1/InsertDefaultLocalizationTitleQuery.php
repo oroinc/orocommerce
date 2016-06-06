3c7e0d9bddf5a00b91d12122752ff03868b8b3cd
Nikita Makarov <mesaverde228@gmail.com>
2016-06-06 15:19:25 +0300

    BAP-10654: Prepare existing code for new Localization Entity - Added migration query to updated Localization titles

---
+++ src/OroB2B/Bundle/FallbackBundle/Migrations/Schema/v1_1/InsertDefaultLocalizationTitleQuery.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace OroB2B\Bundle\FallbackBundle\Migrations\Schema\v1_1;
+
+use Doctrine\DBAL\Query\QueryBuilder;
+
+use Psr\Log\LoggerInterface;
+
+use Oro\Bundle\MigrationBundle\Migration\ParametrizedMigrationQuery;
+use Oro\Bundle\SyncBundle\Tests\Unit\Wamp\PDO;
+
+class InsertDefaultLocalizationTitleQuery extends ParametrizedMigrationQuery
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getDescription()
+    {
+        return ['Adds default titles to migrated (untitled) Localizations'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(LoggerInterface $logger)
+    {
+        /** @var QueryBuilder $qb */
+        $qb = $this->connection->createQueryBuilder();
+        $localizations = $qb->select('l.id, l.name, lt.localized_value_id')
+            ->from('oro_localization', 'l')
+            ->leftJoin('l', 'oro_localization_title', 'lt', 'lt.localization_id = l.id')
+            ->andWhere($qb->expr()->isNull('localized_value_id'))
+            ->orderBy('id')
+            ->execute()->fetchAll(PDO::FETCH_OBJ);
+
+        foreach ($localizations as $localization) {
+
+            $sql = sprintf(
+                'INSERT INTO %s (`string`) VALUES (\'%s\')',
+                'oro_fallback_localization_val',
+                $localization->name
+            );
+
+            $this->connection->exec($sql);
+            $this->logQuery($logger, $sql);
+
+            $fallbackLocalizationValueId = $this->connection->lastInsertId('oro_fallback_localization_val.id');
+
+            $sql = sprintf(
+                'INSERT INTO %s (localization_id,localized_value_id) VALUES (%d,%d)',
+                'oro_localization_title',
+                $localization->id,
+                $fallbackLocalizationValueId
+            );
+
+            $this->connection->exec($sql);
+            $this->logQuery($logger, $sql);
+        }
+    }
+}
+ 
