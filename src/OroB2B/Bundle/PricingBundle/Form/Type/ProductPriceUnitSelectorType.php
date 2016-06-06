f5f6e65687d66d2f09428188e9b6aec38ac86710
Volodymyr Denchyk <devit_dvv@ukr.net>
2016-06-07 00:31:52 +0300

    BB-3390: Fixes after Review - modified UPGRADE_BETA2.md - modified ProductPriceUnitSelectorType - modified Functional tests

--- src/OroB2B/Bundle/PricingBundle/Form/Type/ProductPriceUnitSelectorType.php
+++ src/OroB2B/Bundle/PricingBundle/Form/Type/ProductPriceUnitSelectorType.php
@@ -2,17 +2,17 @@
 
 namespace OroB2B\Bundle\PricingBundle\Form\Type;
 
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormInterface;
 
 use OroB2B\Bundle\ProductBundle\Entity\Product;
 use OroB2B\Bundle\ProductBundle\Entity\ProductUnit;
 use OroB2B\Bundle\ProductBundle\Entity\ProductUnitPrecision;
-use OroB2B\Bundle\ProductBundle\Form\Type\ProductUnitSelectionType;
 
 /**
  * Regular extension is used to change default set of units used to render and validate data
  */
-class ProductPriceUnitSelectorType extends ProductUnitSelectionType
+class ProductPriceUnitSelectorType extends AbstractType
 {
     const NAME = 'orob2b_pricing_product_price_unit_selector';
 
@@ -32,19 +32,44 @@ class ProductPriceUnitSelectorType extends ProductUnitSelectionType
             return parent::getProductUnits($form, $product);
         }
         $productForm = $collectionForm->getParent();
-        if (!$productForm || !$productForm->has('unitPrecisions')) {
+        if (!$productForm ||
+            !$productForm->has('primaryUnitPrecision') ||
+            !$productForm->has('additionalUnitPrecisions')) {
             return parent::getProductUnits($form, $product);
         }
 
-        /** @var ProductUnitPrecision[] $unitPrecisions */
-        $unitPrecisions = $productForm->get('unitPrecisions')->getData();
+        /**
+         * @var ProductUnitPrecision $primaryUnitPrecision
+         * @var ProductUnitPrecision[] $additionalUnitPrecisions
+         */
+        $primaryUnitPrecision = $productForm->get('primaryUnitPrecision')->getData();
+        $additionalUnitPrecisions = $productForm->get('additionalUnitPrecisions')->getData();
         $units = [];
-        if ($unitPrecisions) {
-            foreach ($unitPrecisions as $precision) {
+        if ($primaryUnitPrecision) {
+            $units[] = $primaryUnitPrecision->getUnit();
+        }
+        if ($additionalUnitPrecisions) {
+            foreach ($additionalUnitPrecisions as $precision) {
                 $units[] = $precision->getUnit();
             }
         }
 
         return $units;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getParent()
+    {
+        return 'orob2b_product_unit_selection';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return static::NAME;
     }
 }
