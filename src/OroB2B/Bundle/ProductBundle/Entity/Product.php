16491d3018b4e6cbca3089c1cc2c8921757243f8
Roman Yarmolenko <devxypro@gmail.com>
2016-06-16 15:44:07 +0300

    BB-3269: Product Price Attributes -format fix

--- src/OroB2B/Bundle/ProductBundle/Entity/Product.php
+++ src/OroB2B/Bundle/ProductBundle/Entity/Product.php
@@ -716,11 +716,9 @@ class Product extends ExtendProduct implements OrganizationAwareInterface, \Json
      */
     public function getDefaultName()
     {
-        $names = $this->names->filter(
-            function (LocalizedFallbackValue $name) {
-                return null === $name->getLocalization();
-            }
-        );
+        $names = $this->names->filter(function (LocalizedFallbackValue $name) {
+            return null === $name->getLocalization();
+        });
 
         if ($names->count() > 1) {
             throw new \LogicException('There must be only one default name');
@@ -773,11 +771,9 @@ class Product extends ExtendProduct implements OrganizationAwareInterface, \Json
      */
     public function getDefaultDescription()
     {
-        $descriptions = $this->descriptions->filter(
-            function (LocalizedFallbackValue $description) {
-                return null === $description->getLocalization();
-            }
-        );
+        $descriptions = $this->descriptions->filter(function (LocalizedFallbackValue $description) {
+            return null === $description->getLocalization();
+        });
 
         if ($descriptions->count() > 1) {
             throw new \LogicException('There must be only one default description');
@@ -864,11 +860,9 @@ class Product extends ExtendProduct implements OrganizationAwareInterface, \Json
      */
     public function getDefaultShortDescription()
     {
-        $shortDescriptions = $this->shortDescriptions->filter(
-            function (LocalizedFallbackValue $shortDescription) {
-                return null === $shortDescription->getLocalization();
-            }
-        );
+        $shortDescriptions = $this->shortDescriptions->filter(function (LocalizedFallbackValue $shortDescription) {
+            return null === $shortDescription->getLocalization();
+        });
 
         if ($shortDescriptions->count() > 1) {
             throw new \LogicException('There must be only one default short description');
@@ -938,7 +932,6 @@ class Product extends ExtendProduct implements OrganizationAwareInterface, \Json
         if ($primaryUnitPrecision) {
             $this->addUnitPrecision($primaryUnitPrecision);
         }
-
         return $this;
     }
 
@@ -997,11 +990,9 @@ class Product extends ExtendProduct implements OrganizationAwareInterface, \Json
     {
         $primaryPrecision = $this->getPrimaryUnitPrecision();
         $additionalPrecisions = $this->getUnitPrecisions()
-            ->filter(
-                function ($precision) use ($primaryPrecision) {
-                    return $precision != $primaryPrecision;
-                }
-            );
+            ->filter(function ($precision) use ($primaryPrecision) {
+                return $precision != $primaryPrecision;
+            });
 
         $additionalPrecisionsSorted = new ArrayCollection(array_values($additionalPrecisions->toArray()));
 
