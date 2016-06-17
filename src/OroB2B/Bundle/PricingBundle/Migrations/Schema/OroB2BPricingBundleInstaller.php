7a84fc2594b65718356d73848243fd570e39715e
Vitaliy Berdylo <vberdylo@magecore.com>
2016-06-17 12:39:48 +0300

    BB-3527: Test and merge into master - change migration version for price attributes

--- src/OroB2B/Bundle/PricingBundle/Migrations/Schema/OroB2BPricingBundleInstaller.php
+++ src/OroB2B/Bundle/PricingBundle/Migrations/Schema/OroB2BPricingBundleInstaller.php
@@ -33,7 +33,7 @@ class OroB2BPricingBundleInstaller implements Installation, NoteExtensionAwareIn
      */
     public function getMigrationVersion()
     {
-        return 'v1_3';
+        return 'v1_4';
     }
 
     /**
