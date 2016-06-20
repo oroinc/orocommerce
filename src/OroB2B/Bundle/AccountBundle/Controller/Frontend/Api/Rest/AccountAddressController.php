f54b3b3fbdf054cfcc2e3d94142fcea552477519
Boldizsár Zeyk Zsombor <zboldizsar@pitechplus.com>
2016-06-20 11:42:02 +0300

    BB-3539 Account address crud frontend api - added AccountAddressController - updated layouts, address-book list - functional,unit tests

---
+++ src/OroB2B/Bundle/AccountBundle/Controller/Frontend/Api/Rest/AccountAddressController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace OroB2B\Bundle\AccountBundle\Controller\Frontend\Api\Rest;
+
+use FOS\RestBundle\Controller\Annotations\NamePrefix;
+
+use OroB2B\Bundle\AccountBundle\Controller\Api\Rest\AccountAddressController as BaseAccountAddressController;
+
+/**
+ * @NamePrefix("orob2b_api_account_frontend_")
+ */
+class AccountAddressController extends BaseAccountAddressController
+{
+}
