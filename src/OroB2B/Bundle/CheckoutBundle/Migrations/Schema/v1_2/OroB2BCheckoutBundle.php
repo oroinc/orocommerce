740e471c072edde923659062af4b7a92da280b79
Mykhailo Sulyma <msulima@magecore.com>
2016-06-24 17:34:16 +0300

    BAP-10823: Add migrations to remove relations on workflowItem and workflowState  - updated schema migration

--- src/OroB2B/Bundle/CheckoutBundle/Migrations/Schema/v1_2/OroB2BCheckoutBundle.php
+++ src/OroB2B/Bundle/CheckoutBundle/Migrations/Schema/v1_2/OroB2BCheckoutBundle.php
@@ -16,7 +16,6 @@ class OroB2BCheckoutBundle implements Migration
     {
         $table = $schema->getTable('orob2b_checkout');
         $table->dropColumn('workflow_step_id');
-        $table->dropColumn('workflow_step_id');
-        $table->dropIndex('uniq_e56b559d1023c4ee');
+        $table->dropColumn('workflow_item_id');
     }
 }
