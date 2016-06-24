748a49b6c081345a183cd1f6511805857386ab25
Vladimir Seniuk <vseniuk@magecore.com>
2016-06-24 13:06:53 +0300

    BB-3424: Add Show On Map popup - create  action for datagrid in AccountBundle

---
+++ src/OroB2B/Bundle/AccountBundle/Extension/Action/Actions/MapAction.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Oro\Bundle\AccountBundle\Extension\Action\Actions;
+
+use Oro\Bundle\DataGridBundle\Extension\Action\Actions\AbstractAction;
+
+class MapAction extends AbstractAction
+{
+}
