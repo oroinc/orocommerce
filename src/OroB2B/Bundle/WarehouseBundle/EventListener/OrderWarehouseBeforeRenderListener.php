29bfd09ca7285f33d490cf72ee9b406945ff2625
Boldizsár Zeyk Zsombor <zboldizsar@pitechplus.com>
2016-06-29 16:04:52 +0300

    BB-3609 Warehouse fields on order and order line item pages. - modified DynamicFieldsExtension to be able to ignore fields from event

--- src/OroB2B/Bundle/WarehouseBundle/EventListener/OrderWarehouseBeforeRenderListener.php
+++ src/OroB2B/Bundle/WarehouseBundle/EventListener/OrderWarehouseBeforeRenderListener.php
@@ -5,10 +5,26 @@ namespace OroB2B\Bundle\WarehouseBundle\EventListener;
 use Oro\Bundle\EntityExtendBundle\Event\ValueRenderEvent;
 
 use OroB2B\Bundle\OrderBundle\Entity\Order;
+use OroB2B\Bundle\WarehouseBundle\Entity\Helper\WarehouseCounter;
 use OroB2B\Bundle\WarehouseBundle\Entity\Warehouse;
 
 class OrderWarehouseBeforeRenderListener
 {
+    /**
+     * @var WarehouseCounter
+     */
+    protected $warehouseCounter;
+
+    /**
+     * OrderWarehouseBeforeRenderListener constructor.
+     *
+     * @param WarehouseCounter $warehouseCounter
+     */
+    public function __construct(WarehouseCounter $warehouseCounter)
+    {
+        $this->warehouseCounter = $warehouseCounter;
+    }
+
     /**
      * @param ValueRenderEvent $event
      */
@@ -17,6 +33,12 @@ class OrderWarehouseBeforeRenderListener
         if (!$event->getFieldValue() instanceof Warehouse
             || $event->getFieldConfigId()->getClassName() != Order::class
         ) {
+            return;
+        }
+
+        if (!$this->warehouseCounter->areMoreWarehouses()) {
+            $event->setFieldVisibility(false);
+
             return;
         }
 
