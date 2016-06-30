8f3f695fe097ee4dbc5ecd829253748ef4fed59f
Vitaliy Berdylo <vberdylo@magecore.com>
2016-06-30 19:03:19 +0300

    BB-3666: Create PriceListToProduct entity - create entity

---
+++ src/OroB2B/Bundle/PricingBundle/Entity/PriceListToProduct.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace OroB2B\Bundle\PricingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use OroB2B\Bundle\ProductBundle\Entity\Product;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(
+ *      name="orob2b_price_list_to_product",
+ *      uniqueConstraints={
+ *          @ORM\UniqueConstraint(
+ *              name="orob2b_price_list_to_product_uidx",
+ *              columns={"product_id", "price_list_id"}
+ *          )
+ *      }
+ * )
+ */
+class PriceListToProduct
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var PriceList
+     *
+     * @ORM\ManyToOne(targetEntity="OroB2B\Bundle\PricingBundle\Entity\PriceList")
+     * @ORM\JoinColumn(name="price_list_id", referencedColumnName="id", nullable=false, onDelete="CASCADE")
+     **/
+    protected $priceList;
+
+    /**
+     * @var Product
+     *
+     * @ORM\ManyToOne(targetEntity="OroB2B\Bundle\ProductBundle\Entity\Product")
+     * @ORM\JoinColumn(name="product_id", referencedColumnName="id", nullable=false, onDelete="CASCADE")
+     */
+    protected $product;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="isSystem", type="boolean")
+     * */
+    protected $isManual = true;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return PriceList
+     */
+    public function getPriceList()
+    {
+        return $this->priceList;
+    }
+
+    /**
+     * @param PriceList $priceList
+     * @return $this
+     */
+    public function setPriceList($priceList)
+    {
+        $this->priceList = $priceList;
+
+        return $this;
+    }
+
+    /**
+     * @return Product
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+
+    /**
+     * @param Product $product
+     * @return $this
+     */
+    public function setProduct($product)
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isIsManual()
+    {
+        return $this->isManual;
+    }
+
+    /**
+     * @param boolean $isManual
+     * @return $this
+     */
+    public function setIsManual($isManual)
+    {
+        $this->isManual = $isManual;
+
+        return $this;
+    }
+}
