7ca6c14d7d1ddd3aaacd3e37fc7b4dd6b7260a0f
Oleksandr Nechyporenko <onechyporenko@magecore.com>
2016-07-01 12:48:57 +0300

    BB-3424: Add Show On Map popup  - refactor map-action  - move map-action to AddressBundle in platform

--- src/OroB2B/Bundle/AccountBundle/Extension/Action/Actions/MapAction.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace OroB2B\Bundle\AccountBundle\Extension\Action\Actions;
-
-use Oro\Bundle\DataGridBundle\Extension\Action\Actions\AbstractAction;
-
-class MapAction extends AbstractAction
-{
-}
