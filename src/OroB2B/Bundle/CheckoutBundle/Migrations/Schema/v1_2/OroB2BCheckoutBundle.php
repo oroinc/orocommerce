6cc3c840de70396bc467a7dc1b1f765cd38b7e4e
Mykhailo Sulyma <msulima@magecore.com>
2016-07-06 15:51:25 +0300

    BAP-10820: Testing and updates  - migrations fix

--- src/OroB2B/Bundle/CheckoutBundle/Migrations/Schema/v1_2/OroB2BCheckoutBundle.php
+++ src/OroB2B/Bundle/CheckoutBundle/Migrations/Schema/v1_2/OroB2BCheckoutBundle.php
@@ -5,17 +5,27 @@ namespace OroB2B\Bundle\CheckoutBundle\Migrations\Schema\v1_2;
 use Doctrine\DBAL\Schema\Schema;
 
 use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\OrderedMigrationInterface;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+use Oro\Bundle\WorkflowBundle\Migrations\Schema\v1_14\RemoveWorkflowFieldsTrait;
 
-class OroB2BCheckoutBundle implements Migration
+class OroB2BCheckoutBundle implements Migration, OrderedMigrationInterface
 {
+    use RemoveWorkflowFieldsTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return 40;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function up(Schema $schema, QueryBag $queries)
     {
-        $table = $schema->getTable('orob2b_checkout');
-        $table->dropColumn('workflow_step_id');
-        $table->dropColumn('workflow_item_id');
+        $this->removeWorkflowFields($schema->getTable('orob2b_checkout'));
     }
 }
