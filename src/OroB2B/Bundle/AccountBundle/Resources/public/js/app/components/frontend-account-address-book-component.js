4f40e1e26ccfaf89ff91cd45771cfdd5c922144a
Oleksandr Nechyporenko <onechyporenko@magecore.com>
2016-07-12 16:00:04 +0300

    BB-3739: Addresses list remove button doesn't have a confirmation dialog  - fix CS issues

--- src/OroB2B/Bundle/AccountBundle/Resources/public/js/app/components/frontend-account-address-book-component.js
+++ src/OroB2B/Bundle/AccountBundle/Resources/public/js/app/components/frontend-account-address-book-component.js
@@ -8,6 +8,7 @@ define(function(require) {
     var _ = require('underscore');
     var routing = require('routing');
     var AddressBook = require('orob2baccount/js/address-book');
+    var deleteConfirmation = require('orob2bfrontend/js/app/components/delete-confirmation');
 
     AccountAddressBook = BaseComponent.extend({
         /**
@@ -45,8 +46,8 @@ define(function(require) {
                 addressMapOptions: {'phone': 'phone'},
                 useFormDialog: options.useFormDialog,
                 allowToRemovePrimary: true,
-                confirmation: true,
-                confirmationComponent: 'orob2bfrontend/js/app/components/delete-confirmation'
+                confirmRemove: true,
+                confirmRemoveComponent: deleteConfirmation
             });
 
             addressBook.getCollection().reset(JSON.parse(options.currentAddresses));
