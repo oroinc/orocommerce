5d802302ae2fe6122088246dff93e1ac29171d2a
Alexander Pobereznichenko <opobereznichenko@magecore.com>
2016-07-14 18:21:02 +0300

    BB-3624: Add test for converter     - functional test for repositories

--- src/OroB2B/Bundle/PricingBundle/Tests/Functional/Entity/Repository/PriceListToProductRepositoryTest.php
+++ src/OroB2B/Bundle/PricingBundle/Tests/Functional/Entity/Repository/PriceListToProductRepositoryTest.php
@@ -5,8 +5,13 @@ namespace OroB2B\Bundle\PricingBundle\Tests\Functional\Entity\Repository;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 
 use OroB2B\Bundle\PricingBundle\Entity\PriceList;
+use OroB2B\Bundle\PricingBundle\Entity\PriceListToProduct;
 use OroB2B\Bundle\PricingBundle\Entity\Repository\PriceListToProductRepository;
+use OroB2B\Bundle\PricingBundle\Tests\Functional\DataFixtures\LoadPriceLists;
+use OroB2B\Bundle\PricingBundle\Tests\Functional\DataFixtures\LoadPriceListToProductWithoutPrices;
+use OroB2B\Bundle\PricingBundle\Tests\Functional\DataFixtures\LoadProductPrices;
 use OroB2B\Bundle\ProductBundle\Entity\Product;
+use OroB2B\Bundle\ProductBundle\Tests\Functional\DataFixtures\LoadProductData;
 
 /**
  * @dbIsolation
@@ -18,14 +23,17 @@ class PriceListToProductRepositoryTest extends WebTestCase
      */
     protected $repository;
 
+    /**
+     * @inheritdoc
+     */
     protected function setUp()
     {
         $this->initClient([], $this->generateBasicAuthHeader());
 
         $this->loadFixtures(
             [
-                'OroB2B\Bundle\PricingBundle\Tests\Functional\DataFixtures\LoadProductPrices',
-                'OroB2B\Bundle\PricingBundle\Tests\Functional\DataFixtures\LoadPriceListToProductWithoutPrices',
+                LoadProductPrices::class,
+                LoadPriceListToProductWithoutPrices::class,
             ]
         );
 
@@ -37,7 +45,7 @@ class PriceListToProductRepositoryTest extends WebTestCase
     public function testGetProductsWithoutPrices()
     {
         /** @var PriceList $priceList */
-        $priceList = $this->getReference('price_list_2');
+        $priceList = $this->getReference(LoadPriceLists::PRICE_LIST_2);
 
         // Assert that there are 4 assigned products for given price list
         $this->assertCount(4, $this->repository->findBy(['priceList' => $priceList]));
@@ -50,9 +58,56 @@ class PriceListToProductRepositoryTest extends WebTestCase
 
         // Check that 2 products does not have prices
         $expected = [
-            $this->getReference('product.3')->getId(),
-            $this->getReference('product.4')->getId(),
+            $this->getReference(LoadProductData::PRODUCT_3)->getId(),
+            $this->getReference(LoadProductData::PRODUCT_4)->getId(),
         ];
         $this->assertEquals($expected, $actual);
+    }
+
+    public function testDeleteGeneratedRelations()
+    {
+        /** @var PriceList $priceList */
+        $priceList = $this->getReference(LoadPriceLists::PRICE_LIST_1);
+
+        // delete relation for price lists
+        $this->repository->createQueryBuilder('pltp')
+            ->delete()
+            ->where('pltp.priceList = :pl')
+            ->setParameter('pl', $priceList)
+            ->getQuery()
+            ->execute();
+
+        // CREATE TEST DATA
+        $em = $this->getContainer()->get('doctrine')->getManagerForClass('OroB2BPricingBundle:PriceListToProduct');
+        // manual relation
+        $manualRelation = new PriceListToProduct();
+        /** @var Product $product1 */
+        $product1 = $this->getReference(LoadProductData::PRODUCT_1);
+        $manualRelation->setPriceList($priceList)
+            ->setProduct($product1)
+            ->setManual(true);
+        $em->persist($manualRelation);
+        //not manual relations
+        $nmRelation1 = new PriceListToProduct();
+        /** @var Product $product2 */
+        $product2 = $this->getReference(LoadProductData::PRODUCT_2);
+        $nmRelation1->setPriceList($priceList)
+            ->setProduct($product2)
+            ->setManual(false);
+        $em->persist($nmRelation1);
+        $nmRelation2 = new PriceListToProduct();
+        /** @var Product $product3 */
+        $product3 = $this->getReference(LoadProductData::PRODUCT_3);
+        $nmRelation2->setPriceList($priceList)
+            ->setProduct($product3)
+            ->setManual(false);
+        $em->persist($nmRelation2);
+        $em->flush();
+
+
+        $this->repository->deleteGeneratedRelations($priceList);
+        $actual = $this->repository->findBy(['priceList' => $priceList]);
+        $this->assertCount(1, $actual);
+        $this->assertSame($manualRelation, $actual[0]);
     }
 }
--- src/OroB2B/Bundle/PricingBundle/Tests/Functional/Entity/Repository/ProductPriceRepositoryTest.php
+++ src/OroB2B/Bundle/PricingBundle/Tests/Functional/Entity/Repository/ProductPriceRepositoryTest.php
@@ -4,7 +4,6 @@ namespace OroB2B\Bundle\PricingBundle\Tests\Functional\Entity\Repository;
 
 use Doctrine\Common\Collections\ArrayCollection;
 
-use Doctrine\ORM\EntityManager;
 use Oro\Bundle\CurrencyBundle\Entity\Price;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 
@@ -16,6 +15,7 @@ use OroB2B\Bundle\PricingBundle\Entity\Repository\ProductPriceRepository;
 use OroB2B\Bundle\ProductBundle\Entity\Product;
 use OroB2B\Bundle\ProductBundle\Entity\ProductUnit;
 use OroB2B\Bundle\ProductBundle\Tests\Functional\DataFixtures\LoadProductData;
+use OroB2B\Bundle\PricingBundle\Tests\Functional\DataFixtures\LoadProductPrices;
 
 /**
  * @dbIsolation
@@ -29,14 +29,16 @@ class ProductPriceRepositoryTest extends WebTestCase
      */
     protected $repository;
 
+    /**
+     * @inheritdoc
+     */
     protected function setUp()
     {
         $this->initClient();
 
         $this->loadFixtures(
             [
-                'OroB2B\Bundle\PricingBundle\Tests\Functional\DataFixtures\LoadProductPrices',
-                'OroB2B\Bundle\PricingBundle\Tests\Functional\DataFixtures\LoadPriceLists'
+                LoadProductPrices::class,
             ]
         );
 
@@ -597,50 +599,82 @@ class ProductPriceRepositoryTest extends WebTestCase
     public function testDeleteGeneratedPrices()
     {
         $registry = $this->getContainer()->get('doctrine');
-        /** @var $manager EntityManager */
         $manager = $registry->getManagerForClass(PriceRule::class);
 
         /** @var PriceList $priceList */
         $priceList = $this->getReference('price_list_1');
-
-        /** @var ProductPrice $productPrice */
-        $productPrice = new ProductPrice();
-        $productPrice->setPriceList($priceList);
-        $productPrice->setPrice(Price::create(1, 'USD'));
-        $productPrice->setQuantity(1);
-        /** @var ProductUnit $unit */
-        $unit = $this->getReference('product_unit.box');
-        $productPrice->setUnit($unit);
-        /** @var Product $product */
-        $product = $this->getReference(LoadProductData::PRODUCT_1);
-        $productPrice->setProduct($product);
-
-        $manager->persist($productPrice);
-        $manager->flush($productPrice);
-
-        $rule = new PriceRule();
-        $rule->setRule('1');
-        $rule->setPriority(1);
-        $rule->setQuantity(1);
-        $rule->setPriceList($priceList);
-        $rule->setCurrency('USD');
-
-        $manager->persist($rule);
-        $manager->flush($rule);
-
-        $productPrice->setPriceRule($rule);
-        $manager->flush($productPrice);
-
         /** @var ProductPriceRepository $repository */
         $repository = $manager->getRepository('OroB2BPricingBundle:ProductPrice');
+        $manualPrices = $repository->findBy(['priceList'=>$priceList, 'priceRule' => null]);
 
-        $prices = $repository->findBy(['priceRule' => $rule]);
-        $this->assertNotEmpty($prices);
+        $rule = new PriceRule();
+        $rule->setRule('rule')
+            ->setPriority(1)
+            ->setQuantity(1)
+            ->setPriceList($priceList)
+            ->setCurrency('USD');
+
+        /** @var ProductUnit $unit */
+        $unit = $this->getReference('product_unit.box');
+        /** @var Product $product */
+        $product = $this->getReference(LoadProductData::PRODUCT_1);
+
+        $productPrice = new ProductPrice();
+        $productPrice->setPriceList($priceList)
+            ->setPrice(Price::create(1, 'USD'))
+            ->setQuantity(1)
+            ->setPriceRule($rule)
+            ->setUnit($unit)
+            ->setProduct($product);
+
+        $manager->persist($rule);
+        $manager->persist($productPrice);
+        $manager->flush();
 
         $repository->deleteGeneratedPrices($priceList, $productPrice->getProduct());
 
-        $prices = $repository->findBy(['priceRule' => $rule]);
-        $this->assertEmpty($prices);
+        $actual = $repository->findBy(['priceList'=>$priceList]);
+        $this->assertEquals($manualPrices, $actual);
+    }
+
+    public function testDeleteGeneratedPricesByRule()
+    {
+        $registry = $this->getContainer()->get('doctrine');
+        $manager = $registry->getManagerForClass(PriceRule::class);
+
+        /** @var PriceList $priceList */
+        $priceList = $this->getReference('price_list_1');
+        /** @var ProductPriceRepository $repository */
+        $repository = $manager->getRepository('OroB2BPricingBundle:ProductPrice');
+        $pricesCount = $this->getPricesCount();
+
+        $rule = new PriceRule();
+        $rule->setRule('rule')
+            ->setPriority(1)
+            ->setQuantity(1)
+            ->setPriceList($priceList)
+            ->setCurrency('USD');
+
+        /** @var ProductUnit $unit */
+        $unit = $this->getReference('product_unit.box');
+        /** @var Product $product */
+        $product = $this->getReference(LoadProductData::PRODUCT_1);
+
+        $productPrice = new ProductPrice();
+        $productPrice->setPriceList($priceList)
+            ->setPrice(Price::create(1, 'USD'))
+            ->setQuantity(1)
+            ->setPriceRule($rule)
+            ->setUnit($unit)
+            ->setProduct($product);
+
+        $manager->persist($rule);
+        $manager->persist($productPrice);
+        $manager->flush();
+
+        $repository->deleteGeneratedPricesByRule($rule);
+        $this->assertEmpty($repository->findBy(['priceRule'=>$rule]));
+        $this->assertSame($pricesCount, $this->getPricesCount());
     }
 
     /**
@@ -655,5 +689,19 @@ class ProductPriceRepositoryTest extends WebTestCase
         }
 
         return $priceIds;
+    }
+
+    /**
+     * @return int
+     */
+    protected function getPricesCount()
+    {
+        $repository = $this->getContainer()
+            ->get('doctrine')
+            ->getRepository('OroB2BPricingBundle:ProductPrice');
+        return $repository->createQueryBuilder('pp')
+            ->select('count(pp.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
     }
 }
