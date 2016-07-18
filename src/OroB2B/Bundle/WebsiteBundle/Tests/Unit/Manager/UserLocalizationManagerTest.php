f65f34f8a7022b273a3ab349f4caa66a99e5582c
Ivan Shirko <Ivan.Shyrko@effective-soft.com>
2016-07-18 18:57:20 +0300

    BB-3747: Localization management (On Localization Changed) - add LocalizationVoter - add unit tests

--- src/OroB2B/Bundle/WebsiteBundle/Tests/Unit/Manager/UserLocalizationManagerTest.php
+++ src/OroB2B/Bundle/WebsiteBundle/Tests/Unit/Manager/UserLocalizationManagerTest.php
@@ -2,30 +2,65 @@
 
 namespace OroB2B\Bundle\WebsiteBundle\Tests\Unit\Manager;
 
-use Oro\Bundle\ConfigBundle\Config\ConfigManager;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Bundle\LocaleBundle\DependencyInjection\Configuration;
 use Oro\Bundle\LocaleBundle\Entity\Localization;
 use Oro\Bundle\LocaleBundle\Provider\LocalizationProvider;
+use Oro\Bundle\UserBundle\Entity\BaseUserManager;
 
+use Oro\Component\Testing\Unit\EntityTrait;
+
+use OroB2B\Bundle\AccountBundle\Entity\AccountUserSettings;
+use OroB2B\Bundle\WebsiteBundle\Entity\Website;
 use OroB2B\Bundle\WebsiteBundle\Manager\UserLocalizationManager;
+use OroB2B\Bundle\WebsiteBundle\Manager\WebsiteManager;
 
 class UserLocalizationManagerTest extends \PHPUnit_Framework_TestCase
 {
+    use EntityTrait;
+
+    /** @var Session|\PHPUnit_Framework_MockObject_MockObject */
+    private $session;
+
+    /** @var TokenStorageInterface|\PHPUnit_Framework_MockObject_MockObject */
+    private $tokenStorage;
+
+    /** @var WebsiteManager|\PHPUnit_Framework_MockObject_MockObject */
+    private $websiteManager;
+
+    /** @var BaseUserManager|\PHPUnit_Framework_MockObject_MockObject */
+    private $userManager;
+
     /** @var ConfigManager|\PHPUnit_Framework_MockObject_MockObject */
-    protected $configManager;
+    private $configManager;
 
     /** @var LocalizationProvider|\PHPUnit_Framework_MockObject_MockObject */
-    protected $localizationProvider;
+    private $localizationProvider;
 
     /** @var UserLocalizationManager */
-    protected $manager;
+    private $localizationManager;
 
     /**
      * {@inheritdoc}
      */
     public function setUp()
     {
+        $this->session = $this->getMockBuilder(Session::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->tokenStorage = $this->getMock(TokenStorageInterface::class);
+        $this->configManager = $this->getMockBuilder(ConfigManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->websiteManager = $this->getMockBuilder(WebsiteManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->userManager = $this->getMockBuilder(BaseUserManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
         $this->configManager = $this->getMockBuilder(ConfigManager::class)
             ->disableOriginalConstructor()
             ->getMock();
@@ -34,7 +69,14 @@ class UserLocalizationManagerTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
-        $this->manager = new UserLocalizationManager($this->configManager, $this->localizationProvider);
+        $this->localizationManager = new UserLocalizationManager(
+            $this->session,
+            $this->tokenStorage,
+            $this->configManager,
+            $this->websiteManager,
+            $this->userManager,
+            $this->localizationProvider
+        );
     }
 
     public function testGetEnabledLocalizations()
@@ -51,21 +93,72 @@ class UserLocalizationManagerTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(
             [(new Localization())->setLanguageCode('en')],
-            $this->manager->getEnabledLocalizations()
+            $this->localizationManager->getEnabledLocalizations()
+        );
+    }
+
+    public function testGetDefaultLocalization()
+    {
+        $localization = $this->getEntity(Localization::class, ['id' => 1]);
+
+        $this->configManager->expects($this->once())
+            ->method('get')
+            ->with(Configuration::getConfigKeyByName(Configuration::DEFAULT_LOCALIZATION))
+            ->willReturn($localization->getId());
+
+        $this->localizationProvider->expects($this->once())
+            ->method('getLocalization')
+            ->with($localization->getId())
+            ->willReturn($localization);
+
+        $this->assertEquals(
+            $localization,
+            $this->localizationManager->getDefaultLocalization()
         );
     }
 
     public function testGetCurrentLocalization()
     {
-        $localization = (new Localization())->setLanguageCode('en');
+        /** @var Localization $localization1 */
+        $localization1 = $this->getEntity(Localization::class, ['id' => 1]);
+        $localization2 = $this->getEntity(Localization::class, ['id' => 2]);
+
+        /** @var Website $website **/
+        $website = $this->getEntity(Website::class, ['id' => 1]);
+
+        $userWebsiteSettings = new AccountUserSettings($website);
+        $userWebsiteSettings->setLocalization($localization1);
+
+        $user = $this->getMockBuilder('OroB2B\Bundle\AccountBundle\Entity\AccountUser')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->websiteManager->expects($this->never())
+            ->method('getCurrentWebsite');
+        $token = $this->getMock('Symfony\Component\Security\Core\Authentication\Token\TokenInterface');
+        $token->expects($this->once())
+            ->method('getUser')
+            ->willReturn($user);
+        $user->expects($this->once())
+            ->method('getWebsiteSettings')
+            ->with($website)
+            ->willReturn($userWebsiteSettings);
+        $this->configManager->expects($this->once())
+            ->method('get')
+            ->with(Configuration::getConfigKeyByName(Configuration::ENABLED_LOCALIZATIONS))
+            ->willReturn([$localization1->getId(), $localization2->getId()]);
+        $this->tokenStorage->expects($this->once())
+            ->method('getToken')
+            ->willReturn($token);
 
         $this->localizationProvider->expects($this->once())
             ->method('getLocalizations')
-            ->willReturn([$localization]);
+            ->with([$localization1->getId(), $localization2->getId()])
+            ->willReturn([$localization1, $localization2]);
 
-        $this->assertSame(
-            $localization,
-            $this->manager->getCurrentLocalization()
+        $this->assertEquals(
+            $userWebsiteSettings->getLocalization(),
+            $this->localizationManager->getCurrentLocalization($website)
         );
     }
 }
