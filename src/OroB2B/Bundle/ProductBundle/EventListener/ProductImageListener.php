aba63448909f83b15f8fb3d548ad6f3ca37c63ae
Arsen Aliev <aliev.ars@gmail.com>
2016-07-24 17:39:31 +0300

    BB-1953 Resize Product Images  - added postUpdate & postFlush  - collect changes in postPersist & postUpdate  - send resize event in postFlush

--- src/OroB2B/Bundle/ProductBundle/EventListener/ProductImageListener.php
+++ src/OroB2B/Bundle/ProductBundle/EventListener/ProductImageListener.php
@@ -3,10 +3,14 @@
 namespace OroB2B\Bundle\ProductBundle\EventListener;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PostFlushEventArgs;
 
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
+use Oro\Bundle\AttachmentBundle\Entity\File;
+
 use OroB2B\Bundle\ProductBundle\Entity\ProductImage;
+use OroB2B\Bundle\ProductBundle\Entity\ProductImageType;
 use OroB2B\Bundle\ProductBundle\Event\ProductImageResizeEvent;
 
 class ProductImageListener
@@ -15,6 +19,11 @@ class ProductImageListener
      * @var EventDispatcherInterface
      */
     protected $eventDispatcher;
+
+    /**
+     * @var array
+     */
+    protected $productImagesForResize = [];
 
     /**
      * @param EventDispatcherInterface $eventDispatcher
@@ -29,15 +38,62 @@ class ProductImageListener
      */
     public function postPersist(LifecycleEventArgs $args)
     {
-        /** @var ProductImage $productImage */
-        $productImage = $args->getEntity();
-        if (!$productImage instanceof ProductImage) {
-            return;
-        }
+        /** @var ProductImageType $entity */
+        $productImageType = $args->getEntity();
 
+        if ($productImageType instanceof ProductImageType) {
+            $productImage = $productImageType->getProductImage();
+            $this->addProductImageForResize($productImage);
+        }
+    }
+
+    /**
+     * @param LifecycleEventArgs $args
+     */
+    public function postUpdate(LifecycleEventArgs $args)
+    {
+        $file = $args->getEntity();
+        if ($file instanceof File) {
+            /** @var ProductImage $productImage */
+            $productImage = $args->getEntityManager()->getRepository(ProductImage::class)->findOneByImage($file);
+            if ($productImage && $productImage->getTypes()) {
+                $this->addProductImageForResize($productImage, $forceOption = true);
+            }
+        }
+    }
+
+    /**
+     * @param PostFlushEventArgs $args
+     */
+    public function postFlush(PostFlushEventArgs $args)
+    {
+        foreach ($this->productImagesForResize as $key => $data) {
+            unset($this->productImagesForResize[$key]);
+            $this->sendProductImageResizeEvent($data['productImage'], $data['forceOption']);
+        }
+    }
+
+    /**
+     * @param ProductImage $productImage
+     * @param bool $forceOption
+     */
+    protected function sendProductImageResizeEvent(ProductImage $productImage, $forceOption)
+    {
         $this->eventDispatcher->dispatch(
             ProductImageResizeEvent::NAME,
-            new ProductImageResizeEvent($productImage)
+            new ProductImageResizeEvent($productImage, $forceOption)
         );
+    }
+
+    /**
+     * @param ProductImage $productImage
+     * @param bool $forceOption
+     */
+    protected function addProductImageForResize(ProductImage $productImage, $forceOption = false)
+    {
+        $this->productImagesForResize[$productImage->getId()] = [
+            'productImage' => $productImage,
+            'forceOption' => $forceOption
+        ];
     }
 }
