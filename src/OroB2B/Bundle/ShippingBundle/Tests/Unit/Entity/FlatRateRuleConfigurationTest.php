9a3017b43ce86f56123dd93e20c158e6c46b1674
Oleg Popadko <oleg@popadko.com>
2016-07-29 18:49:35 +0300

    BB-3850: Create edit pages  - fixing tests

--- src/OroB2B/Bundle/ShippingBundle/Tests/Unit/Entity/FlatRateRuleConfigurationTest.php
+++ src/OroB2B/Bundle/ShippingBundle/Tests/Unit/Entity/FlatRateRuleConfigurationTest.php
@@ -92,6 +92,6 @@ class FlatRateRuleConfigurationTest extends \PHPUnit_Framework_TestCase
         $entity->setCurrency('USD');
         $entity->setMethod('UPS');
         $entity->setType('TEST');
-        $this->assertEquals('UPS, TEST, 42 USD', (string)$entity);
+        $this->assertEquals('UPS, 42 USD', (string)$entity);
     }
 }
