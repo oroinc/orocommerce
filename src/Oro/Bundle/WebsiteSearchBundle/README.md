a5f46322c54daddd2d39fb7b1344963bdfe7f554
Filip <filip.gorny@gmail.com>
2016-08-04 12:57:18 +0200

    BB-4032: Product Search UI

---
+++ src/Oro/Bundle/WebsiteSearchBundle/README.md
@@ -0,0 +1,3 @@
+Oro\Bundle\WebsiteSearchBundle
+===============================================
+
---
+++ src/Oro/Bundle/WebsiteSearchBundle/WebsiteSearchBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Oro\Bundle\WebsiteSearchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WebsiteSearchBundle extends Bundle
+{
+}
