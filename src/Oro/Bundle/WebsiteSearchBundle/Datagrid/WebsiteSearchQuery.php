42afbf057fff7fda6706b6a548ee3ee57e30b9f4
Thomas Parys <thomas.parys@wwsh.io>
2016-08-04 15:00:54 +0200

    BB-4053: Create WebsiteSearchQuery - moved Query class to appropriate bundle

---
+++ src/Oro/Bundle/WebsiteSearchBundle/Datagrid/WebsiteSearchQuery.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Oro\Bundle\WebsiteSearchBundle\Datagrid;
+
+use Oro\Bundle\SearchBundle\Engine\EngineV2Interface;
+use Oro\Bundle\SearchBundle\Extension\AbstractSearchQuery;
+use Oro\Bundle\SearchBundle\Query\Query;
+
+class WebsiteSearchQuery extends AbstractSearchQuery
+{
+    /**
+     * @var EngineV2Interface
+     */
+    protected $engine;
+
+    /**
+     * @param EngineV2Interface $engine
+     * @param Query             $query
+     */
+    public function __construct(EngineV2Interface $engine, Query $query)
+    {
+        $this->engine = $engine;
+        $this->query  = $query;
+    }
+
+    /**
+     * No longer needed since we are calling the search engine
+     * directly in execute().
+     *
+     * @deprecated Should be removed from the entire interface
+     *
+     * @return bool
+     */
+    public function query()
+    {
+        throw new \BadMethodCallException('This method should not be called. Use execute() instead');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute()
+    {
+        return $this->engine->search($this->query);
+    }
+}
