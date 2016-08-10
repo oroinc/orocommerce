4edc11293bfc19d72cbde995667da6794f5617ad
Dmitry Khrysev <dkhrysev@magecore.com>
2016-08-10 18:31:28 +0300

    BB-4119: Duplicate PriceList - CR fix

--- src/OroB2B/Bundle/PricingBundle/Tests/Unit/Duplicator/PriceListToProductDuplicatorTest.php
+++ src/OroB2B/Bundle/PricingBundle/Tests/Unit/Duplicator/PriceListToProductDuplicatorTest.php
@@ -81,5 +81,4 @@ class PriceListToProductDuplicatorTest extends \PHPUnit_Framework_TestCase
 
         $this->priceListToProductDuplicator->duplicate($sourcePriceList, $targetPriceList);
     }
-
 }
