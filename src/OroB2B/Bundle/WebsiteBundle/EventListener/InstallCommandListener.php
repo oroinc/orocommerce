380d0ec3141161747ee21dfaa8cf32489809f981
Alexander Pobereznichenko <apobereznichenko@gmail.com>
2016-08-11 22:37:36 +0300

    BB-4014: Add request listener to get website     - CR fixes

--- src/OroB2B/Bundle/WebsiteBundle/EventListener/InstallCommandListener.php
+++ src/OroB2B/Bundle/WebsiteBundle/EventListener/InstallCommandListener.php
@@ -3,6 +3,7 @@
 namespace OroB2B\Bundle\WebsiteBundle\EventListener;
 
 use Oro\Bundle\ConfigBundle\Config\ConfigManager;
+use OroB2B\Bundle\WebsiteBundle\Entity\Repository\WebsiteRepository;
 use OroB2B\Bundle\WebsiteBundle\Entity\Website;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Component\Console\Event\ConsoleTerminateEvent;
@@ -35,12 +36,13 @@ class InstallCommandListener
     public function onTerminate(ConsoleTerminateEvent $event)
     {
         if ('oro:install' === $event->getCommand()->getName()) {
+            /** @var WebsiteRepository $repo */
             $repo = $this->registry->getRepository(Website::class);
             $website = $repo->getDefaultWebsite();
             $url = $this->configManager->get('oro_ui.application_url');
-            $this->configManager->set('oro_b2b_website.url', $url, $website->getId());
-            $this->configManager->set('oro_b2b_website.secure_url', $url, $website->getId());
-            $this->configManager->flush();
+            $this->configManager->set('oro_b2b_website.url', $url, $website);
+            $this->configManager->set('oro_b2b_website.secure_url', $url, $website);
+            $this->configManager->flush($website);
         }
     }
 }
