6ea2b6eb84dc36b9bf638aabbf72334eb5a4cf88
Vladimir.Kuzmin <Vladimir.Kuzmin@effective-soft.com>
2016-08-15 13:14:24 +0300

    BB-3818: Empty popup after success products import  - renamed orob2bproduct/js/app/widget/quick-add-import-widget to oro/quick-add-import-widget  - overrided _onContentLoad function in quick-add-import-widget and executed redirect here.  - used quick-add-import-widget for for import button instead of dialog widget.

--- src/OroB2B/Bundle/ProductBundle/Resources/public/js/app/components/quick-add-copy-paste-form-component.js
+++ src/OroB2B/Bundle/ProductBundle/Resources/public/js/app/components/quick-add-copy-paste-form-component.js
@@ -3,8 +3,9 @@ define(function(require) {
 
     var QuickAddCopyPasteFormComponent;
     var _ = require('underscore');
+    var __ = require('orotranslation/js/translator');
     var $ = require('jquery');
-    var QuickAddImportWidget = require('orob2bproduct/js/app/widget/quick-add-import-widget');
+    var QuickAddImportWidget = require('oro/quick-add-import-widget');
     var BaseComponent = require('oroui/js/app/components/base/component');
 
     QuickAddCopyPasteFormComponent = BaseComponent.extend({
@@ -31,8 +32,19 @@ define(function(require) {
                     return false;
                 }
 
-                var widget = new QuickAddImportWidget({});
+                var widget = new QuickAddImportWidget({
+                    'dialogOptions': {
+                        'title': __('orob2b.product.frontend.quick_add.import_validation.title'),
+                        'modal': true,
+                        'resizable': false,
+                        'width': 820,
+                        'autoResize': true,
+                        'dialogClass': 'ui-dialog-no-scroll'
+                    }
+                });
 
+                widget.stateEnabled = false;
+                widget.incrementalPosition = false;
                 widget.firstRun = false;
                 widget.loadContent(form.serialize(), form.attr('method'), form.attr('action'));
             });
