71a72582e62a917916e4cc91e3c0d7b15558dcf1
Yevhen Shyshkin <yshyshkin@magecore.com>
2016-08-16 13:14:25 +0200

    BB-3678: Replace OroB2B with Oro namespace in OroCommerce application  - fixed installer in WarehouseBundle

--- src/OroB2B/Bundle/WarehouseBundle/Migrations/Schema/OroB2BWarehouseBundleInstaller.php
+++ src/OroB2B/Bundle/WarehouseBundle/Migrations/Schema/OroB2BWarehouseBundleInstaller.php
@@ -4,18 +4,27 @@ namespace OroB2B\Bundle\WarehouseBundle\Migrations\Schema;
 
 use Doctrine\DBAL\Schema\Schema;
 
+use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
 use Oro\Bundle\MigrationBundle\Migration\Installation;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 use Oro\Bundle\NoteBundle\Migration\Extension\NoteExtension;
 use Oro\Bundle\NoteBundle\Migration\Extension\NoteExtensionAwareInterface;
 
-class OroB2BWarehouseBundleInstaller implements Installation, NoteExtensionAwareInterface
+class OroB2BWarehouseBundleInstaller implements Installation, NoteExtensionAwareInterface, ExtendExtensionAwareInterface
 {
     const WAREHOUSE_TABLE_NAME = 'orob2b_warehouse';
     const WAREHOUSE_INVENTORY_LEVEL_TABLE_NAME = 'orob2b_warehouse_inventory_lev';
 
+    const ORDER_TABLE_NAME = 'orob2b_order';
+    const ORDER_LINE_ITEM_TABLE_NAME = 'orob2b_order_line_item';
+
     /** @var  NoteExtension */
     protected $noteExtension;
+
+    /** @var ExtendExtension */
+    protected $extendExtension;
 
     /**
      * {@inheritdoc}
@@ -23,6 +32,14 @@ class OroB2BWarehouseBundleInstaller implements Installation, NoteExtensionAware
     public function setNoteExtension(NoteExtension $noteExtension)
     {
         $this->noteExtension = $noteExtension;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setExtendExtension(ExtendExtension $extendExtension)
+    {
+        $this->extendExtension = $extendExtension;
     }
 
     /**
@@ -37,6 +54,9 @@ class OroB2BWarehouseBundleInstaller implements Installation, NoteExtensionAware
         /** Foreign keys generation **/
         $this->addOroB2BWarehouseForeignKeys($schema);
         $this->addOroB2BWarehouseInventoryLevelForeignKeys($schema);
+
+        /** Extended fields **/
+        $this->addWarehouseRelations($schema);
     }
 
     /**
@@ -44,7 +64,7 @@ class OroB2BWarehouseBundleInstaller implements Installation, NoteExtensionAware
      *
      * @param Schema $schema
      */
-    public function createOroB2BWarehouseTable(Schema $schema)
+    protected function createOroB2BWarehouseTable(Schema $schema)
     {
         $table = $schema->createTable(self::WAREHOUSE_TABLE_NAME);
         $table->addColumn('id', 'integer', ['autoincrement' => true]);
@@ -65,7 +85,7 @@ class OroB2BWarehouseBundleInstaller implements Installation, NoteExtensionAware
      *
      * @param Schema $schema
      */
-    public function createOroB2BWarehouseInventoryLevelTable(Schema $schema)
+    protected function createOroB2BWarehouseInventoryLevelTable(Schema $schema)
     {
         $table = $schema->createTable(self::WAREHOUSE_INVENTORY_LEVEL_TABLE_NAME);
         $table->addColumn('id', 'integer', ['autoincrement' => true]);
@@ -133,6 +153,44 @@ class OroB2BWarehouseBundleInstaller implements Installation, NoteExtensionAware
             ['product_unit_precision_id'],
             ['id'],
             ['onDelete' => 'CASCADE', 'onUpdate' => null]
+        );
+    }
+
+    /**
+     * Add order related extended fields
+     *
+     * @param Schema $schema
+     */
+    protected function addWarehouseRelations(Schema $schema)
+    {
+        if (!$schema->hasTable(self::ORDER_TABLE_NAME) || !$schema->hasTable(self::ORDER_LINE_ITEM_TABLE_NAME)) {
+            return;
+        }
+
+        $warehouseTable = $schema->getTable(self::WAREHOUSE_TABLE_NAME);
+        $orderTable = $schema->getTable(self::ORDER_TABLE_NAME);
+        $orderLineItemTable = $schema->getTable(self::ORDER_LINE_ITEM_TABLE_NAME);
+
+        $this->extendExtension->addManyToOneRelation(
+            $schema,
+            $orderTable,
+            'warehouse',
+            $warehouseTable,
+            'id',
+            [
+                'extend' => ['owner' => ExtendScope::OWNER_CUSTOM, 'without_default' => true]
+            ]
+        );
+
+        $this->extendExtension->addManyToOneRelation(
+            $schema,
+            $orderLineItemTable,
+            'warehouse',
+            $warehouseTable,
+            'id',
+            [
+                'extend' => ['owner' => ExtendScope::OWNER_CUSTOM, 'without_default' => true]
+            ]
         );
     }
 
