213d836f326a9fe7c69e5084b5b112462a3f3e7b
Alex Torchenko <torchenko@gmail.com>
2016-08-16 18:50:26 +0300

    BB-1953 Resize Product Images  - add customer notice when saving product images config

---
+++ src/OroB2B/Bundle/ProductBundle/EventListener/ProductImagesConfigurationListener.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace OroB2B\Bundle\ProductBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Translation\TranslatorInterface;
+
+use Oro\Bundle\ConfigBundle\Event\ConfigSettingsUpdateEvent;
+
+class ProductImagesConfigurationListener
+{
+    const PRODUCT_IMAGE_WATERMARK_SECTION_PREFIX = 'orob2b_product.product_image_watermark';
+
+    /**
+     * @var TranslatorInterface
+     */
+    protected $translator;
+
+    /**
+     * @var Session
+     */
+    private $session;
+
+    /**
+     * @param Session $session
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(Session $session, TranslatorInterface $translator)
+    {
+        $this->session = $session;
+        $this->translator = $translator;
+    }
+
+    /**
+     * @param ConfigSettingsUpdateEvent $event
+     */
+    public function beforeSave(ConfigSettingsUpdateEvent $event)
+    {
+        foreach ($event->getSettings() as $configKey => $setting) {
+            if (false !== strpos($configKey, self::PRODUCT_IMAGE_WATERMARK_SECTION_PREFIX)) {
+                $this->session->getFlashBag()->add(
+                    'info',
+                    $this->translator->trans('orob2b.product.system_configuration.notice.product_image_watermark')
+                );
+
+                return;
+            }
+        }
+    }
+}
