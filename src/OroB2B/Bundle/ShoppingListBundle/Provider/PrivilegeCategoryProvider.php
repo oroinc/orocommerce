72a60bd4e23042b52b74ac9fe0b1c6c8a1e421a4
Dragos Andries <dandries@pitechnologies.ro>
2016-08-17 14:31:58 +0300

    BB-3970: added privilege category providers per bundle - restored PrivilegeCategoryProvider from platform, to its initial state - added providers in each bundle, so that categories are loaded dinamically and reduce dependency

---
+++ src/OroB2B/Bundle/ShoppingListBundle/Provider/PrivilegeCategoryProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace OroB2B\Bundle\ShoppingListBundle\Provider;
+
+use Oro\Bundle\UserBundle\Model\PrivilegeCategory;
+use Oro\Bundle\UserBundle\Provider\PrivilegeCategoryProviderInterface;
+
+class PrivilegeCategoryProvider implements PrivilegeCategoryProviderInterface
+{
+    const NAME = 'shopping';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return self::NAME;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRolePrivilegeCategory()
+    {
+        return new PrivilegeCategory(self::NAME, 'orob2b.shoppinglist.privilege.category.shopping.label', true, 1);
+    }
+}
