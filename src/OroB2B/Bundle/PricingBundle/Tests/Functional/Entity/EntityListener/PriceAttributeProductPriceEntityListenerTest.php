90e1f0cc17896cc03068935f6460a41c2e032707
Vitaliy Berdylo <vberdylo@magecore.com>
2016-08-18 20:35:10 +0300

    BB-4233: Drop PriceRuleChangeTrigger entity - fix merge

--- src/OroB2B/Bundle/PricingBundle/Tests/Functional/Entity/EntityListener/PriceAttributeProductPriceEntityListenerTest.php
+++ src/OroB2B/Bundle/PricingBundle/Tests/Functional/Entity/EntityListener/PriceAttributeProductPriceEntityListenerTest.php
@@ -40,30 +40,30 @@ class PriceAttributeProductPriceEntityListenerTest extends WebTestCase
     {
         /** @var EntityManagerInterface $em */
         $em = $this->getContainer()->get('doctrine')->getManagerForClass(Product::class);
-    
+
         /** @var PriceAttributePriceList $priceAttribute */
         $priceAttribute = $this->getReference('price_attribute_price_list_1');
         /** @var Product $product */
         $product = $this->getReference(LoadProductData::PRODUCT_4);
-    
+
         $price = new PriceAttributeProductPrice();
         $price->setProduct($product)
             ->setPriceList($priceAttribute)
             ->setQuantity(1)
             ->setUnit($this->getReference('product_unit.box'))
             ->setPrice(Price::create(42, 'USD'));
-    
+
         $em->persist($price);
         $em->flush();
-    
+
         $traces = $this->getQueueMessageTraces();
         $this->assertCount(1, $traces);
-    
+
         $trace = $traces[0];
         $productId = $this->getProductIdFromTrace($trace);
         $this->assertNotEmpty($productId);
         $this->assertEquals($product->getId(), $productId);
-    
+
         /** @var PriceList $expectedPriceList */
         $expectedPriceList = $this->getReference(LoadPriceLists::PRICE_LIST_1);
         $this->assertEquals($expectedPriceList->getId(), $this->getPriceListIdFromTrace($trace));
@@ -73,25 +73,25 @@ class PriceAttributeProductPriceEntityListenerTest extends WebTestCase
     {
         /** @var EntityManagerInterface $em */
         $em = $this->getContainer()->get('doctrine')->getManagerForClass(Product::class);
-    
+
         /** @var Product $product */
         $product = $this->getReference(LoadProductData::PRODUCT_1);
-    
+
         /** @var PriceAttributeProductPrice $price */
         $price = $this->getReference('price_attribute_product_price.1');
         $price->setPrice(Price::create(1000, 'USD'));
-    
+
         $em->persist($price);
         $em->flush();
-    
+
         $traces = $this->getQueueMessageTraces();
         $this->assertCount(1, $traces);
-    
+
         $trace = $traces[0];
         $productId = $this->getProductIdFromTrace($trace);
         $this->assertNotEmpty($productId);
         $this->assertEquals($product->getId(), $productId);
-    
+
         /** @var PriceList $expectedPriceList */
         $expectedPriceList = $this->getReference(LoadPriceLists::PRICE_LIST_1);
         $this->assertEquals($expectedPriceList->getId(), $this->getPriceListIdFromTrace($trace));
@@ -101,23 +101,23 @@ class PriceAttributeProductPriceEntityListenerTest extends WebTestCase
     {
         /** @var EntityManagerInterface $em */
         $em = $this->getContainer()->get('doctrine')->getManagerForClass(Product::class);
-    
+
         /** @var Product $product */
         $product = $this->getReference(LoadProductData::PRODUCT_1);
-    
+
         /** @var PriceAttributeProductPrice $price */
         $price = $this->getReference('price_attribute_product_price.1');
         $em->remove($price);
         $em->flush();
-    
+
         $traces = $this->getQueueMessageTraces();
         $this->assertCount(1, $traces);
-    
+
         $trace = $traces[0];
         $productId = $this->getProductIdFromTrace($trace);
         $this->assertNotEmpty($productId);
         $this->assertEquals($product->getId(), $productId);
-    
+
         /** @var PriceList $expectedPriceList */
         $expectedPriceList = $this->getReference(LoadPriceLists::PRICE_LIST_1);
         $this->assertEquals($expectedPriceList->getId(), $this->getPriceListIdFromTrace($trace));
