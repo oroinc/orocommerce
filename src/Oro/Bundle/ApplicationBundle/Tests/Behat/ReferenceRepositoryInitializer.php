db1d2c5679294ee9f93c12809545daec0615ba86
Valentyna Shabala <v.shabala@svitla.com>
2016-08-26 18:38:47 +0300

    BB-3985: functional tests - custom initializer

--- src/Oro/Bundle/ApplicationBundle/Tests/Behat/ReferenceRepositoryInitializer.php
+++ src/Oro/Bundle/ApplicationBundle/Tests/Behat/ReferenceRepositoryInitializer.php
@@ -3,11 +3,91 @@
 namespace Oro\Bundle\ApplicationBundle\Tests\Behat;
 
 use Oro\Bundle\TestFrameworkBundle\Behat\Fixtures\ReferenceRepositoryInitializer as BaseInitializer;
+use Doctrine\ORM\EntityManager;
+use Oro\Bundle\AddressBundle\Entity\AddressType;
+use Oro\Bundle\EntityBundle\ORM\Registry;
+use Oro\Bundle\EntityExtendBundle\OroEntityExtendBundle;
+use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
+use Oro\Bundle\UserBundle\Entity\Repository\RoleRepository;
+use Oro\Bundle\UserBundle\Entity\User;
+use Oro\Bundle\AddressBundle\Entity\Country;
+use Oro\Bundle\AddressBundle\Entity\Repository\RegionRepository;
+use Oro\Bundle\AddressBundle\Entity\Region;
+use Oro\Bundle\AccountBundle\Entity\AccountUserAddress;
+use Oro\Bundle\AccountBundle\Entity\AccountUserRole;
+use Oro\Bundle\PricingBundle\Entity\CombinedPriceList;
+use Oro\Bundle\PricingBundle\Entity\PriceListCurrency;
+use Oro\Bundle\PricingBundle\Entity\PriceList;
+use Oro\Bundle\ProductBundle\Entity\ProductUnit;
+use Oro\Bundle\WebsiteBundle\Entity\Website;
 
 class ReferenceRepositoryInitializer extends BaseInitializer
 {
     public function init()
     {
         parent::init();
+        /** @var CountryRepository $repository */
+        $repository = $this->em->getRepository('OroAddressBundle:Country');
+        /** @var Country $germany */
+        $germany = $repository->findOneBy(['name' => 'Germany']);
+        $this->referenceRepository->set('germany', $germany);
+
+        /** @var RegionRepository $repository */
+        $repository = $this->em->getRepository('OroAddressBundle:Region');
+        /** @var Region $berlin */
+        $berlin = $repository->findOneBy(['name' => 'Berlin']);
+        $this->referenceRepository->set('berlin', $berlin);
+
+        /** @var AccountUserRoleRepository $repository */
+        $repository = $this->em->getRepository('OroAccountBundle:AccountUserRole');
+        /** @var AccountUserRole buyer */
+        $buyer = $repository->findOneBy(['role' => 'ROLE_FRONTEND_BUYER']);
+        $this->referenceRepository->set('buyer', $buyer);
+
+        /** @var ProductUnitRepository $repository */
+        $repository = $this->em->getRepository('OroProductBundle:ProductUnit');
+        /** @var ProductUnit item*/
+        $item = $repository->findOneBy(['code' => 'item']);
+        $this->referenceRepository->set('item', $item);
+
+        /** @var AddressTypeRepository $repository */
+        $repository = $this->em->getRepository('OroAddressBundle:AddressType');
+        /** @var AddressType $billingType*/
+        $billingType = $repository->findOneBy(['name' => 'billing']);
+        $this->referenceRepository->set('billingType', $billingType);
+        /** @var AddressType $shippingType*/
+        $shippingType = $repository->findOneBy(['name' => 'shipping']);
+        $this->referenceRepository->set('shippingType', $shippingType);
+
+
+        /** @var PriceListCurrencyRepository $repository */
+        $repository = $this->em->getRepository('OroPricingBundle:PriceListCurrency');
+        /** @var PriceListCurrency EUR*/
+        $eur = $repository->findOneBy(['currency' => 'EUR']);
+        $this->referenceRepository->set('eur', $eur);
+
+        /** @var PriceListRepository $repository */
+        $repository = $this->em->getRepository('OroPricingBundle:PriceList');
+        /** @var PriceList $pricelist1*/
+        $pricelist1 = $repository->findOneBy(['id' => '1']);
+        $this->referenceRepository->set('pricelist1', $pricelist1);
+
+        /** @var WebsiteRepository $repository */
+        $repository = $this->em->getRepository('OroWebsiteBundle:Website');
+        /** @var Website $website1*/
+        $website1 = $repository->findOneBy(['id' => '1']);
+        $this->referenceRepository->set('website1', $website1);
+
+        /** @var CombinedPriceListRepository $repository */
+        $repository = $this->em->getRepository('OroPricingBundle:CombinedPriceList');
+        /** @var CombinedPriceList $combinedPriceList*/
+        $combinedPriceList = $repository->findOneBy(['id' => '1']);
+        $this->referenceRepository->set('combinedPriceList', $combinedPriceList);
+
+
+        $inventoryStatusClassName = ExtendHelper::buildEnumValueClassName('prod_inventory_status');
+        $enumInventoryStatuses = $this->em->getRepository($inventoryStatusClassName)->findOneBy(['id' => 'in_stock']);
+        $this->referenceRepository->set('enumInventoryStatuses', $enumInventoryStatuses);
+
     }
 }
---
+++ src/Oro/Bundle/ShippingBundle/Tests/Behat/Context/FeatureContext.php
@@ -0,0 +1,232 @@
+<?php
+
+namespace Oro\Bundle\ShippingBundle\Tests\Behat\Context;
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Gherkin\Node\TableNode;
+use Oro\Bundle\DataGridBundle\Tests\Behat\Element\Grid;
+use Oro\Bundle\FormBundle\Tests\Behat\Element\OroForm;
+use Oro\Bundle\NavigationBundle\Tests\Behat\Element\MainMenu;
+use Oro\Bundle\TestFrameworkBundle\Behat\Context\OroFeatureContext;
+use Oro\Bundle\TestFrameworkBundle\Behat\Element\Form;
+use Oro\Bundle\TestFrameworkBundle\Behat\Element\OroElementFactoryAware;
+use Oro\Bundle\TestFrameworkBundle\Tests\Behat\Context\ElementFactoryDictionary;
+use OroB2B\Bundle\CheckoutBundle\Tests\Behat\Element\CheckoutStep;
+use OroB2B\Bundle\ShippingBundle\Tests\Behat\Elements\ShoppingListWidget;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Behat\Symfony2Extension\Context\KernelAwareContext;
+
+class FeatureContext extends OroFeatureContext implements OroElementFactoryAware, KernelAwareContext
+{
+    use ElementFactoryDictionary;
+
+    /**
+     * @var KernelInterface
+     */
+    protected $kernel;
+
+    public function setKernel(KernelInterface $kernelInterface)
+    {
+        $this->kernel = $kernelInterface;
+    }
+
+    /**
+     * @Given there is EUR currency in the system configuration
+     */
+    public function thereIsEurCurrencyInTheSystemConfiguration()
+    {
+        $container = $this->kernel->getContainer();
+        $configManager = $container->get('oro_config.global');
+        $configManager->set('oro_currency.allowed_currencies', ['EUR','USD']);
+        $configManager->flush();
+        $configManager = $container->get('oro_config.manager');
+        $configManager->set('oro_b2b_pricing.enabled_currencies', ['EUR','USD']);
+        $configManager->flush();
+    }
+
+
+    /**
+     * @Given /^I login as (?P<email>\S+)$/
+     */
+    public function loginAsBuyer($email)
+    {
+        $this->visitPath('account/user/login');
+        $this->getSession()->getDriver()->waitForAjax();
+        /** @var OroForm $form */
+        $form = $this->createElement('OroForm');
+        $table = new TableNode([
+            ['Email Address', $email],
+            ['Password', $email]
+        ]);
+        $form->fill($table);
+        $form->pressButton('Sign In');
+    }
+
+    /**
+     * @When /^Buyer is on (?P<checkoutStep>[\w\s]+) Checkout step on (?P<shoppingListName>[\w\s]+)$/
+     */
+    public function buyerIsOnShippingMethodCheckoutStep($checkoutStep, $shoppingListName)
+    {
+        $this->getSession()->resizeWindow(1440, 700, 'current');
+        $this->getSession()->getDriver()->waitForAjax();
+        /** @var ShoppingListWidget $shoppingListWidget */
+        $shoppingListWidget = $this->createElement('ShoppingListWidget');
+        $shoppingListWidget->mouseOver();
+        $shoppingList = $shoppingListWidget->getShoppingList($shoppingListName);
+        $shoppingList->viewDetails();
+        $this->getSession()->getDriver()->waitForAjax();
+        $this->getSession()->getPage()->clickLink('Create Order');
+        $this->getSession()->getDriver()->waitForAjax();
+
+        /** @var CheckoutStep $checkoutStep */
+        $checkoutStep = $this->createElement('CheckoutStep');
+        $checkoutStep->assertTitle('Billing Information');
+        $this->getSession()->getDriver()->waitForAjax();
+        $this->getSession()->getPage()->pressButton('Continue');
+        $this->getSession()->getDriver()->waitForAjax();
+        $checkoutStep->assertTitle('Shipping Information');
+        $this->getSession()->getPage()->pressButton('Continue');
+        $this->getSession()->getDriver()->waitForAjax();
+        $checkoutStep->assertTitle('Shipping Method');
+        sleep(20);
+    }
+
+    /**
+     * @Then There is no shipping method available for this order
+     */
+    public function noShippingMethodsAvailable()
+    {
+        $this->assertSession()->elementContains('css', '.notification_alert', 'No shipping methods are available');
+    }
+
+    /**
+     * @Given Admin User has Shipping Rulesт Full permissions
+     */
+    public function adminUserHasShippingRulesFullPermissions()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given Buyer User with Edit Shipping Address permissions
+     */
+    public function buyerUserWithEditShippingAddressPermissions()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given Shopping Rule Flat Rate Shipping Cost = :arg1
+     */
+    public function shoppingRuleFlatRateShippingCost($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given Shopping Rule Flat Rate Type = per Order by default
+     */
+    public function shoppingRuleFlatRateTypePerOrderByDefault()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given Shopping Rule Flat Rate Handling Fee = :arg1
+     */
+    public function shoppingRuleFlatRateHandlingFee($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given Admin User created Flat Rate Shipping Rule #:arg1 with next data:
+     */
+    public function adminUserCreatedFlatRateShippingRuleWithNextData(TableNode $table)
+    {
+      throw new PendingException();
+    }
+
+    /**
+     * @Given Buyer created order with:
+     */
+    public function buyerCreatedOrderWith(TableNode $table)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then Shipping Type :arg1 is shown for Buyer selection
+     */
+    public function shippingTypeFlatRateEurIsShownForBuyerSelection($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then One the next Checkout step order subtotal is recalculated to :arg1
+     */
+    public function oneTheNextCheckoutStepOrderSubtotalIsRecalculatedTo($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given Admin User edited :arg1 with next data:
+     */
+    public function adminUserEditedWithNextData($arg1, TableNode $table)
+    {
+        $this->getMink()->setDefaultSessionName('second_session');
+        $this->getSession()->resizeWindow(1920, 1080, 'current');
+
+        $this->loginAsAdmin();
+
+        /** @var MainMenu $mainMenu */
+        $mainMenu = $this->createElement('MainMenu');
+        $mainMenu->openAndClick('System/Shipping Rules');
+        $this->getSession()->getDriver()->waitForAjax();
+
+        /** @var Grid $grid */
+        $grid = $this->createElement('Grid');
+        $grid->clickActionLink($arg1, 'Edit');
+        $this->getSession()->getDriver()->waitForAjax();
+
+        /** @var Form $form */
+        $form = $this->createElement('OroForm');
+        $form->fill($table);
+        $form->saveAndClose();
+        $this->getSession()->getDriver()->waitForAjax();
+
+        $this->getSession('second_session')->stop();
+        $this->getMink()->setDefaultSessionName('first_session');
+    }
+
+    protected function loginAsAdmin()
+    {
+        $this->visitPath('admin/user/login');
+        /** @var Form $login */
+        $login = $this->createElement('Login');
+        $login->fill(new TableNode([['Username', 'admin'], ['Password', 'admin']]));
+        $login->pressButton('Log in');
+        $this->getSession()->getDriver()->waitForAjax();
+    }
+
+    /**
+     * @Then Flat Rate is non-visible for Buyer selection
+     */
+    public function flatRateIsNonVisibleForBuyerSelection()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given the product unit :arg1 was uploaded in database
+     */
+    public function test($arg1)
+    {
+        sleep(30);
+    }
+}
+
