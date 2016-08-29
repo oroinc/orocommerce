f419de26c130b510982ea8d7e0f0c63f367ba4d4
Alexander Pobereznichenko <opobereznichenko@magecore.com>
2016-08-29 17:15:49 +0300

    BB-4275: Error during application installation in WebsiteBundle\EventListener\InstallCommandListener

--- src/Oro/Bundle/WebsiteBundle/EventListener/InstallCommandListener.php
+++ src/Oro/Bundle/WebsiteBundle/EventListener/InstallCommandListener.php
@@ -4,9 +4,6 @@ namespace Oro\Bundle\WebsiteBundle\EventListener;
 
 use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Bundle\InstallerBundle\Command\InstallCommand;
-use Oro\Bundle\WebsiteBundle\Entity\Repository\WebsiteRepository;
-use Oro\Bundle\WebsiteBundle\Entity\Website;
-use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Component\Console\Event\ConsoleTerminateEvent;
 
 class InstallCommandListener
@@ -17,18 +14,11 @@ class InstallCommandListener
     protected $configManager;
 
     /**
-     * @var RegistryInterface
-     */
-    protected $registry;
-
-    /**
      * @param ConfigManager $configManager
-     * @param RegistryInterface $registry
      */
-    public function __construct(ConfigManager $configManager, RegistryInterface $registry)
+    public function __construct(ConfigManager $configManager)
     {
         $this->configManager = $configManager;
-        $this->registry = $registry;
     }
 
     /**
@@ -39,13 +29,10 @@ class InstallCommandListener
         $command = $event->getCommand();
         if ($command instanceof InstallCommand) {
             try {
-                /** @var WebsiteRepository $repo */
-                $repo = $this->registry->getRepository(Website::class);
-                $website = $repo->getDefaultWebsite();
                 $url = $this->configManager->get('oro_ui.application_url');
                 $this->configManager->set('oro_b2b_website.url', $url);
                 $this->configManager->set('oro_b2b_website.secure_url', $url);
-                $this->configManager->flush($website);
+                $this->configManager->flush();
             } catch (\Exception $e) {
                 //do nothing in case when application not installed and table not exists
             }
