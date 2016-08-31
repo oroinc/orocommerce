d23e83ee70a5799d4bbd62c6fbdb8f8bd0bd26c7
Oleg Popadko <oleg@popadko.com>
2016-08-31 16:41:04 +0300

    BB-4287: Refactor shipping rule form  - show/hide add button via js

--- src/Oro/Bundle/ShippingBundle/Form/Type/ShippingRuleType.php
+++ src/Oro/Bundle/ShippingBundle/Form/Type/ShippingRuleType.php
@@ -5,7 +5,6 @@ namespace Oro\Bundle\ShippingBundle\Form\Type;
 use Oro\Bundle\CurrencyBundle\Form\Type\CurrencySelectionType;
 use Oro\Bundle\FormBundle\Form\Type\CollectionType;
 use Oro\Bundle\ShippingBundle\Entity\ShippingRule;
-use Oro\Bundle\ShippingBundle\Entity\ShippingRuleMethodConfig;
 use Oro\Bundle\ShippingBundle\Method\ShippingMethodInterface;
 use Oro\Bundle\ShippingBundle\Method\ShippingMethodRegistry;
 use Symfony\Component\Form\AbstractType;
@@ -14,9 +13,10 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class ShippingRuleType extends AbstractType
 {
@@ -28,11 +28,18 @@ class ShippingRuleType extends AbstractType
     protected $methodRegistry;
 
     /**
-     * @param ShippingMethodRegistry $methodRegistry
+     * @var TranslatorInterface
      */
-    public function __construct(ShippingMethodRegistry $methodRegistry)
+    protected $translator;
+
+    /**
+     * @param ShippingMethodRegistry $methodRegistry
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(ShippingMethodRegistry $methodRegistry, TranslatorInterface $translator)
     {
         $this->methodRegistry = $methodRegistry;
+        $this->translator = $translator;
     }
 
     /**
@@ -62,58 +69,19 @@ class ShippingRuleType extends AbstractType
             ->add('stopProcessing', CheckboxType::class, [
                 'required' => false,
                 'label' => 'oro.shipping.shippingrule.stop_processing.label',
+            ])
+            ->add('method', ChoiceType::class, [
+                'mapped' => false,
+                'choices' => $this->getMethods(),
             ]);
-
-        $builder->addEventListener(FormEvents::POST_SET_DATA, [$this, 'postSet']);
-        $builder->addEventListener(FormEvents::PRE_SUBMIT, [$this, 'preSubmit']);
     }
 
     /**
-     * @param FormEvent $event
+     * {@inheritdoc}
      */
-    public function postSet(FormEvent $event)
+    public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        /** @var ShippingRule $data */
-        $data = $event->getData();
-        $form = $event->getForm();
-        if (!$data) {
-            return;
-        }
-
-        $methods = $this->getMethods(array_map(function (ShippingRuleMethodConfig $config) {
-            return $config->getMethod();
-        }, $data->getMethodConfigs()->toArray()));
-
-        if ($methods) {
-            $form->add('method', ChoiceType::class, [
-                'mapped' => false,
-                'choices' => $methods,
-            ]);
-        }
-    }
-
-    /**
-     * @param FormEvent $event
-     */
-    public function preSubmit(FormEvent $event)
-    {
-        $submittedData = $event->getData();
-        $form = $event->getForm();
-        if (!$event->getData()) {
-            return;
-        }
-
-        $methods = $this->getMethods(array_map(function ($methodConfigs) {
-            return $methodConfigs['method'];
-        }, $submittedData['methodConfigs']));
-
-        $form->remove('method');
-        if ($methods) {
-            $form->add('method', ChoiceType::class, [
-                'mapped' => false,
-                'choices' => $methods,
-            ]);
-        }
+        $view->vars['methods'] = $this->getMethods(true);
     }
 
     /**
@@ -135,18 +103,22 @@ class ShippingRuleType extends AbstractType
     }
 
     /**
-     * @param array $configuredMethods
+     * @param bool $translate
      * @return array
      */
-    protected function getMethods(array $configuredMethods = [])
+    protected function getMethods($translate = false)
     {
-        return array_diff_key(array_reduce(
+        return array_reduce(
             $this->methodRegistry->getShippingMethods(),
-            function (array $result, ShippingMethodInterface $method) {
-                $result[$method->getIdentifier()] = $method->getLabel();
+            function (array $result, ShippingMethodInterface $method) use ($translate) {
+                $label = $method->getLabel();
+                if ($translate) {
+                    $label = $this->translator->trans($label);
+                }
+                $result[$method->getIdentifier()] = $label;
                 return $result;
             },
             []
-        ), array_flip($configuredMethods));
+        );
     }
 }
--- src/Oro/Bundle/ShippingBundle/Resources/public/js/app/views/shipping-rule-method-view.js
+++ src/Oro/Bundle/ShippingBundle/Resources/public/js/app/views/shipping-rule-method-view.js
@@ -2,9 +2,8 @@ define([
     'jquery',
     'backbone',
     'underscore',
-    'orotranslation/js/translator',
     'oroui/js/mediator'
-], function($, Backbone, _, __, mediator) {
+], function($, Backbone, _, mediator) {
     'use strict';
 
     /**
@@ -14,7 +13,7 @@ define([
      */
     return Backbone.View.extend({
 
-        requiredOptions: ['methodSelector', 'buttonSelector', 'methodCount', 'updateFlag'],
+        requiredOptions: ['methodSelectSelector', 'buttonSelector', 'methodCount', 'updateFlag', 'methods'],
 
         /**
          * @param options Object
@@ -32,7 +31,20 @@ define([
                 throw new TypeError('Form not found');
             }
             this.form = $(this.form);
-            $(this.el).find(options.buttonSelector).on('click', _.bind(this.changeHandler, this));
+            this.methodSelect = $(this.el).find(this.options.methodSelectSelector);
+            this.button = $(this.el).find(options.buttonSelector);
+
+            this.button.on('click', _.bind(this.changeHandler, this));
+
+            var elements = this.form.find(".orob2b-shipping-rule-method-configs-collection .row-oro.oro-multiselect-holder");
+            var self = this;
+            elements.each(function(index, element) {
+                $(element).parent().on('content:remove', function(e) {
+                    self.updateMethodSelector(element);
+                });
+            });
+
+            this.updateMethodSelector();
         },
 
         /**
@@ -42,7 +54,7 @@ define([
             var $form = this.form;
             var data = $form.serializeArray();
             var url = $form.attr('action');
-            var value = $(this.el).find(this.options.methodSelector).val();
+            var value = $(this.el).find(this.options.methodSelectSelector).val();
             data.push({
                 'name': 'oro_shipping_rule[methodConfigs][' + this.options.methodCount + '][method]',
                 'value': value
@@ -57,6 +69,37 @@ define([
                 type: $form.attr('method'),
                 data: $.param(data)
             });
+        },
+
+        updateMethodSelector: function(removedElement) {
+            var elements = this.form.find(".orob2b-shipping-rule-method-configs-collection .row-oro.oro-multiselect-holder");
+            var methods = [];
+            var self = this;
+
+            elements.each(function(index, element) {
+                if (removedElement && self.getMethod(element) === self.getMethod(removedElement)) {
+                    return;
+                }
+                methods.push($(element).find('input[data-name="field__method"]').val());
+            });
+            if (methods.length === Object.keys(this.options.methods).length) {
+                $(this.el).hide();
+                return;
+            }
+            this.methodSelect.empty(); // remove old options
+            $.each(self.options.methods, function(value, label) {
+                if ($.inArray(value, methods) === -1) {
+                    self.methodSelect.append($("<option></option>").attr("value", value).text(label));
+                }
+            });
+
+            $.uniform.update(this.methodSelect);
+            $(this.el).show();
+        },
+
+
+        getMethod: function(element) {
+            return $(element).find('input[data-name="field__method"]').val();
         }
     });
 });
