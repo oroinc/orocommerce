3ab21d2af84741dc8f4d81f3ff54cb3069efe57a
Vitalii Yeromenko <vyeromenko@magecore.com>
2016-09-02 13:05:12 +0300

    BB-4076: Website search mapping provider - mapSelectedData method for the mapping provider - unit test

--- src/Oro/Bundle/WebsiteSearchBundle/Provider/WebsiteSearchMappingProvider.php
+++ src/Oro/Bundle/WebsiteSearchBundle/Provider/WebsiteSearchMappingProvider.php
@@ -3,6 +3,8 @@
 namespace Oro\Bundle\WebsiteSearchBundle\Provider;
 
 use Oro\Bundle\SearchBundle\Provider\AbstractSearchMappingProvider;
+use Oro\Bundle\SearchBundle\Query\Criteria\Criteria;
+use Oro\Bundle\SearchBundle\Query\Query;
 use Oro\Bundle\WebsiteSearchBundle\Loader\ConfigurationLoaderInterface;
 
 class WebsiteSearchMappingProvider extends AbstractSearchMappingProvider
@@ -35,5 +37,38 @@ class WebsiteSearchMappingProvider extends AbstractSearchMappingProvider
         }
 
         return $this->configuration;
+    }
+
+    /**
+     * @param Query $query
+     * @param array $item
+     * @return array|null
+     */
+    public function mapSelectedData(Query $query, array $item)
+    {
+        $selects = $query->getSelect();
+
+        if (empty($selects)) {
+            return null;
+        }
+
+        $result = [];
+
+        foreach ($selects as $select) {
+            list ($type, $name) = Criteria::explodeFieldTypeName($select);
+
+            $result[$name] = '';
+
+            if (isset($item[$name])) {
+                $value = $item[$name];
+                if (is_array($value)) {
+                    $value = array_shift($value);
+                }
+
+                $result[$name] = $value;
+            }
+        }
+
+        return $result;
     }
 }
--- src/Oro/Bundle/WebsiteSearchBundle/Tests/Unit/Provider/WebsiteSearchMappingProviderTest.php
+++ src/Oro/Bundle/WebsiteSearchBundle/Tests/Unit/Provider/WebsiteSearchMappingProviderTest.php
@@ -2,12 +2,16 @@
 
 namespace Oro\Bundle\WebsiteSearchBundle\Tests\Unit\Provider;
 
+use Oro\Bundle\SearchBundle\Query\Query;
 use Oro\Bundle\SearchBundle\Tests\Unit\Provider\AbstractSearchMappingProviderTest;
 use Oro\Bundle\WebsiteSearchBundle\Loader\ConfigurationLoaderInterface;
 use Oro\Bundle\WebsiteSearchBundle\Provider\WebsiteSearchMappingProvider;
 
 class WebsiteSearchMappingProviderTest extends AbstractSearchMappingProviderTest
 {
+    /** @var WebsiteSearchMappingProvider */
+    protected $provider;
+
     /** @var ConfigurationLoaderInterface|\PHPUnit_Framework_MockObject_MockObject $mappingConfigurationLoader */
     protected $mappingConfigurationLoader;
 
@@ -30,10 +34,59 @@ class WebsiteSearchMappingProviderTest extends AbstractSearchMappingProviderTest
     {
         $this->mappingConfigurationLoader = $this->getMock(ConfigurationLoaderInterface::class);
         $this->mappingConfigurationLoader
-            ->expects($this->once())
+            ->expects($this->any())
             ->method('getConfiguration')
             ->willReturn($this->testMapping);
 
         return new WebsiteSearchMappingProvider($this->mappingConfigurationLoader);
+    }
+
+    public function testMapSelectedData()
+    {
+        $query = new Query();
+        $query->select('title');
+        $query->addSelect('codes');
+
+        $item = [
+            'title' => 'Test item title',
+            'codes' => [
+                'code1',
+                'code2',
+            ],
+            'description' => 'I don\'t want to select it',
+        ];
+
+        $expectedResult = [
+            'title' => 'Test item title',
+            'codes' => 'code1',
+        ];
+
+        $this->assertEquals($expectedResult, $this->provider->mapSelectedData($query, $item));
+    }
+
+    public function testMapSelectedDataEmptySelect()
+    {
+        $query = new Query();
+        $item = [
+            'title' => 'Test item title',
+        ];
+
+        $this->assertNull($this->provider->mapSelectedData($query, $item));
+    }
+
+    public function testMapSelectedDataEmptyItem()
+    {
+        $query = new Query();
+        $query->select('title');
+        $query->addSelect('codes');
+
+        $item = [];
+
+        $expectedResult = [
+            'title' => '',
+            'codes' => '',
+        ];
+
+        $this->assertEquals($expectedResult, $this->provider->mapSelectedData($query, $item));
     }
 }
