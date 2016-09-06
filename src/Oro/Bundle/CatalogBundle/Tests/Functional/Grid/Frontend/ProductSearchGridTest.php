99d075001213cbed69a85007e3f7706f6cf0b0e5
Filip <filip.gorny@gmail.com>
2016-09-06 10:42:07 +0200

    BB-4063: Add functional test for filters - code review fixes

--- src/Oro/Bundle/CatalogBundle/Tests/Functional/Grid/Frontend/ProductSearchGridTest.php
+++ src/Oro/Bundle/CatalogBundle/Tests/Functional/Grid/Frontend/ProductSearchGridTest.php
@@ -86,37 +86,22 @@ class ProductSearchGridTest extends WebTestCase
 
     public function testFilters()
     {
-        $response = $this->client->requestFrontendGrid(
-            [
-                'gridName' => 'frontend-product-search-grid'
-            ],
-            [],
-            true
+        $data = $this->getDatagridData(
+            'frontend-product-search-grid'
         );
-
-        $result = $this->getJsonResponseContent($response, 200);
-
-        $data = $result['data'];
 
         $firstRow = array_shift($data);
         $countWithoutFilters = count($data);
 
-        $response = $this->client->requestFrontendGrid(
+        $filteredData = $this->getDatagridData(
+            'frontend-product-search-grid',
             [
-                'gridName' => 'frontend-product-search-grid'
-            ],
-            [
-                'frontend-product-search-grid[_filter][sku][value]' => $firstRow['sku'],
-                'frontend-product-search-grid[_filter][sku][type]' => '1',
-                'frontend-product-search-grid[_filter][name][value]' => $firstRow['name'],
-                'frontend-product-search-grid[_filter][name][type]' => '1',
-            ],
-            true
+                '[sku][value]' => $firstRow['sku'],
+                '[sku][type]' => '1',
+                '[name][value]' => $firstRow['name'],
+                '[name][type]' => '1',
+            ]
         );
-
-        $result = $this->getJsonResponseContent($response, 200);
-
-        $filteredData = $result['data'];
 
         $this->assertTrue($countWithoutFilters > count($filteredData));
 
@@ -128,37 +113,23 @@ class ProductSearchGridTest extends WebTestCase
 
     public function testAllTextFilter()
     {
-        $response = $this->client->requestFrontendGrid(
-            [
-                'gridName' => 'frontend-product-search-grid'
-            ],
-            [],
-            true
+        $data = $this->getDatagridData(
+            'frontend-product-search-grid'
         );
-
-        $result = $this->getJsonResponseContent($response, 200);
-
-        $data = $result['data'];
 
         $firstRow = array_shift($data);
         $allTextValue = substr($firstRow['name'], 1, -1);
 
         $this->assertNotEmpty($allTextValue);
 
-        $response = $this->client->requestFrontendGrid(
+        $filteredData = $this->getDatagridData(
+            'frontend-product-search-grid',
             [
-                'gridName' => 'frontend-product-search-grid'
-            ],
-            [
-                'frontend-product-search-grid[_filter][sku][all_text]' => $firstRow['sku'],
-                'frontend-product-search-grid[_filter][sku][type]' => '1',
-            ],
-            true
+                '[sku][all_text]' => $firstRow['sku'],
+                '[sku][type]' => '1'
+            ]
         );
 
-        $result = $this->getJsonResponseContent($response, 200);
-
-        $filteredData = $result['data'];
         $found = false;
 
         foreach ($filteredData as $row) {
