22473054e2886f687aa3269433d38f90124777d9
Dragos Andries <dandries@pitechnologies.ro>
2016-09-06 14:46:34 +0300

    BB-4268: added cascade options in entity config also

--- src/Oro/Bundle/SEOBundle/Migrations/Schema/v1_1/OroSEOBundle.php
+++ src/Oro/Bundle/SEOBundle/Migrations/Schema/v1_1/OroSEOBundle.php
@@ -7,12 +7,16 @@ use Doctrine\DBAL\Schema\Schema;
 use Oro\Bundle\CatalogBundle\Entity\Category;
 use Oro\Bundle\CMSBundle\Entity\Page;
 use Oro\Bundle\EntityConfigBundle\Migration\UpdateEntityConfigFieldValueQuery;
+use Oro\Bundle\LocaleBundle\Entity\LocalizedFallbackValue;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 use Oro\Bundle\ProductBundle\Entity\Product;
 
 class OroSEOBundle implements Migration
 {
+    const ENTITY_TO = LocalizedFallbackValue::class;
+    const RELATION_TYPE = 'manyToMany';
+
     /**
      * @inheritdoc
      */
@@ -33,6 +37,15 @@ class OroSEOBundle implements Migration
                     )
                 );
             }
+
+            $queries->addQuery(
+                new UpdateEntityConfigFieldCascadeQuery(
+                    $class,
+                    self::ENTITY_TO,
+                    self::RELATION_TYPE,
+                    $fieldNames
+                )
+            );
         }
     }
 }
