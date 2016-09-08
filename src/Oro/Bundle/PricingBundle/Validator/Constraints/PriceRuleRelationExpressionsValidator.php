6e28c316c98ed9aa6bb6c67c5b06bbccbbca463d
Alexander Pobereznichenko <apobereznichenko@gmail.com>
2016-09-08 19:37:50 +0300

    BB-4371: Allow to set field for qty, unit and currency in addition to predefined static values for rules

--- src/Oro/Bundle/PricingBundle/Validator/Constraints/PriceRuleRelationExpressionsValidator.php
+++ src/Oro/Bundle/PricingBundle/Validator/Constraints/PriceRuleRelationExpressionsValidator.php
@@ -6,23 +6,40 @@ use Oro\Bundle\PricingBundle\Entity\PriceRule;
 use Oro\Bundle\PricingBundle\Expression\ExpressionParser;
 use Oro\Bundle\PricingBundle\Expression\NodeInterface;
 use Oro\Bundle\PricingBundle\Expression\RelationNode;
+use Oro\Bundle\PricingBundle\Provider\PriceRuleFieldsProvider;
+use Oro\Bundle\ProductBundle\Entity\ProductUnit;
+use Oro\Bundle\SearchBundle\Exception\ExpressionSyntaxError;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class PriceRuleRelationExpressionsValidator extends ConstraintValidator
 {
+    const FIELD_ARE_NOT_ALLOWED_MESSAGE = 'oro.pricing.validators.field_are_not_allowed.message';
+    const ONE_EXPRESSION_ALLOWED_MESSAGE = 'oro.pricing.validators.one_expression_allowed.message';
+    const RELATION_NOT_IN_RULE_MESSAGE = 'oro.pricing.validators.relation_not_in_rule.message';
+    const ONLY_PRICE_RELATION_MESSAGE = 'oro.pricing.validators.only_price_relations_available.message';
+
     /**
      * @var ExpressionParser
      */
     protected $parser;
 
     /**
-     * @param ExpressionParser $parser
+     * @var PriceRuleFieldsProvider
      */
-    public function __construct(ExpressionParser $parser)
-    {
+    protected $fieldsProvider;
+
+    /**
+     * @param ExpressionParser $parser
+     * @param PriceRuleFieldsProvider $fieldsProvider
+     */
+    public function __construct(
+        ExpressionParser $parser,
+        PriceRuleFieldsProvider $fieldsProvider
+    ) {
         $this->parser = $parser;
+        $this->fieldsProvider = $fieldsProvider;
     }
 
     /**
@@ -36,6 +53,7 @@ class PriceRuleRelationExpressionsValidator extends ConstraintValidator
         }
 
         $this->validateCurrency($value);
+        $this->validateProductUnit($value);
     }
 
     /**
@@ -44,33 +62,20 @@ class PriceRuleRelationExpressionsValidator extends ConstraintValidator
     protected function validateCurrency(PriceRule $rule)
     {
         $expression = $rule->getCurrencyExpression();
-        if (!$expression) {
-            return;
-        }
-
-        /** @var NodeInterface[] $nodes */
-        $nodes = $this->parser->parse($expression)->getNodes();
         $path = 'currencyExpression';
-        if (count($nodes) !== 1) {
-            $this->addError($path, 'to many nodes');
+        if (
+            !$expression ||
+            null === ($nodes = $this->getNodes($expression, $path)) ||
+            !$this->validateNodesCount($nodes, $path) ||
+            !$this->validateIsRelationNode($nodes[0], $path) ||
+            !$this->validateIsRelationInRule($rule, $nodes[0], $path)
+        ) {
             return;
         }
 
-        /** @var RelationNode $node */
         $node = $nodes[0];
-        if (!$node instanceof RelationNode) {
-            $this->addError($path, 'invalid node');
-            return;
-        }
-
-        if (!$node->getField() !== 'currency') {
-            $this->addError($path, 'invalid node');
-            return;
-        }
-
-        /** @var  $ruleNode */
-        foreach ($this->parser->parse($expression)->getNodes() as $ruleNode) {
-//            if ($ruleNode instanceof RelationNode && $ruleNode->)
+        if ($node->getRelationField() !== 'currency') {
+            $this->addError($path, self::FIELD_ARE_NOT_ALLOWED_MESSAGE, ['%fieldName%' => $node->getRelationField()]);
         }
     }
 
@@ -80,45 +85,125 @@ class PriceRuleRelationExpressionsValidator extends ConstraintValidator
     protected function validateProductUnit(PriceRule $rule)
     {
         $expression = $rule->getProductUnitExpression();
-        if (!$expression) {
-            return;
-        }
-
-        /** @var NodeInterface[] $nodes */
-        $nodes = $this->parser->parse($expression)->getNodes();
-        $path = 'currencyExpression';
-        if (count($nodes) !== 1) {
-            $this->addError($path, 'to many nodes');
+        $path = 'productUnitExpression';
+        if (
+            !$expression ||
+            null === ($nodes = $this->getNodes($expression, $path)) ||
+            !$this->validateNodesCount($nodes, $path) ||
+            !$this->validateIsRelationNode($nodes[0], $path) ||
+            !$this->validateIsRelationInRule($rule, $nodes[0], $path)
+        ) {
             return;
         }
 
         /** @var RelationNode $node */
         $node = $nodes[0];
+        $relationContainerClassName = $this->fieldsProvider->getRealClassName($node->getContainer(), $node->getField());
+        $relationClassName = $this->fieldsProvider
+            ->getRealClassName(
+                $relationContainerClassName,
+                $node->getRelationField()
+            );
+        if (!is_subclass_of($relationClassName, ProductUnit::class)) {
+            $this->addError($path, self::FIELD_ARE_NOT_ALLOWED_MESSAGE, ['%fieldName%' => $node->getRelationField()]);
+        }
+    }
+
+    /**
+     * @param array $nodes
+     * @param string $path
+     * @return bool
+     */
+    protected function validateNodesCount(array $nodes, $path)
+    {
+        if (count($nodes) !== 1) {
+            $this->addError($path, self::ONE_EXPRESSION_ALLOWED_MESSAGE);
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param NodeInterface $node
+     * @param string $path
+     * @return bool
+     */
+    protected function validateIsRelationNode(NodeInterface $node, $path)
+    {
         if (!$node instanceof RelationNode) {
-            $this->addError($path, 'invalid node');
-            return;
+            $this->addError($path, self::ONLY_PRICE_RELATION_MESSAGE);
+            return false;
         }
+        return true;
+    }
 
-        if (!$node->getField() !== 'currency') {
-            $this->addError($path, 'invalid node');
-            return;
+    /**
+     * @param PriceRule $rule
+     * @param RelationNode $node
+     * @param string $path
+     * @return bool
+     */
+    protected function validateIsRelationInRule(PriceRule $rule, RelationNode $node, $path)
+    {
+        if (!$this->isRelationInRule($rule, $node)) {
+            $this->addError($path, self::RELATION_NOT_IN_RULE_MESSAGE, ['%relationName%' => $node->getField()]);
+            return false;
         }
+        return true;
+    }
 
-        /** @var  $ruleNode */
-        foreach ($this->parser->parse($expression)->getNodes() as $ruleNode) {
-//            if ($ruleNode instanceof RelationNode && $ruleNode->)
+    /**
+     * @param string $expression
+     * @param string $path
+     * @return array|NodeInterface[]
+     */
+    protected function getNodes($expression, $path)
+    {
+        try {
+            return $this->parser->parse($expression)->getNodes();
+        } catch (ExpressionSyntaxError $e) {
+            $this->addError($path, $e->getMessage());
+            return null;
         }
+    }
+
+    /**
+     * @param PriceRule $rule
+     * @param RelationNode $node
+     * @return bool
+     */
+    public function isRelationInRule(PriceRule $rule, RelationNode $node)
+    {
+        foreach ($this->parser->parse($rule->getRule())->getNodes() as $ruleNode) {
+            if ($ruleNode instanceof RelationNode && $this->isSameRelation($ruleNode, $node)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param RelationNode $node
+     * @param RelationNode $other
+     * @return bool
+     */
+    public function isSameRelation(RelationNode $node, RelationNode $other)
+    {
+        return $node->getContainer() == $other->getContainer() &&
+        $node->getContainerId() == $other->getContainerId() &&
+        $node->getField() == $other->getField();
     }
 
     /**
      * @param string $path
      * @param string $message
+     * @param array $params
      */
-    protected function addError($path, $message)
+    protected function addError($path, $message, array $params = [])
     {
         /** @var ExecutionContextInterface $context */
         $context = $this->context;
-        $context->buildViolation($message)
+        $context->buildViolation($message, $params)
             ->atPath($path)
             ->addViolation();
     }
