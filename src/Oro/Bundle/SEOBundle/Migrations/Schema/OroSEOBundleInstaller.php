2aad2de08c0218e0318d44e9ab1b5fb50cba86b2
Dragos Andries <dandries@pitechnologies.ro>
2016-09-14 11:06:29 +0300

    BB-4463: Broken import of a products - excluded the meta fields from importexport operations

--- src/Oro/Bundle/SEOBundle/Migrations/Schema/OroSEOBundleInstaller.php
+++ src/Oro/Bundle/SEOBundle/Migrations/Schema/OroSEOBundleInstaller.php
@@ -33,7 +33,7 @@ class OroSEOBundleInstaller implements Installation, ExtendExtensionAwareInterfa
      */
     public function getMigrationVersion()
     {
-        return 'v1_1';
+        return 'v1_2';
     }
 
     /**
@@ -90,6 +90,7 @@ class OroSEOBundleInstaller implements Installation, ExtendExtensionAwareInterfa
             $targetDetailedColumnNames,
             $targetGridColumnNames,
             [
+
                 'extend' => [
                     'owner' => ExtendScope::OWNER_CUSTOM,
                     'without_default' => true,
@@ -97,6 +98,7 @@ class OroSEOBundleInstaller implements Installation, ExtendExtensionAwareInterfa
                 ],
                 'form' => ['is_enabled' => false],
                 'view' => ['is_displayable' => false],
+                'importexport' => ['excluded' => true],
             ]
         );
     }
