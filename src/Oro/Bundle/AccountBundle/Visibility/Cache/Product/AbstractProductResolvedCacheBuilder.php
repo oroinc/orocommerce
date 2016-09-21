f4fda397a41018da60c5236099233382931814fe
Bemza Viacheslav <vbemza@magecore.com>
2016-09-21 16:07:30 +0300

    BB-4523: call product reindexation in product visbility cache builders - add AbstractProductResolvedCacheBuilder for triggering product reindexation

---
+++ src/Oro/Bundle/AccountBundle/Visibility/Cache/Product/AbstractProductResolvedCacheBuilder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Oro\Bundle\AccountBundle\Visibility\Cache\Product;
+
+use Doctrine\Common\Util\ClassUtils;
+use Oro\Bundle\ProductBundle\Entity\Product;
+use Oro\Bundle\WebsiteBundle\Entity\Website;
+use Oro\Bundle\WebsiteSearchBundle\Event\ReindexationTriggerEvent;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+abstract class AbstractProductResolvedCacheBuilder extends AbstractResolvedCacheBuilder
+{
+    /**
+     * @var EventDispatcherInterface
+     */
+    protected $eventDispatcher;
+
+    /**
+     * @param EventDispatcherInterface $eventDispatcher
+     */
+    public function setEventDispatcher(EventDispatcherInterface $eventDispatcher)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+    /**
+     * @param Product $product
+     * @param Website $website
+     */
+    protected function triggerProductReindexation(Product $product, Website $website)
+    {
+        $event = new ReindexationTriggerEvent(
+            ClassUtils::getClass($product),
+            $website->getId(),
+            [$product->getId()],
+            false
+        );
+        $this->eventDispatcher->dispatch(ReindexationTriggerEvent::EVENT_NAME, $event);
+    }
+}
