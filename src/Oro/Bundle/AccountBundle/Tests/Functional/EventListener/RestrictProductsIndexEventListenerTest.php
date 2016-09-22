e2e21642af86bca689480ffc153d2aea50e03bce
Vitalii Yeromenko <vyeromenko@magecore.com>
2016-09-22 14:04:19 +0300

    BB-4466: Add and fix tests - setting concrete value for the config visibility in the functional test

--- src/Oro/Bundle/AccountBundle/Tests/Functional/EventListener/RestrictProductsIndexEventListenerTest.php
+++ src/Oro/Bundle/AccountBundle/Tests/Functional/EventListener/RestrictProductsIndexEventListenerTest.php
@@ -46,6 +46,10 @@ class RestrictProductsIndexEventListenerTest extends AbstractSearchWebTestCase
 
     public function testRestrictIndexEntityEventListener()
     {
+        $configManager = $this->getContainer()->get('oro_config.global');
+        $previousConfigVisibility = $configManager->get('oro_account.product_visibility');
+        $configManager->set('oro_account.product_visibility', 'visible');
+
         $indexer = $this->getContainer()->get('oro_website_search.indexer');
         $searchEngine = $this->getContainer()->get('oro_website_search.engine');
         $indexer->reindex(Product::class, [AbstractIndexer::CONTEXT_WEBSITE_ID_KEY => $this->getDefaultWebsiteId()]);
@@ -65,6 +69,8 @@ class RestrictProductsIndexEventListenerTest extends AbstractSearchWebTestCase
         $this->assertEquals('product.6', $values[3]->getRecordTitle());
         $this->assertEquals('product.7', $values[4]->getRecordTitle());
         $this->assertEquals('product.8', $values[5]->getRecordTitle());
+
+        $configManager->set('oro_account.product_visibility', $previousConfigVisibility);
     }
 
     /**
