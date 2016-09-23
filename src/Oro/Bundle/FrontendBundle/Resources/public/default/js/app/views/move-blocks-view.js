43905136f3dbbc2f0bfa4d1795acf30acba13cdb
Valerii Yustyniuk <valeriy.yustunyk@gmail.com>
2016-09-23 19:29:44 +0300

    BB-4423: Move sign in/sign out menu to sticky panel for mobile devices create prototype for move-blocks-view

--- src/Oro/Bundle/FrontendBundle/Resources/public/default/js/app/views/move-blocks-view.js
+++ src/Oro/Bundle/FrontendBundle/Resources/public/default/js/app/views/move-blocks-view.js
@@ -11,7 +11,7 @@ define(function(require) {
         autoRender: true,
 
         options: {
-            scrollTimeout: 25,
+            resizeTimeout: 25,
             layoutTimeout: 40
         },
 
@@ -25,7 +25,8 @@ define(function(require) {
 
             MoveBlocksView.__super__.initialize.apply(this, arguments);
 
-            this.$document = $(document);
+            this.$window = $(window);
+            this.elements = null;
         },
 
         /**
@@ -34,9 +35,9 @@ define(function(require) {
         delegateEvents: function() {
             MoveBlocksView.__super__.delegateEvents.apply(this, arguments);
 
-            this.$document.on(
+            this.$window.on(
                 'resize' + this.eventNamespace(),
-                _.debounce(_.bind(this.onResize, this), this.options.scrollTimeout)
+                _.debounce(_.bind(this.onResize, this), this.options.resizeTimeout)
             );
 
             mediator.on('layout:reposition',  _.debounce(this.onResize, this.options.layoutTimeout), this);
@@ -48,7 +49,7 @@ define(function(require) {
          * @inheritDoc
          */
         undelegateEvents: function() {
-            this.$document.off(this.eventNamespace());
+            this.$window.off(this.eventNamespace());
             mediator.off(null, null, this);
 
             return MoveBlocksView.__super__.undelegateEvents.apply(this, arguments);
@@ -58,6 +59,7 @@ define(function(require) {
          * @inheritDoc
          */
         render: function() {
+            this.collectElements();
             return this;
         },
 
@@ -81,17 +83,46 @@ define(function(require) {
         },
 
         onResize: function() {
-            this.checkResponsive();
+            var self = this;
+            var windowSize = this.$window.outerWidth();
+
+            if (!this.$elements.length) {
+                return ;
+            }
+
+            $.each(this.$elements, function(index, el) {
+                var $el = $(el);
+                var targetOptions = self.checkTargetResponsive(windowSize, $el.data('responsiveOptions')) || 0;
+
+                (windowSize < targetOptions.breakpoint ? $(targetOptions.moveTo) : $el.data('originalPosition')).append($el);
+            });
         },
 
-        checkResponsive: function(breakpoint, breakpoints, respondToWidth) {
-            for (breakpoint in breakpoints) {
-                if (breakpoints.hasOwnProperty(breakpoint)) {
-                    if (respondToWidth < breakpoints[breakpoint]) {
-                        this.targetBreakpoint = _.breakpoints[breakpoint];
-                    }
+        checkTargetResponsive: function( windowSize, responsiveOptions) {
+            for (var i = 0; i <= responsiveOptions.length -1 ; i++) {
+                if (windowSize < responsiveOptions[i].breakpoint ) {
+                    this.targetBreakpoint = responsiveOptions[i].breakpoint;
+                    return responsiveOptions[i];
                 }
             }
+        },
+
+        createPlaceholder: function() {
+            return $('<div/>').addClass(this.options.placeholderClass);
+        },
+
+        collectElements: function() {
+            this.$elements = $('[data-move-block]');
+
+            if (!this.$elements.length) {
+                return ;
+            }
+
+            $.each(this.$elements, function(index, element) {
+                var $element = $(element);
+                $element.data('originalPosition', $element.parent());
+                $element.data('responsiveOptions', $element.data('move-options').responsive || []);
+            });
         }
     });
 
