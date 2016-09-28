a419d6b0bde9a39179e2b2c25b4fe58045846053
Boldizsár Zeyk Zsombor <zboldizsar@pitechplus.com>
2016-09-28 14:06:41 +0300

    BB-4636 Add limits to warehouse system config

--- src/Oro/Bundle/WarehouseBundle/DependencyInjection/Configuration.php
+++ src/Oro/Bundle/WarehouseBundle/DependencyInjection/Configuration.php
@@ -11,6 +11,9 @@ class Configuration implements ConfigurationInterface
 {
     const ENABLED_WAREHOUSES = 'enabled_warehouses';
     const MANAGE_INVENTORY = 'manage_inventory';
+    const MINIMUM_QUANTITY_TO_ORDER = 'minimum_quantity_to_order';
+    const MAXIMUM_QUANTITY_TO_ORDER = 'maximum_quantity_to_order';
+    const DEFAULT_MAXIMUM_QUANTITY_TO_ORDER = 100000;
 
     /**
      * {@inheritDoc}
@@ -24,6 +27,11 @@ class Configuration implements ConfigurationInterface
             [
                 self::ENABLED_WAREHOUSES => ['type' => 'array', 'value' => []],
                 self::MANAGE_INVENTORY => ['type' => 'boolean', 'value' => false],
+                self::MINIMUM_QUANTITY_TO_ORDER => ['type' => 'integer', 'value' => null],
+                self::MAXIMUM_QUANTITY_TO_ORDER => [
+                    'type' => 'integer',
+                    'value' => self::DEFAULT_MAXIMUM_QUANTITY_TO_ORDER,
+                ],
             ]
         );
 
