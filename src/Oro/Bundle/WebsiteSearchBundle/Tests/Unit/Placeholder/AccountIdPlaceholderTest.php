b9ff65f4f8f0cd8868b9548bb8cf2d8cfbcd61ce
Igor Galaev <igalayev@magecore.com>
2016-09-29 18:13:01 +0300

    BB-4522: Create listener to mix product visibility information - add AccountIdPlaceholder - add and fix tests

---
+++ src/Oro/Bundle/WebsiteSearchBundle/Tests/Unit/Placeholder/AccountIdPlaceholderTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Oro\Bundle\WebsiteSearchBundle\Tests\Unit\Placeholder;
+
+use Oro\Bundle\AccountBundle\Entity\AccountUser;
+use Oro\Bundle\WebsiteSearchBundle\Placeholder\AccountIdPlaceholder;
+
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class AccountIdPlaceholderTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var AccountIdPlaceholder */
+    private $placeholder;
+
+    /** @var TokenStorageInterface|\PHPUnit_Framework_MockObject_MockObject */
+    private $tokenStorage;
+
+    protected function setUp()
+    {
+        $this->tokenStorage = $this->getMockBuilder(TokenStorageInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->placeholder = new AccountIdPlaceholder($this->tokenStorage);
+    }
+
+    protected function tearDown()
+    {
+        unset($this->placeholder, $this->localizationManager);
+    }
+
+    public function testGetPlaceholder()
+    {
+        $this->assertInternalType('string', $this->placeholder->getPlaceholder());
+        $this->assertEquals('ACCOUNT_ID', $this->placeholder->getPlaceholder());
+    }
+
+    public function testGetValueWhenTokenIsNull()
+    {
+        $this->tokenStorage
+            ->expects($this->once())
+            ->method('getToken')
+            ->willReturn(null);
+
+        $this->assertNull($this->placeholder->getValue());
+    }
+
+    public function testGetValueWhenAccountUserIsNotAccountUser()
+    {
+        $token = $this->getMock(TokenInterface::class);
+        $token
+            ->expects($this->once())
+            ->method('getUser')
+            ->willReturn('Anonymous');
+
+        $this->tokenStorage
+            ->expects($this->once())
+            ->method('getToken')
+            ->willReturn($token);
+
+        $this->assertNull($this->placeholder->getValue());
+    }
+
+    public function testGetValueWhenAccountUserIsGiven()
+    {
+        $accountUserId = 7;
+        $accountUser = $this->getMockBuilder(AccountUser::class)
+            ->getMock();
+
+        $accountUser
+            ->expects($this->once())
+            ->method('getId')
+            ->willReturn($accountUserId);
+
+        $token = $this->getMock(TokenInterface::class);
+        $token
+            ->expects($this->once())
+            ->method('getUser')
+            ->willReturn($accountUser);
+
+        $this->tokenStorage
+            ->expects($this->once())
+            ->method('getToken')
+            ->willReturn($token);
+
+        $this->assertEquals($accountUserId, $this->placeholder->getValue());
+    }
+
+    public function testReplace()
+    {
+        $this->assertEquals('visibility_account_1', $this->placeholder->replace('visibility_account_ACCOUNT_ID', 1));
+    }
+}
