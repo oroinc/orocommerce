c7c2e938789ade46f1a64a48f2f331f89627dac0
Dragos Andries <dandries@pitechnologies.ro>
2016-10-03 14:07:37 +0300

    BB-4438: Update Functional Tests - update normalizers unit tests

--- src/Oro/Bundle/WarehouseBundle/Tests/Unit/ImportExport/Normalizer/WarehouseInventoryLevelNormalizerTest.php
+++ src/Oro/Bundle/WarehouseBundle/Tests/Unit/ImportExport/Normalizer/WarehouseInventoryLevelNormalizerTest.php
@@ -2,61 +2,19 @@
 
 namespace Oro\Bundle\WarehouseBundle\Tests\Unit\ImportExport\Normalizer;
 
-use Oro\Bundle\ProductBundle\Entity\ProductUnit;
-use Oro\Bundle\ProductBundle\Entity\ProductUnitPrecision;
-use Oro\Bundle\ProductBundle\Formatter\ProductUnitLabelFormatter;
-use Oro\Bundle\ProductBundle\Rounding\QuantityRoundingService;
-use Oro\Bundle\WarehouseProBundle\Entity\Warehouse;
 use Oro\Bundle\WarehouseBundle\Entity\WarehouseInventoryLevel;
-use Oro\Bundle\WarehouseBundle\ImportExport\Serializer\WarehouseInventoryLevelNormalizer;
 
-class WarehouseInventoryLevelNormalizerTest extends \PHPUnit_Framework_TestCase
+class WarehouseInventoryLevelNormalizerTest extends BaseInventoryLevelNormalizerTestCase
 {
-    /**
-     * @var WarehouseInventoryLevelNormalizer
-     */
-    protected $warehouseInventoryLevelNormalizer;
-
-    /** @var \PHPUnit_Framework_MockObject_MockObject|ProductUnitLabelFormatter */
-    protected $formatter;
-
-    /** @var  \PHPUnit_Framework_MockObject_MockObject|QuantityRoundingService */
-    protected $roundingService;
-
-    protected function setUp()
-    {
-        $this->formatter = $this->getMockBuilder(ProductUnitLabelFormatter::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->roundingService = $this->getMockBuilder(QuantityRoundingService::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->warehouseInventoryLevelNormalizer = new WarehouseInventoryLevelNormalizer(
-            $this->formatter,
-            $this->roundingService
-        );
-    }
 
     /**
      * @dataProvider getNormalizerData
      * @param int $quantity
-     * @param string $wareHouseName
      * @param string $productUnitCode
      */
-    public function testNormalizeShouldGenerateCorrectArray($quantity, $wareHouseName, $productUnitCode)
+    public function testNormalizeShouldGenerateCorrectArray($quantity, $productUnitCode)
     {
-        $object = new WarehouseInventoryLevel();
-        $object->setQuantity($quantity);
-
-        $wareHouse = new Warehouse();
-        $wareHouse->setName($wareHouseName);
-        $object->setWarehouse($wareHouse);
-
-        $unitPrecision = new ProductUnitPrecision();
-        $productUnit = new ProductUnit();
-        $productUnit->setCode($productUnitCode);
-        $unitPrecision->setUnit($productUnit);
-        $object->setProductUnitPrecision($unitPrecision);
+        $object = $this->getInventoryLevelEntity($quantity, $productUnitCode);
         $this->formatter->expects($this->any())
             ->method('format')
             ->willReturn('testCode');
@@ -67,14 +25,13 @@ class WarehouseInventoryLevelNormalizerTest extends \PHPUnit_Framework_TestCase
         $results = $this->warehouseInventoryLevelNormalizer->normalize($object);
         $this->assertArrayHasKey('warehouse', $results);
         $this->assertEquals('testCode', $results['productUnitPrecision']['unit']['code']);
-        $this->assertEquals('testName', $results['warehouse']['name']);
         $this->assertEquals($object->getQuantity(), $results['quantity']);
     }
 
     public function getNormalizerData()
     {
         return [
-            [5, 'testName', 'testCode']
+            [5, 'testCode']
         ];
     }
 
