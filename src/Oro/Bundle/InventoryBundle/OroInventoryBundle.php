1b34ae23c75ce0f678e4c6de7988d76b5d0f209d
Dragos Andries <dandries@pitechnologies.ro>
2016-10-04 12:10:30 +0300

    BB-4430: added migration for inventory entity renaming

--- src/Oro/Bundle/InventoryBundle/OroInventoryBundle.php
+++ src/Oro/Bundle/InventoryBundle/OroInventoryBundle.php
@@ -2,8 +2,20 @@
 
 namespace Oro\Bundle\InventoryBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+use Oro\Bundle\InventoryBundle\DependencyInjection\Compiler\InventoryLevelMigrationPass;
 
 class OroInventoryBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new InventoryLevelMigrationPass());
+    }
 }
