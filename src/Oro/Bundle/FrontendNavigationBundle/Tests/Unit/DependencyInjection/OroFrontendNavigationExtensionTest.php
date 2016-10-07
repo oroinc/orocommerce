36f06de0b7f783a45509f3a5ad5bfdac44450a11
Vladimir Seniuk <vseniuk@magecore.com>
2016-10-07 14:58:54 +0300

    BB-4782: Implement Website scope in commerce-enterprise - created WebsiteMenuBundle - added WebsiteOwnershipProvider

--- src/Oro/Bundle/FrontendNavigationBundle/Tests/Unit/DependencyInjection/OroFrontendNavigationExtensionTest.php
+++ src/Oro/Bundle/FrontendNavigationBundle/Tests/Unit/DependencyInjection/OroFrontendNavigationExtensionTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Oro\Bundle\CatalogBundle\Tests\Unit\DependencyInjection;
+namespace Oro\Bundle\FrontendNavigationBundle\Tests\Unit\DependencyInjection;
 
 use Oro\Bundle\TestFrameworkBundle\Test\DependencyInjection\ExtensionTestCase;
 use Oro\Bundle\FrontendNavigationBundle\DependencyInjection\OroFrontendNavigationExtension;
