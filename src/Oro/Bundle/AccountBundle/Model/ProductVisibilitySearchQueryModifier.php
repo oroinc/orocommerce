1b2f4ee338aa219a611d8fc88209e03dd0345e27
Ilya Antipenko <iantypenko@oroinc.com>
2016-10-13 12:53:04 +0200

    BB-4508: Add “is null” operator to search index  - fix TODOs

--- src/Oro/Bundle/AccountBundle/Model/ProductVisibilitySearchQueryModifier.php
+++ src/Oro/Bundle/AccountBundle/Model/ProductVisibilitySearchQueryModifier.php
@@ -68,11 +68,10 @@ class ProductVisibilitySearchQueryModifier
 
             $defaultField = $this->completeFieldName(ProductVisibilityIndexer::FIELD_IS_VISIBLE_BY_DEFAULT);
 
-            //TODO: Replace isNull operator after BB-4508 is finished
             $expression = $exprBuilder->orX(
                 $exprBuilder->andX(
                     $exprBuilder->eq($defaultField, BaseVisibilityResolved::VISIBILITY_VISIBLE),
-                    $exprBuilder->isNull($accountField)
+                    $exprBuilder->notExists($accountField)
                 ),
                 $exprBuilder->andX(
                     $exprBuilder->eq($defaultField, BaseVisibilityResolved::VISIBILITY_HIDDEN),
