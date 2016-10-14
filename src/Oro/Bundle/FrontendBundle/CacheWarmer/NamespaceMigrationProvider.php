d576e21c8cca362ff93f36a2e3a89c7efd21d1ae
Makar Sichevoy <sichevoy@gmail.com>
2016-10-14 20:25:57 +0300

    BAP-12135: Create migration command

---
+++ src/Oro/Bundle/FrontendBundle/CacheWarmer/NamespaceMigrationProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Oro\Bundle\FrontendBundle\CacheWarmer;
+
+use Oro\Bundle\InstallerBundle\CacheWarmer\NamespaceMigrationProviderInterface;
+
+class NamespaceMigrationProvider implements NamespaceMigrationProviderInterface
+{
+    /** @var string[] */
+    protected $additionConfig
+        = [
+            'OroB2B\Bundle\AccountBundle' => 'OroB2B\Bundle\CustomerBundle',
+            'OroB2BAccountBundle'         => 'OroB2BCustomerBundle',
+            'Oro\Bundle\AccountBundle'    => 'Oro\Bundle\CustomerBundle',
+            'OroAccountBundle'            => 'OroCustomerBundle',
+            'OroB2B'                      => 'Oro',
+            'orob2b'                      => 'oro',
+        ];
+
+    /**
+     * (@inheritdoc}
+     */
+    public function getConfig()
+    {
+        return $this->additionConfig;
+    }
+}
