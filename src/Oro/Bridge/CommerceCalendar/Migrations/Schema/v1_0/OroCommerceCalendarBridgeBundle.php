6196fc69f8749e1f185eca35956c0b2fcfc467a9
Balazs Csaba <csaba.balazs@evozon.com>
2016-10-15 17:18:15 +0300

    CRM-6272: Update dev repositories - Moved oro/calendar-bundle into platform's composer.json - Installed every application to update dev.lock to include oro/calendar-bundle as a dependency - Fixed CommerceCalendarBridge installer priorities

--- src/Oro/Bridge/CommerceCalendar/Migrations/Schema/v1_0/OroCommerceCalendarBridgeBundle.php
+++ src/Oro/Bridge/CommerceCalendar/Migrations/Schema/v1_0/OroCommerceCalendarBridgeBundle.php
@@ -11,6 +11,8 @@ use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
 class OroCommerceCalendarBridgeBundle implements Migration, ActivityExtensionAwareInterface
 {
+    const CALENDAR_EVENT_TABLE = 'oro_calendar_event';
+
     /** @var ActivityExtension */
     protected $activityExtension;
 
@@ -38,27 +40,30 @@ class OroCommerceCalendarBridgeBundle implements Migration, ActivityExtensionAwa
      */
     public static function addCalendarActivityAssociations(Schema $schema, ActivityExtension $activityExtension)
     {
-        $legacyAssociationTables = [
-            'oro_account_user' => 'orob2b_account_user',
-            'oro_order' => 'orob2b_order',
-            'oro_rfp_request' => 'orob2b_rfp_request',
-            'oro_sale_quote' => 'orob2b_sale_quote',
-        ];
+        // no create associations only if calendar bundle is installed
+        if ($schema->hasTable(self::CALENDAR_EVENT_TABLE)) {
+            $legacyAssociationTables = [
+                'oro_account_user' => 'orob2b_account_user',
+                'oro_order' => 'orob2b_order',
+                'oro_rfp_request' => 'orob2b_rfp_request',
+                'oro_sale_quote' => 'orob2b_sale_quote',
+            ];
 
-        $associationTables = [
-            'oro_account_user',
-            'oro_order',
-            'oro_rfp_request',
-            'oro_sale_quote',
-        ];
+            $associationTables = [
+                'oro_account_user',
+                'oro_order',
+                'oro_rfp_request',
+                'oro_sale_quote',
+            ];
 
-        foreach ($associationTables as $tableName) {
-            if (!$schema->hasTable($tableName)) {
-                $tableName = $legacyAssociationTables[$tableName];
-            }
-            $associationTableName = $activityExtension->getAssociationTableName('oro_calendar_event', $tableName);
-            if (!$schema->hasTable($associationTableName)) {
-                $activityExtension->addActivityAssociation($schema, 'oro_calendar_event', $tableName);
+            foreach ($associationTables as $tableName) {
+                if (!$schema->hasTable($tableName)) {
+                    $tableName = $legacyAssociationTables[$tableName];
+                }
+                $associationTableName = $activityExtension->getAssociationTableName(self::CALENDAR_EVENT_TABLE, $tableName);
+                if (!$schema->hasTable($associationTableName)) {
+                    $activityExtension->addActivityAssociation($schema, self::CALENDAR_EVENT_TABLE, $tableName);
+                }
             }
         }
     }
