4eb77347dd0a0d40e19a4875d12f774f84824ddb
Makar Sichevoy <sichevoy@gmail.com>
2016-10-16 20:29:50 +0300

    BAP-12105: Create migration for commerce

--- src/Oro/Bundle/CustomerBundle/Migrations/Schema/v1_8/OroAccountBundle.php
+++ src/Oro/Bundle/CustomerBundle/Migrations/Schema/v1_8/OroAccountBundle.php
@@ -179,7 +179,7 @@ class OroAccountBundle implements Migration, RenameExtensionAwareInterface
             RelationType::MANY_TO_ONE
         ));
 
-        $notes->removeForeignKey('fk_oro_note_account_8d93c122_id');
+        $notes->removeForeignKey('FK_BA066CE16127545E');
         $extension->renameColumn($schema, $queries, $notes, 'account_8d93c122_id', 'account_8d1f63b9_id');
         $extension->addForeignKeyConstraint(
             $schema,
@@ -188,8 +188,7 @@ class OroAccountBundle implements Migration, RenameExtensionAwareInterface
             'oro_account',
             ['account_8d1f63b9_id'],
             ['id'],
-            ['onDelete' => 'SET NULL'],
-            'fk_oro_note_account_8d1f63b9_id'
+            ['onDelete' => 'SET NULL']
         );
         $queries->addQuery(new UpdateExtendRelationQuery(
             'Oro\Bundle\NoteBundle\Entity\Note',
