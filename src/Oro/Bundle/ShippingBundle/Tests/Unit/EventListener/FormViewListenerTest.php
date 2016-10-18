b9cd836a73b568a65ed36d4ae3f25a68d97fdfae
Valentyna Shabala <v.shabala@svitla.com>
2016-10-18 22:03:17 +0300

    BB-4772: Remove OrderBundle to ShippingBundle dependency, unit test

--- src/Oro/Bundle/ShippingBundle/Tests/Unit/EventListener/FormViewListenerTest.php
+++ src/Oro/Bundle/ShippingBundle/Tests/Unit/EventListener/FormViewListenerTest.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Oro\Bundle\UIBundle\Event\BeforeListRenderEvent;
 use Oro\Bundle\UIBundle\View\ScrollData;
 use Oro\Component\Testing\Unit\FormViewListenerTestCase;
+use Oro\Bundle\OrderBundle\Entity\Order;
 use Oro\Bundle\ProductBundle\Entity\Product;
 use Oro\Bundle\ShippingBundle\Entity\ProductShippingOptions;
 use Oro\Bundle\ShippingBundle\EventListener\FormViewListener;
@@ -337,6 +338,73 @@ class FormViewListenerTest extends FormViewListenerTestCase
         );
     }
 
+    public function testOnOrderViewWithoutRequest()
+    {
+        $this->requestStack->expects($this->once())->method('getCurrentRequest')->willReturn(null);
+
+        $event = $this->getBeforeListRenderEventMock();
+        $event->expects($this->never())->method('getScrollData');
+
+        $this->listener->onOrderView($event);
+    }
+
+    public function testOnOrderViewWithoutOrder()
+    {
+        $this->requestStack->expects($this->once())->method('getCurrentRequest')->willReturn($this->request);
+        $this->request->expects($this->once())->method('get')->with('id')->willReturn(8);
+
+        $this->doctrineHelper->expects($this->once())
+            ->method('getEntityReference')
+            ->with('OroOrderBundle:Order', 8)
+            ->willReturn(null);
+
+        $event = $this->getBeforeListRenderEventMock();
+        $event->expects($this->never())->method('getScrollData');
+
+        $this->listener->onOrderView($event);
+    }
+
+    public function testOnOrderView()
+    {
+        $this->requestStack->expects($this->once())
+            ->method('getCurrentRequest')
+            ->willReturn($this->request);
+
+        $this->request->expects($this->once())
+            ->method('get')
+            ->with('id')
+            ->willReturn(47);
+
+        $order = new Order();
+
+        $this->doctrineHelper->expects($this->once())
+            ->method('getEntityReference')
+            ->with('OroOrderBundle:Order', 47)
+            ->willReturn($order);
+
+        $renderedHtml = 'rendered_html';
+
+        /** @var \Twig_Environment|\PHPUnit_Framework_MockObject_MockObject $twig */
+        $twig = $this->getMockBuilder('\Twig_Environment')->disableOriginalConstructor()->getMock();
+        $twig->expects($this->once())
+            ->method('render')
+            ->with(
+                'OroShippingBundle:Order:shipping_information_view.html.twig',
+                ['entity' => $order]
+            )
+            ->willReturn($renderedHtml);
+
+        $event = new BeforeListRenderEvent($twig, $this->getScrollData());
+
+        $this->listener->onOrderView($event);
+
+        $scrollData = $event->getScrollData()->getData();
+        $this->assertEquals(
+            [$renderedHtml],
+            $scrollData[ScrollData::DATA_BLOCKS][1][ScrollData::SUB_BLOCKS][0][ScrollData::DATA]
+        );
+    }
+    
     /**
      * @return ScrollData
      */
