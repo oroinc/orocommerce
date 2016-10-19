cb58d0351c59a95be3aac6b6f322c30abc132422
Thomas Parys <thomas.parys@wwsh.io>
2016-10-19 10:20:26 +0200

    BB-4196: Refactor ProductBundle\ComponentProcessor\ComponentProcessorFilter - merging changes with BB-4197

--- src/Oro/Bundle/ProductBundle/Search/ProductRepository.php
+++ src/Oro/Bundle/ProductBundle/Search/ProductRepository.php
@@ -4,22 +4,38 @@ namespace Oro\Bundle\ProductBundle\Search;
 
 use Oro\Bundle\SearchBundle\Query\Criteria\Criteria;
 use Oro\Bundle\SearchBundle\Query\Result\Item;
+use Oro\Bundle\SearchBundle\Query\SearchQueryInterface;
 use Oro\Bundle\WebsiteSearchBundle\Query\WebsiteSearchRepository;
 
 class ProductRepository extends WebsiteSearchRepository
 {
     /**
      * @param array $skus
+     * @return SearchQueryInterface
+     */
+    public function getFilterSkuQuery($skus)
+    {
+        $searchQuery = $this->createQuery();
+
+        // Convert to uppercase for insensitive search in all DB
+        $upperCaseSkus = array_map("strtoupper", $skus);
+
+        $searchQuery->setFrom('product')
+            ->addSelect('sku')
+            ->getCriteria()
+            ->andWhere(Criteria::expr()->contains('sku_uppercase', implode(', ', $upperCaseSkus)));
+
+        return $searchQuery;
+    }
+
+    /**
+     * @param array $skus
      * @return Item[]
      */
     public function searchFilteredBySkus(array $skus)
     {
-        $searchQuery = $this->createQuery();
-        $searchQuery->addSelect('sku');
+        $searchQuery = $this->getFilterSkuQuery($skus);
         $searchQuery->addSelect('title_LOCALIZATION_ID');
-
-        $searchQuery->getQuery()->getCriteria()
-            ->andWhere(Criteria::expr()->contains('sku', implode(', ', $skus)));
 
         return $searchQuery->getResult()->getElements();
     }
