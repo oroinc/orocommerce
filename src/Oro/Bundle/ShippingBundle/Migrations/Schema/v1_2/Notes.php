252796db862bf7dd38bad2b0a8a00fa6930f59f3
Mykola Sedletskyi <m.sedletskyi@svitla.com>
2016-10-19 19:22:36 +0300

    BB-4885: Add notes to Shipping Rules

---
+++ src/Oro/Bundle/ShippingBundle/Migrations/Schema/v1_2/Notes.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Oro\Bundle\ShippingBundle\Migrations\Schema\v1_2;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+use Oro\Bundle\NoteBundle\Migration\Extension\NoteExtension;
+use Oro\Bundle\NoteBundle\Migration\Extension\NoteExtensionAwareInterface;
+
+class Notes implements Migration, NoteExtensionAwareInterface
+{
+    /** @var NoteExtension */
+    protected $noteExtension;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setNoteExtension(NoteExtension $noteExtension)
+    {
+        $this->noteExtension = $noteExtension;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $this->noteExtension->addNoteAssociation($schema, 'oro_shipping_rule');
+    }
+}
