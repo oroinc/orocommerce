33093c23280a0a529fb12325abadb5a11823cb1e
Daniel Nahrebecki <dnahrebecki@oroinc.com>
2016-10-21 12:00:28 +0200

    BB-4192 Add support for new event to AccountBundle\EventListener\ProductDBQueryRestrictionEventListener

--- src/Oro/Bundle/CustomerBundle/EventListener/ProductDBQueryRestrictionEventListener.php
+++ src/Oro/Bundle/CustomerBundle/EventListener/ProductDBQueryRestrictionEventListener.php
@@ -5,29 +5,28 @@ namespace Oro\Bundle\CustomerBundle\EventListener;
 use Oro\Bundle\CustomerBundle\Model\ProductVisibilityQueryBuilderModifier;
 use Oro\Bundle\FrontendBundle\Request\FrontendHelper;
 use Oro\Bundle\ProductBundle\Event\ProductDBQueryRestrictionEvent;
+use Oro\Bundle\SearchBundle\Query\Modifier\QueryBuilderModifierInterface;
 
 class ProductDBQueryRestrictionEventListener
 {
     /**
      * @var FrontendHelper
      */
-    protected $frontendHelper;
+    private $frontendHelper;
 
     /**
-     * @var ProductVisibilityQueryBuilderModifier
+     * @var QueryBuilderModifierInterface|ProductVisibilityQueryBuilderModifier
      */
-    protected $modifier;
+    private $modifier;
 
     /**
-     * @param FrontendHelper $frontendHelper
-     * @param ProductVisibilityQueryBuilderModifier $modifier
+     * @param FrontendHelper                $frontendHelper
+     * @param QueryBuilderModifierInterface $modifier
      */
-    public function __construct(
-        FrontendHelper $frontendHelper,
-        ProductVisibilityQueryBuilderModifier $modifier
-    ) {
+    public function __construct(FrontendHelper $frontendHelper, QueryBuilderModifierInterface $modifier)
+    {
         $this->frontendHelper = $frontendHelper;
-        $this->modifier = $modifier;
+        $this->modifier       = $modifier;
     }
 
     /**
