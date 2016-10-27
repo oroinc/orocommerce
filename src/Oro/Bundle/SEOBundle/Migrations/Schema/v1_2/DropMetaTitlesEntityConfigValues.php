652581db1cc33038cd4e96efb4a974cb8dfbb489
Vitaliy Berdylo <vberdylo@magecore.com>
2016-10-27 16:01:04 +0300

    BB-5040: Add SEO Form Extension for web catalog node form - change data in oro_entity_config table

--- src/Oro/Bundle/SEOBundle/Migrations/Schema/v1_2/DropMetaTitlesEntityConfigValues.php
+++ src/Oro/Bundle/SEOBundle/Migrations/Schema/v1_2/DropMetaTitlesEntityConfigValues.php
@@ -5,7 +5,6 @@ namespace Oro\Bundle\SEOBundle\Migrations\Schema\v1_2;
 use Doctrine\DBAL\Schema\Schema;
 
 use Oro\Bundle\MigrationBundle\Migration\Migration;
-use Oro\Bundle\MigrationBundle\Migration\ParametrizedSqlMigrationQuery;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
 class DropMetaTitlesEntityConfigValues implements Migration
@@ -15,28 +14,6 @@ class DropMetaTitlesEntityConfigValues implements Migration
      */
     public function up(Schema $schema, QueryBag $queries)
     {
-        $configIndexValueSql = <<<'SQL'
-DELETE FROM oro_entity_config_index_value
-WHERE field_id  = (
-    SELECT id FROM oro_entity_config_field
-    WHERE entity_id = (SELECT id FROM oro_entity_config WHERE class_name = :class)
-    AND field_name = :field_name
-)
-SQL;
-
-        $configFieldSql = <<<'SQL'
-DELETE FROM oro_entity_config_field
-WHERE entity_id = (SELECT id FROM oro_entity_config WHERE class_name = :class)
-AND field_name = :field_name
-SQL;
-
-        $queries->addPostQuery(new ParametrizedSqlMigrationQuery(
-            $configIndexValueSql,
-            ['class' => 'Oro\Bundle\ProductBundle\Entity\Product', 'field_name' => 'metaTitle']
-        ));
-        $queries->addPostQuery(new ParametrizedSqlMigrationQuery(
-            $configFieldSql,
-            ['class' => 'Oro\Bundle\ProductBundle\Entity\Product', 'field_name' => 'metaTitle']
-        ));
+        $queries->addQuery(new DropMetaTitlesEntityConfigValuesQuery());
     }
 }
