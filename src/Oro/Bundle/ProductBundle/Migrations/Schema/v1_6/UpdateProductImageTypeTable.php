58dc80cf0781801359bf8c3fb5c2b736d20d9bb4
Arsen Aliev <aliev.ars@gmail.com>
2016-11-02 17:27:40 +0200

    BB-5224 Missing cascade option in FK on table oro_product_image_type  - fix problem with different FK name (before & after prefix renaming from orob2b to oro)

--- src/Oro/Bundle/ProductBundle/Migrations/Schema/v1_6/UpdateProductImageTypeTable.php
+++ src/Oro/Bundle/ProductBundle/Migrations/Schema/v1_6/UpdateProductImageTypeTable.php
@@ -2,6 +2,7 @@
 
 namespace Oro\Bundle\ProductBundle\Migrations\Schema\v1_6;
 
+use Doctrine\DBAL\Schema\ForeignKeyConstraint;
 use Doctrine\DBAL\Schema\Schema;
 
 use Oro\Bundle\MigrationBundle\Migration\Migration;
@@ -15,7 +16,12 @@ class UpdateProductImageTypeTable implements Migration
     public function up(Schema $schema, QueryBag $queries)
     {
         $table = $schema->getTable('oro_product_image_type');
-        $table->removeForeignKey('FK_3EBD8F1DF6154FFA');
+
+        $foreignKeys = $table->getForeignKeys();
+        /** @var ForeignKeyConstraint $oldForeignKey */
+        $oldForeignKey = reset($foreignKeys);
+        $table->removeForeignKey($oldForeignKey->getName());
+
         $table->addForeignKeyConstraint(
             $schema->getTable('oro_product_image'),
             ['product_image_id'],
