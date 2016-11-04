49535a89a28b79f9f3e927e12011a94201d39d3a
Daniel Nahrebecki <dnahrebecki@oroinc.com>
2016-11-04 09:59:03 +0100

    BB-5266 Investigate failing tests in BB-4032

--- src/Oro/Bundle/SaleBundle/Tests/Functional/Controller/Frontend/ProductControllerTest.php
+++ src/Oro/Bundle/SaleBundle/Tests/Functional/Controller/Frontend/ProductControllerTest.php
@@ -54,9 +54,9 @@ class ProductControllerTest extends WebTestCase
     public function viewDataProvider()
     {
         return [
-            'unit without prices'=> ['product'=> LoadProductData::PRODUCT_2, 'contains'=> 'Request A Quote'],
-            'unit with empty price' => ['product'=> LoadProductData::PRODUCT_4, 'contains'=> 'USD 200.50'],
-            'unit with not empty price'=> ['product'=> LoadProductData::PRODUCT_5, 'contains'=> 'USD 0.00']
+            'unit without prices'       => ['product' => LoadProductData::PRODUCT_2, 'contains' => 'Request A Quote'],
+            'unit with empty price'     => ['product' => LoadProductData::PRODUCT_6, 'contains' => 'USD 200.50'],
+            'unit with not empty price' => ['product' => LoadProductData::PRODUCT_7, 'contains' => 'USD 0.00']
         ];
     }
 
