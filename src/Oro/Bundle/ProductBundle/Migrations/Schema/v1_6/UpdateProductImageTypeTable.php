97b9472a16ac3ba128078c0c82268776b7179a47
Arsen Aliev <aliev.ars@gmail.com>
2016-11-05 22:41:07 +0200

    BB-5224 Missing cascade option in FK on table oro_product_image_type  - process comment  - iterate over all foreign keys to find appropriate

--- src/Oro/Bundle/ProductBundle/Migrations/Schema/v1_6/UpdateProductImageTypeTable.php
+++ src/Oro/Bundle/ProductBundle/Migrations/Schema/v1_6/UpdateProductImageTypeTable.php
@@ -2,7 +2,6 @@
 
 namespace Oro\Bundle\ProductBundle\Migrations\Schema\v1_6;
 
-use Doctrine\DBAL\Schema\ForeignKeyConstraint;
 use Doctrine\DBAL\Schema\Schema;
 
 use Oro\Bundle\MigrationBundle\Migration\Migration;
@@ -10,22 +9,30 @@ use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
 class UpdateProductImageTypeTable implements Migration
 {
+    const PRODUCT_IMAGE_TYPE_TABLE = 'oro_product_image_type';
+    const PRODUCT_IMAGE_TABLE = 'oro_product_image';
+    const FOREIGN_COLUMN = 'id';
+    const LOCAL_COLUMN = 'product_image_id';
+
     /**
      * {@inheritdoc}
      */
     public function up(Schema $schema, QueryBag $queries)
     {
-        $table = $schema->getTable('oro_product_image_type');
+        $table = $schema->getTable(self::PRODUCT_IMAGE_TYPE_TABLE);
 
-        $foreignKeys = $table->getForeignKeys();
-        /** @var ForeignKeyConstraint $oldForeignKey */
-        $oldForeignKey = reset($foreignKeys);
-        $table->removeForeignKey($oldForeignKey->getName());
+        foreach ($table->getForeignKeys() as $foreignKey) {
+            if ($foreignKey->getForeignTableName() === self::PRODUCT_IMAGE_TABLE &&
+                $foreignKey->getLocalColumns() === [self::LOCAL_COLUMN]
+            ) {
+                $table->removeForeignKey($foreignKey->getName());
+            }
+        }
 
         $table->addForeignKeyConstraint(
-            $schema->getTable('oro_product_image'),
-            ['product_image_id'],
-            ['id'],
+            $schema->getTable(self::PRODUCT_IMAGE_TABLE),
+            [self::LOCAL_COLUMN],
+            [self::FOREIGN_COLUMN],
             ['onDelete' => 'CASCADE', 'onUpdate' => null]
         );
     }
