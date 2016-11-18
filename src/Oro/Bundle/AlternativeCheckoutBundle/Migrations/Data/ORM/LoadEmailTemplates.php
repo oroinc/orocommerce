0c5871d744b838de323f747f534d95a7f8ea3afd
Mykhailo Sulyma <msulima@magecore.com>
2016-11-18 19:56:16 +0200

    BB-5389: Update Functional Tests

---
+++ src/Oro/Bundle/AlternativeCheckoutBundle/Migrations/Data/ORM/LoadEmailTemplates.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Oro\Bundle\CheckoutBundle\Migrations\Data\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+use Oro\Bundle\WorkflowBundle\Entity\WorkflowDefinition;
+
+class LoadEmailTemplates extends AbstractFixture implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        /** @var WorkflowDefinition $workflowDefinition */
+        $workflowDefinition = $manager->getRepository('OroWorkflowBundle:WorkflowDefinition')
+            ->find('b2b_flow_alternative_checkout');
+
+        if ($workflowDefinition && $workflowDefinition->isActive()) {
+            $this->container->get('oro_workflow.manager')->deactivateWorkflow($workflowDefinition->getName());
+        }
+    }
+}
