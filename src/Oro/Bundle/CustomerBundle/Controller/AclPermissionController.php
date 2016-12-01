cd6ecaadab850a2fd523566561b865cb5e675428
Vova Soroka <vsoroka@magecore.com>
2016-12-01 18:26:22 +0200

    BB-6029: Appy ACL to transition instead of transition definition

--- src/Oro/Bundle/CustomerBundle/Controller/AclPermissionController.php
+++ src/Oro/Bundle/CustomerBundle/Controller/AclPermissionController.php
@@ -15,7 +15,7 @@ class AclPermissionController extends Controller
      * @Route(
      *      "/acl-access-levels/{oid}/{permission}",
      *      name="oro_account_acl_access_levels",
-     *      requirements={"oid"="[\w]+:[\w\:\(\)]+", "permission"="[\w/]+"},
+     *      requirements={"oid"="[\w]+:[\w\:\(\)\|]+", "permission"="[\w/]+"},
      *      defaults={"_format"="json", "permission"=null}
      * )
      * @Template
--- src/Oro/Bundle/CustomerBundle/Tests/Unit/EventListener/AccountRolePageListenerTest.php
+++ src/Oro/Bundle/CustomerBundle/Tests/Unit/EventListener/AccountRolePageListenerTest.php
@@ -20,11 +20,10 @@ class AccountRolePageListenerTest extends \PHPUnit_Framework_TestCase
         $translator = $this->getMock('Symfony\Component\Translation\TranslatorInterface');
         $translator->expects($this->any())
             ->method('trans')
-            ->willReturnCallback(
-                function ($message) {
-                    return $message . '_trans';
-                }
-            );
+            ->willReturnCallback(function ($value) {
+                return 'translated: ' . $value;
+            });
+
         $this->listener = new AccountRolePageListener($translator);
     }
 
@@ -122,7 +121,10 @@ class AccountRolePageListenerTest extends \PHPUnit_Framework_TestCase
         $data = $event->getFormData();
         $this->assertCount(4, $data['dataBlocks']);
         $workflowBlock = $data['dataBlocks'][2];
-        $this->assertEquals('oro.workflow.workflowdefinition.entity_plural_label_trans', $workflowBlock['title']);
+        $this->assertEquals(
+            'translated: oro.workflow.workflowdefinition.entity_plural_label',
+            $workflowBlock['title']
+        );
         $this->assertEquals(
             [['data' => [$renderedHtml]]],
             $workflowBlock['subblocks']
@@ -202,7 +204,10 @@ class AccountRolePageListenerTest extends \PHPUnit_Framework_TestCase
         $data = $event->getData();
         $this->assertCount(4, $data['dataBlocks']);
         $workflowBlock = $data['dataBlocks'][2];
-        $this->assertEquals('oro.workflow.workflowdefinition.entity_plural_label_trans', $workflowBlock['title']);
+        $this->assertEquals(
+            'translated: oro.workflow.workflowdefinition.entity_plural_label',
+            $workflowBlock['title']
+        );
         $this->assertEquals(
             [['data' => [$renderedHtml]]],
             $workflowBlock['subblocks']
