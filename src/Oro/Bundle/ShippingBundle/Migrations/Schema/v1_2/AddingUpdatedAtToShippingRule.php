4a4339f8a4a415099e835db2e0f03702d6341130
Volodymyr Denchyk <devit_dvv@ukr.net>
2016-12-02 13:50:26 +0200

    BB-5980: Can't upgrade from beta4 cause Migration Error in ShippingBundle

--- src/Oro/Bundle/ShippingBundle/Migrations/Schema/v1_2/AddingUpdatedAtToShippingRule.php
+++ src/Oro/Bundle/ShippingBundle/Migrations/Schema/v1_2/AddingUpdatedAtToShippingRule.php
@@ -14,8 +14,8 @@ class AddingUpdatedAtToShippingRule implements Migration
     public function up(Schema $schema, QueryBag $queries)
     {
         $table = $schema->getTable('oro_shipping_rule');
-        $table->addColumn('created_at', 'datetime', []);
-        $table->addColumn('updated_at', 'datetime', []);
+        $table->addColumn('created_at', 'datetime', ['notnull' => false]);
+        $table->addColumn('updated_at', 'datetime', ['notnull' => false]);
         $table->addIndex(['created_at'], 'idx_oro_shipping_rule_created_at', []);
         $table->addIndex(['updated_at'], 'idx_oro_shipping_rule_updated_at', []);
     }
