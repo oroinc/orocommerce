9658b75f930dc0378747048b8fee2d59b8f9b10e
Sam Ivichuk <sivichuk@oroinc.com>
2016-12-12 21:39:31 +0200

    BB-6318: Create Rule entity in RuleBundle  - added Rule entity with test  - added migration for oro_rule table  - added installer

---
+++ src/Oro/Bundle/RuleBundle/Migrations/Schema/v1_0/OroRuleBundle.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Oro\Bundle\RuleBundle\Migrations\Schema\v1_0;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class OroRuleBundle implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $this->createOroRuleTable($schema);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    protected function createOroRuleTable(Schema $schema)
+    {
+        $table = $schema->createTable('oro_rule');
+
+        $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('name', 'text', []);
+        $table->addColumn('enabled', 'boolean', ['default' => true]);
+        $table->addColumn('sort_order', 'integer', []);
+        $table->addColumn('stop_processing', 'boolean', ['default' => false]);
+        $table->addColumn('expression', 'text', []);
+        $table->addColumn('created_at', 'datetime', []);
+        $table->addColumn('updated_at', 'datetime', ['notnull' => false]);
+
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['created_at'], 'idx_oro_rule_created_at', []);
+        $table->addIndex(['updated_at'], 'idx_oro_rule_updated_at', []);
+    }
+}
