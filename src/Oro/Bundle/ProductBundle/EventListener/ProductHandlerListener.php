c429e6c704e27ceddab7338270e99c885b3a8964
Ilya Antipenko <iantypenko@oroinc.com>
2016-12-13 17:20:53 +0200

    BB-6057: Product variants improvements  - clear extend fields mentioned in variant fields

--- src/Oro/Bundle/ProductBundle/EventListener/ProductHandlerListener.php
+++ src/Oro/Bundle/ProductBundle/EventListener/ProductHandlerListener.php
@@ -4,9 +4,27 @@ namespace Oro\Bundle\ProductBundle\EventListener;
 
 use Oro\Bundle\FormBundle\Event\FormHandler\AfterFormProcessEvent;
 use Oro\Bundle\ProductBundle\Entity\Product;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\PropertyAccess\Exception\NoSuchPropertyException;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
 
 class ProductHandlerListener
 {
+    /** @var PropertyAccessor */
+    protected $propertyAccessor;
+
+    /** @var LoggerInterface */
+    protected $logger;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(PropertyAccessor $propertyAccessor, LoggerInterface $logger)
+    {
+        $this->propertyAccessor = $propertyAccessor;
+        $this->logger = $logger;
+    }
+
     /**
      * @param AfterFormProcessEvent $event
      */
@@ -18,8 +36,24 @@ class ProductHandlerListener
             return;
         }
 
-        if (!$data->isConfigurable()) {
+        if ($data->isConfigurable()) {
+            $this->clearCustomExtendVariantFields($data);
+        } else {
             $data->getVariantLinks()->clear();
+        }
+    }
+
+    /**
+     * @param Product $product
+     */
+    protected function clearCustomExtendVariantFields(Product $product)
+    {
+        foreach ($product->getVariantFields() as $variantField) {
+            try {
+                $this->propertyAccessor->setValue($product, $variantField, null);
+            } catch (NoSuchPropertyException $e) {
+                $this->logger->warning('Can not clear custom extend variant field', ['exception' => $e]);
+            }
         }
     }
 }
