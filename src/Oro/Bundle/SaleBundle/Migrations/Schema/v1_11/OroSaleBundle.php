325987057807a764d82f951aae2ad7df365459d7
Yauheni Kasenka <ykasenka@oroinc.com>
2016-12-19 18:40:19 +0300

    BB-6212: Creating order from quote by different users creates same checkout - updated migration

--- src/Oro/Bundle/SaleBundle/Migrations/Schema/v1_11/OroSaleBundle.php
+++ src/Oro/Bundle/SaleBundle/Migrations/Schema/v1_11/OroSaleBundle.php
@@ -2,13 +2,18 @@
 
 namespace Oro\Bundle\SaleBundle\Migrations\Schema\v1_11;
 
+use Doctrine\DBAL\Platforms\MySqlPlatform;
 use Doctrine\DBAL\Schema\Schema;
 
+use Oro\Bundle\MigrationBundle\Migration\Extension\DatabasePlatformAwareInterface;
+use Oro\Bundle\MigrationBundle\Migration\Extension\DatabasePlatformAwareTrait;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
-class OroSaleBundle implements Migration
+class OroSaleBundle implements Migration, DatabasePlatformAwareInterface
 {
+    use DatabasePlatformAwareTrait;
+
     /**
      * {@inheritdoc}
      */
@@ -16,6 +21,28 @@ class OroSaleBundle implements Migration
     {
         $this->updateOroSaleQuoteDemandTable($schema);
         $this->addOroSaleQuoteDemandForeignKeys($schema);
+
+        $this->updateAccountData($queries);
+    }
+
+    /**
+     * @param QueryBag $queries
+     */
+    protected function updateAccountData(QueryBag $queries)
+    {
+        if ($this->platform instanceof MySqlPlatform) {
+            $queries->addQuery('UPDATE oro_quote_demand d
+                INNER JOIN oro_checkout_source s ON s.quoteDemand_id = d.id
+                INNER JOIN oro_checkout c ON c.source_id = s.id
+                SET d.account_id = c.account_id, d.account_user_id = c.account_user_id');
+        } else {
+            $queries->addQuery('UPDATE oro_quote_demand d
+                SET account_id = c.account_id, account_user_id = c.account_user_id
+                    FROM oro_checkout_source s INNER JOIN oro_checkout c ON c.source_id = s.id
+                    WHERE s.quoteDemand_id = d.id');
+        }
+
+        $queries->addQuery('DELETE FROM oro_quote_demand WHERE account_id IS NULL AND account_user_id IS NULL');
     }
 
     /**
