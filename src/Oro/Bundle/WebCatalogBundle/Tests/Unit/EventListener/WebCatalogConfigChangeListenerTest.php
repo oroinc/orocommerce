e5601b65fae8a59e66468ee58740eb9387107029
Piotr Kowalczyk <piotr.b.kowalczyk@gmail.com>
2016-12-19 09:19:07 +0100

    BB-6264 Create listeners to run reindexation when user changes system configuration (#5459)
    * BB-6264 added configuration configuration change listener to WebCatalogBundle
    
    * BB-6264 Added MultiWebsiteScopeChangeListener overriding WebcatalogConfigChangeListener

---
+++ src/Oro/Bundle/WebCatalogBundle/Tests/Unit/EventListener/WebCatalogConfigChangeListenerTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Oro\Bundle\WebCatalogBundle\Tests\Unit\EventListener;
+
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+use Oro\Bundle\ConfigBundle\Event\ConfigUpdateEvent;
+use Oro\Bundle\WebCatalogBundle\EventListener\WebCatalogConfigChangeListener;
+use Oro\Bundle\WebsiteSearchBundle\Event\ReindexationRequestEvent;
+
+class WebCatalogConfigChangeListenerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var EventDispatcherInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $dispatcher;
+
+    /**
+     * @var WebCatalogConfigChangeListener
+     */
+    protected $webCatalogConfigChangeListener;
+
+    protected function setUp()
+    {
+        $this->dispatcher = $this->getMock(EventDispatcherInterface::class);
+        $this->webCatalogConfigChangeListener = new WebCatalogConfigChangeListener($this->dispatcher);
+    }
+
+    public function testOnConfigurationUpdate()
+    {
+        /** @var ConfigUpdateEvent|\PHPUnit_Framework_MockObject_MockObject $event **/
+        $event = $this->getMockBuilder(ConfigUpdateEvent::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        
+        $event->method('isChanged')
+            ->with(WebCatalogConfigChangeListener::WEB_CATALOG_CONFIGURATION_NAME)
+            ->willReturn(true);
+
+        $this->dispatcher
+            ->expects($this->once())
+            ->method('dispatch')
+            ->with(ReindexationRequestEvent::EVENT_NAME, new ReindexationRequestEvent());
+        
+        $this->webCatalogConfigChangeListener->onConfigurationUpdate($event);
+    }
+
+    public function testOnOtherConfigurationUpdate()
+    {
+        /** @var ConfigUpdateEvent|\PHPUnit_Framework_MockObject_MockObject $event **/
+        $event = $this->getMockBuilder(ConfigUpdateEvent::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $event->method('isChanged')
+            ->with(WebCatalogConfigChangeListener::WEB_CATALOG_CONFIGURATION_NAME)
+            ->willReturn(false);
+
+        $this->dispatcher
+            ->expects($this->never())
+            ->method('dispatch');
+
+        $this->webCatalogConfigChangeListener->onConfigurationUpdate($event);
+    }
+}
