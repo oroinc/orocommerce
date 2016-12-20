1b21ca6a5626e3c7a243633815a34aaa4edd2377
Ilya Antipenko <iantypenko@oroinc.com>
2016-12-20 19:01:45 +0200

    BB-5999: Product Import > Error during import  - fix order of add/remove product in category listener  - add category import/export test

--- src/Oro/Bundle/CatalogBundle/EventListener/ProductStrategyEventListener.php
+++ src/Oro/Bundle/CatalogBundle/EventListener/ProductStrategyEventListener.php
@@ -2,14 +2,10 @@
 
 namespace Oro\Bundle\CatalogBundle\EventListener;
 
-use Oro\Bundle\CatalogBundle\Entity\Category;
 use Oro\Bundle\ProductBundle\ImportExport\Event\ProductStrategyEvent;
 
 class ProductStrategyEventListener extends AbstractProductImportEventListener
 {
-    /** @var Category[] */
-    protected $categories = [];
-
     /**
      * @param ProductStrategyEvent $event
      */
@@ -23,18 +19,16 @@ class ProductStrategyEventListener extends AbstractProductImportEventListener
         $categoryDefaultTitle = $rawData[self::CATEGORY_KEY];
         $product = $event->getProduct();
 
-        $category = $this->getCategoryByDefaultTitle($categoryDefaultTitle);
-        if ($category) {
-            $category->addProduct($product);
-
-            return;
-        }
-
         if ($product->getId()) {
             $category = $this->getCategoryByProduct($product);
             if ($category) {
                 $category->removeProduct($product);
             }
+        }
+
+        $category = $this->getCategoryByDefaultTitle($categoryDefaultTitle);
+        if ($category) {
+            $category->addProduct($product);
         }
     }
 }
---
+++ src/Oro/Bundle/CatalogBundle/Tests/Functional/ImportExport/ImportExportTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Oro\Bundle\CatalogBundle\Tests\Functional\ImportExport;
+
+use Oro\Bundle\CatalogBundle\Entity\Repository\CategoryRepository;
+use Oro\Bundle\CatalogBundle\Tests\Functional\DataFixtures\LoadCategoryData;
+use Oro\Bundle\ProductBundle\Entity\Product;
+use Oro\Bundle\ProductBundle\Tests\Functional\ImportExport\AbstractImportExportTest;
+
+/**
+ * @dbIsolationPerTest
+ */
+class ImportExportTest extends AbstractImportExportTest
+{
+    public function testImportWithCategory()
+    {
+        $strategy = 'oro_product_product.add_or_replace';
+        $productClass = $this->getContainer()->getParameter('oro_product.entity.product.class');
+        $categoryClass = $this->getContainer()->getParameter('oro_catalog.entity.category.class');
+        $this->loadFixtures([LoadCategoryData::class]);
+
+        $filePath = implode(DIRECTORY_SEPARATOR, [__DIR__, 'data', 'import.csv']);
+
+        $this->validateImportFile($strategy, $filePath);
+        $this->doImport('oro_product_product.add_or_replace');
+
+        $productRepository = $this->getRepository($productClass);
+
+        /** @var CategoryRepository $categoryRepository */
+        $categoryRepository = $this->getRepository($categoryClass);
+        $categoryManager = $this->getManager($categoryClass);
+
+        $products = $productRepository->findAll();
+        $this->assertCount(9, $products);
+
+        /** @var Product $sku1Product */
+        $sku1Product = $productRepository->findOneBy(['sku' => 'sku1']);
+
+        $categorySecond = $categoryRepository->findOneByDefaultTitle(LoadCategoryData::SECOND_LEVEL1);
+        $categoryFirst = $categoryRepository->findOneByDefaultTitle(LoadCategoryData::FIRST_LEVEL);
+
+        $categoryFirst->removeProduct($sku1Product);
+        $categorySecond->addProduct($sku1Product);
+
+        $categoryManager->flush();
+        $categoryManager->clear();
+
+        // Reimport after change category
+        $this->validateImportFile($strategy, $filePath);
+        $this->doImport('oro_product_product.add_or_replace');
+
+        /** @var Product $sku1Product */
+        $sku1Product = $productRepository->findOneBy(['sku' => 'sku1']);
+        $category = $categoryRepository->findOneByProduct($sku1Product);
+        $this->assertEquals(LoadCategoryData::SECOND_LEVEL1, $category->getDefaultTitle()->getString());
+    }
+}
