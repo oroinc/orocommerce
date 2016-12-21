8b192314869b5ac56192f80f6348e9cb90f5d987
Yauheni Kasenka <ykasenka@oroinc.com>
2016-12-21 14:10:00 +0300

    BB-6212: Creating order from quote by different users creates same checkout - updated functional tests

--- src/Oro/Bundle/ShoppingListBundle/Tests/Functional/ShoppingListFrontendActionsTest.php
+++ src/Oro/Bundle/ShoppingListBundle/Tests/Functional/ShoppingListFrontendActionsTest.php
@@ -2,10 +2,14 @@
 
 namespace Oro\Bundle\ShoppingListBundle\Tests\Functional;
 
-use Oro\Bundle\FrontendTestFrameworkBundle\Migrations\Data\ORM\LoadAccountUserData;
+use Oro\Bundle\CustomerBundle\Tests\Functional\DataFixtures\LoadAccountUserData;
+use Oro\Bundle\FrontendTestFrameworkBundle\Migrations\Data\ORM\LoadAccountUserData as LoadBaseAccountUserData;
 use Oro\Bundle\FrontendBundle\Tests\Functional\FrontendActionTestCase;
+use Oro\Bundle\PricingBundle\Tests\Functional\DataFixtures\LoadCombinedProductPrices;
 use Oro\Bundle\ShoppingListBundle\Entity\ShoppingList;
 use Oro\Bundle\ShoppingListBundle\Tests\Functional\DataFixtures\LoadShoppingLists;
+use Oro\Bundle\ShoppingListBundle\Tests\Functional\DataFixtures\LoadShoppingListLineItems;
+
 
 /**
  * @dbIsolation
@@ -16,18 +20,18 @@ class ShoppingListFrontendActionsTest extends FrontendActionTestCase
     {
         $this->initClient(
             [],
-            $this->generateBasicAuthHeader(LoadAccountUserData::AUTH_USER, LoadAccountUserData::AUTH_PW)
+            $this->generateBasicAuthHeader(LoadBaseAccountUserData::AUTH_USER, LoadBaseAccountUserData::AUTH_PW)
         );
 
         $this->loadFixtures(
             [
-                'Oro\Bundle\ShoppingListBundle\Tests\Functional\DataFixtures\LoadShoppingListLineItems',
-                'Oro\Bundle\PricingBundle\Tests\Functional\DataFixtures\LoadCombinedProductPrices',
+                LoadShoppingListLineItems::class,
+                LoadCombinedProductPrices::class,
             ]
         );
     }
 
-    public function testCreateCheckout()
+    public function testCreateOrder()
     {
         if (!$this->client->getContainer()->hasParameter('oro_checkout.entity.checkout.class')) {
             $this->markTestSkipped('CheckoutBundle disabled');
@@ -35,23 +39,8 @@ class ShoppingListFrontendActionsTest extends FrontendActionTestCase
 
         /** @var ShoppingList $shoppingList */
         $shoppingList = $this->getReference(LoadShoppingLists::SHOPPING_LIST_1);
-        $this->assertFalse($shoppingList->getLineItems()->isEmpty());
 
-        $crawler = $this->client->request(
-            'GET',
-            $this->getUrl('oro_shopping_list_frontend_view', ['id' => $shoppingList->getId()])
-        );
-
-        $link = $crawler->selectLink('Create Order')->link();
-
-        $this->client->click($link);
-        $this->assertJsonResponseStatusCodeEquals($this->client->getResponse(), 200);
-
-        $data = json_decode($this->client->getResponse()->getContent(), true);
-
-        $this->assertArrayHasKey('workflowItem', $data);
-        $this->assertArrayHasKey('result', $data['workflowItem']);
-        $this->assertArrayHasKey('redirectUrl', $data['workflowItem']['result']);
+        $data = $this->startCheckout($shoppingList);
 
         $crawler = $this->client->request('GET', $data['workflowItem']['result']['redirectUrl']);
 
@@ -59,6 +48,27 @@ class ShoppingListFrontendActionsTest extends FrontendActionTestCase
         foreach ($shoppingList->getLineItems() as $lineItem) {
             $this->assertContains($lineItem->getProduct()->getSku(), $content);
         }
+    }
+
+    public function testCreateOrdersFromSingleShoppingList()
+    {
+        if (!$this->client->getContainer()->hasParameter('oro_checkout.entity.checkout.class')) {
+            $this->markTestSkipped('CheckoutBundle disabled');
+        }
+
+        // start checkout from first user
+        $this->authUser(LoadAccountUserData::LEVEL_1_1_EMAIL, LoadAccountUserData::LEVEL_1_1_PASSWORD);
+        $firstData = $this->startCheckout($this->getReference(LoadShoppingLists::SHOPPING_LIST_7));
+        // continue checkout from first user
+        $secondData = $this->startCheckout($this->getReference(LoadShoppingLists::SHOPPING_LIST_7));
+
+        $this->assertEquals($firstData['workflowItem']['id'], $secondData['workflowItem']['id']);
+
+        // start checkout from second user
+        $this->authUser(LoadAccountUserData::LEVEL_1_EMAIL, LoadAccountUserData::LEVEL_1_PASSWORD);
+        $startData = $this->startCheckout($this->getReference(LoadShoppingLists::SHOPPING_LIST_7));
+
+        $this->assertNotEquals($firstData['workflowItem']['id'], $startData['workflowItem']['id']);
     }
 
     public function testCreateRequest()
@@ -89,6 +99,44 @@ class ShoppingListFrontendActionsTest extends FrontendActionTestCase
         foreach ($shoppingList->getLineItems() as $lineItem) {
             $this->assertContains($lineItem->getProduct()->getSku(), $content);
         }
+    }
+
+    /**
+     * @param string $username
+     * @param string $password
+     */
+    protected function authUser($username, $password)
+    {
+        $this->initClient([], $this->generateBasicAuthHeader($username, $password));
+    }
+
+    /**
+     * @param ShoppingList $shoppingList
+     * @return array
+     */
+    protected function startCheckout(ShoppingList $shoppingList)
+    {
+        $this->assertFalse($shoppingList->getLineItems()->isEmpty());
+
+        $crawler = $this->client->request(
+            'GET',
+            $this->getUrl('oro_shopping_list_frontend_view', ['id' => $shoppingList->getId()])
+        );
+
+        //print_r(['rc' => $this->client->getResponse()->getContent()]);
+
+        $link = $crawler->selectLink('Create Order')->link();
+
+        $this->client->click($link);
+        $this->assertJsonResponseStatusCodeEquals($this->client->getResponse(), 200);
+
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('workflowItem', $data);
+        $this->assertArrayHasKey('result', $data['workflowItem']);
+        $this->assertArrayHasKey('redirectUrl', $data['workflowItem']['result']);
+
+        return $data;
     }
 
     /**
