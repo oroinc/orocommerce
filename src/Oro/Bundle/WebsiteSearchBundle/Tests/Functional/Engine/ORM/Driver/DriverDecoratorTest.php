4d74393d852e3f6d252121c3f062fd52c67e8a53
Alexandr Parkhomenko <oparkhomenko@oroinc.com>
2016-12-28 21:21:33 +0200

    BAP-13017: Provide compatibility or Oro applications with PHP 7.1 - Fixed functional tests

--- src/Oro/Bundle/WebsiteSearchBundle/Tests/Functional/Engine/ORM/Driver/DriverDecoratorTest.php
+++ src/Oro/Bundle/WebsiteSearchBundle/Tests/Functional/Engine/ORM/Driver/DriverDecoratorTest.php
@@ -63,6 +63,25 @@ class DriverDecoratorTest extends WebTestCase
     }
 
     /**
+     * DateTime object starting from PHP 7.1 contains microseconds,
+     * but database doesn't keep microsends in datetime fields.
+     * This method allow us convert dynamic DateTime object to database format.
+     *
+     *
+     * @param $dateTimeObj
+     *
+     * @return \DateTime
+     */
+    protected function trimMicrosecondsFromDateTimeObject($dateTimeObj)
+    {
+        if (! $dateTimeObj instanceof \DateTime) {
+            return $dateTimeObj;
+        }
+
+        return new \DateTime($dateTimeObj->format('Y-m-d H:i:s'), new \DateTimeZone('UTC'));
+    }
+
+    /**
      * @param Item $item
      * @return array
      */
@@ -75,8 +94,8 @@ class DriverDecoratorTest extends WebTestCase
             'recordId' => $item->getRecordId(),
             'title' => $item->getTitle(),
             'changed' => $item->getChanged(),
-            'createdAt' => $item->getCreatedAt(),
-            'updatedAt' => $item->getUpdatedAt(),
+            'createdAt' => $this->trimMicrosecondsFromDateTimeObject($item->getCreatedAt()),
+            'updatedAt' => $this->trimMicrosecondsFromDateTimeObject($item->getUpdatedAt())
         ];
     }
 
