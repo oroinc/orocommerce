f5d2ac5b00d1dd60b094936c48d9f97d6a7a0a83
Semyen Pavlov <spavlov@oroinc.com>
2017-01-05 18:53:14 +0200

    BB-6581: It is possible to create shipping rule with empty country - changing behat tests.

--- src/Oro/Bundle/ShippingBundle/Tests/Behat/Context/FeatureContext.php
+++ src/Oro/Bundle/ShippingBundle/Tests/Behat/Context/FeatureContext.php
@@ -126,14 +126,6 @@ class FeatureContext extends OroFeatureContext implements OroPageObjectAware, Ke
 
         /** @var Form $form */
         $form = $this->createElement('Shipping Rule');
-
-        foreach ($table->getColumn(0) as $columnItem) {
-            if (false !== strpos($columnItem, 'Country')) {
-                $destinationAdd = $form->find('css', '.add-list-item');
-                $destinationAdd->click();
-            }
-        }
-
         $form->fill($table);
         $form->saveAndClose();
         $this->waitForAjax();
