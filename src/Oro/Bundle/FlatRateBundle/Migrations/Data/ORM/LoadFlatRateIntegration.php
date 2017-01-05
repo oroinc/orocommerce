1e4fc829353768e9230bc2bd4bf643b249f791b4
Sam Ivichuk <sivichuk@oroinc.com>
2017-01-05 19:30:42 +0200

    BB-6804: Move FlatRate method to FlatRateBundle  - added LoadFlatRateIntegration data fixture

---
+++ src/Oro/Bundle/FlatRateBundle/Migrations/Data/ORM/LoadFlatRateIntegration.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Oro\Bundle\FlatRateBundle\Migrations\Data\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Oro\Bundle\FlatRateBundle\Entity\FlatRateSettings;
+use Oro\Bundle\FlatRateBundle\Integration\FlatRateChannelType;
+use Oro\Bundle\IntegrationBundle\Entity\Channel;
+use Oro\Bundle\LocaleBundle\Entity\LocalizedFallbackValue;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadFlatRateIntegration extends AbstractFixture implements
+    DependentFixtureInterface,
+    ContainerAwareInterface
+{
+    /** @var ContainerInterface */
+    protected $container;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDependencies()
+    {
+        return [
+            'Oro\Bundle\OrganizationBundle\Migrations\Data\ORM\LoadOrganizationAndBusinessUnitData',
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        if (!$this->container) {
+            return;
+        }
+
+        if (!$this->container->hasParameter('oro_integration.entity.class')) {
+            return;
+        }
+
+        $label = (new LocalizedFallbackValue())->setString('Flat Rate');
+
+        $transport = new FlatRateSettings();
+        $transport->addLabel($label);
+
+        $channel = new Channel();
+        $channel->setType(FlatRateChannelType::TYPE)
+            ->setName('Flat Rate')
+            ->setEnabled(true)
+            ->setOrganization($this->getReference('default_organization'))
+            ->setTransport($transport);
+
+        $manager->persist($channel);
+        $manager->flush();
+    }
+}
