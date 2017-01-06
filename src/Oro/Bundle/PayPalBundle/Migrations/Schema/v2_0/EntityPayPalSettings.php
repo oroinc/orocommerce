1ab2d3502021024cb8157d5c28efe7e508b42f78
Mykola Sedletskyi <msedletskyi@oroinc.com>
2017-01-06 13:49:10 +0200

    BB-6883: Create PayPalSettings Entity

--- src/Oro/Bundle/PayPalBundle/Migrations/Schema/v2_0/EntityPayPalSettings.php
+++ src/Oro/Bundle/PayPalBundle/Migrations/Schema/v2_0/EntityPayPalSettings.php
@@ -22,6 +22,10 @@ class EntityPayPalSettings implements Migration, ExtendExtensionAwareInterface
     public function up(Schema $schema, QueryBag $queries)
     {
         $this->updateOroIntegrationTransportTable($schema);
+        $this->createOroPaypalCreditCardLblTable($schema);
+        $this->createOroPaypalCreditCardShrtLblTable($schema);
+        $this->createOroPaypalXprssChktLblTable($schema);
+        $this->createOroPaypalXprssChktShrtLblTable($schema);
         $this->addOroPaypalCreditCardLblForeignKeys($schema);
         $this->addOroPaypalCreditCardShrtLblForeignKeys($schema);
         $this->addOroPaypalXprssChktLblForeignKeys($schema);
@@ -48,26 +52,26 @@ class EntityPayPalSettings implements Migration, ExtendExtensionAwareInterface
         $table->addColumn('pp_proxy_host', 'string', ['notnull' => false, 'length' => 255]);
         $table->addColumn('pp_proxy_port', 'string', ['notnull' => false, 'length' => 255]);
         $table->addColumn('pp_enable_ssl_verification', 'boolean', ['default' => '1', 'notnull' => false]);
-
-        $this->extendExtension->addEnumField(
-            $schema,
-            'oro_integration_transport',
-            'credit_card_types',
-            'pp_credit_card_types',
-            true
-        );
-        $this->extendExtension->addEnumField(
-            $schema,
-            'oro_integration_transport',
-            'credit_card_payment_action',
-            'pp_credit_card_payment_action'
-        );
-        $this->extendExtension->addEnumField(
-            $schema,
-            'oro_integration_transport',
-            'express_checkout_payment_action',
-            'pp_express_checkout_payment_action'
-        );
+//
+//        $this->extendExtension->addEnumField(
+//            $schema,
+//            'oro_integration_transport',
+//            'credit_card_types',
+//            'pp_credit_card_types',
+//            true
+//        );
+//        $this->extendExtension->addEnumField(
+//            $schema,
+//            'oro_integration_transport',
+//            'credit_card_payment_action',
+//            'pp_credit_card_payment_action'
+//        );
+//        $this->extendExtension->addEnumField(
+//            $schema,
+//            'oro_integration_transport',
+//            'express_checkout_payment_action',
+//            'pp_express_checkout_payment_action'
+//        );
     }
 
 
@@ -157,6 +161,66 @@ class EntityPayPalSettings implements Migration, ExtendExtensionAwareInterface
             ['id'],
             ['onDelete' => 'CASCADE', 'onUpdate' => null]
         );
+    }
+
+    /**
+     * Create oro_paypal_credit_card_lbl table
+     *
+     * @param Schema $schema
+     */
+    protected function createOroPaypalCreditCardLblTable(Schema $schema)
+    {
+        $table = $schema->createTable('oro_paypal_credit_card_lbl');
+        $table->addColumn('transport_id', 'integer', []);
+        $table->addColumn('localized_value_id', 'integer', []);
+        $table->setPrimaryKey(['transport_id', 'localized_value_id']);
+        $table->addUniqueIndex(['localized_value_id'], 'UNIQ_92E5B87EEB576E89');
+        $table->addIndex(['transport_id'], 'IDX_92E5B87E9909C13F', []);
+    }
+
+    /**
+     * Create oro_paypal_credit_card_shrt_lbl table
+     *
+     * @param Schema $schema
+     */
+    protected function createOroPaypalCreditCardShrtLblTable(Schema $schema)
+    {
+        $table = $schema->createTable('oro_paypal_credit_card_shrt_lbl');
+        $table->addColumn('transport_id', 'integer', []);
+        $table->addColumn('localized_value_id', 'integer', []);
+        $table->setPrimaryKey(['transport_id', 'localized_value_id']);
+        $table->addUniqueIndex(['localized_value_id'], 'UNIQ_415165DEEB576E89');
+        $table->addIndex(['transport_id'], 'IDX_415165DE9909C13F', []);
+    }
+
+    /**
+     * Create oro_paypal_xprss_chkt_lbl table
+     *
+     * @param Schema $schema
+     */
+    protected function createOroPaypalXprssChktLblTable(Schema $schema)
+    {
+        $table = $schema->createTable('oro_paypal_xprss_chkt_lbl');
+        $table->addColumn('transport_id', 'integer', []);
+        $table->addColumn('localized_value_id', 'integer', []);
+        $table->setPrimaryKey(['transport_id', 'localized_value_id']);
+        $table->addUniqueIndex(['localized_value_id'], 'UNIQ_386D1FC6EB576E89');
+        $table->addIndex(['transport_id'], 'IDX_386D1FC69909C13F', []);
+    }
+
+    /**
+     * Create oro_paypal_xprss_chkt_shrt_lbl table
+     *
+     * @param Schema $schema
+     */
+    protected function createOroPaypalXprssChktShrtLblTable(Schema $schema)
+    {
+        $table = $schema->createTable('oro_paypal_xprss_chkt_shrt_lbl');
+        $table->addColumn('transport_id', 'integer', []);
+        $table->addColumn('localized_value_id', 'integer', []);
+        $table->setPrimaryKey(['transport_id', 'localized_value_id']);
+        $table->addUniqueIndex(['localized_value_id'], 'UNIQ_A9419ECEB576E89');
+        $table->addIndex(['transport_id'], 'IDX_A9419EC9909C13F', []);
     }
 
     /**
