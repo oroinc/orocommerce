6321dd6318ef1f50c4f047662136dec8695b4d7f
Oleg Popadko <olegpopadko@users.noreply.github.com>
2017-01-13 11:23:11 +0200

    BB-6961: No action on removing of "Flat rate" integration (#6389)
    * BB-6961: No action on removing of "Flat rate" integration
     - creating interfaces and implementations for methods factories, methods identifiers generators
     - creating method and type removal events and listeners
     - creating abstract integration listener
    
    * BB-6961: No action on removing of "Flat rate" integration
     - fixing unit and functional tests
    
    * BB-6961: No action on removing of "Flat rate" integration
     - fixing after cr
    
    * BB-6961: No action on removing of "Flat rate" integration
     - set missing service argument

--- src/Oro/Bundle/FlatRateBundle/Tests/Unit/Builder/FlatRateMethodFromChannelBuilderTest.php
+++ src/Oro/Bundle/FlatRateBundle/Tests/Unit/Builder/FlatRateMethodFromChannelBuilderTest.php
@@ -7,9 +7,13 @@ use Oro\Bundle\FlatRateBundle\Entity\FlatRateSettings;
 use Oro\Bundle\FlatRateBundle\Method\FlatRateMethod;
 use Oro\Bundle\IntegrationBundle\Entity\Channel;
 use Oro\Bundle\LocaleBundle\Helper\LocalizationHelper;
+use Oro\Bundle\ShippingBundle\Method\Identifier\IntegrationMethodIdentifierGeneratorInterface;
 
 class FlatRateMethodFromChannelBuilderTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var IntegrationMethodIdentifierGeneratorInterface|\PHPUnit_Framework_MockObject_MockObject */
+    private $identifierGenerator;
+
     /** @var \PHPUnit_Framework_MockObject_MockObject|LocalizationHelper */
     private $localizationHelper;
 
@@ -18,25 +22,34 @@ class FlatRateMethodFromChannelBuilderTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
+        $this->identifierGenerator = $this->createMock(IntegrationMethodIdentifierGeneratorInterface::class);
+
         $this->localizationHelper = $this->getMockBuilder(LocalizationHelper::class)
             ->disableOriginalConstructor()
             ->getMock();
 
-        $this->builder = new FlatRateMethodFromChannelBuilder($this->localizationHelper);
+        $this->builder = new FlatRateMethodFromChannelBuilder($this->identifierGenerator, $this->localizationHelper);
     }
 
     public function testBuildReturnsCorrectObjectWithLabel()
     {
         $label = 'test';
         $channel = $this->getChannel();
+        $identifier = 'flat_rate_1';
 
         $this->localizationHelper->expects(static::once())
             ->method('getLocalizedValue')
             ->willReturn($label);
 
+        $this->identifierGenerator->expects($this->once())
+            ->method('generateIdentifier')
+            ->with($channel)
+            ->willReturn($identifier);
+
         $method = $this->builder->build($channel);
 
         static::assertInstanceOf(FlatRateMethod::class, $method);
+        static::assertSame($identifier, $method->getIdentifier());
         static::assertSame($label, $method->getLabel());
     }
 
--- src/Oro/Bundle/ShippingBundle/Tests/Functional/Entity/Repository/ShippingMethodConfigRepositoryTest.php
+++ src/Oro/Bundle/ShippingBundle/Tests/Functional/Entity/Repository/ShippingMethodConfigRepositoryTest.php
@@ -3,12 +3,14 @@
 namespace Oro\Bundle\ShippingBundle\Tests\Functional\Entity\Repository;
 
 use Oro\Bundle\ShippingBundle\Entity\Repository\ShippingMethodConfigRepository;
+use Oro\Bundle\ShippingBundle\Entity\ShippingMethodConfig;
+use Oro\Bundle\ShippingBundle\Entity\ShippingMethodsConfigsRule;
 use Oro\Bundle\ShippingBundle\Tests\Functional\DataFixtures\LoadShippingMethodsConfigsRules;
 use Oro\Bundle\ShippingBundle\Tests\Functional\Helper\FlatRateIntegrationTrait;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 
 /**
- * @dbIsolation
+ * @dbIsolationPerTest
  */
 class ShippingMethodConfigRepositoryTest extends WebTestCase
 {
@@ -49,5 +51,38 @@ class ShippingMethodConfigRepositoryTest extends WebTestCase
                 ]
             )
         );
+    }
+
+    public function testFindMethodConfigIdsWithoutTypeConfigs()
+    {
+        /** @var ShippingMethodsConfigsRule $rule */
+        $rule = $this->getReference('shipping_rule_without_type_configs');
+
+        /** @var ShippingMethodConfig $methodConfig */
+        $methodConfig = $rule->getMethodConfigs()->first();
+
+        static::assertEmpty($methodConfig->getTypeConfigs());
+
+        $ids = $this->repository->findIdsWithoutTypeConfigs();
+
+        static::assertEquals([$methodConfig->getId()], $ids);
+    }
+
+    public function testDeleteMethodConfigByIds()
+    {
+        /** @var ShippingMethodsConfigsRule $rule */
+        $rule1 = $this->getReference('shipping_rule.1');
+        $rule2 = $this->getReference('shipping_rule_without_type_configs');
+
+        $ids = [
+            $rule1->getMethodConfigs()->first()->getId(),
+            $rule2->getMethodConfigs()->first()->getId(),
+        ];
+
+        static::assertCount(2, $this->repository->findBy(['id' => $ids]));
+
+        $this->repository->deleteByIds($ids);
+
+        static::assertEmpty($this->repository->findBy(['id' => $ids]));
     }
 }
