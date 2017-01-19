d25dc2644c30e160f2f6b7bf6deca2e93a4dc993
Vitalii Yeromenko <vyeromenko@magecore.com>
2017-01-19 19:52:02 +0200

    BB-7115 Removing extended fields from entity configuration - dispatching event in RemoveRestoreConfigFieldHandler on removing extended fields and checking in the listener in ProductBundle if this field is a variant field in some configurable products

---
+++ src/Oro/Bundle/ProductBundle/EventListener/BeforeRemoveFieldListener.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Oro\Bundle\ProductBundle\EventListener;
+
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Oro\Bundle\EntityConfigBundle\Event\BeforeRemoveFieldEvent;
+use Oro\Bundle\ProductBundle\Entity\Product;
+
+class BeforeRemoveFieldListener
+{
+    /** @var DoctrineHelper */
+    protected $doctrineHelper;
+
+    public function __construct(DoctrineHelper $doctrineHelper)
+    {
+        $this->doctrineHelper = $doctrineHelper;
+    }
+
+    public function onBeforeRemoveField(BeforeRemoveFieldEvent $event)
+    {
+        if ($event->getClassName() !== Product::class && !is_subclass_of($event->getClassName(), Product::class)) {
+            return;
+        }
+
+        $fieldName = $event->getFieldName();
+        $skuList = [];
+
+        /** @var Product[] $configurableProducts */
+        $configurableProducts = $this->doctrineHelper->getEntityRepository(Product::class)->findBy([
+            'type' => Product::TYPE_CONFIGURABLE
+        ]);
+
+        foreach ($configurableProducts as $configurableProduct) {
+            if (in_array($fieldName, $configurableProduct->getVariantFields())) {
+                $event->setHasErrors(true);
+                $skuList[] = $configurableProduct->getSku();
+            }
+        }
+
+        if ($event->hasErrors()) {
+            $event->setValidationMessage(sprintf(
+                '%s%s',
+                'Cannot remove field because it\'s used as a variant field in the following configurable products: ',
+                implode(', ', array_unique($skuList))
+            ));
+        }
+
+
+    }
+}
