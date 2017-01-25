4b7db3fbf0b86e8b7cecc126740110b97a7c895c
Volodymyr Denchyk <devit_dvv@ukr.net>
2017-01-25 12:55:46 +0200

    BB-7059: Remove unnecessary code (#6927)

--- src/Oro/Bundle/PaymentTermBundle/DependencyInjection/OroPaymentTermExtension.php
+++ src/Oro/Bundle/PaymentTermBundle/DependencyInjection/OroPaymentTermExtension.php
@@ -16,10 +16,6 @@ class OroPaymentTermExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-        $container->prependExtensionConfig($this->getAlias(), $config);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('form_types.yml');
         $loader->load('payment.yml');
