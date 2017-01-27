ddd14c075abd4fc065161bf9bbfccb910f572186
Thomas Parys <thomas.parys@wwsh.io>
2017-01-27 10:20:10 +0100

    BB-7339: Price List Recalculation speed improvement - CR fixes

--- src/Oro/Bundle/PricingBundle/Migrations/Schema/v1_7/OroPricingBundle.php
+++ src/Oro/Bundle/PricingBundle/Migrations/Schema/v1_7/OroPricingBundle.php
@@ -7,8 +7,7 @@ use Doctrine\DBAL\Schema\Schema;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
-class OroPricingBundle implements
-    Migration
+class OroPricingBundle implements Migration
 {
 
     /**
