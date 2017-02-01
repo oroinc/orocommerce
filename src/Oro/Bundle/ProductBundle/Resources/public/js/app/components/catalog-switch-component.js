19c41fe3d42092eaa888daebbb19175acdb39f6d
Alex Yegerev <alexriz.webdev@gmail.com>
2017-02-01 12:34:16 +0200

    BB-6464: RangeError, part of url is missing. After work with url by tools.packToQueryString - Used tools.packToQueryString functionality

--- src/Oro/Bundle/ProductBundle/Resources/public/js/app/components/catalog-switch-component.js
+++ src/Oro/Bundle/ProductBundle/Resources/public/js/app/components/catalog-switch-component.js
@@ -1,18 +1,19 @@
 define(function(require) {
     'use strict';
 
-    var CatalogSwitchView;
+    var CatalogSwitchComponent;
     var BaseComponent = require('oroui/js/app/components/base/component');
     var UrlHelper = require('orodatagrid/js/url-helper');
     var $ = require('jquery');
     var _ = require('underscore');
+    var tools = require('oroui/js/tools');
     var mediator = require('oroui/js/mediator');
 
-    CatalogSwitchView = BaseComponent.extend(_.extend({}, UrlHelper, {
+    CatalogSwitchComponent = BaseComponent.extend(_.extend({}, UrlHelper, {
         parameterName: null,
 
         initialize: function(options) {
-            CatalogSwitchView.__super__.initialize.apply(this, arguments);
+            CatalogSwitchComponent.__super__.initialize.apply(this, arguments);
 
             this.parameterName = options.parameterName;
 
@@ -25,11 +26,29 @@ define(function(require) {
                 e.preventDefault();
 
                 var value = $(e.currentTarget).data('catalog-view-trigger');
-                var url = this.addUrlParameter(location.href, this.parameterName, value);
+                var url = this.updateUrlParameter(location.href, this.parameterName, value);
                 mediator.execute('redirectTo', {url: url}, {redirect: true});
             }
+        },
+
+        updateUrlParameter: function(url, param, value) {
+            var urlSplited = url.split('?');
+            var urlObject = {};
+
+            if (urlSplited.length > 1) {
+                var urlObject = tools.unpackFromQueryString(urlSplited[1]);
+            }
+
+            if (!urlObject[param]) {
+                urlObject[param] = {};
+            }
+
+            _.extend(urlObject[param], value);
+            urlSplited[1] = tools.packToQueryString(urlObject);
+
+            return urlSplited.join('?');
         }
     }));
 
-    return CatalogSwitchView;
+    return CatalogSwitchComponent;
 });
