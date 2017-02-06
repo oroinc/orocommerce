0885ff35265e01722ffe736bfec78be1bff04c36
Iaroslav Bratsilo <ibratsilo@oroinc.com>
2017-02-06 14:15:34 +0200

    BB-7484: Add PageTemplateContextConfigurator (look examples)  - applied context configurator  - added test

---
+++ src/Oro/Bundle/FrontendBundle/Layout/Extension/PageTemplateContextConfigurator.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Oro\Bundle\FrontendBundle\Layout\Extension;
+
+use Oro\Bundle\FrontendBundle\DependencyInjection\OroFrontendExtension;
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
+use Oro\Component\Layout\ContextConfiguratorInterface;
+use Oro\Component\Layout\ContextInterface;
+
+use Symfony\Component\OptionsResolver\Options;
+
+class PageTemplateContextConfigurator implements ContextConfiguratorInterface
+{
+    /** @var ConfigManager */
+    private $configManager;
+
+    /**
+     * @param ConfigManager $configManager
+     */
+    public function __construct(ConfigManager $configManager)
+    {
+        $this->configManager = $configManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureContext(ContextInterface $context)
+    {
+        $context->getResolver()
+            ->setDefaults(
+                [
+                    'page_template' => function (Options $options, $value) {
+
+                        if (!$value) {
+                            $pageTemplates = $this->configManager->get(
+                                OroFrontendExtension::ALIAS . ConfigManager::SECTION_MODEL_SEPARATOR . 'page_templates'
+                            );
+
+                            $routeName = $options['route_name'];
+
+                            if (isset($pageTemplates[$routeName]) && $pageTemplates[$routeName]) {
+                                $value = $pageTemplates[$routeName];
+                            }
+                        }
+
+                        return $value;
+                    }
+                ]
+            )
+            ->setAllowedTypes(['page_template' => ['string', 'null']]);
+    }
+}
