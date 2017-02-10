6f2019330ae7e4e2dd4f236be34b65cfba941622
Volodymyr Denchyk <devit_dvv@ukr.net>
2017-02-10 02:13:40 +0200

    BB-6923: Test and Merge - fixed payment_methods layout block rendering on order review step of checkout

--- src/Oro/Bundle/PayPalBundle/Method/View/PayPalExpressCheckoutPaymentMethodView.php
+++ src/Oro/Bundle/PayPalBundle/Method/View/PayPalExpressCheckoutPaymentMethodView.php
@@ -6,59 +6,76 @@ use Oro\Bundle\PaymentBundle\Context\PaymentContextInterface;
 use Oro\Bundle\PaymentBundle\Method\View\PaymentMethodViewInterface;
 use Oro\Bundle\PaymentBundle\Provider\PaymentTransactionProvider;
 use Oro\Bundle\PayPalBundle\Method\Config\PayPalExpressCheckoutConfigInterface;
-use Oro\Bundle\PayPalBundle\Method\PayflowExpressCheckout;
 use Symfony\Component\Form\FormFactoryInterface;
 
 class PayPalExpressCheckoutPaymentMethodView implements PaymentMethodViewInterface
 {
-    /** @var FormFactoryInterface */
+    /**
+     * @var FormFactoryInterface
+     */
     protected $formFactory;
 
-    /** @var PaymentTransactionProvider */
+    /**
+     * @var PaymentTransactionProvider
+     */
     protected $paymentTransactionProvider;
 
-    /** @var PayPalExpressCheckoutConfigInterface */
+    /**
+     * @var PayPalExpressCheckoutConfigInterface
+     */
     protected $config;
 
-    /** @param PayPalExpressCheckoutConfigInterface $config */
+    /**
+     * @param PayPalExpressCheckoutConfigInterface $config
+     */
     public function __construct(PayPalExpressCheckoutConfigInterface $config)
     {
         $this->config = $config;
     }
 
-    /** {@inheritdoc} */
+    /**
+     * {@inheritDoc}
+     */
     public function getOptions(PaymentContextInterface $context)
     {
         return [];
     }
 
-    /** {@inheritdoc} */
+    /**
+     * {@inheritDoc}
+     */
     public function getBlock()
     {
-        return '_payment_methods_payflow_express_checkout_widget';
+        return '_payment_methods_paypal_express_checkout_widget';
     }
 
-    /** {@inheritdoc} */
+    /**
+     * {@inheritDoc}
+     */
     public function getLabel()
     {
         return $this->config->getLabel();
     }
 
-    /** {@inheritdoc} */
+    /**
+     * {@inheritDoc}
+     */
     public function getShortLabel()
     {
         return $this->config->getShortLabel();
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
     public function getAdminLabel()
     {
         return $this->config->getAdminLabel();
     }
 
-    /** {@inheritdoc} */
+    /**
+     * {@inheritDoc}
+     */
     public function getPaymentMethodIdentifier()
     {
         return $this->config->getPaymentMethodIdentifier();
