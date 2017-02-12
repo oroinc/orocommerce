3d5ddfea3ddab14bdfb4acfc546de772170a9b6a
Volodymyr Denchyk <devit_dvv@ukr.net>
2017-02-12 23:31:45 +0200

    BB-7060: Test and Merge - migrations fixed

--- src/Oro/Bundle/PaymentTermBundle/Migrations/Schema/v1_2/CreatePaymentTermShortLabelTable.php
+++ src/Oro/Bundle/PaymentTermBundle/Migrations/Schema/v1_2/CreatePaymentTermShortLabelTable.php
@@ -1,57 +1,53 @@
 <?php
 
-namespace Oro\Bundle\PaymentTermBundle\Migrations\Schema\v1_1;
+namespace Oro\Bundle\PaymentTermBundle\Migrations\Schema\v1_2;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Schema\SchemaException;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
-class CreatePaymentTermTransportLabelTable implements Migration
+class CreatePaymentTermShortLabelTable implements Migration
 {
     /**
-     * @param Schema   $schema
+     * @param Schema $schema
      * @param QueryBag $queries
      *
      * @throws SchemaException
      */
     public function up(Schema $schema, QueryBag $queries)
     {
-        $this->createOroPaymentTermTransportLabelTable($schema);
-        $this->addOroPaymentTermTransportLabelForeignKeys($schema);
+        $this->createOroPaymentTermShortLabelTable($schema);
+        $this->addOroPaymentTermShortLabelForeignKeys($schema);
     }
-
+    
     /**
      * @param Schema $schema
      */
-    private function createOroPaymentTermTransportLabelTable(Schema $schema)
+    private function createOroPaymentTermShortLabelTable(Schema $schema)
     {
-        $table = $schema->createTable('oro_payment_term_trans_label');
-
+        $table = $schema->createTable('oro_payment_term_short_label');
         $table->addColumn('transport_id', 'integer', []);
         $table->addColumn('localized_value_id', 'integer', []);
-
         $table->setPrimaryKey(['transport_id', 'localized_value_id']);
-        $table->addIndex(['transport_id'], 'oro_payment_term_trans_label_transport_id', []);
-        $table->addUniqueIndex(['localized_value_id'], 'oro_payment_term_trans_label_localized_value_id', []);
+        $table->addIndex(['transport_id'], 'oro_payment_term_short_label_transport_id', []);
+        $table->addUniqueIndex(['localized_value_id'], 'oro_payment_term_short_label_localized_value_id', []);
     }
-
+    
     /**
      * @param Schema $schema
      *
      * @throws SchemaException
      */
-    private function addOroPaymentTermTransportLabelForeignKeys(Schema $schema)
+    private function addOroPaymentTermShortLabelForeignKeys(Schema $schema)
     {
-        $table = $schema->getTable('oro_payment_term_trans_label');
-
+        $table = $schema->getTable('oro_payment_term_short_label');
         $table->addForeignKeyConstraint(
             $schema->getTable('oro_fallback_localization_val'),
             ['localized_value_id'],
             ['id'],
             ['onDelete' => 'CASCADE', 'onUpdate' => null]
         );
-
         $table->addForeignKeyConstraint(
             $schema->getTable('oro_integration_transport'),
             ['transport_id'],
