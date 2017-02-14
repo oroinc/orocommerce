8554c08a9a876891c868082328222b84e542bff8
Viacheslav Bemza <vbemza@magecore.com>
2017-02-14 19:14:27 +0100

    BB-7630: Add system config options for changefreq and priority per each provider (#7523)

--- src/Oro/Bundle/SEOBundle/DependencyInjection/OroSEOExtension.php
+++ src/Oro/Bundle/SEOBundle/DependencyInjection/OroSEOExtension.php
@@ -14,6 +14,10 @@ class OroSEOExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+        $container->prependExtensionConfig($this->getAlias(), $config);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
---
+++ src/Oro/Bundle/SEOBundle/Form/Type/SitemapChangefreqType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Oro\Bundle\SEOBundle\Form\Type;
+
+use Oro\Bundle\SEOBundle\DependencyInjection\Configuration;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SitemapChangefreqType extends AbstractType
+{
+    const NAME = 'oro_sitemap_changefreq';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return ChoiceType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'choices' => [
+                Configuration::CHANGEFREQ_ALWAYS
+                    => 'oro.seo.system_configuration.fields.changefreq.choice.always.label',
+                Configuration::CHANGEFREQ_HOURLY
+                    => 'oro.seo.system_configuration.fields.changefreq.choice.hourly.label',
+                Configuration::CHANGEFREQ_DAILY
+                    => 'oro.seo.system_configuration.fields.changefreq.choice.daily.label',
+                Configuration::CHANGEFREQ_WEEKLY
+                    => 'oro.seo.system_configuration.fields.changefreq.choice.weekly.label',
+                Configuration::CHANGEFREQ_MONTHLY
+                    => 'oro.seo.system_configuration.fields.changefreq.choice.monthly.label',
+                Configuration::CHANGEFREQ_YEARLY
+                    => 'oro.seo.system_configuration.fields.changefreq.choice.yearly.label',
+                Configuration::CHANGEFREQ_NEVER
+                    => 'oro.seo.system_configuration.fields.changefreq.choice.never.label',
+            ],
+        ]);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return self::NAME;
+    }
+}
