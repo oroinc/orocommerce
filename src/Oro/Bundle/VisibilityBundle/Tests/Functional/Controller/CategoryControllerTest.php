4436b74f1ff03022c08a86c6925f425d80bdce76
Sergiu Parlea <sparlea@pitechnologies.ro>
2017-02-17 18:50:39 +0200

    BB-7693: Required 'Inventory Threshold' field works incorrect - fix typo

--- src/Oro/Bundle/VisibilityBundle/Tests/Functional/Controller/CategoryControllerTest.php
+++ src/Oro/Bundle/VisibilityBundle/Tests/Functional/Controller/CategoryControllerTest.php
@@ -254,7 +254,7 @@ class CategoryControllerTest extends WebTestCase
         $parameters = $this->explodeArrayPaths($form->getValues());
         $token = $crawler->filterXPath('//input[@name="oro_catalog_category[_token]"]/@value')->text();
 
-        $parameters['oro_catalog_category']['inventoryThreshold']['scalarValue]'] = 0;
+        $parameters['oro_catalog_category']['inventoryThreshold']['scalarValue'] = 0;
         $parameters['oro_catalog_category'] = array_merge(
             $parameters['oro_catalog_category'],
             [
