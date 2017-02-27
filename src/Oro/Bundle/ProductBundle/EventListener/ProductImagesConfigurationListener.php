4594f366686882d1397e823b7dab85d5355ff620
Arsen Aliev <aliev.ars@gmail.com>
2017-02-27 22:14:16 +0200

    BB-7415 dont hide notification after watermark settings saved  - change message type to warning instead of info  - fix test

--- src/Oro/Bundle/ProductBundle/EventListener/ProductImagesConfigurationListener.php
+++ src/Oro/Bundle/ProductBundle/EventListener/ProductImagesConfigurationListener.php
@@ -10,6 +10,7 @@ use Oro\Bundle\ConfigBundle\Event\ConfigSettingsUpdateEvent;
 class ProductImagesConfigurationListener
 {
     const PRODUCT_IMAGE_WATERMARK_SECTION_PREFIX = 'oro_product.product_image_watermark';
+    const MESSAGE_TYPE = 'warning';
 
     /**
      * @var TranslatorInterface
@@ -39,7 +40,7 @@ class ProductImagesConfigurationListener
         foreach ($event->getSettings() as $configKey => $setting) {
             if (false !== strpos($configKey, self::PRODUCT_IMAGE_WATERMARK_SECTION_PREFIX)) {
                 $this->session->getFlashBag()->add(
-                    'info',
+                    self::MESSAGE_TYPE,
                     $this->translator->trans('oro.product.system_configuration.notice.product_image_watermark')
                 );
 
