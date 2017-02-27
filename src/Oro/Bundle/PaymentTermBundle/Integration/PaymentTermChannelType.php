2d26157e499acb7a361487d13dfa8613e3d712d0
Oleg Popadko <opopadko@oroinc.com>
2017-02-27 19:33:24 +0530

    BB-7060: Test and Merge  - adding icon to payment term

--- src/Oro/Bundle/PaymentTermBundle/Integration/PaymentTermChannelType.php
+++ src/Oro/Bundle/PaymentTermBundle/Integration/PaymentTermChannelType.php
@@ -3,16 +3,25 @@
 namespace Oro\Bundle\PaymentTermBundle\Integration;
 
 use Oro\Bundle\IntegrationBundle\Provider\ChannelInterface;
+use Oro\Bundle\IntegrationBundle\Provider\IconAwareIntegrationInterface;
 
-class PaymentTermChannelType implements ChannelInterface
+class PaymentTermChannelType implements ChannelInterface, IconAwareIntegrationInterface
 {
     const TYPE = 'payment_term';
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
     public function getLabel()
     {
         return 'oro.paymentterm.channel_type.label';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getIcon()
+    {
+        return 'bundles/oropaymentterm/img/payment-term-logo.png';
     }
 }
