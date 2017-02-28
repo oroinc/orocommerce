68535c2bf5b76372648bc4a8d362d962b9352e03
Vitalii Yeromenko <vyeromenko@magecore.com>
2017-02-28 19:58:52 +0200

    BB-7834 Add inline edit to inventory_status - changed inventory status field to select type - added inline editing with costom controller action

--- src/Oro/Bundle/ProductBundle/Controller/AjaxProductController.php
+++ src/Oro/Bundle/ProductBundle/Controller/AjaxProductController.php
@@ -2,6 +2,8 @@
 
 namespace Oro\Bundle\ProductBundle\Controller;
 
+use Oro\Bundle\EntityExtendBundle\Entity\AbstractEnumValue;
+use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
 use Oro\Bundle\ProductBundle\Entity\Product;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -36,6 +38,40 @@ class AjaxProductController extends Controller
         }
 
         $product->setDefaultName($name);
+        $this->getDoctrine()->getManagerForClass(Product::class)->flush();
+
+        return new JsonResponse();
+    }
+
+    /**
+     * @Route("/edit-inventory-status", name="oro_product_ajax_edit_inventory_status")
+     * @Method("PATCH")
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function productEditInventoryStatus(Request $request)
+    {
+        $id = $request->get('id');
+        $inventoryStatusId = $request->get('inventoryStatusId');
+
+        if ($id === null || $inventoryStatusId === null) {
+            return new JsonResponse(['code' => 400], Response::HTTP_BAD_REQUEST);
+        }
+
+        $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
+        if (!$product) {
+            return new JsonResponse(['code' => 400], Response::HTTP_BAD_REQUEST);
+        }
+
+        $inventoryStatusClassName = ExtendHelper::buildEnumValueClassName('prod_inventory_status');
+        /** @var AbstractEnumValue $inventoryStatus */
+        $inventoryStatus = $this->getDoctrine()->getRepository($inventoryStatusClassName)->find($inventoryStatusId);
+        if (!$inventoryStatus) {
+            return new JsonResponse(['code' => 400], Response::HTTP_BAD_REQUEST);
+        }
+
+        $product->setInventoryStatus($inventoryStatus);
         $this->getDoctrine()->getManagerForClass(Product::class)->flush();
 
         return new JsonResponse();
