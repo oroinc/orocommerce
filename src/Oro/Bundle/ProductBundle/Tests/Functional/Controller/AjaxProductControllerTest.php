5521f4a3d4c194ad1ece54ef1b93839458d3f1c1
Vitalii Yeromenko <vyeromenko@magecore.com>
2017-03-01 12:42:27 +0200

    BB-7834 Add inline edit to inventory_status - tests

--- src/Oro/Bundle/ProductBundle/Tests/Functional/Controller/AjaxProductControllerTest.php
+++ src/Oro/Bundle/ProductBundle/Tests/Functional/Controller/AjaxProductControllerTest.php
@@ -6,9 +6,13 @@ use Oro\Bundle\ProductBundle\Entity\Product;
 use Oro\Bundle\ProductBundle\Tests\Functional\DataFixtures\LoadProductData;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 
+/**
+ * @dbIsolationPerTest
+ */
 class AjaxProductControllerTest extends WebTestCase
 {
     const NEW_PRODUCT_NAME = 'New default product-1 name';
+    const NEW_INVENTORY_STATUS_ID = 'out_of_stock';
 
     protected function setUp()
     {
@@ -62,6 +66,75 @@ class AjaxProductControllerTest extends WebTestCase
             [
                 'id' => $id,
                 'productName' => self::NEW_PRODUCT_NAME,
+            ]
+        );
+        $result = $this->client->getResponse();
+
+        $this->assertJsonResponseStatusCodeEquals($result, 400);
+    }
+
+    public function testProductEditInventoryStatus()
+    {
+        /** @var Product $product1 */
+        $product1 = $this->getReference(LoadProductData::PRODUCT_1);
+        $this->assertEquals(Product::INVENTORY_STATUS_IN_STOCK, $product1->getInventoryStatus()->getId());
+
+        $this->client->request(
+            'PATCH',
+            $this->getUrl('oro_product_ajax_edit_inventory_status'),
+            [
+                'id' => $product1->getId(),
+                'inventoryStatusId' => self::NEW_INVENTORY_STATUS_ID,
+            ]
+        );
+        $result = $this->client->getResponse();
+
+        $this->assertJsonResponseStatusCodeEquals($result, 200);
+        $this->assertEquals(self::NEW_INVENTORY_STATUS_ID, $product1->getInventoryStatus()->getId());
+    }
+
+    public function testProductEditInventoryStatusEmptyParameters()
+    {
+        $this->client->request(
+            'PATCH',
+            $this->getUrl('oro_product_ajax_edit_inventory_status'),
+            []
+        );
+        $result = $this->client->getResponse();
+
+        $this->assertJsonResponseStatusCodeEquals($result, 400);
+    }
+
+    public function testProductEditInventoryStatusMissingProduct()
+    {
+        /** @var Product $product8 */
+        $product8 = $this->getReference(LoadProductData::PRODUCT_8);
+        $id = $product8->getId() + 999999;
+
+        $this->client->request(
+            'PATCH',
+            $this->getUrl('oro_product_ajax_edit_inventory_status'),
+            [
+                'id' => $id,
+                'inventoryStatusId' => self::NEW_INVENTORY_STATUS_ID,
+            ]
+        );
+        $result = $this->client->getResponse();
+
+        $this->assertJsonResponseStatusCodeEquals($result, 400);
+    }
+
+    public function testProductEditUnknownInventoryStatus()
+    {
+        /** @var Product $product1 */
+        $product1 = $this->getReference(LoadProductData::PRODUCT_1);
+
+        $this->client->request(
+            'PATCH',
+            $this->getUrl('oro_product_ajax_edit_inventory_status'),
+            [
+                'id' => $product1->getId(),
+                'inventoryStatusId' => 'unknown_inventory_status',
             ]
         );
         $result = $this->client->getResponse();
