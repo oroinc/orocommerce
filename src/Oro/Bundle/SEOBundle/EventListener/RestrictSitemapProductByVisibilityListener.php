149e43547ea9953872e240334268a45326200454
Serj Bielanovskyi <sprightly@users.noreply.github.com>
2017-03-14 15:04:37 +0200

    BB-8234: Improve RestrictSitemapProductByVisibilityListener (#8458)

--- src/Oro/Bundle/SEOBundle/EventListener/RestrictSitemapProductByVisibilityListener.php
+++ src/Oro/Bundle/SEOBundle/EventListener/RestrictSitemapProductByVisibilityListener.php
@@ -2,31 +2,22 @@
 
 namespace Oro\Bundle\SEOBundle\EventListener;
 
-use Oro\Bundle\ConfigBundle\Config\ConfigManager;
-use Oro\Bundle\CustomerBundle\Entity\CustomerGroup;
-use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
 use Oro\Bundle\SEOBundle\Event\RestrictSitemapEntitiesEvent;
-use Oro\Bundle\VisibilityBundle\Visibility\ProductVisibilityTrait;
+use Oro\Bundle\VisibilityBundle\Model\ProductVisibilityQueryBuilderModifier;
 
 class RestrictSitemapProductByVisibilityListener
 {
-    use ProductVisibilityTrait;
+    /**
+     * @var ProductVisibilityQueryBuilderModifier
+     */
+    private $productVisibilityQueryBuilderModifier;
 
     /**
-     * @var DoctrineHelper
+     * @param ProductVisibilityQueryBuilderModifier $productVisibilityQueryBuilderModifier
      */
-    private $doctrineHelper;
-
-    /**
-     * @param DoctrineHelper $doctrineHelper
-     * @param ConfigManager $configManager
-     */
-    public function __construct(
-        DoctrineHelper $doctrineHelper,
-        ConfigManager $configManager
-    ) {
-        $this->doctrineHelper = $doctrineHelper;
-        $this->configManager = $configManager;
+    public function __construct(ProductVisibilityQueryBuilderModifier $productVisibilityQueryBuilderModifier)
+    {
+        $this->productVisibilityQueryBuilderModifier = $productVisibilityQueryBuilderModifier;
     }
 
     /**
@@ -34,37 +25,9 @@ class RestrictSitemapProductByVisibilityListener
      */
     public function restrictQueryBuilder(RestrictSitemapEntitiesEvent $event)
     {
-        $queryBuilder = $event->getQueryBuilder();
-        $website = $event->getWebsite();
-
-        $productVisibilityTerm = $this->getProductVisibilityResolvedTermByWebsite(
-            $queryBuilder,
-            $website
+        $this->productVisibilityQueryBuilderModifier->restrictForAnonymous(
+            $event->getQueryBuilder(),
+            $event->getWebsite()
         );
-        $anonymousGroupVisibilityTerm = implode('+', [
-            $productVisibilityTerm,
-            $this->getCustomerGroupProductVisibilityResolvedTermByWebsite(
-                $queryBuilder,
-                $this->getAnonymousCustomerGroup(),
-                $website
-            )
-        ]);
-
-        $queryBuilder->andWhere($queryBuilder->expr()->gt($anonymousGroupVisibilityTerm, 0));
-    }
-
-    /**
-     * @return CustomerGroup|null
-     */
-    private function getAnonymousCustomerGroup()
-    {
-        $anonymousGroupId = $this->configManager->get('oro_customer.anonymous_customer_group');
-
-        /** @var CustomerGroup $customerGroup */
-        $customerGroup = $this->doctrineHelper
-            ->getEntityRepository(CustomerGroup::class)
-            ->find($anonymousGroupId);
-
-        return $customerGroup;
     }
 }
