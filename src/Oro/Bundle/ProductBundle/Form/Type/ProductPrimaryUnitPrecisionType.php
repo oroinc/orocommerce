82696588176adcf001272e90a8d07ed041de72f1
Volodymyr Denchyk <devit_dvv@ukr.net>
2017-03-16 18:17:22 +0200

    BB-7258: Duplication of unit precision on product edit page - fixed bug - added behat test for bugfix check

--- src/Oro/Bundle/ProductBundle/Form/Type/ProductPrimaryUnitPrecisionType.php
+++ src/Oro/Bundle/ProductBundle/Form/Type/ProductPrimaryUnitPrecisionType.php
@@ -2,8 +2,11 @@
 
 namespace Oro\Bundle\ProductBundle\Form\Type;
 
+use Doctrine\ORM\EntityRepository;
 use Oro\Bundle\ProductBundle\Entity\ProductUnitPrecision;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
@@ -28,15 +31,33 @@ class ProductPrimaryUnitPrecisionType extends AbstractType
 
     /**
      * @param FormBuilderInterface $builder
-     * @param array $options
+     * @param array                $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('precision', 'integer', ['type' => 'text', 'required' => false])
-            ->add('conversionRate', 'hidden', ['data' => 1])
-            ->add('sell', 'hidden', ['data' => true])
-        ;
+            ->add(
+                'precision',
+                IntegerType::class,
+                [
+                    'type' => 'text',
+                    'required' => false
+                ]
+            )
+            ->add(
+                'conversionRate',
+                HiddenType::class,
+                [
+                    'data' => 1
+                ]
+            )
+            ->add(
+                'sell',
+                HiddenType::class,
+                [
+                    'data' => true
+                ]
+            );
 
         $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($options) {
             $unitPrecision = $event->getData();
@@ -48,7 +69,20 @@ class ProductPrimaryUnitPrecisionType extends AbstractType
                     ProductUnitSelectionType::NAME,
                     [
                         'attr' => ['class' => 'unit'],
-                        'product' => $unitPrecision ? $unitPrecision->getProduct() : null
+                        'query_builder' => function (EntityRepository $er) use ($unitPrecision) {
+                            $excludedCode = '';
+                            if ($unitPrecision->getProduct()) {
+                                $additionalUnitPrecisions = $unitPrecision->getProduct()->getAdditionalUnitPrecisions();
+                                foreach ($additionalUnitPrecisions as $additionalUnitPrecision) {
+                                    $excludedCode = $additionalUnitPrecision->getUnit()->getCode();
+                                }
+                            }
+
+                            return $er
+                                ->createQueryBuilder('u')
+                                ->where('u.code != :codes')
+                                ->setParameter('codes', $excludedCode);
+                        },
                     ]
                 );
             } else {
@@ -58,7 +92,7 @@ class ProductPrimaryUnitPrecisionType extends AbstractType
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
     public function configureOptions(OptionsResolver $resolver)
     {
@@ -77,7 +111,7 @@ class ProductPrimaryUnitPrecisionType extends AbstractType
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
     public function getBlockPrefix()
     {
--- src/Oro/Bundle/ProductBundle/Tests/Behat/Context/FeatureContext.php
+++ src/Oro/Bundle/ProductBundle/Tests/Behat/Context/FeatureContext.php
@@ -2,18 +2,28 @@
 
 namespace Oro\Bundle\ProductBundle\Tests\Behat\Context;
 
+use Behat\Gherkin\Node\TableNode;
+use Behat\Mink\Element\NodeElement;
 use Behat\Symfony2Extension\Context\KernelAwareContext;
 use Behat\Symfony2Extension\Context\KernelDictionary;
+use Oro\Bundle\DataGridBundle\Tests\Behat\Element\Grid;
+use Oro\Bundle\NavigationBundle\Tests\Behat\Element\MainMenu;
 use Oro\Bundle\TestFrameworkBundle\Behat\Context\OroFeatureContext;
+use Oro\Bundle\TestFrameworkBundle\Behat\Element\Form;
 use Oro\Bundle\TestFrameworkBundle\Behat\Element\OroPageObjectAware;
 use Oro\Bundle\TestFrameworkBundle\Tests\Behat\Context\PageObjectDictionary;
 
+/**
+ * @SuppressWarnings(PHPMD.TooManyMethods)
+ * @SuppressWarnings(PHPMD.TooManyPublicMethods)
+ */
 class FeatureContext extends OroFeatureContext implements OroPageObjectAware, KernelAwareContext
 {
     use PageObjectDictionary, KernelDictionary;
 
     /**
      * @When I fill product name field with :productName value
+     *
      * @param string $productName
      */
     public function fillProductNameFieldWithValue($productName)
@@ -35,8 +45,9 @@ class FeatureContext extends OroFeatureContext implements OroPageObjectAware, Ke
 
     /**
      * @When I add product :productSku with quantity :productQuantity to quick order form
+     *
      * @param string $productSku
-     * @param int $productQuantity
+     * @param int    $productQuantity
      */
     public function addProductToQuickAddForm($productSku, $productQuantity)
     {
@@ -77,8 +88,9 @@ class FeatureContext extends OroFeatureContext implements OroPageObjectAware, Ke
 
     /**
      * @Then quick order form contains product with sku :productSku and quantity :productQuantity
+     *
      * @param string $productSku
-     * @param int $productQuantity
+     * @param int    $productQuantity
      */
     public function quickOrderFormContainsProductWithSkuAndQuantity($productSku, $productQuantity)
     {
@@ -93,5 +105,117 @@ class FeatureContext extends OroFeatureContext implements OroPageObjectAware, Ke
         );
 
         static::assertEquals($productQuantity, $firstQuantityField->getValue());
+    }
+
+    /**
+     * @Then I go to product with sku :productSku edit page
+     *
+     * @param string $productSku
+     */
+    public function goToProductEditPage($productSku)
+    {
+        /** @var MainMenu $mainMenu */
+        $mainMenu = $this->createElement('MainMenu');
+        $mainMenu->openAndClick('Products/ Products');
+        $this->waitForAjax();
+
+        /** @var Grid $grid */
+        $grid = $this->createElement('Grid');
+        $grid->clickActionLink($productSku, 'Edit');
+        $this->waitForAjax();
+    }
+
+    /**
+     * Example: Then I fill product fields with next data:
+     *            | Name                | Name      |
+     *            | SKU                 | SKU       |
+     *            | Status              | enabled   |
+     *            | PrimaryUnit         | item      |
+     *            | PrimaryPrecision    | 0         |
+     *            | AdditionalUnit      | set       |
+     *            | AdditionalPrecision | 0         |
+     * @Then I fill product fields with next data:
+     *
+     * @param TableNode $table
+     *
+     * @return Form
+     */
+    public function fillProductFieldsWithNextData(TableNode $table)
+    {
+        /** @var Form $form */
+        $form = $this->createElement('Product');
+
+        if (in_array('AdditionalUnit', $table->getColumn(0), true)) {
+            $additionalUnitAdd = $form->find('css', 'a.btn.add-list-item');
+            $additionalUnitAdd->click();
+            $this->waitForAjax();
+        }
+        $form->fill($table);
+
+        return $form;
+    }
+
+    /**
+     * Example: Then I save product with next data:
+     *            | Name                | Name      |
+     *            | SKU                 | SKU       |
+     *            | Status              | enabled   |
+     *            | PrimaryUnit         | item      |
+     *            | PrimaryPrecision    | 0         |
+     *            | AdditionalUnit      | set       |
+     *            | AdditionalPrecision | 0         |
+     * @Then I save product with next data:
+     *
+     * @param TableNode $table
+     */
+    public function saveProductWithNextData(TableNode $table)
+    {
+        $form = $this->fillProductFieldsWithNextData($table);
+        $form->saveAndClose();
+        $this->waitForAjax();
+    }
+
+    /**
+     * @Then I should see value ":value" in ":elementName" options
+     *
+     * @param string $value
+     * @param string $elementName
+     *
+     * @return boolean
+     */
+    public function shouldSeeValueInElementOptions($value, $elementName)
+    {
+        static::assertTrue(in_array($value, $this->getOptionsForElement($elementName), true));
+    }
+
+    /**
+     * @Then I should not see value ":value" in ":elementName" options
+     *
+     * @param string $value
+     * @param string $elementName
+     *
+     * @return boolean
+     */
+    public function shouldNotSeeValueInElementOptions($value, $elementName)
+    {
+        static::assertFalse(in_array($value, $this->getOptionsForElement($elementName), true));
+    }
+
+    /**
+     * @param string $elementName
+     *
+     * @return array
+     */
+    protected function getOptionsForElement($elementName)
+    {
+        $element = $this->createElement($elementName);
+        $optionElements = $element->findAll('css', 'option');
+        $options = [];
+        /** @var NodeElement[] $optionElements */
+        foreach ($optionElements as $option) {
+            $options[] = $option->getValue();
+        }
+
+        return $options;
     }
 }
