f0eb86b558bcaf0585a06c42a928317664f1dffb
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2017-03-30 16:02:06 +0300

    BB-8602: Create OroApruveBundle - added OroApruveBundle

---
+++ src/Oro/Bundle/ApruveBundle/Tests/Unit/OroApruveBundleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Oro\Bundle\ApruveBundle\Tests\Unit;
+
+use Oro\Bundle\ApruveBundle\OroApruveBundle;
+
+class OroApruveBundleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        new OroApruveBundle();
+    }
+}
