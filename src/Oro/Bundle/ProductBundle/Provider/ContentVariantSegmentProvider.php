4182eabff36c1411e46760d174678b50e61ea5de
Bemza Viacheslav <vbemza@oroinc.com>
2017-04-11 17:13:36 +0300

    BB-7915: Website Content Management - Product Collection - add ContentVariantSegmentProvider stub

---
+++ src/Oro/Bundle/ProductBundle/Provider/ContentVariantSegmentProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Oro\Bundle\ProductBundle\Provider;
+
+use Oro\Bundle\SegmentBundle\Entity\Segment;
+use Oro\Component\WebCatalog\Entity\ContentVariantInterface;
+
+class ContentVariantSegmentProvider
+{
+    /**
+     * @return Segment[]
+     */
+    public function getContentVariantSegments()
+    {
+        // TODO: will be implemented in BB-8819
+    }
+
+    /**
+     * @param Segment $segment
+     * @return ContentVariantInterface|null
+     */
+    public function getContentVariant(Segment $segment)
+    {
+        // TODO: will be implemented in BB-8819
+    }
+
+    /**
+     * @param Segment $segment
+     * @return bool
+     */
+    public function hasContentVariant(Segment $segment)
+    {
+        // TODO: will be implemented in BB-8819
+    }
+}
