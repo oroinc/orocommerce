08659ca9ebcb3db09e967a51cee396d1c584454b
Oleg Popadko <opopadko@oroinc.com>
2017-04-12 16:46:52 +0530

    BB-8786: Merge DPD shipping integration  - using ShippingMethodValidatorInterface for validating shipping services

--- src/Oro/Bundle/DPDBundle/Tests/Unit/Method/DPDShippingMethodProviderTest.php
+++ src/Oro/Bundle/DPDBundle/Tests/Unit/Method/DPDShippingMethodProviderTest.php
@@ -2,102 +2,29 @@
 
 namespace Oro\Bundle\DPDBundle\Tests\Unit\Method;
 
-use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
-use Oro\Bundle\IntegrationBundle\Entity\Channel;
-use Oro\Bundle\IntegrationBundle\Entity\Repository\ChannelRepository;
-use Oro\Bundle\ShippingBundle\Method\Factory\IntegrationShippingMethodFactoryInterface;
-use Oro\Bundle\DPDBundle\Method\DPDShippingMethod;
 use Oro\Bundle\DPDBundle\Method\DPDShippingMethodProvider;
-use Oro\Bundle\DPDBundle\Provider\ChannelType;
-use Oro\Component\Testing\Unit\EntityTrait;
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Oro\Bundle\ShippingBundle\Method\Factory\IntegrationShippingMethodFactoryInterface;
 
 class DPDShippingMethodProviderTest extends \PHPUnit_Framework_TestCase
 {
-    use EntityTrait;
+    const CHANNEL_TYPE = 'channel_type';
 
-    /**
-     * @var DPDShippingMethodProvider
-     */
-    private $provider;
+    /** @var \PHPUnit_Framework_MockObject_MockObject|IntegrationShippingMethodFactoryInterface */
+    private $methodBuilder;
 
-    /**
-     * @var DoctrineHelper|\PHPUnit_Framework_MockObject_MockObject
-     */
+    /** @var \PHPUnit_Framework_MockObject_MockObject|DoctrineHelper */
     private $doctrineHelper;
-
-    /**
-     * @var IntegrationShippingMethodFactoryInterface|\PHPUnit_Framework_MockObject_MockObject
-     */
-    private $methodFactory;
-
-    /**
-     * @var DPDShippingMethod
-     */
-    private $method;
 
     public function setUp()
     {
-        $repository = $this->createMock(ChannelRepository::class);
+        $this->methodBuilder = $this->createMock(IntegrationShippingMethodFactoryInterface::class);
 
         $this->doctrineHelper = $this->createMock(DoctrineHelper::class);
-
-        $this->doctrineHelper->expects(static::once())
-            ->method('getEntityRepository')
-            ->with('OroIntegrationBundle:Channel')
-            ->willReturn($repository);
-
-        $channel = $this->getEntity(Channel::class, [
-            'id' => 10,
-            'enabled' => true,
-        ]);
-
-        $repository->expects(static::once())
-            ->method('findByType')
-            ->with(ChannelType::TYPE)
-            ->willReturn([$channel]);
-
-        $this->method = $this->createMock(DPDShippingMethod::class);
-        $this->method
-            ->method('getIdentifier')
-            ->willReturn('dpd_10');
-
-        $this->methodFactory = $this->createMock(IntegrationShippingMethodFactoryInterface::class);
-
-        $this->methodFactory->expects($this->once())
-            ->method('create')
-            ->with($channel)
-            ->willReturn($this->method);
-
-        $this->provider = new DPDShippingMethodProvider($this->doctrineHelper, $this->methodFactory);
     }
 
-    public function testGetShippingMethods()
+    public function testConstructor()
     {
-        $methods = $this->provider->getShippingMethods();
-        static::assertCount(1, $methods);
-        $actualMethod = reset($methods);
-        static::assertSame($this->method, $actualMethod);
-    }
-
-    public function testGetShippingMethod()
-    {
-        $method = $this->provider->getShippingMethod($this->method->getIdentifier());
-        static::assertInstanceOf(DPDShippingMethod::class, $method);
-    }
-
-    public function testGetShippingMethodNull()
-    {
-        $method = $this->provider->getShippingMethod('wrong');
-        static::assertNull($method);
-    }
-
-    public function testHasShippingMethod()
-    {
-        static::assertTrue($this->provider->hasShippingMethod($this->method->getIdentifier()));
-    }
-
-    public function testHasShippingMethodFalse()
-    {
-        static::assertFalse($this->provider->hasShippingMethod('wrong'));
+        new DPDShippingMethodProvider(static::CHANNEL_TYPE, $this->doctrineHelper, $this->methodBuilder);
     }
 }
--- src/Oro/Bundle/DPDBundle/Validator/Constraints/RemoveUsedShippingServiceValidator.php
+++ src/Oro/Bundle/DPDBundle/Validator/Constraints/RemoveUsedShippingServiceValidator.php
@@ -2,189 +2,83 @@
 
 namespace Oro\Bundle\DPDBundle\Validator\Constraints;
 
-use Doctrine\Common\Collections\Collection;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Oro\Bundle\DPDBundle\Entity\DPDTransport;
 use Oro\Bundle\IntegrationBundle\Entity\Channel;
-use Oro\Bundle\ShippingBundle\Entity\ShippingMethodConfig;
-use Oro\Bundle\ShippingBundle\Entity\ShippingMethodTypeConfig;
-use Oro\Bundle\ShippingBundle\Method\ShippingMethodRegistry;
-use Oro\Bundle\DPDBundle\Entity\ShippingService;
-use Oro\Bundle\DPDBundle\Method\DPDShippingMethod;
-use Oro\Component\PropertyAccess\PropertyAccessor;
-use Symfony\Component\Form\FormInterface;
+use Oro\Bundle\ShippingBundle\Method\Factory\IntegrationShippingMethodFactoryInterface;
+use Oro\Bundle\ShippingBundle\Method\Validator\Result\ShippingMethodValidatorResultInterface;
+use Oro\Bundle\ShippingBundle\Method\Validator\ShippingMethodValidatorInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class RemoveUsedShippingServiceValidator extends ConstraintValidator
 {
     const ALIAS = 'oro_dpd_remove_used_shipping_service_validator';
 
     /**
-     * @var ManagerRegistry
+     * @internal
      */
-    protected $doctrine;
+    const VIOLATION_PATH = 'applicableShippingServices';
 
     /**
-     * @var ShippingMethodRegistry
+     * @var IntegrationShippingMethodFactoryInterface
      */
-    protected $registry;
+    private $integrationShippingMethodFactory;
 
     /**
-     * @var PropertyAccessor
+     * @var ShippingMethodValidatorInterface
      */
-    protected $propertyAccessor;
+    private $shippingMethodValidator;
 
     /**
-     * @param ManagerRegistry        $doctrine
-     * @param ShippingMethodRegistry $registry
+     * @param IntegrationShippingMethodFactoryInterface $integrationShippingMethodFactory
+     * @param ShippingMethodValidatorInterface          $shippingMethodValidator
      */
-    public function __construct(ManagerRegistry $doctrine, ShippingMethodRegistry $registry)
-    {
-        $this->doctrine = $doctrine;
-        $this->registry = $registry;
+    public function __construct(
+        IntegrationShippingMethodFactoryInterface $integrationShippingMethodFactory,
+        ShippingMethodValidatorInterface $shippingMethodValidator
+    ) {
+        $this->integrationShippingMethodFactory = $integrationShippingMethodFactory;
+        $this->shippingMethodValidator = $shippingMethodValidator;
     }
 
     /**
-     * @param Collection                           $value
-     * @param Constraint|RemoveUsedShippingService $constraint
-     *
-     * @throws \UnexpectedValueException
-     * @throws \InvalidArgumentException
+     * @param DPDTransport                                   $value
+     * @param Constraint|RemoveUsedShippingServiceConstraint $constraint
      */
     public function validate($value, Constraint $constraint)
     {
-        if ($value) {
-            $dpdTypeIds = $this->getDpdTypesIds($value);
-
-            $channelId = $this->getChannelId();
-            if (null !== $channelId) {
-                $methodIdentifier = DPDShippingMethod::IDENTIFIER.'_'.$channelId;
-                $shippingMethod = $this->registry->getShippingMethod($methodIdentifier);
-                if (null !== $shippingMethod) {
-                    $configuredMethods = $this
-                        ->doctrine
-                        ->getManagerForClass('OroShippingBundle:ShippingMethodConfig')
-                        ->getRepository('OroShippingBundle:ShippingMethodConfig')
-                        ->findBy(['method' => $methodIdentifier]);
-                    if (count($configuredMethods) > 0) {
-                        /** @var ShippingMethodConfig $configuredMethod */
-                        foreach ($configuredMethods as $configuredMethod) {
-                            $configuredTypes = $configuredMethod->getTypeConfigs()->toArray();
-                            $enabledTypes = $this->getEnabledTypes($configuredTypes);
-                            $diff = array_diff($enabledTypes, $dpdTypeIds);
-                            if (0 < count($diff) && (count($enabledTypes) >= count($dpdTypeIds))) {
-                                $missingServices = $this
-                                    ->doctrine
-                                    ->getManagerForClass('OroDPDBundle:ShippingService')
-                                    ->getRepository('OroDPDBundle:ShippingService')
-                                    ->findBy(['code' => $diff]);
-
-                                $this->addViolations($missingServices, $constraint->message);
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * @param Collection $value
-     *
-     * @return array
-     */
-    protected function getDpdTypesIds($value)
-    {
-        $dpdTypesIds = [];
-        /** @var ShippingService $dpdService */
-        foreach ($value->toArray() as $dpdService) {
-            $dpdTypesIds[] = $dpdService->getCode();
+        if (!$value instanceof DPDTransport) {
+            return;
         }
 
-        return $dpdTypesIds;
-    }
-
-    /**
-     * @param array $configuredTypes
-     *
-     * @return array
-     */
-    protected function getEnabledTypes($configuredTypes)
-    {
-        $enabledTypes = [];
-        /** @var ShippingMethodTypeConfig $confType */
-        foreach ($configuredTypes as $confType) {
-            if ($confType->isEnabled()) {
-                $enabledTypes[] = $confType->getType();
-            }
+        if (!$value->getChannel() instanceof Channel) {
+            return;
         }
 
-        return $enabledTypes;
+        $upsShippingMethod = $this->integrationShippingMethodFactory->create($value->getChannel());
+        $shippingMethodValidatorResult = $this->shippingMethodValidator->validate($upsShippingMethod);
+
+        $this->handleValidationResult($shippingMethodValidatorResult);
     }
 
     /**
-     * @param array  $missingServices
-     * @param string $message
+     * @param ShippingMethodValidatorResultInterface $shippingMethodValidatorResult
      */
-    protected function addViolations($missingServices, $message)
+    private function handleValidationResult(ShippingMethodValidatorResultInterface $shippingMethodValidatorResult)
     {
-        /** @var ShippingService $service */
-        foreach ($missingServices as $service) {
-            $this->context->addViolation($message, [
-                '{{ service }}' => $service->getDescription(),
-            ]);
-        }
-    }
-
-    /**
-     * @return PropertyAccessor
-     */
-    protected function getPropertyAccessor()
-    {
-        if (!$this->propertyAccessor) {
-            $this->propertyAccessor = new PropertyAccessor();
+        if ($shippingMethodValidatorResult->getErrors()->isEmpty()) {
+            return;
         }
 
-        return $this->propertyAccessor;
-    }
+        /** @var ExecutionContextInterface $context */
+        $context = $this->context;
 
-    /**
-     * @return string|null
-     */
-    protected function getChannelId()
-    {
-        $id = null;
-
-        $form = $this->getForm();
-        while ($form) {
-            if ($form->getData() instanceof Channel) {
-                $id = $form->getData()->getId();
-                break;
-            }
-            $form = $form->getParent();
+        foreach ($shippingMethodValidatorResult->getErrors() as $error) {
+            $context->buildViolation($error->getMessage())
+                ->setTranslationDomain(null)
+                ->atPath(static::VIOLATION_PATH)
+                ->addViolation();
         }
-
-        return $id;
-    }
-
-    /**
-     * @return FormInterface
-     */
-    protected function getForm()
-    {
-        return $this->getPropertyAccessor()->getValue($this->context->getRoot(), $this->getFormPath());
-    }
-
-    /**
-     * @return string
-     */
-    protected function getFormPath()
-    {
-        $path = $this->context->getPropertyPath();
-        $path = str_replace(['children', '.'], '', $path);
-        $path = preg_replace('/\][^\]]*$/', ']', $path);
-
-        return $path;
     }
 }
