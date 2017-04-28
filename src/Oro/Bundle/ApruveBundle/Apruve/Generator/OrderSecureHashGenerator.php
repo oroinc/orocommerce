79faa30e6bf1bfb7ab0eb62829d0b45ef479c41d
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2017-04-28 19:54:16 +0300

    BB-8955: Create secure hash generator - fixed code review issues - replaced ApruveConfig dependency with plain apiKey

--- src/Oro/Bundle/ApruveBundle/Apruve/Generator/OrderSecureHashGenerator.php
+++ src/Oro/Bundle/ApruveBundle/Apruve/Generator/OrderSecureHashGenerator.php
@@ -2,92 +2,78 @@
 
 namespace Oro\Bundle\ApruveBundle\Apruve\Generator;
 
-use Oro\Bundle\ApruveBundle\Apruve\Model\Order\ApruveOrderInterface;
-use Oro\Bundle\ApruveBundle\Method\Config\ApruveConfigInterface;
+use Oro\Bundle\ApruveBundle\Apruve\Model\ApruveLineItem;
+use Oro\Bundle\ApruveBundle\Apruve\Model\ApruveOrder;
 
 /**
  * @see https://docs.apruve.com/guides/merchant-integration-tutorial#1b-creating-a-secure-hash
  */
 class OrderSecureHashGenerator implements OrderSecureHashGeneratorInterface
 {
-    const HASH_ALGO = 'sha256';
+    /**
+     * @internal
+     */
+    const HASH_ALGORITHM = 'sha256';
+
+    /**
+     * @internal
+     */
     const DELIMITER = '';
 
     /**
-     * Order
+     * Order fields order.
      */
-    const ORDER_MERCHANT_ID = 'merchant_id';
-    const ORDER_AMOUNT_CENTS = 'amount_cents';
-    const ORDER_CURRENCY = 'currency';
-    const ORDER_LINE_ITEMS = 'order_items';
-    const ORDER_MERCHANT_ORDER_ID = 'merchant_order_id';
-    const ORDER_TAX_CENTS = 'tax_cents';
-    const ORDER_SHIPPING_CENTS = 'shipping_cents';
-    const ORDER_EXPIRE_AT = 'expire_at';
-    const ORDER_ACCEPTS_PT = 'accepts_payment_terms';
-    const ORDER_FINALIZE_ON_CREATE = 'finalize_on_create';
-    const ORDER_INVOICE_ON_CREATE = 'invoice_on_create';
-
     const ORDER_FIELDS_ORDER = [
-        self::ORDER_MERCHANT_ID,
-        self::ORDER_MERCHANT_ORDER_ID,
-        self::ORDER_AMOUNT_CENTS,
-        self::ORDER_CURRENCY,
-        self::ORDER_TAX_CENTS,
-        self::ORDER_SHIPPING_CENTS,
-        self::ORDER_EXPIRE_AT,
-        self::ORDER_ACCEPTS_PT,
-        self::ORDER_FINALIZE_ON_CREATE,
-        self::ORDER_INVOICE_ON_CREATE,
+        ApruveOrder::MERCHANT_ID,
+        ApruveOrder::MERCHANT_ORDER_ID,
+        ApruveOrder::AMOUNT_CENTS,
+        ApruveOrder::CURRENCY,
+        ApruveOrder::TAX_CENTS,
+        ApruveOrder::SHIPPING_CENTS,
+        ApruveOrder::EXPIRE_AT,
+        ApruveOrder::ACCEPTS_PAYMENT_TERMS,
+        ApruveOrder::FINALIZE_ON_CREATE,
+        ApruveOrder::INVOICE_ON_CREATE,
     ];
 
     /**
-     * Line Item
+     * Line Item fields order.
      */
-    const LINE_ITEM_PRICE_TOTAL_CENTS = 'price_total_cents';
-    /**
-     * Property 'price_total_cents' is not respected by Apruve when secure hash is generated,
-     * hence use 'amount_cents' instead.
-     */
-    const LINE_ITEM_AMOUNT_CENTS = 'amount_cents';
-    const LINE_ITEM_QUANTITY = 'quantity';
-    const LINE_ITEM_VARIANT_INFO = 'variant_info';
-    const LINE_ITEM_SKU = 'sku';
-    const LINE_ITEM_TITLE = 'title';
-    const LINE_ITEM_DESCRIPTION = 'description';
-    const LINE_ITEM_VIEW_PRODUCT_URL = 'view_product_url';
-    const LINE_ITEM_PRICE_EA_CENTS = 'price_ea_cents';
-    const LINE_ITEM_VENDOR = 'vendor';
-    const LINE_ITEM_MERCHANT_NOTES = 'merchant_notes';
-
     const LINE_ITEM_FIELDS_ORDER = [
-        self::LINE_ITEM_TITLE,
-        self::LINE_ITEM_AMOUNT_CENTS,
-        self::LINE_ITEM_PRICE_EA_CENTS,
-        self::LINE_ITEM_QUANTITY,
-        self::LINE_ITEM_MERCHANT_NOTES,
-        self::LINE_ITEM_DESCRIPTION,
-        self::LINE_ITEM_VARIANT_INFO,
-        self::LINE_ITEM_SKU,
-        self::LINE_ITEM_VENDOR,
-        self::LINE_ITEM_VIEW_PRODUCT_URL,
+        ApruveLineItem::TITLE,
+        ApruveLineItem::AMOUNT_CENTS,
+        ApruveLineItem::PRICE_EA_CENTS,
+        ApruveLineItem::QUANTITY,
+        ApruveLineItem::MERCHANT_NOTES,
+        ApruveLineItem::DESCRIPTION,
+        ApruveLineItem::VARIANT_INFO,
+        ApruveLineItem::SKU,
+        ApruveLineItem::VENDOR,
+        ApruveLineItem::VIEW_PRODUCT_URL,
     ];
 
     /**
      * {@inheritDoc}
      */
-    public function generate(ApruveOrderInterface $apruveOrder, ApruveConfigInterface $config)
+    public function generate(ApruveOrder $apruveOrder, $apiKey)
     {
         $data = $apruveOrder->getData();
-        $lineItems =& $data[self::ORDER_LINE_ITEMS];
 
-        // API key must be in the beginning of string.
-        $secureString = $config->getApiKey();
-        $secureString .= $this->makeSecureString($data, self::ORDER_FIELDS_ORDER);
+        // API key must be in the beginning.
+        $secureArray[] = $apiKey;
 
-        foreach ((array) $lineItems as $lineItemData) {
-            $secureString .= $this->makeSecureString($lineItemData, self::LINE_ITEM_FIELDS_ORDER);
+        // Then goes order data.
+        $secureArray = array_merge($secureArray, $this->prepareSecureArray($data, self::ORDER_FIELDS_ORDER));
+
+        // Then goes order line items data.
+        foreach ($data[ApruveOrder::LINE_ITEMS] as $lineItemData) {
+            $secureArray = array_merge(
+                $secureArray,
+                $this->prepareSecureArray($lineItemData, self::LINE_ITEM_FIELDS_ORDER)
+            );
         }
+
+        $secureString = $this->convertToString($secureArray);
 
         return $this->makeSecureHash($secureString);
     }
@@ -99,20 +85,7 @@ class OrderSecureHashGenerator implements OrderSecureHashGeneratorInterface
      */
     protected function makeSecureHash($secureString)
     {
-        return hash(self::HASH_ALGO, $secureString);
-    }
-
-    /**
-     * @param array $array
-     * @param array $arrayTpl
-     *
-     * @return string
-     */
-    protected function makeSecureString(array $array, array $arrayTpl)
-    {
-        $array = $this->prepareDataArray($array, $arrayTpl);
-
-        return $this->convertToString($array);
+        return hash(self::HASH_ALGORITHM, $secureString);
     }
 
     /**
@@ -124,57 +97,24 @@ class OrderSecureHashGenerator implements OrderSecureHashGeneratorInterface
     {
         $array = array_map([$this, 'convertTypes'], $array);
 
-        $string = implode(self::DELIMITER, $array);
-        // Remove line breaks, they are not allowed.
-        $string = str_replace(PHP_EOL, ' ', $string);
-
-        return $string;
+        return implode(self::DELIMITER, $array);
     }
 
     /**
      * @param array $array
-     * @param array $arrayTpl
+     * @param array $requiredOrderedFields
      *
      * @return array
      */
-    protected function prepareDataArray(array $array, array $arrayTpl)
+    protected function prepareSecureArray(array $array, array $requiredOrderedFields)
     {
         // Remove elements which are not present in the array template.
-        $array = array_intersect_key($array, array_flip($arrayTpl));
+        $array = array_intersect_key($array, array_flip($requiredOrderedFields));
 
-        $this->sortWithTemplate($array, $arrayTpl);
+        // Sort elements according to required fields array.
+        $array = array_merge(array_flip($requiredOrderedFields), $array);
 
         return array_values($array);
-    }
-
-    /**
-     * @param array $array
-     * @param array $arrayTpl
-     */
-    protected function sortWithTemplate(array &$array, array $arrayTpl)
-    {
-        uksort($array, $this->getSortCallback($array, $arrayTpl));
-    }
-
-    /**
-     * @param array $array
-     * @param array $arrayTpl
-     *
-     * @return \Closure
-     */
-    protected function getSortCallback(array $array, array $arrayTpl)
-    {
-        $arrayKeys = array_keys($array);
-        return function ($key1, $key2) use ($arrayKeys, $arrayTpl) {
-            $index1 = array_search($key1, $arrayTpl, true);
-            $index2 = array_search($key2, $arrayTpl, true);
-
-            if ($index1 === $index2) {
-                return 0;
-            }
-
-            return $index1 > $index2 ? 1 : -1;
-        };
     }
 
     /**
@@ -189,11 +129,11 @@ class OrderSecureHashGenerator implements OrderSecureHashGeneratorInterface
         switch (true) {
             case ($value === true):
                 return 'true';
-            break;
+                break;
 
             case ($value === false):
                 return 'false';
-            break;
+                break;
 
             default:
                 return $value;
--- src/Oro/Bundle/ApruveBundle/Apruve/Generator/OrderSecureHashGeneratorInterface.php
+++ src/Oro/Bundle/ApruveBundle/Apruve/Generator/OrderSecureHashGeneratorInterface.php
@@ -2,16 +2,15 @@
 
 namespace Oro\Bundle\ApruveBundle\Apruve\Generator;
 
-use Oro\Bundle\ApruveBundle\Apruve\Model\Order\ApruveOrderInterface;
-use Oro\Bundle\ApruveBundle\Method\Config\ApruveConfigInterface;
+use Oro\Bundle\ApruveBundle\Apruve\Model\ApruveOrder;
 
 interface OrderSecureHashGeneratorInterface
 {
     /**
-     * @param ApruveOrderInterface $apruveOrder
-     * @param ApruveConfigInterface $config
+     * @param ApruveOrder $apruveOrder
+     * @param string      $apiKey Merchant API key.
      *
      * @return string
      */
-    public function generate(ApruveOrderInterface $apruveOrder, ApruveConfigInterface $config);
+    public function generate(ApruveOrder $apruveOrder, $apiKey);
 }
