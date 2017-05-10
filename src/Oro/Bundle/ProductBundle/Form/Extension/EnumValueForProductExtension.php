b440e1b98294413818c1fe0080e4af93f1b009b8
Iaroslav Bratsilo <ibratsilo@oroinc.com>
2017-05-10 12:41:00 +0300

    BB-9332: Delete unneeded files after changesproduct family  - fixes according to review

---
+++ src/Oro/Bundle/ProductBundle/Form/Extension/EnumValueForProductExtension.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Oro\Bundle\ProductBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Oro\Bundle\EntityConfigBundle\Config\Id\FieldConfigId;
+use Oro\Bundle\ProductBundle\Entity\Product;
+use Oro\Bundle\EntityExtendBundle\Form\Type\EnumValueType;
+use Oro\Bundle\EntityConfigBundle\Config\ConfigManager;
+
+/**
+ * Restored this class in - #BB-9332
+ */
+class EnumValueForProductExtension extends AbstractTypeExtension
+{
+    const PRODUCT_SKU_IN_TOOLTIP = 10;
+
+    /** @var DoctrineHelper */
+    private $doctrineHelper;
+
+    /** @var ConfigManager */
+    private $configManager;
+
+    /**
+     * @param DoctrineHelper $doctrineHelper
+     * @param ConfigManager $configManager
+     */
+    public function __construct(DoctrineHelper $doctrineHelper, ConfigManager $configManager)
+    {
+        $this->doctrineHelper = $doctrineHelper;
+        $this->configManager = $configManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return EnumValueType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $data = $form->getData();
+        if (!$data || !array_key_exists('id', $data) || !$data['id']) {
+            return;
+        }
+
+        /** @var FieldConfigId $configId */
+        $configId = $form->getParent()->getConfig()->getOption('config_id');
+
+        if (!$this->isValidAttributeEnum($configId)) {
+            return;
+        }
+
+        $configProductsSkuUsingEnum = $this->getProductSkuUsingEnum($configId, $data['id']);
+
+        if (count($configProductsSkuUsingEnum) > self::PRODUCT_SKU_IN_TOOLTIP) {
+            $tooltipParameterMessage = sprintf(
+                '%s ...',
+                implode(', ', array_slice($configProductsSkuUsingEnum, 0, self::PRODUCT_SKU_IN_TOOLTIP))
+            );
+        } else {
+            $tooltipParameterMessage = implode(', ', $configProductsSkuUsingEnum);
+        }
+
+        if ($configProductsSkuUsingEnum) {
+            $view->vars['tooltip'] = 'oro.product.non_deletable_enum_value.tooltip';
+            $view->vars['tooltip_parameters'] = ['%skuList%' => $tooltipParameterMessage];
+            $view->vars['allow_delete'] = false;
+        }
+    }
+
+    /**
+     * @param FieldConfigId|null $configId
+     * @return bool
+     */
+    private function isValidAttributeEnum($configId)
+    {
+        $attributeConfigProvider = $this->configManager->getProvider('attribute');
+        $attributeConfig = $attributeConfigProvider->getConfig($configId->getClassName(), $configId->getFieldName());
+
+        return (!$configId || !$configId instanceof FieldConfigId ||
+            !is_a($configId->getClassName(), Product::class, true) ||
+            !$attributeConfig->is('is_attribute')
+        );
+    }
+
+    /**
+     * @param FieldConfigId $configId
+     * @param int $enumValueId
+     * @return array
+     */
+    private function getProductSkuUsingEnum(FieldConfigId $configId, $enumValueId)
+    {
+        $productRepository = $this->doctrineHelper->getEntityRepositoryForClass(Product::class);
+
+        /** @var Product[] $productsUsedEnumValue */
+        $productsUsedEnumValue = $productRepository->findBy([
+            'type' => Product::TYPE_SIMPLE,
+            $configId->getFieldName() => $enumValueId
+        ]);
+
+        $configProductsSkuUsingEnum = [];
+
+        foreach ($productsUsedEnumValue as $simpleProduct) {
+            foreach ($simpleProduct->getParentVariantLinks() as $parentVariantLink) {
+                $parentProduct = $parentVariantLink->getParentProduct();
+
+                if (in_array($configId->getFieldName(), $parentProduct->getVariantFields())) {
+                    $configProductsSkuUsingEnum[$parentProduct->getSku()] = $parentProduct->getSku();
+                }
+            }
+        }
+
+        return $configProductsSkuUsingEnum;
+    }
+}
