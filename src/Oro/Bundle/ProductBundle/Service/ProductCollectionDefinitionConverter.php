eecbee38f78f22ffdbdd872390d94cc21e7d7288
Dmitry Khrysev <dkhrysev@magecore.com>
2017-05-12 18:29:52 +0300

    BB-9438: Service for transform and reverse transform included/excluded to definition - stub update

--- src/Oro/Bundle/ProductBundle/Service/ProductCollectionDefinitionConverter.php
+++ src/Oro/Bundle/ProductBundle/Service/ProductCollectionDefinitionConverter.php
@@ -15,13 +15,13 @@ class ProductCollectionDefinitionConverter
     /**
      * Put excluded or\and included filters into definition.
      *
-     * @param array $definition
-     * @param array $excluded
-     * @param array $included
+     * @param string $definition
+     * @param string $excluded
+     * @param string $included
      *
-     * @return array
+     * @return string
      */
-    public function putConditionsInDefinition(array $definition, array $excluded = [], array $included = []): array
+    public function putConditionsInDefinition($definition, $excluded = null, $included = null)
     {
         // Stub, will be implemented in BB-9438
         return $definition;
@@ -31,22 +31,22 @@ class ProductCollectionDefinitionConverter
      * Receive definition and return definition without excluded&included,
      * the later will be returned as separate elements. Example or returned array:
      * [
-     *      'definition' => definition array, without excluded&included
-     *      'excluded'   => excluded filter value, if were found in definition
-     *      'included'   => included filter value, if were found in definition
+     *      'definition' => definition string as json encoded array, without excluded&included
+     *      'excluded'   => excluded filter value string, if were found in definition
+     *      'included'   => included filter value string, if were found in definition
      * ]
      *
      * @param array $definition
      *
      * @return array
      */
-    public function getDefinitionParts(array $definition): array
+    public function getDefinitionParts(array $definition)
     {
         // Stub, will be implemented in BB-9438
         return [
             self::DEFINITION_KEY => $definition,
-            self::INCLUDED_FILTER_KEY => ['1','2','3'],
-            self::EXCLUDED_FILTER_KEY => ['1','2','3']
+            self::INCLUDED_FILTER_KEY => '1,2,3',
+            self::EXCLUDED_FILTER_KEY => '4'
         ];
     }
 }
