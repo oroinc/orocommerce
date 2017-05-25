376d023539b71415a52ffb0b962e24f14d665c02
Dmitry Khrysev <x86demon@users.noreply.github.com>
2017-05-25 16:35:38 +0300

    BB-9703: Add AJAX action to get records count for grid (#10514)
    * BB-9703: Add AJAX action to get records count for grid
    
    * BB-9703: Add AJAX action to get records count for grid
    
    * BB-9703: Add AJAX action to get records count for grid
    
    * BB-9703: Add AJAX action to get records count for grid
    - fix docblock during review

---
+++ src/Oro/Bundle/ProductBundle/Tests/Unit/Provider/GridCountProviderTest.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace Oro\Bundle\ProductBundle\Tests\Unit\Provider;
+
+use Doctrine\ORM\QueryBuilder;
+use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
+use Oro\Bundle\DataGridBundle\Datagrid\DatagridInterface;
+use Oro\Bundle\DataGridBundle\Datagrid\ManagerInterface;
+use Oro\Bundle\DataGridBundle\Datagrid\ParameterBag;
+use Oro\Bundle\DataGridBundle\Datasource\DatasourceInterface;
+use Oro\Bundle\DataGridBundle\Datasource\Orm\OrmDatasource;
+use Oro\Bundle\DataGridBundle\Extension\Pager\Orm\Pager;
+use Oro\Bundle\FilterBundle\Grid\Extension\AbstractFilterExtension;
+use Oro\Bundle\ProductBundle\Provider\GridCountProvider;
+use Oro\Bundle\SecurityBundle\SecurityFacade;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class GridCountProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ManagerInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $gridManager;
+
+    /**
+     * @var SecurityFacade|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $securityFacade;
+
+    /**
+     * @var Pager|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $pager;
+
+    /**
+     * @var GridCountProvider
+     */
+    private $provider;
+
+    protected function setUp()
+    {
+        $this->gridManager = $this->createMock(ManagerInterface::class);
+        $this->securityFacade = $this->createMock(SecurityFacade::class);
+        $this->pager = $this->createMock(Pager::class);
+
+        $this->provider = new GridCountProvider($this->gridManager, $this->securityFacade, $this->pager);
+    }
+
+    public function testAclException()
+    {
+        $gridName = 'test';
+
+        $config = $this->createMock(DatagridConfiguration::class);
+        $config->expects($this->once())
+            ->method('getAclResource')
+            ->willReturn('test_acl');
+        $this->gridManager->expects($this->once())
+            ->method('getConfigurationForGrid')
+            ->willReturn($config);
+        $this->securityFacade->expects($this->once())
+            ->method('isGranted')
+            ->with('test_acl')
+            ->willReturn(false);
+
+        $this->expectException(AccessDeniedException::class);
+
+        $this->provider->getGridCount($gridName);
+    }
+
+    public function testGetCountWithNonOrmDataSource()
+    {
+        $gridName = 'test';
+
+        $this->assertConfigCalls();
+
+        $dataSource = $this->createMock(DatasourceInterface::class);
+        $this->assertDatasourceCalls($dataSource);
+
+        $this->assertEquals(0, $this->provider->getGridCount($gridName));
+    }
+
+
+    public function testGetCountWithCustomCountQuery()
+    {
+        $gridName = 'test';
+
+        $this->assertConfigCalls();
+
+        $countQb = $this->createMock(QueryBuilder::class);
+        $qb = $this->createMock(QueryBuilder::class);
+
+        $dataSource = $this->createMock(OrmDatasource::class);
+        $dataSource->expects($this->once())
+            ->method('getQueryBuilder')
+            ->willReturn($qb);
+        $dataSource->expects($this->once())
+            ->method('getCountQb')
+            ->willReturn($countQb);
+        $dataSource->expects($this->once())
+            ->method('getCountQueryHints')
+            ->willReturn([]);
+
+        $this->pager->expects($this->once())
+            ->method('setQueryBuilder')
+            ->with($qb);
+        $this->pager->expects($this->once())
+            ->method('setCountQb')
+            ->with($countQb, []);
+        $this->pager->expects($this->once())
+            ->method('computeNbResult')
+            ->willReturn(42);
+
+        $this->assertDatasourceCalls($dataSource);
+
+        $this->securityFacade->expects($this->never())
+            ->method('isGranted');
+
+        $this->assertEquals(42, $this->provider->getGridCount($gridName));
+    }
+
+    public function testGetCountWithoutCustomCountQuery()
+    {
+        $gridName = 'test';
+
+        $this->assertConfigCalls();
+
+        $qb = $this->createMock(QueryBuilder::class);
+
+        $dataSource = $this->createMock(OrmDatasource::class);
+        $dataSource->expects($this->once())
+            ->method('getQueryBuilder')
+            ->willReturn($qb);
+        $dataSource->expects($this->once())
+            ->method('getCountQb')
+            ->willReturn(null);
+        $dataSource->expects($this->never())
+            ->method('getCountQueryHints');
+
+        $this->pager->expects($this->once())
+            ->method('setQueryBuilder')
+            ->with($qb);
+        $this->pager->expects($this->never())
+            ->method('setCountQb');
+        $this->pager->expects($this->once())
+            ->method('computeNbResult')
+            ->willReturn(42);
+
+        $this->assertDatasourceCalls($dataSource);
+
+        $this->securityFacade->expects($this->never())
+            ->method('isGranted');
+
+        $this->assertEquals(42, $this->provider->getGridCount($gridName));
+    }
+
+    /**
+     * @param \PHPUnit_Framework_MockObject_MockObject $dataSource
+     */
+    private function assertDatasourceCalls(\PHPUnit_Framework_MockObject_MockObject $dataSource)
+    {
+        $parameters = $this->createMock(ParameterBag::class);
+        $parameters->expects($this->once())
+            ->method('set')
+            ->with(AbstractFilterExtension::FILTER_ROOT_PARAM, []);
+        $grid = $this->createMock(DatagridInterface::class);
+        $grid->expects($this->once())
+            ->method('getParameters')
+            ->willReturn($parameters);
+        $grid->expects($this->once())
+            ->method('getAcceptedDatasource')
+            ->willReturn($dataSource);
+        $this->gridManager->expects($this->once())
+            ->method('getDatagridByRequestParams')
+            ->willReturn($grid);
+    }
+
+    private function assertConfigCalls()
+    {
+        $config = $this->createMock(DatagridConfiguration::class);
+        $config->expects($this->once())
+            ->method('getAclResource')
+            ->willReturn(null);
+        $this->gridManager->expects($this->once())
+            ->method('getConfigurationForGrid')
+            ->willReturn($config);
+    }
+}
