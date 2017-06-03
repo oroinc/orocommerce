a613855dda98c83535e05fe3bdc7995ba912faf8
Galaev Igor <igalayev@magecore.com>
2017-06-04 00:44:59 +0300

    BB-9475: Website Content Management - Individual Products in Product Collection - PHASE II - revert behat step logic for set default web catalog in WebCatalogBundle Context

--- src/Oro/Bundle/WebCatalogBundle/Tests/Behat/Context/FeatureContext.php
+++ src/Oro/Bundle/WebCatalogBundle/Tests/Behat/Context/FeatureContext.php
@@ -2,14 +2,11 @@
 
 namespace Oro\Bundle\WebCatalogBundle\Tests\Behat\Context;
 
-use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Behat\Symfony2Extension\Context\KernelAwareContext;
 use Behat\Symfony2Extension\Context\KernelDictionary;
-use Oro\Bundle\ConfigBundle\Tests\Behat\Element\SystemConfigForm;
-use Oro\Bundle\FormBundle\Tests\Behat\Context\FormContext;
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Bundle\TestFrameworkBundle\Behat\Context\OroFeatureContext;
-use Oro\Bundle\TestFrameworkBundle\Tests\Behat\Context\OroMainContext;
-use Oro\Bundle\ConfigBundle\Tests\Behat\Context\FeatureContext as ConfigContext;
+use Oro\Bundle\WebCatalogBundle\Entity\WebCatalog;
 
 /**
  * @SuppressWarnings(PHPMD.TooManyMethods)
@@ -20,51 +17,23 @@ class FeatureContext extends OroFeatureContext implements KernelAwareContext
     use KernelDictionary;
 
     /**
-     * @var OroMainContext
-     */
-    private $oroMainContext;
-
-    /**
-     * @var ConfigContext
-     */
-    private $configContext;
-
-    /**
-     * @var FormContext
-     */
-    private $formContext;
-
-    /**
-     * @BeforeScenario
-     */
-    public function gatherContexts(BeforeScenarioScope $scope)
-    {
-        $environment = $scope->getEnvironment();
-        $this->oroMainContext = $environment->getContext(OroMainContext::class);
-        $this->formContext = $environment->getContext(FormContext::class);
-        $this->configContext = $environment->getContext(ConfigContext::class);
-    }
-
-    /**
      * @Given /^I set "(?P<webCatalogName>[\w\s]+)" as default web catalog$/
      *
      * @param string $webCatalogName
      */
     public function setDefaultWebCatalog($webCatalogName)
     {
-        $this->oroMainContext->iOpenTheMenuAndClick('System/ Configuration');
-        $this->waitForAjax();
-        $this->configContext->clickLinkOnConfigurationSidebar('Websites');
-        $this->configContext->clickLinkOnConfigurationSidebar('Routing');
-        $this->waitForAjax();
+        $webCatalogRepository = $this->getContainer()
+            ->get('oro_entity.doctrine_helper')
+            ->getEntityRepository(WebCatalog::class);
 
-        /** @var SystemConfigForm $systemConfigForm */
-        $systemConfigForm = $this->oroMainContext->createElement('System Config Form');
-        $systemConfigForm->uncheckUseDefaultCheckbox('Web Catalog');
-        $this->oroMainContext->fillField('Web Catalog System Config Select', $webCatalogName);
+        $webCatalog = $webCatalogRepository->findOneBy(['name' => $webCatalogName]);
 
-        $this->formContext->iSaveForm();
-        $this->waitForAjax();
-        $this->oroMainContext->iShouldSeeFlashMessage('Configuration saved');
+        static::assertNotNull($webCatalog, sprintf('Web Catalog with name "%s" not found', $webCatalogName));
+
+        /** @var ConfigManager $configManager */
+        $configManager = $this->getContainer()->get('oro_config.global');
+        $configManager->set('oro_web_catalog.web_catalog', $webCatalog->getId());
+        $configManager->flush();
     }
 }
