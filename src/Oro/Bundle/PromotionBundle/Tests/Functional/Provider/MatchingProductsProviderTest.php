af600c507c8791b2f099254a78b53fa47294a15a
Galaev Igor <igalayev@magecore.com>
2017-06-13 11:22:12 +0300

    BB-10078: Add service for calculation of matching products intersecti… (#10941)
    * BB-10078: Add service for calculation of matching products intersection with line items
    - add MatchingProductsProvider
    - add tests
    
    * BB-10078: Add service for calculation of matching products intersection with line items
    
    - revert changes in check.php
    
    * BB-10078: Add service for calculation of matching products intersection with line items
    - fix typehints
    
    * BB-10078: Add service for calculation of matching products intersection with line items
    - make service private
    
    * BB-10078: Add service for calculation of matching products intersection with line items
    - DiscountLineItemStub small fixes
    
    * BB-10078: Add service for calculation of matching products intersection with line items
    - remove whitespace [skip ci]

---
+++ src/Oro/Bundle/PromotionBundle/Tests/Functional/Provider/MatchingProductsProviderTest.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Oro\Bundle\PromotionBundle\Tests\Functional\Provider;
+
+use Oro\Bundle\ProductBundle\Tests\Functional\DataFixtures\LoadProductData;
+use Oro\Bundle\PromotionBundle\Provider\MatchingProductsProvider;
+use Oro\Bundle\PromotionBundle\Tests\Functional\DataFixtures\LoadPromotionSegmentData;
+use Oro\Bundle\PromotionBundle\Tests\Functional\Stub\DiscountLineItemStub;
+use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+
+class MatchingProductsProviderTest extends WebTestCase
+{
+    /**
+     * @var MatchingProductsProvider
+     */
+    private $provider;
+
+    protected function setUp()
+    {
+        $this->initClient([], static::generateBasicAuthHeader());
+        $this->loadFixtures([LoadPromotionSegmentData::class]);
+        $this->provider = new MatchingProductsProvider(static::getContainer()->get('oro_segment.segment_manager'));
+    }
+
+    public function testGetMatchingProductsWhenEmptyIntersection()
+    {
+        $segment = $this->getReference(LoadPromotionSegmentData::NOT_EMPTY_PROMOTION_SEGMENT);
+
+        $this->assertEquals(
+            [],
+            $this->provider->getMatchingProducts($segment, $this->createLineItems([
+                LoadProductData::PRODUCT_2,
+                LoadProductData::PRODUCT_4,
+                LoadProductData::PRODUCT_5
+            ]))
+        );
+    }
+
+    public function testHasMatchingProductsWhenEmptyIntersection()
+    {
+        $segment = $this->getReference(LoadPromotionSegmentData::NOT_EMPTY_PROMOTION_SEGMENT);
+
+        $this->assertFalse($this->provider->hasMatchingProducts($segment, $this->createLineItems([
+            LoadProductData::PRODUCT_2,
+            LoadProductData::PRODUCT_4,
+            LoadProductData::PRODUCT_5
+        ])));
+    }
+
+    public function testGetMatchingProductsWhenNotEmptyIntersection()
+    {
+        $segment = $this->getReference(LoadPromotionSegmentData::NOT_EMPTY_PROMOTION_SEGMENT);
+
+        $expectedProducts = [
+            $this->getReference(LoadProductData::PRODUCT_1),
+            $this->getReference(LoadProductData::PRODUCT_3)
+        ];
+
+        $this->assertEquals(
+            $expectedProducts,
+            $this->provider->getMatchingProducts($segment, $this->createLineItems([
+                LoadProductData::PRODUCT_1,
+                LoadProductData::PRODUCT_2,
+                LoadProductData::PRODUCT_3,
+                LoadProductData::PRODUCT_4,
+                LoadProductData::PRODUCT_5
+            ]))
+        );
+    }
+
+    public function testHasMatchingProductsWhenNotEmptyIntersection()
+    {
+        $segment = $this->getReference(LoadPromotionSegmentData::NOT_EMPTY_PROMOTION_SEGMENT);
+
+        $this->assertTrue($this->provider->hasMatchingProducts($segment, $this->createLineItems([
+            LoadProductData::PRODUCT_1,
+            LoadProductData::PRODUCT_2,
+            LoadProductData::PRODUCT_3,
+            LoadProductData::PRODUCT_4,
+            LoadProductData::PRODUCT_5
+        ])));
+    }
+
+    public function testGetMatchingProductsWithEmptySegmentSnapshot()
+    {
+        $segment = $this->getReference(LoadPromotionSegmentData::EMPTY_PROMOTION_SEGMENT);
+
+        $this->assertEquals(
+            [],
+            $this->provider->getMatchingProducts($segment, $this->createLineItems([
+                LoadProductData::PRODUCT_1,
+                LoadProductData::PRODUCT_2,
+                LoadProductData::PRODUCT_3
+            ]))
+        );
+    }
+
+    public function testHasMatchingProductsWithEmptySegmentSnapshot()
+    {
+        $segment = $this->getReference(LoadPromotionSegmentData::EMPTY_PROMOTION_SEGMENT);
+
+        $this->assertFalse($this->provider->hasMatchingProducts($segment, $this->createLineItems([
+            LoadProductData::PRODUCT_1,
+            LoadProductData::PRODUCT_2,
+            LoadProductData::PRODUCT_3
+        ])));
+    }
+
+    /**
+     * @param array $productReferences
+     * @return array|DiscountLineItemStub[]
+     */
+    private function createLineItems(array $productReferences)
+    {
+        $lineItems = [];
+        foreach ($productReferences as $productReference) {
+            $lineItem = new DiscountLineItemStub();
+            $lineItem->setProduct($this->getReference($productReference));
+            $lineItems[] = $lineItem;
+        }
+
+        return $lineItems;
+    }
+}
---
+++ src/Oro/Bundle/PromotionBundle/Tests/Unit/Provider/MatchingProductsProviderTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Oro\Bundle\PromotionBundle\Tests\Unit\Provider;
+
+use Doctrine\ORM\QueryBuilder;
+use Oro\Bundle\PromotionBundle\Provider\MatchingProductsProvider;
+use Oro\Bundle\PromotionBundle\Tests\Functional\Stub\DiscountLineItemStub;
+use Oro\Bundle\SegmentBundle\Entity\Manager\SegmentManager;
+use Oro\Bundle\SegmentBundle\Entity\Segment;
+
+class MatchingProductsProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var SegmentManager|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $segmentManager;
+
+    /**
+     * @var MatchingProductsProvider
+     */
+    private $provider;
+
+    protected function setUp()
+    {
+        $this->segmentManager = $this->getMockBuilder(SegmentManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->provider = new MatchingProductsProvider($this->segmentManager);
+    }
+
+    public function testHasMatchingProductsThrowsExceptionWhenNoRootAlias()
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('No root alias for segment\'s query builder');
+
+        $segment = new Segment();
+        $this->expectsQueryBuilderWithNoRootAlias($segment);
+
+        $this->provider->hasMatchingProducts($segment, [new DiscountLineItemStub()]);
+    }
+
+    public function testGetMatchingProductsThrowsExceptionWhenNoRootAlias()
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('No root alias for segment\'s query builder');
+
+        $segment = new Segment();
+        $this->expectsQueryBuilderWithNoRootAlias($segment);
+
+        $this->provider->getMatchingProducts($segment, [new DiscountLineItemStub()]);
+    }
+
+    public function testHasMatchingProductsThrowsExceptionWhenNoQueryBuilder()
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Cannot get query builder for segment');
+
+        $segment = new Segment();
+        $this->expectsNoQueryBuilder($segment);
+
+        $this->provider->hasMatchingProducts($segment, [new DiscountLineItemStub()]);
+    }
+
+    public function testGetMatchingProductsThrowsExceptionWhenNoQueryBuilder()
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Cannot get query builder for segment');
+
+        $segment = new Segment();
+        $this->expectsNoQueryBuilder($segment);
+
+        $this->provider->getMatchingProducts($segment, [new DiscountLineItemStub()]);
+    }
+
+    public function testHasMatchingProductsWhenEmptyLineItems()
+    {
+        static::assertFalse($this->provider->hasMatchingProducts(new Segment(), []));
+    }
+
+    public function testGetMatchingProductsWhenEmptyLineItems()
+    {
+        static::assertEmpty($this->provider->getMatchingProducts(new Segment(), []));
+    }
+
+    /**
+     * @param Segment $segment
+     */
+    private function expectsQueryBuilderWithNoRootAlias(Segment $segment)
+    {
+        $queryBuilder = $this->getMockBuilder(QueryBuilder::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $queryBuilder
+            ->expects($this->once())
+            ->method('getRootAliases')
+            ->willReturn([]);
+
+        $this->segmentManager
+            ->expects($this->once())
+            ->method('getEntityQueryBuilder')
+            ->with($segment)
+            ->willReturn($queryBuilder);
+    }
+
+    /**
+     * @param Segment $segment
+     */
+    private function expectsNoQueryBuilder(Segment $segment)
+    {
+        $this->segmentManager
+            ->expects($this->once())
+            ->method('getEntityQueryBuilder')
+            ->with($segment)
+            ->willReturn(null);
+    }
+}
