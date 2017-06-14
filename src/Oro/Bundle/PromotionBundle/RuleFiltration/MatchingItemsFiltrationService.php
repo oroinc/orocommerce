39a62209f9d572bc70aa7389f60984480f1f32bb
Galaev Igor <igalayev@magecore.com>
2017-06-14 13:55:44 +0300

    BB-10079: Implement matching products filter (#10981)
    * BB-10079: Implement matching products filter
    - add MatchingItemsFiltration service

--- src/Oro/Bundle/PromotionBundle/RuleFiltration/MatchingItemsFiltrationService.php
+++ src/Oro/Bundle/PromotionBundle/RuleFiltration/MatchingItemsFiltrationService.php
@@ -2,27 +2,54 @@
 
 namespace Oro\Bundle\PromotionBundle\RuleFiltration;
 
-use Oro\Bundle\RuleBundle\Entity\RuleOwnerInterface;
+use Oro\Bundle\PromotionBundle\Entity\Promotion;
+use Oro\Bundle\PromotionBundle\Provider\MatchingProductsProvider;
 use Oro\Bundle\RuleBundle\RuleFiltration\RuleFiltrationServiceInterface;
 
+/**
+ * This class filters out promotions which are not applicable to current context (i.e. such promotions cannot be
+ * applied to any product of lineItems from context).
+ */
 class MatchingItemsFiltrationService implements RuleFiltrationServiceInterface
 {
+    /**
+     * @var RuleFiltrationServiceInterface
+     */
+    private $filtrationService;
+
+    /**
+     * @var MatchingProductsProvider
+     */
+    private $matchingProductsProvider;
+
+    /**
+     * @param RuleFiltrationServiceInterface $filtrationService
+     * @param MatchingProductsProvider $matchingProductsProvider
+     */
+    public function __construct(
+        RuleFiltrationServiceInterface $filtrationService,
+        MatchingProductsProvider $matchingProductsProvider
+    ) {
+        $this->filtrationService = $filtrationService;
+        $this->matchingProductsProvider = $matchingProductsProvider;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function getFilteredRuleOwners(array $ruleOwners, array $context): array
     {
-        $filteredOwners = array_values(array_filter($ruleOwners, [$this, 'hasMatchingItems']));
+        $lineItems = $context['lineItems'] ?? [];
 
-        return $filteredOwners;
-    }
+        $filteredOwners = [];
+        if (!empty($lineItems)) {
+            $filteredOwners = array_values(array_filter($ruleOwners, function ($ruleOwner) use ($lineItems) {
+                return $ruleOwner instanceof Promotion
+                    && $this->matchingProductsProvider
+                        ->hasMatchingProducts($ruleOwner->getProductsSegment(), $lineItems);
+            }));
+        }
 
-    /**
-     * @param RuleOwnerInterface $ruleOwner
-     * @return bool
-     */
-    private function hasMatchingItems(RuleOwnerInterface $ruleOwner): bool
-    {
-        return true;
+        return $this->filtrationService->getFilteredRuleOwners($filteredOwners, $context);
     }
 }
