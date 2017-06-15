1360a45f5bac2cb76e245aeff4772db976c16510
Viacheslav Bemza <vbemza@magecore.com>
2017-06-15 16:16:59 +0200

    BB-10103: Implement promotion provider (#11034)

--- src/Oro/Bundle/PromotionBundle/Tests/Unit/DependencyInjection/OroPromotionExtensionTest.php
+++ src/Oro/Bundle/PromotionBundle/Tests/Unit/DependencyInjection/OroPromotionExtensionTest.php
@@ -16,7 +16,7 @@ class OroPromotionExtensionTest extends ExtensionTestCase
             'oro_promotion.rule_filtration.scope_decorator',
             'oro_promotion.rule_filtration.schedule_decorator',
             'oro_promotion.rule_filtration.matching_items',
-            'oro_promotion.context_data_converter',
+            'oro_promotion.promotion.context_data_converter_registry',
             'oro_promotion.promotion_provider',
             'oro_promotion.discount_factory',
             'oro_promotion.discount_type_to_form_type_provider'
