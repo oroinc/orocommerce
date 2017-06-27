942926b569c290563a76d66c194fa33e52bdd577
Anton Khristiansen <akhristiansen@oroinc.com>
2017-06-27 14:56:11 +0300

    [WIP] BB-10290: Implement DiscountValue saving on Order Creation - updating saving discount data

--- src/Oro/Bundle/PromotionBundle/Manager/AppliedDiscountManager.php
+++ src/Oro/Bundle/PromotionBundle/Manager/AppliedDiscountManager.php
@@ -4,7 +4,10 @@ namespace Oro\Bundle\PromotionBundle\Manager;
 
 use Oro\Bundle\OrderBundle\Entity\Order;
 use Oro\Bundle\PromotionBundle\Discount\Converter\ConverterInterface;
+use Oro\Bundle\PromotionBundle\Discount\DiscountInterface;
+use Oro\Bundle\PromotionBundle\Discount\DiscountLineItem;
 use Oro\Bundle\PromotionBundle\Entity\AppliedDiscount;
+use Oro\Bundle\PromotionBundle\Entity\Promotion;
 use Oro\Bundle\PromotionBundle\Executor\PromotionExecutor;
 
 class AppliedDiscountManager
@@ -35,25 +38,61 @@ class AppliedDiscountManager
     {
         $appliedDiscounts = [];
         $discountContext = $this->promotionExecutor->execute($order);
-        $subtotalDiscounts = $discountContext->getSubtotalDiscounts();
-        $promotions = $discountContext->getPromotions();
-        if ($subtotalDiscounts && $promotions) {
-            foreach ($subtotalDiscounts as $discount) {
-                $promotion = $discount->getPromotion();
-                if (!$promotion) {
-                    throw new \LogicException('required promotion is missing');
-                }
-
-                $appliedDiscounts[] = (new AppliedDiscount())
-                    ->setOrder($order)
-                    ->setType($discount->getDiscountType())
-                    ->setAmount($discount->getDiscountValue())
-                    ->setConfigOptions($promotion->getDiscountConfiguration()->getOptions())
-                    ->setOptions(['sourceEntityId' => ''])
-                    ->setPromotion($promotion);
+        /** @var DiscountInterface[] $discountsData */
+        $discountsData = array_merge(
+            $discountContext->getSubtotalDiscounts(),
+            $discountContext->getShippingDiscounts()
+        );
+        if ($discountsData) {
+            foreach ($discountsData as $discount) {
+                $appliedDiscounts[] = $this->createAppliedDiscount($order, $discount);
             }
         }
 
-        return $appliedDiscounts;
+        return array_merge($appliedDiscounts, $this->getLineItemDiscounts($order, $discountContext->getLineItems()));
+    }
+
+    /**
+     * @param Order $order
+     * @param DiscountInterface $discount
+     * @param array $options
+     * @return AppliedDiscount
+     */
+    public function createAppliedDiscount(
+        Order $order,
+        DiscountInterface $discount,
+        array $options = []
+    ): AppliedDiscount {
+        $promotion = $discount->getPromotion();
+        if (!$promotion) {
+            throw new \LogicException('required parameter "promotion" of discount is missing');
+        }
+
+        return (new AppliedDiscount())
+            ->setOrder($order)
+            ->setType($discount->getDiscountType())
+            ->setAmount($discount->getDiscountValue())
+            ->setConfigOptions($promotion->getDiscountConfiguration()->getOptions())
+            ->setOptions($options)
+            ->setPromotion($promotion);
+    }
+
+    /**
+     * @param Order $order
+     * @param array $discountLineItems
+     * @return array
+     */
+    protected function getLineItemDiscounts(Order $order, array $discountLineItems)
+    {
+        $appliedDiscountsData = [];
+        /** @var DiscountLineItem $discountLineItem */
+        foreach ($discountLineItems as $discountLineItem) {
+            $configOptions = ['sourceEntityId' => $discountLineItem->getSourceLineItem()->getId()];
+            foreach ($discountLineItem->getDiscounts() as $discount) {
+                $appliedDiscountsData[] = $this->createAppliedDiscount($order, $discount, $configOptions);
+            }
+        }
+
+        return $appliedDiscountsData;
     }
 }
