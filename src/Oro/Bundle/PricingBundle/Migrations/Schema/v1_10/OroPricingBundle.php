81da46934cb5612ae87829f159bc9d183a8455fa
Vladimir Seniuk <vseniuk@oroinc.com>
2017-06-30 08:38:59 +0300

    BB-9992: Required Update entities after fresh installation - make extendeble Customer User, Customer User Role and Price List entities on update

--- src/Oro/Bundle/PricingBundle/Migrations/Schema/v1_10/OroPricingBundle.php
+++ src/Oro/Bundle/PricingBundle/Migrations/Schema/v1_10/OroPricingBundle.php
@@ -3,16 +3,37 @@
 namespace Oro\Bundle\PricingBundle\Migrations\Schema\v1_10;
 
 use Doctrine\DBAL\Schema\Schema;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+use Oro\Bundle\EntityConfigBundle\Config\ConfigInterface;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+use Oro\Bundle\PricingBundle\Entity\PriceList;
 
-class OroPricingBundle implements Migration
+class OroPricingBundle implements Migration, ContainerAwareInterface
 {
+    use ContainerAwareTrait;
+
     /**
      * @inheritDoc
      */
     public function up(Schema $schema, QueryBag $queries)
     {
         $queries->addQuery(new OroPriceListStrategyQuery());
+
+        $this->updateConfig();
+    }
+
+    public function updateConfig()
+    {
+        $dumper = $this->container->get('oro_entity_extend.tools.dumper');
+        $dumper->updateConfig(function (ConfigInterface $config) {
+            $configId  = $config->getId();
+            $className = $configId->getClassName();
+
+            return $className === PriceList::class;
+        });
     }
 }
