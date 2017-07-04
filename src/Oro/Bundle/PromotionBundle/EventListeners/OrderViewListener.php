9f4ce5b2612bf69c0a39e04e2c65baa291cc6d39
Ilya Antipenko <iantypenko@oroinc.com>
2017-07-04 18:55:55 +0300

    BB-10294: Display list of promotions  - cr fixes

--- src/Oro/Bundle/PromotionBundle/EventListeners/OrderViewListener.php
+++ src/Oro/Bundle/PromotionBundle/EventListeners/OrderViewListener.php
@@ -2,10 +2,9 @@
 
 namespace Oro\Bundle\PromotionBundle\EventListeners;
 
-use Oro\Bundle\OrderBundle\Entity\Order;
-use Oro\Bundle\PromotionBundle\Provider\OrdersAppliedDiscountsProvider;
-use Oro\Bundle\UIBundle\Event\BeforeListRenderEvent;
 use Symfony\Component\Translation\TranslatorInterface;
+use Oro\Bundle\OrderBundle\Entity\Order;
+use Oro\Bundle\UIBundle\Event\BeforeListRenderEvent;
 
 class OrderViewListener
 {
@@ -14,22 +13,30 @@ class OrderViewListener
      */
     protected $translator;
 
+    /**
+     * @param TranslatorInterface $translator
+     */
     public function __construct(TranslatorInterface $translator)
     {
         $this->translator = $translator;
     }
 
+    /**
+     * @param BeforeListRenderEvent $event
+     */
     public function onView(BeforeListRenderEvent $event)
     {
         /** @var Order $order */
         $order = $event->getEntity();
 
         $template = $event->getEnvironment()->render('@OroPromotion/Order/discounts_view.html.twig', [
-            'entity' => $order
+            'entity' => $order,
         ]);
 
+        $blockTitle = $this->translator->trans('oro.promotion.entity_plural_label');
+
         $scrollData = $event->getScrollData();
-        $blockId = $scrollData->addBlock($this->translator->trans('oro.promotion.entity_plural_label'), -75);
+        $blockId = $scrollData->addBlock($blockTitle, -75);
         $subBlockId = $scrollData->addSubBlock($blockId);
         $scrollData->addSubBlockData($blockId, $subBlockId, $template);
     }
