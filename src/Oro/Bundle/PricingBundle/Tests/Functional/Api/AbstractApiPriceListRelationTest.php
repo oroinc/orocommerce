c8e16ac143e0829abba848b6afa89ff4bec6efbd
Artur Vynohradov <avinogradov@oroinc.com>
2017-07-06 20:58:28 +0300

    BB-10607: PriceListToCustomer Commerce EE functional tests - removed most of abstraction from abstract api price list relation test and added most concrete logic to price list to customer test

--- src/Oro/Bundle/PricingBundle/Tests/Functional/Api/AbstractApiPriceListRelationTest.php
+++ src/Oro/Bundle/PricingBundle/Tests/Functional/Api/AbstractApiPriceListRelationTest.php
@@ -3,32 +3,12 @@
 namespace Oro\Bundle\PricingBundle\Tests\Functional\Api;
 
 use Oro\Bundle\ApiBundle\Tests\Functional\RestJsonApiTestCase;
-use Oro\Bundle\PricingBundle\Async\Topics;
-use Oro\Bundle\PricingBundle\Entity\BasePriceListRelation;
-use Oro\Bundle\PricingBundle\Entity\PriceList;
-use Oro\Bundle\PricingBundle\Tests\Functional\DataFixtures\LoadPriceListRelations;
 use Oro\Bundle\PricingBundle\Tests\Functional\Entity\EntityListener\MessageQueueTrait;
-use Oro\Bundle\WebsiteBundle\Entity\Website;
 use Symfony\Component\HttpFoundation\Response;
-use Doctrine\ORM\EntityRepository;
 
 abstract class AbstractApiPriceListRelationTest extends RestJsonApiTestCase
 {
     use MessageQueueTrait;
-
-    /**
-     * {@inheritDoc}
-     */
-    protected function setUp()
-    {
-        parent::setUp();
-
-        $this->loadFixtures(
-            [
-                LoadPriceListRelations::class,
-            ]
-        );
-    }
 
     /**
      * @return string
@@ -40,32 +20,7 @@ abstract class AbstractApiPriceListRelationTest extends RestJsonApiTestCase
      */
     abstract protected function getAliceFilesFolderName(): string;
 
-    /**
-     * @return EntityRepository
-     */
-    abstract protected function getEntityRepository(): EntityRepository;
-
-    /**
-     * @param BasePriceListRelation $entity
-     *
-     * @return array
-     */
-    abstract protected function prepareRebuildPriceListMessagesForEntity(BasePriceListRelation $entity): array;
-
-    /**
-     * @return array
-     */
-    abstract protected function getDeleteListFilter();
-
-    /**
-     * @return array
-     */
-    abstract protected function getExpectedRebuildMessagesOnDeleteList(): array;
-
-    /**
-     * @return BasePriceListRelation
-     */
-    abstract protected function getFirstRelation(): BasePriceListRelation;
+    abstract protected function getFirstRelation();
 
     /**
      * @param int $entityId
@@ -116,139 +71,6 @@ abstract class AbstractApiPriceListRelationTest extends RestJsonApiTestCase
         static::assertContains(
             'unique entity constraint',
             $response->getContent()
-        );
-    }
-
-    public function testCreate()
-    {
-        $response = $this->post(
-            ['entity' => $this->getApiEntityName()],
-            $this->getAliceFilesFolderName().'/create.yml'
-        );
-
-        $this->assertResponseContains(
-            sprintf('../requests/%s/create.yml', $this->getAliceFilesFolderName()),
-            $response
-        );
-    }
-
-    public function testDeleteList()
-    {
-        $this->cleanScheduledRelationMessages();
-
-        $messages = $this->getExpectedRebuildMessagesOnDeleteList();
-
-        $this->cdelete(
-            ['entity' => $this->getApiEntityName()],
-            [
-                'filter' => $this->getDeleteListFilter(),
-            ]
-        );
-
-        foreach ($messages as $message) {
-            static::assertMessageSent(Topics::REBUILD_COMBINED_PRICE_LISTS, $message);
-        }
-    }
-
-    public function testDelete()
-    {
-        $this->cleanScheduledRelationMessages();
-
-        $relation = $this->getFirstRelation();
-        $id = $relation->getId();
-
-        $expectedMessage = $this->prepareRebuildPriceListMessagesForEntity($relation);
-
-        $this->delete(
-            [
-                'entity' => $this->getApiEntityName(),
-                'id' => $id,
-            ]
-        );
-
-        $this->assertNull(
-            $this->getEntityRepository()->find($id)
-        );
-
-        static::assertMessageSent(Topics::REBUILD_COMBINED_PRICE_LISTS, $expectedMessage);
-    }
-
-    public function testUpdate()
-    {
-        $this->cleanScheduledRelationMessages();
-
-        $relation = $this->getFirstRelation();
-        $id = $relation->getId();
-
-        $expectedMessage = $this->prepareRebuildPriceListMessagesForEntity($relation);
-        $expectedMergedAllowed = !$relation->isMergeAllowed();
-        $expectedSortOrder = 999;
-
-        $this->patch(
-            ['entity' => $this->getApiEntityName(), 'id' => (string)$id],
-            [
-                'data' => [
-                    'id' => (string)$id,
-                    'type' => $this->getApiEntityName(),
-                    'attributes' => [
-                        'sortOrder' => $expectedSortOrder,
-                        'mergeAllowed' => $expectedMergedAllowed,
-                    ],
-                ],
-            ]
-        );
-
-        $updatedRelation = $this->getEntityRepository()->find($id);
-
-        static::assertSame($expectedSortOrder, $updatedRelation->getSortOrder());
-        static::assertSame($expectedMergedAllowed, $updatedRelation->isMergeAllowed());
-        static::assertMessageSent(Topics::REBUILD_COMBINED_PRICE_LISTS, $expectedMessage);
-    }
-
-    public function testGetSubResources()
-    {
-        $relation = $this->getFirstRelation();
-
-        $this->assertGetSubResource($relation->getId(), 'priceList', $relation->getPriceList()->getId());
-        $this->assertGetSubResource($relation->getId(), 'website', $relation->getWebsite()->getId());
-    }
-
-    public function testGetRelationships()
-    {
-        $relation = $this->getFirstRelation();
-
-        $response = $this->getRelationship(
-            [
-                'entity' => $this->getApiEntityName(),
-                'id' => $relation->getId(),
-                'association' => 'priceList',
-            ]
-        );
-        $this->assertResponseContains(
-            [
-                'data' => [
-                    'type' => $this->getEntityType(PriceList::class),
-                    'id' => (string)$relation->getPriceList()->getId(),
-                ],
-            ],
-            $response
-        );
-
-        $response = $this->getRelationship(
-            [
-                'entity' => $this->getApiEntityName(),
-                'id' => $relation->getId(),
-                'association' => 'website',
-            ]
-        );
-        $this->assertResponseContains(
-            [
-                'data' => [
-                    'type' => $this->getEntityType(Website::class),
-                    'id' => (string)$relation->getWebsite()->getId(),
-                ],
-            ],
-            $response
         );
     }
 }
