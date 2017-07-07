12da72dbb61544fe720012d3b6e52ec48435494f
Artur Vynohradov <avinogradov@oroinc.com>
2017-07-07 12:34:35 +0300

    BB-10338: PriceListCustomerFallback Commerce functional tests - test for creation now checks existence of created entity by providing conditions for query - changed access to helper methods from private to protected

--- src/Oro/Bundle/PricingBundle/Tests/Functional/Api/PriceListCustomerFallbackTest.php
+++ src/Oro/Bundle/PricingBundle/Tests/Functional/Api/PriceListCustomerFallbackTest.php
@@ -93,9 +93,10 @@ class PriceListCustomerFallbackTest extends RestJsonApiTestCase
             ->findOneBy([
                 'website' => $this->getDefaultWebsite(),
                 'customer' => $this->getReference('customer.level_1.2'),
+                'fallback' => 0
             ]);
 
-        static::assertSame(0, $relation->getFallback());
+        static::assertNotNull($relation);
 
         static::assertMessageSent(
             Topics::REBUILD_COMBINED_PRICE_LISTS,
@@ -233,12 +234,12 @@ class PriceListCustomerFallbackTest extends RestJsonApiTestCase
     /**
      * @return PriceListCustomerFallback
      */
-    private function getFirstRelation()
+    protected function getFirstRelation()
     {
         return $this->getReference(LoadPriceListFallbackSettings::WEBSITE_CUSTOMER_FALLBACK_1);
     }
 
-    private function assertFirstRelationMessageSent()
+    protected function assertFirstRelationMessageSent()
     {
         static::assertMessageSent(
             Topics::REBUILD_COMBINED_PRICE_LISTS,
@@ -254,7 +255,7 @@ class PriceListCustomerFallbackTest extends RestJsonApiTestCase
     /**
      * @return Website
      */
-    private function getDefaultWebsite(): Website
+    protected function getDefaultWebsite(): Website
     {
         return $this->getEntityManager()->getRepository(Website::class)->getDefaultWebsite();
     }
