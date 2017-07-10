504e8899e0b14f4f4c48759b2af820093687dfd5
Vladimir Seniuk <vseniuk@oroinc.com>
2017-07-10 12:36:14 +0300

    BB-9992: Required Update entities after fresh installation

--- src/Oro/Bundle/PricingBundle/Migrations/Schema/v1_10/OroPricingBundle.php
+++ src/Oro/Bundle/PricingBundle/Migrations/Schema/v1_10/OroPricingBundle.php
@@ -4,16 +4,62 @@ namespace Oro\Bundle\PricingBundle\Migrations\Schema\v1_10;
 
 use Doctrine\DBAL\Schema\Schema;
 
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
+use Oro\Bundle\EntityExtendBundle\Migration\LoadEntityConfigStateMigrationQuery;
+use Oro\Bundle\MigrationBundle\Migration\Extension\DataStorageExtension;
+use Oro\Bundle\MigrationBundle\Migration\Extension\DataStorageExtensionAwareInterface;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+use Oro\Bundle\PricingBundle\Entity\PriceList;
 
-class OroPricingBundle implements Migration
+class OroPricingBundle implements
+    Migration,
+    ContainerAwareInterface,
+    DataStorageExtensionAwareInterface
 {
+    use ContainerAwareTrait;
+
+    /** @var DataStorageExtension */
+    private $dataStorageExtension;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDataStorageExtension(DataStorageExtension $dataStorageExtension)
+    {
+        $this->dataStorageExtension = $dataStorageExtension;
+    }
+
     /**
      * @inheritDoc
      */
     public function up(Schema $schema, QueryBag $queries)
     {
+        $this->updateExtendEntity($queries);
+
         $queries->addQuery(new OroPriceListStrategyQuery());
+    }
+
+    /**
+     * @param QueryBag $queries
+     */
+    private function updateExtendEntity(QueryBag $queries)
+    {
+        $configManager = $this->container->get('oro_entity_config.config_manager');
+        $provider = $configManager->getProvider('extend');
+        $entityConfig = $provider->getConfig(PriceList::class);
+        $entityConfig->set('is_extend', true);
+        $entityConfig->set('state', ExtendScope::STATE_ACTIVE);
+
+        $configManager->persist($entityConfig);
+        $configManager->flush();
+        $configManager->clear();
+
+        $queries->addQuery(
+            new LoadEntityConfigStateMigrationQuery($this->dataStorageExtension)
+        );
     }
 }
