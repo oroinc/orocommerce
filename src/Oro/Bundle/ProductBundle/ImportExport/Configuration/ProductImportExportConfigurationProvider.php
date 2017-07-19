c9f07a533fba645da79f4885f23dcc188f947f0d
Sam Ivichuk <sivichuk@oroinc.com>
2017-07-19 21:00:49 +0300

    BB-10951: ProductConfigurationProvider in ProductBundle for showing import/export product buttons  - added ProductImportExportConfigurationProvider for importing, exporting products buttons  - added product import-export translations

---
+++ src/Oro/Bundle/ProductBundle/ImportExport/Configuration/ProductImportExportConfigurationProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Oro\Bundle\ProductBundle\ImportExport\Configuration;
+
+use Oro\Bundle\ImportExportBundle\Configuration\ImportExportConfiguration;
+use Oro\Bundle\ImportExportBundle\Configuration\ImportExportConfigurationInterface;
+use Oro\Bundle\ImportExportBundle\Configuration\ImportExportConfigurationProviderInterface;
+use Oro\Bundle\ProductBundle\Entity\Product;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class ProductImportExportConfigurationProvider implements ImportExportConfigurationProviderInterface
+{
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function get(): ImportExportConfigurationInterface
+    {
+        return new ImportExportConfiguration([
+            ImportExportConfiguration::FIELD_ENTITY_CLASS => Product::class,
+            ImportExportConfiguration::FIELD_EXPORT_PROCESSOR_ALIAS => 'oro_product_product',
+            ImportExportConfiguration::FIELD_EXPORT_TEMPLATE_PROCESSOR_ALIAS => 'oro_product_product_export_template',
+            ImportExportConfiguration::FIELD_IMPORT_PROCESSOR_ALIAS => 'oro_product_product.add_or_replace',
+            ImportExportConfiguration::FIELD_DATA_GRID_NAME => 'products-grid',
+            ImportExportConfiguration::FIELD_IMPORT_BUTTON_LABEL =>
+                $this->translator->trans('oro.product.import.button.label'),
+            ImportExportConfiguration::FIELD_IMPORT_VALIDATION_BUTTON_LABEL =>
+                $this->translator->trans('oro.product.import_validation.button.label'),
+            ImportExportConfiguration::FIELD_EXPORT_TEMPLATE_BUTTON_LABEL =>
+                $this->translator->trans('oro.product.export_template.button.label'),
+            ImportExportConfiguration::FIELD_EXPORT_BUTTON_LABEL =>
+                $this->translator->trans('oro.product.export.button.label'),
+            ImportExportConfiguration::FIELD_IMPORT_POPUP_TITLE =>
+                $this->translator->trans('oro.product.import.popup.title'),
+        ]);
+    }
+}
