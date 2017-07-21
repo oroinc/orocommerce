e529a20f9493ef60f991e8fa6691f16339705e20
Serj Bielanovskyi <sprightly@users.noreply.github.com>
2017-07-21 19:28:53 +0300

    BB-10754: Merge "promotions integration for orders" behats into 1.3 (#11705)
    * BB-10437: Implement behat features
    - base for changes-of-promotion-not-affect-order feature
    - added needed in order to share logic between features meta-steps
    
    (cherry picked from commit 790fed3)
    
    (cherry picked from commit d830858)
    
    * BB-10437: Implement behat features
    - fix fixture names
    
    (cherry picked from commit e2acbc2)
    
    (cherry picked from commit c989f8a)
    
    * BB-10437: Implement behat features
    - changes-of-promotion-not-affect-order feature main part
    
    (cherry picked from commit e09c7f9)
    
    (cherry picked from commit 77abeaa)
    
    * BB-10437: Implement behat features
    - fix discount value field name in behat [skip ci]
    
    (cherry picked from commit 47dbf23)
    
    (cherry picked from commit dd146bb)
    
    * BB-10437: Implement behat features
    - changes-of-promotion-not-affect-order finalized
    
    (cherry picked from commit 18c5472)
    
    (cherry picked from commit a763b25)
    
    * BB-10437: Implement behat features
    - promotions-order-discounts-block
    (cherry picked)
    
    (cherry picked from commit e269495)
    
    * BB-10437: Implement behat features
    - fix feature name
    
    (cherry picked from commit 0bf9240)
    
    * BB-10437: Implement behat features
    - add line items discount info and discount subtotal info check
    
    (cherry picked from commit 34a8771)
    
    * BB-10437: Implement behat features
    - added OrderPromotionsGrid element
    
    (cherry picked from commit 4981185)
    
    * BB-10437: Implement behat features
    - fixed Table Element
    - improved featured
    - uncommented already actual steps
    
    (cherry picked from commit 5a447a4)
    
    * BB-10437: Implement behat features
    - refactor subtotals approach
    - add checks for frontend order view page
    
    (cherry picked from commit 48b2ee0)
    
    * BB-10437: Implement behat features
    - make quotes grid extend Grid
    
    (cherry picked from commit 0361f45)
    
    * BB-10437: Implement behat features
    - checked that change of promotions not affects past orders also at backend view
    - improved features readability
    
    (cherry picked from commit 5954dee)
    
    * BB-10437: Implement behat features
    - improved features readability
    
    (cherry picked from commit 2154281)
    
    * BB-10437: Implement behat features
    - add frontend check for order
    
    (cherry picked from commit 341a135)
    
    * BB-10437: Implement behat features
    - uncomment checks after BB-10687 merged
    
    (cherry picked from commit ebb4e8d)
    
    * BB-10437: Implement behat features
    - note required steps removed
    
    (cherry picked from commit ca8379b)

--- src/Oro/Bundle/CheckoutBundle/Tests/Behat/Element/CheckoutStep.php
+++ src/Oro/Bundle/CheckoutBundle/Tests/Behat/Element/CheckoutStep.php
@@ -3,10 +3,24 @@
 namespace Oro\Bundle\CheckoutBundle\Tests\Behat\Element;
 
 use Oro\Bundle\ShoppingListBundle\Tests\Behat\Element\LineItemsAwareInterface;
+use Oro\Bundle\ShoppingListBundle\Tests\Behat\Element\SubtotalAwareInterface;
+use Oro\Bundle\ShoppingListBundle\Tests\Behat\Element\Subtotals;
 use Oro\Bundle\TestFrameworkBundle\Behat\Element\Element;
 
-class CheckoutStep extends Element implements LineItemsAwareInterface
+class CheckoutStep extends Element implements LineItemsAwareInterface, SubtotalAwareInterface
 {
+    /**
+     * @param string $subtotalName
+     * @return string
+     */
+    public function getSubtotal($subtotalName)
+    {
+        /** @var Subtotals $subtotals */
+        $subtotals = $this->getElement('Subtotals');
+
+        return $subtotals->getSubtotal($subtotalName);
+    }
+
     public function assertTitle($title)
     {
         $currentTitle = $this->getElement('CheckoutStepTitle');
--- src/Oro/Bundle/PromotionBundle/Tests/Behat/Element/PromotionCheckoutStep.php
+++ src/Oro/Bundle/PromotionBundle/Tests/Behat/Element/PromotionCheckoutStep.php
@@ -4,16 +4,8 @@ namespace Oro\Bundle\PromotionBundle\Tests\Behat\Element;
 
 use Oro\Bundle\CheckoutBundle\Tests\Behat\Element\CheckoutStep;
 
-class PromotionCheckoutStep extends CheckoutStep implements DiscountSubtotalAwareInterface
+class PromotionCheckoutStep extends CheckoutStep
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function getDiscountSubtotal()
-    {
-        return $this->getElement('PromotionCheckoutStepDiscountSubtotal')->getText();
-    }
-
     /**
      * {@inheritdoc}
      */
