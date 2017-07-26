55f21740a7f514abc7e430c97155938b6e5c89aa
Anton Khristiansen <akhristiansen@oroinc.com>
2017-07-26 14:34:39 +0300

    BB-11054: Add button “Generate codes” to coupons index page - added default business unit to form

--- src/Oro/Bundle/PromotionBundle/Form/Type/CouponGenerationType.php
+++ src/Oro/Bundle/PromotionBundle/Form/Type/CouponGenerationType.php
@@ -4,7 +4,10 @@ namespace Oro\Bundle\PromotionBundle\Form\Type;
 
 use Oro\Bundle\FormBundle\Form\Type\OroDateTimeType;
 use Oro\Bundle\PromotionBundle\CouponGeneration\Options\CouponGenerationParams;
+use Oro\Bundle\SecurityBundle\Authentication\TokenAccessorInterface;
+use Oro\Bundle\OrganizationBundle\Entity\BusinessUnit;
 
+use Oro\Bundle\UserBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,6 +18,14 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 class CouponGenerationType extends AbstractType
 {
     const NAME = 'oro_promotion_coupon_generation_type';
+
+    /** @var TokenAccessorInterface */
+    protected $tokenAccessor;
+
+    public function __construct(TokenAccessorInterface $tokenAccessor)
+    {
+        $this->tokenAccessor = $tokenAccessor;
+    }
 
     /**
      * {@inheritdoc}
@@ -28,13 +39,14 @@ class CouponGenerationType extends AbstractType
                 [
                     'required' => false,
                     'label' => 'oro.user.owner.label',
+                    'data' => $this->getCurrentBusinessUnit(),
                     'autocomplete_alias' => 'business_units_owner_search_handler',
                     'empty_value' => 'oro.business_unit.form.choose_business_user',
                     'configs' => [
                         'multiple' => false,
-                        'allowClear'  => false,
+                        'allowClear' => false,
                         'autocomplete_alias' => 'business_units_owner_search_handler',
-                        'component'   => 'tree-autocomplete',
+                        'component' => 'tree-autocomplete',
                     ]
                 ]
             )
@@ -90,13 +102,6 @@ class CouponGenerationType extends AbstractType
                 ]
             )->add(
                 'codeType',
-                TextType::class,
-                [
-                    'required' => false,
-                    'label' => 'oro.promotion.coupon.generation.codeType.label',
-                ]
-            )->add(
-                'codeType',
                 ChoiceType::class,
                 [
                     'choices' => [
@@ -128,6 +133,7 @@ class CouponGenerationType extends AbstractType
                 [
                     'required' => false,
                     'label' => 'oro.promotion.coupon.generation.dashesSequence.label',
+                    'attr' => ['class' => 'input-small']
                 ]
             );
     }
@@ -158,5 +164,23 @@ class CouponGenerationType extends AbstractType
     public function getBlockPrefix()
     {
         return self::NAME;
+    }
+
+    /**
+     * @return BusinessUnit|null
+     */
+    protected function getCurrentBusinessUnit()
+    {
+        $currentOrganization = $this->tokenAccessor->getOrganization();
+        $user = $this->tokenAccessor->getUser();
+        if (!$user instanceof User) {
+            return null;
+        }
+
+        return $user->getBusinessUnits()
+            ->filter(function (BusinessUnit $businessUnit) use ($currentOrganization) {
+                return $businessUnit->getOrganization()->getId() === $currentOrganization->getId();
+            })
+            ->first();
     }
 }
