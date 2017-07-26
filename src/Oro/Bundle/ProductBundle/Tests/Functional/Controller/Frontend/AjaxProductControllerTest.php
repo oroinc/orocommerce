c3cd6155a1ef71b395ec003e83f0e923c8151b3b
Yefim Yevtushenko <webevt@users.noreply.github.com>
2017-07-26 17:52:09 +0300

    Backend - Add an ability to get product images by product ID (#11957)
    * BB-10908: Backend - Add an ability to get product images by product ID
    - added ProductImagesURLsProvider
    - added method getImagesFilesByProductId to ProductRepository
    - added oro_product_frontend_ajax_images_by_id route to AjaxProductController
    - added/updated unit & functional tests
    - added ability to fetch filters from request parameters
    - updated tests
    - use GET method

--- src/Oro/Bundle/ProductBundle/Tests/Functional/Controller/Frontend/AjaxProductControllerTest.php
+++ src/Oro/Bundle/ProductBundle/Tests/Functional/Controller/Frontend/AjaxProductControllerTest.php
@@ -2,10 +2,12 @@
 
 namespace Oro\Bundle\ProductBundle\Tests\Functional\Controller\Frontend;
 
-use Oro\Bundle\ProductBundle\Tests\Functional\DataFixtures\LoadFrontendProductData;
-use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 use Oro\Bundle\FrontendTestFrameworkBundle\Migrations\Data\ORM\LoadCustomerUserData;
+use Oro\Bundle\ProductBundle\Entity\Product;
+use Oro\Bundle\ProductBundle\Tests\Functional\DataFixtures\LoadFrontendProductData;
 use Oro\Bundle\ProductBundle\Tests\Functional\DataFixtures\LoadProductData;
+use Oro\Bundle\ProductBundle\Tests\Functional\DataFixtures\LoadProductImageData;
+use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 
 class AjaxProductControllerTest extends WebTestCase
 {
@@ -17,7 +19,8 @@ class AjaxProductControllerTest extends WebTestCase
         );
 
         $this->loadFixtures([
-            LoadFrontendProductData::class
+            LoadFrontendProductData::class,
+            LoadProductImageData::class,
         ]);
     }
 
@@ -69,5 +72,98 @@ class AjaxProductControllerTest extends WebTestCase
                 ],
             ],
         ];
+    }
+
+    /**
+     * @dataProvider productImagesByIdDataProvider
+     *
+     * @param string|int $productReference
+     * @param array      $expectedData
+     */
+    public function testProductImagesById($productReference, array $expectedData)
+    {
+        /** @var Product $product */
+        $product = $this->getReference($productReference);
+        $this->client->request(
+            'GET',
+            $this->getUrl(
+                'oro_product_frontend_ajax_images_by_id',
+                [
+                    'id' => $product->getId(),
+                    'filters' => ['product_gallery_popup'],
+                ]
+            )
+        );
+        $result = $this->client->getResponse();
+        $this->assertJsonResponseStatusCodeEquals($result, 200);
+
+        $data = json_decode($result->getContent(), true);
+        array_walk_recursive($expectedData, function (&$value) use ($product) {
+            if (!is_array($value)) {
+                $value = sprintf($value, $this->getReference('img.'.$product->getSku())->getId());
+            }
+        });
+
+        $this->assertEquals($expectedData, $data);
+    }
+
+    /**
+     * @return array
+     */
+    public function productImagesByIdDataProvider()
+    {
+        return [
+            'product has image' => [
+                'productId' => LoadProductData::PRODUCT_1,
+                'expectedData' => [
+                    [
+                        'product_gallery_popup' => '/media/cache/attachment/resize/%d/product_gallery_popup/product-1',
+                    ],
+                ],
+            ],
+            'product has no images' => [
+                'productId' => LoadProductData::PRODUCT_3,
+                'expectedData' => [],
+            ],
+        ];
+    }
+
+    public function testProductImagesByIdWhenProductIsMissing()
+    {
+        $this->client->request(
+            'GET',
+            $this->getUrl(
+                'oro_product_frontend_ajax_images_by_id',
+                [
+                    'id' => 123456,
+                    'filters' => ['product_gallery_popup'],
+                ]
+            )
+        );
+        $result = $this->client->getResponse();
+        $this->assertJsonResponseStatusCodeEquals($result, 200);
+
+        $data = json_decode($result->getContent(), true);
+        $this->assertEquals([], $data);
+    }
+
+    public function testProductImagesByIdWhenFiltersNamesAreMissing()
+    {
+        /** @var Product $product */
+        $product = $this->getReference(LoadProductData::PRODUCT_1);
+        $this->client->request(
+            'GET',
+            $this->getUrl(
+                'oro_product_frontend_ajax_images_by_id',
+                [
+                    'id' => $product->getId(),
+                ]
+            )
+        );
+        $result = $this->client->getResponse();
+        $this->assertJsonResponseStatusCodeEquals($result, 200);
+
+        $data = json_decode($result->getContent(), true);
+        $this->assertEquals([], $data);
     }
 }
