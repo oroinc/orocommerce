1e5c4917fb62f3562d350bbf074beae13890216f
Dmitriy Pasechnik <dpasechnik@oroinc.com>
2017-07-28 15:10:20 +0300

    BB-11059: Create Generator class  - added generatot with unit test

--- src/Oro/Bundle/PromotionBundle/CouponGeneration/Generator/SimpleCouponGenerator.php
+++ src/Oro/Bundle/PromotionBundle/CouponGeneration/Generator/SimpleCouponGenerator.php
@@ -6,11 +6,71 @@ use Oro\Bundle\PromotionBundle\CouponGeneration\Options\CodeGenerationOptions;
 
 class SimpleCouponGenerator implements CouponGeneratorInterface
 {
+    const DASHES_SYMBOL = '-';
+
     /**
      * {@inheritdoc}
      */
     public function generate(CodeGenerationOptions $options)
     {
-        // TODO: Implement generate() method.
+        $string = $this->generateRandomString($options->getCodeLength(), $options->getCodeType());
+        $string = $options->getCodePrefix() . $string . $options->getCodeSuffix();
+        if (!$options->getDashesSequence()) {
+            return $string;
+        }
+        return implode(static::DASHES_SYMBOL, $this->mbStrSplit($string, $options->getDashesSequence()));
+    }
+
+    /**
+     * @param int $length
+     * @param string $type
+     * @return string
+     */
+    protected function generateRandomString(int $length, string $type): string
+    {
+        if ($length === 0) {
+            return '';
+        }
+        $template = $this->getTemplate($type);
+        $charactersLength = mb_strlen($template);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $template[random_int(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    /**
+     * @param string $type
+     * @return string
+     */
+    protected function getTemplate(string $type): string
+    {
+        switch ($type) {
+            case CodeGenerationOptions::NUMERIC_CODE_TYPE:
+                $characters = '123456789';
+                break;
+            case CodeGenerationOptions::ALPHABETIC_CODE_TYPE:
+                $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                break;
+            default:
+                $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        }
+        return $characters;
+    }
+
+    /**
+     * @param string $string
+     * @param int $length
+     * @return array
+     */
+    protected function mbStrSplit(string $string, int $length): array
+    {
+        $parts = [];
+        $len = mb_strlen($string, 'UTF-8');
+        for ($i = 0; $i < $len; $i += $length) {
+            $parts[] = mb_substr($string, $i, $length);
+        }
+        return $parts;
     }
 }
---
+++ src/Oro/Bundle/PromotionBundle/Tests/Unit/CouponGeneration/Generator/SimpleCouponGeneratorTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Oro\Bundle\PromotionBundle\Tests\Unit\CouponGeneration\Generator;
+
+use Oro\Bundle\PromotionBundle\CouponGeneration\Generator\SimpleCouponGenerator;
+use Oro\Bundle\PromotionBundle\CouponGeneration\Options\CodeGenerationOptions;
+
+class SimpleCouponGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var SimpleCouponGenerator
+     */
+    protected $generator;
+
+    public function setUp()
+    {
+        $this->generator = new SimpleCouponGenerator();
+    }
+
+    /**
+     * @dataProvider generateDataProvider
+     *
+     * @param CodeGenerationOptions $options
+     * @param $expectedPattern
+     */
+    public function testGenerate(CodeGenerationOptions $options, string $expectedPattern)
+    {
+        $this->assertRegExp($expectedPattern, $this->generator->generate($options));
+    }
+
+    public function generateDataProvider()
+    {
+        return [
+            [
+                'options' => (new CodeGenerationOptions())
+                    ->setCodeLength(10),
+                'expected' => '/^[a-zA-Z0-9]{10}$/',
+            ],
+            [
+                'options' => (new CodeGenerationOptions())
+                    ->setCodeType(CodeGenerationOptions::NUMERIC_CODE_TYPE),
+                'expected' => '/^[0-9]{12}$/',
+            ],
+            [
+                'options' => (new CodeGenerationOptions())
+                    ->setCodeType(CodeGenerationOptions::ALPHABETIC_CODE_TYPE),
+                'expected' => '/^[a-zA-Z]{12}$/',
+            ],
+            [
+                'options' => (new CodeGenerationOptions())
+                    ->setCodePrefix('qwe'),
+                'expected' => '/^qwe[a-zA-Z0-9]{12}$/',
+            ],
+            [
+                'options' => (new CodeGenerationOptions())
+                    ->setCodeSuffix('rty'),
+                'expected' => '/^[a-zA-Z0-9]{12}rty$/',
+            ],
+            [
+                'options' => (new CodeGenerationOptions())
+                    ->setDashesSequence(5),
+                'expected' => '/^[a-zA-Z0-9]{5}-[a-zA-Z0-9]{5}-[a-zA-Z0-9]{2}$/',
+            ],
+            [
+                'options' => (new CodeGenerationOptions())
+                    ->setCodeLength(7)
+                    ->setDashesSequence(6),
+                'expected' => '/^[a-zA-Z0-9]{6}-[a-zA-Z0-9]$/',
+            ],
+            [
+                'options' => (new CodeGenerationOptions())
+                    ->setCodeLength(7)
+                    ->setDashesSequence(7),
+                'expected' => '/^[a-zA-Z0-9]{7}$/',
+            ],
+            [
+                'options' => (new CodeGenerationOptions())
+                    ->setCodePrefix('Привет')
+                    ->setCodeSuffix('Медвед')
+                    ->setCodeType(CodeGenerationOptions::NUMERIC_CODE_TYPE)
+                    ->setCodeLength(7)
+                    ->setDashesSequence(2),
+                'expected' => '/^Пр-ив-ет-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]М-ед-ве-д$/',
+            ]
+        ];
+    }
+}
