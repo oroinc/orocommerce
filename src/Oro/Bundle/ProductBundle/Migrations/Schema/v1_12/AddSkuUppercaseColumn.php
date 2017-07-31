28030e5274e0598e75f16f9f03f9236cee9bf269
Andrey Yatsenco <ayatsenko@oroinc.com>
2017-07-31 14:48:26 +0300

    BB-10653: Merge migrations (#12117)
    * BB-10653: Merge migrations
    - backmerge master resolve migration conflict in Product bundle

--- src/Oro/Bundle/ProductBundle/Migrations/Schema/v1_12/AddSkuUppercaseColumn.php
+++ src/Oro/Bundle/ProductBundle/Migrations/Schema/v1_12/AddSkuUppercaseColumn.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Oro\Bundle\ProductBundle\Migrations\Schema\v1_15;
+namespace Oro\Bundle\ProductBundle\Migrations\Schema\v1_12;
 
 use Doctrine\DBAL\Schema\Schema;
 
--- src/Oro/Bundle/ProductBundle/Migrations/Schema/v1_12/CreateRelatedProductsTable.php
+++ src/Oro/Bundle/ProductBundle/Migrations/Schema/v1_12/CreateRelatedProductsTable.php
@@ -1,24 +1,28 @@
 <?php
 
-namespace Oro\Bundle\ProductBundle\Migrations\Schema\v1_16;
+namespace Oro\Bundle\ProductBundle\Migrations\Schema\v1_12;
 
 use Doctrine\DBAL\Schema\Schema;
 
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
-class CreateUpsellProductTable implements Migration
+class CreateRelatedProductsTable implements Migration
 {
+    /**
+     * {@inheritdoc}
+     */
     public function up(Schema $schema, QueryBag $queries)
     {
-        $table = $schema->createTable('oro_product_upsell_product');
+        $table = $schema->createTable('oro_product_related_products');
         $table->addColumn('id', 'integer', ['autoincrement' => true]);
         $table->addColumn('product_id', 'integer', ['notnull' => true]);
-        $table->addColumn('related_item_id', 'integer', ['notnull' => true]);
+        $table->addColumn('related_product_id', 'integer', ['notnull' => true]);
         $table->setPrimaryKey(['id']);
-        $table->addIndex(['product_id'], 'idx_oro_product_upsell_product_product_id', []);
-        $table->addIndex(['related_item_id'], 'idx_oro_product_upsell_product_related_item_id', []);
-        $table->addUniqueIndex(['product_id', 'related_item_id'], 'idx_oro_product_upsell_product_unique');
+        $table->addIndex(['product_id'], 'idx_oro_product_related_products_product_id', []);
+        $table->addIndex(['related_product_id'], 'idx_oro_product_related_products_related_product_id', []);
+        $table->addUniqueIndex(['product_id', 'related_product_id'], 'idx_oro_product_related_products_unique');
+
         $table->addForeignKeyConstraint(
             $schema->getTable('oro_product'),
             ['product_id'],
@@ -27,7 +31,7 @@ class CreateUpsellProductTable implements Migration
         );
         $table->addForeignKeyConstraint(
             $schema->getTable('oro_product'),
-            ['related_item_id'],
+            ['related_product_id'],
             ['id'],
             ['onDelete' => 'CASCADE', 'onUpdate' => null]
         );
