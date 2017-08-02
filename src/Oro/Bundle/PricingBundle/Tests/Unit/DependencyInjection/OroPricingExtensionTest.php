e7773250e6b944675daaedbd47c57d28e53702f7
Sam Ivichuk <sivichuk@oroinc.com>
2017-08-02 16:00:22 +0300

    BB-11234: Add api configuration for ProductPrice API  - added api for ProductPrice entity

--- src/Oro/Bundle/PricingBundle/Tests/Unit/DependencyInjection/OroPricingExtensionTest.php
+++ src/Oro/Bundle/PricingBundle/Tests/Unit/DependencyInjection/OroPricingExtensionTest.php
@@ -33,7 +33,8 @@ class OroPricingExtensionTest extends ExtensionTestCase
             'oro_pricing.api.update_lexemes_on_price_rule_delete_list_processor',
             'oro_pricing.api.on_schedule_delete_processor',
             'oro_pricing.api.on_schedule_delete_list_processor',
-            'oro_pricing.api_form_subscriber.add_schdules_to_price_list'
+            'oro_pricing.api_form_subscriber.add_schdules_to_price_list',
+            'oro_pricing.api.set_price_by_value_and_currency',
         ];
 
         $this->assertDefinitionsLoaded($expectedDefinitions);
