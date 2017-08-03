3f092141ad02e552005306f1fcf2cebb1e23b4ec
Dmitriy Pasechnik <dpasechnik@oroinc.com>
2017-08-03 19:33:15 +0300

    BB-11058: Create inserter  - validation & preview component fix

--- src/Oro/Bundle/PromotionBundle/Resources/public/js/app/components/coupon-generation-preview-component.js
+++ src/Oro/Bundle/PromotionBundle/Resources/public/js/app/components/coupon-generation-preview-component.js
@@ -14,7 +14,7 @@ define(function(require) {
         options: {
             routeName: 'oro_promotion_coupon_generation_preview',
             codePreviewSelector: '#coupon-code-preview',
-            codePreviewFieldsSelector: 'input.promotion-coupon-generation-preview'
+            codePreviewFieldsSelector: '.promotion-coupon-generation-preview'
         },
 
         /**
@@ -31,21 +31,23 @@ define(function(require) {
             this.options = _.defaults(options || {}, this.options);
             this.form = this.options._sourceElement.closest('form');
             this.bindedOnFormFieldChange = _.bind(this.onFormFieldChange, this);
-            this.form.find(this.options.codePreviewFieldsSelector).on('change', this.bindedOnFormFieldChange);
+            this.form.on('change', this.options.codePreviewFieldsSelector, this.bindedOnFormFieldChange);
         },
 
         onFormFieldChange: function(e) {
             var self = this;
-            $.ajax({
-                method: 'POST',
-                url: routing.generate(this.options.routeName),
-                data: this.form.serialize(),
-                success: function(response) {
-                    if (!response.error) {
-                        $(self.options.codePreviewSelector).html(response.code);
+            if ($(e.target).is($(e.currentTarget))) {
+                $.ajax({
+                    method: 'POST',
+                    url: routing.generate(this.options.routeName),
+                    data: this.form.serialize(),
+                    success: function(response) {
+                        if (!response.error) {
+                            $(self.options.codePreviewSelector).html(response.code);
+                        }
                     }
-                }
-            });
+                });
+            }
         },
 
         /**
