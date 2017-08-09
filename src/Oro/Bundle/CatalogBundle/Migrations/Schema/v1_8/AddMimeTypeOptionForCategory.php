d519283e57edb666e888af22cb5220044c3fc3f2
DenysZakharov <dzakharov@oroinc.com>
2017-08-09 16:23:13 +0200

    BB-11249: Allow upload svg (#12361)
    - change ConfigFileValidator
    - change AttachmentExtension
    - fix tests
    - add migrations

---
+++ src/Oro/Bundle/CatalogBundle/Migrations/Schema/v1_8/AddMimeTypeOptionForCategory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Oro\Bundle\CatalogBundle\Migrations\Schema\v1_8;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\CatalogBundle\Entity\Category;
+use Oro\Bundle\AttachmentBundle\Migration\SetAllowedMimeTypesForImageFieldQuery;
+use Oro\Bundle\CatalogBundle\Migrations\Schema\OroCatalogBundleInstaller;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class AddMimeTypeOptionForCategory implements Migration
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $queries->addQuery(new SetAllowedMimeTypesForImageFieldQuery(
+            Category::class,
+            'smallImage',
+            OroCatalogBundleInstaller::MIME_TYPES
+        ));
+        $queries->addQuery(new SetAllowedMimeTypesForImageFieldQuery(
+            Category::class,
+            'largeImage',
+            OroCatalogBundleInstaller::MIME_TYPES
+        ));
+    }
+}
