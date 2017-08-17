50fda423bc11da42c12bda45cf9761cb57edc446
Iaroslav Bratsilo <ibratsilo@oroinc.com>
2017-08-17 18:56:35 +0300

    BB-11500: Change start checkout button behavior  - modified workflows  - modified layout updates and templates

--- src/Oro/Bundle/CheckoutBundle/Condition/IsWorkflowStartFromShoppingListAllowed.php
+++ src/Oro/Bundle/CheckoutBundle/Condition/IsWorkflowStartFromShoppingListAllowed.php
@@ -30,15 +30,38 @@ class IsWorkflowStartFromShoppingListAllowed
     }
 
     /**
+     * Allows button for logged user
      * @return bool
      */
-    public function isAllowed()
+    public function isAllowedForLogged()
     {
-        $isAllowed = true;
         if ($this->tokenStorage->getToken() instanceof AnonymousCustomerUserToken) {
-            $isAllowed = $this->featureChecker->isFeatureEnabled('guest_checkout');
+            return false;
         }
 
-        return $isAllowed;
+        return true;
+    }
+
+    /**
+     * Allows button for Guest user and only if Guest checkout feature is enabled
+     * @return bool
+     */
+    public function isAllowedForGuest()
+    {
+        return $this->isAllowedForAny(false);
+    }
+
+    /**
+     * Allows button if User logged or feature enabled for Guest
+     * @param bool $allowedByDefault
+     * @return bool
+     */
+    public function isAllowedForAny($allowedByDefault = true)
+    {
+        if ($this->tokenStorage->getToken() instanceof AnonymousCustomerUserToken) {
+            return $this->featureChecker->isFeatureEnabled('guest_checkout');
+        }
+
+        return $allowedByDefault;
     }
 }
