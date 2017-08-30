34a28cd189123927258acea75b3962284f729de8
Pavel Usachev <pusachev@oroinc.com>
2017-08-30 12:49:36 +0300

    BB-11693: Create stub page for previously purchased products (#12965)
    - added ProductController in Order Bundle
    - added basic page layout

---
+++ src/Oro/Bundle/OrderBundle/Controller/Frontend/ProductsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Oro\Bundle\OrderBundle\Controller\Frontend;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+use Oro\Bundle\LayoutBundle\Annotation\Layout;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+use Oro\Bundle\OrderBundle\Controller\AbstractOrderController;
+
+class ProductsController extends AbstractOrderController
+{
+    /**
+     * @Route("/previouslyPurchased", name="oro_order_products_frontend_previously_purchased")
+     * @AclAncestor("oro_order_frontend_view")
+     * @Layout(vars={"entity_class", "grid_config"})
+     *
+     * @return array
+     */
+    public function previouslyPurchasedAction()
+    {
+        return [
+            'entity_class' => $this->container->getParameter('oro_product.entity.product.class'),
+            'grid_config' => [
+                /** @TODO will be change in  BB-11695 */
+                'frontend-product-search-grid'
+            ],
+        ];
+    }
+}
