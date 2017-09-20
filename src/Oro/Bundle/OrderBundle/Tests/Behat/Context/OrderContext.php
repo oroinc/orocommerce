2457a73b2182e0b6a9d0df3b7dfd0e7e255a7db8
Serj Bielanovskyi <sprightly@users.noreply.github.com>
2017-09-20 17:00:53 +0300

    BB-12022: Behat test - fix base, cover validations (#13300)
    * BB-12022: Behat test - fix base, cover validations
    - fixed tests that become broken after feature refactoring
    
    * BB-12022: Behat test - fix base, cover validations
    - added base for coupons-promotions-in-order-validation.feature
    - covered "Apply already applied coupon" and "Apply coupon for already applied promotion" scenarios
    - [skip ci]
    
    * BB-12022: Behat test - fix base, cover validations
    - covered "Try to add already expired coupon", "Coupon usage limit is exceeded for current customer user", "Coupon usage limit is exceeded"
    - [skip ci]
    
    * BB-12022: Behat test - fix base, cover validations
    - covered "Try to apply coupon that promotion is not applicable for order", "Try to apply coupon that have no promotion"
    - [skip ci]
    
    * BB-12022: Behat test - fix base, cover validations
    - covered "Deactivate automatic promotion", "Delete automatic promotion"
    
    * BB-12022: Behat test - fix base, cover validations
    
    * BB-12069: Behat test - manage promotions with coupons
    - add behat tests for promotion with coupons and covered case from BB-11877
    
    * BB-12022: Behat test - fix base, cover validations
    - remove unnecessary code [skip ci]

--- src/Oro/Bundle/OrderBundle/Tests/Behat/Context/OrderContext.php
+++ src/Oro/Bundle/OrderBundle/Tests/Behat/Context/OrderContext.php
@@ -5,12 +5,15 @@ namespace Oro\Bundle\OrderBundle\Tests\Behat\Context;
 use Behat\Symfony2Extension\Context\KernelAwareContext;
 use Behat\Symfony2Extension\Context\KernelDictionary;
 
+use Oro\Bundle\OrderBundle\Tests\Behat\Element\BackendOrder;
+use Oro\Bundle\OrderBundle\Tests\Behat\Element\BackendOrderLineItem;
 use Oro\Bundle\OrderBundle\Tests\Behat\Element\CollectionTable;
 use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Bundle\EntityExtendBundle\Entity\AbstractEnumValue;
 use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
 use Oro\Bundle\OrderBundle\Entity\Order;
 use Oro\Bundle\TestFrameworkBundle\Behat\Context\OroFeatureContext;
+use Oro\Bundle\TestFrameworkBundle\Behat\Driver\OroSelenium2Driver;
 use Oro\Bundle\TestFrameworkBundle\Behat\Element\OroPageObjectAware;
 use Oro\Bundle\TestFrameworkBundle\Tests\Behat\Context\PageObjectDictionary;
 
@@ -81,7 +84,7 @@ class OrderContext extends OroFeatureContext implements OroPageObjectAware, Kern
     /**
      * Example: I click on Edit action for "Percent" row in "Discounts" table
      *
-     * @Then /^I click on (?P<actionName>[\w\s]+) action for "(?P<rowValue>[\w\s]+)" row in "(?P<elementName>[\w\s]+)" table/
+     * @Then /^(?:|I )click on (?P<actionName>[\w\s]+) action for "(?P<rowValue>[\w\s]+)" row in "(?P<elementName>[\w\s]+)" table/
      * @param string $actionName
      * @param string $rowValue
      * @param string $elementName
@@ -99,5 +102,72 @@ class OrderContext extends OroFeatureContext implements OroPageObjectAware, Kern
         );
 
         $table->clickActionLink($rowValue, $actionName);
+    }
+
+    /**
+     * Example: I click on Edit action for "Percent" row in "Discounts" table
+     *
+     * @When /^(?:|I )click delete backend order line item "(?P<productSKU>[^"]+)"$/
+     *
+     * @param string $productSKU
+     */
+    public function clickDeleteBackendOrderLineItem($productSKU)
+    {
+        /** @var BackendOrder $order */
+        $order = $this->createElement('BackendOrder');
+        /** @var BackendOrderLineItem[] $lineItems */
+        $lineItems = $order->getLineItems();
+
+        foreach ($lineItems as $lineItem) {
+            if ($lineItem->getProductSKU() === $productSKU) {
+                $lineItem->delete();
+            }
+        }
+    }
+
+    /**
+     * Example: I set "2" Quantity for first backend order line item
+     *
+     * @When /^(?:|I )set "(?P<value>[\w\s]+)" (?P<field>[\w\s]+) for (?P<number>[\w\s]+) backend order line item$/
+     *
+     * @param string $value
+     * @param string $field
+     * @param string $number
+     */
+    public function setFieldValueForBackendOrderLineItem($value, $field, $number)
+    {
+        /** @var BackendOrder $order */
+        $order = $this->createElement('BackendOrder');
+        /** @var BackendOrderLineItem[] $lineItems */
+        $lineItems = $order->getLineItems();
+
+        $lineItemIndex = $this->getNumberFromString($number) - 1;
+        self::assertArrayHasKey($lineItemIndex, $lineItems);
+
+        $context = $lineItems[$lineItemIndex];
+        $fieldElement = $context->find('named', ['field', $field]);
+        if (!$fieldElement) {
+            $fieldElement = $this->createElement($field, $context);
+        }
+
+        /** @var OroSelenium2Driver $driver */
+        $driver = $this->getSession()->getDriver();
+        $driver->typeIntoInput($fieldElement->getXpath(), $value);
+    }
+
+    /**
+     * @param string $stringNumber
+     * @return int
+     */
+    private function getNumberFromString($stringNumber)
+    {
+        switch (trim($stringNumber)) {
+            case 'first':
+                return 1;
+            case 'second':
+                return 2;
+            default:
+                return (int)$stringNumber;
+        }
     }
 }
