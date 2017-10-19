4b939df661df886073215b3e098df4efcd3e16cd
Florin1 <forban@pitechnologies.ro>
2017-10-19 14:23:15 +0300

    BB-11796: Product images API replace image types (#14100)
    * BB-11796: Product images API replace image types
    - added product image type collection validator
    - refactor product image collection validator
    - added product image helper
    - refactor product image provider
    - refactor product image type validator
    - added product image collection api processor
    - added/refactored unit tests for product image validator
    - added unit test for product image api processor
    
    * PR CS fixes.
    
    * CS fix.
    
    * Pr fix.
    
    * Pr fix.

---
+++ src/Oro/Bundle/ProductBundle/Tests/Unit/Helper/ProductImageHelperTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Oro\Bundle\ProductBundle\Tests\Unit\Helper;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+use Oro\Bundle\ProductBundle\Entity\ProductImage;
+use Oro\Bundle\ProductBundle\Entity\ProductImageType;
+use Oro\Bundle\ProductBundle\Helper\ProductImageHelper;
+
+class ProductImageHelperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ProductImageHelper $productImageHelper
+     */
+    protected $productImageHelper;
+
+    protected function setUp()
+    {
+        $this->productImageHelper = new ProductImageHelper();
+    }
+
+    public function testCountImagesByType()
+    {
+        $productImageType = new ProductImageType('main');
+
+        $productImage = new ProductImage();
+        $productImage->addType($productImageType);
+
+        $collection = new ArrayCollection(
+            [
+                $productImage
+            ]
+        );
+
+        $countValues = $this->productImageHelper->countImagesByType($collection);
+
+        $this->assertEquals(
+            [
+                'main' => 1
+            ],
+            $countValues
+        );
+    }
+
+    public function testEmpty()
+    {
+        $countValues = $this->productImageHelper->countImagesByType(new ArrayCollection());
+        $this->assertEmpty($countValues);
+    }
+}
