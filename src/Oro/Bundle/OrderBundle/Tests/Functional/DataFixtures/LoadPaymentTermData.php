77d5da1245700b78ef9914d3d6ae36b27ae2e1ec
Daniel Nahrebecki <dnahrebecki@oroinc.com>
2017-10-25 09:57:47 +0200

    BB-12598: Enable API for PaymentTerm (#14265)
    - enable payment term endpoint
    - cover payment term, customer, customer groups and orders with functional tests
    - documentation

--- src/Oro/Bundle/OrderBundle/Tests/Functional/DataFixtures/LoadPaymentTermData.php
+++ src/Oro/Bundle/OrderBundle/Tests/Functional/DataFixtures/LoadPaymentTermData.php
@@ -10,6 +10,7 @@ use Oro\Bundle\PaymentTermBundle\Entity\PaymentTerm;
 class LoadPaymentTermData extends AbstractFixture
 {
     const PAYMENT_TERM_NET_10 = 'payment_term.net_10';
+    const PAYMENT_TERM_NET_20 = 'payment_term.net_20';
 
     /**
      * {@inheritdoc}
@@ -19,9 +20,14 @@ class LoadPaymentTermData extends AbstractFixture
         $paymentTerm = new PaymentTerm();
         $paymentTerm->setLabel('net 10');
 
+        $paymentTerm20 = new PaymentTerm();
+        $paymentTerm20->setLabel('net 20');
+
         $manager->persist($paymentTerm);
+        $manager->persist($paymentTerm20);
         $manager->flush();
 
         $this->addReference(self::PAYMENT_TERM_NET_10, $paymentTerm);
+        $this->addReference(self::PAYMENT_TERM_NET_20, $paymentTerm20);
     }
 }
