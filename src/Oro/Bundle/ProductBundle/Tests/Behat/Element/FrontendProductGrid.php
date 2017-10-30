4eb0491610f018d060d7947fe1850fe3b2b0353c
Galaev Igor <igalayev@magecore.com>
2017-10-30 14:35:13 +0200

    BB-11205: Behat - add mass actions support for product frontend grid - add scenario for configurable products mass action availability

---
+++ src/Oro/Bundle/ProductBundle/Tests/Behat/Element/FrontendProductGrid.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Oro\Bundle\ProductBundle\Tests\Behat\Element;
+
+use Oro\Bundle\FrontendBundle\Tests\Behat\Element\Grid;
+
+class FrontendProductGrid extends Grid
+{
+    const DEFAULT_MAPPINGS = [
+        'GridRow' => 'FrontendGridRow',
+        'GridTable' => 'ProductFrontendGridTable',
+        'GridToolbarPaginator' => 'FrontendGridToolbarPaginator',
+        'MassActionHeadCheckbox' => 'ProductFrontendMassActionHeadCheckbox',
+        'MassActionButton' => 'ProductFrontendMassActionButton',
+        'GridFloatingMenu' => 'ProductFrontendGridFloatingMenu',
+        'GridColumnManager' => 'FrontendGridColumnManager',
+        'GridFilterManager' => 'FrontendGridFilterManager',
+    ];
+}
