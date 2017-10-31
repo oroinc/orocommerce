f86e060f53a54cbb4c09fa55cd0f4419c7fcf856
Ivan Shirko <ishirko@oroinc.com>
2017-10-31 20:38:10 +0300

    BB-12632: Create Updater for updating Payment Method of Checkout (#14420)
    * BB-12632: Create Updater for updating Payment Method of Checkout
    - add updater
    - add unit tests
    - cr updates

--- src/Oro/Bundle/CheckoutBundle/Tests/Unit/Model/Updater/CheckoutUpdaterTestCase.php
+++ src/Oro/Bundle/CheckoutBundle/Tests/Unit/Model/Updater/CheckoutUpdaterTestCase.php
@@ -5,30 +5,64 @@ namespace Oro\Bundle\CheckoutBundle\Tests\Unit\Model\Updater;
 use Oro\Bundle\OrderBundle\Entity\Order;
 use Oro\Bundle\WorkflowBundle\Entity\WorkflowDefinition;
 use Oro\Bundle\WorkflowBundle\WorkflowData\WorkflowDataUpdaterInterface;
+use Oro\Component\Testing\Unit\EntityTrait;
 
 abstract class CheckoutUpdaterTestCase extends \PHPUnit_Framework_TestCase
 {
+    use EntityTrait;
+
     /** @var WorkflowDataUpdaterInterface */
     protected $updater;
 
-    public function testIsApplicableUnsupportedWorkflow()
+    /**
+     * @dataProvider isApplicableDataProvider
+     *
+     * @param array $recordGroups
+     * @param mixed $source
+     * @param bool $expected
+     */
+    public function testIsApplicable(array $recordGroups, $source, $expected)
     {
-        $this->assertFalse($this->updater->isApplicable(new WorkflowDefinition(), new Order()));
+        /** @var WorkflowDefinition $workflowDefinition */
+        $workflowDefinition = $this->getEntity(WorkflowDefinition::class, ['exclusiveRecordGroups' => $recordGroups]);
+
+        $this->assertSame($expected, $this->updater->isApplicable($workflowDefinition, $source));
     }
 
-    public function testIsApplicableUnsupportedSource()
+
+    /**
+     * @return array
+     */
+    public function isApplicableDataProvider()
     {
-        $workflow = new WorkflowDefinition();
-        $workflow->setExclusiveRecordGroups(['b2b_checkout_flow']);
+        $order = new Order();
 
-        $this->assertFalse($this->updater->isApplicable($workflow, new \stdClass()));
-    }
-
-    public function testIsApplicable()
-    {
-        $workflow = new WorkflowDefinition();
-        $workflow->setExclusiveRecordGroups(['b2b_checkout_flow']);
-
-        $this->assertTrue($this->updater->isApplicable($workflow, new Order()));
+        return [
+            'no groups' => [
+                'recordGroups' => [],
+                'source' => $order,
+                'expected' => false,
+            ],
+            'wrong groups' => [
+                'recordGroups' => ['test_group'],
+                'source' => $order,
+                'expected' => false,
+            ],
+            'no source' => [
+                'recordGroups' => ['b2b_checkout_flow'],
+                'source' => null,
+                'expected' => false,
+            ],
+            'wrong source' => [
+                'recordGroups' => ['b2b_checkout_flow'],
+                'source' => new \stdClass(),
+                'expected' => false,
+            ],
+            'right data' => [
+                'recordGroups' => ['test_group', 'b2b_checkout_flow'],
+                'source' => $order,
+                'expected' => true,
+            ],
+        ];
     }
 }
