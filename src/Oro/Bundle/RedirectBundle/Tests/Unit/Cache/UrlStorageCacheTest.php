fa94771036699a3e61502ba1bea20b30afcd6237
Thomas Parys <thomas.parys@wwsh.io>
2017-11-07 17:25:48 +0100

    Revert "BB-12803: Redis-compatible slug caching (#14582)" (#14586)
    This reverts commit 6cc74c728f2b4fbede5f1f10d6d0d581a657e6fb.

--- src/Oro/Bundle/RedirectBundle/Tests/Unit/Cache/UrlStorageCacheTest.php
+++ src/Oro/Bundle/RedirectBundle/Tests/Unit/Cache/UrlStorageCacheTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Oro\Bundle\RedirectBundle\Tests\Unit\Cache;
+namespace Oro\Bundle\RedirectBundle\Tests\Cache;
 
 use Doctrine\Common\Cache\ArrayCache;
 use Doctrine\Common\Cache\Cache;
