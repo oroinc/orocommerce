3caaea9d36dab8d6f1b4c9c1b2d00aeb4e439618
Thomas Parys <thomas.parys@wwsh.io>
2017-11-08 12:41:05 +0100

    BB-12548: Slug URL generation fallback (#14211)
    * BB-12548: Slug URL generation fallback
    - added slug retrieval fallback from the database
    - added test
    - refactored the solution according to proposal
    - introduced sluggable URL providers

--- src/Oro/Bundle/RedirectBundle/Tests/Functional/DataFixtures/LoadSlugsData.php
+++ src/Oro/Bundle/RedirectBundle/Tests/Functional/DataFixtures/LoadSlugsData.php
@@ -14,6 +14,7 @@ class LoadSlugsData extends AbstractFixture implements DependentFixtureInterface
     const SLUG_URL_ANONYMOUS = '/slug/anonymous';
     const SLUG_URL_USER = '/slug/customer';
     const SLUG_URL_PAGE = '/slug/page';
+    const SLUG_URL_PAGE_2 = '/slug/page2';
     const SLUG_TEST_DUPLICATE_URL = '/slug/first';
     const SLUG_TEST_DUPLICATE_REFERENCE = 'reference:/slug/first';
     const SLUG_TEST_ONLY = '__test-only__';
@@ -25,6 +26,8 @@ class LoadSlugsData extends AbstractFixture implements DependentFixtureInterface
     {
         /** @var Page $page */
         $page = $this->getReference(LoadPageData::PAGE_1);
+        /** @var Page $page2 */
+        $page2 = $this->getReference(LoadPageData::PAGE_2);
         $anonymousSlug = $this
             ->createSlug($manager, self::SLUG_URL_ANONYMOUS, 'oro_cms_frontend_page_view', ['id' => $page->getId()]);
         $anonymousSlug->setSlugPrototype('anonymous');
@@ -41,6 +44,14 @@ class LoadSlugsData extends AbstractFixture implements DependentFixtureInterface
         );
         $pageSlug = $this->createSlug($manager, self::SLUG_URL_PAGE, 'oro_customer_frontend_customer_user_index', []);
         $pageSlug->setSlugPrototype('page');
+
+        $page2Slug = $this->createSlug(
+            $manager,
+            self::SLUG_URL_PAGE_2,
+            'oro_cms_frontend_page_view',
+            ['id' => $page2->getId()]
+        );
+        $page2Slug->setSlugPrototype('page2');
 
         $this->createSlug($manager, self::SLUG_TEST_ONLY, '__test__', []);
 
