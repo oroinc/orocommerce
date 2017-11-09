04ac96dbb568802aa124643062caf38d3acb0d26
Mykhailo Sulyma <msulima@oroinc.com>
2017-11-09 07:58:16 +0200

    BB-12757: Add entity filter type (#14597)

--- src/Oro/Bundle/WebsiteSearchBundle/Tests/Unit/Attribute/Type/ManyToManySearchableAttributeTypeTest.php
+++ src/Oro/Bundle/WebsiteSearchBundle/Tests/Unit/Attribute/Type/ManyToManySearchableAttributeTypeTest.php
@@ -2,9 +2,12 @@
 
 namespace Oro\Bundle\WebsiteSearchBundle\Tests\Unit\Attribute\Type;
 
+use Oro\Bundle\SearchBundle\Query\Query;
 use Oro\Bundle\WebsiteSearchBundle\Attribute\Type\ManyToManySearchableAttributeType;
+use Oro\Bundle\WebsiteSearchBundle\Attribute\Type\SearchableAttributeTypeInterface;
+use Oro\Bundle\WebsiteSearchBundle\Placeholder\LocalizationIdPlaceholder;
 
-class ManyToManySearchableAttributeTypeTest extends ManyToOneSearchableAttributeTypeTest
+class ManyToManySearchableAttributeTypeTest extends SearchableAttributeTypeTestCase
 {
     /**
      * {@inheritdoc}
@@ -12,5 +15,50 @@ class ManyToManySearchableAttributeTypeTest extends ManyToOneSearchableAttribute
     protected function getSearchableAttributeTypeClassName()
     {
         return ManyToManySearchableAttributeType::class;
+    }
+
+    public function testGetFilterStorageFieldType()
+    {
+        $this->assertSame(
+            Query::TYPE_TEXT,
+            $this->getSearchableAttributeType()->getFilterStorageFieldType()
+        );
+    }
+
+    public function testGetSorterStorageFieldType()
+    {
+        $this->assertSame(
+            Query::TYPE_TEXT,
+            $this->getSearchableAttributeType()->getSorterStorageFieldType()
+        );
+    }
+
+    public function testGetFilterType()
+    {
+        $this->assertSame(
+            SearchableAttributeTypeInterface::FILTER_TYPE_STRING,
+            $this->getSearchableAttributeType()->getFilterType()
+        );
+    }
+
+    public function testIsLocalizable()
+    {
+        $this->assertTrue($this->getSearchableAttributeType()->isLocalizable($this->attribute));
+    }
+
+    public function testGetFilterableFieldName()
+    {
+        $this->assertSame(
+            self::FIELD_NAME . '_' . LocalizationIdPlaceholder::NAME,
+            $this->getSearchableAttributeType()->getFilterableFieldName($this->attribute)
+        );
+    }
+
+    public function testGetSortableFieldName()
+    {
+        $this->assertSame(
+            self::FIELD_NAME . '_' . LocalizationIdPlaceholder::NAME,
+            $this->getSearchableAttributeType()->getSortableFieldName($this->attribute)
+        );
     }
 }
