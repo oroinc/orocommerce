c61002ef90cc43f63ba805756b314ab3f347c7f6
Ivan Klymenko <ivan@oroinc.com>
2017-11-22 16:05:21 +0200

    BB-12944: Failed OroRedirectBundle\Tests\Functional\Routing\FrontendRoutingTest and SlugRepositoryTest on CI (#15008)
    - skip tests

--- src/Oro/Bundle/RedirectBundle/Tests/Functional/Routing/FrontendRoutingTest.php
+++ src/Oro/Bundle/RedirectBundle/Tests/Functional/Routing/FrontendRoutingTest.php
@@ -24,7 +24,8 @@ class FrontendRoutingTest extends WebTestCase
 
     public function testSlugRouting()
     {
-        $this->markTestSkipped('BB-12944');
+        $this->markTestSkipped('BB-12944: Unstable test');
+        
         /** @var Page $page */
         $page = $this->getReference(LoadPageData::PAGE_1);
 
@@ -53,7 +54,8 @@ class FrontendRoutingTest extends WebTestCase
 
     public function testSlugRoutingAuthentication()
     {
-        $this->markTestSkipped('BB-12944');
+        $this->markTestSkipped('BB-12944: Unstable test');
+        
         /** @var Slug $slug */
         $slug = $this->getReference(LoadSlugsData::SLUG_URL_USER);
         $this->client->request(
