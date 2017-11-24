3e858add8000dcc844da7ad1ede54d422a783e6e
Vitalii Yeromenko <vyeromenko@magecore.com>
2017-11-24 12:02:30 +0100

    BB-12993: Update matrix form in SL line item data (#15064)
    - updated provider to support matrix form in popup on shopping list
    - updated unit test
    - changed config option value from group to none
    - renamed matriForm field in the front end data for line items in shopping list

--- src/Oro/Bundle/ProductBundle/DependencyInjection/Configuration.php
+++ src/Oro/Bundle/ProductBundle/DependencyInjection/Configuration.php
@@ -58,7 +58,7 @@ class Configuration implements ConfigurationInterface
     const MATRIX_FORM_ON_PRODUCT_VIEW_POPUP = 'popup';
     const MATRIX_FORM_ON_SHOPPING_LIST = 'matrix_form_on_shopping_list';
     const MATRIX_FORM_ON_SHOPPING_LIST_INLINE = 'inline';
-    const MATRIX_FORM_ON_SHOPPING_LIST_GROUP = 'group';
+    const MATRIX_FORM_ON_SHOPPING_LIST_NONE = 'none';
     const MATRIX_FORM_ON_SHOPPING_LIST_POPUP = 'popup';
     const MATRIX_FORM_ON_PRODUCT_LISTING = 'matrix_form_on_product_listing';
     const MATRIX_FORM_ON_PRODUCT_LISTING_NONE = 'none';
