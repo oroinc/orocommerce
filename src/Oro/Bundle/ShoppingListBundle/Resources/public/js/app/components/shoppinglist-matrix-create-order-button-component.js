06878642ced1c1cd62efd9dcf08c3a25c7959d76
Artur Vynohradov <avinogradov@oroinc.com>
2017-11-29 14:43:42 +0200

    BB-13040: Improve “Create Order” button - refactored button component to refactor shoppinglist matrix button component

--- src/Oro/Bundle/ShoppingListBundle/Resources/public/js/app/components/shoppinglist-matrix-create-order-button-component.js
+++ src/Oro/Bundle/ShoppingListBundle/Resources/public/js/app/components/shoppinglist-matrix-create-order-button-component.js
@@ -3,12 +3,9 @@ define(function(require) {
     'use strict';
 
     var _ = require('underscore');
-    var __ = require('orotranslation/js/translator');
-    var $ = require('jquery');
     var mediator = require('oroui/js/mediator');
     var ButtonComponent = require('oroworkflow/js/app/components/button-component');
     var StandardConfirmation = require('oroui/js/standart-confirmation');
-    var TransitionHandler = require('oroworkflow/js/transition-handler');
     var t = require('orotranslation/js/translator');
     var ShoppingListMatrixCreateOrderButtonComponent;
 
@@ -38,46 +35,37 @@ define(function(require) {
         },
 
         /**
-         * @inheritDoc
+         * @param clickedButton
+         * @private
          */
-        _processButton: function() {
+        _onClickButtonEnabledDisplayTypeDialog: function(clickedButton) {
             var self = this;
-            if (this.$button.data('enabled')) {
-                if (this.options.displayType === 'dialog') {
-                    this.$button.data('executor', function() {
-                        TransitionHandler.call(self.$button);
-                    });
-                    this.$button.on('click', function(e) {
-                        e.preventDefault();
+            var childArgument = arguments;
 
-                        self.showConfirmation(_.bind(function() {
-                            $(this).data('executor').call();
-                        }, this));
-                    });
-                } else {
-                    this.$button.on('click', function(e) {
-                        e.preventDefault();
+            var confirmationCallback = function() {
+                ShoppingListMatrixCreateOrderButtonComponent.__super__
+                    ._onClickButtonEnabledDisplayTypeDialog
+                    .apply(self, childArgument);
+            };
 
-                        self.showConfirmation(_.bind(function() {
-                            mediator.execute('redirectTo', {url: self.$button.data('transition-url')}, {redirect: true});
-                        }, self));
-                    });
-                }
-            } else {
-                this.$button.on('click', function(e) {
-                    e.preventDefault();
-                });
-                if (this.$button.data('transition-condition-messages')) {
-                    this.$button.popover({
-                        'html': true,
-                        'placement': 'bottom',
-                        'container': $('body'),
-                        'trigger': 'hover',
-                        'title': '<i class="fa-exclamation-circle"></i>' + __('Unmet conditions'),
-                        'content': this.$button.data('transition-condition-messages')
-                    });
-                }
-            }
+            this.showConfirmation(confirmationCallback);
+        },
+
+        /**
+         * @param clickedButton
+         * @private
+         */
+        _onClickButtonEnabledDisplayTypeNotDialog: function(clickedButton) {
+            var self = this;
+            var childArgument = arguments;
+
+            var confirmationCallback = function() {
+                ShoppingListMatrixCreateOrderButtonComponent.__super__
+                    ._onClickButtonEnabledDisplayTypeNotDialog
+                    .apply(self, childArgument);
+            };
+
+            this.showConfirmation(confirmationCallback);
         },
 
         showConfirmation: function(callback) {
