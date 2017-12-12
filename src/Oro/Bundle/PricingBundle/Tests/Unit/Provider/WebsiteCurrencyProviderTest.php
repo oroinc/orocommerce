0ddfeecb181d3101a983784bf89337f06acb2bf1
Dmitriy Pasechnik <dpasechnik@oroinc.com>
2017-12-12 11:01:12 +0200

    BB-13184: Canadian site - wrong shopping list subtotals in admin panel (#15459)
    * BB-13184: Canadian site - wrong shopping list subtotals in admin panel
     - shopping list subtotal calculation logic redone

---
+++ src/Oro/Bundle/PricingBundle/Tests/Unit/Provider/WebsiteCurrencyProviderTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Oro\Bundle\PricingBundle\Tests\Unit\Provider;
+
+use Oro\Bundle\CurrencyBundle\Provider\CurrencyProviderInterface;
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Oro\Bundle\PricingBundle\Provider\WebsiteCurrencyProvider;
+use Oro\Bundle\WebsiteBundle\Entity\Repository\WebsiteRepository;
+
+class WebsiteCurrencyProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var CurrencyProviderInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $currencyProvider;
+
+    /**
+     * @var DoctrineHelper|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $doctrineHelper;
+
+    /**
+     * @var WebsiteCurrencyProvider
+     */
+    protected $provider;
+
+    public function setUp()
+    {
+        $this->currencyProvider = $this->createMock(CurrencyProviderInterface::class);
+        $this->doctrineHelper = $this->createMock(DoctrineHelper::class);
+
+        $this->provider = new WebsiteCurrencyProvider($this->currencyProvider, $this->doctrineHelper);
+    }
+
+    public function testGetWebsiteDefaultCurrency()
+    {
+        $this->currencyProvider->expects($this->atLeastOnce())->method('getDefaultCurrency')->willReturn('USD');
+        $this->assertEquals('USD', $this->provider->getWebsiteDefaultCurrency(1));
+        $this->assertEquals('USD', $this->provider->getWebsiteDefaultCurrency(2));
+        $this->assertEquals('USD', $this->provider->getWebsiteDefaultCurrency(3));
+    }
+
+    public function testGetAllWebsitesCurrencies()
+    {
+        $this->currencyProvider->expects($this->atLeastOnce())->method('getDefaultCurrency')->willReturn('EUR');
+
+        $repo = $this->createMock(WebsiteRepository::class);
+        $this->doctrineHelper->expects($this->once())->method('getEntityRepository')->willReturn($repo);
+
+        $repo->expects($this->once())->method('getWebsiteIdentifiers')->willReturn([1, 2, 3, 4, 5]);
+
+        $this->assertEquals(
+            [1 => 'EUR', 2 => 'EUR', 3 => 'EUR', 4 => 'EUR', 5 => 'EUR'],
+            $this->provider->getAllWebsitesCurrencies()
+        );
+    }
+}
