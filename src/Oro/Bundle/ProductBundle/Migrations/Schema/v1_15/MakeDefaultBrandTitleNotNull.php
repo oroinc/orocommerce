751ff58a2c346ce7279701a88ad1febb3fe1e956
Yaroslav Bracilo <yarik.br@gmail.com>
2017-12-27 12:22:34 +0200

    BB-13274: Denormalize Brand name default value (#15714)
    BB-13274: Denormalize Brand name default value
     - applied oro_brand table denormalization
     - added needed migration

---
+++ src/Oro/Bundle/ProductBundle/Migrations/Schema/v1_15/MakeDefaultBrandTitleNotNull.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Oro\Bundle\ProductBundle\Migrations\Schema\v1_15;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\CatalogBundle\Migrations\Schema\OroCatalogBundleInstaller;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\OrderedMigrationInterface;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class MakeDefaultCategoryTitleNotNull implements
+    Migration,
+    OrderedMigrationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $table = $schema->getTable('oro_brand');
+        $table->getColumn('default_title')->setNotnull(true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return 30;
+    }
+}
