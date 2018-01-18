c5095c4e5c7990105a24d57daae8fb4dd88cc45a
Mykhailo Sulyma <msulima@oroinc.com>
2018-01-18 15:17:45 +0200

    BB-13422: Product assignment in the price list of the deleted product wasn't removed
    - added logic to remove manual price list to product assignment where removed last product price in this list

--- src/Oro/Bundle/PricingBundle/Entity/EntityListener/ProductPriceCPLEntityListener.php
+++ src/Oro/Bundle/PricingBundle/Entity/EntityListener/ProductPriceCPLEntityListener.php
@@ -2,6 +2,7 @@
 
 namespace Oro\Bundle\PricingBundle\Entity\EntityListener;
 
+use Doctrine\Common\Persistence\ObjectRepository;
 use Oro\Bundle\CommerceEntityBundle\Storage\ExtraActionEntityStorageInterface;
 use Oro\Bundle\PlatformBundle\EventListener\OptionalListenerInterface;
 use Oro\Bundle\PlatformBundle\EventListener\OptionalListenerTrait;
@@ -9,9 +10,12 @@ use Oro\Bundle\PricingBundle\Async\Topics;
 use Oro\Bundle\PricingBundle\Entity\PriceList;
 use Oro\Bundle\PricingBundle\Entity\PriceListToProduct;
 use Oro\Bundle\PricingBundle\Entity\ProductPrice;
+use Oro\Bundle\PricingBundle\Entity\Repository\PriceListToProductRepository;
+use Oro\Bundle\PricingBundle\Entity\Repository\ProductPriceRepository;
 use Oro\Bundle\PricingBundle\Event\ProductPriceRemove;
 use Oro\Bundle\PricingBundle\Event\ProductPriceSaveAfterEvent;
 use Oro\Bundle\PricingBundle\Model\PriceListTriggerHandler;
+use Oro\Bundle\PricingBundle\Sharding\ShardManager;
 use Oro\Bundle\ProductBundle\Entity\Product;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -35,18 +39,26 @@ class ProductPriceCPLEntityListener implements OptionalListenerInterface
     protected $priceListTriggerHandler;
 
     /**
+     * @var ShardManager
+     */
+    protected $shardManager;
+
+    /**
      * @param ExtraActionEntityStorageInterface $extraActionsStorage
      * @param RegistryInterface $registry
      * @param PriceListTriggerHandler $priceListTriggerHandler
+     * @param ShardManager $shardManager
      */
     public function __construct(
         ExtraActionEntityStorageInterface $extraActionsStorage,
         RegistryInterface $registry,
-        PriceListTriggerHandler $priceListTriggerHandler
+        PriceListTriggerHandler $priceListTriggerHandler,
+        ShardManager $shardManager
     ) {
         $this->extraActionsStorage = $extraActionsStorage;
         $this->registry = $registry;
         $this->priceListTriggerHandler = $priceListTriggerHandler;
+        $this->shardManager = $shardManager;
     }
 
     /**
@@ -65,7 +77,9 @@ class ProductPriceCPLEntityListener implements OptionalListenerInterface
      */
     public function onRemove(ProductPriceRemove $event)
     {
-        $this->handleChanges($event->getPrice());
+        $productPrice = $event->getPrice();
+        $this->removePriceListToProductRelation($productPrice);
+        $this->handleChanges($productPrice);
     }
 
     /**
@@ -107,14 +121,36 @@ class ProductPriceCPLEntityListener implements OptionalListenerInterface
     }
 
     /**
+     * @param ProductPrice $productPrice
+     */
+    protected function removePriceListToProductRelation(ProductPrice $productPrice)
+    {
+        $priceList = $productPrice->getPriceList();
+        $product = $productPrice->getProduct();
+
+        if (!$priceList || !$product || !$priceList->getId() || !$product->getId()) {
+            return;
+        }
+
+        /** @var ProductPriceRepository $repository */
+        $repository = $this->getRepository(ProductPrice::class);
+
+        $prices = $repository->findByPriceListAndProductSkus($this->shardManager, $priceList, [$product->getSku()]);
+        if (!$prices) {
+            /** @var PriceListToProductRepository $repository */
+            $repository = $this->getRepository(PriceListToProduct::class);
+            $repository->deleteManualRelations($priceList, $product);
+        }
+    }
+
+    /**
      * @param Product $product
      * @param PriceList $priceList
      * @return null|PriceListToProduct
      */
     protected function findRelation(Product $product, PriceList $priceList)
     {
-        $relation = $this->registry->getManagerForClass(PriceListToProduct::class)
-            ->getRepository(PriceListToProduct::class)
+        $relation = $this->getRepository(PriceListToProduct::class)
             ->findOneBy(
                 [
                     'product' => $product,
@@ -123,5 +159,14 @@ class ProductPriceCPLEntityListener implements OptionalListenerInterface
             );
 
         return $relation;
+    }
+
+    /**
+     * @param string $className
+     * @return ObjectRepository
+     */
+    protected function getRepository(string $className): ObjectRepository
+    {
+        return $this->registry->getManagerForClass($className)->getRepository($className);
     }
 }
