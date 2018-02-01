7b5172297ae9fb8e788477c8432233734a65123b
Andrew Fetisov <afetisov@oroinc.com>
2018-02-01 17:04:49 +0200

    BB-13238: Empty configurable product can be added to RFQ (#16110)
    - added check for empty configurable product on creating an RFQ
    - showing confirmation dialog to create RFQ with empty configurable product
    - fix confirm messages
    - changed js message
    - added flash message for RFQ if products have not been added
    - CSS fixes
    - popup logic fix
    - behat updated
    - js fixes for creating rfq for not empty configurable product

---
+++ src/Oro/Bundle/RFPBundle/Tests/Unit/Provider/ProductAvailabilityProviderTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Oro\Bundle\RFPBundle\Tests\Unit\Provider;
+
+use Oro\Bundle\ProductBundle\Entity\Product;
+use Oro\Bundle\RFPBundle\Provider\ProductAvailabilityProvider;
+use Oro\Component\Testing\Unit\EntityTrait;
+
+class ProductAvailabilityProviderTest extends \PHPUnit_Framework_TestCase
+{
+    use EntityTrait;
+
+    /**
+     * @return array
+     */
+    public function isProductApplicableForRFPDataProvider()
+    {
+        return [
+            'simple product' => [
+                'type' => Product::TYPE_SIMPLE,
+                'expected' => true,
+            ],
+            'configurable product' => [
+                'type' => Product::TYPE_CONFIGURABLE,
+                'expected' => false,
+            ],
+        ];
+    }
+
+    /**
+     * @param string $type
+     * @param bool $expected
+     * @dataProvider isProductApplicableForRFPDataProvider
+     */
+    public function testIsProductApplicableForRFP($type, $expected)
+    {
+        $provider = new ProductAvailabilityProvider();
+        $product = $this->getEntity(Product::class, ['id' => 1, 'type' => $type]);
+
+        $this->assertEquals($expected, $provider->isProductApplicableForRFP($product));
+    }
+}
