4523486b07ada544fde8baf6154d08221e460a33
Dmitry Khrysev <x86demon@users.noreply.github.com>
2018-02-04 15:34:16 +0200

    BB-13606: Price rule expression fails when string attribute compared with numeric value [2.6]  (#16493)
    * BB-13606: Price rule expression fails when string attribute compared with numeric value

--- src/Oro/Component/Expression/Tests/Unit/QueryExpressionConverter/ValueNodeConverterTest.php
+++ src/Oro/Component/Expression/Tests/Unit/QueryExpressionConverter/ValueNodeConverterTest.php
@@ -4,6 +4,7 @@ namespace Oro\Component\Expression\Tests\Unit\QueryExpressionConverter;
 
 use Doctrine\ORM\Query\Expr;
 use Oro\Component\Expression\Node\NodeInterface;
+use Oro\Component\Expression\QueryExpressionConverter\QueryExpressionConverterInterface;
 use Oro\Component\Expression\QueryExpressionConverter\ValueNodeConverter;
 use Oro\Component\Expression\Node\ValueNode;
 
@@ -24,11 +25,11 @@ class ValueNodeConverterTest extends \PHPUnit_Framework_TestCase
      * @param mixed $value
      * @param string|int $expected
      * @param array $expectedParameters
+     * @param array $params
      */
-    public function testConvert($value, $expected, array $expectedParameters)
+    public function testConvert($value, $expected, array $expectedParameters, array $params)
     {
         $expr = new Expr();
-        $params = [];
 
         $node = new ValueNode($value);
         $converter = new ValueNodeConverter();
@@ -42,20 +43,32 @@ class ValueNodeConverterTest extends \PHPUnit_Framework_TestCase
     public function valuesDataProvider()
     {
         return [
-            [
+            'scalar value' => [
                 'test',
                 ':_vn0',
-                ['_vn0' => 'test']
-            ],
-            [
-                42,
-                42,
+                ['_vn0' => 'test'],
                 []
             ],
-            [
+            'numeric value' => [
+                42,
+                42,
+                [],
+                []
+            ],
+            'scalar value force parametrization' => [
+                42,
+                ':_vn0',
+                [
+                    QueryExpressionConverterInterface::REQUIRE_PARAMETRIZATION => true,
+                    '_vn0' => 42
+                ],
+                [QueryExpressionConverterInterface::REQUIRE_PARAMETRIZATION => true]
+            ],
+            'array value' => [
                 [1, 3, 5],
                 ':_vn0',
-                ['_vn0' => [1, 3, 5]]
+                ['_vn0' => [1, 3, 5]],
+                []
             ]
         ];
     }
