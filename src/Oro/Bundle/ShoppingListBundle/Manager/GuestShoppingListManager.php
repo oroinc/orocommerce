0b481096a0c952b5eb00102d42ddda061a14510d
Vladimir Seniuk <vladimirseniuk@users.noreply.github.com>
2018-02-16 21:49:50 +0400

    BB-13672: Guest shopping lists are being created regardless "guest shopping list" config value (#16744)
    - fixed error with creating shopping list for visitor when guest shopping list is disabled
    - fixed tests

--- src/Oro/Bundle/ShoppingListBundle/Manager/GuestShoppingListManager.php
+++ src/Oro/Bundle/ShoppingListBundle/Manager/GuestShoppingListManager.php
@@ -2,21 +2,63 @@
 
 namespace Oro\Bundle\ShoppingListBundle\Manager;
 
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+
+use Oro\Bundle\CustomerBundle\Entity\CustomerVisitor;
+use Oro\Bundle\CustomerBundle\Security\Token\AnonymousCustomerUserToken;
 use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Oro\Bundle\FeatureToggleBundle\Checker\FeatureCheckerHolderTrait;
+use Oro\Bundle\ShoppingListBundle\Entity\ShoppingList;
 use Oro\Bundle\UserBundle\Entity\User;
+use Oro\Bundle\WebsiteBundle\Manager\WebsiteManager;
 
 class GuestShoppingListManager
 {
+    use FeatureCheckerHolderTrait;
+
     /** @var DoctrineHelper */
     private $doctrineHelper;
 
+    /** @var TokenStorageInterface */
+    private $tokenStorage;
+
+    /** @var WebsiteManager */
+    private $websiteManager;
+
+    /** @var TranslatorInterface */
+    private $translator;
+
     /**
-     * DefaultGuestShoppingListManager constructor.
      * @param DoctrineHelper $doctrineHelper
      */
     public function __construct(DoctrineHelper $doctrineHelper)
     {
         $this->doctrineHelper = $doctrineHelper;
+    }
+
+    /**
+     * @param TokenStorageInterface $tokenStorage
+     */
+    public function setTokenStorage(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    /**
+     * @param WebsiteManager $websiteManager
+     */
+    public function setWebsiteManager(WebsiteManager $websiteManager)
+    {
+        $this->websiteManager = $websiteManager;
+    }
+
+    /**
+     * @param TranslatorInterface $translator
+     */
+    public function setTranslator(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
     }
 
     /**
@@ -32,5 +74,67 @@ class GuestShoppingListManager
         }
 
         return $userRepository->findOneBy([], ['id' => 'ASC']);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isGuestShoppingListAvailable()
+    {
+        return $this->tokenStorage->getToken() instanceof AnonymousCustomerUserToken && $this->isFeaturesEnabled();
+    }
+
+    /**
+     * @return ShoppingList
+     */
+    public function getShoppingListForCustomerVisitor()
+    {
+        $token = $this->tokenStorage->getToken();
+        if (!$token instanceof AnonymousCustomerUserToken) {
+            throw new \LogicException(sprintf('Token should be instance of %s.', AnonymousCustomerUserToken::class));
+        }
+
+        /** @var CustomerVisitor $customerVisitor */
+        $customerVisitor = $token->getVisitor();
+        if ($customerVisitor === null) {
+            throw new \LogicException('Customer visitor is empty.');
+        }
+
+        $currentWebsite = $this->websiteManager->getCurrentWebsite();
+        if ($currentWebsite === null) {
+            throw new \LogicException('Current website is empty.');
+        }
+
+        /** @var ShoppingList[] $shoppingLists */
+        $shoppingLists = $customerVisitor->getShoppingLists();
+        foreach ($shoppingLists as $shoppingList) {
+            if (!$website = $shoppingList->getWebsite()) {
+                continue;
+            }
+
+            if ($website->getId() === $currentWebsite->getId()) {
+                return $shoppingList->setCurrent(true);
+            }
+        }
+
+        //Create new SL if no one still exists
+        $shoppingList = new ShoppingList();
+        $shoppingList
+            ->setOrganization($currentWebsite->getOrganization())
+            ->setCustomer(null)
+            ->setCustomerUser(null)
+            ->setWebsite($currentWebsite)
+            ->setCurrent(true);
+
+        $shoppingList->setLabel($this->translator->trans('oro.shoppinglist.default.label'));
+
+        $em = $this->doctrineHelper->getEntityManager(ShoppingList::class);
+        $em->persist($shoppingList);
+
+        //Link customer visitor to shopping list
+        $customerVisitor->addShoppingList($shoppingList);
+        $em->flush([$shoppingList, $customerVisitor]);
+
+        return $shoppingList;
     }
 }
