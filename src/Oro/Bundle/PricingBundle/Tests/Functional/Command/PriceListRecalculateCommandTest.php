78bea808be97e0bf7f8f5c9e032b79ddbc9efe44
Dmitry Khrysev <x86demon@users.noreply.github.com>
2018-05-08 12:08:36 +0300

    BB-14272: Duplicated website reindex messages during price recalculate (#17996)
     - start collecting for `combined_price_list_trigger_handler` added to command
     - added session levels support to `combined_price_list_trigger_handler`
     - updated tests

--- src/Oro/Bundle/PricingBundle/Tests/Functional/Command/PriceListRecalculateCommandTest.php
+++ src/Oro/Bundle/PricingBundle/Tests/Functional/Command/PriceListRecalculateCommandTest.php
@@ -3,6 +3,7 @@
 namespace Oro\Bundle\PricingBundle\Tests\Functional\Command;
 
 use Oro\Bundle\EntityBundle\Manager\Db\EntityTriggerManager;
+use Oro\Bundle\MessageQueueBundle\Test\Functional\MessageQueueAssertTrait;
 use Oro\Bundle\PricingBundle\Command\PriceListRecalculateCommand;
 use Oro\Bundle\PricingBundle\PricingStrategy\MinimalPricesCombiningStrategy;
 use Oro\Bundle\PricingBundle\Tests\Functional\DataFixtures\LoadDependentPriceListRelations;
@@ -16,6 +17,8 @@ use Oro\Bundle\WebsiteBundle\Tests\Functional\DataFixtures\LoadWebsiteData;
 
 class PriceListRecalculateCommandTest extends WebTestCase
 {
+    use MessageQueueAssertTrait;
+
     /**
      * {@inheritdoc}
      */
@@ -38,6 +41,7 @@ class PriceListRecalculateCommandTest extends WebTestCase
      * @param $expectedMessage
      * @param array $params
      * @param int $expectedCount
+     * @param int $expectedMesssages
      * @param array $websites
      * @param array $customerGroups
      * @param array $customers
@@ -47,6 +51,7 @@ class PriceListRecalculateCommandTest extends WebTestCase
         $expectedMessage,
         array $params,
         $expectedCount,
+        $expectedMesssages,
         array $websites = [],
         array $customerGroups = [],
         array $customers = [],
@@ -54,6 +59,9 @@ class PriceListRecalculateCommandTest extends WebTestCase
     ) {
         $this->clearCombinedPrices();
         $this->assertCombinedPriceCount(0);
+
+        $this->getMessageCollector()->clear();
+        $this->assertCount(0, $this->getSentMessages());
 
         $this->getContainer()->get('oro_pricing.builder.combined_price_list_builder')->resetCache();
         $this->getContainer()->get('oro_pricing.builder.website_combined_price_list_builder')->resetCache();
@@ -94,9 +102,13 @@ class PriceListRecalculateCommandTest extends WebTestCase
         $result = $this->runCommand(PriceListRecalculateCommand::NAME, $params);
         $this->assertContains($expectedMessage, $result);
         $this->assertCombinedPriceCount($expectedCount);
+
+        $this->assertCount($expectedMesssages, $this->getSentMessages());
     }
 
     /**
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     *
      * @return array
      */
     public function commandDataProvider()
@@ -105,22 +117,26 @@ class PriceListRecalculateCommandTest extends WebTestCase
             'all' => [
                 'expected_message' => 'Start processing',
                 'params' => ['--all'],
-                'expectedCount' => 64 // 2 + 52 + 8 + 2 = config + all levels for website1, website2 & website3
+                'expectedCount' => 64, // 2 + 52 + 8 + 2 = config + all levels for website1, website2 & website3
+                'expectedMessages' => 5,
             ],
             'all with triggers off' => [
                 'expected_message' => 'Start processing',
                 'params' => ['--all', '--disable-triggers'],
-                'expectedCount' => 64 // 2 + 52 + 8 + 2 = config + all levels for website1, website2 & website3
+                'expectedCount' => 64, // 2 + 52 + 8 + 2 = config + all levels for website1, website2 & website3
+                'expectedMessages' => 5,
             ],
             'empty run' => [
                 'expected_message' => 'ATTENTION',
                 'params' => [],
                 'expectedCount' => 0,
+                'expectedMessages' => 0,
             ],
             'website 1' => [
                 'expected_message' => 'Start processing',
                 'params' => [],
                 'expectedCount' => 38,
+                'expectedMessages' => 3,
                 'website' => [LoadWebsiteData::WEBSITE1],
                 'customerGroup' => [],
                 'customer' => []
@@ -129,6 +145,7 @@ class PriceListRecalculateCommandTest extends WebTestCase
                 'expected_message' => 'Start processing',
                 'params' => [],
                 'expectedCount' => 22,  // 14 + 8 = customer.level_1_1 + website2
+                'expectedMessages' => 2,
                 'website' => [],
                 'customerGroup' => [],
                 'customer' => ['customer.level_1_1']
@@ -137,6 +154,7 @@ class PriceListRecalculateCommandTest extends WebTestCase
                 'expected_message' => 'Start processing',
                 'params' => [],
                 'expectedCount' => 4,
+                'expectedMessages' => 2,
                 'website' => [],
                 'customerGroup' => [],
                 'customer' => ['customer.level_1.2']
@@ -145,6 +163,7 @@ class PriceListRecalculateCommandTest extends WebTestCase
                 'expected_message' => 'Start processing',
                 'params' => [],
                 'expectedCount' => 14,
+                'expectedMessages' => 2,
                 'website' => [],
                 'customerGroup' => [],
                 'customer' => ['customer.level_1.3']
@@ -153,6 +172,7 @@ class PriceListRecalculateCommandTest extends WebTestCase
                 'expected_message' => 'Start processing',
                 'params' => [],
                 'expectedCount' => 24, // 6 + 4 + 14 = customer.level_1_1 + customer.level_1.2 + customer.level_1.3
+                'expectedMessages' => 2,
                 'website' => [],
                 'customerGroup' => ['customer_group.group1'], // doesn't has own price list
                 'customer' => []
@@ -161,6 +181,7 @@ class PriceListRecalculateCommandTest extends WebTestCase
                 'expected_message' => 'Start the process',
                 'params' => [],
                 'expectedCount' => 60,
+                'expectedMessages' => 4,
                 'website' => [],
                 'customerGroup' => [],
                 'customer' => [],
@@ -170,6 +191,7 @@ class PriceListRecalculateCommandTest extends WebTestCase
                 'expected_message' => 'Start the process',
                 'params' => ['--include-dependent'],
                 'expectedCount' => 62,
+                'expectedMessages' => 4,
                 'website' => [],
                 'customerGroup' => [],
                 'customer' => [],
@@ -178,12 +200,14 @@ class PriceListRecalculateCommandTest extends WebTestCase
             'verbosity_verbose' => [
                 'expected_message' => 'Processing combined price list id:',
                 'params' => ['--all', '-v'],
-                'expectedCount' => 64
+                'expectedCount' => 64,
+                'expectedMessages' => 5,
             ],
             'verbosity_very_verbose' => [
                 'expected_message' => 'Processing price list:',
                 'params' => ['--all', '-vv'],
-                'expectedCount' => 64
+                'expectedCount' => 64,
+                'expectedMessages' => 5,
             ],
         ];
     }
