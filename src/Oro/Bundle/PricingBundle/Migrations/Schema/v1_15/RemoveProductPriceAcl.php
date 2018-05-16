5d40ab08a53b95405380de23240f9d5eb83b83dc
Vladimir Seniuk <vladimirseniuk@users.noreply.github.com>
2018-05-16 18:44:44 +0300

    BB-14331: Product Price entity should not be available on Customer User role view and edit pages (#18213)
    BB-14331: Product Price entity should not be available on Customer User role view and edit pages
    - removed ACL for Product Price entity

---
+++ src/Oro/Bundle/PricingBundle/Migrations/Schema/v1_15/RemoveProductPriceAcl.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Oro\Bundle\PricingBundle\Migrations\Schema\v1_15;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+use Oro\Bundle\EntityConfigBundle\Migration\UpdateEntityConfigEntityValueQuery;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\ParametrizedSqlMigrationQuery;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+use Oro\Bundle\PricingBundle\Entity\ProductPrice;
+
+class RemoveProductPriceAcl implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $entityClass = 'Oro\\Bundle\\PricingBundle\\Entity\\ProductPrice';
+
+        $query = new ParametrizedSqlMigrationQuery();
+        $query->addSql(
+            'DELETE FROM acl_classes WHERE class_type = :class_type',
+            ['class_type' => $entityClass],
+            ['class_type' => Type::STRING]
+        );
+
+        $queries->addPostQuery($query);
+        $queries->addPostQuery(
+            new UpdateEntityConfigEntityValueQuery(ProductPrice::class, 'security', 'type', null)
+        );
+        $queries->addPostQuery(
+            new UpdateEntityConfigEntityValueQuery(ProductPrice::class, 'security', 'group_name', null)
+        );
+        $queries->addPostQuery(
+            new UpdateEntityConfigEntityValueQuery(ProductPrice::class, 'security', 'category', null)
+        );
+    }
+}
