4c2c29a1dfe7653f663b96d8165459b4194007bb
Galaev Igor <igalayev@oroinc.com>
2018-06-21 11:31:45 +0300

    BB-14572: Submit Order button issues on Single Page Checkout (#18718)
    * BB-14572: Submit Order button issues on Single Page Checkout
    - reload Submit Order button using layout_subtree_update
    - add SinglePageTransitionProvider decorator to transition always isAllowed
    - add behat test to check that Submit Order is activated when suitable Shipping address is chosen

---
+++ src/Oro/Bundle/CheckoutBundle/Layout/DataProvider/SinglePageTransitionProvider.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Oro\Bundle\CheckoutBundle\Layout\DataProvider;
+
+use Oro\Bundle\CheckoutBundle\Model\TransitionData;
+use Oro\Bundle\WorkflowBundle\Entity\WorkflowItem;
+
+/**
+ * This transition provider returns continue transition which is always allowed, this transition is used to build
+ * form for single page checkout. As transition is always allowed no field in form is disabled because of that.
+ */
+class SinglePageTransitionProvider implements TransitionProviderInterface
+{
+    /**
+     * @var TransitionProviderInterface
+     */
+    private $transitionProvider;
+
+    /**
+     * @param TransitionProviderInterface $transitionProvider
+     */
+    public function __construct(TransitionProviderInterface $transitionProvider)
+    {
+        $this->transitionProvider = $transitionProvider;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getContinueTransition(WorkflowItem $workflowItem, $transitionName = null)
+    {
+        $transitionData = $this->transitionProvider->getContinueTransition($workflowItem, $transitionName);
+
+        return $transitionData
+            ? new TransitionData($transitionData->getTransition(), true, $transitionData->getErrors())
+            : null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBackTransitions(WorkflowItem $workflowItem)
+    {
+        return $this->transitionProvider->getBackTransitions($workflowItem);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBackTransition(WorkflowItem $workflowItem)
+    {
+        return $this->transitionProvider->getBackTransition($workflowItem);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function clearCache()
+    {
+        $this->transitionProvider->clearCache();
+    }
+}
