40f71febe4c1024a8ea4c5937eb6cadeef08d5c5
Mykhailo Kasianenko <mkasianenko@oroinc.com>
2018-07-14 16:17:02 +0300

    BAP-17230: Remove deprecated class Oro\Bundle\SecurityBundle\SecurityFacade -- SecurityFacade removed -- dependency on it changed with AuthorizationCheckerInterface

--- src/Oro/Bundle/SaleBundle/Form/Type/QuoteType.php
+++ src/Oro/Bundle/SaleBundle/Form/Type/QuoteType.php
@@ -15,7 +15,6 @@ use Oro\Bundle\FormBundle\Form\Type\OroDateType;
 use Oro\Bundle\OrderBundle\EventListener\PossibleShippingMethodEventListener;
 use Oro\Bundle\SaleBundle\Entity\Quote;
 use Oro\Bundle\SaleBundle\Provider\QuoteAddressSecurityProvider;
-use Oro\Bundle\SecurityBundle\SecurityFacade;
 use Oro\Bundle\UserBundle\Form\Type\UserMultiSelectType;
 use Oro\Bundle\UserBundle\Form\Type\UserSelectType;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -27,6 +26,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\Exception\AccessException;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class QuoteType extends AbstractType
 {
@@ -44,25 +44,25 @@ class QuoteType extends AbstractType
     /** @var EventSubscriberInterface */
     protected $quoteFormSubscriber;
 
-    /** @var SecurityFacade */
-    protected $securityFacade;
+    /** @var AuthorizationCheckerInterface */
+    protected $authorizationChecker;
 
     /**
      * @param QuoteAddressSecurityProvider $quoteAddressSecurityProvider
      * @param ConfigManager $configManager
      * @param EventSubscriberInterface $quoteFormSubscriber
-     * @param SecurityFacade $securityFacade
+     * @param AuthorizationCheckerInterface $authorizationChecker
      */
     public function __construct(
         QuoteAddressSecurityProvider $quoteAddressSecurityProvider,
         ConfigManager $configManager,
         EventSubscriberInterface $quoteFormSubscriber,
-        SecurityFacade $securityFacade
+        AuthorizationCheckerInterface $authorizationChecker
     ) {
         $this->quoteAddressSecurityProvider = $quoteAddressSecurityProvider;
         $this->configManager = $configManager;
         $this->quoteFormSubscriber = $quoteFormSubscriber;
-        $this->securityFacade = $securityFacade;
+        $this->authorizationChecker = $authorizationChecker;
     }
 
     /**
@@ -199,8 +199,8 @@ class QuoteType extends AbstractType
         $resolver->setDefaults([
             'data_class' => $this->dataClass,
             'csrf_token_id' => 'sale_quote',
-            'allow_prices_override' => $this->securityFacade->isGranted('oro_quote_prices_override'),
-            'allow_add_free_form_items' => $this->securityFacade->isGranted('oro_quote_add_free_form_items'),
+            'allow_prices_override' => $this->authorizationChecker->isGranted('oro_quote_prices_override'),
+            'allow_add_free_form_items' => $this->authorizationChecker->isGranted('oro_quote_add_free_form_items'),
         ]);
     }
 
--- src/Oro/Bundle/SaleBundle/Tests/Unit/Form/Type/QuoteTypeTest.php
+++ src/Oro/Bundle/SaleBundle/Tests/Unit/Form/Type/QuoteTypeTest.php
@@ -29,7 +29,6 @@ use Oro\Bundle\SaleBundle\Form\Type\QuoteProductRequestType;
 use Oro\Bundle\SaleBundle\Form\Type\QuoteProductType;
 use Oro\Bundle\SaleBundle\Form\Type\QuoteType;
 use Oro\Bundle\SaleBundle\Provider\QuoteAddressSecurityProvider;
-use Oro\Bundle\SecurityBundle\SecurityFacade;
 use Oro\Bundle\TestFrameworkBundle\Test\Form\MutableFormEventSubscriber;
 use Oro\Bundle\UserBundle\Entity\User;
 use Oro\Bundle\UserBundle\Form\Type\UserMultiSelectType;
@@ -39,6 +38,7 @@ use Oro\Component\Testing\Unit\PreloadedExtension;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Translation\TranslatorInterface;
 
 class QuoteTypeTest extends AbstractTest
@@ -57,8 +57,8 @@ class QuoteTypeTest extends AbstractTest
     /** @var \PHPUnit\Framework\MockObject\MockObject|QuoteFormSubscriber */
     protected $quoteFormSubscriber;
 
-    /** @var \PHPUnit\Framework\MockObject\MockObject|SecurityFacade */
-    protected $securityFacade;
+    /** @var \PHPUnit\Framework\MockObject\MockObject|AuthorizationCheckerInterface */
+    protected $authorizationChecker;
 
     /**
      * {@inheritdoc}
@@ -81,7 +81,7 @@ class QuoteTypeTest extends AbstractTest
             ->with('oro_currency.default_currency')
             ->willReturn('USD');
 
-        $this->securityFacade = $this->createMock(SecurityFacade::class);
+        $this->authorizationChecker = $this->createMock(AuthorizationCheckerInterface::class);
 
         $this->configureQuoteProductOfferFormatter();
 
@@ -89,7 +89,7 @@ class QuoteTypeTest extends AbstractTest
             $this->quoteAddressSecurityProvider,
             $this->configManager,
             $this->quoteFormSubscriber,
-            $this->securityFacade
+            $this->authorizationChecker
         );
 
         $this->formType->setDataClass(Quote::class);
@@ -98,11 +98,11 @@ class QuoteTypeTest extends AbstractTest
 
     public function testConfigureOptions()
     {
-        $this->securityFacade->expects($this->at(0))
+        $this->authorizationChecker->expects($this->at(0))
             ->method('isGranted')
             ->with('oro_quote_prices_override')
             ->willReturn(true);
-        $this->securityFacade->expects($this->at(1))
+        $this->authorizationChecker->expects($this->at(1))
             ->method('isGranted')
             ->with('oro_quote_add_free_form_items')
             ->willReturn(false);
