4573a2da8339c10902857579e7c3177cb2fb801d
Yefim Yevtushenko <webevt@users.noreply.github.com>
2018-08-01 22:22:28 +0300

    BB-14758: Product unit name not translated in RFQ confirmation email (#19354)
    - added oro_format_short_product_unit_label twig filter
    - fixed rfq confirmation rmail template
    - added fixture for confimation template fixing
    - fixed product unit translation on Quick Order Form page
    - fixed product unit translation in pricing block in mobile view
    - updated tests
    - updated dev docs
    - added emailWithFieldMustContainsTheFollowing step to EmailContext
    - fixed email checking steps in EmailContext
    - added TranslationKeys references for product units
    - updated localization checking scenario to quick_order_form_ee.feature and quick_order_form.feature
    - added email checking scenario to guest_rfq.feature
    - added oro_format_product_unit_label filter to allowed during email rendering
    - added pricing_localization.feature to check product units in mobile view

--- src/Oro/Bundle/ProductBundle/Twig/ProductUnitLabelExtension.php
+++ src/Oro/Bundle/ProductBundle/Twig/ProductUnitLabelExtension.php
@@ -5,6 +5,9 @@ namespace Oro\Bundle\ProductBundle\Twig;
 use Oro\Bundle\ProductBundle\Formatter\ProductUnitLabelFormatter;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+/**
+ * Provides TWIG functions for product unit label formatting.
+ */
 class ProductUnitLabelExtension extends \Twig_Extension
 {
     const NAME = 'oro_product_unit_label';
@@ -39,6 +42,11 @@ class ProductUnitLabelExtension extends \Twig_Extension
                 [$this, 'format'],
                 ['is_safe' => ['html']]
             ),
+            new \Twig_SimpleFilter(
+                'oro_format_short_product_unit_label',
+                [$this, 'formatShort'],
+                ['is_safe' => ['html']]
+            ),
         ];
     }
 
@@ -51,6 +59,16 @@ class ProductUnitLabelExtension extends \Twig_Extension
     public function format($unitCode, $isShort = false, $isPlural = false)
     {
         return $this->getFormatter()->format($unitCode, $isShort, $isPlural);
+    }
+
+    /**
+     * @param string $unitCode
+     * @param bool $isPlural
+     * @return string
+     */
+    public function formatShort($unitCode, $isPlural = false)
+    {
+        return $this->format($unitCode, true, $isPlural);
     }
 
     /**
