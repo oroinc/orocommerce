3e585ac217dda6f9088134c801b36be04e951d28
Yefim Yevtushenko <webevt@users.noreply.github.com>
2018-08-01 13:22:25 +0300

    BB-14758: Product unit name not translated in RFQ confirmation email (#19357)
    - added oro_format_short_product_unit_label twig filter
    - fixed rfq confirmation rmail template
    - added fixture for confimation template fixing
    - fixed product unit translation on Quick Order Form page
    - fixed product unit translation in pricing block in mobile view
    - updated tests
    - updated dev docs
    - added emailWithFieldMustContainsTheFollowing step to EmailContext
    - fixed email checking steps in EmailContext
    - added TranslationKeys references for product units
    - updated localization checking scenario to quick_order_form_ee.feature and quick_order_form.feature
    - added email checking scenario to guest_rfq.feature
    - added oro_format_product_unit_label filter to allowed during email rendering
    - added pricing_localization.feature to check product units in mobile view
    - fixed product unit in Your price block is not short in mobile view

--- src/Oro/Bundle/PricingBundle/Tests/Behat/Features/export_products_in_price_list.feature
+++ src/Oro/Bundle/PricingBundle/Tests/Behat/Features/export_products_in_price_list.feature
@@ -13,7 +13,7 @@ Feature: Export Products in Price list
     And I click view "priceListForExport" in grid
     When I press "Export"
     Then I should see "Export started successfully. You will receive email notification upon completion." flash message
-    And Email should contains the following "Export performed successfully. 4 products were exported. Download" text
+    And Email should contains the following "Export performed successfully. 4 product prices were exported. Download" text
     And Exported file for "ProductPrice" contains following rows in any order:
       | Product SKU | Quantity | Unit Code | Price   | Currency |
       | PSKU1       | 1        | item      | 6.0000  | USD      |
--- src/Oro/Bundle/ProductBundle/Tests/Behat/ReferenceRepositoryInitializer.php
+++ src/Oro/Bundle/ProductBundle/Tests/Behat/ReferenceRepositoryInitializer.php
@@ -9,6 +9,7 @@ use Oro\Bundle\ProductBundle\Entity\ProductUnit;
 use Oro\Bundle\ProductBundle\Entity\Repository\ProductUnitRepository;
 use Oro\Bundle\ProductBundle\Migrations\Data\ORM\LoadProductDefaultAttributeFamilyData;
 use Oro\Bundle\TestFrameworkBundle\Behat\Isolation\ReferenceRepositoryInitializerInterface;
+use Oro\Bundle\TranslationBundle\Entity\Repository\TranslationKeyRepository;
 use Oro\Bundle\TranslationBundle\Entity\TranslationKey;
 
 class ReferenceRepositoryInitializer implements ReferenceRepositoryInitializerInterface
@@ -64,13 +65,30 @@ class ReferenceRepositoryInitializer implements ReferenceRepositoryInitializerIn
     }
 
     /**
+     * Adds to references for TranslationKey objects for product units.
+     * Format: translation_key_%domain%_%translation_key_dots_replaced_by_underscores%
+     * Example: translation_key_jsmessages_oro_product_product_unit_bottle_value_label
+     *
      * @param Registry $doctrine
      * @param AliceCollection $referenceRepository
      */
     private function initProductUnitTranslations(Registry $doctrine, AliceCollection $referenceRepository): void
     {
-        $repository = $doctrine->getManager()->getRepository(TranslationKey::class);
-        $translationKey = $repository->findOneBy(['key' => 'oro.product.product_unit.item.label.full']);
-        $referenceRepository->set('translation_key_oro_product_product_unit_item_label_full', $translationKey);
+        /** @var TranslationKeyRepository $repository */
+        $repository = $doctrine->getManager()->getRepository('OroTranslationBundle:TranslationKey');
+        $qb = $repository->createQueryBuilder('tk');
+        $qb->orWhere($qb->expr()->like('tk.key', $qb->expr()->literal('oro.product_unit.%')))
+            ->orWhere($qb->expr()->like('tk.key', $qb->expr()->literal('oro.product.product_unit.%')));
+        $translationKeys = $qb->getQuery()->execute();
+
+        /** @var TranslationKey $translationKey */
+        foreach ($translationKeys as $translationKey) {
+            $referenceKey = sprintf(
+                'translation_key_%s_%s',
+                $translationKey->getDomain(),
+                str_replace('.', '_', $translationKey->getKey())
+            );
+            $referenceRepository->set($referenceKey, $translationKey);
+        }
     }
 }
