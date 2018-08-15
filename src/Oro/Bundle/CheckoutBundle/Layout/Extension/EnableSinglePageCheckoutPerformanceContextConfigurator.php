c0849b6325ebba4aafd24363f12e230b66b5eb83
Vladimir Seniuk <vseniuk@oroinc.com>
2018-08-15 13:11:57 +0300

    BB-13611: Single Page Checkout Performance - fixed enable_single_page_checkout_performance context_configurator

--- src/Oro/Bundle/CheckoutBundle/Layout/Extension/EnableSinglePageCheckoutPerformanceContextConfigurator.php
+++ src/Oro/Bundle/CheckoutBundle/Layout/Extension/EnableSinglePageCheckoutPerformanceContextConfigurator.php
@@ -5,26 +5,19 @@ namespace Oro\Bundle\CheckoutBundle\Layout\Extension;
 use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Component\Layout\ContextConfiguratorInterface;
 use Oro\Component\Layout\ContextInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 class EnableSinglePageCheckoutPerformanceContextConfigurator implements ContextConfiguratorInterface
 {
     const ENABLE_SINGLE_PAGE_CHECKOUT_PERFORMANCE_OPTION_NAME = 'enable_single_page_checkout_performance';
 
-    /** @var RequestStack */
-    private $requestStack;
-
     /** @var ConfigManager */
     private $configManager;
 
-
     /**
-     * @param RequestStack $requestStack
      * @param ConfigManager $configManager
      */
-    public function __construct(RequestStack $requestStack, ConfigManager $configManager)
+    public function __construct(ConfigManager $configManager)
     {
-        $this->requestStack = $requestStack;
         $this->configManager = $configManager;
     }
 
@@ -33,12 +26,9 @@ class EnableSinglePageCheckoutPerformanceContextConfigurator implements ContextC
      */
     public function configureContext(ContextInterface $context)
     {
-        $request = $this->requestStack->getCurrentRequest();
-        if (!$request) {
-            return;
-        }
-
-        $context->getResolver()->setDefined(self::ENABLE_SINGLE_PAGE_CHECKOUT_PERFORMANCE_OPTION_NAME);
+        $context->getResolver()
+            ->setRequired([self::ENABLE_SINGLE_PAGE_CHECKOUT_PERFORMANCE_OPTION_NAME])
+            ->setAllowedTypes([self::ENABLE_SINGLE_PAGE_CHECKOUT_PERFORMANCE_OPTION_NAME => ['boolean']]);
 
         $context->set(
             self::ENABLE_SINGLE_PAGE_CHECKOUT_PERFORMANCE_OPTION_NAME,
