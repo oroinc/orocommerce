91f08c30d9f8f608f78adf1cca7ea0dfb5640391
Vitaliy Berdylo <vberdylo@oroinc.com>
2018-08-29 14:22:43 +0300

    BB-14921: Promotions slows checkout performance (#19715)
    * BB-14921: Promotions slows checkout performance
    - add cache to MatchingProductsProvider::getMatchingProducts

--- src/Oro/Bundle/PromotionBundle/Discount/BuyXGetYDiscount.php
+++ src/Oro/Bundle/PromotionBundle/Discount/BuyXGetYDiscount.php
@@ -62,7 +62,7 @@ class BuyXGetYDiscount extends AbstractDiscount implements DiscountProductUnitCo
     {
         foreach ($discountContext->getLineItems() as $discountLineItem) {
             foreach ($this->getMatchingProducts() as $discountMatchingProduct) {
-                if ($discountLineItem->getProduct() === $discountMatchingProduct
+                if ($discountLineItem->getProduct()->getId() === $discountMatchingProduct->getId()
                     && $discountLineItem->getProductUnitCode() === $this->discountProductUnitCode
                     && $discountLineItem->getQuantity() >= $this->getRequiredQuantity()
                 ) {
