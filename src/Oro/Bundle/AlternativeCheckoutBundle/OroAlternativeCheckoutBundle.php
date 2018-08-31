6e04b7f61cc2ce56aef95b4c906b54d71424f1d9
Eugene Chetverikov <EugeneC@users.noreply.github.com>
2018-08-31 15:24:31 +0300

    BB-15058: Decrease number of PropertyAccessor calls (#19682)

--- src/Oro/Bundle/AlternativeCheckoutBundle/OroAlternativeCheckoutBundle.php
+++ src/Oro/Bundle/AlternativeCheckoutBundle/OroAlternativeCheckoutBundle.php
@@ -2,18 +2,8 @@
 
 namespace Oro\Bundle\AlternativeCheckoutBundle;
 
-use Oro\Bundle\CheckoutBundle\DependencyInjection\Compiler\CheckoutCompilerPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OroAlternativeCheckoutBundle extends Bundle
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function build(ContainerBuilder $container)
-    {
-        $container->addCompilerPass(new CheckoutCompilerPass());
-        parent::build($container);
-    }
 }
