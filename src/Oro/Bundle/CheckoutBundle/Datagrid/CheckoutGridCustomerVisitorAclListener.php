760e4ec626a78b23555c76c0187640e0511a8570
Yurii Muratov <ymuratov@oroinc.com>
2018-09-20 15:26:34 +0300

    BB-15023: Deny access for visitors to `frontend-checkouts-grid` datagrid (#20016)

--- src/Oro/Bundle/CheckoutBundle/Datagrid/CheckoutGridCustomerVisitorAclListener.php
+++ src/Oro/Bundle/CheckoutBundle/Datagrid/CheckoutGridCustomerVisitorAclListener.php
@@ -2,33 +2,24 @@
 
 namespace Oro\Bundle\CheckoutBundle\Datagrid;
 
-use Oro\Bundle\CustomerBundle\Entity\CustomerVisitor;
 use Oro\Bundle\CustomerBundle\Security\Token\AnonymousCustomerUserToken;
-use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
 use Oro\Bundle\DataGridBundle\Event\BuildBefore;
-use Oro\Bundle\FeatureToggleBundle\Checker\FeatureChecker;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
- * Applies ACL rule to filter checkouts by current customer visitor.
- * The applyVisitorAcl method in this class should be converted to AccessRule in BB-15019.
+ * Denies access for anonymous users to `frontend-checkouts-grid` datagrid.
  */
 class CheckoutGridCustomerVisitorAclListener
 {
-    /** @var FeatureChecker */
-    private $featureChecker;
-
     /** @var TokenStorageInterface */
     private $tokenStorage;
 
     /**
-     * @param FeatureChecker        $featureChecker
      * @param TokenStorageInterface $tokenStorage
      */
-    public function __construct(FeatureChecker $featureChecker, TokenStorageInterface $tokenStorage)
+    public function __construct(TokenStorageInterface $tokenStorage)
     {
-        $this->featureChecker = $featureChecker;
         $this->tokenStorage = $tokenStorage;
     }
 
@@ -37,35 +28,8 @@ class CheckoutGridCustomerVisitorAclListener
      */
     public function onBuildBefore(BuildBefore $event)
     {
-        $token = $this->tokenStorage->getToken();
-        if (!$token instanceof AnonymousCustomerUserToken) {
-            return;
+        if ($this->tokenStorage->getToken() instanceof AnonymousCustomerUserToken) {
+            throw new AccessDeniedException('Anonymous users are not allowed.');
         }
-
-        if (!$this->featureChecker->isFeatureEnabled('guest_checkout')) {
-            throw new AccessDeniedException('The guest checkout is disabled.');
-        }
-
-        $visitor = $token->getVisitor();
-        if (null !== $visitor) {
-            $this->applyVisitorAcl($event->getConfig(), $visitor->getId());
-        }
-    }
-
-    /**
-     * @param DatagridConfiguration $config
-     * @param int                   $visitorId
-     */
-    private function applyVisitorAcl(DatagridConfiguration $config, $visitorId)
-    {
-        $query = $config->getOrmQuery();
-        $query
-            ->addInnerJoin('checkout.source', 'source')
-            ->addAndWhere(sprintf(
-                'EXISTS(SELECT 1 FROM %s visitor WHERE visitor.id = %d'
-                . ' AND source.shoppingList MEMBER OF visitor.shoppingLists)',
-                CustomerVisitor::class,
-                $visitorId
-            ));
     }
 }
