0099a7c4193c83b1299b773d17a9e4ca49038b4e
Yevhen Shyshkin <yshyshkin@oroinc.com>
2018-10-11 11:54:36 +0200

    BAP-17621: Fix deprecates as ES6  - rolled back to nGram filter  - rolled back fixes in tests

--- src/Oro/Bundle/CatalogBundle/Tests/Functional/Grid/Frontend/ProductSearchGridTest.php
+++ src/Oro/Bundle/CatalogBundle/Tests/Functional/Grid/Frontend/ProductSearchGridTest.php
@@ -161,13 +161,13 @@ class ProductSearchGridTest extends FrontendWebTestCase
             'sku' => [
                 'filter' => 'sku',
                 'field' => 'sku',
-                'searchString' => substr(LoadProductData::PRODUCT_1, 0, 3),
+                'searchString' => substr(LoadProductData::PRODUCT_1, 2, 5),
                 'expectedFieldValue' => LoadProductData::PRODUCT_1
             ],
             'names filter' => [
                 'filter' => 'names',
                 'field' => 'name',
-                'searchString' => substr(LoadProductData::PRODUCT_1_DEFAULT_NAME, 0, 10),
+                'searchString' => substr(LoadProductData::PRODUCT_1_DEFAULT_NAME, 7, 12),
                 'expectedFieldValue' => LoadProductData::PRODUCT_1_DEFAULT_NAME
             ]
         ];
@@ -209,7 +209,7 @@ class ProductSearchGridTest extends FrontendWebTestCase
         return [
             'sku not like' => [
                 'sku',
-                LoadProductData::PRODUCT_8,
+                substr(LoadProductData::PRODUCT_8, 5),
                 [
                     LoadProductData::PRODUCT_1,
                     LoadProductData::PRODUCT_2,
@@ -220,10 +220,10 @@ class ProductSearchGridTest extends FrontendWebTestCase
                 ],
             ],
             'sku not like inside' => [
-                'sku', substr(LoadProductData::PRODUCT_8, 0, 1), [],
+                'sku', substr(LoadProductData::PRODUCT_8, 3, 4), [],
             ],
             'names not like inside' => [
-                'names', substr(LoadProductData::PRODUCT_1_DEFAULT_NAME, 0, 10), [
+                'names', substr(LoadProductData::PRODUCT_1_DEFAULT_NAME, 6, 12), [
                     LoadProductData::PRODUCT_2_DEFAULT_NAME,
                     LoadProductData::PRODUCT_3_DEFAULT_NAME,
                     LoadProductData::PRODUCT_6_DEFAULT_NAME,
