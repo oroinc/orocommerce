203eda59946bde83fe74692c186dbfb752387d27
Mykhailo Sulyma <msulima@oroinc.com>
2018-10-12 13:35:36 +0300

    BB-15333: Add twig functions to form link based on quote and its website (#20305)
    - added twig extension with related service definition
    - added compiler pass to allow twig function in twig sandbox
    - added unit tests

---
+++ src/Oro/Bundle/SaleBundle/Twig/QuoteGuestAccessExtension.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Oro\Bundle\SaleBundle\Twig;
+
+use Oro\Bundle\SaleBundle\Entity\Quote;
+use Oro\Bundle\SaleBundle\Provider\GuestQuoteAccessProviderInterface;
+use Oro\Bundle\WebsiteBundle\Resolver\WebsiteUrlResolver;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class QuoteGuestAccessExtension extends \Twig_Extension
+{
+    const NAME = 'oro_sale_quote_guest_access';
+
+    /** @var ContainerInterface */
+    private $container;
+
+    /**
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('guest_access_link', [$this, 'getGuestAccessLink'])
+        ];
+    }
+
+    /**
+     * @param Quote $quote
+     * @return string|null
+     */
+    public function getGuestAccessLink(Quote $quote): ?string
+    {
+        if (!$quote->getWebsite() || !$this->getGuestLinkAccessProvider()->isGranted($quote)) {
+            return null;
+        }
+
+        return $this->getWebsiteUrlResolver()
+            ->getWebsitePath(
+                'oro_sale_quote_frontend_guest_access',
+                ['guest_access_id' => $quote->getGuestAccessId()],
+                $quote->getWebsite()
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return static::NAME;
+    }
+
+    /**
+     * @return GuestQuoteAccessProviderInterface
+     */
+    private function getGuestLinkAccessProvider(): GuestQuoteAccessProviderInterface
+    {
+        return $this->container->get('oro_sale.provider.guest_quote_access.link');
+    }
+
+    /**
+     * @return WebsiteUrlResolver
+     */
+    private function getWebsiteUrlResolver(): WebsiteUrlResolver
+    {
+        return $this->container->get('oro_website.resolver.website_url_resolver');
+    }
+}
