240530a128c162023dee93450d60a4562e4d85d0
Ruslan Panasiuk <rpanasiuk@oroinc.com>
2018-10-16 11:30:42 +0300

    BAP-16104: Escape Twig code in default email templates

--- src/Oro/Bundle/RFPBundle/Migrations/Data/ORM/ConvertRFQRequestConfirmationEmail.php
+++ src/Oro/Bundle/RFPBundle/Migrations/Data/ORM/ConvertRFQRequestConfirmationEmail.php
@@ -7,7 +7,6 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Oro\Bundle\EmailBundle\Entity\EmailTemplate;
 use Oro\Bundle\EmailBundle\Migrations\Data\ORM\AbstractEmailFixture;
 use Oro\Bundle\RFPBundle\Entity\Request;
-use Symfony\Component\VarDumper\VarDumper;
 
 /**
  * Added html tag around twig tags
@@ -49,7 +48,6 @@ class ConvertRFQRequestConfirmationEmail extends AbstractEmailFixture implements
         $existingParsedTemplate = EmailTemplate::parseContent($existingTemplate);
         $existingEmailTemplate = $this->findExistingTemplate($manager, $existingParsedTemplate);
         if ($existingEmailTemplate) {
-            VarDumper::dump('TODO:: Email template is updated');
             $this->updateExistingTemplate($existingEmailTemplate, $templateContent);
         }
     }
