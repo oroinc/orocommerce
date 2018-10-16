54685ad783b6c130d08e3f5d189694194211bef9
Dmitry Khrysev <dkhrysev@oroinc.com>
2018-10-16 14:33:11 +0300

    BB-15168: Impossible to proceed to checkout with the custom Customer User Role - replaced set of similar ORM migrations with one - added mass ACL update migration - added ACL data for OrderItem, RFQ and Quote related entities

---
+++ src/Oro/Bundle/OrderBundle/Migrations/Data/ORM/UpdateFrontendLineItemPermissions.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Oro\Bundle\ConsentBundle\Migrations\Data\ORM;
+
+use Oro\Bundle\CustomerBundle\Migrations\Data\ORM\AbstractMassUpdateCustomerUserRolePermissions;
+use Oro\Bundle\OrderBundle\Entity\OrderLineItem;
+
+/**
+ * Update OrderLineItem default permissions for predefined roles.
+ */
+class UpdateFrontendLineItemPermissions extends AbstractMassUpdateCustomerUserRolePermissions
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getACLData(): array
+    {
+        return [
+            'ROLE_FRONTEND_ADMINISTRATOR' => [
+                'entity:' . OrderLineItem::class => ['VIEW_SYSTEM', 'CREATE_SYSTEM', 'EDIT_SYSTEM', 'DELETE_SYSTEM']
+            ],
+            'ROLE_FRONTEND_BUYER' => [
+                'entity:' . OrderLineItem::class => ['VIEW_SYSTEM', 'CREATE_SYSTEM', 'EDIT_SYSTEM', 'DELETE_SYSTEM']
+            ],
+            'ROLE_FRONTEND_ANONYMOUS' => [
+                'entity:' . OrderLineItem::class => ['VIEW_NONE', 'CREATE_NONE', 'EDIT_NONE', 'DELETE_NONE']
+            ]
+        ];
+    }
+}
