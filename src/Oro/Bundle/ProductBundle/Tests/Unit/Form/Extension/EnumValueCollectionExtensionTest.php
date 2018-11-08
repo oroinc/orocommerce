d2b2a106caf132a1a4b9defa6c734ca6123fa52c
Serhii Zhuravel <szhuravel@oroinc.com>
2018-11-08 07:47:42 +0200

    BB-15238: Product attribute edit page is slow with hundreds of enum values

--- src/Oro/Bundle/ProductBundle/Tests/Unit/Form/Extension/EnumValueCollectionExtensionTest.php
+++ src/Oro/Bundle/ProductBundle/Tests/Unit/Form/Extension/EnumValueCollectionExtensionTest.php
@@ -3,15 +3,15 @@
 namespace Oro\Bundle\ProductBundle\Tests\Unit\Form\Extension;
 
 use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Oro\Bundle\EntityConfigBundle\Config\ConfigInterface;
+use Oro\Bundle\EntityConfigBundle\Config\ConfigManager;
 use Oro\Bundle\EntityConfigBundle\Config\Id\FieldConfigId;
+use Oro\Bundle\EntityConfigBundle\Provider\ConfigProvider;
 use Oro\Bundle\EntityExtendBundle\Form\Type\EnumValueCollectionType;
 use Oro\Bundle\ProductBundle\Entity\Product;
+use Oro\Bundle\ProductBundle\Entity\Repository\ProductRepository;
 use Oro\Bundle\ProductBundle\Form\Extension\EnumValueCollectionExtension;
 use Oro\Component\Testing\Unit\EntityTrait;
-use Oro\Bundle\EntityConfigBundle\Config\ConfigManager;
-use Oro\Bundle\ProductBundle\Entity\Repository\ProductRepository;
-use Oro\Bundle\EntityConfigBundle\Config\ConfigInterface;
-use Oro\Bundle\EntityConfigBundle\Provider\ConfigProvider;
 use Symfony\Component\Form\FormConfigInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
