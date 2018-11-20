46ba1800c6cd01ad8fe411f54a653936e4d36254
Vova Soroka <vsoroka@oroinc.com>
2018-11-20 17:10:25 +0200

    BB-15701: Backport Customer Address API implementation to v3.0 (#20906)
    Backport Customer Address API implementation to v3.0

--- src/Oro/Bundle/PricingBundle/Tests/Unit/Api/ProductPrice/Processor/LoadNormalizedProductPriceWithNormalizedIdTest.php
+++ src/Oro/Bundle/PricingBundle/Tests/Unit/Api/ProductPrice/Processor/LoadNormalizedProductPriceWithNormalizedIdTest.php
@@ -2,6 +2,8 @@
 
 namespace Oro\Bundle\PricingBundle\Tests\Unit\Api\ProductPrice\Processor;
 
+use Oro\Bundle\ApiBundle\Config\EntityDefinitionConfig;
+use Oro\Bundle\ApiBundle\Metadata\EntityMetadata;
 use Oro\Bundle\ApiBundle\Processor\ActionProcessorBagInterface;
 use Oro\Bundle\ApiBundle\Processor\Get\GetContext;
 use Oro\Bundle\ApiBundle\Tests\Unit\Processor\FormProcessorTestCase;
@@ -41,21 +43,39 @@ class LoadNormalizedProductPriceWithNormalizedIdTest extends FormProcessorTestCa
 
     public function testProcessCompiles()
     {
-        $this->context->setId(1);
+        $productPriceId = 'test';
+        $priceListId = 12;
 
-        $getContext = new GetContext($this->configProvider, $this->metadataProvider);
+        $getConfig = new EntityDefinitionConfig();
+        $getConfig->set('config_key', 'config_value');
+        $getMetadata = new EntityMetadata();
+        $getMetadata->set('metadata_key', 'metadata_value');
+
+        $this->normalizer->expects(self::once())
+            ->method('normalize')
+            ->with($productPriceId)
+            ->willReturn('test-12');
 
         $processor = $this->createMock(ActionProcessorInterface::class);
-        $processor
-            ->expects(static::once())
-            ->method('createContext')
-            ->willReturn($getContext);
-
-        $this->processorBag
-            ->expects(static::once())
+        $this->processorBag->expects(self::once())
             ->method('getProcessor')
             ->willReturn($processor);
+        $processor->expects(self::once())
+            ->method('createContext')
+            ->willReturn(new GetContext($this->configProvider, $this->metadataProvider));
+        $processor->expects(self::once())
+            ->method('process')
+            ->willReturnCallback(function (GetContext $getContext) use ($getConfig, $getMetadata) {
+                self::assertEquals('test-12', $getContext->getId());
+                $getContext->setConfig($getConfig);
+                $getContext->setMetadata($getMetadata);
+            });
 
+        $this->context->set('price_list_id', $priceListId);
+        $this->context->setId($productPriceId);
         $this->processor->process($this->context);
+
+        self::assertEquals($getConfig, $this->context->getConfig());
+        self::assertEquals($getMetadata, $this->context->getMetadata());
     }
 }
