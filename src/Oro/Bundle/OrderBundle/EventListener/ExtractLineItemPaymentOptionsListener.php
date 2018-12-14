a9d09fbc6fbf075abc423a8118578af9d0302067
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2018-12-14 20:27:15 +0200

    BB-15562: The Name and Description product are not translatable in Pa… (#21071)
    - when paying, the name and product description have localized values
    - added behat
    - updated unit test

--- src/Oro/Bundle/OrderBundle/EventListener/ExtractLineItemPaymentOptionsListener.php
+++ src/Oro/Bundle/OrderBundle/EventListener/ExtractLineItemPaymentOptionsListener.php
@@ -2,6 +2,7 @@
 
 namespace Oro\Bundle\OrderBundle\EventListener;
 
+use Oro\Bundle\FrontendLocalizationBundle\Manager\UserLocalizationManager;
 use Oro\Bundle\OrderBundle\Entity\OrderLineItem;
 use Oro\Bundle\PaymentBundle\Event\ExtractLineItemPaymentOptionsEvent;
 use Oro\Bundle\PaymentBundle\Model\LineItemOptionModel;
@@ -12,6 +13,9 @@ class ExtractLineItemPaymentOptionsListener
     /** @var HtmlTagHelper */
     protected $htmlTagHelper;
 
+    /** @var UserLocalizationManager */
+    protected $userLocalizationManager;
+
     /**
      * @param HtmlTagHelper $htmlTagHelper
      */
@@ -21,12 +25,23 @@ class ExtractLineItemPaymentOptionsListener
     }
 
     /**
+     * @param UserLocalizationManager $userLocalizationManager
+     */
+    public function setUserLocalizationManager(UserLocalizationManager $userLocalizationManager): void
+    {
+        $this->userLocalizationManager = $userLocalizationManager;
+    }
+
+    /**
      * @param ExtractLineItemPaymentOptionsEvent $event
      */
     public function onExtractLineItemPaymentOptions(ExtractLineItemPaymentOptionsEvent $event)
     {
         $entity = $event->getEntity();
         $lineItems = $entity->getLineItems();
+        $localization = $this->userLocalizationManager
+            ? $this->userLocalizationManager->getCurrentLocalization()
+            : null;
 
         foreach ($lineItems as $lineItem) {
             if (!$lineItem instanceof OrderLineItem) {
@@ -40,10 +55,8 @@ class ExtractLineItemPaymentOptionsListener
             }
 
             $lineItemModel = new LineItemOptionModel();
-
-            $name = implode(' ', array_filter([$product->getSku(), (string)$product->getDefaultName()]));
-            $description = $this->htmlTagHelper->stripTags((string)$product->getDefaultShortDescription());
-
+            $name = implode(' ', array_filter([$product->getSku(), (string)$product->getName($localization)]));
+            $description = $this->htmlTagHelper->stripTags((string)$product->getShortDescription($localization));
             $lineItemModel
                 ->setName($name)
                 ->setDescription($description)
