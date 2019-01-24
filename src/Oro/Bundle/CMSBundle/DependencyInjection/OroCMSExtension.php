14a386b2a0b37f92f8348001c7d99016399ca476
rchaliuk <rchaliuk@oroinc.com>
2019-01-24 13:56:24 +0200

    BB-15699: Simplify login page management (#21726)
    BB-15699: Simplify login page management

--- src/Oro/Bundle/CMSBundle/DependencyInjection/OroCMSExtension.php
+++ src/Oro/Bundle/CMSBundle/DependencyInjection/OroCMSExtension.php
@@ -7,6 +7,9 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
+/**
+ * This is the class that loads and manages bundle configuration
+ */
 class OroCMSExtension extends Extension
 {
     const ALIAS = 'oro_cms';
@@ -25,6 +28,11 @@ class OroCMSExtension extends Extension
         $loader->load('block_types.yml');
 
         $container->prependExtensionConfig($this->getAlias(), array_intersect_key($config, array_flip(['settings'])));
+
+        $container->setParameter(
+            sprintf('%s.%s.%s', self::ALIAS, Configuration::DIRECT_EDITING, Configuration::LOGIN_PAGE_CSS_FIELD_OPTION),
+            $config[Configuration::DIRECT_EDITING][Configuration::LOGIN_PAGE_CSS_FIELD_OPTION]
+        );
     }
 
     /**
