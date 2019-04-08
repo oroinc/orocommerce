744883414e61e063692a76f120634f8e6ba90b14
Viacheslav Bemza <vbemza@oroinc.com>
2019-04-08 11:23:38 +0300

    BAP-18586: Integer configuration values returned as strings after cache refresh (#23339)
    * BAP-18586: Integer configuration values returned as strings after cache refresh
    - add value normalization
    - cover by unit test

--- src/Oro/Bundle/ShoppingListBundle/Tests/Unit/Manager/ShoppingListLimitManagerTest.php
+++ src/Oro/Bundle/ShoppingListBundle/Tests/Unit/Manager/ShoppingListLimitManagerTest.php
@@ -314,24 +314,6 @@ class ShoppingListLimitManagerTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals(true, $this->shoppingListLimitManager->isOnlyOneEnabled());
     }
 
-    public function testIsOnlyOneEnabledStringLimitAndListOne()
-    {
-        $this->tokenAccessor
-            ->expects($this->once())
-            ->method('hasUser')
-            ->willReturn(true);
-
-        $this->configManager
-            ->expects($this->once())
-            ->method('get')
-            ->with('oro_shopping_list.shopping_list_limit')
-            ->willReturn('1');
-
-        $this->configureCount(1);
-
-        $this->assertEquals(true, $this->shoppingListLimitManager->isOnlyOneEnabled());
-    }
-
     /**
      * @param int $count
      */
