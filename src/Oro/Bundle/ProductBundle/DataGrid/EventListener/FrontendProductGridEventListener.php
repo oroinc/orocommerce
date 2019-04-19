19a6d77a9902406b73e5bd63b3c8c438409e07f1
Mykhailo Sulyma <msulima@oroinc.com>
2019-04-19 17:18:07 +0300

    BB-16525: Do not initialize product grid filters/sorters that are not present in product families (#23474)
     - removed listener which should disable excess filters and sorters
     - updated logic of listener which should add only required filters and sorters

--- src/Oro/Bundle/ProductBundle/DataGrid/EventListener/FrontendProductGridEventListener.php
+++ src/Oro/Bundle/ProductBundle/DataGrid/EventListener/FrontendProductGridEventListener.php
@@ -2,54 +2,96 @@
 
 namespace Oro\Bundle\ProductBundle\DataGrid\EventListener;
 
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
+use Oro\Bundle\DataGridBundle\Datagrid\Manager;
 use Oro\Bundle\DataGridBundle\Event\PreBuild;
+use Oro\Bundle\DataGridBundle\Provider\State\DatagridStateProviderInterface;
 use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
 use Oro\Bundle\EntityConfigBundle\Attribute\AttributeConfigurationProvider;
 use Oro\Bundle\EntityConfigBundle\Attribute\AttributeTypeRegistry;
+use Oro\Bundle\EntityConfigBundle\Attribute\Entity\AttributeFamily;
 use Oro\Bundle\EntityConfigBundle\Entity\FieldConfigModel;
+use Oro\Bundle\EntityConfigBundle\Entity\Repository\AttributeFamilyRepository;
 use Oro\Bundle\EntityConfigBundle\Manager\AttributeManager;
 use Oro\Bundle\FilterBundle\Form\Type\Filter\NumberFilterTypeInterface;
+use Oro\Bundle\ProductBundle\DependencyInjection\Configuration;
 use Oro\Bundle\ProductBundle\Entity\Product;
+use Oro\Bundle\ProductBundle\Search\ProductRepository;
+use Oro\Bundle\SearchBundle\Datagrid\Datasource\SearchDatasource;
 use Oro\Bundle\SearchBundle\Query\Query;
 use Oro\Bundle\WebsiteSearchBundle\Attribute\Type\SearchAttributeTypeInterface;
 use Oro\Bundle\WebsiteSearchBundle\Placeholder\EnumIdPlaceholder;
 use Oro\Bundle\WebsiteSearchBundle\Placeholder\LocalizationIdPlaceholder;
+use Oro\Component\DependencyInjection\ServiceLink;
 
 /**
  * Updates configuration of frontend products grid and add filter or sorter on it
- * depends on product attributes configuration
+ * depends on product attributes configuration and information about product families that are used in products
  */
 class FrontendProductGridEventListener
 {
     /** @var AttributeManager */
-    protected $attributeManager;
+    private $attributeManager;
 
     /** @var AttributeTypeRegistry */
-    protected $attributeTypeRegistry;
+    private $attributeTypeRegistry;
 
     /** @var AttributeConfigurationProvider */
-    protected $configurationProvider;
+    private $configurationProvider;
 
     /** @var DoctrineHelper */
-    protected $doctrineHelper;
+    private $doctrineHelper;
+
+    /** @var ProductRepository */
+    private $productRepository;
+
+    /** @var ServiceLink */
+    private $datagridManagerLink;
+
+    /** @var DatagridStateProviderInterface */
+    private $filtersStateProvider;
+
+    /** @var DatagridStateProviderInterface */
+    private $sortersStateProvider;
+
+    /** @var ConfigManager */
+    private $configManager;
+
+    /** @var bool */
+    private $inProgress = false;
 
     /**
      * @param AttributeManager $attributeManager
      * @param AttributeTypeRegistry $attributeTypeRegistry
      * @param AttributeConfigurationProvider $configurationProvider
+     * @param ProductRepository $productRepository
      * @param DoctrineHelper $doctrineHelper
+     * @param ServiceLink $datagridManagerLink
+     * @param DatagridStateProviderInterface $filtersStateProvider
+     * @param DatagridStateProviderInterface $sortersStateProvider
+     * @param ConfigManager $configManager
      */
     public function __construct(
         AttributeManager $attributeManager,
         AttributeTypeRegistry $attributeTypeRegistry,
         AttributeConfigurationProvider $configurationProvider,
-        DoctrineHelper $doctrineHelper
+        ProductRepository $productRepository,
+        DoctrineHelper $doctrineHelper,
+        ServiceLink $datagridManagerLink,
+        DatagridStateProviderInterface $filtersStateProvider,
+        DatagridStateProviderInterface $sortersStateProvider,
+        ConfigManager $configManager
     ) {
         $this->attributeManager = $attributeManager;
         $this->attributeTypeRegistry = $attributeTypeRegistry;
         $this->configurationProvider = $configurationProvider;
+        $this->productRepository = $productRepository;
         $this->doctrineHelper = $doctrineHelper;
+        $this->datagridManagerLink = $datagridManagerLink;
+        $this->filtersStateProvider = $filtersStateProvider;
+        $this->sortersStateProvider = $sortersStateProvider;
+        $this->configManager = $configManager;
     }
 
     /**
@@ -57,8 +99,16 @@ class FrontendProductGridEventListener
      */
     public function onPreBuild(PreBuild $event)
     {
+        if ($this->inProgress) {
+            return;
+        }
+        $this->inProgress = true;
+
         $config = $event->getConfig();
         $attrs = $this->attributeManager->getAttributesByClass(Product::class);
+
+        $addedFilterAttrs = [];
+        $addedSorterAttrs = [];
 
         foreach ($attrs as $attr) {
             $attributeType = $this->getAttributeType($attr);
@@ -69,13 +119,24 @@ class FrontendProductGridEventListener
             $label = $this->configurationProvider->getAttributeLabel($attr);
 
             if ($attributeType->isFilterable($attr) && $this->configurationProvider->isAttributeFilterable($attr)) {
-                $this->addFilter($config, $attr, $attributeType, $label);
+                $addedFilterAttrs[$attr->getId()] = $this->addFilter($config, $attr, $attributeType, $label);
             }
 
             if ($attributeType->isSortable($attr) && $this->configurationProvider->isAttributeSortable($attr)) {
-                $this->addSorter($config, $attr, $attributeType, $label);
+                $addedSorterAttrs[$attr->getId()] = $this->addSorter($config, $attr, $attributeType, $label);
             }
         }
+
+        $hideAttrs = $this->getAttributesToHide(
+            $config,
+            array_unique(array_merge(array_keys($addedFilterAttrs), array_keys($addedSorterAttrs)))
+        );
+        if ($hideAttrs) {
+            $this->checkFilters($event, $addedFilterAttrs, $hideAttrs);
+            $this->checkSorters($event, $addedSorterAttrs, $hideAttrs);
+        }
+
+        $this->inProgress = false;
     }
 
     /**
@@ -102,6 +163,7 @@ class FrontendProductGridEventListener
      * @param FieldConfigModel $attribute
      * @param SearchAttributeTypeInterface $attributeType
      * @param string $label
+     * @return string
      */
     protected function addFilter(
         DatagridConfiguration $config,
@@ -124,6 +186,26 @@ class FrontendProductGridEventListener
         }
 
         $config->addFilter($alias, $this->applyAdditionalParams($attribute, $attributeType, $params));
+
+        return $alias;
+    }
+
+    /**
+     * @param PreBuild $event
+     * @param array $addedFilterAttrs
+     * @param array $hideAttrs
+     */
+    protected function checkFilters(PreBuild $event, array $addedFilterAttrs, array $hideAttrs): void
+    {
+        $config = $event->getConfig();
+
+        $filtersState = $this->filtersStateProvider->getState($config, $event->getParameters());
+        foreach ($addedFilterAttrs as $attrId => $filterAlias) {
+            // check that filter must be hidden and not in use
+            if (in_array($attrId, $hideAttrs, true) && !array_key_exists($filterAlias, $filtersState)) {
+                $config->removeFilter($filterAlias);
+            }
+        }
     }
 
     /**
@@ -161,6 +243,7 @@ class FrontendProductGridEventListener
      * @param FieldConfigModel $attribute
      * @param SearchAttributeTypeInterface $attributeType
      * @param string $label
+     * @return string
      */
     protected function addSorter(
         DatagridConfiguration $config,
@@ -178,6 +261,26 @@ class FrontendProductGridEventListener
                 'data_name' => sprintf('%s.%s', $attributeType->getSorterStorageFieldType(), $name),
             ]
         );
+
+        return $alias;
+    }
+
+    /**
+     * @param PreBuild $event
+     * @param array $addedSorterAttrs
+     * @param array $hideAttrs
+     */
+    protected function checkSorters(PreBuild $event, array $addedSorterAttrs, array $hideAttrs): void
+    {
+        $config = $event->getConfig();
+
+        $sortersState = $this->sortersStateProvider->getState($config, $event->getParameters());
+        foreach ($addedSorterAttrs as $attrId => $sorterAlias) {
+            // check that sorter must be hidden and not in use
+            if (in_array($attrId, $hideAttrs, true) && !array_key_exists($sorterAlias, $sortersState)) {
+                $config->removeSorter($sorterAlias);
+            }
+        }
     }
 
     /**
@@ -186,7 +289,7 @@ class FrontendProductGridEventListener
      */
     private function clearName($name)
     {
-        $placeholders = ['_'.LocalizationIdPlaceholder::NAME => '', '_'.EnumIdPlaceholder::NAME => ''];
+        $placeholders = ['_' . LocalizationIdPlaceholder::NAME => '', '_' . EnumIdPlaceholder::NAME => ''];
 
         return strtr($name, $placeholders);
     }
@@ -212,5 +315,63 @@ class FrontendProductGridEventListener
         $mapping = $metadata->getAssociationMapping($fieldName);
 
         return $mapping['targetEntity'] ?? null;
+    }
+
+    /**
+     * @param DatagridConfiguration $config
+     * @param array $attributes
+     * @return array
+     */
+    private function getAttributesToHide(DatagridConfiguration $config, array $attributes): array
+    {
+        $hideAttributes = [];
+
+        $configKey = Configuration::getConfigKeyByName(Configuration::LIMIT_FILTERS_SORTERS_ON_PRODUCT_LISTING);
+        if ($this->configManager->get($configKey)) {
+            /** @var Manager $datagridManager */
+            $datagridManager = $this->datagridManagerLink->getService();
+
+            /** @var SearchDatasource $datasource */
+            $datasource = $datagridManager->getDatagrid($config->getName())
+                ->acceptDatasource()
+                ->getDatasource();
+
+            $data = $this->productRepository
+                ->getFamilyAttributeCountsQuery($datasource->getSearchQuery(), 'familyAttributesCount')
+                ->getResult()
+                ->getAggregatedData();
+
+            if (!empty($data['familyAttributesCount'])) {
+                $hideAttributes = $this->getDisabledSortAndFilterAttributes(
+                    $attributes,
+                    array_keys($data['familyAttributesCount'])
+                );
+            }
+        }
+
+        return $hideAttributes;
+    }
+
+    /**
+     * @param array $attributes
+     * @param array $activeAttributeFamilyIds
+     *
+     * @return array
+     */
+    private function getDisabledSortAndFilterAttributes(array $attributes, array $activeAttributeFamilyIds): array
+    {
+        /** @var AttributeFamilyRepository $attributeFamilyRepository */
+        $attributeFamilyRepository = $this->doctrineHelper->getEntityRepository(AttributeFamily::class);
+        $familyIdsForAttributes = $attributeFamilyRepository->getFamilyIdsForAttributes($attributes);
+
+        return array_filter(
+            $attributes,
+            function ($attrId) use ($familyIdsForAttributes, $activeAttributeFamilyIds) {
+                // skip attributes without product families or
+                return empty($familyIdsForAttributes[$attrId]) ||
+                    // skip attributes that are not included to active attribute families
+                    empty(array_intersect($activeAttributeFamilyIds, $familyIdsForAttributes[$attrId]));
+            }
+        );
     }
 }
