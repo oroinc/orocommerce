83c7198f75d96c0349604be72a10a8f259d90a00
Vladimir Seniuk <vseniuk@oroinc.com>
2019-09-18 16:57:02 +0300

    BB-17578: Backmerge GrapesJS frontend story - reverted OroRichTextType - replaced OroRichTextType with WYSIWYGType for PageType - added GrapesJS for WYSIWYGType - reverted strict mode for HTML

--- src/Oro/Bundle/CMSBundle/Form/Type/WYSIWYGType.php
+++ src/Oro/Bundle/CMSBundle/Form/Type/WYSIWYGType.php
@@ -4,12 +4,39 @@ namespace Oro\Bundle\CMSBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Provides WYSIWYG editor functionality.
  */
 class WYSIWYGType extends AbstractType
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['attr']['data-page-component-module'] = $options['page-component']['module'];
+        $view->vars['attr']['data-page-component-options'] = json_encode($options['page-component']['options']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'page-component' => [
+                'module' => 'oroui/js/app/components/view-component',
+                'options' => [
+                    'view' => 'orocms/js/app/views/grapesjs-editor-view'
+                ]
+            ]
+        ]);
+    }
+
     /**
      * {@inheritdoc}
      */
