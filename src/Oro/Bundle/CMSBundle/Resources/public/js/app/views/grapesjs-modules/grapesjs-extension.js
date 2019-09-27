5b3de8ed57cfc75e07100d9c86346fda309cab3c
Vladimir Seniuk <vladimirseniuk@users.noreply.github.com>
2019-09-27 12:26:02 +0300

    BB-17578: Backmerge GrapesJS frontend story (#25232)
    * BB-17578: Backmerge GrapesJS frontend story
    - updated composer dev locks
    - removed ContentBuilderBundle
    - moved all GrapesJS logic to CMSBundle
    - reverted OroRichTextType
    - replaced OroRichTextType with WYSIWYGType for PageType
    - added GrapesJS for WYSIWYGType
    - reverted strict mode for HTML
    - changed wysiwyg styles type from textarea to hidden type
    - changed RichTextType to WYSIWYGType for content in TextContentVariantType
    - updated unit tests
    - moved grapesjs library from platform to commerce
    - locked phpunit version (see BAP-19259)

---
+++ src/Oro/Bundle/CMSBundle/Resources/public/js/app/views/grapesjs-modules/grapesjs-extension.js
@@ -0,0 +1,46 @@
+define(function(require) {
+    'use strict';
+
+    var ExtensionModule = function(options) {
+        _.extend(this, _.pick(options, ['view', 'builder']));
+
+        this.componentCssIdRegexp = /(\[component-id-view([\d]*)\])/g;
+        this.componentHtmlIdRegexp = /(<div component-id-view([\d]*))/g;
+        this.cssSelectorRegexp = /([(.|#)?\w-*]*?(\s)?{)/g;
+
+        this.init();
+    };
+
+    ExtensionModule.prototype = {
+        init: function() {
+            this.wrapMethods();
+
+            _.extend(this.view, _.pick(this, ['removeCSSContainerId']));
+        },
+
+        wrapMethods: function() {
+            var uniqId = 'component-id-' + _.uniqueId('view');
+
+            this.builder.getHtml = _.wrap(this.builder.getHtml, _.bind(function(func) {
+                var html = func();
+                if (this.componentHtmlIdRegexp.test(html)) {
+                    html = $(html).unwrap().html();
+                }
+                html = !html ? html : '<div ' + uniqId + '>' + html + '</div>';
+                return html;
+            }, this));
+
+            this.builder.getCss = _.wrap(this.builder.getCss, _.bind(function(func) {
+                var css = this.removeCSSContainerId(func());
+                css = css.replace(this.cssSelectorRegexp, '[' + uniqId + '] $&');
+                return css;
+            }, this));
+        },
+
+        removeCSSContainerId: function(cssText) {
+            return cssText.replace(this.componentCssIdRegexp, '');
+        }
+    };
+
+    return ExtensionModule;
+});
--- src/Oro/Bundle/CMSBundle/Tests/Unit/Form/Type/WYSIWYGStylesTypeTest.php
+++ src/Oro/Bundle/CMSBundle/Tests/Unit/Form/Type/WYSIWYGStylesTypeTest.php
@@ -3,7 +3,8 @@
 namespace Oro\Bundle\CMSBundle\Tests\Unit\Form\Type;
 
 use Oro\Bundle\CMSBundle\Form\Type\WYSIWYGStylesType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\Test\FormIntegrationTestCase;
 
 class WYSIWYGStylesTypeTest extends FormIntegrationTestCase
@@ -11,7 +12,7 @@ class WYSIWYGStylesTypeTest extends FormIntegrationTestCase
     public function testGetParent()
     {
         $type = new WYSIWYGStylesType();
-        $this->assertEquals(TextareaType::class, $type->getParent());
+        $this->assertEquals(HiddenType::class, $type->getParent());
     }
 
     public function testSubmit()
@@ -19,5 +20,15 @@ class WYSIWYGStylesTypeTest extends FormIntegrationTestCase
         $form = $this->factory->create(WYSIWYGStylesType::class);
         $form->submit('h1 { color: black; }');
         $this->assertEquals('h1 { color: black; }', $form->getData());
+    }
+
+    public function testFinishView()
+    {
+        $view = new FormView();
+        $form = $this->factory->create(WYSIWYGStylesType::class);
+        $type = new WYSIWYGStylesType();
+        $type->finishView($view, $form, []);
+
+        $this->assertEquals('wysiwyg_styles', $view->vars['attr']['data-grapesjs-styles']);
     }
 }
