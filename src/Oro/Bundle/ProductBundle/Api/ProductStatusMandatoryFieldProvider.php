63c945542cad79b31ab3f363c0c8c68daab23eaa
Vova Soroka <vsoroka@oroinc.com>
2019-09-30 09:40:52 +0300

    BAP-10066: Remove TODOs (#25346)

--- src/Oro/Bundle/ProductBundle/Api/ProductStatusMandatoryFieldProvider.php
+++ src/Oro/Bundle/ProductBundle/Api/ProductStatusMandatoryFieldProvider.php
@@ -9,8 +9,6 @@ use Oro\Bundle\ProductBundle\Entity\Product;
  * Adds the product status field to SELECT clause for all product queries.
  * As "partial" is used API queries, the product status is required to correct work of ProductStatusVoter.
  * @see \Oro\Bundle\ProductBundle\Acl\Voter\ProductStatusVoter
- * This class can be removed if EntitySerializer component will be reimplemented
- * to use DBAL instead of ORM (BAP-10066).
  */
 class ProductStatusMandatoryFieldProvider implements MandatoryFieldProviderInterface
 {
