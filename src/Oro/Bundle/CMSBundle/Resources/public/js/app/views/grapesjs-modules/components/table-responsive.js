fa3ed3bbc50fa110570bee27cb14674941f10506
Oleksandr Musiienko <omusiienko@oroinc.com>
2019-10-07 11:48:52 +0300

    BB-17571: WYSIWYG form field - fix js tests

--- src/Oro/Bundle/CMSBundle/Resources/public/js/app/views/grapesjs-modules/components/table-responsive.js
+++ src/Oro/Bundle/CMSBundle/Resources/public/js/app/views/grapesjs-modules/components/table-responsive.js
@@ -3,55 +3,66 @@ define(function(require) {
 
     var TableResponsiveComponent;
     var _ = require('underscore');
+    var BaseComponent = require('oroui/js/app/components/base/component');
 
     /**
      * Create responsive table component type for builder
-     * @param context
-     * @constructor
      */
-    TableResponsiveComponent = function TableResponsiveComponent(context) {
-        var ComponentId = 'table-responsive';
-        var domComps = context.DomComponents;
-        var dType = domComps.getType('default');
-        var dModel = dType.model;
-        var dView = dType.view;
+    TableResponsiveComponent = BaseComponent.extend({
+        /**
+         * @inheritDoc
+         */
+        constructor: function TableResponsiveComponent() {
+            TableResponsiveComponent.__super__.constructor.apply(this, arguments);
+        },
 
-        domComps.addType(ComponentId, {
-            model: dModel.extend({
-                defaults: _.extend({}, dModel.prototype.defaults, {
-                    type: ComponentId,
-                    tagName: 'div',
-                    draggable: ['div'],
-                    droppable: ['table', 'tbody', 'thead', 'tfoot'],
-                    classes: [ComponentId]
+        /**
+         * @inheritDoc
+         */
+        initialize: function(options) {
+            var ComponentId = 'table-responsive';
+            var domComps = options.DomComponents;
+            var dType = domComps.getType('default');
+            var dModel = dType.model;
+            var dView = dType.view;
+
+            domComps.addType(ComponentId, {
+                model: dModel.extend({
+                    defaults: _.extend({}, dModel.prototype.defaults, {
+                        type: ComponentId,
+                        tagName: 'div',
+                        draggable: ['div'],
+                        droppable: ['table', 'tbody', 'thead', 'tfoot'],
+                        classes: [ComponentId]
+                    }),
+                    constructor: function () {
+                        dModel.prototype.constructor.apply(this, arguments);
+                    },
+                    initialize: function (o, opt) {
+                        dModel.prototype.initialize.apply(this, arguments);
+                        var components = this.get('components');
+                        if (!components.length) {
+                            components.add({
+                                type: 'table'
+                            });
+                        }
+                    }
+                }, {
+                    isComponent: function (el) {
+                        var result = '';
+                        if (el.tagName === 'DIV' && el.className.indexOf(ComponentId) !== -1) {
+                            result = {
+                                type: ComponentId
+                            };
+                        }
+
+                        return result;
+                    }
                 }),
-                constructor: function () {
-                    dModel.prototype.constructor.apply(this, arguments);
-                },
-                initialize: function(o, opt) {
-                    dModel.prototype.initialize.apply(this, arguments);
-                    var components = this.get('components');
-                    if (!components.length) {
-                        components.add({
-                            type: 'table'
-                        });
-                    }
-                }
-            }, {
-                isComponent: function(el) {
-                    var result = '';
-                    if (el.tagName === 'DIV' && el.className.indexOf(ComponentId) !== -1) {
-                        result = {
-                            type: ComponentId
-                        };
-                    }
-
-                    return result;
-                }
-            }),
-            view: dView
-        });
-    };
+                view: dView
+            });
+        }
+    });
 
     return TableResponsiveComponent;
 });
