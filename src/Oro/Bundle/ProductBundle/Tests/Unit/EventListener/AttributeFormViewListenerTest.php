0e7d70c095b1387c004ee59f3d06dfa2dfe5f864
Mykhailo Sulyma <msulima@oroinc.com>
2019-10-25 15:01:20 +0300

    BB-17970: Handle logic for Attribute Groups (#25525)
     - updated view and update templates
     - updated all listeners related to product`s view and edit pages
     - fixed failed builds
     - added render_content to product description

--- src/Oro/Bundle/ProductBundle/Tests/Unit/EventListener/AttributeFormViewListenerTest.php
+++ src/Oro/Bundle/ProductBundle/Tests/Unit/EventListener/AttributeFormViewListenerTest.php
@@ -2,16 +2,23 @@
 
 namespace Oro\Bundle\ProductBundle\Tests\Unit\EventListener;
 
+use Oro\Bundle\CMSBundle\DBAL\Types\WYSIWYGType;
 use Oro\Bundle\EntityConfigBundle\Attribute\Entity\AttributeFamily;
+use Oro\Bundle\EntityConfigBundle\Config\Config;
+use Oro\Bundle\EntityConfigBundle\Config\Id\FieldConfigId;
 use Oro\Bundle\EntityConfigBundle\Entity\FieldConfigModel;
 use Oro\Bundle\EntityConfigBundle\Manager\AttributeManager;
+use Oro\Bundle\EntityConfigBundle\Provider\ConfigProvider;
 use Oro\Bundle\EntityConfigBundle\Tests\Unit\Stub\AttributeGroupStub;
 use Oro\Bundle\LocaleBundle\Entity\LocalizedFallbackValue;
+use Oro\Bundle\ProductBundle\Entity\Product;
 use Oro\Bundle\ProductBundle\EventListener\AttributeFormViewListener;
 use Oro\Bundle\TestFrameworkBundle\Entity\TestActivityTarget;
 use Oro\Bundle\UIBundle\Event\BeforeListRenderEvent;
 use Oro\Bundle\UIBundle\View\ScrollData;
 use Oro\Component\Testing\Unit\EntityTrait;
+use Symfony\Component\Form\FormView;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
 
 class AttributeFormViewListenerTest extends \PHPUnit\Framework\TestCase
@@ -29,6 +36,16 @@ class AttributeFormViewListenerTest extends \PHPUnit\Framework\TestCase
     private $attributeManager;
 
     /**
+     * @var ConfigProvider|\PHPUnit\Framework\MockObject\MockObject
+     */
+    private $entityConfigProvider;
+
+    /**
+     * @var TranslatorInterface|\PHPUnit\Framework\MockObject\MockObject
+     */
+    private $translator;
+
+    /**
      * @var AttributeFormViewListener
      */
     private $listener;
@@ -38,15 +55,29 @@ class AttributeFormViewListenerTest extends \PHPUnit\Framework\TestCase
      */
     public function setUp()
     {
-        $this->environment = $this->getMockBuilder(Environment::class)
-            ->disableOriginalConstructor()
-            ->getMock();
+        $this->environment = $this->createMock(Environment::class);
+        $this->attributeManager = $this->createMock(AttributeManager::class);
 
-        $this->attributeManager = $this->getMockBuilder(AttributeManager::class)
-            ->disableOriginalConstructor()
-            ->getMock();
+        $this->entityConfigProvider = $this->createMock(ConfigProvider::class);
+        $this->entityConfigProvider->expects($this->any())
+            ->method('getConfig')
+            ->with(Product::class, 'wysiwyg')
+            ->willReturn(new Config(
+                new FieldConfigId('attachment', Product::class, 'wysiwyg', WYSIWYGType::TYPE),
+                ['label' => 'wysiwyg field label']
+            ));
 
-        $this->listener = new AttributeFormViewListener($this->attributeManager);
+        $this->translator = $this->createMock(TranslatorInterface::class);
+        $this->translator->expects($this->any())
+            ->method('trans')
+            ->with('wysiwyg field label')
+            ->willReturn('translated wysiwyg field label');
+
+        $this->listener = new AttributeFormViewListener(
+            $this->attributeManager,
+            $this->entityConfigProvider,
+            $this->translator
+        );
     }
 
     /**
@@ -110,6 +141,13 @@ class AttributeFormViewListenerTest extends \PHPUnit\Framework\TestCase
             $this->getEntity(FieldConfigModel::class, ['id' => 1, 'fieldName' => 'productPriceAttributesPrices']);
         $shortDescription = $this->getEntity(FieldConfigModel::class, ['id' => 1, 'fieldName' => 'shortDescriptions']);
         $descriptions = $this->getEntity(FieldConfigModel::class, ['id' => 1, 'fieldName' => 'descriptions']);
+        $wysiwyg = $this->getEntity(
+            FieldConfigModel::class,
+            ['id' => 1, 'fieldName' => 'wysiwyg', 'type' => WYSIWYGType::TYPE, 'data' => [
+                'view' => ['is_displayable' => true],
+                'form' => ['is_enabled' => true]
+            ]]
+        );
 
         return [
             'move attribute field to other group not allowed (inventory_status)' => [
@@ -344,6 +382,352 @@ class AttributeFormViewListenerTest extends \PHPUnit\Framework\TestCase
                                     'data' => ['someField' => 'field template'],
                                 ],
                             ],
+                        ],
+                    ],
+                ],
+            ],
+            'move wysiwyg attribute field to own group' => [
+                'groupsData' => [
+                    ['group' => $group1, 'attributes' => [$wysiwyg]],
+                ],
+                'scrollData' => [
+                    ScrollData::DATA_BLOCKS => [
+                        'existingGroup' => [
+                            'title' => 'Group1Title',
+                            'useSubBlockDivider' => true,
+                            'subblocks' => [
+                                [
+                                    'data' => [
+                                        'someField' => 'field template',
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                'templateHtml' => 'expected template data',
+                'expectedData' => [
+                    ScrollData::DATA_BLOCKS => [
+                        'existingGroup' => [
+                            'title' => 'Group1Title',
+                            'useSubBlockDivider' => true,
+                            'subblocks' => [
+                                [
+                                    'data' => [
+                                        'someField' => 'field template'
+                                    ],
+                                ],
+                            ],
+                        ],
+                        'wysiwyg' => [
+                            'title' => 'translated wysiwyg field label',
+                            'useSubBlockDivider' => true,
+                            'subblocks' => [
+                                [
+                                    'data' => [
+                                        'wysiwyg' => 'expected template data'
+                                    ],
+                                ],
+                            ],
+                            'priority' => 501,
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider editDataProvider
+     *
+     * @param array $groupsData
+     * @param array $scrollData
+     * @param string $fieldName
+     * @param string $templateHtml
+     * @param array $expectedData
+     */
+    public function testOnEdit(
+        array $groupsData,
+        array $scrollData,
+        string $fieldName,
+        $templateHtml,
+        array $expectedData
+    ) {
+        $entity = $this->getEntity(TestActivityTarget::class, [
+            'attributeFamily' => $this->getEntity(AttributeFamily::class),
+        ]);
+
+        $this->environment
+            ->expects($templateHtml ? $this->once() : $this->never())
+            ->method('render')
+            ->willReturn($templateHtml);
+
+        $this->attributeManager
+            ->expects($this->once())
+            ->method('getGroupsWithAttributes')
+            ->willReturn($groupsData);
+
+        $scrollData = new ScrollData($scrollData);
+
+        $attributeView = new FormView();
+        if (!$templateHtml) {
+            $attributeView->setRendered();
+        }
+
+        $formView = new FormView();
+        $formView->children[$fieldName] = $attributeView;
+
+        $listEvent = new BeforeListRenderEvent($this->environment, $scrollData, $entity, $formView);
+        $this->listener->onEdit($listEvent);
+
+        $this->assertEquals($expectedData, $listEvent->getScrollData()->getData());
+    }
+
+    /**
+     * @return array
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function editDataProvider()
+    {
+        $label = $this->getEntity(LocalizedFallbackValue::class, ['string' => 'Group1Title']);
+        $group1 = $this->getEntity(AttributeGroupStub::class, ['code' => 'group1', 'label' => $label]);
+
+        $attributeVisible = $this->getEntity(
+            FieldConfigModel::class,
+            [
+                'id' => 1,
+                'fieldName' => 'someField',
+                'data' => [
+                    'view' => ['is_displayable' => true],
+                    'form' => ['is_enabled' => true]
+                ]
+            ]
+        );
+
+        $inventoryStatus = $this->getEntity(FieldConfigModel::class, ['id' => 1, 'fieldName' => 'inventory_status']);
+        $images = $this->getEntity(FieldConfigModel::class, ['id' => 1, 'fieldName' => 'images']);
+        $productPriceAttributesPrices =
+            $this->getEntity(FieldConfigModel::class, ['id' => 1, 'fieldName' => 'productPriceAttributesPrices']);
+        $wysiwyg = $this->getEntity(
+            FieldConfigModel::class,
+            ['id' => 1, 'fieldName' => 'wysiwyg', 'type' => WYSIWYGType::TYPE, 'data' => [
+                'view' => ['is_displayable' => true],
+                'form' => ['is_enabled' => true]
+            ]]
+        );
+
+        return [
+            'move attribute field to other group not allowed (inventory_status)' => [
+                'groupsData' => [
+                    ['group' => $group1, 'attributes' => [$inventoryStatus]],
+                ],
+                'scrollData' => [
+                    ScrollData::DATA_BLOCKS => [
+                        'existingGroup' => [
+                            'title' => 'Group1Title',
+                            'useSubBlockDivider' => true,
+                            'subblocks' => [
+                                [
+                                    'data' => [
+                                        'inventory_status' => 'field template',
+                                        'otherField' => 'field template',
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                'fieldName' => 'inventory_status',
+                'templateHtml' => false,
+                'expectedData' => [
+                    ScrollData::DATA_BLOCKS => [
+                        'existingGroup' => [
+                            'title' => 'Group1Title',
+                            'useSubBlockDivider' => true,
+                            'subblocks' => [
+                                [
+                                    'data' => [
+                                        'inventory_status' => 'field template',
+                                        'otherField' => 'field template',
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'move attribute field to other group not allowed (images)' => [
+                'groupsData' => [
+                    ['group' => $group1, 'attributes' => [$images]],
+                ],
+                'scrollData' => [
+                    ScrollData::DATA_BLOCKS => [
+                        'existingGroup' => [
+                            'title' => 'Group1Title',
+                            'useSubBlockDivider' => true,
+                            'subblocks' => [
+                                [
+                                    'data' => [
+                                        'images' => 'field template',
+                                        'otherField' => 'field template',
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                'fieldName' => 'images',
+                'templateHtml' => false,
+                'expectedData' => [
+                    ScrollData::DATA_BLOCKS => [
+                        'existingGroup' => [
+                            'title' => 'Group1Title',
+                            'useSubBlockDivider' => true,
+                            'subblocks' => [
+                                [
+                                    'data' => [
+                                        'images' => 'field template',
+                                        'otherField' => 'field template',
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'move attribute field to other group not allowed (productPriceAttributesPrices)' => [
+                'groupsData' => [
+                    ['group' => $group1, 'attributes' => [$productPriceAttributesPrices]],
+                ],
+                'scrollData' => [
+                    ScrollData::DATA_BLOCKS => [
+                        'existingGroup' => [
+                            'title' => 'Group1Title',
+                            'useSubBlockDivider' => true,
+                            'subblocks' => [
+                                [
+                                    'data' => [
+                                        'productPriceAttributesPrices' => 'field template',
+                                        'otherField' => 'field template',
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                'fieldName' => 'productPriceAttributesPrices',
+                'templateHtml' => false,
+                'expectedData' => [
+                    ScrollData::DATA_BLOCKS => [
+                        'existingGroup' => [
+                            'title' => 'Group1Title',
+                            'useSubBlockDivider' => true,
+                            'subblocks' => [
+                                [
+                                    'data' => [
+                                        'productPriceAttributesPrices' => 'field template',
+                                        'otherField' => 'field template',
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'move attribute field to other group' => [
+                'groupsData' => [
+                    ['group' => $group1, 'attributes' => [$attributeVisible]],
+                ],
+                'scrollData' => [
+                    ScrollData::DATA_BLOCKS => [
+                        'existingGroup' => [
+                            'title' => 'Group1Title',
+                            'useSubBlockDivider' => true,
+                            'subblocks' => [
+                                [
+                                    'data' => [
+                                        'someField' => 'field template',
+                                        'otherField' => 'field template',
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                'fieldName' => 'someField',
+                'templateHtml' => false,
+                'expectedData' => [
+                    ScrollData::DATA_BLOCKS => [
+                        'existingGroup' => [
+                            'title' => 'Group1Title',
+                            'useSubBlockDivider' => true,
+                            'subblocks' => [
+                                [
+                                    'data' => [
+                                        'otherField' => 'field template',
+                                    ],
+                                ],
+                            ],
+                        ],
+                        'group1' => [
+                            'title' => 'Group1Title',
+                            'useSubBlockDivider' => true,
+                            'subblocks' => [
+                                [
+                                    'data' => [
+                                        'someField' => 'field template'
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'move wysiwyg attribute field to own group' => [
+                'groupsData' => [
+                    ['group' => $group1, 'attributes' => [$wysiwyg]],
+                ],
+                'scrollData' => [
+                    ScrollData::DATA_BLOCKS => [
+                        'existingGroup' => [
+                            'title' => 'Group1Title',
+                            'useSubBlockDivider' => true,
+                            'subblocks' => [
+                                [
+                                    'data' => [
+                                        'someField' => 'field template',
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                'fieldName' => 'wysiwyg',
+                'templateHtml' => 'expected template data',
+                'expectedData' => [
+                    ScrollData::DATA_BLOCKS => [
+                        'existingGroup' => [
+                            'title' => 'Group1Title',
+                            'useSubBlockDivider' => true,
+                            'subblocks' => [
+                                [
+                                    'data' => [
+                                        'someField' => 'field template'
+                                    ],
+                                ],
+                            ],
+                        ],
+                        'wysiwyg' => [
+                            'title' => 'translated wysiwyg field label',
+                            'useSubBlockDivider' => true,
+                            'subblocks' => [
+                                [
+                                    'data' => [
+                                        'wysiwyg' => 'expected template data'
+                                    ],
+                                ],
+                            ],
+                            'priority' => 501,
                         ],
                     ],
                 ],
--- src/Oro/Bundle/SEOBundle/EventListener/ProductFormViewListener.php
+++ src/Oro/Bundle/SEOBundle/EventListener/ProductFormViewListener.php
@@ -6,6 +6,9 @@ use Oro\Bundle\ProductBundle\Entity\Product;
 use Oro\Bundle\UIBundle\Event\BeforeListRenderEvent;
 use Oro\Component\Exception\UnexpectedTypeException;
 
+/**
+ * Adds SEO information to the product view and edit pages.
+ */
 class ProductFormViewListener extends BaseFormViewListener
 {
     /**
@@ -49,7 +52,7 @@ class ProductFormViewListener extends BaseFormViewListener
         ]);
         $scrollData = $event->getScrollData();
         $blockLabel = $this->translator->trans('oro.seo.label');
-        $scrollData->addNamedBlock(self::SEO_BLOCK_ID, $blockLabel, 800);
+        $scrollData->addNamedBlock(self::SEO_BLOCK_ID, $blockLabel, 1700);
         $subBlock = $scrollData->addSubBlock(self::SEO_BLOCK_ID);
         $scrollData->addSubBlockData(self::SEO_BLOCK_ID, $subBlock, $slugsTemplate, 'generatedSlugs');
         $scrollData->addSubBlockData(self::SEO_BLOCK_ID, $subBlock, $titleTemplate, 'metaTitles');
@@ -62,7 +65,7 @@ class ProductFormViewListener extends BaseFormViewListener
      */
     public function onProductEdit(BeforeListRenderEvent $event)
     {
-        $this->addEditPageBlock($event);
+        $this->addEditPageBlock($event, 1700);
     }
 
     /**
