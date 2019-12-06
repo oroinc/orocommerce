39d2df0f18e282637bf2287fbf415d5af8b215f3
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2019-12-06 14:51:24 +0200

    BB-18063: Create voter for draftable entities (#26018)
    - added migration
    - added draft source extension
    - added draft voter

--- src/Oro/Bundle/CMSBundle/Migrations/Schema/OroCMSBundleInstaller.php
+++ src/Oro/Bundle/CMSBundle/Migrations/Schema/OroCMSBundleInstaller.php
@@ -127,9 +127,11 @@ class OroCMSBundleInstaller implements
         $table->addColumn('draft_project_id', 'integer', ['notnull' => false]);
         $table->addColumn('draft_source_id', 'integer', ['notnull' => false]);
         $table->addColumn('draft_uuid', 'guid', ['notnull' => false]);
+        $table->addColumn('draft_owner_id', 'integer', ['notnull' => false]);
         $table->setPrimaryKey(['id']);
         $table->addIndex(['draft_project_id'], 'IDX_BCE4CB4A2E26AC0B');
         $table->addIndex(['draft_source_id'], 'IDX_BCE4CB4A953C1C61');
+        $table->addIndex(['draft_owner_id'], 'IDX_BCE4CB4ADCA3D9F3');
     }
 
     /**
@@ -155,6 +157,12 @@ class OroCMSBundleInstaller implements
         $table->addForeignKeyConstraint(
             $schema->getTable('oro_cms_page'),
             ['draft_source_id'],
+            ['id'],
+            ['onDelete' => 'CASCADE']
+        );
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_user'),
+            ['draft_owner_id'],
             ['id'],
             ['onDelete' => 'CASCADE']
         );
--- src/Oro/Bundle/CMSBundle/Migrations/Schema/v1_7/CreateOroCmsPageDraftColumns.php
+++ src/Oro/Bundle/CMSBundle/Migrations/Schema/v1_7/CreateOroCmsPageDraftColumns.php
@@ -30,8 +30,10 @@ class CreateOroCmsPageDraftColumns implements Migration
         $table->addColumn('draft_project_id', 'integer', ['notnull' => false]);
         $table->addColumn('draft_source_id', 'integer', ['notnull' => false]);
         $table->addColumn('draft_uuid', 'guid', ['notnull' => false]);
+        $table->addColumn('draft_owner_id', 'integer', ['notnull' => false]);
         $table->addIndex(['draft_project_id'], 'IDX_BCE4CB4A2E26AC0B');
         $table->addIndex(['draft_source_id'], 'IDX_BCE4CB4A953C1C61');
+        $table->addIndex(['draft_owner_id'], 'IDX_BCE4CB4ADCA3D9F3');
     }
 
     /**
@@ -49,6 +51,12 @@ class CreateOroCmsPageDraftColumns implements Migration
         $table->addForeignKeyConstraint(
             $schema->getTable('oro_cms_page'),
             ['draft_source_id'],
+            ['id'],
+            ['onDelete' => 'CASCADE']
+        );
+        $table->addForeignKeyConstraint(
+            $schema->getTable('oro_user'),
+            ['draft_owner_id'],
             ['id'],
             ['onDelete' => 'CASCADE']
         );
