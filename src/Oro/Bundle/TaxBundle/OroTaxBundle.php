d9fd08b359e75317a6ec503de5dbd1fdd9c31fd8
Vova Soroka <vsoroka@oroinc.com>
2019-12-26 15:20:55 +0200

    BAP-19552: Remove unneeded compiler passes to register tagged services with priority (#26282)

--- src/Oro/Bundle/TaxBundle/OroTaxBundle.php
+++ src/Oro/Bundle/TaxBundle/OroTaxBundle.php
@@ -5,11 +5,14 @@ namespace Oro\Bundle\TaxBundle;
 use Oro\Bundle\TaxBundle\DependencyInjection\CompilerPass\AddressMatcherRegistryPass;
 use Oro\Bundle\TaxBundle\DependencyInjection\CompilerPass\ResolverEventConnectorPass;
 use Oro\Bundle\TaxBundle\DependencyInjection\CompilerPass\TaxMapperPass;
-use Oro\Bundle\TaxBundle\DependencyInjection\CompilerPass\TaxProviderPass;
 use Oro\Bundle\TaxBundle\DependencyInjection\OroTaxExtension;
+use Oro\Component\DependencyInjection\Compiler\PriorityTaggedServiceViaAddMethodCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * The TaxBundle bundle class.
+ */
 class OroTaxBundle extends Bundle
 {
     /**
@@ -17,7 +20,11 @@ class OroTaxBundle extends Bundle
      */
     public function build(ContainerBuilder $container)
     {
-        $container->addCompilerPass(new TaxProviderPass());
+        $container->addCompilerPass(new PriorityTaggedServiceViaAddMethodCompilerPass(
+            'oro_tax.provider.tax_provider_registry',
+            'addProvider',
+            'oro_tax.tax_provider'
+        ));
         $container->addCompilerPass(new TaxMapperPass());
         $container->addCompilerPass(new ResolverEventConnectorPass());
         $container->addCompilerPass(new AddressMatcherRegistryPass());
