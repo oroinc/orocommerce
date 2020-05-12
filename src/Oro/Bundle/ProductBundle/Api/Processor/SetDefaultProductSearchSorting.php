3d72518b5aaaec98bb5ece81ff57920fbdc16508
Vova Soroka <vsoroka@oroinc.com>
2020-05-12 17:53:51 +0300

    BAP-19885: Make it possible to remove default sorting in API (#27763)

--- src/Oro/Bundle/ProductBundle/Api/Processor/SetDefaultProductSearchSorting.php
+++ src/Oro/Bundle/ProductBundle/Api/Processor/SetDefaultProductSearchSorting.php
@@ -4,6 +4,7 @@ namespace Oro\Bundle\ProductBundle\Api\Processor;
 
 use Oro\Bundle\ApiBundle\Collection\Criteria;
 use Oro\Bundle\ApiBundle\Config\EntityDefinitionConfig;
+use Oro\Bundle\ApiBundle\Config\SortersConfig;
 use Oro\Bundle\ApiBundle\Processor\Shared\SetDefaultSorting;
 
 /**
@@ -16,10 +17,10 @@ class SetDefaultProductSearchSorting extends SetDefaultSorting
     /**
      * {@inheritdoc}
      */
-    protected function getDefaultValue(EntityDefinitionConfig $config): array
+    protected function getDefaultValue(EntityDefinitionConfig $config, ?SortersConfig $configOfSorters): array
     {
         $orderBy = $config->getOrderBy();
-        if (empty($orderBy)) {
+        if (!$orderBy && $this->isSorterEnabled(self::RELEVANCE_SORT_FIELD, $configOfSorters, false)) {
             $orderBy = [self::RELEVANCE_SORT_FIELD => Criteria::ASC];
         }
 
