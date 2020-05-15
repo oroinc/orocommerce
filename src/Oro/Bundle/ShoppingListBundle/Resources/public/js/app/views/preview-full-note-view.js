7939f4dae412d378871c5aec8a5d90f462774ad4
ValeriyYustunyk <ValeriyYustunyk@users.noreply.github.com>
2020-05-15 10:27:51 -0400

    BB:19266 Truncate note and show full note in popover (#27737)

---
+++ src/Oro/Bundle/ShoppingListBundle/Resources/public/js/app/views/preview-full-note-view.js
@@ -0,0 +1,49 @@
+import BaseView from 'oroui/js/app/views/base/view';
+
+const PreviewFullNoteView = BaseView.extend({
+    /**
+     * @inheritDoc
+     */
+    events: {
+        mouseenter: 'onMouseOver'
+    },
+
+    autoRender: true,
+
+    popoverConfig: {
+        placement: 'bottom',
+        animation: false
+    },
+
+    /**
+     * @inheritDoc
+     */
+    constructor: function PreviewFullNoteView(options) {
+        PreviewFullNoteView.__super__.constructor.call(this, options);
+    },
+
+    /**
+     * @inheritDoc
+     */
+    initialize(options) {
+        this.popoverConfig = {...this.popoverConfig, ...options.popoverConfig};
+        PreviewFullNoteView.__super__.initialize.call(this, options);
+    },
+
+    onMouseOver() {
+        const note = this.$('[data-role="note"]')[0];
+
+        if (note.offsetWidth === note.scrollWidth) {
+            return;
+        }
+
+        this.$el.popover({
+            ...this.popoverConfig,
+            content: note.innerText
+        });
+        this.$el.popover('show');
+        this.$el.one('mouseleave', () => this.$el.popover('dispose'));
+    }
+});
+
+export default PreviewFullNoteView;
