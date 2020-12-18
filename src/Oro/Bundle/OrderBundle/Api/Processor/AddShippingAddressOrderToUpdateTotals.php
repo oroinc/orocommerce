ece0d5a4929bac3cee6e3f2bf168a87ef76179da
Vova Soroka <vsoroka@oroinc.com>
2020-12-18 13:59:46 +0200

    BB-19965: Order tax is not recalculated after order shipping address was updated via API

---
+++ src/Oro/Bundle/OrderBundle/Api/Processor/AddShippingAddressOrderToUpdateTotals.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Oro\Bundle\OrderBundle\Api\Processor;
+
+use Oro\Bundle\ApiBundle\Processor\CustomizeFormData\CustomizeFormDataContext;
+use Oro\Bundle\ApiBundle\Util\DoctrineHelper;
+use Oro\Bundle\OrderBundle\Entity\Order;
+use Oro\Bundle\OrderBundle\Entity\OrderAddress;
+use Oro\Component\ChainProcessor\ContextInterface;
+use Oro\Component\ChainProcessor\ProcessorInterface;
+
+/**
+ * Adds an order associated with an order shipping address entity to the list of orders
+ * for which totals need to be updated.
+ */
+class AddShippingAddressOrderToUpdateTotals implements ProcessorInterface
+{
+    /** @var DoctrineHelper */
+    private $doctrineHelper;
+
+    /**
+     * @param DoctrineHelper $doctrineHelper
+     */
+    public function __construct(DoctrineHelper $doctrineHelper)
+    {
+        $this->doctrineHelper = $doctrineHelper;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContextInterface $context)
+    {
+        /** @var CustomizeFormDataContext $context */
+
+        if (!$context->getForm()->isValid()) {
+            return;
+        }
+
+        $order = $this->getOrder($context->getData());
+        if (null !== $order) {
+            UpdateOrderTotals::addOrderToUpdateTotals($order, $context->getSharedData());
+        }
+    }
+
+    /**
+     * @param OrderAddress $orderAddress
+     *
+     * @return Order|null
+     */
+    private function getOrder(OrderAddress $orderAddress): ?Order
+    {
+        return $this->doctrineHelper->createQueryBuilder(Order::class, 'o')
+            ->where('o.shippingAddress = :shippingAddress')
+            ->setParameter('shippingAddress', $orderAddress)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+}
