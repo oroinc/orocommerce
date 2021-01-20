2e48530cb21d65ad81094ea8b58e3488a0861920
Igor Krynychanskyi <ikrynychanskyi@oroinc.com>
2021-01-20 18:25:04 +0200

    BB-20290: Update entity config.yml to add new field config  - added new configuration value to attribute scope in entity config field  - implemented migration to make product fields usable in export

---
+++ src/Oro/Bundle/ProductBundle/Migrations/Data/ORM/EnableAttributesUsableInExport.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Oro\Bundle\ProductBundle\Migrations\Data\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+
+/**
+ * Data migration to make specified attributes available for export.
+ */
+class EnableAttributesUsableInExport extends AbstractFixture implements ContainerAwareInterface
+{
+    use MakeProductAttributesTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->updateProductAttributes(
+            [
+                'sku' => [
+                    'use_in_export' => true,
+                ],
+                'names' => [
+                    'use_in_export' => true,
+                ],
+                'inventory_status' => [
+                    'use_in_export' => true,
+                ],
+            ]
+        );
+    }
+}
