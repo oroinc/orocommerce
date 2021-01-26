2848d803c5b0f45bba97507d14fc60ca10bcd6dc
Viacheslav Chekanovskyi <vchekanovskyi@oroinc.com>
2021-01-26 17:07:21 +0000

    BB-19996: Range filter - implement frontend extend price range filter

--- src/Oro/Bundle/PricingBundle/Resources/public/js/filter/frontend-product-price-filter.js
+++ src/Oro/Bundle/PricingBundle/Resources/public/js/filter/frontend-product-price-filter.js
@@ -1,33 +1,102 @@
-define([
-    'jquery',
-    'underscore',
-    'orotranslation/js/translator',
-    'oro/filter/product-price-filter'
-], function($, _, __, ProductPriceFilter) {
-    'use strict';
+import ProductPriceFilter from 'oro/filter/product-price-filter';
+import template from 'tpl-loader!oropricing/templates/product/pricing-range-filter.html';
+import unitTemplate from 'tpl-loader!oropricing/templates/product/pricing-range-units-filter.html';
+import localeSettings from 'orolocale/js/locale-settings';
+import tools from 'oroui/js/tools';
+
+/**
+ * Frontend product price filter
+ *
+ * @export  oro/filter/product-price-filter
+ * @class   oro.filter.FrontendProductPriceFilter
+ * @extends oro.filter.ProductPriceFilter
+ */
+const FrontendProductPriceFilter = ProductPriceFilter.extend({
+    /**
+     * @inheritDoc
+     */
+    template: template,
 
     /**
-     * Frontend product price filter
-     *
-     * @export  oro/filter/product-price-filter
-     * @class   oro.filter.FrontendProductPriceFilter
-     * @extends oro.filter.ProductPriceFilter
+     * @inheritDoc
      */
-    const FrontendProductPriceFilter = ProductPriceFilter.extend({
-        /**
-         * @property {Object}
-         */
-        criteriaValueSelectors: _.defaults({
-            type: 'select[data-choice-value-select]'
-        }, ProductPriceFilter.prototype.criteriaValueSelectors),
+    unitTemplate: unitTemplate,
 
-        /**
-         * @inheritDoc
-         */
-        constructor: function FrontendProductPriceFilter(options) {
-            FrontendProductPriceFilter.__super__.constructor.call(this, options);
+    /**
+     * @inheritDoc
+     */
+    criteriaValueSelectors: {
+        ...ProductPriceFilter.prototype.criteriaValueSelectors,
+        type: '[data-choice-value-select]'
+    },
+
+    typeValues: {
+        ...ProductPriceFilter.prototype.typeValues,
+        moreThan: 2,
+        lessThan: 6,
+        equalsOrMoreThan: 1,
+        equalsOrLessThan: 5
+    },
+
+    events: {
+        'change [data-choice-value-select]': '_onChangeChoiceValue'
+    },
+
+    showChoices: true,
+
+    /**
+     * @inheritDoc
+     */
+    constructor: function FrontendProductPriceFilter(options) {
+        FrontendProductPriceFilter.__super__.constructor.call(this, options);
+    },
+
+    swapValues(data) {
+        if (!this.isApplicable(data.type)) {
+            return data;
         }
-    });
 
-    return FrontendProductPriceFilter;
+        if (data.value && data.value_end) {
+            // if both values are filled
+            // start/end values if end value is lower than start
+            if (parseFloat(data.value_end) < parseFloat(data.value)) {
+                [data.value, data.value_end] = [parseFloat(data.value_end), parseFloat(data.value)];
+            }
+        }
+
+        if (!data.value && data.value_end) {
+            [data.value, data.value_end] = [parseFloat(data.value_end), ''];
+        }
+
+        return data;
+    },
+
+    isUpdatable(newValue, oldValue) {
+        return !tools.isEqualsLoosely(newValue, oldValue);
+    },
+
+    _formatRawValue(value) {
+        const result = FrontendProductPriceFilter.__super__._formatRawValue.call(this, value);
+
+        return {
+            ...result,
+            value: result.value ? result.value.toString() : '',
+            value_end: result.value_end ? result.value_end.toString() : ''
+        };
+    },
+
+    getTemplateData() {
+        const data = FrontendProductPriceFilter.__super__.getTemplateData.call(this);
+
+        data.showChoices = this.showChoices;
+        data.currency = {
+            isPrepend: localeSettings.isCurrencySymbolPrepend(),
+            symbol: localeSettings.getCurrencySymbol(),
+            extended: localeSettings.getCurrencySymbol().length > 1
+        };
+
+        return data;
+    }
 });
+
+export default FrontendProductPriceFilter;
