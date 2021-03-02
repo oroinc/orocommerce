9e085706e75c5d354a4660792dc76bcd4d3742ca
Alex Yegerev <1755578+lexxcode@users.noreply.github.com>
2021-03-02 11:27:42 +0200

    BB-20439: Tooltips do not show up on web catalog category content variant edit page - Added popover init

--- src/Oro/Bundle/WebCatalogBundle/Resources/public/js/app/views/variant-body-view.js
+++ src/Oro/Bundle/WebCatalogBundle/Resources/public/js/app/views/variant-body-view.js
@@ -1,88 +1,86 @@
-define(function(require) {
-    'use strict';
+import BaseView from 'oroui/js/app/views/base/view';
+import LoadingMaskView from 'oroui/js/app/views/loading-mask-view';
+import layout from 'oroui/js/layout';
+import _ from 'underscore';
 
-    const BaseView = require('oroui/js/app/views/base/view');
-    const LoadingMaskView = require('oroui/js/app/views/loading-mask-view');
-    const _ = require('underscore');
+const VariantBodyView = BaseView.extend({
+    options: {},
+    selectors: {
+        container: null,
+        trigger: null,
+        body: null
+    },
+    expanded: false,
+    loadingMaskView: null,
+    $trigger: null,
+    $container: null,
 
-    const VariantBodyView = BaseView.extend({
-        options: {},
-        selectors: {
-            container: null,
-            trigger: null,
-            body: null
-        },
-        expanded: false,
-        loadingMaskView: null,
-        $trigger: null,
-        $container: null,
+    /**
+     * @inheritDoc
+     */
+    constructor: function VariantBodyView(options) {
+        VariantBodyView.__super__.constructor.call(this, options);
+    },
 
-        /**
-         * @inheritDoc
-         */
-        constructor: function VariantBodyView(options) {
-            VariantBodyView.__super__.constructor.call(this, options);
-        },
+    /**
+     * @inheritDoc
+     */
+    initialize(options) {
+        this.options = _.defaults(options || {}, this.options);
+        this.initLayout().done(_.bind(this.handleLayoutInit, this));
+    },
 
-        /**
-         * @inheritDoc
-         */
-        initialize: function(options) {
-            this.options = _.defaults(options || {}, this.options);
-            this.initLayout().done(_.bind(this.handleLayoutInit, this));
-        },
+    handleLayoutInit() {
+        this.$trigger = this.options.el.closest('.content-variant-item').find(this.options.selectors.trigger);
+        this.$container = this.options.el.closest('.content-variant-item').find(this.options.selectors.container);
+        this.loadingMaskView = new LoadingMaskView({container: this.$container});
 
-        handleLayoutInit: function() {
-            this.$trigger = this.options.el.closest('.content-variant-item').find(this.options.selectors.trigger);
-            this.$container = this.options.el.closest('.content-variant-item').find(this.options.selectors.container);
-            this.loadingMaskView = new LoadingMaskView({container: this.$container});
+        this.initializeCollapsedState();
+        this.$trigger.on('click', _.bind(this.onToggle, this));
+        layout.initPopover(this.$container);
+    },
 
-            this.initializeCollapsedState();
-            this.$trigger.on('click', _.bind(this.onToggle, this));
-        },
-
-        onToggle: function(e) {
-            if (this.$container.find(this.options.selectors.body).length !== 0) {
-                return;
-            }
-
-            if (this.$container.hasClass('show')) {
-                return;
-            }
-
-            this.loadingMaskView.show();
-            this.loadBody();
-        },
-
-        initializeCollapsedState: function() {
-            if (this.options.expanded) {
-                this.loadingMaskView.show();
-                this.$container.addClass('show');
-                this.loadBody();
-            } else {
-                this.$trigger.addClass('collapsed');
-            }
-        },
-
-        loadBody: function() {
-            const bodyPrototype = this.$el.data('body-prototype');
-            if (bodyPrototype) {
-                this.$container
-                    .append(bodyPrototype)
-                    .trigger('content:changed');
-
-                this.validate();
-            }
-            this.loadingMaskView.hide();
-        },
-
-        validate: function() {
-            const $form = this.$el.closest('form');
-            if ($form.data('validator')) {
-                $form.validate();
-            }
+    onToggle(e) {
+        if (this.$container.find(this.options.selectors.body).length !== 0) {
+            return;
         }
-    });
 
-    return VariantBodyView;
+        if (this.$container.hasClass('show')) {
+            return;
+        }
+
+        this.loadingMaskView.show();
+        this.loadBody();
+    },
+
+    initializeCollapsedState() {
+        if (this.options.expanded) {
+            this.loadingMaskView.show();
+            this.$container.addClass('show');
+            this.loadBody();
+        } else {
+            this.$trigger.addClass('collapsed');
+        }
+    },
+
+    loadBody() {
+        const bodyPrototype = this.$el.data('body-prototype');
+        if (bodyPrototype) {
+            this.$container
+                .append(bodyPrototype)
+                .trigger('content:changed');
+
+            this.validate();
+        }
+        this.loadingMaskView.hide();
+    },
+
+    validate() {
+        const $form = this.$el.closest('form');
+        if ($form.data('validator')) {
+            $form.validate();
+        }
+    }
 });
+
+export default VariantBodyView;
