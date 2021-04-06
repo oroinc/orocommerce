8ddfd4a8765d5525f9d245cd5735da75f32ddc80
Eugene Chetverikov <EugeneC@users.noreply.github.com>
2021-04-06 19:03:29 +0300

    BB-20517: Add mapper decorators (#29558)
    - decorators for OrderLineItemMapper, OrderMapper and TotalResolver that takes into account whether the "tax after promotion" option is enabled

--- src/Oro/Bundle/TaxBundle/Resolver/TotalResolver.php
+++ src/Oro/Bundle/TaxBundle/Resolver/TotalResolver.php
@@ -42,27 +42,9 @@ class TotalResolver implements ResolverInterface
             return;
         }
 
-        $taxResults = [];
-        $data = ResultElement::create(BigDecimal::zero(), BigDecimal::zero(), BigDecimal::zero(), BigDecimal::zero());
+        [$data, $taxResults] = $this->mergeItemsData($taxable);
 
-        foreach ($taxable->getItems() as $taxableItem) {
-            $taxableItemResult = $taxableItem->getResult();
-            $row = $taxableItemResult->getRow();
-
-            if ($this->settingsProvider->isStartCalculationOnItem()) {
-                $this->roundingResolver->round($row);
-            }
-
-            try {
-                $mergedData = $this->mergeData($data, $row);
-                $mergedTaxResults = $this->mergeTaxResultElements($taxResults, $taxableItemResult);
-            } catch (NumberFormatException $e) {
-                continue;
-            }
-
-            $data = $mergedData;
-            $taxResults = $mergedTaxResults;
-        }
+        $data = $this->mergeAdditionalData($taxable, $data);
 
         $data = $this->mergeShippingData($taxable, $data);
 
@@ -134,5 +116,37 @@ class TotalResolver implements ResolverInterface
         $resultElement = $taxable->getResult()->offsetGet(Result::SHIPPING);
 
         return $this->mergeData($target, $resultElement);
+    }
+
+    protected function mergeItemsData(Taxable $taxable): array
+    {
+        $taxResults = [];
+        $data = ResultElement::create(BigDecimal::zero(), BigDecimal::zero(), BigDecimal::zero(), BigDecimal::zero());
+
+        foreach ($taxable->getItems() as $taxableItem) {
+            $taxableItemResult = $taxableItem->getResult();
+            $row = $taxableItemResult->getRow();
+
+            if ($this->settingsProvider->isStartCalculationOnItem()) {
+                $this->roundingResolver->round($row);
+            }
+
+            try {
+                $mergedData = $this->mergeData($data, $row);
+                $mergedTaxResults = $this->mergeTaxResultElements($taxResults, $taxableItemResult);
+            } catch (NumberFormatException $e) {
+                continue;
+            }
+
+            $data = $mergedData;
+            $taxResults = $mergedTaxResults;
+        }
+
+        return [$data, $taxResults];
+    }
+
+    protected function mergeAdditionalData(Taxable $taxable, ResultElement $target): ResultElement
+    {
+        return $target;
     }
 }
