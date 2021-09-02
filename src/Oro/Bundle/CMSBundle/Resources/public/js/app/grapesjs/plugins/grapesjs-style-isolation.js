07c480b719ff67e6600145ff4e2ecd43703a3b57
Viacheslav Chekanovskiy <vchekanovskyi@oroinc.com>
2021-09-02 14:37:39 +0300

    BAP-20709: GrapesJS style isolation inccorect with tag selector (#30377)
    * BAP-20709: GrapesJS style isolation inccorect with tag selector

--- src/Oro/Bundle/CMSBundle/Resources/public/js/app/grapesjs/plugins/grapesjs-style-isolation.js
+++ src/Oro/Bundle/CMSBundle/Resources/public/js/app/grapesjs/plugins/grapesjs-style-isolation.js
@@ -1,70 +1,9 @@
 import GrapesJS from 'grapesjs';
-import {uniqueId, each} from 'underscore';
 import $ from 'jquery';
+import ContentIsolation, {escapeWrapper, stripRestrictedAttrs} from './components/content-isolation';
 
-import CONSTANTS from 'orocms/js/app/grapesjs/constants';
-const ISOLATION_SCOPE = `${CONSTANTS.ISOLATION_PREFIX}-`;
-
-const componentHtmlIdRegexp = new RegExp(`(<div id="${ISOLATION_SCOPE}([\\w]*))`, 'g');
-// @deprecated
-const componentCssIdRegexp = new RegExp(`(\\[id="${ISOLATION_SCOPE}([\\w]*)"\\])`, 'g');
-const cssSelectorRegexp = /(?:[\.\#])[\#\.\w\:\-\s\(\)\[\]\=\"]+\s?(?=\{)/g;
-const cssWrapperScopeRegexp = new RegExp(`^#${ISOLATION_SCOPE}[\\w]+\\{`);
-const cssChildrenScopeRegexp = new RegExp(`#${ISOLATION_SCOPE}[\\w]*\\s+`, 'g');
-
-const FORBIDDEN_ATTR = ['draggable', 'data-gjs[-\\w]+'];
-
-/**
- * Test regexp
- * Prevent shift regexp index
- * @param html
- * @returns {boolean}
- */
-const hasIsolation = html => {
-    componentHtmlIdRegexp.lastIndex = 0;
-    return componentHtmlIdRegexp.test(html);
-};
-
-export const escapeWrapper = html => {
-    if (hasIsolation(html)) {
-        html = $(html).html();
-        html = escapeWrapper(html);
-    }
-
-    return html;
-};
-
-export const escapeCss = css => css
-    .replace(cssWrapperScopeRegexp, '#wrapper{')
-    .replace(componentCssIdRegexp, '')
-    .replace(cssChildrenScopeRegexp, '');
-
-export const getWrapperAttrs = html => {
-    const attrs = {};
-    if (hasIsolation(html)) {
-        const $wrapper = $(html);
-        each($wrapper[0].attributes, attr => attrs[attr.name] = $wrapper.attr(attr.name));
-    }
-    delete attrs.id;
-    return attrs;
-};
-
-export const stripRestrictedAttrs = html => {
-    FORBIDDEN_ATTR.forEach(attr => {
-        html = html.replace(new RegExp(`([\\s])${attr}((=\"([^"]*)\")|(=\'([^']*)\'))?`, 'g'), '');
-    });
-
-    return html;
-};
-
-function randomId(length = 20) {
-    return uniqueId(
-        [...Array(length)].map(i => (~~(Math.random() * 36)).toString(36)).join('')
-    );
-}
-
-export default GrapesJS.plugins.add('grapesjs-style-isolation', (editor, options) => {
-    const scopeId = ISOLATION_SCOPE + randomId();
+export default GrapesJS.plugins.add('grapesjs-style-isolation', editor => {
+    const contentIsolation = new ContentIsolation();
 
     editor.getIsolatedHtml = content => {
         const wrapper = editor.getWrapper();
@@ -76,16 +15,7 @@ export default GrapesJS.plugins.add('grapesjs-style-isolation', (editor, options
         }
 
         if (wrapperClasses.length || wrapper.styleToString().length || html.length) {
-            const root = document.createElement('div');
-
-            root.id = scopeId;
-            root.innerHTML = html;
-
-            if (wrapperClasses.length) {
-                root.className = wrapperClasses;
-            }
-
-            html = root.outerHTML;
+            html = contentIsolation.isolateHtml(html, wrapperClasses);
         }
 
         return html;
@@ -96,12 +26,12 @@ export default GrapesJS.plugins.add('grapesjs-style-isolation', (editor, options
         let css = '';
 
         if (wrapperCss.length) {
-            css += `#${scopeId}{${wrapperCss}}`;
+            css += `#${contentIsolation.scopeId}{${wrapperCss}}`;
         }
 
         css += editor.CssComposer.getAll().reduce((acc, rule) => {
             // Do not remove space in replace phrase
-            acc += rule.toCSS().replace(cssSelectorRegexp, ` #${scopeId} $&`);
+            acc += contentIsolation.isolateCss(rule.toCSS());
             return acc;
         }, '');
 
@@ -115,10 +45,7 @@ export default GrapesJS.plugins.add('grapesjs-style-isolation', (editor, options
             return '';
         }
 
-        css = css
-            .replace(cssWrapperScopeRegexp, '#wrapper{')
-            .replace(componentCssIdRegexp, '')
-            .replace(cssChildrenScopeRegexp, '');
+        css = contentIsolation.escapeCssIsolation(css);
 
         const errors = editor.CodeValidator.validate(`<style>${css}</style>`);
 
