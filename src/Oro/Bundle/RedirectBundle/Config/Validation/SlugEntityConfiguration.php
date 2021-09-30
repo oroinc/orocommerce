e310487a3044f7c76c75bad9698bbbff689e8895
Ivan <imakarenkov@oroinc.com>
2021-09-30 23:30:33 +0300

    BAP-3938: Introduce config tree for all entity configs (#29994)
    - introduced EntityConfigInterface, FieldConfigInterface and EntityConfigInterface interfaces to validate entity config options
    - added validation of the entity config on ConfigManager::persist() call
    - added documentation on how to extend the entity config validation
    - added configuration and the documentation for all the available configuration options
    - removed unused fields configs: `is_total` and `is_total_currency`
    - removed unused entity configs: `totals_mapping`
    - created command `oro:entity:config:validation:dump-reference` to dump the entity config configuration refference
    
    Co-authored-by: Andrey Yatsenko <yatsenco@gmail.com>
    Co-authored-by: Kateryna Isakova <kisakova@oroinc.com>

---
+++ src/Oro/Bundle/RedirectBundle/Config/Validation/SlugEntityConfiguration.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Oro\Bundle\RedirectBundle\Config\Validation;
+
+use Oro\Bundle\EntityConfigBundle\Config\Validation\EntityConfigInterface;
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
+
+/**
+ * Provides validations entity config for slug scope.
+ */
+class SlugEntityConfiguration implements EntityConfigInterface
+{
+    public function getSectionName(): string
+    {
+        return 'slug';
+    }
+
+    public function configure(NodeBuilder $nodeBuilder): void
+    {
+        $nodeBuilder
+            ->scalarNode('source')
+                ->info('`string` slug source field name.')
+            ->end()
+        ;
+    }
+}
