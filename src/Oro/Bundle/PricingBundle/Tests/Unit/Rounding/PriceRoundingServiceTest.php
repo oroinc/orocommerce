e9bbb23bc5a7f004f071241899e68fe5fbb35736
Vova Soroka <vsoroka@oroinc.com>
2021-11-08 00:00:09 +0200

    Refactor some unit tests in platform package (#31089)
    - refactor unit tests in the following bundles: BatchBundle, BusinessEntitiesBundle, CronBundle, DataAuditBundle, DataGridBundle, EntityPaginationBundle, ReminderBundle, SyncBundle

--- src/Oro/Bundle/PricingBundle/Tests/Unit/Rounding/PriceRoundingServiceTest.php
+++ src/Oro/Bundle/PricingBundle/Tests/Unit/Rounding/PriceRoundingServiceTest.php
@@ -2,46 +2,38 @@
 
 namespace Oro\Bundle\PricingBundle\Tests\Unit\Rounding;
 
+use Oro\Bundle\CurrencyBundle\Rounding\AbstractRoundingService;
+use Oro\Bundle\CurrencyBundle\Rounding\RoundingServiceInterface;
 use Oro\Bundle\CurrencyBundle\Tests\Unit\Rounding\AbstractRoundingServiceTest;
 use Oro\Bundle\PricingBundle\Rounding\PriceRoundingService;
 
 class PriceRoundingServiceTest extends AbstractRoundingServiceTest
 {
-    /** {@inheritdoc} */
-    protected function getRoundingService()
+    /**
+     * {@inheritdoc}
+     */
+    protected function getRoundingService(): AbstractRoundingService
     {
         return new PriceRoundingService($this->configManager);
     }
 
-    /** {@inheritdoc} */
-    protected function prepareConfigManager($roundingType, $precision)
+    /**
+     * {@inheritdoc}
+     */
+    protected function prepareConfigManager(string $roundingType, int $precision): void
     {
         $this->configManager->expects($this->atMost(2))
             ->method('get')
             ->with($this->isType('string'))
-            ->willReturnMap(
-                [
-                    [
-                        'oro_pricing.rounding_type',
-                        false,
-                        false,
-                        null,
-                        $roundingType,
-                    ],
-                    [
-                        'oro_pricing.precision',
-                        false,
-                        false,
-                        null,
-                        $precision,
-                    ],
-                ]
-            );
+            ->willReturnMap([
+                ['oro_pricing.rounding_type', false, false, null, $roundingType],
+                ['oro_pricing.precision', false, false, null, $precision]
+            ]);
     }
 
     public function testEmptyConfigValues()
     {
-        $this->prepareConfigManager(PriceRoundingService::ROUND_HALF_UP, PriceRoundingService::FALLBACK_PRECISION);
+        $this->prepareConfigManager(RoundingServiceInterface::ROUND_HALF_UP, PriceRoundingService::FALLBACK_PRECISION);
 
         $this->assertEquals(15.1235, $this->service->round(15.123456));
 
