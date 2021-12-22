e154e7e3e1d1820fc2cb530aa87c90fdae2295c5
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2021-12-22 10:09:28 +0200

    BB-20994: Add possibility to have separate layout  if  product filters in sidebar (#31419)
    - added filters_position to layout context on product listing pages

--- src/Oro/Bundle/CatalogBundle/Controller/Frontend/ProductController.php
+++ src/Oro/Bundle/CatalogBundle/Controller/Frontend/ProductController.php
@@ -2,8 +2,10 @@
 
 namespace Oro\Bundle\CatalogBundle\Controller\Frontend;
 
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Bundle\LayoutBundle\Annotation\Layout;
 use Oro\Bundle\ProductBundle\DataGrid\DataGridThemeHelper;
+use Oro\Bundle\ProductBundle\DependencyInjection\Configuration;
 use Oro\Bundle\ProductBundle\Entity\Product;
 use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,7 +20,7 @@ class ProductController extends AbstractController
      * View List of ALL products
      *
      * @Route("/allproducts", name="oro_catalog_frontend_product_allproducts")
-     * @Layout(vars={"entity_class", "grid_config", "theme_name"})
+     * @Layout(vars={"entity_class", "grid_config", "theme_name", "filters_position"})
      * @AclAncestor("oro_product_frontend_view")
      *
      * @return array
@@ -28,10 +30,12 @@ class ProductController extends AbstractController
         return [
             'entity_class' => Product::class,
             'grid_config' => [
-                'frontend-catalog-allproducts-grid'
+                'frontend-catalog-allproducts-grid',
             ],
-            'theme_name' => $this->get(DataGridThemeHelper::class)
-                ->getTheme('frontend-catalog-allproducts-grid')
+            'theme_name' => $this->container->get(DataGridThemeHelper::class)
+                ->getTheme('frontend-catalog-allproducts-grid'),
+            'filters_position' => $this->container->get(ConfigManager::class)
+                ->get(Configuration::getConfigKeyByName('filters_position')),
         ];
     }
 
@@ -42,6 +46,7 @@ class ProductController extends AbstractController
     {
         return array_merge(parent::getSubscribedServices(), [
             DataGridThemeHelper::class,
+            ConfigManager::class,
         ]);
     }
 }
