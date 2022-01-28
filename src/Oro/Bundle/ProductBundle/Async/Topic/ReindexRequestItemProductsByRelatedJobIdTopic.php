862197f97acefe290e8a88c48a726ee0a2d8c4b0
Aleksandr Parkhomenko <alexandr-parkhomenko@users.noreply.github.com>
2022-01-28 11:20:20 +0200

    BB-20599: Improve full product collections reindex speed (5.0) (#31704)
     - added logic to buffer product indexation requests
     - added logic to send only unique products to reindex on root job finish
     - changed product collection indexation to use implemented approach, MQ messages routed to new topic

---
+++ src/Oro/Bundle/ProductBundle/Async/Topic/ReindexRequestItemProductsByRelatedJobIdTopic.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Oro\Bundle\ProductBundle\Async\Topic;
+
+use Oro\Component\MessageQueue\Topic\AbstractTopic;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Dispatches search reindexation event for all records that found by given relatedJobId.
+ */
+class ReindexRequestItemProductsByRelatedJobIdTopic extends AbstractTopic
+{
+    public const NAME = 'oro_product.reindex_request_item_products_by_related_job';
+
+    public static function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public static function getDescription(): string
+    {
+        return 'Dispatches search reindexation event for all records that found by given relatedJobId.';
+    }
+
+    public function configureMessageBody(OptionsResolver $resolver): void
+    {
+        $resolver->setRequired([
+            'relatedJobId'
+        ]);
+
+        $resolver->setAllowedTypes('relatedJobId', ['int']);
+    }
+}
