a90685c806058a47544f9b8e78aa832ee8680b73
Viacheslav Chekanovskiy <vchekanovskyi@oroinc.com>
2022-02-03 17:00:17 +0200

    BB-20213: Main grapejs view (#31717)
    - add base-type-builderSpec.js
    - add component-managerSpec.js
    - add grapesjs-content-parserSpec.js
    - add grapesjs-editor-viewSpec.js

---
+++ src/Oro/Bundle/CMSBundle/Tests/JS/base-type-builderSpec.js
@@ -0,0 +1,111 @@
+import 'jasmine-jquery';
+import grapesJS from 'grapesjs';
+import _ from 'underscore';
+import ComponentRestriction from 'orocms/js/app/grapesjs/plugins/components/component-restriction';
+import BaseTypeBuilder from 'orocms/js/app/grapesjs/type-builders/base-type-builder';
+import html from 'text-loader!./fixtures/grapesjs-editor-view-fixture.html';
+
+describe('orocms/js/app/grapesjs/type-builders/base-type-builder', () => {
+    let baseTypeBuilder;
+    let editor;
+
+    const TestTypeBuilder = BaseTypeBuilder.extend({
+        modelMixin: {
+            defaults: {
+                testValue1: 'Test Value 1',
+                testValue2: 'Test Value 2',
+                testValue3: 'Test Value 3'
+            }
+        },
+
+        button: {
+            label: 'Test',
+            category: 'Test',
+            attributes: {
+                'class': 'fa fa-code'
+            }
+        },
+
+        template: _.template(`<div>Test</div>`),
+
+        commands: {
+            'test-command1': () => {
+                return true;
+            },
+            'test-command2': () => {
+                return true;
+            }
+        },
+
+        isComponent() {
+            return {
+                type: 'base'
+            };
+        }
+    });
+
+    beforeEach(() => {
+        window.setFixtures(html);
+        editor = grapesJS.init({
+            container: document.querySelector('.page-content-editor')
+        });
+
+        editor.ComponentRestriction = new ComponentRestriction(editor, {});
+    });
+
+    afterEach(() => {
+        editor.destroy();
+    });
+
+    describe('component "BaseTypeBuilder"', () => {
+        beforeEach(() => {
+            baseTypeBuilder = new TestTypeBuilder({
+                editor,
+                componentType: 'test'
+            });
+
+            baseTypeBuilder.execute();
+        });
+
+        afterEach(() => {
+            baseTypeBuilder.dispose();
+        });
+
+        it('check to be defined', () => {
+            expect(baseTypeBuilder).toBeDefined();
+            expect(baseTypeBuilder).toBeInstanceOf(TestTypeBuilder);
+            expect(baseTypeBuilder.componentType).toEqual('test');
+        });
+
+        it('check is component type defined', () => {
+            const type = baseTypeBuilder.editor.DomComponents.getType('test');
+            expect(type).toBeDefined();
+            expect(type.id).toEqual('test');
+        });
+
+        it('check is component type button', () => {
+            const button = baseTypeBuilder.editor.BlockManager.get(baseTypeBuilder.componentType);
+            expect(button).toBeDefined();
+            expect(button.get('category').get('label')).toEqual('Test');
+        });
+
+        it('check base model extend', () => {
+            expect(baseTypeBuilder.Model.isComponent).toBeDefined();
+            expect(baseTypeBuilder.Model.isComponent()).toEqual({
+                type: 'base'
+            });
+            expect(baseTypeBuilder.Model.componentType).toEqual(baseTypeBuilder.componentType);
+
+            expect(baseTypeBuilder.Model.prototype.defaults.testValue1).toEqual('Test Value 1');
+            expect(baseTypeBuilder.Model.prototype.defaults.testValue2).toEqual('Test Value 2');
+            expect(baseTypeBuilder.Model.prototype.defaults.testValue3).toEqual('Test Value 3');
+
+            expect(baseTypeBuilder.Model.prototype.editor).toEqual(editor);
+        });
+
+        it('check editor commands defined', () => {
+            expect(baseTypeBuilder.editor.Commands.get('test-command1')).toBeDefined();
+            expect(baseTypeBuilder.editor.Commands.get('test-command2')).toBeDefined();
+        });
+    });
+});
---
+++ src/Oro/Bundle/CMSBundle/Tests/JS/grapesjs-editor-viewSpec.js
@@ -0,0 +1,59 @@
+import 'jquery.validate';
+import 'jasmine-jquery';
+import 'oroui/js/app/modules/input-widgets';
+import GrapesjsEditorView from 'orocms/js/app/grapesjs/grapesjs-editor-view';
+import html from 'text-loader!./fixtures/grapesjs-editor-view-fixture.html';
+
+describe('orocms/js/app/grapesjs/grapesjs-editor-view', () => {
+    let grapesjsEditorView;
+
+    beforeEach(done => {
+        window.setFixtures(html);
+        grapesjsEditorView = new GrapesjsEditorView({
+            el: '#grapesjs-view',
+            themes: [{
+                label: 'Test',
+                stylesheet: '',
+                active: true
+            }],
+            disableDeviceManager: true
+        });
+
+        grapesjsEditorView.builder.on('load', () => {
+            setTimeout(() => done(), 250);
+        });
+    });
+
+    afterEach(() => {
+        grapesjsEditorView.dispose();
+    });
+
+    describe('view "GrapesjsEditorView"', () => {
+        it('is view defined', () => {
+            expect(grapesjsEditorView).toBeTruthy();
+            expect(grapesjsEditorView).toBeInstanceOf(GrapesjsEditorView);
+        });
+
+        it('check default content', () => {
+            expect(grapesjsEditorView.builder.getHtml()).toEqual('Default content');
+        });
+
+        it('check container should be created', () => {
+            expect(grapesjsEditorView.$container).toBeMatchedBy('.grapesjs.gjs-editor-cont');
+        });
+
+        it('check "ComponentRestriction" should be defined', () => {
+            expect(grapesjsEditorView.builder.ComponentRestriction).toBeDefined();
+        });
+
+        it('check "RteEditor" should be defined', () => {
+            expect(grapesjsEditorView.builder.RteEditor).toBeDefined();
+            expect(grapesjsEditorView.builder.RteEditor.collection).toBeDefined();
+            expect(grapesjsEditorView.builder.RteEditor.collection.length).toBe(10);
+        });
+
+        it('check "CodeValidator" should be defined', () => {
+            expect(grapesjsEditorView.builder.CodeValidator).toBeDefined();
+        });
+    });
+});
