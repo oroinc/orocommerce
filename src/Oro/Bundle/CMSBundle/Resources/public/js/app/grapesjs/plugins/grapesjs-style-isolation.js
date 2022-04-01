4413d646f25ecd7b9f206be5551e0b38a8558224
Viacheslav Chekanovskiy <vchekanovskyi@oroinc.com>
2022-04-01 14:42:39 +0300

    Excessive scope is added to media query after subsequent import to WYSIWYG (#32287)

--- src/Oro/Bundle/CMSBundle/Resources/public/js/app/grapesjs/plugins/grapesjs-style-isolation.js
+++ src/Oro/Bundle/CMSBundle/Resources/public/js/app/grapesjs/plugins/grapesjs-style-isolation.js
@@ -51,7 +51,7 @@ export default GrapesJS.plugins.add('grapesjs-style-isolation', (editor, {editor
         return contentIsolation.escapeCssIsolation(css);
     };
 
-    editor.getPureStyle = (css = '') => {
+    editor.getPureStyleString = (css = '') => {
         if (!css.length) {
             return '';
         }
@@ -67,7 +67,15 @@ export default GrapesJS.plugins.add('grapesjs-style-isolation', (editor, {editor
             return false;
         }
 
-        const _res = editor.Parser.parseCss(css).reduce((acc, rule, index, collection) => {
+        return css;
+    };
+
+    editor.getPureStyle = (css = '') => {
+        if (!css.length) {
+            return '';
+        }
+
+        const _res = editor.Parser.parseCss(editor.getPureStyleString(css)).reduce((acc, rule, index, collection) => {
             const {state = '', atRuleType = '', mediaText = '', selectorsAdd = ''} = rule;
             const key = rule.selectors.join('') + state + atRuleType + mediaText + selectorsAdd;
 
