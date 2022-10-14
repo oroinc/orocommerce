3045fee839689ac7bfabb38e7f9048102dbc4ad4
Amaury Vallier <amaury.vallier@oroinc.com>
2022-10-14 08:55:41 +0200

    BB-21803: Fix for CollectionSortOrder demo data dependencies

--- src/Oro/Bundle/WebCatalogBundle/Migrations/Data/Demo/ORM/LoadSortOrderForProductCollectionsContentVariantsDemoData.php
+++ src/Oro/Bundle/WebCatalogBundle/Migrations/Data/Demo/ORM/LoadSortOrderForProductCollectionsContentVariantsDemoData.php
@@ -7,6 +7,7 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Oro\Bundle\ProductBundle\Entity\CollectionSortOrder;
 use Oro\Bundle\ProductBundle\Entity\Product;
+use Oro\Bundle\ProductBundle\Migrations\Data\Demo\ORM\LoadProductDemoData;
 use Oro\Bundle\SegmentBundle\Entity\Segment;
 
 class LoadSortOrderForProductCollectionsContentVariantsDemoData extends AbstractFixture implements
@@ -18,6 +19,7 @@ class LoadSortOrderForProductCollectionsContentVariantsDemoData extends Abstract
     public function getDependencies()
     {
         return [
+            LoadProductDemoData::class,
             LoadSegmentsForWebCatalogDemoData::class,
         ];
     }
@@ -30,20 +32,20 @@ class LoadSortOrderForProductCollectionsContentVariantsDemoData extends Abstract
         $sortOrders = $this->getCollectionSortOrders();
         $products = $manager
             ->getRepository(Product::class)
-            ->findBy(['id' => array_column($sortOrders, 'productId')]);
+            ->findBy(['sku' => array_column($sortOrders, 'productSku')]);
         $segments = $manager
             ->getRepository(Segment::class)
-            ->findBy(['id' => array_column($sortOrders, 'segmentId')]);
+            ->findBy(['name' => array_column($sortOrders, 'segmentName')]);
 
         foreach ($sortOrders as $sortOrder) {
             $collectionSortOrder = new CollectionSortOrder();
             foreach ($segments as $segment) {
-                if ($segment->getId() === $sortOrder['segmentId']) {
+                if ($segment->getName() === $sortOrder['segmentName']) {
                     $collectionSortOrder->setSegment($segment);
                 }
             }
             foreach ($products as $product) {
-                if ($product->getId() === $sortOrder['productId']) {
+                if ($product->getSku() === $sortOrder['productSku']) {
                     $collectionSortOrder->setProduct($product);
                 }
             }
@@ -61,18 +63,18 @@ class LoadSortOrderForProductCollectionsContentVariantsDemoData extends Abstract
     {
         return [
             [
-                'segmentId' => 4,
-                'productId' => 6,
+                'segmentName' => 'New Arrivals / Lighting Products',
+                'productSku' => '2CF67',
                 'sortOrder' => 1,
             ],
             [
-                'segmentId' => 4,
-                'productId' => 9,
+                'segmentName' => 'New Arrivals / Lighting Products',
+                'productSku' => '2JD90',
                 'sortOrder' => 0.2,
             ],
             [
-                'segmentId' => 5,
-                'productId' => 24,
+                'segmentName' => 'New Arrivals / Architectural Floodlighting',
+                'productSku' => '4HJ92',
                 'sortOrder' => 0,
             ]
         ];
