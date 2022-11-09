74669b13f31cba31cbaa30fa749f1e0ae631f71b
Yevhen Shyshkin <yshyshkin@oroinc.com>
2022-11-09 13:25:35 +0100

    BB-21929: Set sort order values for products in the sample data  - fixed demo data

--- src/Oro/Bundle/WebCatalogBundle/Migrations/Data/Demo/ORM/LoadSortOrderForProductCollectionsContentVariantsDemoData.php
+++ src/Oro/Bundle/WebCatalogBundle/Migrations/Data/Demo/ORM/LoadSortOrderForProductCollectionsContentVariantsDemoData.php
@@ -68,18 +68,73 @@ class LoadSortOrderForProductCollectionsContentVariantsDemoData extends Abstract
             [
                 'segmentName' => 'New Arrivals / Lighting Products',
                 'productSku' => '2CF67',
-                'sortOrder' => 1,
+                'sortOrder' => 6,
             ],
             [
                 'segmentName' => 'New Arrivals / Lighting Products',
                 'productSku' => '2JD90',
-                'sortOrder' => 0.2,
+                'sortOrder' => 9,
+            ],
+            [
+                'segmentName' => 'New Arrivals / Lighting Products',
+                'productSku' => '3UK92',
+                'sortOrder' => 21,
+            ],
+            [
+                'segmentName' => 'New Arrivals / Architectural Floodlighting',
+                'productSku' => '3TU20',
+                'sortOrder' => 20,
             ],
             [
                 'segmentName' => 'New Arrivals / Architectural Floodlighting',
                 'productSku' => '4HJ92',
-                'sortOrder' => 0,
-            ]
+                'sortOrder' => 24,
+            ],
+            [
+                'segmentName' => 'New Arrivals / Architectural Floodlighting',
+                'productSku' => '5TU10',
+                'sortOrder' => 34,
+            ],
+            [
+                'segmentName' => 'New Arrivals / Office Furniture',
+                'productSku' => '3ET67',
+                'sortOrder' => 15,
+            ],
+            [
+                'segmentName' => 'New Arrivals / Office Furniture',
+                'productSku' => '4KL66',
+                'sortOrder' => 25,
+            ],
+            [
+                'segmentName' => 'New Arrivals / Office Furniture',
+                'productSku' => '6GH85',
+                'sortOrder' => 38,
+            ],
+            [
+                'segmentName' => 'New Arrivals / Office Furniture',
+                'productSku' => '6PM40',
+                'sortOrder' => 40,
+            ],
+            [
+                'segmentName' => 'New Arrivals / Retail Supplies',
+                'productSku' => '1AB92',
+                'sortOrder' => 2,
+            ],
+            [
+                'segmentName' => 'New Arrivals / Retail Supplies',
+                'productSku' => '2LM04',
+                'sortOrder' => 11,
+            ],
+            [
+                'segmentName' => 'New Arrivals / Retail Supplies',
+                'productSku' => '4PJ19',
+                'sortOrder' => 26,
+            ],
+            [
+                'segmentName' => 'New Arrivals / Retail Supplies',
+                'productSku' => '7TY55',
+                'sortOrder' => 48,
+            ],
         ];
     }
 }
