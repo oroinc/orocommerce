5bc37047abd1d36b876b4a09d291a08bab5d1ec3
Vova Soroka <vsoroka@oroinc.com>
2022-12-30 02:33:20 +0200

    BB-21884: Shipping rules management for Marketplace Seller organization (#34570)
    - add strict types to Oro\Bundle\ShippingBundle\Checker\ShippingRuleEnabledCheckerInterface and Oro\Bundle\ShippingBundle\Checker\ShippingMethodEnabledByIdentifierCheckerInterface

--- src/Oro/Bundle/ShippingBundle/Checker/ShippingRuleEnabledCheckerInterface.php
+++ src/Oro/Bundle/ShippingBundle/Checker/ShippingRuleEnabledCheckerInterface.php
@@ -4,12 +4,10 @@ namespace Oro\Bundle\ShippingBundle\Checker;
 
 use Oro\Bundle\ShippingBundle\Entity\ShippingMethodsConfigsRule;
 
+/**
+ * Represents a service to check whether a shipping rule can be enabled or not.
+ */
 interface ShippingRuleEnabledCheckerInterface
 {
-    /**
-     * @param ShippingMethodsConfigsRule $rule
-     *
-     * @return bool
-     */
-    public function canBeEnabled(ShippingMethodsConfigsRule $rule);
+    public function canBeEnabled(ShippingMethodsConfigsRule $rule): bool;
 }
