6b53ab6de1e510b01da56aaa2cf1ae2e0cf2a8ef
Vitalii Yeromenko <vyeromenko@oroinc.com>
2023-01-13 11:27:41 +0200

    BB-22015 Cover Promotions functionality with behat tests (#34494)
    * BB-22015 Cover Promotions functionality with behat tests
    - added behat scenario for discounts in sub orders
    - additional behat fixtures for different promotions
    - Fixed incorrect line items discounts.

--- src/Oro/Bundle/CheckoutBundle/Tests/Unit/Provider/MultiShipping/Total/PromotionSubtotalProviderDecoratorTest.php
+++ src/Oro/Bundle/CheckoutBundle/Tests/Unit/Provider/MultiShipping/Total/PromotionSubtotalProviderDecoratorTest.php
@@ -2,8 +2,7 @@
 
 namespace Oro\Bundle\CheckoutBundle\Tests\Unit\Provider\MultiShipping\Total;
 
-use Oro\Bundle\CheckoutBundle\Entity\Checkout;
-use Oro\Bundle\CheckoutBundle\Provider\MultiShipping\SplitCheckoutProvider;
+use Oro\Bundle\CheckoutBundle\Provider\MultiShipping\SplitEntitiesProviderInterface;
 use Oro\Bundle\CheckoutBundle\Provider\MultiShipping\Total\PromotionSubtotalProviderDecorator;
 use Oro\Bundle\CurrencyBundle\Rounding\RoundingServiceInterface;
 use Oro\Bundle\OrderBundle\Entity\Order;
@@ -16,19 +15,19 @@ class PromotionSubtotalProviderDecoratorTest extends TestCase
 {
     private SubtotalProvider|MockObject $baseSubtotalProvider;
     private RoundingServiceInterface|MockObject $rounding;
-    private SplitCheckoutProvider|MockObject $splitCheckoutProvider;
+    private SplitEntitiesProviderInterface|MockObject $splitEntitiesProvider;
     private PromotionSubtotalProviderDecorator $subtotalProvider;
 
     protected function setUp(): void
     {
         $this->baseSubtotalProvider = $this->createMock(SubtotalProvider::class);
         $this->rounding = $this->createMock(RoundingServiceInterface::class);
-        $this->splitCheckoutProvider = $this->createMock(SplitCheckoutProvider::class);
+        $this->splitEntitiesProvider = $this->createMock(SplitEntitiesProviderInterface::class);
 
         $this->subtotalProvider = new PromotionSubtotalProviderDecorator(
             $this->baseSubtotalProvider,
             $this->rounding,
-            $this->splitCheckoutProvider
+            $this->splitEntitiesProvider
         );
     }
 
@@ -54,6 +53,10 @@ class PromotionSubtotalProviderDecoratorTest extends TestCase
             SubtotalProvider::SHIPPING_DISCOUNT_SUBTOTAL =>
                 $this->createSubtotal('shipping', 0.00, 'Shipping')
         ];
+
+        $this->splitEntitiesProvider->expects($this->once())
+            ->method('getSplitEntities')
+            ->willReturn([$subOrder1, $subOrder2]);
 
         $this->baseSubtotalProvider->expects($this->exactly(2))
             ->method('getSubtotal')
@@ -81,61 +84,7 @@ class PromotionSubtotalProviderDecoratorTest extends TestCase
         $this->assertFalse($shippingTotal->isVisible());
     }
 
-    public function testGetCheckoutSubtotal()
-    {
-        $checkout = new Checkout();
-        $subCheckout1 = new Checkout();
-        $subCheckout2 = new Checkout();
-
-        $subTotals1 = [
-            SubtotalProvider::ORDER_DISCOUNT_SUBTOTAL =>
-                $this->createSubtotal('discount', 10.00, 'Discount'),
-            SubtotalProvider::SHIPPING_DISCOUNT_SUBTOTAL =>
-                $this->createSubtotal('shipping', 0.00, 'Shipping')
-        ];
-
-        $subTotals2 = [
-            SubtotalProvider::ORDER_DISCOUNT_SUBTOTAL =>
-                $this->createSubtotal('discount', 5.00, 'Discount'),
-            SubtotalProvider::SHIPPING_DISCOUNT_SUBTOTAL =>
-                $this->createSubtotal('shipping', 0.00, 'Shipping')
-        ];
-
-        $this->splitCheckoutProvider->expects($this->once())
-            ->method('getSubCheckouts')
-            ->willReturn([$subCheckout1, $subCheckout2]);
-
-        $this->baseSubtotalProvider->expects($this->exactly(2))
-            ->method('getSubtotal')
-            ->willReturnMap([
-                [$subCheckout1, $subTotals1],
-                [$subCheckout2, $subTotals2]
-            ]);
-
-        $this->rounding->expects($this->exactly(2))
-            ->method('round')
-            ->willReturnOnConsecutiveCalls(15.00, 0.00);
-
-        $subtotals = $this->subtotalProvider->getSubtotal($checkout);
-
-        $this->assertCount(2, $subtotals);
-        $this->assertArrayHasKey(SubtotalProvider::ORDER_DISCOUNT_SUBTOTAL, $subtotals);
-        $this->assertArrayHasKey(SubtotalProvider::SHIPPING_DISCOUNT_SUBTOTAL, $subtotals);
-
-        $discountTotal = $subtotals[SubtotalProvider::ORDER_DISCOUNT_SUBTOTAL];
-        $this->assertEquals(15.00, $discountTotal->getAmount());
-        $this->assertTrue($discountTotal->isVisible());
-
-        $shippingTotal = $subtotals[SubtotalProvider::SHIPPING_DISCOUNT_SUBTOTAL];
-        $this->assertEquals(0.00, $shippingTotal->getAmount());
-        $this->assertFalse($shippingTotal->isVisible());
-    }
-
-    /**
-     * @param object $entity
-     * @dataProvider getTestGetSubtotalWithoutSubOrdersData
-     */
-    public function testGetSubtotalWithoutSubOrders($entity)
+    public function testGetSubtotalWithoutSubEntities()
     {
         $baseSubtotals = [
             SubtotalProvider::ORDER_DISCOUNT_SUBTOTAL =>
@@ -144,15 +93,15 @@ class PromotionSubtotalProviderDecoratorTest extends TestCase
                 $this->createSubtotal('shipping', 0.00, 'Shipping')
         ];
 
+        $this->splitEntitiesProvider->expects($this->once())
+            ->method('getSplitEntities')
+            ->willReturn([]);
+
         $this->baseSubtotalProvider->expects($this->once())
             ->method('getSubtotal')
             ->willReturn($baseSubtotals);
 
-        $this->splitCheckoutProvider->expects($this->any())
-            ->method('getSubCheckouts')
-            ->willReturn([]);
-
-        $subtotals = $this->subtotalProvider->getSubtotal($entity);
+        $subtotals = $this->subtotalProvider->getSubtotal(new Order());
         $this->assertCount(2, $subtotals);
         $this->assertArrayHasKey(SubtotalProvider::ORDER_DISCOUNT_SUBTOTAL, $subtotals);
         $this->assertArrayHasKey(SubtotalProvider::SHIPPING_DISCOUNT_SUBTOTAL, $subtotals);
@@ -166,19 +115,34 @@ class PromotionSubtotalProviderDecoratorTest extends TestCase
         $this->assertFalse($shippingTotal->isVisible());
     }
 
-    public function getTestGetSubtotalWithoutSubOrdersData()
+    public function testGetSubtotalWhenEntityIsNotSupportedBySplitFunctionality()
     {
-        return [
-            'Entity is not supported' => [
-                'entity' => new \StdClass(),
-            ],
-            'Order without subOrders' => [
-                'entity' => new Order()
-            ],
-            'Checkout without subCheckouts' => [
-                'entity' => new Checkout()
-            ]
+        $baseSubtotals = [
+            SubtotalProvider::ORDER_DISCOUNT_SUBTOTAL =>
+                $this->createSubtotal('discount', 10.00, 'Discount'),
+            SubtotalProvider::SHIPPING_DISCOUNT_SUBTOTAL =>
+                $this->createSubtotal('shipping', 0.00, 'Shipping')
         ];
+
+        $this->splitEntitiesProvider->expects($this->never())
+            ->method('getSplitEntities');
+
+        $this->baseSubtotalProvider->expects($this->once())
+            ->method('getSubtotal')
+            ->willReturn($baseSubtotals);
+
+        $subtotals = $this->subtotalProvider->getSubtotal(new \StdClass());
+        $this->assertCount(2, $subtotals);
+        $this->assertArrayHasKey(SubtotalProvider::ORDER_DISCOUNT_SUBTOTAL, $subtotals);
+        $this->assertArrayHasKey(SubtotalProvider::SHIPPING_DISCOUNT_SUBTOTAL, $subtotals);
+
+        $discountTotal = $subtotals[SubtotalProvider::ORDER_DISCOUNT_SUBTOTAL];
+        $this->assertEquals(10.00, $discountTotal->getAmount());
+        $this->assertTrue($discountTotal->isVisible());
+
+        $shippingTotal = $subtotals[SubtotalProvider::SHIPPING_DISCOUNT_SUBTOTAL];
+        $this->assertEquals(0.00, $shippingTotal->getAmount());
+        $this->assertFalse($shippingTotal->isVisible());
     }
 
     private function createSubtotal($type, $amount, $label): Subtotal
