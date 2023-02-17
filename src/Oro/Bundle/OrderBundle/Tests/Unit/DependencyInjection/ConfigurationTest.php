0fc3e3dcf3380ac8ce29cf8e5b10fc8682dbb127
Vova Soroka <vsoroka@oroinc.com>
2023-02-17 11:36:07 +0200

    BB-21884: Remove unneeded AvailableGroupingFieldsConfigType form type (#34884)
    - remove Oro\Bundle\CheckoutBundle\Form\Type\AvailableGroupingFieldsConfigType
    - remove Oro\Bundle\CheckoutBundle\Provider\MultiShipping\FieldsOptionsProvider
    - rename some multi shipping related DIC services

--- src/Oro/Bundle/OrderBundle/Tests/Unit/DependencyInjection/ConfigurationTest.php
+++ src/Oro/Bundle/OrderBundle/Tests/Unit/DependencyInjection/ConfigurationTest.php
@@ -5,77 +5,42 @@ namespace Oro\Bundle\OrderBundle\Tests\Unit\DependencyInjection;
 use Oro\Bundle\OrderBundle\DependencyInjection\Configuration;
 use Oro\Bundle\OrderBundle\Provider\OrderStatusesProviderInterface;
 use Oro\Bundle\ProductBundle\Entity\Product;
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\Processor;
 
 class ConfigurationTest extends \PHPUnit\Framework\TestCase
 {
-    public function testGetConfigTreeBuilder()
+    public function testEmptyConfiguration(): void
     {
-        $configuration = new Configuration();
-        $builder = $configuration->getConfigTreeBuilder();
-        $this->assertInstanceOf(TreeBuilder::class, $builder);
-    }
-
-    /**
-     * @dataProvider processConfigurationDataProvider
-     */
-    public function testProcessConfiguration(array $configs, array $expected)
-    {
-        $configuration = new Configuration();
-        $processor = new Processor();
-        $this->assertEquals($expected, $processor->processConfiguration($configuration, $configs));
-    }
-
-    public function processConfigurationDataProvider(): array
-    {
-        return [
-            'empty' => [
-                'configs'  => [[]],
-                'expected' => [
-                    'settings' => [
-                        'resolved' => true,
-                        'backend_product_visibility' => [
-                            'value' => [
-                                Product::INVENTORY_STATUS_IN_STOCK,
-                                Product::INVENTORY_STATUS_OUT_OF_STOCK
-                            ],
-                            'scope' => 'app'
-                        ],
-                        'frontend_product_visibility' => [
-                            'value' => [
-                                Product::INVENTORY_STATUS_IN_STOCK,
-                                Product::INVENTORY_STATUS_OUT_OF_STOCK
-                            ],
-                            'scope' => 'app'
-                        ],
-                        'order_automation_enable_cancellation' => [
-                            'value' => false,
-                            'scope' => 'app'
-                        ],
-                        'order_automation_applicable_statuses' => [
-                            'value' => [OrderStatusesProviderInterface::INTERNAL_STATUS_OPEN],
-                            'scope' => 'app'
-                        ],
-                        'order_automation_target_status' => [
-                            'value' => OrderStatusesProviderInterface::INTERNAL_STATUS_CANCELLED,
-                            'scope' => 'app'
-                        ],
-                        'order_creation_new_internal_order_status' => [
-                            'value' => OrderStatusesProviderInterface::INTERNAL_STATUS_OPEN,
-                            'scope' => 'app'
-                        ],
-                        'order_previously_purchased_period' => [
-                            'value' => 90,
-                            'scope' => 'app'
-                        ],
-                        'enable_purchase_history' => [
-                            'value' => false,
-                            'scope' => 'app'
-                        ],
-                    ]
+        $this->assertEquals(
+            [
+                'settings' => [
+                    'resolved' => true,
+                    'backend_product_visibility' => [
+                        'value' => [Product::INVENTORY_STATUS_IN_STOCK, Product::INVENTORY_STATUS_OUT_OF_STOCK],
+                        'scope' => 'app'
+                    ],
+                    'frontend_product_visibility' => [
+                        'value' => [Product::INVENTORY_STATUS_IN_STOCK, Product::INVENTORY_STATUS_OUT_OF_STOCK],
+                        'scope' => 'app'
+                    ],
+                    'order_automation_enable_cancellation' => ['value' => false, 'scope' => 'app'],
+                    'order_automation_applicable_statuses' => [
+                        'value' => [OrderStatusesProviderInterface::INTERNAL_STATUS_OPEN],
+                        'scope' => 'app'
+                    ],
+                    'order_automation_target_status' => [
+                        'value' => OrderStatusesProviderInterface::INTERNAL_STATUS_CANCELLED,
+                        'scope' => 'app'
+                    ],
+                    'order_creation_new_internal_order_status' => [
+                        'value' => OrderStatusesProviderInterface::INTERNAL_STATUS_OPEN,
+                        'scope' => 'app'
+                    ],
+                    'order_previously_purchased_period' => ['value' => 90, 'scope' => 'app'],
+                    'enable_purchase_history' => ['value' => false, 'scope' => 'app'],
                 ]
-            ]
-        ];
+            ],
+            (new Processor())->processConfiguration(new Configuration(), [])
+        );
     }
 }
