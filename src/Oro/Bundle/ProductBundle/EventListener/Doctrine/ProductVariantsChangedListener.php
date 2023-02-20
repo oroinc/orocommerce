018f7a2a9dd23f014df38ad15002e9f7c78e7a11
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2023-02-20 13:23:40 +0200

    BAP-21860: Creation of simple variant of configurable product (#34858)

--- src/Oro/Bundle/ProductBundle/EventListener/Doctrine/ProductVariantsChangedListener.php
+++ src/Oro/Bundle/ProductBundle/EventListener/Doctrine/ProductVariantsChangedListener.php
@@ -26,7 +26,7 @@ class ProductVariantsChangedListener
      */
     public function prePersist(ProductVariantLink $productVariantLink, LifecycleEventArgs $event)
     {
-        if ($productVariantLink->getProduct()) {
+        if ($this->isSupported($productVariantLink)) {
             $this->productReindexManager->reindexProduct($productVariantLink->getProduct(), null, true, ['main']);
         }
     }
@@ -36,8 +36,13 @@ class ProductVariantsChangedListener
      */
     public function preRemove(ProductVariantLink $productVariantLink)
     {
-        if ($productVariantLink->getProduct()) {
+        if ($this->isSupported($productVariantLink)) {
             $this->productReindexManager->reindexProduct($productVariantLink->getProduct(), null, true, ['main']);
         }
+    }
+
+    private function isSupported(ProductVariantLink $productVariantLink): bool
+    {
+        return (bool) $productVariantLink->getProduct()?->getId();
     }
 }
