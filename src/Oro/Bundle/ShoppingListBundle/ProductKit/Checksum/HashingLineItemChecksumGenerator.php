4d3c5e4d546aa4379a24cd68cb9e00ee1a9a280e
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2023-03-20 11:00:04 +0200

    BB-22289: [Backend] Create checksum generator for product kit item line items (#35058)
    - added line item checksum generator
    - adedd unit tests

---
+++ src/Oro/Bundle/ShoppingListBundle/ProductKit/Checksum/HashingLineItemChecksumGenerator.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oro\Bundle\ShoppingListBundle\ProductKit\Checksum;
+
+use Oro\Bundle\ShoppingListBundle\Entity\LineItem;
+
+/**
+ * Line item checksum generator that makes a hash from the checksum coming from the inner generator.
+ */
+class HashingLineItemChecksumGenerator implements LineItemChecksumGeneratorInterface
+{
+    private LineItemChecksumGeneratorInterface $innerChecksumGenerator;
+
+    public function __construct(LineItemChecksumGeneratorInterface $innerChecksumGenerator)
+    {
+        $this->innerChecksumGenerator = $innerChecksumGenerator;
+    }
+
+    public function getChecksum(LineItem $lineItem): string
+    {
+        $checksum = $this->innerChecksumGenerator->getChecksum($lineItem);
+
+        return sha1((string)$checksum);
+    }
+}
