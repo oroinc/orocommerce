dc125485652821f3e1e88ae32126168b35e5d3e9
Vova Soroka <vsoroka@oroinc.com>
2023-03-21 20:19:34 +0200

    BB-21694: Product PATCH updated API request with included visibility records leaves orphaned records in database (#35078)

--- src/Oro/Bundle/ShippingBundle/Tests/Functional/Api/RestJsonApi/ProductTest.php
+++ src/Oro/Bundle/ShippingBundle/Tests/Functional/Api/RestJsonApi/ProductTest.php
@@ -3,7 +3,6 @@
 namespace Oro\Bundle\ShippingBundle\Tests\Functional\Api\RestJsonApi;
 
 use Oro\Bundle\ApiBundle\Tests\Functional\RestJsonApiTestCase;
-use Oro\Bundle\ProductBundle\Entity\Product;
 use Oro\Bundle\ProductBundle\Tests\Functional\DataFixtures\LoadProductData;
 use Oro\Bundle\ProductBundle\Tests\Functional\DataFixtures\LoadProductUnitPrecisions;
 use Oro\Bundle\ShippingBundle\Entity\ProductShippingOptions;
@@ -11,6 +10,7 @@ use Oro\Bundle\ShippingBundle\Tests\Functional\DataFixtures\LoadProductShippingO
 
 /**
  * @dbIsolationPerTest
+ * @SuppressWarnings(PHPMD.TooManyPublicMethods)
  */
 class ProductTest extends RestJsonApiTestCase
 {
@@ -20,20 +20,39 @@ class ProductTest extends RestJsonApiTestCase
         $this->loadFixtures([LoadProductUnitPrecisions::class, LoadProductShippingOptions::class]);
     }
 
-    private function findProductShippingOptionsByProduct(Product $product): ?ProductShippingOptions
+    private function getProductId(string $reference): int
+    {
+        return $this->getReference($reference)->getId();
+    }
+
+    private function getShippingOptionId(string $reference): int
+    {
+        return $this->getReference($reference)->getId();
+    }
+
+    /**
+     * @param int $productId
+     *
+     * @return ProductShippingOptions[]
+     */
+    private function findShippingOptionsByProduct(int $productId): array
     {
         return $this->getEntityManager()
             ->getRepository(ProductShippingOptions::class)
-            ->findOneBy(['product' => $product]);
+            ->findBy(['product' => $productId]);
     }
 
-    public function testGetShouldReturnShippingOptions()
+    private function findShippingOption(int $id): ?ProductShippingOptions
     {
-        /** @var Product $product */
-        $product = $this->getReference(LoadProductData::PRODUCT_1);
+        return $this->getEntityManager()
+            ->getRepository(ProductShippingOptions::class)
+            ->find(ProductShippingOptions::class, $id);
+    }
 
+    public function testGetShouldReturnShippingOptions(): void
+    {
         $response = $this->get(
-            ['entity' => 'products', 'id' => $product->getId()]
+            ['entity' => 'products', 'id' => '<toString(@product-1->id)>']
         );
 
         $this->assertResponseContains(
@@ -61,7 +80,7 @@ class ProductTest extends RestJsonApiTestCase
         );
     }
 
-    public function testGetListShouldReturnShippingOptions()
+    public function testGetListShouldReturnShippingOptions(): void
     {
         $response = $this->cget(
             ['entity' => 'products'],
@@ -95,26 +114,21 @@ class ProductTest extends RestJsonApiTestCase
         );
     }
 
-    public function testUpdateShouldChangeProductShippingOptions()
+    public function testUpdateShouldChangeProductShippingOptions(): void
     {
-        /** @var Product $product */
-        $product = $this->getReference(LoadProductData::PRODUCT_2);
-        /** @var ProductShippingOptions $shippingOption */
-        $shippingOption = $this->getReference(LoadProductShippingOptions::PRODUCT_SHIPPING_OPTIONS_1);
+        $productId = $this->getProductId(LoadProductData::PRODUCT_2);
+        $shippingOptionId = $this->getShippingOptionId(LoadProductShippingOptions::PRODUCT_SHIPPING_OPTIONS_1);
 
         $response = $this->patch(
-            ['entity' => 'products', 'id' => (string)$product->getId()],
+            ['entity' => 'products', 'id' => (string)$productId],
             [
                 'data' => [
                     'type'          => 'products',
-                    'id'            => (string)$product->getId(),
+                    'id'            => (string)$productId,
                     'relationships' => [
                         'productShippingOptions' => [
                             'data' => [
-                                [
-                                    'type' => 'productshippingoptions',
-                                    'id'   => (string)$shippingOption->getId()
-                                ]
+                                ['type' => 'productshippingoptions', 'id' => (string)$shippingOptionId]
                             ]
                         ]
                     ]
@@ -126,14 +140,11 @@ class ProductTest extends RestJsonApiTestCase
             [
                 'data' => [
                     'type'          => 'products',
-                    'id'            => '<toString(@product-2->id)>',
+                    'id'            => (string)$productId,
                     'relationships' => [
                         'productShippingOptions' => [
                             'data' => [
-                                [
-                                    'type' => 'productshippingoptions',
-                                    'id'   => '<toString(@product_shipping_options.1->id)>'
-                                ]
+                                ['type' => 'productshippingoptions', 'id' => (string)$shippingOptionId]
                             ]
                         ]
                     ]
@@ -142,24 +153,28 @@ class ProductTest extends RestJsonApiTestCase
             $response
         );
 
-        $updatedShippingOptions = $this->findProductShippingOptionsByProduct($product);
-        self::assertEquals($shippingOption->getId(), $updatedShippingOptions->getId());
+        $updatedShippingOptions = $this->findShippingOptionsByProduct($productId);
+        self::assertCount(1, $updatedShippingOptions);
+        self::assertEquals($shippingOptionId, $updatedShippingOptions[0]->getId());
     }
 
-    public function testUpdateShouldSetProductShippingOptionsToNull()
+    public function testUpdateShouldChangeProductShippingOptionsAndDeleteUnused(): void
     {
-        /** @var Product $product */
-        $product = $this->getReference(LoadProductData::PRODUCT_2);
+        $productId = $this->getProductId(LoadProductData::PRODUCT_1);
+        $shippingOptionId = $this->getShippingOptionId(LoadProductShippingOptions::PRODUCT_SHIPPING_OPTIONS_1);
+        $unusedShippingOptionId = $this->getShippingOptionId(LoadProductShippingOptions::PRODUCT_SHIPPING_OPTIONS_2);
 
         $response = $this->patch(
-            ['entity' => 'products', 'id' => (string)$product->getId()],
+            ['entity' => 'products', 'id' => (string)$productId],
             [
                 'data' => [
                     'type'          => 'products',
-                    'id'            => (string)$product->getId(),
+                    'id'            => (string)$productId,
                     'relationships' => [
                         'productShippingOptions' => [
-                            'data' => null
+                            'data' => [
+                                ['type' => 'productshippingoptions', 'id' => (string)$shippingOptionId]
+                            ]
                         ]
                     ]
                 ]
@@ -170,7 +185,52 @@ class ProductTest extends RestJsonApiTestCase
             [
                 'data' => [
                     'type'          => 'products',
-                    'id'            => '<toString(@product-2->id)>',
+                    'id'            => (string)$productId,
+                    'relationships' => [
+                        'productShippingOptions' => [
+                            'data' => [
+                                ['type' => 'productshippingoptions', 'id' => (string)$shippingOptionId]
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            $response
+        );
+
+        $updatedShippingOptions = $this->findShippingOptionsByProduct($productId);
+        self::assertCount(1, $updatedShippingOptions);
+        self::assertEquals($shippingOptionId, $updatedShippingOptions[0]->getId());
+
+        self::assertNull($this->findShippingOption($unusedShippingOptionId));
+    }
+
+    public function testUpdateShouldSetProductShippingOptionsToEmptyAndDeleteUnused(): void
+    {
+        $productId = $this->getProductId(LoadProductData::PRODUCT_1);
+        $shippingOption1Id = $this->getShippingOptionId(LoadProductShippingOptions::PRODUCT_SHIPPING_OPTIONS_1);
+        $shippingOption2Id = $this->getShippingOptionId(LoadProductShippingOptions::PRODUCT_SHIPPING_OPTIONS_1);
+
+        $response = $this->patch(
+            ['entity' => 'products', 'id' => (string)$productId],
+            [
+                'data' => [
+                    'type'          => 'products',
+                    'id'            => (string)$productId,
+                    'relationships' => [
+                        'productShippingOptions' => [
+                            'data' => []
+                        ]
+                    ]
+                ]
+            ]
+        );
+
+        $this->assertResponseContains(
+            [
+                'data' => [
+                    'type'          => 'products',
+                    'id'            => (string)$productId,
                     'relationships' => [
                         'productShippingOptions' => [
                             'data' => []
@@ -181,27 +241,32 @@ class ProductTest extends RestJsonApiTestCase
             $response
         );
 
-        self::assertNull($this->findProductShippingOptionsByProduct($product));
+        self::assertCount(0, $this->findShippingOptionsByProduct($productId));
+
+        self::assertNull($this->findShippingOption($shippingOption1Id));
+        self::assertNull($this->findShippingOption($shippingOption2Id));
     }
 
-    public function testCreateShouldSetShippingOptionsForNewProduct()
+    public function testCreateShouldSetShippingOptionsForNewProduct(): void
     {
         $response = $this->post(
             ['entity' => 'products'],
             'product/product_create.yml'
         );
 
+        $productId = (int)$this->getResourceId($response);
+        $shippingOptions = $this->findShippingOptionsByProduct($productId);
+        self::assertCount(1, $shippingOptions);
+
         $this->assertResponseContains(
             [
                 'data' => [
                     'type'          => 'products',
+                    'id'            => (string)$productId,
                     'relationships' => [
                         'productShippingOptions' => [
                             'data' => [
-                                [
-                                    'type' => 'productshippingoptions',
-                                    'id'   => '<toString(@product_shipping_options.1->id)>'
-                                ]
+                                ['type' => 'productshippingoptions', 'id' => (string)$shippingOptions[0]->getId()]
                             ]
                         ]
                     ]
@@ -209,35 +274,31 @@ class ProductTest extends RestJsonApiTestCase
             ],
             $response
         );
-
-        $product = $this->getEntityManager()->getReference(Product::class, $this->getResourceId($response));
-        $shippingOptions = $this->findProductShippingOptionsByProduct($product);
-        self::assertSame(
-            $this->getReference(LoadProductShippingOptions::PRODUCT_SHIPPING_OPTIONS_1)->getId(),
-            $shippingOptions->getId()
-        );
     }
 
-    public function testShouldDeleteProductShippingOptionsWhenProductIsDeleted()
+    public function testDeleteShouldDeleteProductShippingOptions(): void
     {
-        /** @var Product $product */
-        $product = $this->getReference(LoadProductData::PRODUCT_1);
+        $productId = $this->getProductId(LoadProductData::PRODUCT_1);
+        $shippingOption1Id = $this->getShippingOptionId(LoadProductShippingOptions::PRODUCT_SHIPPING_OPTIONS_1);
+        $shippingOption2Id = $this->getShippingOptionId(LoadProductShippingOptions::PRODUCT_SHIPPING_OPTIONS_1);
 
         // guard - the deleting product should have shipping options
-        $this->assertInstanceOf(ProductShippingOptions::class, $this->findProductShippingOptionsByProduct($product));
+        $this->assertCount(2, $this->findShippingOptionsByProduct($productId));
 
-        $this->delete(['entity' => 'products', 'id' => $product->getId()]);
+        $this->delete(['entity' => 'products', 'id' => $productId]);
 
-        $this->assertNull($this->findProductShippingOptionsByProduct($product));
+        $this->assertCount(0, $this->findShippingOptionsByProduct($productId));
+
+        self::assertNull($this->findShippingOption($shippingOption1Id));
+        self::assertNull($this->findShippingOption($shippingOption2Id));
     }
 
-    public function testGetSubresourceForShippingOptionsShouldReturnProductShippingOptions()
+    public function testGetSubresourceForShippingOptionsShouldReturnProductShippingOptions(): void
     {
-        /** @var Product $product */
-        $product = $this->getReference(LoadProductData::PRODUCT_1);
+        $productId = $this->getProductId(LoadProductData::PRODUCT_1);
 
         $response = $this->getSubresource(
-            ['entity' => 'products', 'id' => $product->getId(), 'association' => 'productShippingOptions']
+            ['entity' => 'products', 'id' => $productId, 'association' => 'productShippingOptions']
         );
 
         $this->assertResponseContains(
@@ -248,10 +309,7 @@ class ProductTest extends RestJsonApiTestCase
                         'id'            => '<toString(@product_shipping_options.1->id)>',
                         'relationships' => [
                             'product' => [
-                                'data' => [
-                                    'type' => 'products',
-                                    'id' => (string)$product->getId()
-                                ]
+                                'data' => ['type' => 'products', 'id' => (string)$productId]
                             ]
                         ]
                     ],
@@ -260,10 +318,7 @@ class ProductTest extends RestJsonApiTestCase
                         'id'            => '<toString(@product_shipping_options.2->id)>',
                         'relationships' => [
                             'product' => [
-                                'data' => [
-                                    'type' => 'products',
-                                    'id' => (string)$product->getId()
-                                ]
+                                'data' => ['type' => 'products', 'id' => (string)$productId]
                             ]
                         ]
                     ]
@@ -273,52 +328,83 @@ class ProductTest extends RestJsonApiTestCase
         );
     }
 
-    public function testGetRelationshipForShippingOptionsShouldReturnProductShippingOptions()
+    public function testGetRelationshipForShippingOptionsShouldReturnProductShippingOptions(): void
     {
-        /** @var Product $product */
-        $product = $this->getReference(LoadProductData::PRODUCT_1);
-
         $response = $this->getRelationship(
-            ['entity' => 'products', 'id' => $product->getId(), 'association' => 'productShippingOptions']
+            ['entity' => 'products', 'id' => '<toString(@product-1->id)>', 'association' => 'productShippingOptions']
         );
 
         $this->assertResponseContains(
             [
                 'data' => [
-                    [
-                        'type' => 'productshippingoptions',
-                        'id'   => '<toString(@product_shipping_options.1->id)>'
-                    ],
-                    [
-                        'type' => 'productshippingoptions',
-                        'id'   => '<toString(@product_shipping_options.2->id)>'
-                    ]
+                    ['type' => 'productshippingoptions', 'id' => '<toString(@product_shipping_options.1->id)>'],
+                    ['type' => 'productshippingoptions', 'id' => '<toString(@product_shipping_options.2->id)>']
                 ]
             ],
             $response
         );
     }
 
-    public function testUpdateRelationshipForShippingOptionsShouldChangeProductShippingOptions()
+    public function testUpdateRelationshipForShippingOptionsShouldChangeProductShippingOptions(): void
     {
-        /** @var Product $product */
-        $product = $this->getReference(LoadProductData::PRODUCT_2);
-        /** @var ProductShippingOptions $shippingOption */
-        $shippingOption = $this->getReference(LoadProductShippingOptions::PRODUCT_SHIPPING_OPTIONS_1);
+        $productId = $this->getProductId(LoadProductData::PRODUCT_2);
+        $shippingOptionId = $this->getShippingOptionId(LoadProductShippingOptions::PRODUCT_SHIPPING_OPTIONS_1);
 
         $this->patchRelationship(
-            ['entity' => 'products', 'id' => (string)$product->getId(), 'association' => 'productShippingOptions'],
+            ['entity' => 'products', 'id' => (string)$productId, 'association' => 'productShippingOptions'],
             [
                 'data' => [
-                    [
-                        'type' => 'productshippingoptions',
-                        'id'   => '<toString(@product_shipping_options.1->id)>'
-                    ]
+                    ['type' => 'productshippingoptions', 'id' => (string)$shippingOptionId]
                 ]
             ]
         );
 
-        $updatedShippingOptions = $this->findProductShippingOptionsByProduct($product);
-        self::assertEquals($shippingOption->getId(), $updatedShippingOptions->getId());
+        $updatedShippingOptions = $this->findShippingOptionsByProduct($productId);
+        self::assertCount(1, $updatedShippingOptions);
+        self::assertEquals($shippingOptionId, $updatedShippingOptions[0]->getId());
+    }
+
+    public function testUpdateRelationshipForShippingOptionsShouldChangeProductShippingOptionsAndDeleteUnused(): void
+    {
+        $productId = $this->getProductId(LoadProductData::PRODUCT_1);
+        $shippingOptionId = $this->getShippingOptionId(LoadProductShippingOptions::PRODUCT_SHIPPING_OPTIONS_1);
+        $unusedShippingOptionId = $this->getShippingOptionId(LoadProductShippingOptions::PRODUCT_SHIPPING_OPTIONS_2);
+
+        $this->patchRelationship(
+            ['entity' => 'products', 'id' => (string)$productId, 'association' => 'productShippingOptions'],
+            [
+                'data' => [
+                    ['type' => 'productshippingoptions', 'id' => (string)$shippingOptionId]
+                ]
+            ]
+        );
+
+        $updatedShippingOptions = $this->findShippingOptionsByProduct($productId);
+        self::assertCount(1, $updatedShippingOptions);
+        self::assertEquals($shippingOptionId, $updatedShippingOptions[0]->getId());
+
+        self::assertNull($this->findShippingOption($unusedShippingOptionId));
+    }
+
+    public function testDeleteRelationshipForShippingOptionsShouldChangeProductShippingOptionsAndDeleteUnused(): void
+    {
+        $productId = $this->getProductId(LoadProductData::PRODUCT_1);
+        $shippingOptionId = $this->getShippingOptionId(LoadProductShippingOptions::PRODUCT_SHIPPING_OPTIONS_2);
+        $unusedShippingOptionId = $this->getShippingOptionId(LoadProductShippingOptions::PRODUCT_SHIPPING_OPTIONS_1);
+
+        $this->deleteRelationship(
+            ['entity' => 'products', 'id' => (string)$productId, 'association' => 'productShippingOptions'],
+            [
+                'data' => [
+                    ['type' => 'productshippingoptions', 'id' => (string)$unusedShippingOptionId]
+                ]
+            ]
+        );
+
+        $updatedShippingOptions = $this->findShippingOptionsByProduct($productId);
+        self::assertCount(1, $updatedShippingOptions);
+        self::assertEquals($shippingOptionId, $updatedShippingOptions[0]->getId());
+
+        self::assertNull($this->findShippingOption($unusedShippingOptionId));
     }
 }
