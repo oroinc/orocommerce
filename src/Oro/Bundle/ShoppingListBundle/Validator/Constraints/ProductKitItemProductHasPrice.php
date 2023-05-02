4f7814bc4a67105a91c9063f41eb89a7c2ef267a
Chetverikov Yevhen <103036822+chetverikovyevhen@users.noreply.github.com>
2023-05-02 17:41:11 +0300

    BB-22456: [Backend] Hide kit item products without prices in Configure and Add dialog (#35466)
    * BB-22456: [Backend] Hide kit item products without prices in Configure and Add dialog
    - added ProductKitItemProductHasPrice validation constraint;
    - added ProductKitItemProductHasPriceValidator;
    - added test
    - updated functional tests

---
+++ src/Oro/Bundle/ShoppingListBundle/Validator/Constraints/ProductKitItemProductHasPrice.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Oro\Bundle\ShoppingListBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Validates that {@see ProductKitItemProduct} has prices for {@see Product}.
+ */
+class ProductKitItemProductHasPrice extends Constraint
+{
+    public string $productHasNoPriceMessage
+        = 'oro.shoppinglist.product_kit_is_available_for_purchase.product.has_price.message';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
---
+++ src/Oro/Bundle/ShoppingListBundle/Validator/Constraints/ProductKitItemProductHasPriceValidator.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Oro\Bundle\ShoppingListBundle\Validator\Constraints;
+
+use Oro\Bundle\PricingBundle\Provider\FrontendProductPricesDataProvider;
+use Oro\Bundle\ProductBundle\Entity\Product;
+use Oro\Bundle\ProductBundle\Entity\ProductKitItemProduct;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
+
+/**
+ * Validates that {@see ProductKitItemProduct} has prices for {@see Product}.
+ */
+class ProductKitItemProductHasPriceValidator extends ConstraintValidator
+{
+    private FrontendProductPricesDataProvider $frontendProductPricesDataProvider;
+
+    public function __construct(FrontendProductPricesDataProvider $frontendProductPricesDataProvider)
+    {
+        $this->frontendProductPricesDataProvider = $frontendProductPricesDataProvider;
+    }
+
+    /**
+     * @param Product $value
+     * @param Constraint|ProductKitItemProductHasPrice $constraint
+     *
+     * {@inheritdoc}
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        if (!$constraint instanceof ProductKitItemProductHasPrice) {
+            throw new UnexpectedTypeException($constraint, ProductKitItemProductHasPrice::class);
+        }
+
+        if ($value === null) {
+            return;
+        }
+
+        if (!$value instanceof ProductKitItemProduct) {
+            throw new UnexpectedValueException($value, ProductKitItemProduct::class);
+        }
+
+        $product = $value->getProduct();
+
+        $prices = $this->frontendProductPricesDataProvider
+            ->getAllPricesForProducts([$product]);
+
+        $productPrice = $prices[$product->getId()][$value->getKitItem()->getProductUnit()->getCode()] ?? null;
+        if ($productPrice === null) {
+            $this->context
+                ->buildViolation($constraint->productHasNoPriceMessage)
+                ->atPath('product')
+                ->addViolation();
+        }
+    }
+}
