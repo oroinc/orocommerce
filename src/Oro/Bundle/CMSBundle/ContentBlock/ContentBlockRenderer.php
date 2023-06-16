001f3de86d4203f9fd905e37f9729fd6860e6df1
Andrii Yatsenko <ayatsenko@oroinc.com>
2023-06-16 16:37:13 +0300

    DOC-1260: Removed unsed import

--- src/Oro/Bundle/CMSBundle/ContentBlock/ContentBlockRenderer.php
+++ src/Oro/Bundle/CMSBundle/ContentBlock/ContentBlockRenderer.php
@@ -5,7 +5,6 @@ namespace Oro\Bundle\CMSBundle\ContentBlock;
 use Oro\Bundle\CMSBundle\Layout\DataProvider\ContentBlockDataProvider;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
-use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 use Twig\Environment;
 use Twig\Template;
