d2143b0a38f2cdfdc78057d26ab06ddb18f4d826
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2023-07-07 11:36:43 +0300

    BB-22151: Non-default content variant of content block can be saved (#35941)
    * BB-22151: Non-default content variant of content block can be saved without restrictions (#35692)

---
+++ src/Oro/Bundle/CMSBundle/Validator/Constraints/TextContentVariantScopeValidator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Oro\Bundle\CMSBundle\Validator\Constraints;
+
+use Oro\Bundle\ScopeBundle\Entity\Scope;
+use Oro\Bundle\ScopeBundle\Manager\ScopeManager;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * Checks if the default scopes are used.
+ */
+class TextContentVariantScopeValidator extends ConstraintValidator
+{
+    public function __construct(private ScopeManager $scopeManager)
+    {
+    }
+
+    public function validate($value, Constraint $constraint): void
+    {
+        if (!$value instanceof Scope) {
+            return;
+        }
+
+        $defaultScope = $this->scopeManager->findDefaultScope();
+        if ($defaultScope->getRowHash() !== $value->getRowHash()) {
+            return;
+        }
+
+        $this->context->addViolation($constraint->message);
+    }
+}
