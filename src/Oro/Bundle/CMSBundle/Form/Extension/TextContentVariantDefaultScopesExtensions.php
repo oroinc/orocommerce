516905c88a6ed9011fa1f9a2a6ed90646224d3fb
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2023-07-07 11:36:17 +0300

    BB-22151: Non-default content variant of content block can be saved (#35939)
    * BB-22151: Non-default content variant of content block can be saved without restrictions (#35692)

---
+++ src/Oro/Bundle/CMSBundle/Form/Extension/TextContentVariantDefaultScopesExtensions.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Oro\Bundle\CMSBundle\Form\Extension;
+
+use Oro\Bundle\CMSBundle\Form\Type\TextContentVariantType;
+use Oro\Bundle\CMSBundle\Validator\Constraints\TextContentVariantScope;
+use Oro\Bundle\FormBundle\Utils\FormUtils;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+
+/**
+ * Adds additional validation for scopes on content variants.
+ */
+class TextContentVariantDefaultScopesExtensions extends AbstractTypeExtension
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function ($event) {
+            $contentVariant = $event->getData();
+            if ($contentVariant['default'] ?? false) {
+                return;
+            }
+
+            $constraints = ['scope_constraints' => [new TextContentVariantScope()]];
+            FormUtils::mergeFieldOptionsRecursive($event->getForm(), 'scopes', $constraints);
+        });
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        return [TextContentVariantType::class];
+    }
+}
---
+++ src/Oro/Bundle/CMSBundle/Tests/Unit/Form/Extension/TextContentVariantDefaultScopesExtensionsTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Oro\Bundle\CMSBundle\Tests\Unit\Form\Extension;
+
+use Oro\Bundle\CMSBundle\Form\Extension\TextContentVariantDefaultScopesExtensions;
+use Oro\Bundle\CMSBundle\Form\Type\TextContentVariantType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class TextContentVariantDefaultScopesExtensionsTest extends \PHPUnit\Framework\TestCase
+{
+    private TextContentVariantDefaultScopesExtensions $extension;
+
+    protected function setUp(): void
+    {
+        $this->extension = new TextContentVariantDefaultScopesExtensions();
+    }
+
+    public function testGetExtendedTypes(): void
+    {
+        $this->assertEquals(
+            [TextContentVariantType::class],
+            TextContentVariantDefaultScopesExtensions::getExtendedTypes()
+        );
+    }
+
+    public function testBuildForm(): void
+    {
+        $builder = $this->createMock(FormBuilderInterface::class);
+        $builder
+            ->expects($this->once())
+            ->method('addEventListener');
+
+        $this->extension->buildForm($builder, []);
+    }
+}
