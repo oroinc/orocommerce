aab0b26332476598d8fa15374a0370e8e471c655
Nikita Makarov <nmakarov@oroinc.com>
2023-08-02 13:59:04 +0300

    BB-22265: Wrong menu item labels after assigning global web-catalog as root (#36093)
    - added isAllowedForBackoffice extra option in ContentNodeTreeBuilder and CategoryTreeBuilder
    - made use of isAllowedForBackoffice in MenuUpdateTreeHandler to hide menu items
    - added authorization checks in MenuUpdateTargetTypeExtension, MenuContentNodesProvider
    - added the check for root menu item "displayed" and "isAllowed" option in menu_widget template block
    - added behat test

--- src/Oro/Bundle/WebCatalogBundle/Tests/Unit/Stub/ContentNodeStub.php
+++ src/Oro/Bundle/WebCatalogBundle/Tests/Unit/Stub/ContentNodeStub.php
@@ -3,15 +3,20 @@
 namespace Oro\Bundle\WebCatalogBundle\Tests\Unit\Stub;
 
 use Oro\Bundle\WebCatalogBundle\Entity\ContentNode;
+use Oro\Bundle\WebCatalogBundle\Entity\WebCatalog;
 
 class ContentNodeStub extends ContentNode
 {
-    public function __construct(?int $id = null)
+    public function __construct(?int $id = null, ?WebCatalog $webCatalog = null)
     {
         parent::__construct();
 
         if ($id !== null) {
             $this->id = $id;
+        }
+
+        if ($webCatalog !== null) {
+            $this->webCatalog = $webCatalog;
         }
     }
 
