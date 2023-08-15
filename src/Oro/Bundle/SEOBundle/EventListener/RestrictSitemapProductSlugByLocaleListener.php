00454cebcf28d328d97b076f735dfc843840bfe2
Stevenson Kuo <42029775+StevensonKuo@users.noreply.github.com>
2023-08-15 20:03:13 +0800

    BB-14763   Website sitemap includes slugs for disabled...... 5.0 (#36103)

---
+++ src/Oro/Bundle/SEOBundle/EventListener/RestrictSitemapProductSlugByLocaleListener.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Oro\Bundle\SEOBundle\EventListener;
+
+use Oro\Bundle\RedirectBundle\Generator\CanonicalUrlGenerator;
+use Oro\Bundle\SEOBundle\Event\RestrictSitemapEntitiesEvent;
+use Oro\Bundle\WebsiteBundle\Entity\Website;
+use Oro\Bundle\WebsiteBundle\Provider\AbstractWebsiteLocalizationProvider;
+
+/**
+ * Restrict product urls when using directing canonical Url with enabled localization.
+ */
+class RestrictSitemapProductSlugByLocaleListener
+{
+    public function __construct(
+        private CanonicalUrlGenerator $canonicalUrlGenerator,
+        private AbstractWebsiteLocalizationProvider $websiteLocalizationProvider
+    ) {
+    }
+
+    public function restrictQueryBuilder(RestrictSitemapEntitiesEvent $event): void
+    {
+        /** @var Website $website */
+        $website = $event->getWebsite();
+        if ($this->canonicalUrlGenerator->isDirectUrlEnabled($website)) {
+            $queryBuilder = $event->getQueryBuilder();
+            $localizationIds = array_keys($this->websiteLocalizationProvider->getLocalizations($website));
+            $expr = $queryBuilder->expr();
+
+            $case = <<<CASE
+                (SELECT count(existp.id)
+                FROM OroProductBundle:Product existp
+                LEFT JOIN existp.slugs exists
+                LEFT JOIN exists.localization existl
+                WHERE existp = entityAlias AND existl IN (:ids)) < :localeCounts
+CASE;
+            $queryBuilder->leftJoin('slugs.localization', 'localization')
+                ->andWhere($expr->orX(
+                    $expr->andX($expr->isNull('localization'), $case),
+                    $expr->in('localization', ':ids')
+                ))
+                ->setParameter('ids', $localizationIds)
+                ->setParameter('localeCounts', count($localizationIds))
+            ;
+        }
+    }
+}
---
+++ src/Oro/Bundle/SEOBundle/Tests/Functional/EventListener/RestrictSitemapProductSlugByLocaleListenerTest.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace Oro\Bundle\SEOBundle\Tests\Functional\EventListener;
+
+use Doctrine\ORM\QueryBuilder;
+use Oro\Bundle\ConfigBundle\Tests\Functional\Traits\ConfigManagerAwareTestTrait;
+use Oro\Bundle\LocaleBundle\Entity\Localization;
+use Oro\Bundle\LocaleBundle\Tests\Functional\DataFixtures\LoadLocalizationData;
+use Oro\Bundle\ProductBundle\Entity\Product;
+use Oro\Bundle\ProductBundle\Tests\Functional\DataFixtures\LoadProductData;
+use Oro\Bundle\RedirectBundle\DependencyInjection\Configuration;
+use Oro\Bundle\RedirectBundle\Entity\Slug;
+use Oro\Bundle\RedirectBundle\Generator\CanonicalUrlGenerator;
+use Oro\Bundle\SEOBundle\Event\RestrictSitemapEntitiesEvent;
+use Oro\Bundle\SEOBundle\EventListener\RestrictSitemapProductSlugByLocaleListener;
+use Oro\Bundle\SEOBundle\Sitemap\Provider\UrlItemsProvider;
+use Oro\Bundle\SEOBundle\Tests\Functional\DataFixtures\RestrictSitemapCmsPageByWebCatalogListener as FixtureDir;
+use Oro\Bundle\SEOBundle\Tests\Functional\DataFixtures\RestrictSitemapCmsPageByWebCatalogListener\LoadWebsiteData;
+use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+use Oro\Bundle\WebsiteBundle\Entity\Website;
+use Oro\Bundle\WebsiteBundle\Provider\AbstractWebsiteLocalizationProvider;
+
+/**
+ * @dbIsolationPerTest
+ */
+class RestrictSitemapProductSlugByLocaleListenerTest extends WebTestCase
+{
+    use ConfigManagerAwareTestTrait;
+
+    protected function setUp(): void
+    {
+        $this->initClient([], self::generateBasicAuthHeader());
+        $this->loadFixtures([
+            LoadProductData::class,
+            LoadWebsiteData::class,
+            LoadLocalizationData::class
+        ]);
+
+        $this->updateProductSlug();
+    }
+
+    public function testRestrictQueryBuilderMatchedLocalizedSlug(): void
+    {
+        $localizationId = $this->getReference('en_CA')->getId();
+        $this->updateConfigs([$localizationId]);
+        /** @var Website $website */
+        $website = $this->getReference(FixtureDir\LoadWebsiteData::WEBSITE_DEFAULT);
+
+        $queryBuilder = $this->createQueryBuilder();
+
+        $urlGenerator = self::getCanonicalUrlGenerator();
+        $localizationProvider = self::getWebsiteLocalizationProvider();
+        $listener = new RestrictSitemapProductSlugByLocaleListener($urlGenerator, $localizationProvider);
+        $listener->restrictQueryBuilder(new RestrictSitemapEntitiesEvent($queryBuilder, time(), $website));
+
+        $results = $queryBuilder->getQuery()->getResult();
+
+        /** @var Product $product1 */
+        $product1 = $this->getReference(LoadProductData::PRODUCT_1);
+        /** @var Product $product2 */
+        $product2 = $this->getReference(LoadProductData::PRODUCT_2);
+        foreach ($results as $prodUrl) {
+            if ($prodUrl['id'] === $product1->getId()) {
+                $this->assertEquals('/product-1-slug-ca', $prodUrl['url']);
+            }
+
+            if ($prodUrl['id'] === $product2->getId()) {
+                $this->assertEquals('/product-2', $prodUrl['url']);
+            }
+        }
+    }
+
+    public function testRestrictQueryBuilderNoMatchedLocalizedSlug(): void
+    {
+        $localizationIds = [
+            $this->getReference('en_US')->getId(),
+            $this->getReference('en_CA')->getId(),
+            $this->getReference('es')->getId()
+        ];
+        $this->updateConfigs($localizationIds);
+        /** @var Website $website */
+        $website = $this->getReference(FixtureDir\LoadWebsiteData::WEBSITE_DEFAULT);
+        $queryBuilder = $this->createQueryBuilder();
+
+        $urlGenerator = self::getCanonicalUrlGenerator();
+        $localizationProvider = self::getWebsiteLocalizationProvider();
+        $listener = new RestrictSitemapProductSlugByLocaleListener($urlGenerator, $localizationProvider);
+        $listener->restrictQueryBuilder(new RestrictSitemapEntitiesEvent($queryBuilder, time(), $website));
+
+        $results = $queryBuilder->getQuery()->getResult();
+
+        /** @var Product $product1 */
+        $product1 = $this->getReference(LoadProductData::PRODUCT_1);
+        /** @var Product $product2 */
+        $product2 = $this->getReference(LoadProductData::PRODUCT_2);
+        $prodSlugs = [];
+        foreach ($results as $prodUrl) {
+            if ($prodUrl['id'] === $product1->getId()) {
+                $this->assertContains($prodUrl['url'], ['/product-1-slug-ca', '/product-1']);
+                $prodSlugs[] = $prodUrl;
+            }
+
+            if ($prodUrl['id'] === $product2->getId()) {
+                $this->assertEquals('/product-2', $prodUrl['url']);
+                $prodSlugs[] = $prodUrl;
+            }
+        }
+        $this->assertCount(3, $prodSlugs);
+        unset($prodSlugs);
+    }
+
+    private function updateProductSlug(): void
+    {
+        /** @var Product $product1 */
+        $product1 = $this->getReference(LoadProductData::PRODUCT_1);
+        $entityManager = self::getContainer()->get('doctrine')->getManagerForClass(Product::class);
+        $slugDefault = new Slug();
+        $slugDefault->setUrl('/product-1')
+            ->setRouteName('oro_product_frontend_product_view')
+            ->setRouteParameters(['id' => $product1->getId()]);
+        $slugCA = new Slug();
+        /** @var Localization $localizationCA */
+        $localizationCA = $this->getReference('en_CA');
+        $slugCA->setUrl('/product-1-slug-ca')->setLocalization($localizationCA)
+            ->setRouteName('oro_product_frontend_product_view')
+            ->setRouteParameters(['id' => $product1->getId()]);
+        $product1->addSlug($slugDefault)->addSlug($slugCA);
+
+        /** @var Product $product2 */
+        $product2 = $this->getReference(LoadProductData::PRODUCT_2);
+        $entityManager = self::getContainer()->get('doctrine')->getManagerForClass(Product::class);
+        $slug2Default = new Slug();
+        $slug2Default->setUrl('/product-2')
+            ->setRouteName('oro_product_frontend_product_view')
+            ->setRouteParameters(['id' => $product2->getId()]);
+        $product2->addSlug($slug2Default);
+
+        $entityManager->persist($product1);
+        $entityManager->persist($product2);
+        $entityManager->flush();
+    }
+
+    private function updateConfigs(array $localizationIds): void
+    {
+        $website = $this->getReference(FixtureDir\LoadWebsiteData::WEBSITE_DEFAULT);
+
+        $configManager = self::getConfigManager('global');
+        $configManager->set('oro_product.general_frontend_product_visibility', []);
+        $configManager->set('oro_redirect.canonical_url_type', Configuration::DIRECT_URL);
+        $configManager->set('oro_locale.enabled_localizations', $localizationIds, $website);
+        $configManager->flush();
+    }
+
+    private function createQueryBuilder(): QueryBuilder
+    {
+        /** @var QueryBuilder $qb */
+        $qb = self::getContainer()->get('doctrine')
+            ->getManagerForClass(Product::class)
+            ->getRepository(Product::class)
+            ->createQueryBuilder(UrlItemsProvider::ENTITY_ALIAS);
+
+        $qb->select(UrlItemsProvider::ENTITY_ALIAS . '.id');
+        $qb->leftJoin(UrlItemsProvider::ENTITY_ALIAS . '.slugs', 'slugs');
+        $qb->addSelect('slugs.url');
+
+        return $qb;
+    }
+
+    private static function getCanonicalUrlGenerator(): CanonicalUrlGenerator
+    {
+        return self::getContainer()->get('oro_redirect.generator.canonical_url');
+    }
+
+    private static function getWebsiteLocalizationProvider(): AbstractWebsiteLocalizationProvider
+    {
+        return self::getContainer()->get('oro_website.provider.website_localization');
+    }
+}
