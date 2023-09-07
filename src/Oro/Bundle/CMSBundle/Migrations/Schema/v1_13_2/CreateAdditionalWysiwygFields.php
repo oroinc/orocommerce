5f33049ac0528f0e0018d3ef0ade31df25afa09c
Eugene Zibrov <106163563+eugene-zibrov@users.noreply.github.com>
2023-09-07 11:55:11 +0300

    BB-21424: Internal Server Error on product edit after WYSIWYG attribute import (#36348)
    - add migration to avoid upgrade errors

---
+++ src/Oro/Bundle/CMSBundle/Migrations/Schema/v1_13_2/CreateAdditionalWysiwygFields.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Oro\Bundle\CMSBundle\Migrations\Schema\v1_13_2;
+
+use Oro\Bundle\CMSBundle\Migration\RepairAllWysiwygFieldsSchemaMigration;
+
+/**
+ * Adds additional WYSIWYG fields (_style and _properties) if they did not exist before.
+ */
+class CreateAdditionalWysiwygFields extends RepairAllWysiwygFieldsSchemaMigration
+{
+}
