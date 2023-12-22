6303b43c8f508694393349784aa53be95f80ee2e
Dima Sherstuyk <43877109+DimaSherstuyk@users.noreply.github.com>
2023-12-22 10:16:32 +0200

    BB-23260: Simple products are not to be removed if they are part of a product kit (#37154)
    Added postponed delete entities logic to DeleteMassActionHandler
    Updated functional test

--- src/Oro/Bundle/ProductBundle/DataGrid/Extension/MassAction/ProductDeleteMassActionHandler.php
+++ src/Oro/Bundle/ProductBundle/DataGrid/Extension/MassAction/ProductDeleteMassActionHandler.php
@@ -7,21 +7,24 @@ use Oro\Bundle\ProductBundle\Entity\Product;
 use Oro\Bundle\ProductBundle\Entity\ProductKitItem;
 
 /**
- * Delete mass action handler for Product entity. Skips the product if a product is referenced by product kit items.
+ * Delete mass action handler for Product entity. Postpones the product if a product is referenced by product kit items.
  */
 class ProductDeleteMassActionHandler extends DeleteMassActionHandler
 {
     /**
      * {@inheritdoc}
+     * @deprecated since 5.1
      *
      * @param Product $entity
      */
     protected function isDeleteAllowed($entity)
     {
-        if ($this->authorizationChecker->isGranted('DELETE', $entity)) {
-            return !$this->registry->getRepository(ProductKitItem::class)->findProductKitsSkuByProduct($entity, 1);
-        }
+        return $this->authorizationChecker->isGranted('DELETE', $entity);
+    }
 
-        return false;
+    protected function isPostponed(object $entity): bool
+    {
+        return $entity->isSimple() &&
+            $this->registry->getRepository(ProductKitItem::class)->findProductKitsSkuByProduct($entity, 1);
     }
 }
