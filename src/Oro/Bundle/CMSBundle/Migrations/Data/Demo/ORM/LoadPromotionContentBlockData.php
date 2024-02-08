bbc5b452cd753a616605d9bc5f815b17d83e8869
Nikita Makarov <nmakarov@oroinc.com>
2024-02-08 20:00:55 +0300

    BB-23459: Add demo data with Promotional Content block (#37526)

---
+++ src/Oro/Bundle/CMSBundle/Migrations/Data/Demo/ORM/LoadPromotionContentBlockData.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oro\Bundle\CMSBundle\Migrations\Data\Demo\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Oro\Bundle\CMSBundle\Entity\ContentBlock;
+use Oro\Bundle\CMSBundle\Entity\TextContentVariant;
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
+use Oro\Bundle\FrontendBundle\DependencyInjection\Configuration;
+use Oro\Bundle\LocaleBundle\Entity\LocalizedFallbackValue;
+use Oro\Bundle\OrganizationBundle\Entity\Organization;
+use Oro\Bundle\OrganizationBundle\Migrations\Data\ORM\LoadOrganizationAndBusinessUnitData;
+use Oro\Bundle\UserBundle\DataFixtures\UserUtilityTrait;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Loads promotional content block data and configures system config for organizations
+ */
+class LoadPromotionContentBlockData extends AbstractFixture implements
+    DependentFixtureInterface,
+    ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+    use UserUtilityTrait;
+
+    public function getDependencies(): array
+    {
+        return [
+            LoadOrganizationAndBusinessUnitData::class,
+        ];
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $data = Yaml::parseFile($this->getFilePathsFromLocator($this->getDataSource()));
+
+        foreach ($this->getOrganizations($manager) as $organization) {
+            $systemConfigBlock = null;
+            foreach ($data as $alias => $properties) {
+                $blockAlias = sprintf('%s-%d', $alias, $organization->getId());
+                $block = $this->findContentBlock($blockAlias, $manager);
+
+                if (!$block) {
+                    $title = new LocalizedFallbackValue();
+                    $title->setString($properties['title'] ?? 'Promotional Content');
+                    $manager->persist($title);
+
+                    $variant = new TextContentVariant();
+                    $variant->setDefault($properties['variant']['default'] ?? false);
+                    $variant->setContent($properties['variant']['content']);
+                    $manager->persist($variant);
+
+                    $block = new ContentBlock();
+                    $block->setOrganization($organization);
+                    $block->setOwner($organization->getBusinessUnits()->first());
+                    $block->setAlias($blockAlias);
+                    $block->addTitle($title);
+                    $block->addContentVariant($variant);
+                    $manager->persist($block);
+                }
+
+                if ($properties['useForSystemConfig'] ?? false) {
+                    $systemConfigBlock = $block;
+                }
+
+                if (!$this->hasReference($blockAlias)) {
+                    $this->setReference($blockAlias, $block);
+                }
+            }
+
+            if ($systemConfigBlock) {
+                $manager->flush();
+                /** @var ConfigManager $configManager */
+                $configManager = $this->container->get('oro_config.organization');
+
+                $configKey = Configuration::getConfigKeyByName(Configuration::PROMOTIONAL_CONTENT);
+                $value = $configManager->get($configKey, scopeIdentifier: $organization);
+                if (!$value) {
+                    $configManager->set($configKey, $systemConfigBlock->getId(), $organization);
+                    $configManager->flush($organization);
+                }
+            }
+        }
+
+        $manager->flush();
+    }
+
+    protected function getDataSource(): string
+    {
+        return '@OroCMSBundle/Migrations/Data/Demo/ORM/data/promotional_content.yml';
+    }
+
+    /**
+     * @return Organization[]
+     */
+    protected function getOrganizations(ObjectManager $manager): array
+    {
+        return [$manager->getRepository(Organization::class)->getFirst()];
+    }
+
+    protected function getFilePathsFromLocator(string $path): string
+    {
+        return $this->container->get('file_locator')->locate($path, first: true);
+    }
+
+    protected function findContentBlock(string $alias, ObjectManager $manager): ?ContentBlock
+    {
+        return $manager->getRepository(ContentBlock::class)->findOneBy(['alias' => $alias]);
+    }
+}
+
+
