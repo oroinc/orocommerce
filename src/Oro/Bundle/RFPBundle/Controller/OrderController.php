5bb873b9076337099d8c1c31386f4bd659e983ea
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2024-04-03 09:52:39 +0200

    BB-23760: Quote/order created from RFQ in global organization is not pre-populated with RFQ data (#37953)

--- src/Oro/Bundle/RFPBundle/Controller/OrderController.php
+++ src/Oro/Bundle/RFPBundle/Controller/OrderController.php
@@ -4,16 +4,15 @@ namespace Oro\Bundle\RFPBundle\Controller;
 
 use Oro\Bundle\ProductBundle\Storage\ProductDataStorage;
 use Oro\Bundle\RFPBundle\Entity\Request as RFPRequest;
-use Oro\Bundle\RFPBundle\Entity\RequestProduct;
 use Oro\Bundle\RFPBundle\Entity\RequestProductItem;
-use Oro\Bundle\RFPBundle\Storage\OffersDataStorage;
+use Oro\Bundle\RFPBundle\Storage\RequestToOrderDataStorage;
 use Oro\Bundle\SecurityBundle\Attribute\AclAncestor;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * Controller actions to create Order entity using RFQ entity as source.
+ * Controller actions to create Order entity using RFQ entity as a source.
  */
 class OrderController extends AbstractController
 {
@@ -21,31 +20,20 @@ class OrderController extends AbstractController
     #[AclAncestor('oro_order_create')]
     public function createAction(RFPRequest $request): Response
     {
-        $data = [ProductDataStorage::ENTITY_DATA_KEY => $this->getEntityData($request)];
-
-        $offers = [];
-        foreach ($request->getRequestProducts() as $lineItem) {
-            $data[ProductDataStorage::ENTITY_ITEMS_DATA_KEY][] = [
-                ProductDataStorage::PRODUCT_SKU_KEY => $lineItem->getProductSku(),
-                ProductDataStorage::PRODUCT_ID_KEY => $lineItem->getProduct()->getId(),
-                'comment' => $lineItem->getComment(),
-                ProductDataStorage::PRODUCT_KIT_ITEM_LINE_ITEMS_DATA_KEY => $this->getKitItemLineItemsData($lineItem),
-            ];
-
-            $itemOffers = [];
-            foreach ($lineItem->getRequestProductItems() as $productItem) {
-                $itemOffers[] = $this->getOfferData($productItem);
-            }
-            $offers[] = $itemOffers;
-        }
-
-        $this->container->get(ProductDataStorage::class)->set($data);
-        $this->container->get(OffersDataStorage::class)->set($offers);
+        /** @var RequestToOrderDataStorage $storage */
+        $storage = $this->container->get(RequestToOrderDataStorage::class);
+        $storage->saveToStorage($request);
 
         return $this->redirectToRoute('oro_order_create', [ProductDataStorage::STORAGE_KEY => true]);
     }
 
-    private function getEntityData(RFPRequest $request): array
+    /**
+     * @deprecated, use {@see RequestToOrderDataStorage} instead.
+     *
+     * @param RFPRequest $request
+     * @return array
+     */
+    protected function getEntityData(RFPRequest $request)
     {
         $data = [];
 
@@ -67,7 +55,13 @@ class OrderController extends AbstractController
         return $data;
     }
 
-    private function getOfferData(RequestProductItem $productItem): array
+    /**
+     * @deprecated, use {@see RequestToOrderDataStorage} instead.
+     *
+     * @param RequestProductItem $productItem
+     * @return array
+     */
+    protected function getOfferData(RequestProductItem $productItem): array
     {
         $data = [
             'quantity' => $productItem->getQuantity(),
@@ -83,33 +77,8 @@ class OrderController extends AbstractController
         return $data;
     }
 
-    private function getKitItemLineItemsData(RequestProduct $requestProduct): array
-    {
-        $kitItemLineItemsData = [];
-        foreach ($requestProduct->getKitItemLineItems() as $kitItemLineItem) {
-            $kitItemLineItemsData[] = [
-                ProductDataStorage::PRODUCT_KIT_ITEM_LINE_ITEM_KIT_ITEM_KEY => $kitItemLineItem->getKitItem()?->getId(),
-                ProductDataStorage::PRODUCT_KIT_ITEM_LINE_ITEM_PRODUCT_KEY => $kitItemLineItem->getProduct()?->getId(),
-                ProductDataStorage::PRODUCT_KIT_ITEM_LINE_ITEM_PRODUCT_UNIT_KEY =>
-                    $kitItemLineItem->getProductUnit()?->getCode(),
-                ProductDataStorage::PRODUCT_KIT_ITEM_LINE_ITEM_QUANTITY_KEY => $kitItemLineItem->getQuantity(),
-            ];
-        }
-
-        return $kitItemLineItemsData;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     public static function getSubscribedServices(): array
     {
-        return array_merge(
-            parent::getSubscribedServices(),
-            [
-                ProductDataStorage::class,
-                OffersDataStorage::class,
-            ]
-        );
+        return array_merge(parent::getSubscribedServices(), [RequestToOrderDataStorage::class]);
     }
 }
