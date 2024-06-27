702788f4b35e4aa01dc0fae9c44e35282a11fe90
Viacheslav Dubrovskyi <vdubrovskyi@oroinc.com>
2024-06-27 20:08:34 +0300

    Reapply "Merge branch 'master' of ssh://github.com/laboro/dev into ticket/OPI-1329_fix3"
    This reverts commit 046a92374a50f602634a344e4f8ea816aa87ac1b.

--- src/Oro/Bundle/ProductBundle/Resources/public/js/app/datagrid/header-cell/backend-action-header-cell.js
+++ src/Oro/Bundle/ProductBundle/Resources/public/js/app/datagrid/header-cell/backend-action-header-cell.js
@@ -15,6 +15,8 @@ define(function(require, exports, module) {
     const oroui = _.macros('oroui');
     const config = require('module-config').default(module.id);
 
+    const PRODUCT_ACTION_AREA_CSS_VAR = '--product-action-area-height';
+
     const shoppingListAddAction = config.shoppingListAddAction || {
         type: 'addproducts',
         data_identifier: 'product.id',
@@ -55,6 +57,10 @@ define(function(require, exports, module) {
         events: {
             'click [data-fullscreen-trigger]': 'showFullScreen',
             'click [data-undo-selection]': 'undoSelection'
+        },
+
+        listen: {
+            'layout:reposition mediator': 'handleAreaVisibility'
         },
 
         /**
@@ -246,7 +252,7 @@ define(function(require, exports, module) {
                 $dropdownToggle.prependTo(panel.$el);
 
                 $mainLuncher
-                    .addClass('btn btn-main btn--inverse add-to-shopping-list-button')
+                    .addClass('btn btn--inverse btn-main add-to-shopping-list-button')
                     .removeClass('disabled')
                     .prependTo(panel.$el);
             } else {
@@ -254,7 +260,7 @@ define(function(require, exports, module) {
                     panel.renderMainLauncher().$el
                 );
                 panel.launchers.forEach(launcher => {
-                    launcher.$el.addClass('btn btn--full btn--inverse');
+                    launcher.$el.addClass('btn btn--inverse btn--full');
                 });
 
                 panel.$el.addClass(extraClasses);
@@ -273,6 +279,8 @@ define(function(require, exports, module) {
                 $('[data-action-panel]').removeClass('hidden');
                 this.subview('actionsPanel').enable();
             }
+
+            this.handleAreaVisibility();
         },
 
         _renderAsGroup() {
@@ -282,8 +290,9 @@ define(function(require, exports, module) {
             this.getActionContainer().append(
                 panel.renderMainLauncher().$el
             );
+
             panel.launchers.forEach(launcher => {
-                launcher.$el.addClass('btn btn--inverse btn--full add-to-shopping-list-button');
+                launcher.$el.addClass('btn btn--inverse btn--full');
             });
             panel.$el.addClass(extraClasses);
             if (panel.actions.length > 1) {
@@ -295,6 +304,10 @@ define(function(require, exports, module) {
                 });
                 panel.$el.append($dropdownToggle);
                 $dropdownToggle.html(oroui.renderIcon({name: 'chevron-up'}));
+
+                panel.launchers.forEach(launcher => {
+                    launcher.$el.addClass('btn-main add-to-shopping-list-button');
+                });
             }
 
             this._replaceablePanelClasses = `${extraClasses} show`;
@@ -310,6 +323,8 @@ define(function(require, exports, module) {
                 $('[data-action-panel]').removeClass('hidden');
                 this.subview('actionsPanel').enable();
             }
+
+            this.handleAreaVisibility();
         },
 
         _renderAsFullscreen() {
@@ -348,6 +363,14 @@ define(function(require, exports, module) {
             });
             this.defineRenderingStrategy();
             fullscreen.show();
+        },
+
+        handleAreaVisibility() {
+            if (this.$el.is(':visible')) {
+                document.body.style.setProperty(PRODUCT_ACTION_AREA_CSS_VAR, `${this.$el.height()}px`);
+            } else {
+                document.body.style.removeProperty(PRODUCT_ACTION_AREA_CSS_VAR);
+            }
         },
 
         onShowFullScreen() {
