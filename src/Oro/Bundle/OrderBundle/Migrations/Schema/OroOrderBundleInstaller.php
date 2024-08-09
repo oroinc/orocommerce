f76e17ad2870ea19c2a69d27deac10ecfc5bd53d
vsheverov <120404762+vsheverov@users.noreply.github.com>
2024-08-09 15:52:30 +0200

    BB-24500: AddCreatedByField migration in Order bundle is not executed due to migration number conflict in 6.0 (#39073)
    * BB-24500: AddCreatedByField migration in Order bundle is not executed due to migration number conflict in 6.0
    - updated version to v1_20
    
    * BB-24500: AddCreatedByField migration in Order bundle is not executed due to migration number conflict in 6.0
    - updated version to v1_20

--- src/Oro/Bundle/OrderBundle/Migrations/Schema/OroOrderBundleInstaller.php
+++ src/Oro/Bundle/OrderBundle/Migrations/Schema/OroOrderBundleInstaller.php
@@ -40,7 +40,7 @@ class OroOrderBundleInstaller implements
      */
     public function getMigrationVersion(): string
     {
-        return 'v1_19';
+        return 'v1_20';
     }
 
     /**
---
+++ src/Oro/Bundle/OrderBundle/Migrations/Schema/v1_20/AddCreatedByField.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Oro\Bundle\OrderBundle\Migrations\Schema\v1_20;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class AddCreatedByField implements Migration
+{
+    public function up(Schema $schema, QueryBag $queries): void
+    {
+        $table = $schema->getTable('oro_order');
+        if (!$table->hasColumn('created_by_user_id')) {
+            $table->addColumn('created_by_user_id', 'integer', ['notnull' => false]);
+
+            $table->addForeignKeyConstraint(
+                $schema->getTable('oro_user'),
+                ['created_by_user_id'],
+                ['id'],
+                ['onUpdate' => null, 'onDelete' => 'SET NULL']
+            );
+        }
+    }
+}
