bda0601ac89698984e827662c6f98714f2ab6526
Vova Soroka <vsoroka@oroinc.com>
2024-11-07 19:34:56 +0200

    BB-24800: Refactor order and quote address managers (#39724)

--- src/Oro/Bundle/SaleBundle/Model/QuoteAddressManager.php
+++ src/Oro/Bundle/SaleBundle/Model/QuoteAddressManager.php
@@ -2,67 +2,30 @@
 
 namespace Oro\Bundle\SaleBundle\Model;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Util\ClassUtils;
-use Doctrine\Persistence\ManagerRegistry;
 use Oro\Bundle\AddressBundle\Entity\AbstractAddress;
 use Oro\Bundle\CustomerBundle\Entity\CustomerAddress;
 use Oro\Bundle\CustomerBundle\Entity\CustomerUserAddress;
 use Oro\Bundle\OrderBundle\Manager\AbstractAddressManager;
 use Oro\Bundle\SaleBundle\Entity\QuoteAddress;
-use Oro\Bundle\SaleBundle\Provider\QuoteAddressProvider;
 
+/**
+ * Contains get and update methods for quote addresses.
+ */
 class QuoteAddressManager extends AbstractAddressManager
 {
-    /** @var string */
-    protected $quoteAddressClass;
-
-    /**
-     * @param QuoteAddressProvider $quoteAddressProvider
-     * @param ManagerRegistry $registry
-     * @param string $quoteAddressClass
-     */
-    public function __construct(
-        QuoteAddressProvider $quoteAddressProvider,
-        ManagerRegistry $registry,
-        $quoteAddressClass
-    ) {
-        $this->quoteAddressClass = $quoteAddressClass;
-
-        $this->map = new ArrayCollection();
-
-        parent::__construct($quoteAddressProvider, $registry);
-    }
-
-    /**
-     * @param AbstractAddress|null $address
-     * @param QuoteAddress|null $quoteAddress
-     *
-     * @return QuoteAddress
-     */
-    public function updateFromAbstract(AbstractAddress $address = null, QuoteAddress $quoteAddress = null)
-    {
+    public function updateFromAbstract(
+        AbstractAddress $address = null,
+        QuoteAddress $quoteAddress = null
+    ): QuoteAddress {
         if (!$quoteAddress) {
-            $quoteAddress = new $this->quoteAddressClass();
+            $quoteAddress = new QuoteAddress();
         }
 
-        if ($address) {
-            $addressClassName = ClassUtils::getClass($address);
-            $addressMetadata = $this->registry->getManagerForClass($addressClassName)
-                ->getClassMetadata($addressClassName);
-
-            foreach ($addressMetadata->getFieldNames() as $fieldName) {
-                $this->setValue($address, $quoteAddress, $fieldName);
-            }
-
-            foreach ($addressMetadata->getAssociationNames() as $associationName) {
-                $this->setValue($address, $quoteAddress, $associationName);
-            }
+        if (null !== $address) {
+            $this->copyAddress($address, $quoteAddress);
         }
-
         $quoteAddress->setCustomerAddress(null);
         $quoteAddress->setCustomerUserAddress(null);
-
         if ($address instanceof CustomerAddress) {
             $quoteAddress->setCustomerAddress($address);
         } elseif ($address instanceof CustomerUserAddress) {
