d8a69a270a85370e8eef473cede5d8025f7d174e
Vova Soroka <vsoroka@oroinc.com>
2025-03-27 19:46:49 +0200

    BAP-20199: Provide a way to get "guest" access token using OAuth Authorization Code grant type (#40883)

--- src/Oro/Bundle/ShoppingListBundle/EventListener/InteractiveLoginListener.php
+++ src/Oro/Bundle/ShoppingListBundle/EventListener/InteractiveLoginListener.php
@@ -8,7 +8,6 @@ use Oro\Bundle\CustomerBundle\Entity\CustomerVisitor;
 use Oro\Bundle\CustomerBundle\Entity\CustomerVisitorManager;
 use Oro\Bundle\CustomerBundle\Security\AnonymousCustomerUserAuthenticator;
 use Oro\Bundle\DataAuditBundle\EventListener\SendChangedEntitiesToMessageQueueListener;
-use Oro\Bundle\ShoppingListBundle\Entity\ShoppingList;
 use Oro\Bundle\ShoppingListBundle\Manager\GuestShoppingListMigrationManager;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,7 +15,7 @@ use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
- * Listener to migrate guest shopping list during interactive login.
+ * Migrates guest-created shopping list to customer user during interactive login.
  */
 class InteractiveLoginListener
 {
@@ -30,61 +29,80 @@ class InteractiveLoginListener
     ) {
     }
 
+    /**
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
     public function onInteractiveLogin(InteractiveLoginEvent $event): void
     {
+        $user = $event->getAuthenticationToken()->getUser();
+        if (!$user instanceof CustomerUser) {
+            return;
+        }
+
+        if (!$this->configManager->get('oro_shopping_list.availability_for_guests')) {
+            return;
+        }
+
+        $isFlashMessageAllowed = false;
+        $visitor = $this->getVisitorFromCookie($event->getRequest());
+        if ($visitor) {
+            $isFlashMessageAllowed = true;
+        } else {
+            $visitor = $this->getVisitorFromAttributes($event->getRequest());
+        }
+        if (!$visitor) {
+            return;
+        }
+
+        $visitorShoppingList = $visitor->getShoppingLists()->first();
+        if (!$visitorShoppingList) {
+            return;
+        }
+
+        // Disable data audit 'cause it could fail the consumer
+        $this->sendChangedEntitiesToMessageQueueListener->setEnabled(false);
         try {
-            if (!$this->isApplicable($event)) {
-                return;
-            }
-
-            $visitor = $this->getVisitorFromRequest($event->getRequest());
-            if (!$visitor) {
-                return;
-            }
-
-            // Disable data audit 'cause it could fail the consumer
-            $this->sendChangedEntitiesToMessageQueueListener->setEnabled(false);
-            $user = $event->getAuthenticationToken()->getUser();
-
-            /** @var ShoppingList $visitorShoppingList */
-            $visitorShoppingList = $visitor->getShoppingLists()->first();
-            if (!$visitorShoppingList) {
-                return;
-            }
-
-            $operationCode = $this->guestShoppingListMigrationManager
-                ->migrateGuestShoppingList($visitor, $user, $visitorShoppingList);
-
-            if ($operationCode === GuestShoppingListMigrationManager::OPERATION_MERGE) {
-                $this->addFlashMessage($event, 'notice', 'oro.shoppinglist.flash.merge');
-            }
+            $operationCode = $this->guestShoppingListMigrationManager->migrateGuestShoppingList(
+                $visitor,
+                $user,
+                $visitorShoppingList
+            );
         } catch (\Exception $e) {
             $this->logger->error('Migration of the guest shopping list failed.', ['exception' => $e]);
+
+            return;
         } finally {
             $this->sendChangedEntitiesToMessageQueueListener->setEnabled();
         }
+
+        if ($isFlashMessageAllowed && GuestShoppingListMigrationManager::OPERATION_MERGE === $operationCode) {
+            $this->addFlashMessage($event, 'notice', 'oro.shoppinglist.flash.merge');
+        }
     }
 
-    private function isApplicable(InteractiveLoginEvent $event): bool
-    {
-        return $this->configManager->get('oro_shopping_list.availability_for_guests')
-            && $event->getAuthenticationToken()->getUser() instanceof CustomerUser
-            && $event->getRequest()->cookies->has(AnonymousCustomerUserAuthenticator::COOKIE_NAME);
-    }
-
-    private function getVisitorFromRequest(Request $request): ?CustomerVisitor
+    private function getVisitorFromCookie(Request $request): ?CustomerVisitor
     {
         $value = $request->cookies->get(AnonymousCustomerUserAuthenticator::COOKIE_NAME);
         if (!$value) {
             return null;
         }
 
-        $sessionId = json_decode(base64_decode($value));
+        $sessionId = json_decode(base64_decode($value), null, 512, JSON_THROW_ON_ERROR);
         if (\is_array($sessionId) && isset($sessionId[1])) {
             // BC compatibility (can be removed in v7.0): get sessionId from old format of the cookie value
             $sessionId = $sessionId[1];
         }
         if (!\is_string($sessionId) || !$sessionId) {
+            return null;
+        }
+
+        return $this->visitorManager->find($sessionId);
+    }
+
+    private function getVisitorFromAttributes(Request $request): ?CustomerVisitor
+    {
+        $sessionId = $request->attributes->get('visitor_session_id');
+        if (!$sessionId) {
             return null;
         }
 
