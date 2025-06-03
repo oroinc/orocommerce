369f3f093a140f2741b508b9c4812a81e7c7ce8e
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2025-06-03 11:26:18 +0200

    BB-25629: Product visibility relies on Non-Authenticated Visitors customer group that only exists in one organization (#41411)

--- src/Oro/Bundle/SEOBundle/EventListener/RestrictSitemapCategoryListener.php
+++ src/Oro/Bundle/SEOBundle/EventListener/RestrictSitemapCategoryListener.php
@@ -32,6 +32,6 @@ class RestrictSitemapCategoryListener
         $qb->andWhere($qb->expr()->eq(sprintf('%s.organization', UrlItemsProvider::ENTITY_ALIAS), ':organization'));
         $qb->setParameter('organization', $website->getOrganization());
 
-        $this->categoryVisibilityQueryBuilderModifier->restrictForAnonymous($qb);
+        $this->categoryVisibilityQueryBuilderModifier->restrictForAnonymous($qb, $website->getOrganization());
     }
 }
