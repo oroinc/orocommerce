844953e0782686ef763c0c5bd671f2bd9e3236b7
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2025-06-25 15:55:16 +0200

    BB-25763: "Non-Authenticated Visitors" customer user role can be configured to allow guest users access to quotes and RFQs of others (#41694)

---
+++ src/Oro/Bundle/RFPBundle/Tests/Unit/Datagrid/Extension/FrontendRequestsGridExtensionTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Oro\Bundle\RFPBundle\Tests\Unit\Datagrid\Extension;
+
+use Oro\Bundle\CustomerBundle\Security\Token\AnonymousCustomerUserToken;
+use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
+use Oro\Bundle\DataGridBundle\Datagrid\ParameterBag;
+use Oro\Bundle\FrontendBundle\Request\FrontendHelper;
+use Oro\Bundle\RFPBundle\Datagrid\Extension\FrontendRequestsGridExtension;
+use Oro\Bundle\SecurityBundle\Authentication\TokenAccessorInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class FrontendRequestsGridExtensionTest extends TestCase
+{
+    private FrontendRequestsGridExtension $extension;
+    private TokenAccessorInterface&MockObject $tokenAccessor;
+    private FrontendHelper&MockObject $frontendHelper;
+
+
+    #[\Override]
+    protected function setUp(): void
+    {
+        $this->tokenAccessor = self::createMock(TokenAccessorInterface::class);
+        $this->frontendHelper = self::createMock(FrontendHelper::class);
+
+        $this->extension = new FrontendRequestsGridExtension(
+            $this->tokenAccessor,
+            $this->frontendHelper
+        );
+        $this->extension->setParameters(new ParameterBag([]));
+    }
+
+
+    /**
+     * @dataProvider isApplicableDataProvider
+     */
+    public function testIsApplicable(
+        array $gridConfig,
+        bool $frontendRequest,
+        TokenInterface $token,
+        bool $expected
+    ) {
+        $configuration = DatagridConfiguration::create($gridConfig);
+
+        $this->tokenAccessor->expects(self::any())
+            ->method('getToken')
+            ->willReturn($token);
+
+        $this->frontendHelper->expects(self::any())
+            ->method('isFrontendRequest')
+            ->willReturn($frontendRequest);
+
+        self::assertEquals($expected, $this->extension->isApplicable($configuration));
+    }
+
+    public function isApplicableDataProvider(): array
+    {
+        return [
+            'Applicable' => [
+                'gridConfig' => ['name' => 'frontend-requests-grid'],
+                'frontendRequest' => true,
+                'token' => self::createMock(AnonymousCustomerUserToken::class),
+                'expected' => true,
+            ],
+            'No Frontend Requests' => [
+                'gridConfig' => ['name' => 'frontend-requests-grid'],
+                'frontendRequest' => false,
+                'token' => self::createMock(AnonymousCustomerUserToken::class),
+                'expected' => false,
+            ],
+            'Invalid token' => [
+                'gridConfig' => ['name' => 'frontend-requests-grid'],
+                'frontendRequest' => false,
+                'token' => self::createMock(TokenInterface::class),
+                'expected' => false,
+            ],
+        ];
+    }
+}
