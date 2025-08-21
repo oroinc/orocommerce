c1c37ff8b53aac9f486edeb60eea99b90c514566
Vova Soroka <vsoroka@oroinc.com>
2025-08-21 18:13:42 +0300

    BB-25507: Migration from 5.0.9 to 6.0.3 (and newer) recalculates existing order totals (#42339)

---
+++ src/Oro/Bundle/OrderBundle/Migrations/Schema/v1_27/PrecalculateOrderTotal.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Oro\Bundle\OrderBundle\Migrations\Schema\v1_27;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+class PrecalculateOrderTotal implements Migration, ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    #[\Override]
+    public function up(Schema $schema, QueryBag $queries): void
+    {
+        $queries->addQuery(new PrecalculateOrderTotalMigrationQuery(
+            $this->container->get('oro_message_queue.message_producer')
+        ));
+    }
+}
