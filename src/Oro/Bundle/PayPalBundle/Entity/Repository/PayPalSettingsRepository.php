1a26c32cec9ab52679d85c83155534c84cc0b84c
Yevhenii Zhadan <146938608+yevhenii101991@users.noreply.github.com>
2025-10-28 12:31:10 +0200

    BB-17384: Payment integrations from the first non global organization… (#42959)

--- src/Oro/Bundle/PayPalBundle/Entity/Repository/PayPalSettingsRepository.php
+++ src/Oro/Bundle/PayPalBundle/Entity/Repository/PayPalSettingsRepository.php
@@ -2,23 +2,35 @@
 
 namespace Oro\Bundle\PayPalBundle\Entity\Repository;
 
-use Doctrine\ORM\EntityRepository;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Oro\Bundle\PayPalBundle\Entity\PayPalSettings;
+use Oro\Bundle\SecurityBundle\ORM\Walker\AclHelper;
 
-class PayPalSettingsRepository extends EntityRepository
+/**
+ * Repository for PayPalSettings entity
+ */
+class PayPalSettingsRepository extends ServiceEntityRepository
 {
+    private ?AclHelper $aclHelper = null;
+
+    public function setAclHelper(AclHelper $aclHelper): self
+    {
+        $this->aclHelper = $aclHelper;
+
+        return $this;
+    }
     /**
      * @param string $type
      * @return PayPalSettings[]
      */
     public function getEnabledSettingsByType($type)
     {
-        return $this->createQueryBuilder('settings')
+        $qb = $this->createQueryBuilder('settings')
             ->innerJoin('settings.channel', 'channel')
             ->andWhere('channel.enabled = true')
             ->andWhere('channel.type = :type')
-            ->setParameter('type', $type)
-            ->getQuery()
-            ->getResult();
+            ->setParameter('type', $type);
+
+        return $this->aclHelper?->apply($qb)->getResult();
     }
 }
--- src/Oro/Bundle/PayPalBundle/Tests/Functional/DataFixtures/LoadPayPalChannelData.php
+++ src/Oro/Bundle/PayPalBundle/Tests/Functional/DataFixtures/LoadPayPalChannelData.php
@@ -8,35 +8,54 @@ use Doctrine\Persistence\ObjectManager;
 use Oro\Bundle\IntegrationBundle\Entity\Channel;
 use Oro\Bundle\PayPalBundle\Entity\PayPalSettings;
 use Oro\Bundle\TestFrameworkBundle\Tests\Functional\DataFixtures\LoadOrganization;
+use Oro\Bundle\TestFrameworkBundle\Tests\Functional\DataFixtures\LoadSecondOrganizationWithBusinessUnit;
 use Oro\Bundle\TestFrameworkBundle\Tests\Functional\DataFixtures\LoadUser;
 
 class LoadPayPalChannelData extends AbstractFixture implements DependentFixtureInterface
 {
     public const PAYPAL_PAYFLOW_GATAWAY1 = 'paypal:channel_1';
     public const PAYPAL_PAYFLOW_GATAWAY2 = 'paypal:channel_2';
+    public const PAYPAL_PAYFLOW_GATAWAY3 = 'paypal:channel_5';
     public const PAYPAL_PAYMENTS_PRO1 = 'paypal:channel_3';
     public const PAYPAL_PAYMENTS_PRO2 = 'paypal:channel_4';
+    public const PAYPAL_PAYMENTS_PRO3 = 'paypal:channel_6';
 
     private array $channelData = [
         self::PAYPAL_PAYFLOW_GATAWAY1 => [
             'name' => 'PayPal1',
             'type' => 'paypal_payflow_gateway',
             'enabled' => true,
+            'organization' => LoadOrganization::ORGANIZATION,
         ],
         self::PAYPAL_PAYFLOW_GATAWAY2 => [
             'name' => 'PayPal2',
             'type' => 'paypal_payflow_gateway',
             'enabled' => false,
+            'organization' => LoadOrganization::ORGANIZATION,
+        ],
+        self::PAYPAL_PAYFLOW_GATAWAY3 => [
+            'name' => 'PayPal5',
+            'type' => 'paypal_payflow_gateway',
+            'enabled' => true,
+            'organization' => LoadSecondOrganizationWithBusinessUnit::SECOND_ORGANIZATION,
         ],
         self::PAYPAL_PAYMENTS_PRO1 => [
             'name' => 'PayPal3',
             'type' => 'paypal_payments_pro',
             'enabled' => true,
+            'organization' => LoadOrganization::ORGANIZATION,
         ],
         self::PAYPAL_PAYMENTS_PRO2 => [
             'name' => 'PayPal4',
             'type' => 'paypal_payments_pro',
             'enabled' => true,
+            'organization' => LoadOrganization::ORGANIZATION,
+        ],
+        self::PAYPAL_PAYMENTS_PRO3 => [
+            'name' => 'PayPal6',
+            'type' => 'paypal_payments_pro',
+            'enabled' => true,
+            'organization' => LoadSecondOrganizationWithBusinessUnit::SECOND_ORGANIZATION,
         ],
     ];
 
@@ -45,7 +64,7 @@ class LoadPayPalChannelData extends AbstractFixture implements DependentFixtureI
      */
     public function getDependencies(): array
     {
-        return [LoadOrganization::class, LoadUser::class];
+        return [LoadOrganization::class, LoadUser::class, LoadSecondOrganizationWithBusinessUnit::class];
     }
 
     /**
@@ -59,7 +78,7 @@ class LoadPayPalChannelData extends AbstractFixture implements DependentFixtureI
             $entity->setType($data['type']);
             $entity->setEnabled($data['enabled']);
             $entity->setDefaultUserOwner($this->getReference(LoadUser::USER));
-            $entity->setOrganization($this->getReference(LoadOrganization::ORGANIZATION));
+            $entity->setOrganization($this->getReference($data['organization']));
             $entity->setTransport(new PayPalSettings());
             $this->setReference($reference, $entity);
             $manager->persist($entity);
