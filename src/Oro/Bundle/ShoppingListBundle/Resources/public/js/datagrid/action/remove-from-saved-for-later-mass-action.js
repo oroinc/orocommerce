da4177e040498cb0f4a8a045776a4650453bf95a
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2026-05-29 10:07:52 +0200

    BB-26515: The dialog "Saved for later" doesn't appear after clicking on "Checkout" button (#45374)

--- src/Oro/Bundle/ShoppingListBundle/Resources/public/js/datagrid/action/remove-from-saved-for-later-mass-action.js
+++ src/Oro/Bundle/ShoppingListBundle/Resources/public/js/datagrid/action/remove-from-saved-for-later-mass-action.js
@@ -23,6 +23,7 @@ const RemoveFromSavedForLaterMassAction = SaveForLaterMassAction.extend({
 
         this._showAjaxSuccessMessage(data);
         mediator.trigger('products:removed-from-saved-for-later', {gridName: this.datagrid.name, data: data});
+        mediator.trigger('shopping-list:refresh');
     },
 
     /**
