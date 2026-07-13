2ef99d20927f0cb299c9d846cd5911c667dadcda
Mykhailo Kotliar <mkotliar@oroinc.com>
2026-07-13 20:43:20 +0300

    BB-27517: "External ID" field should be not visible by default (7.0) (#45870)

--- src/Oro/Bundle/OrderBundle/Migrations/Schema/OroOrderBundleInstaller.php
+++ src/Oro/Bundle/OrderBundle/Migrations/Schema/OroOrderBundleInstaller.php
@@ -48,7 +48,7 @@ class OroOrderBundleInstaller implements
     #[\Override]
     public function getMigrationVersion(): string
     {
-        return 'v7_0_2_1';
+        return 'v7_0_2_2';
     }
 
     #[\Override]
@@ -184,7 +184,9 @@ class OroOrderBundleInstaller implements
         $table->addColumn('external_id', 'string', ['length' => 36, 'notnull' => false, OroOptions::KEY => [
             ExtendOptionsManager::MODE_OPTION => ConfigModel::MODE_READONLY,
             'extend' => ['is_extend' => true, 'owner' => ExtendScope::OWNER_CUSTOM],
-            'datagrid' => ['is_visible' => DatagridScope::IS_VISIBLE_HIDDEN],
+            'datagrid' => ['is_visible' => DatagridScope::IS_VISIBLE_FALSE],
+            'form' => ['is_enabled' => false],
+            'view' => ['is_displayable' => false],
             'importexport' => ['excluded' => true],
             'dataaudit' => ['auditable' => true],
             'email' => ['available_in_template' => true],
